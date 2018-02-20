fa4e232d73bf235f62bd540bc3ae97ce042c4b2e
Greg Fitzgerald <garious@gmail.com>
2018-02-20 16:26:11 -0700

    Add serialization/deseriation support to event log
    See bincode and serde_json for usage:
    https://github.com/TyOverby/bincode
    
    Fixes #1

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,9 @@
 #![cfg_attr(feature = "unstable", feature(test))]
 pub mod log;
 pub mod historian;
-extern crate digest;
+extern crate generic_array;
 extern crate rayon;
+extern crate serde;
+#[macro_use]
+extern crate serde_derive;
 extern crate sha2;
