c6134cc25be834413c606f6404aede34caf062f7
Greg Fitzgerald <garious@gmail.com>
2018-02-26 17:01:22 -0700

    Allow the historian to track ownership of any type of data

--- src/bin/demo.rs
+++ src/bin/demo.rs
@@ -6,7 +6,7 @@ use std::thread::sleep;
 use std::time::Duration;
 use std::sync::mpsc::SendError;
 
-fn create_log(hist: &Historian) -> Result<(), SendError<Event<Sha256Hash>>> {
+fn create_log(hist: &Historian<Sha256Hash>) -> Result<(), SendError<Event<Sha256Hash>>> {
     sleep(Duration::from_millis(15));
     let data = Sha256Hash::default();
     hist.sender.send(Event::Discovery { data })?;
