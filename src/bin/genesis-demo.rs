b6d8f737ca0d444e5bfc0e75fa879164222a23a9
Greg Fitzgerald <garious@gmail.com>
2018-03-07 16:58:01 -0700

    Introducing, the mint
    Use the mint to pair a new private key with new tokens.

--- src/bin/genesis-demo.rs
+++ src/bin/genesis-demo.rs
@@ -7,6 +7,7 @@ use silk::transaction::Transaction;
 use silk::log::create_entries;
 use silk::signature::{KeyPair, KeyPairUtil, PublicKey};
 use silk::hash::Hash;
+use std::io::stdin;
 
 fn transfer(from: &KeyPair, (to, tokens): (PublicKey, i64), last_id: Hash) -> Event {
     Event::Transaction(Transaction::new(&from, to, tokens, last_id))
@@ -16,15 +17,14 @@ fn main() {
     let alice = (KeyPair::new().pubkey(), 200);
     let bob = (KeyPair::new().pubkey(), 100);
 
-    let gen = Genesis::new(500);
+    let gen: Genesis = serde_json::from_reader(stdin()).unwrap();
     let from = gen.keypair();
     let seed = gen.seed();
     let mut events = gen.create_events();
     events.push(transfer(&from, alice, seed));
     events.push(transfer(&from, bob, seed));
 
-    let entries = create_entries(&seed, events);
-    for entry in entries {
+    for entry in create_entries(&seed, events) {
         println!("{}", serde_json::to_string(&entry).unwrap());
     }
 }
--- src/bin/genesis.rs
+++ src/bin/genesis.rs
@@ -1,18 +1,14 @@
 //! A command-line executable for generating the chain's genesis block.
 
-extern crate ring;
 extern crate serde_json;
 extern crate silk;
 
 use silk::genesis::Genesis;
-use silk::log::verify_slice;
 use std::io::stdin;
 
 fn main() {
     let gen: Genesis = serde_json::from_reader(stdin()).unwrap();
-    let entries = gen.create_entries();
-    verify_slice(&entries, &entries[0].id);
-    for x in entries {
+    for x in gen.create_entries() {
         println!("{}", serde_json::to_string(&x).unwrap());
     }
 }
