01359717697f4beba4d169cb9a7965370f3be2e1
Anatoly Yakovenko <aeyakovenko@gmail.com>
2018-03-07 13:47:13 -0800

    Fast UdpSocket reader
    * message needs to fit into 256 bytes
    * allocator to keep track of blocks of messages
    * udp socket receiver server that fills up the block as fast as possible
    * udp socket sender server that sends out the block as fast as possible

--- src/lib.rs
+++ src/lib.rs
@@ -8,12 +8,16 @@ pub mod log;
 pub mod mint;
 pub mod logger;
 pub mod historian;
+pub mod streamer;
 pub mod accountant;
 pub mod accountant_skel;
 pub mod accountant_stub;
+pub mod result;
 extern crate bincode;
 extern crate chrono;
 extern crate generic_array;
+#[macro_use]
+extern crate log as logging;
 extern crate rayon;
 extern crate ring;
 extern crate serde;
@@ -22,3 +26,7 @@ extern crate serde_derive;
 extern crate serde_json;
 extern crate sha2;
 extern crate untrusted;
+
+#[cfg(test)]
+#[macro_use]
+extern crate matches;
