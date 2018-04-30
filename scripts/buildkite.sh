7983439adab6b355b8f0e4951080729908c50775
Anatoly Yakovenko <aeyakovenko@gmail.com>
2018-04-29 23:43:43 -0700

    move CI specific scripts to solana-labs/buildkite repo
    Former-commit-id: 77dd1bdd4a314975e36eaee0cbd8661ae526c2f9

--- scripts/buildkite.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-export LD_LIBRARY_PATH=/usr/local/cuda/lib64
-source $HOME/.cargo/env
-export PATH=$PATH:/usr/local/cuda/bin
-cp /tmp/libcuda_verify_ed25519.a .
-cargo test --features=cuda
