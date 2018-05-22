5841e040e25fc34a681757b87dc737316343b6d8
Anatoly Yakovenko <aeyakovenko@gmail.com>
2018-05-22 11:08:13 -0700

    rebase
    fmt
    
    cleanup

---
+++ multinode-demo/validator.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+cd /home/ubuntu/solana
+git pull
+scp  ubuntu@18.206.1.146:~/solana/leader.json .
+scp  ubuntu@18.206.1.146:~/solana/genesis.log .
+export RUST_LOG=solana::crdt=trace
+cat genesis.log | cargo run --release --bin solana-testnode -- -s replicator.json -r leader.json -b 9000 -d
