75e69eecfa06b9f381b5f56b6e5de8106ba3d69c
Stephen Akridge <sakridge@gmail.com>
2018-05-23 16:01:28 -0700

    Fix nightly bench

--- src/signature.rs
+++ src/signature.rs
@@ -129,13 +129,11 @@ mod bench {
 
     use self::test::Bencher;
     use super::*;
-    use std::collections::HashSet;
-    use std::iter::FromIterator;
 
     #[bench]
     fn bench_gen_keys(b: &mut Bencher) {
         let seed: &[_] = &[1, 2, 3, 4];
         let rnd = GenKeys::new(seed);
-        b.iter(|| rnd.gen_n_keys(1000));
+        b.iter(|| rnd.gen_n_keypairs(1000));
     }
 }
