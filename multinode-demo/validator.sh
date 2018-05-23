89924a38fff3f82164a889905f0915e77b0161ce
Anatoly Yakovenko <anatoly@solana.com>
2018-05-23 22:45:48 +0000

    cuda

--- multinode-demo/validator.sh
+++ multinode-demo/validator.sh
@@ -4,4 +4,4 @@ git pull
 scp  ubuntu@18.206.1.146:~/solana/leader.json .
 scp  ubuntu@18.206.1.146:~/solana/genesis.log .
 export RUST_LOG=solana::crdt=trace
-cat genesis.log | cargo run --release --bin solana-testnode -- -s replicator.json -r leader.json -b 9000 -d
+cat genesis.log | cargo run --release --bin --features=cuda solana-testnode -- -s replicator.json -r leader.json -b 9000 -d
