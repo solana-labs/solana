1022f1b0c6bc5019a1695665365966f3c4871da5
Anatoly Yakovenko <anatoly@solana.com>
2018-05-24 23:26:05 +0000

    logs

--- multinode-demo/leader.sh
+++ multinode-demo/leader.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd /home/ubuntu/solana
 git pull
-export RUST_LOG=solana::crdt=info
+export RUST_LOG=solana=info
 cat genesis.log | cargo run --release --features cuda --bin solana-testnode -- -s leader.json -b 8000 -d | grep INFO
 #cat genesis.log | cargo run --release --bin solana-testnode -- -s leader.json -b 8000 -d
