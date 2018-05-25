b0eab8729f776a8580a597ffe49001fb6cfc545b
Stephen Akridge <sakridge@gmail.com>
2018-05-25 10:56:43 -0700

    Add erasure ci script

---
+++ ci/test-erasure.sh
@@ -0,0 +1,23 @@
+#!/bin/bash -e
+
+cd $(dirname $0)/..
+
+if [[ -z "$libgf_complete_URL" ]]; then
+  echo libgf_complete_URL undefined
+  exit 1
+fi
+
+if [[ -z "$libJerasure_URL" ]]; then
+  echo libJerasure_URL undefined
+  exit 1
+fi
+
+export LD_LIBRARY_PATH=/usr/local/cuda/lib64
+export PATH=$PATH:/usr/local/cuda/bin
+curl -X GET -o libJerasure.so "$libJerasure_URL"
+curl -X GET -o libgf_complete.so "$libgf_complete"
+
+source $HOME/.cargo/env
+cargo test --features="erasure"
+
+exit 0
