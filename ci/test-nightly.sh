a391d2a379544e551f939f22cf3c50cfadd44252
Greg Fitzgerald <garious@gmail.com>
2018-05-25 20:47:06 -0600

    Only check formatting in stable build

--- ci/test-nightly.sh
+++ ci/test-nightly.sh
@@ -6,7 +6,6 @@ rustc --version
 cargo --version
 
 rustup component add rustfmt-preview
-cargo fmt -- --write-mode=diff
 cargo build --verbose --features unstable
 cargo test --verbose --features unstable
 
