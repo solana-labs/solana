931bdbd5cd2ec7f353a7e9db79322621eb5842b1
Greg Fitzgerald <garious@gmail.com>
2018-05-26 20:25:44 -0600

    Fix typo

--- multinode-demo/leader.sh
+++ multinode-demo/leader.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 export RUST_LOG=solana=info
 sudo sysctl -w net.core.rmem_max=26214400
-cat genesis.log leader.log | cargo run --release --features cuda --bin solana-testnode -- -s leader.json -l leader.json -b 8000 -d 2>&1 | tee leader-tee.log
+cat genesis.log leader.log | cargo run --release --features cuda --bin solana-fullnode -- -s leader.json -l leader.json -b 8000 -d 2>&1 | tee leader-tee.log
