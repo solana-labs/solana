3058f80489cb3aab865ee4a183eacd2698fdda7c
Ubuntu <ubuntu@ip-172-31-21-30.us-east-2.compute.internal>
2018-05-30 18:03:28 +0000

    log

--- multinode-demo/client.sh
+++ multinode-demo/client.sh
@@ -13,4 +13,4 @@ rsync -v -e ssh "$LEADER:~/solana/leader.json" .
 rsync -v -e ssh "$LEADER:~/solana/mint-demo.json" .
 
 cargo run --release --bin solana-client-demo -- \
-  -l leader.json -c 8100 -n 1 < mint-demo.json
+  -l leader.json -c 8100 -n 1 < mint-demo.json 2>&1 | tee client.log
