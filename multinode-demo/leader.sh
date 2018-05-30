f6d72881b0a0aab5feae84f856376c59208de58b
Ubuntu <ubuntu@ip-172-31-14-94.us-east-2.compute.internal>
2018-05-30 14:17:04 +0000

    fix

--- multinode-demo/leader.sh
+++ multinode-demo/leader.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 export RUST_LOG=solana=info
 sudo sysctl -w net.core.rmem_max=26214400
-cat genesis.log leader.log | cargo run --release --features cuda --bin solana-fullnode -- -s leader.json -l leader.json -b 8000 -d 2>&1 | tee leader-tee.log
+rm leader.log
+export RUST_BACKTRACE=1
+cat genesis.log leader.log | cargo run --features cuda --bin solana-fullnode -- -s leader.json -l leader.json -b 8000 -d 2>&1 | tee leader-tee.log
