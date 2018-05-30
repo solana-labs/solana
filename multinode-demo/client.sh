68955bfcf4ab54df6e90594524acc482275c7994
Stephen Akridge <sakridge@gmail.com>
2018-05-30 12:02:13 -0700

    Change multinode script argument to leader path
    Some may have cloned their code in different place

--- multinode-demo/client.sh
+++ multinode-demo/client.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 
 if [[ -z "$1" ]]; then
-  echo "usage: $0 [leader machine]"
+  echo "usage: $0 [network path to solana repo on leader machine]"
   exit 1
 fi
 
@@ -9,8 +9,8 @@ LEADER="$1"
 
 set -x
 export RUST_LOG=solana=info
-rsync -v -e ssh "$LEADER:~/solana/leader.json" .
-rsync -v -e ssh "$LEADER:~/solana/mint-demo.json" .
+rsync -v -e ssh "$LEADER/leader.json" .
+rsync -v -e ssh "$LEADER/mint-demo.json" .
 
 cargo run --release --bin solana-client-demo -- \
   -l leader.json -c 8100 -n 1 < mint-demo.json 2>&1 | tee client.log
