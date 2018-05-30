5c3c843903c94f2c02be47132f20d357e5354a60
Anatoly Yakovenko <aeyakovenko@gmail.com>
2018-05-30 10:00:58 -0700

    comments

--- multinode-demo/leader.sh
+++ multinode-demo/leader.sh
@@ -2,5 +2,4 @@
 export RUST_LOG=solana=info
 sudo sysctl -w net.core.rmem_max=26214400
 rm leader.log
-export RUST_BACKTRACE=1
-cat genesis.log leader.log | cargo run --features cuda --bin solana-fullnode -- -s leader.json -l leader.json -b 8000 -d 2>&1 | tee leader-tee.log
+cat genesis.log leader.log | cargo run --features=cuda,erasure --bin solana-fullnode -- -s leader.json -l leader.json -b 8000 -d 2>&1 | tee leader-tee.log
