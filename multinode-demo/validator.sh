2888e45fea97ca6e424f8f1b7c55d1b4ad5cce23
Anatoly Yakovenko <aeyakovenko@gmail.com>
2018-05-30 10:00:58 -0700

    comments

--- multinode-demo/validator.sh
+++ multinode-demo/validator.sh
@@ -20,5 +20,5 @@ export RUST_LOG=solana=info
 sudo sysctl -w net.core.rmem_max=26214400
 
 cat genesis.log leader.log | \
-  cargo run --release --features cuda --bin solana-fullnode -- \
+  cargo run --release --features=cuda,erasure --bin solana-fullnode -- \
     -l validator.json -s validator.json -v leader.json -b 9000 -d 2>&1 | tee validator-tee.log
