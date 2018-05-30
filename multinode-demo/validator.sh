78ae1d43c8df4143ea6c01c010a0d679ebbba017
Anatoly Yakovenko <aeyakovenko@gmail.com>
2018-05-30 10:46:48 -0700

    cleanup

--- multinode-demo/validator.sh
+++ multinode-demo/validator.sh
@@ -12,13 +12,12 @@ set -x
 rsync -v -e ssh "$LEADER:~/solana/mint-demo.json" .
 rsync -v -e ssh "$LEADER:~/solana/leader.json" .
 rsync -v -e ssh "$LEADER:~/solana/genesis.log" .
-rsync -v -e ssh "$LEADER:~/solana/leader.log" .
 rsync -v -e ssh "$LEADER:~/solana/libcuda_verify_ed25519.a" .
 
 export RUST_LOG=solana=info
 
 sudo sysctl -w net.core.rmem_max=26214400
 
-cat genesis.log leader.log | \
+cat genesis.log | \
   cargo run --release --features=cuda --bin solana-fullnode -- \
     -l validator.json -s validator.json -v leader.json -b 9000 -d 2>&1 | tee validator-tee.log
