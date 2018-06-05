8eb76ab2a55cfeb651fa59fbe53d5058d750ccc9
Stephen Akridge <sakridge@gmail.com>
2018-06-05 13:30:36 -0700

    Fix shellcheck

--- ci/test-erasure.sh
+++ ci/test-erasure.sh
@@ -2,14 +2,14 @@
 
 set -o xtrace
 
-cd $(dirname $0)/..
+cd "$(dirname $0)/.."
 
-if [[ -z "$libgf_complete_URL" ]]; then
+if [[ -z "${libgf_complete_URL:-}" ]]; then
   echo libgf_complete_URL undefined
   exit 1
 fi
 
-if [[ -z "$libJerasure_URL" ]]; then
+if [[ -z "${libJerasure_URL:-}" ]]; then
   echo libJerasure_URL undefined
   exit 1
 fi
@@ -22,6 +22,7 @@ ln -s libJerasure.so libJerasure.so.2.0.0
 ln -s libgf_complete.so libgf_complete.so.1.0.0
 export LD_LIBRARY_PATH=$PWD:$LD_LIBRARY_PATH
 
+# shellcheck disable=SC1090    # <-- shellcheck can't follow ~
 source $HOME/.cargo/env
 cargo test --features="erasure"
 
