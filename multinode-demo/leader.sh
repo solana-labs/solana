d9cbbcb46a53e3c4a914b8cd6a0a2e91af36815c
Anatoly Yakovenko <anatoly@solana.com>
2018-06-07 22:02:08 +0000

    terrible scripts

--- multinode-demo/leader.sh
+++ multinode-demo/leader.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 export RUST_LOG=solana=info
 sudo sysctl -w net.core.rmem_max=26214400
-cargo run --release --bin solana-fullnode -- -l leader.json < genesis.log
+cargo run --release --bin solana-fullnode --features=cuda -- -l leader.json < genesis.log
