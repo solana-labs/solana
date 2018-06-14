ec713c18c48547c78b9987a2c6e55f407fc6fab7
Stephen Akridge <sakridge@gmail.com>
2018-06-14 10:35:59 -0700

    Revert client.sh script to use cargo

--- multinode-demo/client.sh
+++ multinode-demo/client.sh
@@ -16,6 +16,5 @@ export RUST_LOG=solana=info
 rsync -v -e ssh "$LEADER/leader.json" .
 rsync -v -e ssh "$LEADER/mint-demo.json" .
 
-#cargo run --release --bin solana-client-demo -- \
-./target/release/solana-client-demo \
+cargo run --release --bin solana-client-demo -- \
   -n $COUNT -l leader.json -d < mint-demo.json 2>&1 | tee client.log
