fc51cff5490927ae69d744f224a40829d224c8e6
Rob Walker <rob@solana.com>
2018-06-14 17:11:55 -0700

    clean up demo bash scripts
     * allow other level of RUST logging
     * avoid "echo" in favor of printf (builtin)
     * single quotes for literals, double quotes for variables

--- multinode-demo/client.sh
+++ multinode-demo/client.sh
@@ -1,20 +1,18 @@
 #!/bin/bash -e
 
-if [[ -z "$1" ]]; then
-  echo "usage: $0 [network path to solana repo on leader machine] [number of nodes in the network if greater then 1]"
+if [[ -z $1 ]]; then
+  printf 'usage: %s [network path to solana repo on leader machine] [number of nodes in the network if greater then 1]' "$0"
   exit 1
 fi
 
-LEADER="$1"
-COUNT="$2"
-if [[ -z "$2" ]]; then
-    COUNT=1
-fi
+LEADER=$1
+COUNT=${2:-1}
 
 set -x
-export RUST_LOG=solana=info
-rsync -v -e ssh "$LEADER/leader.json" .
-rsync -v -e ssh "$LEADER/mint-demo.json" .
+rsync -v -e ssh "$LEADER"/{leader.json,mint-demo.json} .
+
+# if RUST_LOG is unset, default to info
+export RUST_LOG=${RUST_LOG:-solana=info}
 
 cargo run --release --bin solana-client-demo -- \
-  -n $COUNT -l leader.json -d < mint-demo.json 2>&1 | tee client.log
+  -n "$COUNT" -l leader.json -d < mint-demo.json 2>&1 | tee client.log
