0de48d42ff608070a2e8d135b59263161a5e91ae
Stephen Akridge <sakridge@gmail.com>
2018-06-18 13:04:01 -0700

    Fix cargo bench nightly

--- src/signature.rs
+++ src/signature.rs
@@ -131,7 +131,8 @@ mod bench {
 
     #[bench]
     fn bench_gen_keys(b: &mut Bencher) {
-        let seed: &[_] = &[1, 2, 3, 4];
+        let mut seed = [0u8; 32];
+        seed[0..3].copy_from_slice(&[1, 2, 3, 4]);
         let rnd = GenKeys::new(seed);
         b.iter(|| rnd.gen_n_keypairs(1000));
     }
