1c97bf50b6f56fde6b20014208c22d5c3ba2c109
Greg Fitzgerald <greg@solana.com>
2018-06-19 09:37:28 -0700

    Fix nightly
    No longer ignore failures in the nightly build.

--- src/signature.rs
+++ src/signature.rs
@@ -131,9 +131,7 @@ mod bench {
 
     #[bench]
     fn bench_gen_keys(b: &mut Bencher) {
-        let mut seed = [0u8; 32];
-        seed[0..3].copy_from_slice(&[1, 2, 3, 4]);
-        let rnd = GenKeys::new(seed);
+        let rnd = GenKeys::new([0u8; 32]);
         b.iter(|| rnd.gen_n_keypairs(1000));
     }
 }
