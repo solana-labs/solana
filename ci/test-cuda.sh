89fc0ad7a98151e2a0a4317d30cc48a560c1eb9a
Michael Vines <mvines@gmail.com>
2018-06-20 15:11:59 -0700

    Add convenience script to download performance libraries

--- ci/test-cuda.sh
+++ ci/test-cuda.sh
@@ -2,17 +2,10 @@
 
 cd "$(dirname "$0")/.."
 
-LIB=libcuda_verify_ed25519.a
-if [[ ! -r $LIB ]]; then
-  if [[ -z "${libcuda_verify_ed25519_URL:-}" ]]; then
-    echo "$0 skipped.  Unable to locate $LIB"
-    exit 0
-  fi
+./fetch-perf-libs.sh
 
-  export LD_LIBRARY_PATH=/usr/local/cuda/lib64
-  export PATH=$PATH:/usr/local/cuda/bin
-  curl -X GET -o $LIB "$libcuda_verify_ed25519_URL"
-fi
+export LD_LIBRARY_PATH=/usr/local/cuda/lib64
+export PATH=$PATH:/usr/local/cuda/bin
 
 # shellcheck disable=SC1090    # <-- shellcheck can't follow ~
 source ~/.cargo/env
--- ci/test-erasure.sh
+++ ci/test-erasure.sh
@@ -4,22 +4,8 @@ set -o xtrace
 
 cd "$(dirname "$0")/.."
 
-if [[ -z "${libgf_complete_URL:-}" ]]; then
-  echo libgf_complete_URL undefined
-  exit 1
-fi
+./fetch-perf-libs.sh
 
-if [[ -z "${libJerasure_URL:-}" ]]; then
-  echo libJerasure_URL undefined
-  exit 1
-fi
-
-curl -X GET -o libJerasure.so "$libJerasure_URL"
-curl -X GET -o libgf_complete.so "$libgf_complete_URL"
-
-ln -s libJerasure.so libJerasure.so.2
-ln -s libJerasure.so libJerasure.so.2.0.0
-ln -s libgf_complete.so libgf_complete.so.1.0.0
 export LD_LIBRARY_PATH=$PWD:$LD_LIBRARY_PATH
 
 # shellcheck disable=SC1090    # <-- shellcheck can't follow ~
