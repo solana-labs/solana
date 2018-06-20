3658d3cd7532f65cee9d1521e724d09ae8d3130f
Pankaj Garg <pankaj@Pankajs-MacBook-Pro.local>
2018-06-20 14:30:32 -0700

    Updated setup and validator scripts
    * Setup will us -b to set validator ports
    * Validator script fixed to use correct log file

--- multinode-demo/setup.sh
+++ multinode-demo/setup.sh
@@ -6,4 +6,4 @@ cargo run --release --bin solana-mint-demo <<<"${TOKENS}" > mint-demo.json
 cargo run --release --bin solana-genesis-demo < mint-demo.json > genesis.log
 
 cargo run --release --bin solana-fullnode-config -- -d > leader.json
-cargo run --release --bin solana-fullnode-config -- -d > validator.json
+cargo run --release --bin solana-fullnode-config -- -b 9000 -d > validator.json
