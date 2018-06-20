e90e7b734dc49b090c8e8ae6123efe55a1eae20e
Pankaj Garg <pankaj@Pankajs-MacBook-Pro.local>
2018-06-20 14:30:32 -0700

    Updated setup and leader scripts
    * Setup will us -b to set validator ports
    * Leader script fixed to append .log to the log file
    * Updated readme file

--- multinode-demo/leader.sh
+++ multinode-demo/leader.sh
@@ -7,4 +7,4 @@ set -x
 [[ $(uname) = Linux ]] && sudo sysctl -w net.core.rmem_max=26214400
 
 cargo run --release --bin solana-fullnode -- \
-      -l leader.json < genesis.log tx-*.log > tx-"$(date -u +%Y%m%d%k%M%S%N)"
+      -l leader.json < genesis.log tx-*.log > tx-"$(date -u +%Y%m%d%k%M%S%N)".log
