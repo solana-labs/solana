7e5f0471b5f85a742d689c999be0be8aca87e65a
Michael Vines <mvines@gmail.com>
2018-06-21 13:19:46 -0700

    Add simple CUDA version check, warn on mismatch

--- fetch-perf-libs.sh
+++ fetch-perf-libs.sh
@@ -17,6 +17,21 @@ fi
   tar zxvf solana-perf.tgz
 )
 
+if [[ -r /usr/local/cuda/version.txt && -r cuda-version.txt ]]; then
+  if ! diff /usr/local/cuda/version.txt cuda-version.txt > /dev/null; then
+    echo ==============================================
+    echo Warning: possible CUDA version mismatch
+    echo
+    echo "Expected version: $(cat cuda-version.txt)"
+    echo "Detected version: $(cat /usr/local/cuda/version.txt)"
+    echo ==============================================
+  fi
+else
+  echo ==============================================
+  echo Warning: unable to validate CUDA version
+  echo ==============================================
+fi
+
 echo "Downloaded solana-perf version: $(cat solana-perf-HEAD.txt)"
 
 exit 0
