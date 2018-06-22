3747d8caadb09f1a0a59139baad87d7d3e352cac
Pankaj Garg <pankaj@Pankajs-MacBook-Pro.local>
2018-06-22 11:37:42 -0700

    Run multiple instances from same workspace
    * Support running leader and validators from multiple machines
      using the same NFS mounted workspace.
    * Changes to setup, leader and validator scripts

--- multinode-demo/setup.sh
+++ multinode-demo/setup.sh
@@ -5,5 +5,11 @@ TOKENS=${1:-1000000000}
 cargo run --release --bin solana-mint-demo <<<"${TOKENS}" > mint-demo.json
 cargo run --release --bin solana-genesis-demo < mint-demo.json > genesis.log
 
-cargo run --release --bin solana-fullnode-config -- -d > leader.json
-cargo run --release --bin solana-fullnode-config -- -b 9000 -d > validator.json
+IPADDR="$(ifconfig  | grep 'inet addr:'| grep -v '127.0.0.1' | cut -d: -f2 | awk '{ print $1}')"
+
+if [ -z "$IPADDR" ]; then
+    IPADDR="$(ifconfig  | grep 'inet '| grep -v '127.0.0.1' | cut -d: -f2 | awk '{ print $2}')"
+fi
+
+cargo run --release --bin solana-fullnode-config -- -d > leader-$IPADDR.json
+cargo run --release --bin solana-fullnode-config -- -b 9000 -d > validator-$IPADDR.json
