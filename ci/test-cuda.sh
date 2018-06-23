ec86b1dffab5f966e580f52b7b87920863d3ef32
Michael Vines <mvines@gmail.com>
2018-06-23 00:23:25 -0700

    Adapt to GCP-based CI

--- ci/test-cuda.sh
+++ ci/test-cuda.sh
@@ -7,8 +7,6 @@ cd "$(dirname "$0")/.."
 export LD_LIBRARY_PATH=/usr/local/cuda/lib64
 export PATH=$PATH:/usr/local/cuda/bin
 
-# shellcheck disable=SC1090    # <-- shellcheck can't follow ~
-source ~/.cargo/env
 export RUST_BACKTRACE=1
 cargo test --features=cuda
 
