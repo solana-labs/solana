c73a17398de8f9b802d0fef8b8b0955943ed7a68
Greg Fitzgerald <greg@solana.com>
2018-06-24 11:32:06 -0600

    Use llvm-cov instead of gcov
    @marco-c called this a hack, but since grcov isn't working
    out-of-the-box (panics on call to gcov), we'll take a stab at using
    llvm-cov.

--- ci/coverage.sh
+++ ci/coverage.sh
@@ -12,7 +12,7 @@ ls -l target/cov/report/index.html
 if [[ -z "$CODECOV_TOKEN" ]]; then
   echo CODECOV_TOKEN undefined
 else
-  bash <(curl -s https://codecov.io/bash)
+  bash <(curl -s https://codecov.io/bash) -x 'llvm-cov gcov'
 fi
 
 exit 0
