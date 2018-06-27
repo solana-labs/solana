a167d0d33148b4bb602b449cc030078a00e49c7c
Tyera Eulberg <teulberg@gmail.com>
2018-06-27 00:15:43 -0600

    CI cleanup

--- multinode-demo/simple-client.sh
+++ multinode-demo/simple-client.sh
@@ -4,11 +4,11 @@
 #
 
 here=$(dirname "$0")
+# shellcheck source=multinode-demo/common.sh
 source "$here"/common.sh
 SOLANA_CONFIG_DIR=config-client-demo
 
 leader=${1:-${here}/..}  # Default to local solana repo
-count=${2:-1}
 
 rsync_leader_url=$(rsync_url "$leader")
 
@@ -16,5 +16,6 @@ set -ex
 mkdir -p $SOLANA_CONFIG_DIR
 rsync -vPz "$rsync_leader_url"/config/leader.json $SOLANA_CONFIG_DIR/
 
+# shellcheck disable=SC2086 # $solana_simple_client_demo should not be quoted
 exec $solana_simple_client_demo \
   -l $SOLANA_CONFIG_DIR/leader.json -d
