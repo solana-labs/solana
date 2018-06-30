c06582ba40280a452d46b74d52a43099c4778e7f
Greg Fitzgerald <greg@solana.com>
2018-06-29 22:53:06 -0600

    Wallet no longer uses global mint.json

--- multinode-demo/wallet.sh
+++ multinode-demo/wallet.sh
@@ -16,7 +16,6 @@ rsync_leader_url=$(rsync_url "$leader")
 set -ex
 mkdir -p $SOLANA_CONFIG_DIR
 rsync -vPz "$rsync_leader_url"/config/leader.json $SOLANA_CONFIG_DIR/
-rsync -vPz "$rsync_leader_url"/config/mint.json $SOLANA_CONFIG_DIR/
 
 # shellcheck disable=SC2086 # $solana_wallet should not be quoted
 exec $solana_wallet \
