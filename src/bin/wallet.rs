09e871b8dea5d3ddc25171b0330e676f83832683
Greg Fitzgerald <greg@solana.com>
2018-07-02 20:15:40 -0600

    Fix wallet doc

--- src/bin/wallet.rs
+++ src/bin/wallet.rs
@@ -282,7 +282,7 @@ fn display_actions() {
     println!("  address   Get your public key");
     println!("  balance   Get your account balance");
     println!("  airdrop   Request a batch of tokens");
-    println!("  pay       Spend your tokens as fast as possible");
+    println!("  pay       Send tokens to a public key");
     println!("  confirm   Confirm your last payment by signature");
     println!("");
 }
