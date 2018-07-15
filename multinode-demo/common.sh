79b3564a266cd53bd51fa7611f2e4f0ef4fd70da
Michael Vines <mvines@gmail.com>
2018-07-14 21:22:14 -0700

    Log metrics params to stderr
    Keep stdout clean for the actual program.  This is a specific concern for the
    wallet command, where there exists tests that capture stdout from the wallet to
    confirm transactions.

--- multinode-demo/common.sh
+++ multinode-demo/common.sh
@@ -84,29 +84,29 @@ configure_metrics() {
   for param in "${metrics_params[@]}"; do
     IFS='=' read -r -a pair <<< "$param"
     if [[ "${#pair[@]}" != 2 ]]; then
-      echo Error: invalid metrics parameter: "$param"
+      echo Error: invalid metrics parameter: "$param" >&2
     else
       declare name="${pair[0]}"
       declare value="${pair[1]}"
       case "$name" in
       host)
         export INFLUX_HOST="$value"
-        echo INFLUX_HOST="$INFLUX_HOST"
+        echo INFLUX_HOST="$INFLUX_HOST" >&2
         ;;
       db)
         export INFLUX_DATABASE="$value"
-        echo INFLUX_DATABASE="$INFLUX_DATABASE"
+        echo INFLUX_DATABASE="$INFLUX_DATABASE" >&2
         ;;
       u)
         export INFLUX_USERNAME="$value"
-        echo INFLUX_USERNAME="$INFLUX_USERNAME"
+        echo INFLUX_USERNAME="$INFLUX_USERNAME" >&2
         ;;
       p)
         export INFLUX_PASSWORD="$value"
-        echo INFLUX_PASSWORD="********"
+        echo INFLUX_PASSWORD="********" >&2
         ;;
       *)
-        echo Error: Unknown metrics parameter name: "$name"
+        echo Error: Unknown metrics parameter name: "$name" >&2
         ;;
       esac
     fi
