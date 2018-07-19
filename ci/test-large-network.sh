41951ab52d74855a68bc88683b6dabb44bc0545a
Anatoly Yakovenko <aeyakovenko@gmail.com>
2018-07-18 21:45:22 -0700

    tune networking for test

--- ci/test-large-network.sh
+++ ci/test-large-network.sh
@@ -2,7 +2,11 @@
 
 cd "$(dirname "$0")/.."
 
-export RUST_BACKTRACE=1
+# shellcheck source=multinode-demo/common.sh
+source "$here"/common.sh
 
+tune_networking
+
+export RUST_BACKTRACE=1
 set -x
 exec cargo test --release test_multi_node_dynamic_network -- --ignored
