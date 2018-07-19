dff1a001ab03eb0579df8573ed3db2ddc000092e
Anatoly Yakovenko <aeyakovenko@gmail.com>
2018-07-18 22:11:34 -0700

    fixup!

--- ci/test-large-network.sh
+++ ci/test-large-network.sh
@@ -1,13 +1,9 @@
 #!/bin/bash -e
-
 here=$(dirname "$0")
+sudo sysctl -w net.core.rmem_max=26214400 1>/dev/null 2>/dev/null
+sudo sysctl -w net.core.rmem_default=26214400 1>/dev/null 2>/dev/null
 
-# shellcheck source=multinode-demo/common.sh
-source $here/../multinode-demo/common.sh
-
-tune_networking
 
 export RUST_BACKTRACE=1
 set -x
-cd $here/..
 exec cargo test --release test_multi_node_dynamic_network -- --ignored
