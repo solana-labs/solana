d9c0b8cfc46ba83f1ced92172b79bcd65635b28f
Anatoly Yakovenko <aeyakovenko@gmail.com>
2018-07-18 21:32:10 -0700

    fixup!

--- ci/test-large-network.sh
+++ ci/test-large-network.sh
@@ -5,4 +5,4 @@ cd "$(dirname "$0")/.."
 export RUST_BACKTRACE=1
 
 set -x
-exec cargo test --release --ignored test_multi_node_dynamic_network
+exec cargo test --release test_multi_node_dynamic_network -- --ignored
