e35ebf669c53a01fab620a499c206ee9918ad4b5
Anatoly Yakovenko <aeyakovenko@gmail.com>
2018-07-18 22:08:49 -0700

    fix bash

--- ci/test-large-network.sh
+++ ci/test-large-network.sh
@@ -1,12 +1,13 @@
 #!/bin/bash -e
 
-cd "$(dirname "$0")/.."
+here=$(dirname "$0")
 
 # shellcheck source=multinode-demo/common.sh
-source multinode-demo/common.sh
+source $here/../multinode-demo/common.sh
 
 tune_networking
 
 export RUST_BACKTRACE=1
 set -x
+cd $here/..
 exec cargo test --release test_multi_node_dynamic_network -- --ignored
