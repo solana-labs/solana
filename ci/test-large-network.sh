fece91c4d16799f8dbcb2841f4cd10b246047985
anatoly yakovenko <anatoly@solana.com>
2018-07-19 07:50:44 -0700

    Test multi node dynamic network ci (#696)
    Buildkite automation for multinode test.  This test is ignored by default because it requires a large cpu machine to run.

---
+++ ci/test-large-network.sh
@@ -0,0 +1,8 @@
+#!/bin/bash -e
+
+here=$(dirname "$0")
+cd "$here"/..
+export RUST_BACKTRACE=1
+export RUST_LOG=multinode=info
+set -x
+exec cargo test --release test_multi_node_dynamic_network -- --ignored
