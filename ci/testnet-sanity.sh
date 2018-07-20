a041c416b194f1f6c6e48d61b6845581a3c8076b
Michael Vines <mvines@gmail.com>
2018-07-20 08:50:08 -0700

    Generalize testnet deployment scripts

--- ci/testnet-sanity.sh
+++ ci/testnet-sanity.sh
@@ -6,19 +6,25 @@
 cd "$(dirname "$0")/.."
 source multinode-demo/common.sh
 
-TESTNET=$1
-if [[ -z $TESTNET ]]; then
-  TESTNET=testnet.solana.com
+NET_URL=$1
+if [[ -z $NET_URL ]]; then
+  NET_URL=testnet.solana.com
 fi
 
-echo "--- $TESTNET: wallet sanity"
-multinode-demo/test/wallet-sanity.sh $TESTNET
+EXPECTED_NODE_COUNT=$2
+if [[ -z $EXPECTED_NODE_COUNT ]]; then
+  EXPECTED_NODE_COUNT=50
+fi
 
-echo "--- $TESTNET: node count"
-if [[ $TESTNET = testnet.solana.com ]]; then
+
+echo "--- $NET_URL: wallet sanity"
+multinode-demo/test/wallet-sanity.sh $NET_URL
+
+echo "--- $NET_URL: node count"
+if [[ $NET_URL = testnet.solana.com ]]; then
   echo "TODO: Remove this block when a release > 0.7.0 is deployed"
 else
-  $solana_client_demo $TESTNET 50 -c  # <-- Expect to see at least 50 nodes
+  $solana_client_demo $NET_URL $EXPECTED_NODE_COUNT -c
 fi
 
 exit 0
