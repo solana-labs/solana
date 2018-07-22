7436d3e373ab1b78542c7dc260e3bb2a5e994db8
Michael Vines <mvines@gmail.com>
2018-07-21 23:15:36 -0700

    Add data point for testnet startup and shutdown

---
+++ ci/metrics_write_datapoint.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -e
+
+point=$1
+if [[ -z $point ]]; then
+  echo "Data point not specified"
+  exit 1
+fi
+
+echo "Influx data point: $point"
+if [[ -z $INFLUX_DATABASE || -z $INFLUX_USERNAME || -z $INFLUX_PASSWORD ]]; then
+  echo Influx user credentials not found
+  exit 0
+fi
+
+echo "https://metrics.solana.com:8086/write?db=${INFLUX_DATABASE}&u=${INFLUX_USERNAME}&p=${INFLUX_PASSWORD}" \
+  | xargs curl -XPOST --data-binary "$point"
