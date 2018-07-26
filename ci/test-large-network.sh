4f0c92b10a9181eed84016aae3295d096cb780b2
Rob Walker <rob@solana.com>
2018-07-25 20:37:27 -0700

    large network back to erasure

--- ci/test-large-network.sh
+++ ci/test-large-network.sh
@@ -3,8 +3,11 @@
 here=$(dirname "$0")
 cd "$here"/..
 
+./fetch-perf-libs.sh
+export LD_LIBRARY_PATH+=:$PWD
+
 export RUST_BACKTRACE=1
 export RUST_LOG=multinode=info
 
 set -x
-exec cargo test --release test_multi_node_dynamic_network -- --ignored
+exec cargo test --release --features=erasure test_multi_node_dynamic_network -- --ignored
