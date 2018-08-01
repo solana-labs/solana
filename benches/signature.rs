e9f8b5b9db15074f6bb6563029761a3fc22d9b37
Greg Fitzgerald <greg@solana.com>
2018-07-31 18:15:43 -0600

    Fix bench

--- benches/signature.rs
+++ benches/signature.rs
@@ -6,7 +6,7 @@ use criterion::{Bencher, Criterion};
 use solana::signature::GenKeys;
 
 fn bench_gen_keys(b: &mut Bencher) {
-    let rnd = GenKeys::new([0u8; 32]);
+    let mut rnd = GenKeys::new([0u8; 32]);
     b.iter(|| rnd.gen_n_keypairs(1000));
 }
 
