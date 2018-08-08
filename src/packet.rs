7d2d4303a41ffc49e8394cca1fcfa84db3d50fef
Stephen Akridge <sakridge@gmail.com>
2018-08-08 01:26:16 +0000

    Lower UDP data size to 64k - 128 bytes
    rust API gives errors for packets larger than ~65500 and
    wikipedia says 65507 is the max size, lowering this avoids the errors.

--- src/packet.rs
+++ src/packet.rs
@@ -20,7 +20,7 @@ pub type PacketRecycler = Recycler<Packets>;
 pub type BlobRecycler = Recycler<Blob>;
 
 pub const NUM_PACKETS: usize = 1024 * 8;
-pub const BLOB_SIZE: usize = 64 * 1024;
+pub const BLOB_SIZE: usize = (64 * 1024 - 128); // wikipedia says there should be 20b for ipv4 headers
 pub const BLOB_DATA_SIZE: usize = BLOB_SIZE - BLOB_HEADER_SIZE;
 pub const PACKET_DATA_SIZE: usize = 256;
 pub const NUM_BLOBS: usize = (NUM_PACKETS * PACKET_DATA_SIZE) / BLOB_SIZE;
