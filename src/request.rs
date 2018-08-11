96ed1f7e262601ab1a603fa726f43968967ce7f1
Anatoly Yakovenko <aeyakovenko@gmail.com>
2018-08-10 21:13:34 -0700

    Account type with state
    comments
    
    fixups!
    
    fixups!
    
    fixups for a real Result<> from get_balance()
    
    on 2nd thought, be more rigorous
    
    Merge branch 'rob-solana-accounts_with_state' into accounts_with_state
    
    update
    
    review comments
    
    comments
    
    get rid of option

--- src/request.rs
+++ src/request.rs
@@ -1,12 +1,13 @@
 //! The `request` module defines the messages for the thin client.
 
+use bank::Account;
 use hash::Hash;
 use signature::{Pubkey, Signature};
 
 #[cfg_attr(feature = "cargo-clippy", allow(large_enum_variant))]
 #[derive(Serialize, Deserialize, Debug, Clone, Copy)]
 pub enum Request {
-    GetBalance { key: Pubkey },
+    GetAccount { key: Pubkey },
     GetLastId,
     GetTransactionCount,
     GetSignature { signature: Signature },
@@ -22,9 +23,20 @@ impl Request {
 
 #[derive(Serialize, Deserialize, Debug)]
 pub enum Response {
-    Balance { key: Pubkey, val: i64 },
-    LastId { id: Hash },
-    TransactionCount { transaction_count: u64 },
-    SignatureStatus { signature_status: bool },
-    Finality { time: usize },
+    Account {
+        key: Pubkey,
+        account: Option<Account>,
+    },
+    LastId {
+        id: Hash,
+    },
+    TransactionCount {
+        transaction_count: u64,
+    },
+    SignatureStatus {
+        signature_status: bool,
+    },
+    Finality {
+        time: usize,
+    },
 }
