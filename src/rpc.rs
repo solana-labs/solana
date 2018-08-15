d9fac860159f174fd3bbd4ae5bd6008f1854e091
Tyera Eulberg <tyera@solana.com>
2018-08-15 10:37:02 -0600

    Use jsonrpc git repo, allowing removal of Default bound for Metadata

--- src/rpc.rs
+++ src/rpc.rs
@@ -19,29 +19,32 @@ pub struct JsonRpcService {
 }
 
 impl JsonRpcService {
-    pub fn new(bank: Arc<Bank>, rpc_addr: SocketAddr) -> Self {
-        let request_processor = JsonRpcRequestProcessor::new(bank);
-        let thread_hdl = Builder::new()
-            .name("solana-jsonrpc".to_string())
-            .spawn(move || {
-                let mut io = MetaIoHandler::default();
-                let rpc = RpcSolImpl;
-                io.extend_with(rpc.to_delegate());
+    pub fn new(
+        bank: Arc<Bank>,
+        rpc_addr: SocketAddr,
+    ) -> Self {
+            let request_processor = JsonRpcRequestProcessor::new(bank);
+            let thread_hdl = Builder::new()
+                .name("solana-jsonrpc".to_string())
+                .spawn(move || {
+                    let mut io = MetaIoHandler::default();
+                    let rpc = RpcSolImpl;
+                    io.extend_with(rpc.to_delegate());
 
-                let server = ServerBuilder::new(io)
-                    .meta_extractor(move |_req: &hyper::Request| Meta {
-                        request_processor: Some(request_processor.clone()),
-                    })
-                    .threads(4)
-                    .cors(DomainsValidation::AllowOnly(vec![
-                        AccessControlAllowOrigin::Any,
-                    ]))
-                    .start_http(&rpc_addr)
-                    .unwrap();
-                server.wait();
-                ()
-            })
-            .unwrap();
+                    let server = ServerBuilder::with_meta_extractor(io, move |_req: &hyper::Request| Meta {
+                            request_processor: request_processor.clone(),
+                        })
+                        .threads(4)
+                        .cors(DomainsValidation::AllowOnly(vec![
+                            AccessControlAllowOrigin::Any,
+                        ]))
+                        .start_http(
+                            &rpc_addr,
+                        ).unwrap();
+                    server.wait();
+                    ()
+                })
+                .unwrap();
         JsonRpcService { thread_hdl }
     }
 }
@@ -58,16 +61,9 @@ impl Service for JsonRpcService {
 
 #[derive(Clone)]
 pub struct Meta {
-    pub request_processor: Option<JsonRpcRequestProcessor>,
+    pub request_processor: JsonRpcRequestProcessor,
 }
 impl Metadata for Meta {}
-impl Default for Meta {
-    fn default() -> Self {
-        Meta {
-            request_processor: None,
-        }
-    }
-}
 
 build_rpc_trait! {
     pub trait RpcSol {
@@ -107,10 +103,10 @@ impl RpcSol for RpcSolImpl {
         } else {
             let signature = Signature::new(&signature_vec);
             let req = JsonRpcRequest::GetSignature { signature };
-            let resp = meta.request_processor.unwrap().process_request(req);
+            let resp = meta.request_processor.process_request(req);
             match resp {
                 Some(Response::SignatureStatus { signature_status }) => Ok(signature_status),
-                Some(_) => Err(Error {
+                Some(_) => Err(Error{
                     code: ErrorCode::ServerError(-32002),
                     message: "Server error: bad response".to_string(),
                     data: None,
@@ -133,10 +129,10 @@ impl RpcSol for RpcSolImpl {
         } else {
             let pubkey = Pubkey::new(&pubkey_vec);
             let req = JsonRpcRequest::GetBalance { key: pubkey };
-            let resp = meta.request_processor.unwrap().process_request(req);
+            let resp = meta.request_processor.process_request(req);
             match resp {
                 Some(Response::Balance { key, val }) => Ok((bs58::encode(key).into_string(), val)),
-                Some(_) => Err(Error {
+                Some(_) => Err(Error{
                     code: ErrorCode::ServerError(-32002),
                     message: "Server error: bad response".to_string(),
                     data: None,
@@ -151,10 +147,10 @@ impl RpcSol for RpcSolImpl {
     }
     fn get_finality(&self, meta: Self::Metadata) -> Result<usize> {
         let req = JsonRpcRequest::GetFinality;
-        let resp = meta.request_processor.unwrap().process_request(req);
+        let resp = meta.request_processor.process_request(req);
         match resp {
             Some(Response::Finality { time }) => Ok(time),
-            Some(_) => Err(Error {
+            Some(_) => Err(Error{
                 code: ErrorCode::ServerError(-32002),
                 message: "Server error: bad response".to_string(),
                 data: None,
@@ -168,10 +164,10 @@ impl RpcSol for RpcSolImpl {
     }
     fn get_last_id(&self, meta: Self::Metadata) -> Result<String> {
         let req = JsonRpcRequest::GetLastId;
-        let resp = meta.request_processor.unwrap().process_request(req);
+        let resp = meta.request_processor.process_request(req);
         match resp {
             Some(Response::LastId { id }) => Ok(bs58::encode(id).into_string()),
-            Some(_) => Err(Error {
+            Some(_) => Err(Error{
                 code: ErrorCode::ServerError(-32002),
                 message: "Server error: bad response".to_string(),
                 data: None,
@@ -185,10 +181,10 @@ impl RpcSol for RpcSolImpl {
     }
     fn get_transaction_count(&self, meta: Self::Metadata) -> Result<u64> {
         let req = JsonRpcRequest::GetTransactionCount;
-        let resp = meta.request_processor.unwrap().process_request(req);
+        let resp = meta.request_processor.process_request(req);
         match resp {
             Some(Response::TransactionCount { transaction_count }) => Ok(transaction_count),
-            Some(_) => Err(Error {
+            Some(_) => Err(Error{
                 code: ErrorCode::ServerError(-32002),
                 message: "Server error: bad response".to_string(),
                 data: None,
@@ -230,7 +226,10 @@ impl JsonRpcRequestProcessor {
     }
 
     /// Process Request items sent via JSON-RPC.
-    fn process_request(&self, msg: JsonRpcRequest) -> Option<Response> {
+    fn process_request(
+        &self,
+        msg: JsonRpcRequest,
+    ) -> Option<Response> {
         match msg {
             JsonRpcRequest::GetBalance { key } => {
                 let val = self.bank.get_balance(&key);
@@ -292,7 +291,7 @@ mod tests {
         let rpc = RpcSolImpl;
         io.extend_with(rpc.to_delegate());
         let meta = Meta {
-            request_processor: Some(request_processor),
+            request_processor: request_processor,
         };
 
         let req = format!(
@@ -300,10 +299,7 @@ mod tests {
             bob_pubkey
         );
         let res = io.handle_request_sync(&req, meta.clone());
-        let expected = format!(
-            r#"{{"jsonrpc":"2.0","result":["{}", 20],"id":1}}"#,
-            bob_pubkey
-        );
+        let expected = format!(r#"{{"jsonrpc":"2.0","result":["{}", 20],"id":1}}"#, bob_pubkey);
         let expected: Response =
             serde_json::from_str(&expected).expect("expected response deserialization");
 
@@ -311,7 +307,9 @@ mod tests {
             .expect("actual response deserialization");
         assert_eq!(expected, result);
 
-        let req = format!(r#"{{"jsonrpc":"2.0","id":1,"method":"solana_getTransactionCount"}}"#);
+        let req = format!(
+            r#"{{"jsonrpc":"2.0","id":1,"method":"solana_getTransactionCount"}}"#
+        );
         let res = io.handle_request_sync(&req, meta.clone());
         let expected = format!(r#"{{"jsonrpc":"2.0","result":1,"id":1}}"#);
         let expected: Response =
@@ -323,12 +321,15 @@ mod tests {
     }
     #[test]
     fn test_rpc_request_bad_parameter_type() {
+        let alice = Mint::new(10_000);
+        let bank = Bank::new(&alice);
+
         let mut io = MetaIoHandler::default();
         let rpc = RpcSolImpl;
         io.extend_with(rpc.to_delegate());
         let req = r#"{"jsonrpc":"2.0","id":1,"method":"solana_getBalance","params":[1234567890]}"#;
         let meta = Meta {
-            request_processor: None,
+            request_processor: JsonRpcRequestProcessor::new(Arc::new(bank)),
         };
 
         let res = io.handle_request_sync(req, meta);
@@ -342,13 +343,16 @@ mod tests {
     }
     #[test]
     fn test_rpc_request_bad_signature() {
+        let alice = Mint::new(10_000);
+        let bank = Bank::new(&alice);
+
         let mut io = MetaIoHandler::default();
         let rpc = RpcSolImpl;
         io.extend_with(rpc.to_delegate());
         let req =
             r#"{"jsonrpc":"2.0","id":1,"method":"solana_confirmTransaction","params":["a1b2c3d4e5"]}"#;
         let meta = Meta {
-            request_processor: None,
+            request_processor: JsonRpcRequestProcessor::new(Arc::new(bank)),
         };
 
         let res = io.handle_request_sync(req, meta);
