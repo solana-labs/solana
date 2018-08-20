a1ec549630a50d9657f3b01364b55fd7da2653b4
Michael Vines <mvines@gmail.com>
2018-08-20 09:59:26 -0700

    Pin nightly rust for more controlled updating

--- ci/docker-rust-nightly/build.sh
+++ ci/docker-rust-nightly/build.sh
@@ -3,4 +3,10 @@
 cd "$(dirname "$0")"
 
 docker build -t solanalabs/rust-nightly .
-docker push solanalabs/rust-nightly
+
+maybeEcho=
+if [[ -z $CI ]]; then
+  echo "Not CI, skipping |docker push|"
+  maybeEcho="echo"
+fi
+$maybeEcho docker push solanalabs/rust-nightly
