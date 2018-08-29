43b4207101e02247ff6d5f371c57e9b166a32f62
Michael Vines <mvines@gmail.com>
2018-08-29 07:48:38 -0700

    Run oom-monitor in net/ testnets

--- net/remote/remote_client.sh
+++ net/remote/remote_client.sh
@@ -10,6 +10,9 @@ PATH="$HOME"/.cargo/bin:"$PATH"
 rsync -vPrz "$1":~/.cargo/bin/solana* ~/.cargo/bin/
 
 numNodes=1 # TODO: Pass this in
-export USE_INSTALL=1
 
-multinode-demo/client.sh "$1":~/solana $numNodes --loop -s 600 --sustained >client.log 2>&1 &
+./script/install-earlyoom.sh
+./scripts/oom-monitor.sh  > oom-monitor.log 2>&1 &
+
+export USE_INSTALL=1
+multinode-demo/client.sh "$1":~/solana $numNodes --loop -s 600 --sustained > client.log 2>&1 &
--- net/remote/remote_leader.sh
+++ net/remote/remote_leader.sh
@@ -6,10 +6,13 @@ loadConfigFile
 
 PATH="$HOME"/.cargo/bin:"$PATH"
 
+./fetch-perf-libs.sh
+
+./script/install-earlyoom.sh
+./scripts/oom-monitor.sh  > oom-monitor.log 2>&1 &
+
 export USE_INSTALL=1
 export SOLANA_CUDA=1
-
-./fetch-perf-libs.sh
 ./multinode-demo/setup.sh
-./multinode-demo/drone.sh >drone.log 2>&1 &
-./multinode-demo/leader.sh >leader.log 2>&1 &
+./multinode-demo/drone.sh > drone.log 2>&1 &
+./multinode-demo/leader.sh > leader.log 2>&1 &
