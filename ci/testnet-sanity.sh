8d9223294936432d5e4a8dc5319a42baa79b51c3
Michael Vines <mvines@gmail.com>
2018-09-28 07:32:49 -0700

    Specify zone

--- ci/testnet-sanity.sh
+++ ci/testnet-sanity.sh
@@ -9,11 +9,12 @@ usage() {
     echo "Error: $*"
   fi
   cat <<EOF
-usage: $0 [name]
+usage: $0 [name] [zone]
 
 Sanity check a CD testnet
 
   name  - name of the network
+  zone  - zone of the network
 
   Note: the SOLANA_METRICS_CONFIG environment variable is used to configure
         metrics
@@ -22,11 +23,13 @@ EOF
 }
 
 netName=$1
+zone=$2
 [[ -n $netName ]] || usage ""
+[[ -n $zone ]] || usage "Zone not specified"
 
 set -x
 echo --- gce.sh config
-net/gce.sh config -p "$netName"
+net/gce.sh config -p "$netName" -z "$zone"
 net/init-metrics.sh -e
 echo --- net.sh sanity
 net/net.sh sanity \
