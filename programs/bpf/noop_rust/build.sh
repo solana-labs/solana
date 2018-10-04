233a66360b83527c6b45cba0bf760c0cc005b8be
Jack May <jack@solana.com>
2018-10-04 02:29:54 -0700

    fix shell script quote

--- programs/bpf/noop_rust/build.sh
+++ programs/bpf/noop_rust/build.sh
@@ -6,5 +6,5 @@ OUTDIR="${1:-../../../target/debug/}"
 mkdir -p "$OUTDIR"
 # cargo +nightly rustc --release -- -C panic=abort --emit=llvm-ir
 cargo +nightly rustc --release -- -C panic=abort --emit=llvm-bc
-cp "$INTERDIR"/deps/noop_rust-*.bc "$OUTDIR"/"noop_rust.bc
+cp "$INTERDIR"/deps/noop_rust-*.bc "$OUTDIR"/noop_rust.bc 
 /usr/local/opt/llvm/bin/llc -march=bpf -filetype=obj -o "$OUTDIR"/noop_rust.o "$OUTDIR"/noop_rust.bc
