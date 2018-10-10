715556a61145510e4a6ddf43b2f795f8a2d2450b
Michael Vines <mvines@gmail.com>
2018-10-10 14:16:07 -0700

    test: add Layout.rustString()

--- web3.js/src/layout.js
+++ web3.js/src/layout.js
@@ -16,3 +16,37 @@ export const publicKey = (property: string = 'publicKey'): Object => {
 export const uint64 = (property: string = 'uint64'): Object => {
   return BufferLayout.blob(8, property);
 };
+
+
+/**
+ * Layout for a Rust String type
+ */
+export const rustString = (property: string = 'string') => {
+  const rsl = BufferLayout.struct(
+    [
+      BufferLayout.u32('length'),
+      BufferLayout.u32('lengthPadding'),
+      BufferLayout.blob(
+        BufferLayout.offset(BufferLayout.u32(), -8),
+        'chars',
+      ),
+    ],
+    property,
+  );
+  const _decode = rsl.decode.bind(rsl);
+  const _encode = rsl.encode.bind(rsl);
+
+  rsl.decode = (buffer, offset) => {
+    const data = _decode(buffer, offset);
+    return data.chars.toString('utf8');
+  };
+
+  rsl.encode = (str, buffer, offset) => {
+    const data = {
+      chars: Buffer.from(str, 'utf8'),
+    };
+    return _encode(data, buffer, offset);
+  };
+
+  return rsl;
+};
