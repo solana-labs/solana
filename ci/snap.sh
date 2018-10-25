b982595c73758ce9b958668d312f34b353492273
Michael Vines <mvines@gmail.com>
2018-10-24 19:36:50 -0700

    Add version check and rustup

--- ci/snap.sh
+++ ci/snap.sh
@@ -2,6 +2,13 @@
 
 cd "$(dirname "$0")/.."
 
+if ! ci/version-check.sh stable; then
+  # This job doesn't run within a container, try once to upgrade tooling on a
+  # version check failure
+  rustup install stable
+  ci/version-check.sh stable
+fi
+
 DRYRUN=
 if [[ -z $BUILDKITE_BRANCH ]] || ./ci/is-pr.sh; then
   DRYRUN="echo"
