205d55a57972b676bcb7987e3586a13cde0c8992
OEM Configuration (temporary user) <wumu727@gmail.com>
2018-10-25 14:41:22 -0700

    Cleanup for PR comments

--- src/poh_recorder.rs
+++ src/poh_recorder.rs
@@ -16,8 +16,6 @@ pub enum PohRecorderError {
     MaxHeightReached,
 }
 
-//TODO: Implement PohRecorderValidator
-
 #[derive(Clone)]
 pub struct PohRecorder {
     is_virtual: bool,
@@ -36,7 +34,7 @@ impl PohRecorder {
         // TODO: amortize the cost of this lock by doing the loop in here for
         // some min amount of hashes
         let mut poh = self.poh.lock().unwrap();
-        if self.check_max_tick_height_reached(&*poh) {
+        if self.is_max_tick_height_reached(&*poh) {
             Err(Error::PohRecorderError(PohRecorderError::MaxHeightReached))
         } else {
             poh.hash();
@@ -49,7 +47,7 @@ impl PohRecorder {
         // hasn't been reached.
         // This guarantees PoH order and Entry production and banks LastId queue is the same
         let mut poh = self.poh.lock().unwrap();
-        if self.check_max_tick_height_reached(&*poh) {
+        if self.is_max_tick_height_reached(&*poh) {
             Err(Error::PohRecorderError(PohRecorderError::MaxHeightReached))
         } else if self.is_virtual {
             self.generate_and_store_tick(&mut *poh);
@@ -69,7 +67,7 @@ impl PohRecorder {
         // Register and send the entry out while holding the lock.
         // This guarantees PoH order and Entry production and banks LastId queue is the same.
         let mut poh = self.poh.lock().unwrap();
-        if self.check_max_tick_height_reached(&*poh) {
+        if self.is_max_tick_height_reached(&*poh) {
             Err(Error::PohRecorderError(PohRecorderError::MaxHeightReached))
         } else {
             self.record_and_send_txs(&mut *poh, mixin, txs)?;
@@ -110,7 +108,7 @@ impl PohRecorder {
         }
     }
 
-    fn check_max_tick_height_reached(&self, poh: &Poh) -> bool {
+    fn is_max_tick_height_reached(&self, poh: &Poh) -> bool {
         if let Some(max_tick_height) = self.max_tick_height {
             poh.tick_height >= max_tick_height
         } else {
