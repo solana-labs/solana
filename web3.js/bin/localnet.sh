df05880550b4f20ad76dc20f35bae2a5b377435e
Michael Vines <mvines@gmail.com>
2018-10-25 17:55:50 -0700

    fix: block until the JSON RPC API is available

--- web3.js/bin/localnet.sh
+++ web3.js/bin/localnet.sh
@@ -75,6 +75,13 @@ up)
       --env RUST_LOG="$RUST_LOG" \
       solanalabs/solana:"$channel"
 
+    curl \
+      --retry 10 \
+      --retry-connrefused \
+      -X POST \
+      -H "Content-Type: application/json" \
+      -d '{"jsonrpc":"2.0","id":1, "method":"getTransactionCount"}' \
+      http://localhost:8899
   )
   ;;
 down)
