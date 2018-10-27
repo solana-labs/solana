e438e70a991bf067c3f904c18ca897ed9460cb5a
jackcmay <jack@solana.com>
2018-10-26 19:38:07 -0700

    Cleanup c programs (#1620)
    Cleanup C programs

--- programs/bpf/move_funds_c/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -ex
-
-OUTDIR="${1:-../../../target/release/}"
-THISDIR=$(dirname "$0")
-mkdir -p "$OUTDIR"
-/usr/local/opt/llvm/bin/clang -Werror -target bpf -O2 -emit-llvm -fno-builtin -o "$OUTDIR"/move_funds_c.bc -c "$THISDIR"/src/move_funds.c
-/usr/local/opt/llvm/bin/llc -march=bpf -filetype=obj -function-sections -o "$OUTDIR"/move_funds_c.o "$OUTDIR"/move_funds_c.bc
-
-#/usr/local/opt/llvm/bin/llvm-objdump -color -source -disassemble "$OUTDIR"/move_funds_c.o
--- programs/bpf/tictactoe_c/dump.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-/usr/local/opt/llvm/bin/llvm-objdump -color -source -disassemble ../../../target/debug/tictactoe_c.o
