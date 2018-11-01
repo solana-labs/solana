8930c6cb3925cd4d46dbe72d5fdc7d9ebc2ae499
Michael Vines <mvines@gmail.com>
2018-10-31 23:21:21 -0700

    fix: update bpf sdk header

--- web3.js/examples/bpf-c-noop/src/noop.c
+++ web3.js/examples/bpf-c-noop/src/noop.c
@@ -3,22 +3,17 @@
  * passed to it
  */
 
-#include <sol_bpf.h>
-
-/**
- * Number of SolKeyedAccounts expected. The program should bail if an
- * unexpected number of accounts are passed to the program's entrypoint
- */
-#define NUM_KA 1
+#include <solana_sdk.h>
 
 extern bool entrypoint(const uint8_t *input) {
-  SolKeyedAccounts ka[NUM_KA];
-  uint8_t *data;
+  SolKeyedAccounts ka[1];
+  uint64_t ka_len;
+  const uint8_t *data;
   uint64_t data_len;
 
-  if (!sol_deserialize(input, NUM_KA, ka, &data, &data_len)) {
+  if (!sol_deserialize(input, ka, SOL_ARRAY_SIZE(ka), &ka_len, &data, &data_len)) {
     return false;
   }
-  sol_print_params(NUM_KA, ka, data, data_len);
+  sol_print_params(ka_len, ka, data, data_len);
   return true;
 }
