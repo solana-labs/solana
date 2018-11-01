5ecb9da8018191dfa5cc15264f76adba43b280ee
Michael Vines <mvines@gmail.com>
2018-10-31 20:28:24 -0700

    Fix up bpf numeric types

--- programs/bpf/c/sdk/inc/sol_bpf.h
+++ programs/bpf/c/sdk/inc/sol_bpf.h
@@ -10,14 +10,18 @@ extern "C" {
 /**
  * Numeric types
  */
+#ifndef __LP64__
+#error LP64 data model required
+#endif
+
 typedef signed char int8_t;
 typedef unsigned char uint8_t;
-typedef signed int int16_t;
-typedef unsigned int uint16_t;
-typedef signed long int int32_t;
-typedef unsigned long int uint32_t;
-typedef signed long long int int64_t;
-typedef unsigned long long int uint64_t;
+typedef signed short int16_t;
+typedef unsigned short uint16_t;
+typedef signed int int32_t;
+typedef unsigned int uint32_t;
+typedef signed long int int64_t;
+typedef unsigned long int uint64_t;
 
 /**
  * Boolean type
