7fa90b0f88ff0d1b6b389de78fb8a337947b0740
Michael Vines <mvines@gmail.com>
2018-11-02 16:09:34 -0700

    fix: use secure websocket when https JSON RPC endpoint is requested

--- web3.js/test/url.js
+++ web3.js/test/url.js
@@ -3,6 +3,10 @@
 /**
  * The connection url to use when running unit tests against a live network
  */
+
 export const url = 'http://localhost:8899/';
+
+//export const url = 'https://api.testnet.solana.com/';
+//export const url = 'https://api.testnet.solana.com/';
 //export const url = 'http://testnet.solana.com:8899/';
 
