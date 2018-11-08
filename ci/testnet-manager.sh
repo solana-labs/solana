b0405db5a9b54358a54613b6166813998dfbd766
Michael Vines <mvines@gmail.com>
2018-11-07 20:11:00 -0800

    Assign static IPs to {edge,beta}.testnet.solana.com

--- ci/testnet-manager.sh
+++ ci/testnet-manager.sh
@@ -201,7 +201,7 @@ start() {
       export NO_LEDGER_VERIFY=1
       export NO_VALIDATOR_SANITY=1
       ci/testnet-deploy.sh edge-testnet-solana-com ec2 us-west-1a \
-        -s "$CHANNEL_OR_TAG" -n 3 -c 0 -P \
+        -s "$CHANNEL_OR_TAG" -n 3 -c 0 -P -a eipalloc-0ccd4f2239886fa94 \
         ${maybeDelete:+-d}
     )
     ;;
@@ -225,7 +225,7 @@ start() {
       export NO_LEDGER_VERIFY=1
       export NO_VALIDATOR_SANITY=1
       ci/testnet-deploy.sh beta-testnet-solana-com ec2 us-west-1a \
-        -t "$CHANNEL_OR_TAG" -n 3 -c 0 -P \
+        -t "$CHANNEL_OR_TAG" -n 3 -c 0 -P -a eipalloc-0f286cf8a0771ce35 \
         ${maybeDelete:+-d}
     )
     ;;
