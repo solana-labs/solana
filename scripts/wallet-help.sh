230a5f95e32ece5bad9d8dc8ed0ea9f5019f90a1
Greg Fitzgerald <greg@solana.com>
2018-11-08 13:56:36 -0700

    Pacify shellcheck

--- scripts/wallet-help.sh
+++ scripts/wallet-help.sh
@@ -5,11 +5,16 @@ cd "$(dirname "$0")"/..
 cargo build
 export PATH=$PWD/target/debug:$PATH
 
-commands=("" address airdrop balance cancel confirm deploy get-transaction-count pay send-signature send-timestamp)
+echo "\`\`\`"
+solana-wallet --help
+echo "\`\`\`"
+echo ""
+
+commands=(address airdrop balance cancel confirm deploy get-transaction-count pay send-signature send-timestamp)
 
 for x in "${commands[@]}"; do
-    echo '```'
-    solana-wallet ${x} --help
-    echo '```'
-    echo ''
+    echo "\`\`\`"
+    solana-wallet "${x}" --help
+    echo "\`\`\`"
+    echo ""
 done
