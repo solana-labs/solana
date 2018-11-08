7ef0b815ec7d27f7c1150afc35cfa8d8c5cf2aed
Michael Vines <mvines@gmail.com>
2018-11-07 17:42:29 -0800

    Remove docker0 interface if present

---
+++ net/scripts/remove-docker-interface.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -ex
+#
+# Some instances have docker running and docker0 network interface confuses
+# gossip and airdrops fail.  As a workaround for now simply remove the docker0
+# interface
+#
+
+[[ $(uname) = Linux ]] || exit 1
+[[ $USER = root ]] || exit 1
+
+ip link delete docker0
