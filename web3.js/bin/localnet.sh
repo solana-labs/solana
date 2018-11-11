680d2d18ead1505c3fe52a602a58333a7c6a879a
Michael Vines <mvines@gmail.com>
2018-11-11 08:13:20 -0800

    refactor: use env to locate bash

--- web3.js/bin/localnet.sh
+++ web3.js/bin/localnet.sh
@@ -1,4 +1,5 @@
-#!/bin/bash -e
+#!/usr/bin/env bash
+set -e
 
 usage() {
   exitcode=0
--- web3.js/test/fixtures/noop/build.sh
+++ web3.js/test/fixtures/noop/build.sh
@@ -1,4 +1,5 @@
-#!/bin/bash -ex
+#!/usr/bin/env bash
+set -ex
 
 make -C ../../../examples/bpf-c-noop/
 cp ../../../examples/bpf-c-noop/out/noop.o .
