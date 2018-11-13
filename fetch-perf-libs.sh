c1045bd1ce89d97ff3111906d6223d3f4694cdc3
Michael Vines <mvines@gmail.com>
2018-11-12 19:40:11 -0800

    Switch to perf-libs v0.11.0 for CUDA 10 support

--- fetch-perf-libs.sh
+++ fetch-perf-libs.sh
@@ -16,7 +16,7 @@ mkdir -p target/perf-libs
   cd target/perf-libs
   (
     set -x
-    curl https://solana-perf.s3.amazonaws.com/v0.10.6/x86_64-unknown-linux-gnu/solana-perf.tgz | tar zxvf -
+    curl https://solana-perf.s3.amazonaws.com/v0.11.0/x86_64-unknown-linux-gnu/solana-perf.tgz | tar zxvf -
   )
 
   if [[ -r solana-perf-CUDA_HOME.txt ]]; then
