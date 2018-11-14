bba19ce66706c64feb53b91773b2249f44568788
Michael Vines <mvines@gmail.com>
2018-11-14 09:55:27 -0800

    Catch up to solana-genesis tokens argument name change

--- ci/docker-solana/entrypoint.sh
+++ ci/docker-solana/entrypoint.sh
@@ -8,7 +8,7 @@ solana-keygen -o /config/leader-keypair.json
 solana-keygen -o /config/drone-keypair.json
 
 solana-fullnode-config --keypair=/config/leader-keypair.json -l > /config/leader-config.json
-solana-genesis --tokens=1000000000 --mint /config/drone-keypair.json --bootstrap_leader /config/leader-config.json --ledger /ledger 
+solana-genesis --num_tokens 1000000000 --mint /config/drone-keypair.json --bootstrap_leader /config/leader-config.json --ledger /ledger
 
 solana-drone --keypair /config/drone-keypair.json --network 127.0.0.1:8001 &
 drone=$!
