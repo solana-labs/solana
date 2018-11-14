a489ebf619c673a0dae10b13ad5f084c9feb78ec
Michael Vines <mvines@gmail.com>
2018-11-14 10:30:52 -0800

    fix: catch up to sol_deserialize() signature change

--- web3.js/examples/bpf-c-noop/src/noop.c
+++ web3.js/examples/bpf-c-noop/src/noop.c
@@ -10,12 +10,14 @@ extern bool entrypoint(const uint8_t *input) {
   uint64_t ka_len;
   const uint8_t *data;
   uint64_t data_len;
+  SolClusterInfo info;
 
   sol_log("Hello World");
 
-  if (!sol_deserialize(input, ka, SOL_ARRAY_SIZE(ka), &ka_len, &data, &data_len)) {
+  if (!sol_deserialize(input, ka, SOL_ARRAY_SIZE(ka), &ka_len, &data, &data_len, &info)) {
     return false;
   }
-  sol_log_params(ka_len, ka, data, data_len);
+  sol_log_64(info.tick_height, 0, 0, 0, 0);
+  sol_log_params(ka, ka_len, data, data_len);
   return true;
 }
