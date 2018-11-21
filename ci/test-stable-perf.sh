4591fd797b99bd0adf6e8c0ec5699d18034f54f5
Jack May <jack@solana.com>
2018-11-21 11:11:53 -0800

    Default to stable because that's what we expect

--- ci/test-stable-perf.sh
+++ ci/test-stable-perf.sh
@@ -10,6 +10,7 @@ if ! ci/version-check.sh stable; then
   # This job doesn't run within a container, try once to upgrade tooling on a
   # version check failure
   rustup install stable
+  rustup default stable
   ci/version-check.sh stable
 fi
 export RUST_BACKTRACE=1
