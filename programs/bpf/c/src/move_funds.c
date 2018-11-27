83c0711760bd4674b303166d9e7f5ebe5ec85829
Michael Vines <mvines@gmail.com>
2018-11-27 15:15:56 -0800

    Rename SolKeyedAccounts to SolKeyedAccount

--- programs/bpf/c/src/move_funds.c
+++ programs/bpf/c/src/move_funds.c
@@ -6,13 +6,13 @@
 #include <solana_sdk.h>
 
 /**
- * Number of SolKeyedAccounts expected. The program should bail if an
+ * Number of SolKeyedAccount expected. The program should bail if an
  * unexpected number of accounts are passed to the program's entrypoint
  */
 #define NUM_KA 3
 
 extern bool entrypoint(const uint8_t *input) {
-  SolKeyedAccounts ka[NUM_KA];
+  SolKeyedAccount ka[NUM_KA];
   const uint8_t *data;
   uint64_t data_len;
 
