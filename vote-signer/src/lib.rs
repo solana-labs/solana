403dd1392ca65459eb4609d0fea517d2db81f38b
Pankaj Garg <pankaj@solana.com>
2018-11-29 23:01:23 +0000

    Vote signing JSON RPC service
    - barebone service that listens for RPC requests

---
+++ vote-signer/src/lib.rs
@@ -0,0 +1,21 @@
+pub mod rpc;
+
+extern crate bincode;
+extern crate bs58;
+extern crate byteorder;
+extern crate bytes;
+#[macro_use]
+extern crate log;
+extern crate serde;
+extern crate serde_derive;
+extern crate solana_metrics;
+extern crate solana_sdk;
+extern crate tokio;
+extern crate tokio_codec;
+
+extern crate solana;
+
+extern crate solana_jsonrpc_core as jsonrpc_core;
+extern crate solana_jsonrpc_http_server as jsonrpc_http_server;
+#[macro_use]
+extern crate solana_jsonrpc_macros as jsonrpc_macros;
