376d7569f2e03114bbe3ad7dba8143fca9142d46
Michael Vines <mvines@gmail.com>
2018-12-01 10:24:04 -0800

    grooming

--- sdk/bpf/scripts/install.sh
+++ sdk/bpf/scripts/install.sh
@@ -7,13 +7,16 @@ cd "$(dirname "$0")"/..
 version=v2.3.2
 if [[ ! -d criterion-$version ]]; then
   (
-    [[ ! -d criterion-$version ]] || exit 0
+    if [[ "$(uname)" = Darwin ]]; then
+      machine=osx
+    else
+      machine=linux
+    fi
+
     set -ex
-    wget https://github.com/Snaipe/Criterion/releases/download/$version/criterion-$version-osx-x86_64.tar.bz2
-    wget https://github.com/Snaipe/Criterion/releases/download/$version/criterion-$version-linux-x86_64.tar.bz2
-    tar jxf criterion-$version-osx-x86_64.tar.bz2
-    tar jxf criterion-$version-linux-x86_64.tar.bz2
-    rm -rf criterion-$version-osx-x86_64.tar.bz2 criterion-$version-linux-x86_64.tar.bz2
+    wget --progress=dot:mega https://github.com/Snaipe/Criterion/releases/download/$version/criterion-$version-$machine-x86_64.tar.bz2
+    tar jxf criterion-$version-$machine-x86_64.tar.bz2
+    rm -rf criterion-$version-$machine-x86_64.tar.bz2
 
     [[ ! -f criterion-$version/README.md ]]
     echo "https://github.com/Snaipe/Criterion/releases/tag/$version" > criterion-$version/README.md
@@ -28,27 +31,29 @@ fi
 # Install LLVM
 # if changing version here must also change in bpf.mk
 version=v0.0.1
-if [ "$(uname)" == "Darwin" ]; then
-  machine=macos
-else
-  machine=linux
-fi
 if [[ ! -d llvm/native-$version ]]; then
   (
+    if [[ "$(uname)" = Darwin ]]; then
+      machine=macos
+    else
+      machine=linux
+    fi
+
     set -ex
     mkdir -p llvm/native-$version
     cd llvm/native-$version
-    wget https://github.com/solana-labs/llvm-builder/releases/download/$version/solana-llvm-$machine.tgz
+    wget --progress=dot:giga https://github.com/solana-labs/llvm-builder/releases/download/$version/solana-llvm-$machine.tgz
     tar xzf solana-llvm-$machine.tgz
     rm -rf solana-llvm-$machine.tgz
 
     [[ ! -f llvm/native-$version/README.md ]]
-    echo "https://github.com/solana-labs/solana-web3.js/releases/tag/$version" > README.md
+    echo "https://github.com/solana-labs/llvm-builder/releases/tag/$version" > README.md
   )
-fi
-# shellcheck disable=SC2181
-if [[ $? -ne 0 ]]; then
-  rm -rf llvm/native-$version
-  exit 1
+
+  # shellcheck disable=SC2181
+  if [[ $? -ne 0 ]]; then
+    rm -rf llvm/native-$version
+    exit 1
+  fi
 fi
 
