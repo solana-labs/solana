9aa8298c85e1e9dda6cc230c315e24655401889e
Jack May <jack@solana.com>
2018-12-02 13:16:49 -0800

    Use bzip2

--- sdk/bpf/scripts/install.sh
+++ sdk/bpf/scripts/install.sh
@@ -11,14 +11,15 @@ if [[ ! -r criterion-version.md ]]; then
     else
       machine=linux
     fi
+    filename=criterion-$version-$machine-x86_64.tar.bz2
 
     set -ex
     rm -rf criterion
     mkdir criterion
     cd criterion
-    wget --progress=dot:mega https://github.com/Snaipe/Criterion/releases/download/$version/criterion-$version-$machine-x86_64.tar.bz2
-    tar --strip-components 1 -jxf criterion-$version-$machine-x86_64.tar.bz2
-    rm -rf criterion-$version-$machine-x86_64.tar.bz2
+    wget --progress=dot:mega https://github.com/Snaipe/Criterion/releases/download/$version/$filename
+    tar --strip-components 1 -jxf $filename
+    rm -rf $filename
 
     echo "https://github.com/Snaipe/Criterion/releases/tag/$version" > ../criterion-version.md
   )
@@ -30,7 +31,7 @@ if [[ ! -r criterion-version.md ]]; then
 fi
 
 # Install LLVM
-version=v0.0.1
+version=v0.0.2
 if [[ ! -f llvm-native-version.md ]]; then
   (
     if [[ "$(uname)" = Darwin ]]; then
@@ -38,18 +39,18 @@ if [[ ! -f llvm-native-version.md ]]; then
     else
       machine=linux
     fi
+    filename=solana-llvm-$machine.tar.bz2
 
     set -ex
     rm -rf llvm-native
     mkdir -p llvm-native
     cd llvm-native
-    wget --progress=dot:giga https://github.com/solana-labs/llvm-builder/releases/download/$version/solana-llvm-$machine.tgz
-    tar xzf solana-llvm-$machine.tgz
-    rm -rf solana-llvm-$machine.tgz
+    wget --progress=dot:giga https://github.com/solana-labs/llvm-builder/releases/download/$version/$filename
+    tar -jxf $filename
+    rm -rf $filename
 
     echo "https://github.com/solana-labs/llvm-builder/releases/tag/$version" > ../llvm-native-version.md
   )
-
   # shellcheck disable=SC2181
   if [[ $? -ne 0 ]]; then
     rm -rf llvm-native
