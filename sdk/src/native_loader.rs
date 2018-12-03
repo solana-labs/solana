63a758508af77889e3abd7b5ddd59e6b79e2ee72
Michael Vines <mvines@gmail.com>
2018-12-03 13:31:11 -0800

    Add sdk native_loader.rs

---
+++ sdk/src/native_loader.rs
@@ -0,0 +1,9 @@
+use pubkey::Pubkey;
+
+pub const NATIVE_LOADER_PROGRAM_ID: [u8; 32] = [
+    1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+];
+
+pub fn id() -> Pubkey {
+    Pubkey::new(&NATIVE_LOADER_PROGRAM_ID)
+}
