0c337790727698a8c741b7db88bee20baf661e9e
Rob Walker <rob@solana.com>
2018-12-08 11:52:53 -0800

    ..

--- ci/audit.sh
+++ ci/audit.sh
@@ -17,23 +17,23 @@ _() {
 
 maybe_cargo_install() {
   set -x
-  for i in "$@"; do
-      declare cmd=${i%:*}
-      declare crate=${i#*:}
+  declare crate=$1
+  shift
 
-      exit=0 && "$cmd" --help > /dev/null 2>&1 || exit=$?
+  exit=0 && "$@" > /dev/null 2>&1 || exit=$?
 
-      # remove when https://github.com/RustSec/cargo-audit/issues/56
-      #  is resolved
-      [[ $cmd == cargo-audit ]] && (( exit == 2 || exit == 127 )) && exit=0
+  # remove when https://github.com/RustSec/cargo-audit/issues/56
+  #  is resolved
+  [[ $crate == cargo-audit ]] && (( exit == 2 )) && exit=0
 
-      if (( exit )) ; then
-        _ cargo install "$crate"
-      fi
-  done
+  if (( exit )) ; then
+    _ cargo install "$crate"
+  fi
+
 }
 
-maybe_cargo_install cargo-audit cargo-tree
+maybe_cargo_install cargo-audit cargo audit --help
+maybe_cargo_install cargo-tree cargo tree --help
 
 _ cargo tree
 _ cargo audit
