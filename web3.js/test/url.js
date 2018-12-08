7ef329cd17cd919fa1710b37af3d3dfb7990df6a
Michael Vines <mvines@gmail.com>
2018-12-08 10:13:43 -0800

    refactor: add other testnet urls for easy manual selection

--- web3.js/test/url.js
+++ web3.js/test/url.js
@@ -6,6 +6,8 @@
 
 export const url = 'http://localhost:8899/';
 
-//export const url = 'https://api.testnet.solana.com/';
-//export const url = 'https://api.testnet.solana.com/';
+//export const url = 'http://edge.testnet.solana.com:8899/';
+//export const url = 'http://beta.edge.testnet.solana.com:8899/';
+
 //export const url = 'http://testnet.solana.com:8899/';
+//export const url = 'https://api.testnet.solana.com/';
