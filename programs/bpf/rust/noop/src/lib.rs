a9bf5db2e34289517f657794e215190094f623d8
Michael Vines <mvines@gmail.com>
2018-12-13 23:07:26 -0800

    clippy fix

--- programs/bpf/rust/noop/src/lib.rs
+++ programs/bpf/rust/noop/src/lib.rs
@@ -6,9 +6,9 @@ use std::mem::transmute;
 #[link_section = ".text,entrypoint"] // TODO platform independent needed
 pub extern "C" fn entrypoint(_raw: *mut u8) {
     let bpf_func_trace_printk = unsafe {
-        transmute::<u64, extern "C" fn(u64, u64, u64, u64, u64)>(
-            rbpf::helpers::BPF_TRACE_PRINTK_IDX as u64,
-        )
+        transmute::<u64, extern "C" fn(u64, u64, u64, u64, u64)>(u64::from(
+            rbpf::helpers::BPF_TRACE_PRINTK_IDX,
+        ))
     };
 
     bpf_func_trace_printk(0, 0, 1, 2, 3);
