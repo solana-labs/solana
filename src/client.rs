fab41b7f85f43d2c15dcce260affe8b14c036483
Michael Vines <mvines@gmail.com>
2018-12-14 17:14:49 -0800

    Move src/netutil.rs into its own crate

--- src/client.rs
+++ src/client.rs
@@ -1,8 +1,7 @@
 use crate::cluster_info::{NodeInfo, FULLNODE_PORT_RANGE};
-use crate::netutil::bind_in_range;
 use crate::thin_client::ThinClient;
 
 pub fn mk_client(r: &NodeInfo) -> ThinClient {
-    let (_, transactions_socket) = bind_in_range(FULLNODE_PORT_RANGE).unwrap();
+    let (_, transactions_socket) = solana_netutil::bind_in_range(FULLNODE_PORT_RANGE).unwrap();
     ThinClient::new(r.rpc, r.tpu, transactions_socket)
 }
