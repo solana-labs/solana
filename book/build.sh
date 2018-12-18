c8d27f6424c051afa03403cbc307909b5546c35b
Michael Vines <mvines@gmail.com>
2018-12-18 14:11:01 -0800

    Drop _ to clean up CI logs, apply more -j

--- book/build.sh
+++ book/build.sh
@@ -3,21 +3,16 @@ set -e
 
 cd "$(dirname "$0")"
 
-_() {
-  echo "--- $*"
-  "$@"
-}
-
 cargo_install_unless() {
   declare crate=$1
   shift
 
   "$@" > /dev/null 2>&1 || \
-    _ cargo install "$crate"
+    cargo install "$crate"
 }
 
 export PATH=$CARGO_HOME/bin:$PATH
 cargo_install_unless mdbook mdbook --help
 cargo_install_unless svgbob_cli svgbob --help
 
-_ make
+make -j"$(nproc)"
