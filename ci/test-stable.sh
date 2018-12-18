497ee02f38b43c3a8bce417cd33e03ab379bc883
Michael Vines <mvines@gmail.com>
2018-12-18 13:12:44 -0800

    Remove duplication between test-stable{,-perf}.sh

--- ci/test-stable.sh
+++ ci/test-stable.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env bash
 set -e
 
+FEATURES="$1"
+
 cd "$(dirname "$0")/.."
 
-ci/version-check.sh stable
+# Clear cached json keypair files
+rm -rf "$HOME/.config/solana"
+
+ci/version-check-with-upgrade.sh stable
 export RUST_BACKTRACE=1
 export RUSTFLAGS="-D warnings"
 
@@ -13,24 +18,24 @@ _() {
 }
 
 _ scripts/ulimit-n.sh
-_ cargo build --all --verbose
-_ cargo test --all --verbose --lib -- --nocapture --test-threads=1
+_ cargo build --all --verbose --features="$FEATURES"
+_ cargo test --all --verbose - --features="$FEATURES" --lib -- --nocapture --test-threads=1
 
-# Run integration tests serially
-for test in tests/*.rs; do
-  test=${test##*/} # basename x
-  test=${test%.rs} # basename x .rs
-  _ cargo test --verbose --test="$test" -- --test-threads=1 --nocapture
-done
-
-# Run native program tests
+# Run native program tests (without $FEATURES)
 for program in programs/native/*; do
-  echo --- "$program"
+  echo --- "$program" test
   (
     set -x
     cd "$program"
     cargo test --verbose -- --nocapture
   )
+done
+
+# Run integration tests serially
+for test in tests/*.rs; do
+  test=${test##*/} # basename x
+  test=${test%.rs} # basename x .rs
+  _ cargo test --verbose --features="$FEATURES" --test="$test" -- --test-threads=1 --nocapture
 done
 
 echo --- ci/localnet-sanity.sh
