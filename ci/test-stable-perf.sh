6f6c350781282df06a30f8a171ac3629c78a5252
Michael Vines <mvines@gmail.com>
2018-12-18 18:21:46 -0800

    Skip stable-perf of no .rs files are modified in a PR

--- ci/test-stable-perf.sh
+++ ci/test-stable-perf.sh
@@ -1,7 +1,23 @@
 #!/usr/bin/env bash
 set -e
-
 cd "$(dirname "$0")/.."
+
+annotate() {
+  ${BUILDKITE:-false} && {
+    buildkite-agent annotate "$@"
+  }
+}
+
+ci/affects-files.sh \
+  .rs$ \
+  ci/test-stable-perf.sh \
+  ci/test-stable.sh \
+|| {
+  annotate --style info --context test-stable-perf \
+    "Stable Perf skipped as no .rs files were modified"
+  exit 0
+}
+
 
 ./fetch-perf-libs.sh
 # shellcheck source=/dev/null
