80a3d30457bc18b4560e65c943b617687571def9
Michael Vines <mvines@gmail.com>
2018-12-19 10:30:24 -0800

    Build/install native programs within cargo-install-all.sh

--- scripts/cargo-install-all.sh
+++ scripts/cargo-install-all.sh
@@ -1,13 +1,27 @@
 #!/usr/bin/env bash
 #
 # |cargo install| of the top-level crate will not install binaries for
-# other workspace creates.
+# other workspace crates or native program crates.
 set -e
-cd "$(dirname "$0")/.."
+
+if [[ -z $1 ]]; then
+  echo Install directory not specified
+  exit 1
+fi
+installDir="$(mkdir -p "$1"; cd "$1"; pwd)"
+cargoFeatures="$2"
+echo "Install location: $installDir"
+
+cd "$(dirname "$0")"/..
 
 SECONDS=0
 
-CRATES=(
+(
+  set -x
+  cargo build --all --release --features="$cargoFeatures"
+)
+
+BIN_CRATES=(
   drone
   keygen
   fullnode
@@ -18,11 +32,23 @@ CRATES=(
   ledger-tool
   wallet
 )
-for crate in "${CRATES[@]}"; do
+
+for crate in "${BIN_CRATES[@]}"; do
   (
     set -x
-    cargo install --path "$crate" "$@"
+    cargo install --force --path "$crate" --root "$installDir" --features="$cargoFeatures"
   )
 done
 
+for dir in programs/native/*; do
+  for program in echo target/release/deps/lib{,solana_}"$(basename "$dir")"{,_program}.{so,dylib,dll}; do
+    if [[ -f $program ]]; then
+      mkdir -p "$installDir/bin/deps"
+      rm -f "$installDir/bin/deps/$(basename "$program")"
+      cp -v "$program" "$installDir"/bin/deps
+    fi
+  done
+done
+
+du -a "$installDir"
 echo "Done after $SECONDS seconds"
