77038f4be137e8245273873fbb7995d3b3e76ae4
Greg Fitzgerald <greg@solana.com>
2019-01-07 09:38:19 -0700

    Optimize has_duplicates() for short slices

---
+++ benches/runtime.rs
@@ -0,0 +1,14 @@
+#![feature(test)]
+
+extern crate test;
+
+use solana::runtime::*;
+use test::Bencher;
+
+#[bench]
+fn bench_has_duplicates(bencher: &mut Bencher) {
+    bencher.iter(|| {
+        let data = test::black_box([1, 2, 3]);
+        assert!(!has_duplicates(&data));
+    })
+}
