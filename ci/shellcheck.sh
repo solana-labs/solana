5c87ddc80ee51fc4930e988d7f3a66ceb826274b
Michael Vines <mvines@gmail.com>
2019-01-08 14:25:44 -0800

    nit: hide echo

--- ci/shellcheck.sh
+++ ci/shellcheck.sh
@@ -4,15 +4,15 @@
 set -e
 
 cd "$(dirname "$0")/.."
-
-set -x
-find . -name "*.sh" \
-    -not -regex ".*/ci/semver_bash/.*" \
-    -not -regex ".*/.cargo/.*" \
-    -not -regex ".*/node_modules/.*" \
-    -not -regex ".*/target/.*" \
-    -print0 \
-  | xargs -0 \
-      ci/docker-run.sh koalaman/shellcheck --color=always --external-sources --shell=bash
-
+(
+  set -x
+  find . -name "*.sh" \
+      -not -regex ".*/ci/semver_bash/.*" \
+      -not -regex ".*/.cargo/.*" \
+      -not -regex ".*/node_modules/.*" \
+      -not -regex ".*/target/.*" \
+      -print0 \
+    | xargs -0 \
+        ci/docker-run.sh koalaman/shellcheck --color=always --external-sources --shell=bash
+)
 echo --- ok
