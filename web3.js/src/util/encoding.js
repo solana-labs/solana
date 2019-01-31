ac6e503b357a5cd034249dda421ab71f08670dec
Tyera Eulberg <tyera@solana.com>
2019-01-31 02:15:02 -0700

    fix: Add transaction encoding helper functions

---
+++ web3.js/src/util/encoding.js
@@ -0,0 +1,32 @@
+// @flow
+
+export function decodeLength(bytes: Array<number>): number {
+  let len = 0;
+  let size = 0;
+  // eslint-disable-next-line no-constant-condition
+  while (true) {
+    let elem = bytes.shift();
+    len |= (elem & 0x7f) << (size * 7);
+    size += 1;
+    if ((elem & 0x80) === 0) {
+      break;
+    }
+  }
+  return len;
+}
+
+export function encodeLength(bytes: Array<number>, len: number) {
+  let rem_len = len;
+  // eslint-disable-next-line no-constant-condition
+  while (true) {
+    let elem = rem_len & 0x7f;
+    rem_len >>= 7;
+    if (rem_len == 0) {
+      bytes.push(elem);
+      break;
+    } else {
+      elem |= 0x80;
+      bytes.push(elem);
+    }
+  }
+}
