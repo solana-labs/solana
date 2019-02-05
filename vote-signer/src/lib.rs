4985b682c3f8cca2e3055ccb73153bceffa21955
Tyera Eulberg <tyera@solana.com>
2019-02-04 17:00:10 -0700

    Move vote_signer back to paritytech/jsonrpc

--- vote-signer/src/lib.rs
+++ vote-signer/src/lib.rs
@@ -2,11 +2,7 @@ pub mod rpc;
 
 #[macro_use]
 extern crate log;
-extern crate solana_jsonrpc_core as jsonrpc_core;
-extern crate solana_jsonrpc_http_server as jsonrpc_http_server;
 extern crate solana_sdk;
-#[macro_use]
-extern crate solana_jsonrpc_macros as jsonrpc_macros;
 #[cfg(test)]
 #[macro_use]
 extern crate serde_json;
