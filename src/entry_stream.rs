b95e691961c99ff06bb01f763ae6c5fa5ef824cc
Sunny Gleason <sunny.gleason@gmail.com>
2019-02-05 13:26:55 -0500

    RFC: synthetic 'block' event to inform block boundaries

--- src/entry_stream.rs
+++ src/entry_stream.rs
@@ -3,6 +3,7 @@
 //! real-time access to entries.
 
 use crate::entry::Entry;
+use crate::leader_scheduler::DEFAULT_TICKS_PER_SLOT;
 use crate::result::Result;
 use chrono::Utc;
 use std::io::prelude::*;
@@ -22,17 +23,46 @@ impl EntryStream {
     pub fn new(socket: String) -> Self {
         EntryStream { socket }
     }
+
+    fn socket_write(&mut self, payload: &str) -> Result<()> {
+        let mut socket = UnixStream::connect(Path::new(&self.socket))?;
+        socket.write_all(payload.as_bytes())?;
+        socket.shutdown(Shutdown::Write)?;
+
+        Ok(())
+    }
 }
 
 impl EntryStreamHandler for EntryStream {
     fn stream_entries(&mut self, entries: &[Entry]) -> Result<()> {
-        let mut socket = UnixStream::connect(Path::new(&self.socket))?;
         for entry in entries {
             let json = serde_json::to_string(&entry)?;
-            let payload = format!(r#"{{"dt":"{}","entry":{}}}"#, Utc::now().to_rfc3339(), json);
-            socket.write_all(payload.as_bytes())?;
+            let payload = format!(
+                r#"{{"dt":"{}","entry":{}}}{}"#,
+                Utc::now().to_rfc3339(),
+                json,
+                "\n",
+            );
+            self.socket_write(&payload)?;
+
+            // on block boundary, emit "block" event which signals that a block is closed
+            // TODO: look at this in the context of forks, where the tuple (tick_height, entry.id)
+            // might not necessarily map 1:1 to block_id
+            if ((entry.tick_height + 1) % DEFAULT_TICKS_PER_SLOT) == 0 {
+                let json = serde_json::to_string(&entry.id)?;
+                let payload = format!(
+                    r#"{{"dt":"{}","block":{{"id":{},"tick_height":{},"count":{},"block_height":{}}}}}{}"#,
+                    Utc::now().to_rfc3339(),
+                    json,
+                    entry.tick_height + 1 - DEFAULT_TICKS_PER_SLOT,
+                    DEFAULT_TICKS_PER_SLOT,
+                    (entry.tick_height + 1) / DEFAULT_TICKS_PER_SLOT,
+                    "\n",
+                );
+                self.socket_write(&payload)?;
+            }
         }
-        socket.shutdown(Shutdown::Write)?;
+
         Ok(())
     }
 }
