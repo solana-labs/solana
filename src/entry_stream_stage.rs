ec076eb680eb65d7119a933b20b538d2a6b50b31
Tyera Eulberg <tyera@solana.com>
2019-02-13 11:42:56 -0700

    Review comments

--- src/entry_stream_stage.rs
+++ src/entry_stream_stage.rs
@@ -59,28 +59,32 @@ impl EntryStreamStage {
     ) -> Result<()> {
         let timeout = Duration::new(1, 0);
         let entries = ledger_entry_receiver.recv_timeout(timeout)?;
+        let leader_scheduler = entry_stream.leader_scheduler.read().unwrap();
 
         for entry in &entries {
+            let slot = leader_scheduler.tick_height_to_slot(entry.tick_height);
+            let leader_id = leader_scheduler
+                .get_leader_for_slot(slot)
+                .map(|leader| leader.to_string())
+                .unwrap_or_else(|| "None".to_string());
+
             if entry.is_tick() && entry_stream.queued_block.is_some() {
                 let queued_block = entry_stream.queued_block.as_ref();
                 let block_tick_height = queued_block.unwrap().tick_height;
                 let block_id = queued_block.unwrap().id;
                 entry_stream
-                    .emit_block_event(block_tick_height, block_id)
+                    .emit_block_event(slot, &leader_id, block_tick_height, block_id)
                     .unwrap_or_else(|e| {
                         error!("Entry Stream error: {:?}, {:?}", e, entry_stream.output);
                     });
                 entry_stream.queued_block = None;
             }
-            entry_stream.emit_entry_event(&entry).unwrap_or_else(|e| {
-                error!("Entry Stream error: {:?}, {:?}", e, entry_stream.output);
-            });
-            if 0 == entry_stream
-                .leader_scheduler
-                .read()
-                .unwrap()
-                .num_ticks_left_in_slot(entry.tick_height)
-            {
+            entry_stream
+                .emit_entry_event(slot, &leader_id, &entry)
+                .unwrap_or_else(|e| {
+                    error!("Entry Stream error: {:?}, {:?}", e, entry_stream.output);
+                });
+            if 0 == leader_scheduler.num_ticks_left_in_slot(entry.tick_height) {
                 entry_stream.queued_block = Some(EntryStreamBlock {
                     tick_height: entry.tick_height,
                     id: entry.id,
