04e93efb34a148a56cce5295c521f1c90de82c66
Anatoly Yakovenko <aeyakovenko@gmail.com>
2019-02-19 16:17:36 -0800

    PoH Service receives a leader; PoH Record caches ticks until a leader is there to pick them up;

--- src/poh_recorder.rs
+++ src/poh_recorder.rs
@@ -14,70 +14,87 @@ use std::sync::{Arc, Mutex};
 pub enum PohRecorderError {
     InvalidCallingObject,
     MaxHeightReached,
+    MinHeightNotReached,
+}
+
+#[derive(Clone)]
+pub struct WorkingLeader {
+    pub bank: Arc<Bank>,
+    pub sender: Sender<Vec<Entry>>,
+    pub min_tick_height: u64,
+    pub max_tick_height: u64,
 }
 
 #[derive(Clone)]
 pub struct PohRecorder {
     poh: Arc<Mutex<Poh>>,
-    max_tick_height: u64,
+    tick_cache: Arc<Mutex<Vec<Entry>>>,
 }
 
 impl PohRecorder {
-    pub fn max_tick_height(&self) -> u64 {
-        self.max_tick_height
-    }
-
-    pub fn hash(&self) -> Result<()> {
+    pub fn hash(&self) {
         // TODO: amortize the cost of this lock by doing the loop in here for
         // some min amount of hashes
         let mut poh = self.poh.lock().unwrap();
 
-        self.check_tick_height(&poh)?;
-
         poh.hash();
+    }
 
+    fn flush_cache(&self, leader: &WorkingLeader) -> Result<()> {
+        let mut cache = vec![];
+        std::mem::swap(&mut cache, &mut self.tick_cache.lock().unwrap());
+        if !cache.is_empty() {
+            for t in &cache {
+                leader.bank.register_tick(&t.id);
+            }
+            leader.sender.send(cache)?;
+        }
         Ok(())
     }
 
-    pub fn tick(&mut self, bank: &Arc<Bank>, sender: &Sender<Vec<Entry>>) -> Result<()> {
+    pub fn tick(&self, leader: &WorkingLeader) -> Result<()> {
         // Register and send the entry out while holding the lock if the max PoH height
         // hasn't been reached.
         // This guarantees PoH order and Entry production and banks LastId queue is the same
         let mut poh = self.poh.lock().unwrap();
 
-        self.check_tick_height(&poh)?;
+        Self::check_tick_height(&poh, leader).map_err(|e| {
+            let tick = Self::generate_tick(&mut poh);
+            self.tick_cache.lock().unwrap().push(tick);
+            e
+        })?;
+                                                      ;
+        self.flush_cache(leader)?;
 
-        self.register_and_send_tick(&mut *poh, bank, sender)
+        Self::register_and_send_tick(&mut *poh, leader)
     }
 
-    pub fn record(
-        &self,
-        mixin: Hash,
-        txs: Vec<Transaction>,
-        sender: &Sender<Vec<Entry>>,
-    ) -> Result<()> {
+    pub fn record(&self, mixin: Hash, txs: Vec<Transaction>, leader: &WorkingLeader) -> Result<()> {
         // Register and send the entry out while holding the lock.
         // This guarantees PoH order and Entry production and banks LastId queue is the same.
         let mut poh = self.poh.lock().unwrap();
 
-        self.check_tick_height(&poh)?;
+        Self::check_tick_height(&poh, leader)?;
+        self.flush_cache(leader)?;
 
-        self.record_and_send_txs(&mut *poh, mixin, txs, sender)
+        Self::record_and_send_txs(&mut *poh, mixin, txs, leader)
     }
 
     /// A recorder to synchronize PoH with the following data structures
     /// * bank - the LastId's queue is updated on `tick` and `record` events
     /// * sender - the Entry channel that outputs to the ledger
-    pub fn new(tick_height: u64, last_entry_id: Hash, max_tick_height: u64) -> Self {
+    pub fn new(tick_height: u64, last_entry_id: Hash) -> Self {
         let poh = Arc::new(Mutex::new(Poh::new(last_entry_id, tick_height)));
-        PohRecorder {
-            poh,
-            max_tick_height,
-        }
+        let tick_cache = Arc::new(Mutex::new(vec![]));
+        PohRecorder { poh, tick_cache }
     }
 
-    fn check_tick_height(&self, poh: &Poh) -> Result<()> {
-        if poh.tick_height >= self.max_tick_height {
+    fn check_tick_height(poh: &Poh, leader: &WorkingLeader) -> Result<()> {
+        if poh.tick_height < leader.min_tick_height {
+            Err(Error::PohRecorderError(
+                PohRecorderError::MinHeightNotReached,
+            ))
+        } else if poh.tick_height >= leader.max_tick_height {
             Err(Error::PohRecorderError(PohRecorderError::MaxHeightReached))
         } else {
             Ok(())
@@ -85,11 +102,10 @@ impl PohRecorder {
     }
 
     fn record_and_send_txs(
-        &self,
         poh: &mut Poh,
         mixin: Hash,
         txs: Vec<Transaction>,
-        sender: &Sender<Vec<Entry>>,
+        leader: &WorkingLeader,
     ) -> Result<()> {
         let entry = poh.record(mixin);
         assert!(!txs.is_empty(), "Entries without transactions are used to track real-time passing in the ledger and can only be generated with PohRecorder::tick function");
@@ -99,25 +115,24 @@ impl PohRecorder {
             id: entry.id,
             transactions: txs,
         };
-        sender.send(vec![entry])?;
+        leader.sender.send(vec![entry])?;
         Ok(())
     }
 
-    fn register_and_send_tick(
-        &self,
-        poh: &mut Poh,
-        bank: &Arc<Bank>,
-        sender: &Sender<Vec<Entry>>,
-    ) -> Result<()> {
+    fn generate_tick(poh: &mut Poh) -> Entry {
         let tick = poh.tick();
-        let tick = Entry {
+        Entry {
             tick_height: tick.tick_height,
             num_hashes: tick.num_hashes,
             id: tick.id,
             transactions: vec![],
-        };
-        bank.register_tick(&tick.id);
-        sender.send(vec![tick])?;
+        }
+    }
+
+    fn register_and_send_tick(poh: &mut Poh, leader: &WorkingLeader) -> Result<()> {
+        let tick = Self::generate_tick(poh);
+        leader.bank.register_tick(&tick.id);
+        leader.sender.send(vec![tick])?;
         Ok(())
     }
 }
@@ -137,32 +152,65 @@ mod tests {
         let bank = Arc::new(Bank::new(&genesis_block));
         let prev_id = bank.last_id();
         let (entry_sender, entry_receiver) = channel();
-        let mut poh_recorder = PohRecorder::new(0, prev_id, 2);
+        let poh_recorder = PohRecorder::new(0, prev_id);
+
+        let leader = WorkingLeader {
+            bank,
+            sender: entry_sender,
+            min_tick_height: 0,
+            max_tick_height: 2,
+        };
 
         //send some data
         let h1 = hash(b"hello world!");
         let tx = test_tx();
-        poh_recorder
-            .record(h1, vec![tx.clone()], &entry_sender)
-            .unwrap();
+        poh_recorder.record(h1, vec![tx.clone()], &leader).unwrap();
         //get some events
         let e = entry_receiver.recv().unwrap();
         assert_eq!(e[0].tick_height, 0); // super weird case, but ok!
 
-        poh_recorder.tick(&bank, &entry_sender).unwrap();
+        poh_recorder.tick(&leader).unwrap();
         let e = entry_receiver.recv().unwrap();
         assert_eq!(e[0].tick_height, 1);
 
-        poh_recorder.tick(&bank, &entry_sender).unwrap();
+        poh_recorder.tick(&leader).unwrap();
         let e = entry_receiver.recv().unwrap();
         assert_eq!(e[0].tick_height, 2);
 
         // max tick height reached
-        assert!(poh_recorder.tick(&bank, &entry_sender).is_err());
-        assert!(poh_recorder.record(h1, vec![tx], &entry_sender).is_err());
+        assert!(poh_recorder.tick(&leader).is_err());
+        assert!(poh_recorder.record(h1, vec![tx], &leader).is_err());
 
         //make sure it handles channel close correctly
         drop(entry_receiver);
-        assert!(poh_recorder.tick(&bank, &entry_sender).is_err());
+        assert!(poh_recorder.tick(&leader).is_err());
+    }
+
+    #[test]
+    fn test_poh_recorder_tick_cache() {
+        let (genesis_block, _mint_keypair) = GenesisBlock::new(2);
+        let bank = Arc::new(Bank::new(&genesis_block));
+        let prev_id = bank.last_id();
+        let (entry_sender, entry_receiver) = channel();
+        let poh_recorder = PohRecorder::new(0, prev_id);
+
+        let leader = WorkingLeader {
+            bank,
+            sender: entry_sender,
+            min_tick_height: 1,
+            max_tick_height: 2,
+        };
+
+        // tick should be cached
+        assert!(poh_recorder.tick(&leader).is_err());
+        assert!(entry_receiver.try_recv().is_err());
+
+        // leader should be at the right height
+        poh_recorder.tick(&leader).unwrap();
+
+        let e = entry_receiver.recv().unwrap();
+        assert_eq!(e[0].tick_height, 1);
+        let e = entry_receiver.recv().unwrap();
+        assert_eq!(e[0].tick_height, 2);
     }
 }
--- src/poh_service.rs
+++ src/poh_service.rs
@@ -1,14 +1,12 @@
 //! The `poh_service` module implements a service that records the passing of
 //! "ticks", a measure of time in the PoH stream
 
-use crate::entry::Entry;
-use crate::poh_recorder::PohRecorder;
-use crate::result::Result;
+use crate::poh_recorder::{PohRecorder, PohRecorderError, WorkingLeader};
+use crate::result::{Error, Result};
 use crate::service::Service;
-use solana_runtime::bank::Bank;
 use solana_sdk::timing::NUM_TICKS_PER_SECOND;
 use std::sync::atomic::{AtomicBool, Ordering};
-use std::sync::mpsc::Sender;
+use std::sync::mpsc::{channel, Receiver, Sender, TryRecvError};
 use std::sync::Arc;
 use std::thread::{self, sleep, Builder, JoinHandle};
 use std::time::Duration;
@@ -46,55 +44,72 @@ impl PohService {
     }
 
     pub fn new(
-        bank: Arc<Bank>,
-        sender: Sender<Vec<Entry>>,
         poh_recorder: PohRecorder,
         config: PohServiceConfig,
         poh_exit: Arc<AtomicBool>,
-    ) -> Self {
+    ) -> (Self, Sender<WorkingLeader>) {
         // PohService is a headless producer, so when it exits it should notify the banking stage.
         // Since channel are not used to talk between these threads an AtomicBool is used as a
         // signal.
         let poh_exit_ = poh_exit.clone();
+        let (leader_sender, leader_receiver) = channel();
         // Single thread to generate ticks
         let tick_producer = Builder::new()
             .name("solana-poh-service-tick_producer".to_string())
             .spawn(move || {
-                let mut poh_recorder_ = poh_recorder;
-                let sender = sender.clone();
-                let bank = bank.clone();
+                let mut poh_recorder = poh_recorder;
+                let leader_receiver = leader_receiver;
                 let return_value =
-                    Self::tick_producer(&bank, &sender, &mut poh_recorder_, config, &poh_exit_);
+                    Self::tick_producer(&leader_receiver, &mut poh_recorder, config, &poh_exit_);
                 poh_exit_.store(true, Ordering::Relaxed);
                 return_value
             })
             .unwrap();
 
-        Self {
-            tick_producer,
-            poh_exit,
-        }
+        (
+            Self {
+                tick_producer,
+                poh_exit,
+            },
+            leader_sender,
+        )
     }
 
     fn tick_producer(
-        bank: &Arc<Bank>,
-        sender: &Sender<Vec<Entry>>,
+        leader_receiver: &Receiver<WorkingLeader>,
         poh: &mut PohRecorder,
         config: PohServiceConfig,
         poh_exit: &AtomicBool,
     ) -> Result<()> {
+        let mut leader = None;
         loop {
+            if leader.is_none() {
+                let e = leader_receiver.try_recv();
+                leader = match e {
+                    Err(TryRecvError::Empty) => None,
+                    _ => Some(e?),
+                };
+            }
             match config {
                 PohServiceConfig::Tick(num) => {
                     for _ in 1..num {
-                        poh.hash()?;
+                        poh.hash();
                     }
                 }
                 PohServiceConfig::Sleep(duration) => {
                     sleep(duration);
                 }
             }
-            poh.tick(&bank, sender)?;
+            let e = if let Some(ref current_leader) = leader {
+                poh.tick(current_leader)
+            } else {
+                Ok(())
+            };
+            match e {
+                Err(Error::PohRecorderError(PohRecorderError::MinHeightNotReached)) => (),
+                Err(Error::PohRecorderError(PohRecorderError::MaxHeightReached)) => leader = None,
+                e => e?,
+            };
             if poh_exit.load(Ordering::Relaxed) {
                 return Ok(());
             }
@@ -114,6 +129,7 @@ impl Service for PohService {
 mod tests {
     use super::*;
     use crate::test_tx::test_tx;
+    use solana_runtime::bank::Bank;
     use solana_sdk::genesis_block::GenesisBlock;
     use solana_sdk::hash::hash;
     use std::sync::mpsc::channel;
@@ -124,12 +140,18 @@ mod tests {
         let bank = Arc::new(Bank::new(&genesis_block));
         let prev_id = bank.last_id();
         let (entry_sender, entry_receiver) = channel();
-        let poh_recorder = PohRecorder::new(bank.tick_height(), prev_id, std::u64::MAX);
+        let poh_recorder = PohRecorder::new(bank.tick_height(), prev_id);
         let exit = Arc::new(AtomicBool::new(false));
+        let working_leader = WorkingLeader {
+            bank: bank.clone(),
+            sender: entry_sender,
+            min_tick_height: bank.tick_height(),
+            max_tick_height: std::u64::MAX,
+        };
 
         let entry_producer: JoinHandle<Result<()>> = {
             let poh_recorder = poh_recorder.clone();
-            let entry_sender = entry_sender.clone();
+            let working_leader = working_leader.clone();
             let exit = exit.clone();
 
             Builder::new()
@@ -139,7 +161,7 @@ mod tests {
                         // send some data
                         let h1 = hash(b"hello world!");
                         let tx = test_tx();
-                        poh_recorder.record(h1, vec![tx], &entry_sender).unwrap();
+                        poh_recorder.record(h1, vec![tx], &working_leader).unwrap();
 
                         if exit.load(Ordering::Relaxed) {
                             break Ok(());
@@ -150,13 +172,13 @@ mod tests {
         };
 
         const HASHES_PER_TICK: u64 = 2;
-        let poh_service = PohService::new(
-            bank,
-            entry_sender,
-            poh_recorder,
+        let (poh_service, leader_sender) = PohService::new(
+            poh_recorder.clone(),
             PohServiceConfig::Tick(HASHES_PER_TICK as usize),
             Arc::new(AtomicBool::new(false)),
         );
+
+        leader_sender.send(working_leader.clone()).expect("send");
 
         // get some events
         let mut hashes = 0;
