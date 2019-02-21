67beddb269fabea7143966a3c4b52ee1154cc7c1
Greg Fitzgerald <greg@solana.com>
2019-02-21 13:13:16 -0700

    Delete BankForks::finalized_bank()

--- src/bank_forks.rs
+++ src/bank_forks.rs
@@ -23,14 +23,6 @@ impl BankForks {
         self.banks[&self.working_bank_id].clone()
     }
 
-    pub fn finalized_bank(&self) -> Arc<Bank> {
-        let mut bank = self.working_bank();
-        while let Some(parent) = bank.parent() {
-            bank = parent;
-        }
-        bank
-    }
-
     pub fn insert(&mut self, bank_id: u64, bank: Bank) {
         self.banks.insert(bank_id, Arc::new(bank));
     }
@@ -53,13 +45,11 @@ mod tests {
         let tick_height = bank.tick_height();
         let bank_forks = BankForks::new(0, bank);
         assert_eq!(bank_forks.working_bank().tick_height(), tick_height);
-        assert_eq!(bank_forks.finalized_bank().tick_height(), tick_height);
     }
 
     #[test]
     fn test_bank_forks_parent() {
         let bank = Bank::default();
-        let finalized_bank_id = bank.tick_height();
         let mut bank_forks = BankForks::new(0, bank);
         let child_bank = Bank::new_from_parent(&bank_forks.working_bank());
         child_bank.register_tick(&Hash::default());
@@ -67,6 +57,5 @@ mod tests {
         bank_forks.insert(child_bank_id, child_bank);
         bank_forks.set_working_bank_id(child_bank_id);
         assert_eq!(bank_forks.working_bank().tick_height(), child_bank_id);
-        assert_eq!(bank_forks.finalized_bank().tick_height(), finalized_bank_id);
     }
 }
