af03ece9c0069882d342be17f6facc4d7703ee6f
Greg Fitzgerald <greg@solana.com>
2019-02-22 08:57:02 -0700

    Revert "Run featurized tests on sub-packages (#2867)"
    This reverts commit 3610768888e3e4823627518dcc1bdadcf5cf1854.

--- ci/test-stable-perf.sh
+++ ci/test-stable-perf.sh
@@ -1,1 +1,45 @@
-test-stable.sh
+#!/usr/bin/env bash
+set -e
+cd "$(dirname "$0")/.."
+
+annotate() {
+  ${BUILDKITE:-false} && {
+    buildkite-agent annotate "$@"
+  }
+}
+
+ci/affects-files.sh \
+  .rs$ \
+  Cargo.lock$ \
+  Cargo.toml$ \
+  ci/test-stable-perf.sh \
+  ci/test-stable.sh \
+|| {
+  annotate --style info --context test-stable-perf \
+    "Stable Perf skipped as no .rs files were modified"
+  exit 0
+}
+
+# Run all BPF C tests
+make -C programs/bpf/c tests
+
+# Must be built out of band
+make -C programs/bpf/rust/noop/ all
+
+FEATURES=bpf_c,bpf_rust,erasure,chacha
+if [[ $(uname) = Darwin ]]; then
+  ./build-perf-libs.sh
+else
+  # Enable persistence mode to keep the CUDA kernel driver loaded, avoiding a
+  # lengthy and unexpected delay the first time CUDA is involved when the driver
+  # is not yet loaded.
+  sudo --non-interactive ./net/scripts/enable-nvidia-persistence-mode.sh
+
+  ./fetch-perf-libs.sh
+  # shellcheck source=/dev/null
+  source ./target/perf-libs/env.sh
+  FEATURES=$FEATURES,cuda
+fi
+
+exec ci/test-stable.sh "$FEATURES"
+
