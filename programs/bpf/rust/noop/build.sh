8feb4d8bab3e434e4035d75118a3dd341fd481dd
Jack May <jack@solana.com>
2019-02-26 22:21:31 -0800

    env variables overwrite .cargo/config, just use env variables

--- programs/bpf/rust/noop/build.sh
+++ programs/bpf/rust/noop/build.sh
@@ -2,7 +2,18 @@
 
 set -ex
 
-# ensure the sdk is installed
+export RUSTFLAGS="$RUSTFLAGS \
+    -C lto=no -C opt-level=2 \
+    -C link-arg=-Tbpf.ld \
+    -C link-arg=-z -C link-arg=notext \
+    -C link-arg=--Bdynamic \
+    -C link-arg=-shared \
+    -C link-arg=--entry=entrypoint \
+     --sysroot ../../../../sdk/bpf/rust-bpf-sysroot"
+
+export CARGO_TARGET_BPFEL_UNKNOWN_UNKNOWN_LINKER="../../../../sdk/bpf/llvm-native/bin/ld.lld"
+
+# Ensure the sdk is installed
 ../../../../sdk/bpf/scripts/install.sh
 
 cargo +bpf build --release --target=bpfel_unknown_unknown -v
