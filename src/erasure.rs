8c65638ccb4c5ac8a01966636fd9d8dc182b56c2
Sagar Dhawan <sagar@solana.com>
2019-02-27 13:06:24 -0800

    fixup! Reintroduce leader_id to blobs

--- src/erasure.rs
+++ src/erasure.rs
@@ -511,7 +511,7 @@ pub mod test {
     use crate::window::WindowSlot;
     use rand::{thread_rng, Rng};
     use solana_sdk::pubkey::Pubkey;
-    use solana_sdk::signature::Keypair;
+    use solana_sdk::signature::{Keypair, KeypairUtil};
     use std::sync::Arc;
 
     #[test]
