15ad61148c1ca35571b013beb649bbfd4743b91f
Anatoly Yakovenko <aeyakovenko@gmail.com>
2019-03-09 06:55:52 -0800

    fixup! fixup! fixup! fixup! fixup! bump ticks per second
    fixup!

--- core/src/fullnode.rs
+++ core/src/fullnode.rs
@@ -110,7 +110,7 @@ impl Fullnode {
             PohRecorder::new(bank.tick_height(), bank.last_blockhash());
         let poh_recorder = Arc::new(Mutex::new(poh_recorder));
         let poh_service = PohService::new(poh_recorder.clone(), &config.tick_config, &exit);
-        poh_recorder.lock().unwrap().clear_bank_signal = blocktree.new_blobs_signals.first();
+        poh_recorder.lock().unwrap().clear_bank_signal = blocktree.new_blobs_signals.first().cloned();
         assert_eq!(
             blocktree.new_blobs_signals.len(),
             1,
