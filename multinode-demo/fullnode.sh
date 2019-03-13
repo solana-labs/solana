42ad297778b96492899332a9d6151756fed4d63c
Michael Vines <mvines@gmail.com>
2019-03-13 13:03:54 -0700

    Add case for --rpc-drone-address

--- multinode-demo/fullnode.sh
+++ multinode-demo/fullnode.sh
@@ -48,6 +48,9 @@ while [[ ${1:0:1} = - ]]; do
   elif [[ $1 = --rpc-port ]]; then
     extra_fullnode_args+=("$1" "$2")
     shift 2
+  elif [[ $1 = --rpc-drone-address ]]; then
+    extra_fullnode_args+=("$1" "$2")
+    shift 2
   else
     echo "Unknown argument: $1"
     exit 1
