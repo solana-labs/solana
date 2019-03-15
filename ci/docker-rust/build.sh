0b27195e908594134dece0b29254b782b6986330
Michael Vines <mvines@gmail.com>
2019-03-15 08:54:41 -0700

    Only push newly built container

--- ci/docker-rust/build.sh
+++ ci/docker-rust/build.sh
@@ -8,5 +8,4 @@ docker build -t solanalabs/rust .
 read -r rustc version _ < <(docker run solanalabs/rust rustc --version)
 [[ $rustc = rustc ]]
 docker tag solanalabs/rust:latest solanalabs/rust:"$version"
-
-docker push solanalabs/rust
+docker push solanalabs/rust:"$version"
