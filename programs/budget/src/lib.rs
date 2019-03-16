ee39f31d814f1421f0dd74e4130163e34543380c
Greg Fitzgerald <greg@solana.com>
2019-03-15 20:48:11 -0600

    Add Runtime object. Allow any number of static loaders.

--- programs/budget/src/lib.rs
+++ programs/budget/src/lib.rs
@@ -12,11 +12,11 @@ fn entrypoint(
     program_id: &Pubkey,
     keyed_accounts: &mut [KeyedAccount],
     data: &[u8],
-    _tick_height: u64,
+    tick_height: u64,
 ) -> Result<(), ProgramError> {
     solana_logger::setup();
 
     trace!("process_instruction: {:?}", data);
     trace!("keyed_accounts: {:?}", keyed_accounts);
-    process_instruction(program_id, keyed_accounts, data)
+    process_instruction(program_id, keyed_accounts, data, tick_height)
 }
