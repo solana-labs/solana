b9a2c70636c2adb10dcc3e431c348ba30a9e85bf
Michael Vines <mvines@gmail.com>
2019-03-16 13:36:47 -0700

    Remove ThinClient::transfer()

--- tests/thin_client.rs
+++ tests/thin_client.rs
@@ -6,7 +6,9 @@ use solana::gossip_service::discover;
 use solana_client::thin_client::create_client;
 use solana_client::thin_client::{retry_get_balance, ThinClient};
 use solana_logger;
-use solana_sdk::signature::{Keypair, KeypairUtil};
+use solana_sdk::hash::Hash;
+use solana_sdk::pubkey::Pubkey;
+use solana_sdk::signature::{Keypair, KeypairUtil, Signature};
 use solana_sdk::system_instruction::SystemInstruction;
 use solana_sdk::system_transaction::SystemTransaction;
 use solana_vote_api::vote_state::VoteState;
@@ -15,6 +17,24 @@ use std::fs::remove_dir_all;
 use std::net::UdpSocket;
 use std::thread::sleep;
 use std::time::Duration;
+
+pub fn transfer(
+    thin_client: &ThinClient,
+    lamports: u64,
+    keypair: &Keypair,
+    to: &Pubkey,
+    blockhash: &Hash,
+) -> std::io::Result<Signature> {
+    debug!(
+        "transfer: lamports={} from={:?} to={:?} blockhash={:?}",
+        lamports,
+        keypair.pubkey(),
+        to,
+        blockhash
+    );
+    let transaction = SystemTransaction::new_account(keypair, to, lamports, *blockhash, 0);
+    thin_client.transfer_signed(&transaction)
+}
 
 #[test]
 fn test_thin_client_basic() {
@@ -31,9 +51,7 @@ fn test_thin_client_basic() {
     let blockhash = client.get_recent_blockhash();
     info!("test_thin_client blockhash: {:?}", blockhash);
 
-    let signature = client
-        .transfer(500, &alice, &bob_pubkey, &blockhash)
-        .unwrap();
+    let signature = transfer(&client, 500, &alice, &bob_pubkey, &blockhash).unwrap();
     info!("test_thin_client signature: {:?}", signature);
     client.poll_for_signature(&signature).unwrap();
 
@@ -90,9 +108,14 @@ fn test_register_vote_account() {
     // Create the validator account, transfer some lamports to that account
     let validator_keypair = Keypair::new();
     let blockhash = client.get_recent_blockhash();
-    let signature = client
-        .transfer(500, &alice, &validator_keypair.pubkey(), &blockhash)
-        .unwrap();
+    let signature = transfer(
+        &client,
+        500,
+        &alice,
+        &validator_keypair.pubkey(),
+        &blockhash,
+    )
+    .unwrap();
 
     client.poll_for_signature(&signature).unwrap();
 
@@ -162,18 +185,14 @@ fn test_zero_balance_after_nonzero() {
     info!("Alice has {} lamports", starting_alice_balance);
 
     info!("Give Bob 500 lamports");
-    let signature = client
-        .transfer(500, &alice, &bob_keypair.pubkey(), &blockhash)
-        .unwrap();
+    let signature = transfer(&client, 500, &alice, &bob_keypair.pubkey(), &blockhash).unwrap();
     client.poll_for_signature(&signature).unwrap();
 
     let bob_balance = client.poll_get_balance(&bob_keypair.pubkey());
     assert_eq!(bob_balance.unwrap(), 500);
 
     info!("Take Bob's 500 lamports away");
-    let signature = client
-        .transfer(500, &bob_keypair, &alice.pubkey(), &blockhash)
-        .unwrap();
+    let signature = transfer(&client, 500, &bob_keypair, &alice.pubkey(), &blockhash).unwrap();
     client.poll_for_signature(&signature).unwrap();
     let alice_balance = client.poll_get_balance(&alice.pubkey()).unwrap();
     assert_eq!(alice_balance, starting_alice_balance);
