a54854abc7ce5d99f0e7865ada98f71bfcf1c28d
Greg Fitzgerald <greg@solana.com>
2019-03-18 06:50:17 -0600

    Do Budget verification in BudgetScript

--- programs/budget_api/src/budget_script.rs
+++ programs/budget_api/src/budget_script.rs
@@ -18,6 +18,9 @@ impl BudgetScript {
         lamports: u64,
         expr: BudgetExpr,
     ) -> Script {
+        if !expr.verify(lamports) {
+            panic!("invalid budget expression");
+        }
         let space = serialized_size(&BudgetState::new(expr.clone())).unwrap();
         let instructions = vec![
             SystemInstruction::new_program_account(&from, contract, lamports, space, &id()),
@@ -68,5 +71,38 @@ impl BudgetScript {
         };
 
         Self::new_account(from, contract, lamports, expr)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::budget_expr::BudgetExpr;
+
+    #[test]
+    fn test_budget_script_verify() {
+        let alice_pubkey = Keypair::new().pubkey();
+        let bob_pubkey = Keypair::new().pubkey();
+        BudgetScript::pay(&alice_pubkey, &bob_pubkey, 1); // No panic! indicates success.
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_budget_script_overspend() {
+        let alice_pubkey = Keypair::new().pubkey();
+        let bob_pubkey = Keypair::new().pubkey();
+        let budget_pubkey = Keypair::new().pubkey();
+        let expr = BudgetExpr::new_payment(2, &bob_pubkey);
+        BudgetScript::new_account(&alice_pubkey, &budget_pubkey, 1, expr);
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_budget_script_underspend() {
+        let alice_pubkey = Keypair::new().pubkey();
+        let bob_pubkey = Keypair::new().pubkey();
+        let budget_pubkey = Keypair::new().pubkey();
+        let expr = BudgetExpr::new_payment(1, &bob_pubkey);
+        BudgetScript::new_account(&alice_pubkey, &budget_pubkey, 2, expr);
     }
 }
