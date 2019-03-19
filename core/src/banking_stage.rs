cb093557f0b6ae34ae09201fa024e726bb75e010
Pankaj Garg <pankaj@solana.com>
2019-03-19 11:49:06 -0700

    Fix potential crash in banking stage

--- core/src/banking_stage.rs
+++ core/src/banking_stage.rs
@@ -150,7 +150,9 @@ impl BankingStage {
         // Buffer the packets if I am the next leader
         // or, if it was getting sent to me
         let leader_id = match poh_recorder.lock().unwrap().bank() {
-            Some(bank) => leader_schedule_utils::slot_leader_at(bank.slot() + 1, &bank).unwrap(),
+            Some(bank) => {
+                leader_schedule_utils::slot_leader_at(bank.slot() + 1, &bank).unwrap_or_default()
+            }
             None => rcluster_info
                 .leader_data()
                 .map(|x| x.id)
