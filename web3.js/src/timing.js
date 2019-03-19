33bc00d7111587c51754d55aef70602d20a3fe47
Michael Vines <mvines@gmail.com>
2019-03-19 13:32:42 -0700

    fix: adjust DEFAULT_TICKS_PER_SLOT

--- web3.js/src/timing.js
+++ web3.js/src/timing.js
@@ -3,5 +3,4 @@
 // These constants should match the values in
 // https://github.com/solana-labs/solana/blob/master/sdk/src/timing.rs
 export const NUM_TICKS_PER_SECOND = 10;
-export const DEFAULT_TICKS_PER_SLOT = 160;
-export const DEFAULT_SLOTS_PER_EPOCH = 64;
+export const DEFAULT_TICKS_PER_SLOT = 8;
