8f31a2eccc248f5338be8092dcbacfe48e591cd6
Anatoly Yakovenko <aeyakovenko@gmail.com>
2019-03-28 16:06:02 -0700

    tests

--- runtime/src/status_cache.rs
+++ runtime/src/status_cache.rs
@@ -97,187 +97,121 @@ impl<T: Clone> StatusCache<T> {
     }
 }
 
-// #[cfg(test)]
-// mod tests {
-//     use super::*;
-//     use solana_sdk::hash::hash;
-//     use solana_sdk::transaction::TransactionError;
-//
-//     type BankStatusCache = StatusCache<TransactionError>;
-//
-//     #[test]
-//     fn test_has_signature() {
-//         let sig = Signature::default();
-//         let blockhash = hash(Hash::default().as_ref());
-//         let mut status_cache = BankStatusCache::new(&blockhash);
-//         assert_eq!(status_cache.has_signature(&sig), false);
-//         assert_eq!(status_cache.get_signature_status(&sig), None);
-//         status_cache.add(&sig);
-//         assert_eq!(status_cache.has_signature(&sig), true);
-//         assert_eq!(status_cache.get_signature_status(&sig), Some(Ok(())));
-//     }
-//
-//     #[test]
-//     fn test_has_signature_checkpoint() {
-//         let sig = Signature::default();
-//         let blockhash = hash(Hash::default().as_ref());
-//         let mut first = BankStatusCache::new(&blockhash);
-//         first.add(&sig);
-//         assert_eq!(first.get_signature_status(&sig), Some(Ok(())));
-//         let blockhash = hash(blockhash.as_ref());
-//         let second = StatusCache::new(&blockhash);
-//         let checkpoints = [&second, &first];
-//         assert_eq!(
-//             BankStatusCache::get_signature_status_all(&checkpoints, &sig),
-//             Some((1, Ok(()))),
-//         );
-//         assert!(StatusCache::has_signature_all(&checkpoints, &sig));
-//     }
-//
-//     #[test]
-//     fn test_new_cache() {
-//         let sig = Signature::default();
-//         let blockhash = hash(Hash::default().as_ref());
-//         let mut first = BankStatusCache::new(&blockhash);
-//         first.add(&sig);
-//         assert_eq!(first.get_signature_status(&sig), Some(Ok(())));
-//         let blockhash = hash(blockhash.as_ref());
-//         first.new_cache(&blockhash);
-//         assert_eq!(first.get_signature_status(&sig), Some(Ok(())));
-//         assert!(first.has_signature(&sig));
-//         first.clear();
-//         assert_eq!(first.get_signature_status(&sig), None);
-//         assert!(!first.has_signature(&sig));
-//     }
-//
-//     #[test]
-//     fn test_new_cache_full() {
-//         let sig = Signature::default();
-//         let blockhash = hash(Hash::default().as_ref());
-//         let mut first = BankStatusCache::new(&blockhash);
-//         first.add(&sig);
-//         assert_eq!(first.get_signature_status(&sig), Some(Ok(())));
-//         for _ in 0..(MAX_CACHE_ENTRIES + 1) {
-//             let blockhash = hash(blockhash.as_ref());
-//             first.new_cache(&blockhash);
-//         }
-//         assert_eq!(first.get_signature_status(&sig), None);
-//         assert!(!first.has_signature(&sig));
-//     }
-//
-//     #[test]
-//     fn test_status_cache_squash_has_signature() {
-//         let sig = Signature::default();
-//         let blockhash = hash(Hash::default().as_ref());
-//         let mut first = BankStatusCache::new(&blockhash);
-//         first.add(&sig);
-//         assert_eq!(first.get_signature_status(&sig), Some(Ok(())));
-//
-//         // give first a merge
-//         let blockhash = hash(blockhash.as_ref());
-//         first.new_cache(&blockhash);
-//
-//         let blockhash = hash(blockhash.as_ref());
-//         let mut second = BankStatusCache::new(&blockhash);
-//         first.freeze();
-//         second.squash(&[&first]);
-//
-//         assert_eq!(second.get_signature_status(&sig), Some(Ok(())));
-//         assert!(second.has_signature(&sig));
-//     }
-//
-//     #[test]
-//     fn test_status_cache_squash_overflow() {
-//         let mut blockhash = hash(Hash::default().as_ref());
-//         let mut cache = BankStatusCache::new(&blockhash);
-//
-//         let parents: Vec<_> = (0..MAX_CACHE_ENTRIES)
-//             .map(|_| {
-//                 blockhash = hash(blockhash.as_ref());
-//
-//                 let mut p = BankStatusCache::new(&blockhash);
-//                 p.freeze();
-//                 p
-//             })
-//             .collect();
-//
-//         let mut parents_refs: Vec<_> = parents.iter().collect();
-//
-//         blockhash = hash(Hash::default().as_ref());
-//         let mut root = BankStatusCache::new(&blockhash);
-//
-//         let sig = Signature::default();
-//         root.add(&sig);
-//
-//         parents_refs.push(&root);
-//
-//         assert_eq!(root.get_signature_status(&sig), Some(Ok(())));
-//         assert!(root.has_signature(&sig));
-//
-//         // will overflow
-//         cache.squash(&parents_refs);
-//
-//         assert_eq!(cache.get_signature_status(&sig), None);
-//         assert!(!cache.has_signature(&sig));
-//     }
-//
-//     #[test]
-//     fn test_failure_status() {
-//         let sig = Signature::default();
-//         let blockhash = hash(Hash::default().as_ref());
-//         let mut first = StatusCache::new(&blockhash);
-//         first.add(&sig);
-//         first.save_failure_status(&sig, TransactionError::DuplicateSignature);
-//         assert_eq!(first.has_signature(&sig), true);
-//         assert_eq!(
-//             first.get_signature_status(&sig),
-//             Some(Err(TransactionError::DuplicateSignature)),
-//         );
-//     }
-//
-//     #[test]
-//     fn test_clear_signatures() {
-//         let sig = Signature::default();
-//         let blockhash = hash(Hash::default().as_ref());
-//         let mut first = StatusCache::new(&blockhash);
-//         first.add(&sig);
-//         assert_eq!(first.has_signature(&sig), true);
-//         first.save_failure_status(&sig, TransactionError::DuplicateSignature);
-//         assert_eq!(
-//             first.get_signature_status(&sig),
-//             Some(Err(TransactionError::DuplicateSignature)),
-//         );
-//         first.clear();
-//         assert_eq!(first.has_signature(&sig), false);
-//         assert_eq!(first.get_signature_status(&sig), None);
-//     }
-//     #[test]
-//     fn test_clear_signatures_all() {
-//         let sig = Signature::default();
-//         let blockhash = hash(Hash::default().as_ref());
-//         let mut first = StatusCache::new(&blockhash);
-//         first.add(&sig);
-//         assert_eq!(first.has_signature(&sig), true);
-//         let mut second = StatusCache::new(&blockhash);
-//         let mut checkpoints = [&mut second, &mut first];
-//         BankStatusCache::clear_all(&mut checkpoints);
-//         assert_eq!(
-//             BankStatusCache::has_signature_all(&checkpoints, &sig),
-//             false
-//         );
-//     }
-//
-//     #[test]
-//     fn test_status_cache_freeze() {
-//         let sig = Signature::default();
-//         let blockhash = hash(Hash::default().as_ref());
-//         let mut cache: StatusCache<()> = StatusCache::new(&blockhash);
-//
-//         cache.freeze();
-//         cache.freeze();
-//
-//         cache.add(&sig);
-//         assert_eq!(cache.has_signature(&sig), false);
-//     }
-//
-// }
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use solana_sdk::hash::hash;
+
+    type BankStatusCache = StatusCache<()>;
+
+    #[test]
+    fn test_empty_has_no_sigs() {
+        let sig = Signature::default();
+        let blockhash = hash(Hash::default().as_ref());
+        let mut status_cache = BankStatusCache::default();
+        assert_eq!(
+            status_cache.get_signature_status(&sig, &blockhash, &HashMap::new()),
+            None
+        );
+        assert_eq!(
+            status_cache.get_signature_status_slow(&sig, &HashMap::new()),
+            None
+        );
+    }
+
+    #[test]
+    fn test_find_sig_with_ancestor_fork() {
+        let sig = Signature::default();
+        let mut status_cache = BankStatusCache::default();
+        let blockhash = hash(Hash::default().as_ref());
+        let ancestors = vec![(0, 1)].into_iter().collect();
+        status_cache.register_hash(blockhash, 0);
+        status_cache.insert(&blockhash, &sig, 0, ());
+        assert_eq!(
+            status_cache.get_signature_status(&sig, &blockhash, &ancestors),
+            Some((0, ()))
+        );
+        assert_eq!(
+            status_cache.get_signature_status_slow(&sig, &ancestors),
+            Some((1, ()))
+        );
+    }
+
+    #[test]
+    fn test_find_sig_without_ancestor_fork() {
+        let sig = Signature::default();
+        let mut status_cache = BankStatusCache::default();
+        let blockhash = hash(Hash::default().as_ref());
+        let ancestors = HashMap::new();
+        status_cache.register_hash(blockhash, 0);
+        status_cache.insert(&blockhash, &sig, 0, ());
+        assert_eq!(
+            status_cache.get_signature_status(&sig, &blockhash, &ancestors),
+            None
+        );
+        assert_eq!(
+            status_cache.get_signature_status_slow(&sig, &ancestors),
+            None
+        );
+    }
+
+    #[test]
+    fn test_find_sig_with_root_ancestor_fork() {
+        let sig = Signature::default();
+        let mut status_cache = BankStatusCache::default();
+        let blockhash = hash(Hash::default().as_ref());
+        let ancestors = vec![(2, 2)].into_iter().collect();
+        status_cache.register_hash(blockhash, 0);
+        status_cache.insert(&blockhash, &sig, 0, ());
+        status_cache.add_root(0);
+        assert_eq!(
+            status_cache.get_signature_status(&sig, &blockhash, &ancestors),
+            Some((0, ()))
+        );
+        assert_eq!(
+            status_cache.get_signature_status_slow(&sig, &ancestors),
+            Some((1, ()))
+        );
+    }
+
+    #[test]
+    fn test_root_expires() {
+        let sig = Signature::default();
+        let mut status_cache = BankStatusCache::default();
+        let blockhash = hash(Hash::default().as_ref());
+        let ancestors = HashMap::new();
+        status_cache.register_hash(blockhash, 0);
+        status_cache.insert(&blockhash, &sig, 0, ());
+        for i in 0..(MAX_CACHE_ENTRIES + 1) {
+            status_cache.add_root(i as u64);
+        }
+        assert_eq!(
+            status_cache.get_signature_status(&sig, &blockhash, &ancestors),
+            None
+        );
+        assert_eq!(
+            status_cache.get_signature_status_slow(&sig, &ancestors),
+            None
+        );
+    }
+
+    #[test]
+    fn test_clear() {
+        let sig = Signature::default();
+        let mut status_cache = BankStatusCache::default();
+        let blockhash = hash(Hash::default().as_ref());
+        let ancestors = HashMap::new();
+        status_cache.register_hash(blockhash, 0);
+        status_cache.insert(&blockhash, &sig, 0, ());
+        status_cache.add_root(0);
+        status_cache.clear();
+        assert_eq!(
+            status_cache.get_signature_status(&sig, &blockhash, &ancestors),
+            None
+        );
+        assert_eq!(
+            status_cache.get_signature_status_slow(&sig, &ancestors),
+            None
+        );
+    }
+}
