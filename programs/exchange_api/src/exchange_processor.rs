ea73145ccb2fa5f2b8fe1ad096a1012a0ded1766
Jack May <jack@solana.com>
2019-04-02 15:06:25 -0700

    fmt

--- programs/exchange_api/src/exchange_processor.rs
+++ programs/exchange_api/src/exchange_processor.rs
@@ -162,7 +162,11 @@ impl ExchangeProcessor {
 
         Self::is_account_unallocated(&ka[1].account.data[..])?;
         Self::serialize(
-            &ExchangeState::Account(TokenAccountInfo::default().owner(&ka[0].unsigned_key()).tokens(100_000,100_000,100_000,100_000)),
+            &ExchangeState::Account(
+                TokenAccountInfo::default()
+                    .owner(&ka[0].unsigned_key())
+                    .tokens(100_000, 100_000, 100_000, 100_000),
+            ),
             &mut ka[1].account.data[..],
         )
     }
