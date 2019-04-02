dd4c512954ad4447b03791a04542dc6a6b00cc39
Greg Fitzgerald <greg@solana.com>
2019-04-02 07:08:11 -0600

    Rename Wallet's id to keypair

--- wallet/tests/request_airdrop.rs
+++ wallet/tests/request_airdrop.rs
@@ -24,7 +24,7 @@ fn test_wallet_request_airdrop() {
     let rpc_client = RpcClient::new_socket(leader_data.rpc);
 
     let balance = rpc_client
-        .retry_get_balance(&bob_config.id.pubkey(), 1)
+        .retry_get_balance(&bob_config.keypair.pubkey(), 1)
         .unwrap()
         .unwrap();
     assert_eq!(balance, 50);
