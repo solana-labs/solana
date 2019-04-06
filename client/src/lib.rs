8357b5c4e7cbffe0e4935eab50decf5e4863989c
Tyera Eulberg <tyera@solana.com>
2019-04-05 20:57:38 -0600

    Remove RpcSignatureStatus

--- client/src/lib.rs
+++ client/src/lib.rs
@@ -3,8 +3,4 @@ pub mod mock_rpc_client_request;
 pub mod rpc_client;
 pub mod rpc_client_request;
 pub mod rpc_request;
-pub mod rpc_signature_status;
 pub mod thin_client;
-
-#[macro_use]
-extern crate serde_derive;
