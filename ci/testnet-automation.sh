4fb8fd4e0227c5cec1a7f7c401117be4b5fd3cdd
Pankaj Garg <pankaj@solana.com>
2019-04-15 14:18:11 -0700

    change parsing for shellcheck

--- ci/testnet-automation.sh
+++ ci/testnet-automation.sh
@@ -21,7 +21,7 @@ source ci/upload-ci-artifact.sh
 [[ -n $CHANNEL ]] || CHANNEL=beta
 [[ -n $ADDITIONAL_FLAGS ]] || ADDITIONAL_FLAGS=""
 
-IFS=, TESTNET_CLOUD_ZONES=( {$TESTNET_ZONES} )
+TESTNET_CLOUD_ZONES=(); while read -d, ; do TESTNET_CLOUD_ZONES+=( $REPLY ); done <<< "${TESTNET_ZONES},"
 
 launchTestnet() {
   declare nodeCount=$1
