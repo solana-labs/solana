17abd0754bc698729daf58da24f3f91624f85383
Pankaj Garg <pankaj@solana.com>
2019-04-15 13:36:25 -0700

    revert back to IFS approach, as readarray is older version in CI

--- ci/testnet-automation.sh
+++ ci/testnet-automation.sh
@@ -21,7 +21,7 @@ source ci/upload-ci-artifact.sh
 [[ -n $CHANNEL ]] || CHANNEL=beta
 [[ -n $ADDITIONAL_FLAGS ]] || ADDITIONAL_FLAGS=""
 
-readarray -d, -t TESTNET_CLOUD_ZONES <<< "${TESTNET_ZONES}"
+IFS=, TESTNET_CLOUD_ZONES=( {$TESTNET_ZONES} )
 
 launchTestnet() {
   declare nodeCount=$1
