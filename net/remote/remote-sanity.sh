0e790c0f65fb39b69e4c93beb866ec5b68bd3f75
Michael Vines <mvines@gmail.com>
2019-04-15 21:13:44 -0700

    Remove stake from `./net sanity` ephemeral validator

--- net/remote/remote-sanity.sh
+++ net/remote/remote-sanity.sh
@@ -133,7 +133,7 @@ echo "--- $entrypointIp: validator sanity"
 if $validatorSanity; then
   (
     set -x -o pipefail
-    timeout 10s ./multinode-demo/fullnode-x.sh \
+    timeout 10s ./multinode-demo/fullnode-x.sh --no-stake \
       "$entrypointRsyncUrl" \
       "$entrypointIp:8001" 2>&1 | tee validator-sanity.log
   ) || {
