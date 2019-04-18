0945a1446fd596830b2b468ec9e2b4e7aa3437d9
Tyera Eulberg <teulberg@gmail.com>
2019-04-18 11:20:33 -0600

    Revert "Migrate from ring to ed25519-dalek, take 2 (#3844)"
    This reverts commit e9b82bacdad0de664fff7b1e779c55313870b231.

--- core/src/gen_keys.rs
+++ core/src/gen_keys.rs
@@ -4,6 +4,7 @@ use rand::{Rng, SeedableRng};
 use rand_chacha::ChaChaRng;
 use rayon::prelude::*;
 use solana_sdk::signature::Keypair;
+use untrusted::Input;
 
 pub struct GenKeys {
     generator: ChaChaRng,
@@ -25,14 +26,10 @@ impl GenKeys {
         (0..n).map(|_| self.gen_seed()).collect()
     }
 
-    pub fn gen_keypair(&mut self) -> Keypair {
-        Keypair::generate(&mut self.generator)
-    }
-
     pub fn gen_n_keypairs(&mut self, n: u64) -> Vec<Keypair> {
         self.gen_n_seeds(n)
             .into_par_iter()
-            .map(|seed| Keypair::generate(&mut ChaChaRng::from_seed(seed)))
+            .map(|seed| Keypair::from_seed_unchecked(Input::from(&seed)).unwrap())
             .collect()
     }
 }
@@ -53,17 +50,6 @@ mod tests {
         for _ in 0..100 {
             assert_eq!(gen0.gen_seed().to_vec(), gen1.gen_seed().to_vec());
         }
-    }
-
-    #[test]
-    fn test_gen_keypair_is_deterministic() {
-        let seed = [0u8; 32];
-        let mut gen0 = GenKeys::new(seed);
-        let mut gen1 = GenKeys::new(seed);
-        assert_eq!(
-            gen0.gen_keypair().to_bytes().to_vec(),
-            gen1.gen_keypair().to_bytes().to_vec()
-        );
     }
 
     fn gen_n_pubkeys(seed: [u8; 32], n: u64) -> HashSet<Pubkey> {
