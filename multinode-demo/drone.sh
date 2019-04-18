2cd556e43c728fa7b1269bf3e276976dc198de3c
Jack May <jack@solana.com>
2019-04-18 15:42:20 -0700

    Add drone cap parameter (#3876)

--- multinode-demo/drone.sh
+++ multinode-demo/drone.sh
@@ -7,18 +7,6 @@ here=$(dirname "$0")
 # shellcheck source=multinode-demo/common.sh
 source "$here"/common.sh
 
-usage() {
-  if [[ -n $1 ]]; then
-    echo "$*"
-    echo
-  fi
-  echo "usage: $0]"
-  echo
-  echo " Run an airdrop drone"
-  echo
-  exit 1
-}
-
 [[ -f "$SOLANA_CONFIG_DIR"/mint-id.json ]] || {
   echo "$SOLANA_CONFIG_DIR/mint-id.json not found, create it by running:"
   echo
@@ -31,6 +19,7 @@ set -ex
 trap 'kill "$pid" && wait "$pid"' INT TERM ERR
 $solana_drone \
   --keypair "$SOLANA_CONFIG_DIR"/mint-id.json \
+  "$@" \
   > >($drone_logger) 2>&1 &
 pid=$!
 wait "$pid"
