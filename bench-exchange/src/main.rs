244a832d79eccedad150b50fdc72d0890294c70e
Stephen Akridge <sakridge@gmail.com>
2019-04-19 13:08:49 -0700

    Helper function for filtering/creating clients in bench-exchange

--- bench-exchange/src/main.rs
+++ bench-exchange/src/main.rs
@@ -2,12 +2,9 @@ pub mod bench;
 mod cli;
 pub mod order_book;
 
-use crate::bench::{airdrop_lamports, do_bench_exchange, Config};
+use crate::bench::{airdrop_lamports, do_bench_exchange, get_clients, Config};
 use log::*;
-use solana::cluster_info::FULLNODE_PORT_RANGE;
-use solana::contact_info::ContactInfo;
 use solana::gossip_service::discover_nodes;
-use solana_client::thin_client::create_client;
 use solana_sdk::signature::KeypairUtil;
 
 fn main() {
@@ -35,21 +32,7 @@ fn main() {
         panic!("Failed to discover nodes");
     });
 
-    let clients: Vec<_> = nodes
-        .iter()
-        .filter_map(|node| {
-            let cluster_entrypoint = node.clone();
-            let cluster_addrs = cluster_entrypoint.client_facing_addr();
-            if ContactInfo::is_valid_address(&cluster_addrs.0)
-                && ContactInfo::is_valid_address(&cluster_addrs.1)
-            {
-                let client = create_client(cluster_addrs, FULLNODE_PORT_RANGE);
-                Some(client)
-            } else {
-                None
-            }
-        })
-        .collect();
+    let clients = get_clients(nodes);
 
     info!("{} nodes found", clients.len());
     if clients.len() < num_nodes {
