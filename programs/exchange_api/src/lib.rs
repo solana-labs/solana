e71ab55288201a6ba0f2591b9e595c3e67a85007
Michael Vines <mvines@gmail.com>
2019-04-25 17:58:49 -0700

    Rename in-tree program_ids to be base-58 human readable (#4001)

--- programs/exchange_api/src/lib.rs
+++ programs/exchange_api/src/lib.rs
@@ -8,8 +8,8 @@ extern crate solana_metrics;
 use solana_sdk::pubkey::Pubkey;
 
 pub const EXCHANGE_PROGRAM_ID: [u8; 32] = [
-    134, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0,
+    3, 147, 111, 103, 210, 47, 14, 213, 108, 116, 49, 115, 232, 171, 14, 111, 167, 140, 221, 234,
+    33, 70, 185, 192, 42, 31, 141, 152, 0, 0, 0, 0,
 ];
 
 pub fn check_id(program_id: &Pubkey) -> bool {
