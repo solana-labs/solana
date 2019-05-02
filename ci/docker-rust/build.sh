4a22deeff7fa2f6ed0d43a104bc950217dedc3db
Rob Walker <rob@solana.com>
2019-05-02 15:31:28 -0700

    push latest, too

--- ci/docker-rust/build.sh
+++ ci/docker-rust/build.sh
@@ -9,3 +9,4 @@ read -r rustc version _ < <(docker run solanalabs/rust rustc --version)
 [[ $rustc = rustc ]]
 docker tag solanalabs/rust:latest solanalabs/rust:"$version"
 docker push solanalabs/rust:"$version"
+docker push solanalabs/rust:latest
