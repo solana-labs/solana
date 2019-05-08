5719b8f2514bd5c97d0df17468f14623c20fcbee
Pankaj Garg <pankaj@solana.com>
2019-05-08 11:20:06 -0700

    Change remote node's ssh config to allow more login retries (#4215)
    automerge

--- net/scripts/network-config.sh
+++ net/scripts/network-config.sh
@@ -9,3 +9,7 @@ sudo sysctl -w net.core.rmem_max=1610612736
 
 sudo sysctl -w net.core.wmem_default=1610612736
 sudo sysctl -w net.core.wmem_max=1610612736
+
+echo "MaxAuthTries 60" | sudo tee -a /etc/ssh/sshd_config
+sudo service sshd restart
+sudo systemctl restart sshd
