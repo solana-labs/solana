bec546179b6c63eb01b729800a29c7c1ff8f3631
Rob Walker <rob@solana.com>
2019-05-13 12:44:06 -0700

    get program names from programs

--- programs/vote_program/src/lib.rs
+++ programs/vote_program/src/lib.rs
@@ -1,3 +1,9 @@
-use solana_vote_api::vote_instruction::process_instruction;
+#[macro_export]
+macro_rules! solana_vote_program {
+    () => {
+        ("solana_vote_program".to_string(), solana_vote_api::id())
+    };
+}
 
+use solana_vote_api::vote_instruction::process_instruction;
 solana_sdk::solana_entrypoint!(process_instruction);
