faca8e4265168cb28f101cb8c4f3369fbd477183
Jack May <jack@solana.com>
2019-05-17 12:38:23 -0700

    Remove metrics docker container even if not running

--- metrics/scripts/stop.sh
+++ metrics/scripts/stop.sh
@@ -2,19 +2,15 @@
 #
 # Stops local metrics
 #
-
 cd "$(dirname "$0")"
-
 for container in influxdb grafana; do
-  if [ "$(docker ps -q -f name=$container)" ]; then
+  if [ "$(docker ps -q -a -f name=$container)" ]; then
   (
     set +e
-    docker kill $container
-    docker rm $container
+    docker rm -f $container
     exit 0
   )
   fi
- done
-
- echo Local metrics stopped
- exit 0
+done
+echo Local metrics stopped
+exit 0
