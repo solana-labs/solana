16aa00b9c0371e0d0f17878faf87c862c2b87f88
Rob Walker <rob@solana.com>
2019-05-21 15:24:02 -0700

    change unwrap to expect where WSL sometimes aborts

--- runtime/src/native_loader.rs
+++ runtime/src/native_loader.rs
@@ -32,8 +32,12 @@ const PLATFORM_FILE_EXTENSION_NATIVE: &str = "so";
 const PLATFORM_FILE_EXTENSION_NATIVE: &str = "dll";
 
 fn create_path(name: &str) -> PathBuf {
-    let current_exe = env::current_exe().unwrap();
-    let current_exe_directory = PathBuf::from(current_exe.parent().unwrap());
+    let current_exe =
+        env::current_exe().expect(&format!("create_path(\"{}\"): current exe not found", name));
+    let current_exe_directory = PathBuf::from(current_exe.parent().expect(&format!(
+        "create_path(\"{}\"): no parent directory of {:?}",
+        name, current_exe
+    )));
     let library_file_name = PathBuf::from(PLATFORM_FILE_PREFIX_NATIVE.to_string() + name)
         .with_extension(PLATFORM_FILE_EXTENSION_NATIVE);
 
