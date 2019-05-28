abe67c070067bc66184abc72753c548a20213ca2
Michael Vines <mvines@gmail.com>
2019-05-28 15:23:36 -0700

    Lock down blockexplorer version

--- net/remote/remote-node.sh
+++ net/remote/remote-node.sh
@@ -130,7 +130,7 @@ local|tar)
       ./multinode-demo/drone.sh > drone.log 2>&1 &
 
       export BLOCKEXPLORER_GEOIP_WHITELIST=$PWD/net/config/geoip.yml
-      npm install @solana/blockexplorer@1
+      npm install @solana/blockexplorer@1.8.12
       npx solana-blockexplorer > blockexplorer.log 2>&1 &
 
       # Confirm the blockexplorer is accessible
