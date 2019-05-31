ea26eeb8fa8549afdb257ef19c38eae887edd42b
Michael Vines <mvines@gmail.com>
2019-05-31 07:42:41 -0700

    chore(deps-dev): bump flow-bin from 0.98.1 to 0.99.1

--- web3.js/flow-typed/superstruct.js
+++ web3.js/flow-typed/superstruct.js
@@ -1,5 +1,13 @@
 declare module 'superstruct' {
+  declare type StructFunc = {
+      (any): any,
+      union(schema: any): any;
+      list(schema: any): any;
+      literal(schema: any): any;
+
+  };
+
   declare module.exports: {
-    struct(schema: any): any;
+    struct: StructFunc;
   }
 }
