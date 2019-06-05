f238c441360dfe452b0162b42cde7e41704be278
Jack May <jack@solana.com>
2019-06-04 18:01:35 -0700

    trace

--- ci/test-stable.sh
+++ ci/test-stable.sh
@@ -45,6 +45,8 @@ test-stable-perf)
     exit 0
   }
 
+  RUST_LOG=solana_bpf_loader=info,solana_rbpf=trace; cargo test --features="bpf_rust"
+
   # BPF program tests
   _ make -C programs/bpf/c tests
   _ cargo +"$rust_stable" test \
