1c7462858cf16a5a02a422f3db933b832fa8060e
Pankaj Garg <pankaj@solana.com>
2019-06-06 10:27:59 -0700

    Change default setting for real PoH in testnet scripts

--- multinode-demo/setup.sh
+++ multinode-demo/setup.sh
@@ -24,7 +24,7 @@ default_arg --mint "$SOLANA_CONFIG_DIR"/mint-keypair.json
 default_arg --lamports 100000000000000
 default_arg --bootstrap-leader-lamports 424242
 default_arg --lamports-per-signature 1
-default_arg --hashes-per-tick sleep
+default_arg --hashes-per-tick auto
 $solana_genesis "${args[@]}"
 
 test -d "$SOLANA_RSYNC_CONFIG_DIR"/ledger
