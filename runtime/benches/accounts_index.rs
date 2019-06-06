4a3e0fcb538782450909c01b1c26753751a1a2b0
Stephen Akridge <stephen@solana.com>
2019-06-06 14:20:58 -0700

    Add accounts_index bench

---
+++ runtime/benches/accounts_index.rs
@@ -0,0 +1,39 @@
+#![feature(test)]
+
+extern crate test;
+
+use rand::{thread_rng, Rng};
+use solana_runtime::accounts_db::AccountInfo;
+use solana_runtime::accounts_index::AccountsIndex;
+use solana_sdk::pubkey::Pubkey;
+use test::Bencher;
+
+#[bench]
+fn bench_accounts_index(bencher: &mut Bencher) {
+    const NUM_PUBKEYS: usize = 10_000;
+    let pubkeys: Vec<_> = (0..NUM_PUBKEYS)
+        .into_iter()
+        .map(|_| Pubkey::new_rand())
+        .collect();
+
+    const NUM_FORKS: u64 = 16;
+
+    let mut index = AccountsIndex::<AccountInfo>::default();
+    for f in 0..NUM_FORKS {
+        for _p in 0..NUM_PUBKEYS {
+            index.insert(f, &pubkeys[_p], AccountInfo::default());
+        }
+    }
+
+    let mut fork = NUM_FORKS;
+    let mut root = 0;
+    bencher.iter(|| {
+        for _p in 0..NUM_PUBKEYS {
+            let pubkey = thread_rng().gen_range(0, NUM_PUBKEYS);
+            index.insert(fork, &pubkeys[pubkey], AccountInfo::default());
+        }
+        index.add_root(root);
+        root += 1;
+        fork += 1;
+    });
+}
