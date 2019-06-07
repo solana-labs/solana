e12c750d4ce9a1ce7ab1fd0afc2bc62020976fe4
Jack May <jack@solana.com>
2019-06-07 15:49:14 -0700

    nudge

--- programs/bpf/rust/dump.sh
+++ programs/bpf/rust/dump.sh
@@ -15,7 +15,7 @@ cp ./target/dump.txt ./targetdump-last.txt 2>/dev/null
 set -ex
 
 ls -la ./target/bpfel-unknown-unknown/release/solana_bpf_rust_"$1".so > ./target/dump_mangled.txt
-greadelf -aW ./target/bpfel-unknown-unknown/release/solana_bpf_rust_"$1".so >> ./target/dump_mangled.txt
+readelf -aW ./target/bpfel-unknown-unknown/release/solana_bpf_rust_"$1".so >> ./target/dump_mangled.txt
 llvm-objdump -print-imm-hex --source --disassemble ./target/bpfel-unknown-unknown/release/solana_bpf_rust_"$1".so >> ./target/dump_mangled.txt
 sed s/://g < ./target/dump_mangled.txt | rustfilt > ./target/dump.txt
 
