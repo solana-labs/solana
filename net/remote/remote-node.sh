343e13802d209c4a1d9993c86a4c3e244381cd67
Pankaj Garg <pankaj@solana.com>
2019-06-10 16:34:29 -0700

    fix script

--- net/remote/remote-node.sh
+++ net/remote/remote-node.sh
@@ -85,7 +85,7 @@ local|tar)
     mkdir ./solana-node-stakes
     if [[ -n $stakeNodesInGenesisBlock ]]; then
       for i in $(seq 1 "$numNodes"); do
-        solana-keygen new -f -o ./solana-node-keys/"$i"
+        solana-keygen new -o ./solana-node-keys/"$i"
         pubkey="$(solana-keygen pubkey ./solana-node-keys/"$i")"
         echo "${pubkey}: $stakeNodesInGenesisBlock" >> ./solana-node-stakes/fullnode-stakes.yml
       done
