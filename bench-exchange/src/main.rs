1aadd045bd9b984f89b1776599bfef49a39c0e54
Pankaj Garg <pankaj@solana.com>
2019-06-12 09:59:09 -0700

    review comments

--- bench-exchange/src/main.rs
+++ bench-exchange/src/main.rs
@@ -9,7 +9,6 @@ extern crate solana_exchange_program;
 use crate::bench::{airdrop_lamports, do_bench_exchange, Config};
 use log::*;
 use solana::gossip_service::{discover_cluster, get_multi_client};
-use solana_client::thin_client::ThinClient;
 use solana_sdk::signature::KeypairUtil;
 
 fn main() {
@@ -51,35 +50,37 @@ fn main() {
         read_from_client_file,
     };
 
-    if !write_to_client_file {
-        info!("Connecting to the cluster");
-        let (nodes, _replicators) =
-            discover_cluster(&entrypoint_addr, num_nodes).unwrap_or_else(|_| {
-                panic!("Failed to discover nodes");
-            });
+    let clients = match write_to_client_file {
+        false => {
+            info!("Connecting to the cluster");
+            let (nodes, _replicators) = discover_cluster(&entrypoint_addr, num_nodes)
+                .unwrap_or_else(|_| {
+                    panic!("Failed to discover nodes");
+                });
 
-        let (client, num_clients) = get_multi_client(&nodes);
+            let (client, num_clients) = get_multi_client(&nodes);
 
-        info!("{} nodes found", num_clients);
-        if num_clients < num_nodes {
-            panic!("Error: Insufficient nodes discovered");
+            info!("{} nodes found", num_clients);
+            if num_clients < num_nodes {
+                panic!("Error: Insufficient nodes discovered");
+            }
+
+            if !read_from_client_file {
+                info!("Funding keypair: {}", config.identity.pubkey());
+
+                let accounts_in_groups = batch_size * account_groups;
+                const NUM_SIGNERS: u64 = 2;
+                airdrop_lamports(
+                    &client,
+                    &drone_addr,
+                    &config.identity,
+                    fund_amount * (accounts_in_groups + 1) as u64 * NUM_SIGNERS,
+                );
+            }
+            vec![client]
         }
+        _ => vec![],
+    };
 
-        if !read_from_client_file {
-            info!("Funding keypair: {}", config.identity.pubkey());
-
-            let accounts_in_groups = batch_size * account_groups;
-            const NUM_SIGNERS: u64 = 2;
-            airdrop_lamports(
-                &client,
-                &drone_addr,
-                &config.identity,
-                fund_amount * (accounts_in_groups + 1) as u64 * NUM_SIGNERS,
-            );
-        }
-        do_bench_exchange(vec![client], config);
-    } else {
-        let clients: Vec<ThinClient> = vec![];
-        do_bench_exchange(clients, config);
-    }
+    do_bench_exchange(clients, config);
 }
