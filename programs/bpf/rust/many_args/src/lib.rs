bc44516eb4a880243edc95a97c471f2d42a99cd8
Jack May <jack@solana.com>
2019-06-12 13:04:53 -0700

    Add test to exercise more args then registers (#4661)

---
+++ programs/bpf/rust/many_args/src/lib.rs
@@ -0,0 +1,23 @@
+//! @brief Example Rust-based BPF program tests loop iteration
+
+#![no_std]
+
+mod helper;
+
+extern crate solana_sdk_bpf_utils;
+
+use solana_sdk_bpf_utils::log::*;
+
+#[no_mangle]
+pub extern "C" fn entrypoint(_input: *mut u8) -> bool {
+    sol_log("call same package");
+    assert_eq!(crate::helper::many_args(1, 2, 3, 4, 5, 6, 7, 8, 9), 45);
+    sol_log("call another package");
+    assert_eq!(
+        solana_bpf_rust_many_args_dep::many_args(1, 2, 3, 4, 5, 6, 7, 8, 9),
+        45
+    );
+
+    sol_log("Success");
+    true
+}
--- programs/bpf/tests/programs.rs
+++ programs/bpf/tests/programs.rs
@@ -86,6 +86,7 @@ mod bpf {
             let programs = [
                 ("solana_bpf_rust_alloc", true),
                 ("solana_bpf_rust_iter", true),
+                // ("solana_bpf_rust_many_args", true),  // Issue #3099
                 ("solana_bpf_rust_noop", true),
                 ("solana_bpf_rust_dep_crate", true),
                 ("solana_bpf_rust_panic", false),
