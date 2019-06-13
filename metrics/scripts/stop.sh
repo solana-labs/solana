1cd8c1865e4e1a30849e586fd6f25178d9b0efda
Michael Vines <mvines@gmail.com>
2019-06-13 11:48:35 -0700

    Generate random passwords and keep them out of the environment/program args

--- metrics/scripts/stop.sh
+++ metrics/scripts/stop.sh
@@ -7,12 +7,11 @@ set -e
 
 for container in influxdb grafana; do
   if [ "$(docker ps -q -a -f name=$container)" ]; then
-  (
-    set +e
-    docker rm -f $container
-    exit 0
-  )
+    echo Stopping $container
+    (
+      set +e
+      docker rm -f $container
+      exit 0
+    )
   fi
 done
-
-echo Local metrics stopped
