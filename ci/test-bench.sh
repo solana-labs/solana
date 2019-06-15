1a550fc4f332e6ff245e93dd7ae3be2604710980
Jack May <jack@solana.com>
2019-06-14 18:39:41 -0700

    nudge

--- ci/test-bench.sh
+++ ci/test-bench.sh
@@ -41,8 +41,8 @@ BENCH_FILE=bench_output.log
 BENCH_ARTIFACT=current_bench_results.log
 
 # Clear the C dependency files, if dependeny moves these files are not regenerated
-find target/debug/bpf -name '*.d' -delete
-find target/release/bpf -name '*.d' -delete
+test -d target/debug/bpf find target/debug/bpf -name '*.d' -delete
+test -d target/release/bpf find target/release/bpf -name '*.d' -delete
 
 # Ensure all dependencies are built
 _ cargo +$rust_nightly build --all --release
