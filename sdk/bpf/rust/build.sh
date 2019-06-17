2a7a9fdf03ea623b5d40d1a6b81c528a32f6610a
Jack May <jack@solana.com>
2019-06-17 11:04:38 -0700

    Re-org SDK dir (#4690)

--- sdk/bpf/rust/build.sh
+++ sdk/bpf/rust/build.sh
@@ -23,8 +23,8 @@ set -e
 "$bpf_sdk"/scripts/install.sh
 
 # Use the SDK's version of llvm to build the compiler-builtins for BPF
-export CC="$bpf_sdk/llvm-native/bin/clang"
-export AR="$bpf_sdk/llvm-native/bin/llvm-ar"
+export CC="$bpf_sdk/dependencies/llvm-native/bin/clang"
+export AR="$bpf_sdk/dependencies/llvm-native/bin/llvm-ar"
 # Use the SDK's version of Rust to build for BPF
 export RUSTUP_TOOLCHAIN=bpf
 export RUSTFLAGS="
@@ -32,12 +32,12 @@ export RUSTFLAGS="
     -C lto=no \
     -C opt-level=2 \
     -C link-arg=-z -C link-arg=notext \
-    -C link-arg=-T$bpf_sdk/rust-utils/bpf.ld \
+    -C link-arg=-T$bpf_sdk/rust/bpf.ld \
     -C link-arg=--Bdynamic \
     -C link-arg=-shared \
     -C link-arg=--entry=entrypoint \
-    -C linker=$bpf_sdk/llvm-native/bin/ld.lld"
-export XARGO_RUST_SRC="$bpf_sdk/rust-bpf-sysroot/src"
+    -C linker=$bpf_sdk/dependencies/llvm-native/bin/ld.lld"
+export XARGO_RUST_SRC="$bpf_sdk/dependencies/rust-bpf-sysroot/src"
 xargo build --target bpfel-unknown-unknown --release -v
 
 { { set +x; } 2>/dev/null; echo Success; }
