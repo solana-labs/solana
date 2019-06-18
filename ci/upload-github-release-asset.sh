bd633d2b8155e8630c832895ea99392b9b01c242
Michael Vines <mvines@gmail.com>
2019-06-17 20:42:09 -0700

    Add CI_REPO_SLUG (#4714)
    automerge

--- ci/upload-github-release-asset.sh
+++ ci/upload-github-release-asset.sh
@@ -8,8 +8,6 @@
 #
 set -e
 
-REPO_SLUG=solana-labs/solana
-
 if [[ -z $1 ]]; then
   echo No files specified
   exit 1
@@ -25,20 +23,25 @@ if [[ -z $CI_TAG ]]; then
   exit 1
 fi
 
+if [[ -z $CI_REPO_SLUG ]]; then
+  echo Error: CI_REPO_SLUG not defined
+  exit 1
+fi
+
 releaseId=$( \
-  curl -s "https://api.github.com/repos/$REPO_SLUG/releases/tags/$CI_TAG" \
+  curl -s "https://api.github.com/repos/$CI_REPO_SLUG/releases/tags/$CI_TAG" \
   | grep -m 1 \"id\": \
   | sed -ne 's/^[^0-9]*\([0-9]*\),$/\1/p' \
 )
 echo "Github release id for $CI_TAG is $releaseId"
 
 for file in "$@"; do
-  echo "--- Uploading $file to tag $CI_TAG of $REPO_SLUG"
+  echo "--- Uploading $file to tag $CI_TAG of $CI_REPO_SLUG"
   curl \
     --data-binary @"$file" \
     -H "Authorization: token $GITHUB_TOKEN" \
     -H "Content-Type: application/octet-stream" \
-    "https://uploads.github.com/repos/$REPO_SLUG/releases/$releaseId/assets?name=$(basename "$file")"
+    "https://uploads.github.com/repos/$CI_REPO_SLUG/releases/$releaseId/assets?name=$(basename "$file")"
   echo
 done
 
