63503ad589919e5a8fcaffbb96fda7395e588fe2
Rob Walker <rob@solana.com>
2019-06-19 17:36:25 -0700

    remove build --all (#4737)
    * remove build  all
    
    * Update test-stable.sh

--- ci/test-stable.sh
+++ ci/test-stable.sh
@@ -31,8 +31,6 @@ testName=$(basename "$0" .sh)
 case $testName in
 test-stable)
   echo "Executing $testName"
-
-  _ cargo +"$rust_stable" build --all ${V:+--verbose}
   _ cargo +"$rust_stable" test --all ${V:+--verbose} -- --nocapture --test-threads=1
   ;;
 test-stable-perf)
@@ -75,7 +73,6 @@ test-stable-perf)
   fi
 
   # Run root package library tests
-  _ cargo +"$rust_stable" build --all ${V:+--verbose} --features="$ROOT_FEATURES"
   _ cargo +"$rust_stable" test --manifest-path=core/Cargo.toml ${V:+--verbose} --features="$ROOT_FEATURES" -- --nocapture --test-threads=1
   ;;
 *)
