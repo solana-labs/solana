d543b75b69d55f6efba19e7026f58d0a8a37c1fd
Jack May <jack@solana.com>
2019-06-21 01:29:47 -0700

    nudge

--- programs/bpf/rust/128bit/src/lib.rs
+++ programs/bpf/rust/128bit/src/lib.rs
@@ -11,22 +11,30 @@ use solana_sdk_bpf_utils::info;
 pub extern "C" fn entrypoint(_input: *mut u8) -> bool {
     let x: u128 = 1;
     let y = x.rotate_right(1);
-    assert_eq!(y, 170141183460469231731687303715884105728);
-    
-    assert_eq!(u128::max_value(), 340282366920938463463374607431768211455);
+    assert_eq!(y, 170_141_183_460_469_231_731_687_303_715_884_105_728);
+
+    assert_eq!(
+        u128::max_value(),
+        340_282_366_920_938_463_463_374_607_431_768_211_455
+    );
 
     let mut z = u128::max_value();
     z -= 1;
-    assert_eq!(z, 340282366920938463463374607431768211454);
+    assert_eq!(z, 340_282_366_920_938_463_463_374_607_431_768_211_454);
 
     let x = u64::max_value();
-    let y = u64::max_value();
-    assert_eq!(x as u128 + y as u128, 36893488147419103230);
+    assert_eq!(u128::from(x) + u128::from(x), 36_893_488_147_419_103_230);
 
-    let x = solana_bpf_rust_128bit_dep::work(u64::max_value() as u128, u64::max_value() as u128);
+    let x = solana_bpf_rust_128bit_dep::work(
+        u128::from(u64::max_value()),
+        u128::from(u64::max_value()),
+    );
     assert_eq!(x.wrapping_shr(64) as u64, 1);
-    assert_eq!(x.wrapping_shl(64).wrapping_shr(64) as u64, 0xfffffffffffffffe);
-    assert_eq!(x, 0x1fffffffffffffffe);
+    assert_eq!(
+        x.wrapping_shl(64).wrapping_shr(64) as u64,
+        0xffff_ffff_ffff_fffe
+    );
+    assert_eq!(x, 0xffff_ffff_ffff_fffe);
 
     info!("Success");
     true
