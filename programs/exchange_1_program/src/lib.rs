be992350ae8d56160e6e373badb533645530f682
patrick <patrick@solana.com>
2019-06-25 16:36:19 -0600

    update program/api names, dependencies

--- programs/exchange_1_program/src/lib.rs
+++ programs/exchange_1_program/src/lib.rs
@@ -1,12 +1,12 @@
 #[macro_export]
-macro_rules! solana_exchange_program {
+macro_rules! solana_exchange_1_program {
     () => {
         (
-            "solana_exchange_program".to_string(),
-            solana_exchange_api::id(),
+            "solana_exchange_1_program".to_string(),
+            solana_exchange_1_api::id(),
         )
     };
 }
-use solana_exchange_api::exchange_processor::process_instruction;
+use solana_exchange_1_api::exchange_processor::process_instruction;
 
 solana_sdk::solana_entrypoint!(process_instruction);
