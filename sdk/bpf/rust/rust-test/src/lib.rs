c47eb5af0b00c97347a01e172d8554345f40bf2c
Jack May <jack@solana.com>
2019-07-08 17:31:31 -0700

    clippy

--- sdk/bpf/rust/rust-test/src/lib.rs
+++ sdk/bpf/rust/rust-test/src/lib.rs
@@ -1,9 +1,9 @@
 //! @brief Solana Rust-based BPF program utility functions and types
 
 #[no_mangle]
-pub fn sol_log_(message: *const u8, length: u64) {
+pub unsafe fn sol_log_(message: *const u8, length: u64) {
     std::println!("sol_log_");
-    let slice = unsafe { std::slice::from_raw_parts(message, length as usize) };
+    let slice = std::slice::from_raw_parts(message, length as usize);
     let string = std::str::from_utf8(&slice).unwrap();
     std::println!("{}", string);
 }
