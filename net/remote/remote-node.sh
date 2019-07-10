dc6da6fcca5aa2aa9ea195206acb30b89a67a31d
Michael Vines <mvines@gmail.com>
2019-07-10 09:33:10 -0700

    Bump @solana/blockexplorer to v1.17.2

--- net/remote/remote-node.sh
+++ net/remote/remote-node.sh
@@ -231,7 +231,7 @@ local|tar)
       fi
 
       export BLOCKEXPLORER_GEOIP_WHITELIST=$PWD/net/config/geoip.yml
-      npm install @solana/blockexplorer@1.10.3
+      npm install @solana/blockexplorer@1.17.2
       npx solana-blockexplorer > blockexplorer.log 2>&1 &
 
       # Confirm the blockexplorer is accessible
