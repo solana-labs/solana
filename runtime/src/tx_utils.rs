d6604e1a9e3e552098d40622f3c89c4244a46171
Parth <desaiparth08@gmail.com>
2019-07-11 00:57:00 +0530

    adding tx_utils test

--- runtime/src/tx_utils.rs
+++ runtime/src/tx_utils.rs
@@ -37,3 +37,39 @@ impl<'a, T> Iterator for OrderedIterator<'a, T> {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_ordered_iterator_custom_order() {
+        let vec: Vec<usize> = vec![1,2,3,4];
+        let custom_order: Vec<usize> = vec![3,1,0,2];
+
+        let ordered_iterator = OrderedIterator::new(&vec, Some(&custom_order));
+        let expected_response: Vec<usize> = vec![4,2,1,3];
+
+        let resp : Vec<(&usize, &usize)> = ordered_iterator
+            .zip(expected_response.iter())
+            .filter(|(actual_elem, expected_elem)| {
+                *actual_elem == *expected_elem
+            }).collect();
+
+        assert_eq!(resp.len(), custom_order.len());
+    }
+
+    #[test]
+    fn test_ordered_iterator_original_order() {
+        let vec: Vec<usize> = vec![1,2,3,4];
+        let ordered_iterator = OrderedIterator::new(&vec, None);
+
+        let resp : Vec<(&usize, &usize)> = ordered_iterator
+            .zip(vec.iter())
+            .filter(|(actual_elem, expected_elem)| {
+                *actual_elem == *expected_elem
+            }).collect();
+
+        assert_eq!(resp.len(), vec.len());
+    }
+}
