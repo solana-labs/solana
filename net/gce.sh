d6fb42976a67d36de5c2f6c2dc108a2fdd873e88
Dan Albert <dan@solana.com>
2019-07-11 17:01:16 -0600

    bypass quoted long arg

--- net/gce.sh
+++ net/gce.sh
@@ -159,6 +159,9 @@ while [[ -n $1 ]]; do
     elif [[ $1 = --fullnode-additional-disk-size-gb ]]; then
       fullNodeAdditionalDiskSizeInGb="$2"
       shift 2
+    elif [[ $1 == --machine-type* ]]; then # Bypass quoted long args for GPUs
+      shortArgs+=("$1")
+      shift
     else
       usage "Unknown long option: $1"
     fi
