089dd101f29e0de420d91c3ff4e61dbae06b9f7b
Parth <desaiparth08@gmail.com>
2019-07-11 13:26:38 +0530

    styling fix

--- runtime/benches/transaction_utils.rs
+++ runtime/benches/transaction_utils.rs
@@ -2,10 +2,10 @@
 
 extern crate test;
 
-use test::Bencher;
-use solana_runtime::transaction_utils::OrderedIterator;
 use rand::seq::SliceRandom;
 use rand::thread_rng;
+use solana_runtime::transaction_utils::OrderedIterator;
+use test::Bencher;
 
 #[bench]
 fn bench_ordered_iterator_with_order_shuffling(bencher: &mut Bencher) {
@@ -13,6 +13,7 @@ fn bench_ordered_iterator_with_order_shuffling(bencher: &mut Bencher) {
     bencher.iter(|| {
         let mut order: Vec<usize> = (0..100_usize).collect();
         order.shuffle(&mut thread_rng());
-        let ordered_iterator_resp: Vec<&usize> = OrderedIterator::new(&vec, Some(&order)).collect();
+        let _ordered_iterator_resp: Vec<&usize> =
+            OrderedIterator::new(&vec, Some(&order)).collect();
     });
 }
