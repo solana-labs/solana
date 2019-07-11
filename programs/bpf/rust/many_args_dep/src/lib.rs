2860d2fe2792c041bd57d41612ca50d59380ab54
Jack May <jack@solana.com>
2019-07-11 14:27:18 -0800

    Pull in support for Rust-BPF stack argument passing (#5038)

--- programs/bpf/rust/many_args_dep/src/lib.rs
+++ programs/bpf/rust/many_args_dep/src/lib.rs
@@ -23,6 +23,34 @@ pub fn many_args(
     arg1 + arg2 + arg3 + arg4 + arg5 + arg6 + arg7 + arg8 + arg9
 }
 
+#[derive(Debug, PartialEq)]
+pub struct Ret {
+    pub group1: u128,
+    pub group2: u128,
+    pub group3: u128,
+}
+
+pub fn many_args_sret(
+    arg1: u64,
+    arg2: u64,
+    arg3: u64,
+    arg4: u64,
+    arg5: u64,
+    arg6: u64,
+    arg7: u64,
+    arg8: u64,
+    arg9: u64,
+) -> Ret {
+    info!("Another package");
+    info!(arg1, arg2, arg3, arg4, arg5);
+    info!(arg6, arg7, arg8, arg9, 0);
+    Ret {
+        group1: u128::from(arg1) + u128::from(arg2) + u128::from(arg3),
+        group2: u128::from(arg4) + u128::from(arg5) + u128::from(arg6),
+        group3: u128::from(arg7) + u128::from(arg8) + u128::from(arg9),
+    }
+}
+
 #[cfg(test)]
 mod test {
     extern crate std;
