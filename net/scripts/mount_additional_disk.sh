778dcc7e8b7fd78c7076f2effdecffef827d4d01
Dan Albert <dan@solana.com>
2019-07-11 11:03:07 -0600

    Shellcheck

--- net/scripts/mount_additional_disk.sh
+++ net/scripts/mount_additional_disk.sh
@@ -2,17 +2,17 @@
 set -x
 
 disk=sdb
-if [[ -z $(lsblk | grep ${disk}) ]]; then
+if ! lsblk | grep -q ${disk} ; then
   echo "${disk} does not exist"
 else
-  if [[ -n $(mount | grep ${disk}) ]]; then
+  if mount | grep -q ${disk} ; then
     echo "${disk} is already mounted"
   else
     sudo mkfs.ext4 -F /dev/"$disk"
     sudo mkdir -p /mnt/disks/"$disk"
     sudo mount /dev/"$disk" /mnt/disks/"$disk"
     sudo chmod a+w /mnt/disks/"$disk"
-    if [[ -z $(mount | grep ${disk}) ]]; then
+    if ! mount | grep -q ${disk} ; then
       echo "${disk} failed to mount!"
       exit 1
     fi
