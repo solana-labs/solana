29dded3dd38fc24678ee3307750eb2d3146161cc
Parth <desaiparth08@gmail.com>
2019-07-11 13:17:24 +0530

    add benchmarking code for OrderedIterator and Tx shuffling

---
+++ runtime/benches/transaction_utils.rs
@@ -0,0 +1,18 @@
+#![feature(test)]
+
+extern crate test;
+
+use test::Bencher;
+use solana_runtime::transaction_utils::OrderedIterator;
+use rand::seq::SliceRandom;
+use rand::thread_rng;
+
+#[bench]
+fn bench_ordered_iterator_random_order(bencher: &mut Bencher) {
+    let vec: Vec<usize> = (0..100_usize).collect();
+    bencher.iter(|| {
+        let mut order: Vec<usize> = (0..100_usize).collect();
+        order.shuffle(&mut thread_rng());
+        let ordered_iterator_resp: Vec<&usize> = OrderedIterator::new(&vec, Some(&order)).collect();
+    });
+}
