9b1b0d0cf4391df996b36b6ab06d6751102e9ba0
Rob Walker <rob@solana.com>
2019-07-12 16:07:23 -0700

    nit

--- sdk/src/sysvar/clock.rs
+++ sdk/src/sysvar/clock.rs
@@ -6,12 +6,12 @@ use bincode::serialized_size;
 
 pub use crate::timing::{Epoch, Slot};
 
-crate::solana_name_id!(ID, "SysvarC1ock11111111111111111111111111111111");
-
 const ID: [u8; 32] = [
     6, 167, 213, 23, 24, 199, 116, 201, 40, 86, 99, 152, 105, 29, 94, 182, 139, 94, 184, 163, 155,
     75, 109, 92, 115, 85, 91, 33, 0, 0, 0, 0,
 ];
+
+crate::solana_name_id!(ID, "SysvarC1ock11111111111111111111111111111111");
 
 #[repr(C)]
 #[derive(Serialize, Deserialize, Debug, Default, PartialEq)]
