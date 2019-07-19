a8a9907acecf80f2cd43a36011b9723d067b7fb6
Michael Vines <mvines@gmail.com>
2019-07-19 08:31:39 -0700

    Disable restart

--- net/remote/remote-sanity.sh
+++ net/remote/remote-sanity.sh
@@ -158,7 +158,7 @@ echo "--- $sanityTargetIp: validator sanity"
 if $validatorSanity; then
   (
     set -x -o pipefail
-    timeout 10s ./multinode-demo/validator-x.sh --stake 0 \
+    timeout 10s ./multinode-demo/validator-x.sh --no-restart --stake 0 \
       "$entrypointRsyncUrl" \
       "$sanityTargetIp:8001" 2>&1 | tee validator-sanity.log
   ) || {
