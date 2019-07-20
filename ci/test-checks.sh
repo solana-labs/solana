d6372a930c369c63c877f0da8e5dc52251318cb1
Rob Walker <rob@solana.com>
2019-07-20 15:50:26 -0700

    disable audit until crossbeam epoch release (#5208)

--- ci/test-checks.sh
+++ ci/test-checks.sh
@@ -14,7 +14,7 @@ do_bpf_check() {
         _ cargo +"$rust_stable" fmt --all -- --check
         _ cargo +"$rust_nightly" clippy --all -- --version
         _ cargo +"$rust_nightly" clippy --all -- --deny=warnings
-        _ cargo +"$rust_stable" audit
+#        _ cargo +"$rust_stable" audit
 }
 
 (
@@ -33,7 +33,7 @@ do_bpf_check() {
 _ cargo +"$rust_stable" fmt --all -- --check
 _ cargo +"$rust_stable" clippy --all -- --version
 _ cargo +"$rust_stable" clippy --all -- --deny=warnings
-_ cargo +"$rust_stable" audit
+#_ cargo +"$rust_stable" audit
 _ ci/nits.sh
 _ ci/order-crates-for-publishing.py
 _ book/build.sh
