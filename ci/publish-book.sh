f2f10da8c96b091822359a18d3da83fb76119374
Dan Albert <dan@solana.com>
2019-07-22 12:02:09 -0600

    Trigger buildkite build on correct tag

--- ci/publish-book.sh
+++ ci/publish-book.sh
@@ -2,6 +2,47 @@
 set -e
 
 cd "$(dirname "$0")/.."
+BOOK="book"
+
+eval "$(ci/channel-info.sh)"
+
+if [[ -n $PUBLISH_BOOK_TAG ]]; then
+  CURRENT_TAG="$(git describe --tags)"
+  COMMIT_TO_PUBLISH="$(git rev-list -n 1 ${PUBLISH_BOOK_TAG})"
+
+  # book is manually published at a specified release tag
+  if [[ $PUBLISH_BOOK_TAG != $CURRENT_TAG ]]; then
+    (
+      cat <<EOF
+steps:
+  - trigger: "$BUILDKITE_PIPELINE_SLUG"
+    async: true
+    build:
+      message: "$BUILDKITE_MESSAGE"
+      commit: "$COMMIT_TO_PUBLISH"
+      env:
+        PUBLISH_BOOK_TAG: "$PUBLISH_BOOK_TAG"
+EOF
+    ) | buildkite-agent pipeline upload
+    exit 0
+  fi
+  repo=git@github.com:solana-labs/book.git
+else
+  # book-edge and book-beta are published automatically on the tip of the branch
+  case $CHANNEL in
+  edge)
+    repo=git@github.com:solana-labs/book-edge.git
+    ;;
+  beta)
+    repo=git@github.com:solana-labs/book-beta.git
+    ;;
+  *)
+   echo "--- publish skipped"
+   exit 0
+   ;;
+  esac
+  BOOK=$CHANNEL
+fi
 
 book/build.sh
 
