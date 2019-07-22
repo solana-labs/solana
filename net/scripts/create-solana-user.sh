2a2110c8fe08803b1580daeb8ac830d4d27484cf
Dan Albert <dan@solana.com>
2019-07-22 15:22:30 -0600

    Do not attempt to create solana user multiple times

--- net/scripts/create-solana-user.sh
+++ net/scripts/create-solana-user.sh
@@ -4,26 +4,29 @@ set -ex
 [[ $(uname) = Linux ]] || exit 1
 [[ $USER = root ]] || exit 1
 
-adduser solana --gecos "" --disabled-password --quiet
-adduser solana sudo
-adduser solana adm
-echo "solana ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
-id solana
+if grep -q solana /etc/passwd ; then
+  echo "User solana already exists"
+else
+  adduser solana --gecos "" --disabled-password --quiet
+  adduser solana sudo
+  adduser solana adm
+  echo "solana ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
+  id solana
 
-[[ -r /solana-id_ecdsa ]] || exit 1
-[[ -r /solana-id_ecdsa.pub ]] || exit 1
+  [[ -r /solana-id_ecdsa ]] || exit 1
+  [[ -r /solana-id_ecdsa.pub ]] || exit 1
 
-sudo -u solana bash -c "
-  mkdir -p /home/solana/.ssh/
-  cd /home/solana/.ssh/
-  cp /solana-id_ecdsa.pub authorized_keys
-  umask 377
-  cp /solana-id_ecdsa id_ecdsa
-  echo \"
-    Host *
-    BatchMode yes
-    IdentityFile ~/.ssh/id_ecdsa
-    StrictHostKeyChecking no
-  \" > config
-"
-
+  sudo -u solana bash -c "
+    mkdir -p /home/solana/.ssh/
+    cd /home/solana/.ssh/
+    cp /solana-id_ecdsa.pub authorized_keys
+    umask 377
+    cp /solana-id_ecdsa id_ecdsa
+    echo \"
+      Host *
+      BatchMode yes
+      IdentityFile ~/.ssh/id_ecdsa
+      StrictHostKeyChecking no
+    \" > config
+  "
+fi
