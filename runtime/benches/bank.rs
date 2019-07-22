264a3d7ddebf0cb7236a72459c817cdf42f37030
sakridge <stephen@solana.com>
2019-07-22 13:57:28 -0700

    Increase ticks_per_slot for banking benchmark (#5221)

--- runtime/benches/bank.rs
+++ runtime/benches/bank.rs
@@ -120,7 +120,8 @@ fn do_bench_transactions(
 ) {
     solana_logger::setup();
     let ns_per_s = 1_000_000_000;
-    let (genesis_block, mint_keypair) = create_genesis_block(100_000_000);
+    let (mut genesis_block, mint_keypair) = create_genesis_block(100_000_000);
+    genesis_block.ticks_per_slot = 100;
     let mut bank = Bank::new(&genesis_block);
     bank.add_instruction_processor(Pubkey::new(&BUILTIN_PROGRAM_ID), process_instruction);
     bank.register_native_instruction_processor(
