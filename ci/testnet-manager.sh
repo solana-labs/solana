1d6bafbc77f32c5aa6dae1094b60b1becbc224be
Michael Vines <mvines@gmail.com>
2019-07-31 14:18:05 -0700

    Move tds to edge (#5366)

--- ci/testnet-manager.sh
+++ ci/testnet-manager.sh
@@ -156,8 +156,8 @@ testnet-demo)
   : "${GCE_LOW_QUOTA_NODE_COUNT:=70}"
   ;;
 tds)
-  CHANNEL_OR_TAG=beta
-  CHANNEL_BRANCH=$BETA_CHANNEL
+  CHANNEL_OR_TAG=edge
+  CHANNEL_BRANCH=$EDGE_CHANNEL
   ;;
 *)
   echo "Error: Invalid TESTNET=$TESTNET"
