9b6283b2a2c082bb169add010e9a969e2313c885
Jack May <jack@solana.com>
2019-07-30 23:02:09 -0700

    nit

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -21,12 +21,5 @@ mod system_instruction_processor;
 #[macro_use]
 extern crate solana_metrics;
 
-// #[macro_use]
-// extern crate solana_bpf_loader_program;
-// #[macro_use]
-// extern crate solana_stake_program;
-// #[macro_use]
-// extern crate solana_vote_program;
-
 #[macro_use]
 extern crate serde_derive;
