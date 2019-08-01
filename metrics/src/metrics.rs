3ab1b46ef7655ba36dd65f737aeec4dcfec5de62
Justin Starry <jstarry@users.noreply.github.com>
2019-08-01 09:11:49 -0400

    Fix vote metrics (#5377)

--- metrics/src/metrics.rs
+++ metrics/src/metrics.rs
@@ -337,7 +337,6 @@ impl MetricsAgent {
     }
 
     pub fn submit(&self, mut point: influxdb::Point, level: log::Level) {
-        point.add_tag("host_id", influxdb::Value::String(HOST_ID.to_string()));
         if point.timestamp.is_none() {
             point.timestamp = Some(timing::timestamp() as i64);
         }
