687818aad61fc999f49caccda1c10630e3b8cfc9
TristanDebrunner <tristan@solana.com>
2019-08-12 16:41:17 -0600

    Run sdk-c through clippy separately (#5504)

--- ci/test-checks.sh
+++ ci/test-checks.sh
@@ -11,8 +11,13 @@ export RUST_BACKTRACE=1
 export RUSTFLAGS="-D warnings"
 
 _ cargo +"$rust_stable" fmt --all -- --check
+
+# Clippy gets stuck for unknown reasons if sdk-c is included in the build, so check it separately.
+# See https://github.com/solana-labs/solana/issues/5503
 _ cargo +"$rust_stable" clippy --version
-_ cargo +"$rust_stable" clippy --all -- --deny=warnings
+_ cargo +"$rust_stable" clippy --all --exclude solana-sdk-c -- --deny=warnings
+_ cargo +"$rust_stable" clippy --manifest-path sdk-c/Cargo.toml -- --deny=warnings
+
 _ cargo +"$rust_stable" audit --version
 _ cargo +"$rust_stable" audit
 _ ci/nits.sh
