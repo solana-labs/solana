65bae82d3f2601d78169431fcbf618057214fa6c
pat <patrick@solana.com>
2019-08-20 14:41:17 -0600

    add btc_spv_bin and top lvl workspace entry

---
+++ programs/btc_spv_bin/src/blockheade.rs
@@ -0,0 +1,76 @@
+use reqwest;
+use clap;
+use serde::Deserialize;
+use clap::{Arg, App};
+
+// pub type blockHash = [u8; 32];
+pub type BlockHeader = [u8; 80];
+
+#[derive(Deserialize)]
+struct JsonBH {
+    hash: String,
+    ver: u16,
+    prev_block: String,
+    mrkl_root: String,
+    time: u64,
+    bits: u64,
+    nonce: u64,
+    n_tx: u64,
+    size: u64,
+    block_index: u64,
+    main_chain: bool,
+    height: u64,
+    received_time: u64,
+    relayed_by: String,
+}
+
+fn GetHeaderJson(hash: &str) -> JsonBH {
+    let qs   = format!("https://www.blockchain.info/rawblock/{}", hash);
+    let body = reqwest::get(&qs);
+    match body {
+        Err(e) => panic!("rest request failed {}", e),
+        Ok(mut n) => {
+            if n.status().is_success() {
+                let jsonbh: JsonBH = n.json().unwrap();
+                jsonbh
+            }
+            else {
+                panic!("request failed");
+            }
+         }
+    }
+}
+
+fn GetHeaderRaw(hash: &str) -> String {
+    let qs   = format!("https://blockchain.info/block/{}?format=hex", hash);
+    let body = reqwest::get(&qs);
+    match body {
+        Err(e) => panic!("rest request failed {}", e),
+        Ok(mut n) => {
+            if n.status().is_success(){
+                let textbh: String = n.text().unwrap();
+                let hs = &textbh[0..160]; // 160 characters since it's in hex format
+                let header: String = hs.to_string();
+                header
+            }
+            else {
+                panic!("request failed");
+            }
+        }
+    }
+}
+
+fn main() {
+    let matches = App::new("header fetch util")
+                        .arg(Arg::with_name("blockhash"))
+                            .help("block hash to get header from")
+                        .get_matches();
+
+    let testhash  = "0000000000000bae09a7a393a8acded75aa67e46cb81f7acaa5ad94f9eacd103";
+    let blockhash = matches.value_of("blockhash").unwrap_or(testhash);
+    let headerraw = GetHeaderRaw(&blockhash);
+    println!("header - {}", headerraw);
+    println!("hash   - {}", blockhash);
+    println!("length - {}", headerraw.len());
+    // println!("{}", std::str::from_utf8(&header));
+}
