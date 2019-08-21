3cfa51cbe8920b23c713debe3177c2202cd5ed9d
Trent Nelson <trent@solana.com>
2019-08-20 21:51:53 -0600

    Extract/install NVIDIA persistenced in a proper tempdir

--- net/datacenter-node-install/setup-dc-node-2.sh
+++ net/datacenter-node-install/setup-dc-node-2.sh
@@ -12,6 +12,11 @@ set -xe
 "$HERE"/setup-cuda.sh
 
 # setup persistence mode across reboots
-tar -xvf /usr/share/doc/NVIDIA_GLX-1.0/sample/nvidia-persistenced-init.tar.bz2
-"$HERE"/nvidia-persistenced-init/install.sh systemd
-rm -r nvidia-persistenced-init
+TMPDIR="$(mktemp)"
+mkdir -p "$TMPDIR"
+if pushd "$TMPDIR"; then
+  tar -xvf /usr/share/doc/NVIDIA_GLX-1.0/sample/nvidia-persistenced-init.tar.bz2
+  ./nvidia-persistenced-init/install.sh systemd
+  popd
+  rm -rf "$TMPDIR"
+fi
