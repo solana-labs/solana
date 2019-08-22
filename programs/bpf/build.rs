9502082cdaf2be6b28b365be645c52e0f9c7da4d
Justin Starry <jstarry@users.noreply.github.com>
2019-08-22 14:47:25 -0400

    Revert "Add test program for BPF memory corruption bug (#5603)"
    This reverts commit 63d62c33c611e74a6c54ba00d350ecedd0073d12.

--- programs/bpf/build.rs
+++ programs/bpf/build.rs
@@ -76,8 +76,6 @@ fn main() {
             "external_spend",
             "noop",
             "panic",
-            // ISSUE: https://github.com/solana-labs/solana/issues/5602
-            // "stack_bug",
             "tick_height",
         ];
         for program in rust_programs.iter() {
