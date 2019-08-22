b6d494656db8e9bad826adf4981a8f6c479172ba
Jack May <jack@solana.com>
2019-08-22 15:59:15 -0700

    test

--- ci/test-stable.sh
+++ ci/test-stable.sh
@@ -58,7 +58,7 @@ test-stable-perf)
   _ make -C programs/bpf/c tests
   _ export RUST_LOG=solana_rbpf=trace; cargo +"$rust_stable" test \
     --manifest-path programs/bpf/Cargo.toml \
-    --no-default-features --features=bpf_c,bpf_rust
+    --no-default-features --features=bpf_c,bpf_rust -- --nocapture test_program_bpf_c
 
   # # Run root package tests with these features
   # ROOT_FEATURES=
