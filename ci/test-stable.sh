e88023da8f530b59ef9527aa40441cb28341c866
Sagar Dhawan <sagar@solana.com>
2019-09-11 09:26:59 -0700

    restrict ci to local_cluster only

--- ci/test-stable.sh
+++ ci/test-stable.sh
@@ -33,7 +33,7 @@ test-stable)
   echo "Executing $testName"
 
   _ cargo +"$rust_stable" build --all --tests --bins ${V:+--verbose}
-  _ cargo +"$rust_stable" test --all ${V:+--verbose} -- --nocapture --test-threads=1
+  _ cargo +"$rust_stable" test  --package solana-local-cluster --test local_cluster ${V:+--verbose} -- --nocapture --test-threads=1
   ;;
 test-stable-perf)
   echo "Executing $testName"
@@ -77,7 +77,9 @@ test-stable-perf)
 
   # Run root package library tests
   _ cargo +"$rust_stable" build --all --tests --bins ${V:+--verbose} --features="$ROOT_FEATURES"
-  _ cargo +"$rust_stable" test --manifest-path=core/Cargo.toml ${V:+--verbose} --features="$ROOT_FEATURES" -- --nocapture --test-threads=1
+  _ cargo +"$rust_stable" test  --package solana-local-cluster --test local_cluster --manifest-path=core/Cargo.toml ${V:+--verbose} --features="$ROOT_FEATURES" -- --nocapture --test-threads=1
+#  _ cargo +"$rust_stable" test --manifest-path=core/Cargo.toml ${V:+--verbose} --features="$ROOT_FEATURES" -- --nocapture --test-threads=1
+#  _ cargo +"$rust_stable" test --manifest-path=core/Cargo.toml ${V:+--verbose} --features="$ROOT_FEATURES" -- --nocapture --test-threads=1
   ;;
 *)
   echo "Error: Unknown test: $testName"
