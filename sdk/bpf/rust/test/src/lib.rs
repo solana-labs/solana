fbd8b005b262b0c720edeefbfaa6b9e9da46ad24
Jack May <jack@solana.com>
2019-09-18 16:44:57 -0700

    nudge

--- sdk/bpf/rust/test/src/lib.rs
+++ sdk/bpf/rust/test/src/lib.rs
@@ -1,15 +1,13 @@
   //! @brief Solana Rust-based BPF program utility functions and types
 
-#[allow(dead_code)]
 #[no_mangle]
-pub unsafe extern "C" fn sol_log_(message: *const u8, length: u64) {
+pub unsafe fn sol_log_(message: *const u8, length: u64) {
     let slice = std::slice::from_raw_parts(message, length as usize);
     let string = std::str::from_utf8(&slice).unwrap();
     std::println!("{}", string);
 }
 
-#[allow(dead_code)]
 #[no_mangle]
-pub extern "C" fn sol_log_64_(arg1: u64, arg2: u64, arg3: u64, arg4: u64, arg5: u64) {
+pub fn sol_log_64_(arg1: u64, arg2: u64, arg3: u64, arg4: u64, arg5: u64) {
     std::println!("{} {} {} {} {}", arg1, arg2, arg3, arg4, arg5);
 }
