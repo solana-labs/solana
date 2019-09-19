962c06fff4ff7ad56a9f1a2d3cba8bef4fe56ced
Michael Vines <mvines@gmail.com>
2019-09-18 21:47:53 -0700

    Set SOLANA_CUDA=1 explictly

--- multinode-demo/common.sh
+++ multinode-demo/common.sh
@@ -56,7 +56,7 @@ else
       maybe_release=--release
     fi
     declare manifest_path="--manifest-path=$SOLANA_ROOT/$crate/Cargo.toml"
-    printf "cargo run $manifest_path $maybe_release $maybe_package --bin %s %s -- " "$program" "$features"
+    printf "cargo $CARGO_TOOLCHAIN run $manifest_path $maybe_release $maybe_package --bin %s %s -- " "$program" "$features"
   }
 fi
 
