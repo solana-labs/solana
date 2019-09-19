9917ece82692fa809ec5ab885958843c359bc7dc
Michael Vines <mvines@gmail.com>
2019-09-19 10:35:06 -0700

    Kill the old blockexplorer harder

--- net/remote/remote-node.sh
+++ net/remote/remote-node.sh
@@ -271,8 +271,15 @@ EOF
       cat > ~/solana/restart-explorer <<EOF
 #!/bin/bash -ex
       cd ~/solana
-      npm install @solana/blockexplorer@1
+
+      if [[ -f blockexplorer.pid ]]; then
+        pgid=\$(ps opgid= \$(cat blockexplorer.pid) | tr -d '[:space:]')
+        if [[ -n \$pgid ]]; then
+          kill -- -\$pgid
+        fi
+      fi
       killall node || true
+      npm install @solana/blockexplorer@1
       export BLOCKEXPLORER_GEOIP_WHITELIST=$PWD/net/config/geoip.yml
       npx solana-blockexplorer > blockexplorer.log 2>&1 &
       echo \$! > blockexplorer.pid
