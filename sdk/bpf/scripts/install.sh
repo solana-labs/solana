abaccd68828b6755f8b71e1c3040a7db9fc1d70d
Jack May <jack@solana.com>
2019-09-20 11:21:01 -0700

    Pull in Rust-BPF v0.1.6 (#5997)
    automerge

--- sdk/bpf/scripts/install.sh
+++ sdk/bpf/scripts/install.sh
@@ -80,7 +80,7 @@ if [[ ! -f llvm-native-$machine-$version.md ]]; then
 fi
 
 # Install Rust-BPF
-version=v0.1.5
+version=v0.1.6
 if [[ ! -f rust-bpf-$machine-$version.md ]]; then
   (
     filename=solana-rust-bpf-$machine.tar.bz2
