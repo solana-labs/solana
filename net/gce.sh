06b445ac070f8a3fa87210d45b1f1d0306b8b5a0
sakridge <sakridge@gmail.com>
2019-09-26 15:52:03 -0700

    Skip if --custom-cpu is used as well. (#6130)

--- net/gce.sh
+++ net/gce.sh
@@ -172,7 +172,7 @@ while [[ -n $1 ]]; do
     elif [[ $1 = --fullnode-additional-disk-size-gb ]]; then
       fullNodeAdditionalDiskSizeInGb="$2"
       shift 2
-    elif [[ $1 == --machine-type* ]]; then # Bypass quoted long args for GPUs
+    elif [[ $1 == --machine-type* || $1 == --custom-cpu* ]]; then # Bypass quoted long args for GPUs
       shortArgs+=("$1")
       shift
     else
