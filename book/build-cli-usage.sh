1eb8aa25a90c519ac4dbff77ee9bfa2d52866933
Rob Walker <rob@solana.com>
2019-09-27 21:31:08 -0700

    shellcheck

--- book/build-cli-usage.sh
+++ book/build-cli-usage.sh
@@ -12,7 +12,7 @@ cat src/api-reference/.cli.md > "$out"
 section() {
   declare mark=${2:-"###"}
   declare section=$1
-  read name rest <<<"$section"
+  read -r name rest <<<"$section"
 
   printf '%s %s
 ' "$mark" "$name"
@@ -26,7 +26,7 @@ section() {
 section "$usage" >> "$out"
 
 in_subcommands=0
-while read subcommand rest; do
+while read -r subcommand rest; do
   [[ $subcommand == "SUBCOMMANDS:" ]] && in_subcommands=1 && continue
   if ((in_subcommands)); then
       section "$(cargo -q run -p solana-cli -- help "$subcommand" | sed 's|'"$HOME"'|~|g')" "####" >> "$out"
