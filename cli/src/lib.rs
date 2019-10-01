160fbd87a7f060dc686a2f73e18155b771f4a153
Justin Starry <justin@solana.com>
2019-10-01 12:55:11 -0400

    Add native_token module to sdk

--- cli/src/lib.rs
+++ cli/src/lib.rs
@@ -9,11 +9,3 @@ pub mod stake;
 pub mod validator_info;
 pub mod vote;
 pub mod wallet;
-
-pub(crate) fn lamports_to_sol(lamports: u64) -> f64 {
-    lamports as f64 / 2u64.pow(34) as f64
-}
-
-pub(crate) fn sol_to_lamports(sol: f64) -> u64 {
-    (sol * 2u64.pow(34) as f64) as u64
-}
