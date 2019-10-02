c31ca1fc1ba35075d6d6ee7b509c12273ea03a11
Jack May <jack@solana.com>
2019-10-02 12:02:27 -0700

    fix: flow expose transactionInstruction (#499)

--- web3.js/module.flow.js
+++ web3.js/module.flow.js
@@ -225,7 +225,7 @@ declare module '@solana/web3.js' {
 
     constructor(opts?: TransactionCtorFields): Transaction;
     add(
-      ...items: Array<Transaction | TransactionInstructionCtorFields>
+      ...items: Array<Transaction | TransactionInstruction | TransactionInstructionCtorFields>
     ): Transaction;
     sign(...signers: Array<Account>): void;
     signPartial(...partialSigners: Array<PublicKey | Account>): void;
