e334b95117ca66153edff12905505b966effd74e
Greg Fitzgerald <greg@solana.com>
2019-10-04 13:20:50 -0600

    Apply review feedback

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -1,35 +1,5 @@
-pub use sealevel::accounts;
-pub use sealevel::accounts_db;
-pub use sealevel::accounts_index;
-pub use sealevel::append_vec;
-pub use sealevel::bank;
-pub use sealevel::bank_client;
-pub use sealevel::bloom;
-pub use sealevel::epoch_schedule;
-pub use sealevel::genesis_utils;
-pub use sealevel::loader_utils;
-pub use sealevel::message_processor;
-pub use sealevel::rent_collector;
-pub use sealevel::stakes;
-pub use sealevel::status_cache;
-pub use sealevel::storage_utils;
-pub use sealevel::transaction_batch;
-pub use sealevel::transaction_utils;
-//
-//#[macro_use]
-//extern crate solana_metrics;
-//
-//#[macro_use]
-//extern crate solana_vote_program;
-//
-//#[macro_use]
-//extern crate solana_stake_program;
-//
-//#[macro_use]
-//extern crate solana_bpf_loader_program;
-//
-//#[macro_use]
-//extern crate serde_derive;
-//
-//extern crate fs_extra;
-//extern crate tempfile;
+pub use sealevel::{
+    accounts, accounts_db, accounts_index, append_vec, bank, bank_client, bloom, epoch_schedule,
+    genesis_utils, loader_utils, message_processor, rent_collector, stakes, status_cache,
+    storage_utils, transaction_batch, transaction_utils,
+};
