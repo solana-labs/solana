65605979c3597c4fc5f560ca15781f4fb0cf6949
Justin Starry <justin@solana.com>
2019-10-06 20:08:05 -0400

    Refactor test

--- core/src/blocktree_processor.rs
+++ core/src/blocktree_processor.rs
@@ -993,15 +993,17 @@ pub mod tests {
     }
 
     #[test]
-    fn test_process_entries_with_callback() {
+    fn test_process_ledger_options_entry_callback() {
         let GenesisBlockInfo {
             genesis_block,
             mint_keypair,
             ..
         } = create_genesis_block(100);
-        let bank = Arc::new(Bank::new(&genesis_block));
+        let (ledger_path, last_entry_hash) = create_new_tmp_ledger!(&genesis_block);
+        let blocktree =
+            Blocktree::open(&ledger_path).expect("Expected to successfully open database ledger");
+        let blockhash = genesis_block.hash();
         let keypairs = [Keypair::new(), Keypair::new(), Keypair::new()];
-        let blockhash = bank.last_blockhash();
 
         let tx = system_transaction::create_user_account(
             &mint_keypair,
@@ -1009,7 +1011,7 @@ pub mod tests {
             1,
             blockhash,
         );
-        let entry_1 = next_entry(&blockhash, 1, vec![tx]);
+        let entry_1 = next_entry(&last_entry_hash, 1, vec![tx]);
 
         let tx = system_transaction::create_user_account(
             &mint_keypair,
@@ -1018,6 +1020,21 @@ pub mod tests {
             blockhash,
         );
         let entry_2 = next_entry(&entry_1.hash, 1, vec![tx]);
+
+        let mut entries = vec![entry_1, entry_2];
+        entries.extend(create_ticks(genesis_block.ticks_per_slot, last_entry_hash));
+        blocktree
+            .write_entries(
+                1,
+                0,
+                0,
+                genesis_block.ticks_per_slot,
+                None,
+                true,
+                &Arc::new(Keypair::new()),
+                &entries,
+            )
+            .unwrap();
 
         let callback_counter: Arc<RwLock<usize>> = Arc::default();
         let entry_callback = {
@@ -1031,12 +1048,13 @@ pub mod tests {
             })
         };
 
-        assert_eq!(
-            process_entries_with_callback(&bank, &[entry_1, entry_2], true, &Some(entry_callback)),
-            Ok(())
-        );
+        let opts = ProcessOptions {
+            override_num_threads: Some(1),
+            entry_callback: Some(entry_callback),
+            ..ProcessOptions::default()
+        };
+        process_blocktree(&genesis_block, &blocktree, None, opts).unwrap();
         assert_eq!(*callback_counter.write().unwrap(), 2);
-        assert_eq!(bank.last_blockhash(), blockhash);
     }
 
     #[test]
