d3cc5645c0526bb09594d1b78ceb8f0690078aa3
Justin Starry <justin@solana.com>
2019-10-15 18:10:36 -0400

    Fix recently added test

--- core/src/broadcast_stage/standard_broadcast_run.rs
+++ core/src/broadcast_stage/standard_broadcast_run.rs
@@ -420,7 +420,7 @@ mod test {
             entries: ticks.clone(),
             time_elapsed: Duration::new(3, 0),
             bank: bank0.clone(),
-            last_tick_height: (ticks.len() - 1) as u64,
+            last_tick_height: ticks.len() as u64,
         };
 
         let mut standard_broadcast_run = StandardBroadcastRun::new(leader_keypair);
