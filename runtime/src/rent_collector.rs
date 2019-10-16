40d60851523f37663bb66eb921c5d448e907e675
Sagar Dhawan <sagar@solana.com>
2019-10-16 12:41:53 -0700

    Revert "collect rent from accounts (take:2) (#6360)"
    This reverts commit c1b401a04ace4d83dee0d10923cae5a9a2d13460.

--- runtime/src/rent_collector.rs
+++ runtime/src/rent_collector.rs
@@ -35,9 +35,9 @@ impl RentCollector {
     // updates this account's lamports and status and returns
     //  the account rent collected, if any
     //
-    pub fn update(&self, account: &mut Account) -> u64 {
+    pub fn update(&self, mut account: Account) -> Option<(Account, u64)> {
         if account.data.is_empty() || account.rent_epoch > self.epoch {
-            0
+            Some((account, 0))
         } else {
             let slots_elapsed: u64 = (account.rent_epoch..=self.epoch)
                 .map(|epoch| self.epoch_schedule.get_slots_in_epoch(epoch + 1))
@@ -53,15 +53,13 @@ impl RentCollector {
                 if account.lamports > rent_due {
                     account.rent_epoch = self.epoch + 1;
                     account.lamports -= rent_due;
-                    rent_due
+                    Some((account, rent_due))
                 } else {
-                    let rent_charged = account.lamports;
-                    *account = Account::default();
-                    rent_charged
+                    None
                 }
             } else {
                 // maybe collect rent later, leave account alone
-                0
+                Some((account, 0))
             }
         }
     }
