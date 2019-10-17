854c62e20870affe3f2ce893b1a39b451540b292
Pankaj Garg <pankaj@solana.com>
2019-10-17 14:52:24 -0700

    Reduce kernel networking buffer for rmem and wmem (#6422)
    automerge

--- net/datacenter-node-install/setup-procfs-knobs.sh
+++ net/datacenter-node-install/setup-procfs-knobs.sh
@@ -11,10 +11,10 @@ ensure_env || exit 1
 cat > /etc/sysctl.d/20-solana-node.conf <<EOF
 
 # Solana networking requirements
-net.core.rmem_default=1610612736
-net.core.rmem_max=1610612736
-net.core.wmem_default=1610612736
-net.core.wmem_max=1610612736
+net.core.rmem_default=134217728
+net.core.rmem_max=134217728
+net.core.wmem_default=134217728
+net.core.wmem_max=134217728
 
 # Solana earlyoom setup
 kernel.sysrq=$(( $(cat /proc/sys/kernel/sysrq) | 64 ))
