391f0173be0d117e18ca2559ccdf2098cf32f595
Michael Vines <mvines@gmail.com>
2019-10-17 14:25:44 -0700

    Remove unused set_thread_count()

--- rayon-threadlimit/src/lib.rs
+++ rayon-threadlimit/src/lib.rs
@@ -1,21 +1,14 @@
 #[macro_use]
 extern crate lazy_static;
 
-use std::sync::RwLock;
-
 //TODO remove this hack when rayon fixes itself
 
 lazy_static! {
-// reduce the number of threads each pool is allowed to half the cpu core count, to avoid rayon
-// hogging cpu
-    static ref MAX_RAYON_THREADS: RwLock<usize> =
-        RwLock::new(sys_info::cpu_num().unwrap() as usize / 2);
+    // reduce the number of threads each pool is allowed to half the cpu core count, to avoid rayon
+    // hogging cpu
+    static ref MAX_RAYON_THREADS: usize = sys_info::cpu_num().unwrap() as usize / 2;
 }
 
 pub fn get_thread_count() -> usize {
-    *MAX_RAYON_THREADS.read().unwrap()
-}
-
-pub fn set_thread_count(count: usize) {
-    *MAX_RAYON_THREADS.write().unwrap() = count;
+    *MAX_RAYON_THREADS
 }
