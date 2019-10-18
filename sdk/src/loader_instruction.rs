3cffede84a2ae035224f2c7d2e463cf3759bd0ad
Jack May <jack@solana.com>
2019-10-18 15:43:14 -0700

    apply to LoaderInstruction::Write

--- sdk/src/loader_instruction.rs
+++ sdk/src/loader_instruction.rs
@@ -9,7 +9,11 @@ pub enum LoaderInstruction {
     /// * key[0] - the account to write into.
     ///
     /// The transaction must be signed by key[0]
-    Write { offset: u32, bytes: Vec<u8> },
+    Write {
+        offset: u32,
+        #[serde(with = "serde_bytes")]
+        bytes: Vec<u8>,
+    },
 
     /// Finalize an account loaded with program data for execution.
     /// The exact preparation steps is loader specific but on success the loader must set the executable
