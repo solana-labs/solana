afaadf2713e45bfb1bde0e2ba7a926d231d05461
Dan Albert <dan@solana.com>
2019-10-21 17:29:17 -0600

    Rebase and fix typo

--- system-test/testnet-performance/testnet-automation-json-parser.py
+++ system-test/testnet-performance/testnet-automation-json-parser.py
@@ -5,6 +5,6 @@ data=json.load(sys.stdin)
 
 if 'results' in data:
    for result in data['results']:
-      print result['series'][0]['columns'][1].encode() + ': ' + result['series'][0]['values'][0][1]
+      print result['series'][0]['columns'][1].encode() + ': ' + str(result['series'][0]['values'][0][1])
 else:
    print "No results returned from CURL request"
