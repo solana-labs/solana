0ab8af29a4fb680de96541babc661f3c819ce859
Stephen Akridge <sakridge@gmail.com>
2019-10-21 13:38:14 -0700

    Exclude net/log in rsync script

--- net/net.sh
+++ net/net.sh
@@ -389,6 +389,7 @@ startCommon() {
   fi
   [[ -z "$externalNodeSshKey" ]] || ssh-copy-id -f -i "$externalNodeSshKey" "${sshOptions[@]}" "solana@$ipAddress"
   rsync -vPrc -e "ssh ${sshOptions[*]}" \
+    --exclude 'net/log' \
     "$SOLANA_ROOT"/{fetch-perf-libs.sh,scripts,net,multinode-demo} \
     "$ipAddress":~/solana/
 }
