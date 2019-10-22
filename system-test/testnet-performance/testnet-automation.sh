fecb70d7fe91647aa1f0467c981c6975058fed00
Dan Albert <dan@solana.com>
2019-10-21 21:15:53 -0600

    shellcheck

--- system-test/testnet-performance/testnet-automation.sh
+++ system-test/testnet-performance/testnet-automation.sh
@@ -91,6 +91,7 @@ launchTestnet() {
         -p "$TESTNET_TAG" ${TESTNET_CLOUD_ZONES[@]/#/"-z "} ${ADDITIONAL_FLAGS[@]/#/" "}
       ;;
     colo)
+    # shellcheck disable=SC2068
       net/colo.sh create \
         -n "$NUMBER_OF_VALIDATOR_NODES" -c "$NUMBER_OF_CLIENT_NODES" -g \
         -p "$TESTNET_TAG" ${ADDITIONAL_FLAGS[@]/#/" "}
