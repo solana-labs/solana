01fe7c90a554333e99d834ef1fc5f2c3832dacd5
Dan Albert <dan@solana.com>
2019-10-23 11:04:15 -0600

    Do not break build on missing curl results (#6516)

--- system-test/testnet-performance/testnet-automation-json-parser.py
+++ system-test/testnet-performance/testnet-automation-json-parser.py
@@ -5,6 +5,7 @@ data=json.load(sys.stdin)
 
 if 'results' in data:
    for result in data['results']:
-      print result['series'][0]['columns'][1].encode() + ': ' + str(result['series'][0]['values'][0][1])
+      if 'series' in result:
+         print result['series'][0]['columns'][1].encode() + ': ' + str(result['series'][0]['values'][0][1])
 else:
    print "No results returned from CURL request"
