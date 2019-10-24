6787edd5a8fd30e63fe25269c11a078918ee4faf
Dan Albert <dan@solana.com>
2019-10-23 20:33:06 -0600

    Add 5 node GCE test cases

--- system-test/testnet-performance/testnet-automation-json-parser.py
+++ system-test/testnet-performance/testnet-automation-json-parser.py
@@ -7,5 +7,7 @@ if 'results' in data:
    for result in data['results']:
       if 'series' in result:
          print result['series'][0]['columns'][1].encode() + ': ' + str(result['series'][0]['values'][0][1])
+      else:
+         print "An expected result from CURL request is missing"
 else:
    print "No results returned from CURL request"
