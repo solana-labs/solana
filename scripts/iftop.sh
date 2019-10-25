37b436f9929eae3323d04ed6ff713200a634bf56
Pankaj Garg <pankaj@solana.com>
2019-10-25 21:18:22 +0000

    more shellchecks

--- scripts/iftop.sh
+++ scripts/iftop.sh
@@ -8,7 +8,7 @@ set -e
 
 cd "$(dirname "$0")"
 
-sudo iftop -i $(ifconfig | grep mtu | grep -iv loopback | grep -i running | awk 'BEGIN { FS = ":" } ; {print $1}') -nNbBP  -t \
+sudo iftop -i "$(ifconfig | grep mtu | grep -iv loopback | grep -i running | awk 'BEGIN { FS = ":" } ; {print $1}')" -nNbBP  -t \
 	| awk '{ if ($3 ~ "=>") { print $2, $7 } else if ($2 ~ "<=") { print $1, $6 }} ' \
 	| awk 'NR%2{printf "%s ",$0;next;}1' \
 	| awk '{ print $1, "<=>", $3, ":",  $2, $4 }'
