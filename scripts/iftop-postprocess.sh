8214d9c4b532f65fa85611daa92859003142f000
Pankaj Garg <pankaj@solana.com>
2019-10-31 20:32:55 -0700

    Compare iftop logs using log-analyzer

--- scripts/iftop-postprocess.sh
+++ scripts/iftop-postprocess.sh
@@ -24,9 +24,9 @@ awk '{ if ($3 ~ "=>") { print $2, $7 } else if ($2 ~ "<=") { print $1, $6 }} ' <
   | awk '{ print "{ \"a\": \""$1"\", " "\"b\": \""$3"\", \"a_to_b\": \""$2"\", \"b_to_a\": \""$4"\"}," }' > "$2"
 
 if [ "$#" -ne 3 ]; then
-  solana-log-analyzer -i "$2"
+  solana-log-analyzer iftop -f "$2"
 else
-  solana-log-analyzer -i "$2" map-IP --priv "$(hostname -i)" --pub "$3"
+  solana-log-analyzer iftop -f "$2" map-IP --priv "$(hostname -i)" --pub "$3"
 fi
 
 exit 1
