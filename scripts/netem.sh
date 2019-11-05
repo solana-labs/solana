5ddcbe50d99de5cca7d6c10e818470ffc5cf0204
Pankaj Garg <pankaj@solana.com>
2019-11-05 13:44:26 -0800

    review comments

--- scripts/netem.sh
+++ scripts/netem.sh
@@ -13,6 +13,8 @@ if sudo true; then
   sudo="sudo -n"
 fi
 
+set -x
+
 iface="$(ifconfig | grep mtu | grep -iv loopback | grep -i running | awk 'BEGIN { FS = ":" } ; {print $1}')"
 # shellcheck disable=SC2086 # Do not want to quote $2. It has space separated arguments for netem
 $sudo tc qdisc "$1" dev "$iface" root netem $2
