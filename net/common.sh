aec37f2bc8b83889239ad794ec090f9cfc1880c4
Justin Starry <justin@solana.com>
2019-11-07 14:12:24 -0500

    Clean up net/log symlinks

--- net/common.sh
+++ net/common.sh
@@ -19,7 +19,7 @@ if [[ -d $netLogDir && ! -L $netLogDir ]]; then
   mv "$netLogDir" "$netDir"/log.old
 fi
 mkdir -p "$netConfigDir" "$netLogDateDir"
-ln -sf "$netLogDateDir" "$netLogDir"
+ln -sfh "$netLogDateDir" "$netLogDir"
 
 SOLANA_ROOT="$netDir"/..
 # shellcheck source=scripts/configure-metrics.sh
@@ -131,4 +131,3 @@ _setup_secondary_mount() {
     fi
   )
 }
-
