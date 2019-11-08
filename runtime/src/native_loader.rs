83d8532d0359818d9d12f54481dc75b219f12e60
Jack May <jack@solana.com>
2019-11-08 11:43:22 -0800

    TODO already covered by issue

--- runtime/src/native_loader.rs
+++ runtime/src/native_loader.rs
@@ -60,7 +60,7 @@ fn library_open(path: &PathBuf) -> std::io::Result<Library> {
 
 #[cfg(not(windows))]
 fn library_open(path: &PathBuf) -> std::io::Result<Library> {
-    // TODO linux tls bug can cause crash on dlclose(), workaround by never unloading
+    // Linux tls bug can cause crash on dlclose(), workaround by never unloading
     Library::open(Some(path), libc::RTLD_NODELETE | libc::RTLD_NOW)
 }
 
