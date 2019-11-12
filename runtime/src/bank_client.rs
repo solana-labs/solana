7e673686c5a5a03923ee444e2542982e16b78a9e
Greg Fitzgerald <greg@solana.com>
2019-11-12 11:16:45 -0700

    Revive the parallel bank client from v0.16

--- runtime/src/bank_client.rs
+++ runtime/src/bank_client.rs
@@ -239,7 +239,11 @@ impl SyncClient for BankClient {
 impl BankClient {
     fn run(bank: &Bank, transaction_receiver: Receiver<Transaction>) {
         while let Ok(tx) = transaction_receiver.recv() {
-            let _ = bank.process_transaction(&tx);
+            let mut transactions = vec![tx];
+            while let Ok(tx) = transaction_receiver.try_recv() {
+                transactions.push(tx);
+            }
+            let _ = bank.process_transactions(&transactions);
         }
     }
 
