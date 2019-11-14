77c7cade45a2c8a488044e3b543191c7775d3275
Dan Albert <dan@solana.com>
2019-11-14 10:12:32 -0500

    Consolidate error messaging into result detail

--- system-test/testnet-performance/upload_results_to_slack.sh
+++ system-test/testnet-performance/upload_results_to_slack.sh
@@ -83,16 +83,6 @@ upload_results_to_slack() {
 			"type": "section",
 			"text": {
 				"type": "mrkdwn",
-				"text": "Test Status: \n\`\`\`$STEP\`\`\`"
-			}
-		},
-		{
-			"type": "divider"
-		},
- 		{
-			"type": "section",
-			"text": {
-				"type": "mrkdwn",
 				"text": "Result Details: \n\`\`\`$RESULT_DETAILS\`\`\`"
 			}
 		}
