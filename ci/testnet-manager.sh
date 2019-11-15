cac5d9005cc24262f79288b6323c9824433c73c8
Michael Vines <mvines@gmail.com>
2019-11-15 15:51:30 -0700

    Pull TdS transaction fees to 0

--- ci/testnet-manager.sh
+++ ci/testnet-manager.sh
@@ -546,7 +546,7 @@ deploy() {
         ${maybeInternalNodesLamports} \
         ${maybeExternalAccountsFile} \
         ${maybeLamports} \
-        --target-lamports-per-signature 1 \
+        --target-lamports-per-signature 0 \
         --slots-per-epoch 4096 \
         ${maybeAdditionalDisk}
     )
