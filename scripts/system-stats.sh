a8b85d72ba639e38a3c81a27f9b4f785efb63fac
Sagar Dhawan <sagar@solana.com>
2019-11-14 19:25:48 -0800

    Address review comments

--- scripts/system-stats.sh
+++ scripts/system-stats.sh
@@ -9,25 +9,16 @@ set -e
 # need to cd like this to avoid #SC1091
 cd "$(dirname "$0")/.."
 source scripts/configure-metrics.sh
-cd scripts
-
-cpu_usage=0
-ram_usage=0
-
-update_cpustat() {
-# collect the total cpu usage by subtracting idle usage from 100%
-cpu_usage=$(top -bn1 | grep '%Cpu(s):' | sed "s/.*, *\([0-9.]*\)%* id.*/\1/" | awk '{print 100 - $1}')
-# collect the total ram usage by dividing used memory / total memory
-ram_total_and_usage=$(top -bn1 | grep 'MiB Mem'| sed "s/.*: *\([0-9.]*\)%* total.*, *\([0-9.]*\)%* used.*/\1 \2/")
-read -r total used <<< "$ram_total_and_usage"
-ram_usage=$(awk "BEGIN {print $used / $total * 100}")
-}
 
 while true; do
-  update_cpustat
-  report="cpu_usage=$cpu_usage,ram_usage=$ram_usage"
+  # collect the total cpu usage by subtracting idle usage from 100%
+  cpu_usage=$(top -bn1 | grep '%Cpu(s):' | sed "s/.*, *\([0-9.]*\)%* id.*/\1/" | awk '{print 100 - $1}')
+  # collect the total ram usage by dividing used memory / total memory
+  ram_total_and_usage=$(top -bn1 | grep 'MiB Mem'| sed "s/.*: *\([0-9.]*\)%* total.*, *\([0-9.]*\)%* used.*/\1 \2/")
+  read -r total used <<< "$ram_total_and_usage"
+  ram_usage=$(awk "BEGIN {print $used / $total * 100}")
 
-  echo "$report"
-  ./metrics-write-datapoint.sh "system-stats,hostname=$HOSTNAME $report"
+  report="cpu_usage=$cpu_usage,ram_usage=$ram_usage"
+  ./scripts/metrics-write-datapoint.sh "system-stats,hostname=$HOSTNAME $report"
   sleep 1
 done
