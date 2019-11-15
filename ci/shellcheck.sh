a695b56baaeac6c91a52315c31e88f8013b60e1c
Dan Albert <dan@solana.com>
2019-11-15 15:48:43 +0000

    Pull a fixed and working version of shellcheck docker imaage

--- ci/shellcheck.sh
+++ ci/shellcheck.sh
@@ -7,6 +7,6 @@ cd "$(dirname "$0")/.."
 (
   set -x
   git ls-files -- '*.sh' ':(exclude)ci/semver_bash' \
-    | xargs ci/docker-run.sh koalaman/shellcheck --color=always --external-sources --shell=bash
+    | xargs ci/docker-run.sh koalaman/shellcheck@sha256:fe24ab9a9b6b62d3adb162f4a80e006b6a63cae8c6ffafbae45772bab85e7294 --color=always --external-sources --shell=bash
 )
 echo --- ok
