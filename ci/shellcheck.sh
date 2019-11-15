22c457fb8d7f728c5f4a5a5d17c9ae5e6c0900a4
Dan Albert <dan@solana.com>
2019-11-15 14:42:41 -0500

    fix shellcheck docker

--- ci/shellcheck.sh
+++ ci/shellcheck.sh
@@ -13,6 +13,6 @@ cd "$(dirname "$0")/.."
       -not -regex ".*/target/.*" \
       -print0 \
     | xargs -0 \
-        ci/docker-run.sh koalaman/shellcheck --color=always --external-sources --shell=bash
+        ci/docker-run.sh koalaman/shellcheck@sha256:fe24ab9a9b6b62d3adb162f4a80e006b6a63cae8c6ffafbae45772bab85e7294 --color=always --external-sources --shell=bash
 )
 echo --- ok
