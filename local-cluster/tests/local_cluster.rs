c4859cec3584248cffd566b00e16d21cd148d021
Anatoly Yakovenko <anatoly@solana.com>
2019-11-14 21:01:32 -0800

    try in parallel

--- local-cluster/tests/local_cluster.rs
+++ local-cluster/tests/local_cluster.rs
@@ -256,19 +256,16 @@ fn run_network_partition(partitions: &[usize]) {
 }
 
 #[test]
-#[serial]
 fn test_network_partition_1_2() {
     run_network_partition(&[1, 2])
 }
 
 #[test]
-#[serial]
 fn test_network_partition_1_1() {
     run_network_partition(&[1, 1])
 }
 
 #[test]
-#[serial]
 fn test_network_partition_1_1_1() {
     run_network_partition(&[1, 1, 1])
 }
