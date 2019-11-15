892ca196f11040d816af347aa774e67785d32bb8
Michael Vines <mvines@gmail.com>
2019-11-15 10:39:05 -0700

    Improve error message when unable to read a file (#6978)

--- genesis/src/main.rs
+++ genesis/src/main.rs
@@ -33,6 +33,7 @@ pub enum AccountFileFormat {
 fn pubkey_from_file(key_file: &str) -> Result<Pubkey, Box<dyn error::Error>> {
     read_pubkey_file(key_file)
         .or_else(|_| read_keypair_file(key_file).map(|keypair| keypair.pubkey()))
+        .map_err(|err| format!("Failed to read {}: {}", key_file, err).into())
 }
 
 fn pubkey_from_str(key_str: &str) -> Result<Pubkey, Box<dyn error::Error>> {
