2d352d7f84506ccc99ec7944dac750050d5cd839
Tyera Eulberg <tyera@solana.com>
2019-11-19 08:18:13 -0700

    Remove duplicated args

--- multinode-demo/validator.sh
+++ multinode-demo/validator.sh
@@ -95,12 +95,6 @@ while [[ -n $1 ]]; do
       gossip_entrypoint=$2
       args+=("$1" "$2")
       shift 2
-    elif [[ $1 = --limit-ledger-size ]]; then
-      args+=("$1")
-      shift
-    elif [[ $1 = --skip-poh-verify ]]; then
-      args+=("$1")
-      shift
     elif [[ $1 = --no-snapshot-fetch ]]; then
       args+=("$1")
       shift
