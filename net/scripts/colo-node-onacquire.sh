fbf702251d279353127dc1b64506bbfc00eda35b
Trent Nelson <trent@solana.com>
2019-11-22 13:52:10 -0700

    simplify logic

--- net/scripts/colo-node-onacquire.sh
+++ net/scripts/colo-node-onacquire.sh
@@ -12,11 +12,12 @@ CREATION_INFO="${CREATION_INFO:-"Creation info unavailable"}"
 if [[ ! -f "${SOLANA_LOCK_FILE}" ]]; then
   exec 9>>"${SOLANA_LOCK_FILE}"
   flock -x -n 9 || ( echo "Failed to acquire lock!" 1>&2 && exit 1 )
-  ( [[ -n "${SOLANA_USER}" ]] && {
+  SOLANA_USER="${SOLANA_USER:?"SOLANA_USER undefined"}"
+  {
     echo "export SOLANA_LOCK_USER=${SOLANA_USER}"
     echo "export SOLANA_LOCK_INSTANCENAME=${INSTANCE_NAME}"
     echo "[[ -v SSH_TTY -a -f \"${HOME}/.solana-motd\" ]] && cat \"${HOME}/.solana-motd\" 1>&2"
-  } >&9 ) || ( rm "${SOLANA_LOCK_FILE}" && echo "SOLANA_USER undefined" 1>&2 && false )
+  } >&9
   exec 9>&-
   cat > /solana-scratch/id_ecdsa <<EOF
 ${SSH_PRIVATE_KEY_TEXT}
