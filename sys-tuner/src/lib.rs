63b763be042560ca9513858cc80bf759547d8911
Michael Vines <mvines@gmail.com>
2019-12-02 18:57:45 -0700

    Fix windows build

--- sys-tuner/src/lib.rs
+++ sys-tuner/src/lib.rs
@@ -1,10 +1,15 @@
 use log::*;
-use unix_socket::UnixStream;
 
 pub const SOLANA_SYS_TUNER_PATH: &str = "/tmp/solana-sys-tuner";
 
+#[cfg(unix)]
 pub fn request_realtime_poh() {
     info!("Sending tuning request");
-    let status = UnixStream::connect(SOLANA_SYS_TUNER_PATH);
+    let status = unix_socket::UnixStream::connect(SOLANA_SYS_TUNER_PATH);
     info!("Tuning request status {:?}", status);
+}
+
+#[cfg(not(unix))]
+pub fn request_realtime_poh() {
+    info!("Tuning request ignored on this platform");
 }
