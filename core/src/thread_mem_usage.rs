03e8032a6ea7b938c1759a93de3ae1d6cb299128
Andrei Ivasko <cyberbono3@gmail.com>
2019-12-06 11:46:34 -0800

    guidance needed

--- core/src/thread_mem_usage.rs
+++ core/src/thread_mem_usage.rs
@@ -1,10 +1,15 @@
+use serde_derive::{Deserialize, Serialize};
+use std::collections::HashMap;
+use std::fs::File;
+use std::io::{Read, Write};
+use std::path::Path;
 #[cfg(unix)]
-use jemalloc_ctl::thread;
+use std::thread;
 
 pub fn datapoint(_name: &'static str) {
     #[cfg(unix)]
     {
-        let allocated = thread::allocatedp::mib().unwrap();
+        let allocated = jemalloc_ctl::thread::allocatedp::mib().unwrap();
         let allocated = allocated.read().unwrap();
         let mem = allocated.get();
         solana_metrics::datapoint_debug!("thread-memory", (_name, mem as i64, i64));
@@ -13,14 +18,14 @@ pub fn datapoint(_name: &'static str) {
 
 pub struct Allocatedp {
     #[cfg(unix)]
-    allocated: thread::ThreadLocal<u64>,
+    allocated: jemalloc_ctl::thread::ThreadLocal<u64>,
 }
 
 impl Allocatedp {
     pub fn default() -> Self {
         #[cfg(unix)]
         {
-            let allocated = thread::allocatedp::mib().unwrap();
+            let allocated = jemalloc_ctl::thread::allocatedp::mib().unwrap();
             let allocated = allocated.read().unwrap();
             Self { allocated }
         }
@@ -35,5 +40,51 @@ impl Allocatedp {
         }
         #[cfg(not(unix))]
         0
+    }
+}
+
+#[derive(PartialEq, Debug, Serialize, Deserialize)]
+struct MapFile(HashMap<String, u64>);
+
+impl MapFile {
+    pub fn write_to_file(&self, path: &Path) {
+        let mut file = File::create(path).expect("Failed to create / write a file.");
+        let str = serde_json::to_string_pretty(self).expect("Error serializing the file.");
+        if let Err(err) = file.write_all(str.as_bytes()) {
+            panic!("Failed to write a file {}", err);
+        }
+    }
+
+    pub fn from_file(path: &Path) -> Self {
+        let mut file = File::open(path).expect("Could not open a file.");
+        let mut content = String::new();
+        file.read_to_string(&mut content)
+            .expect("Could not read from key file.");
+        serde_json::from_str(&content).expect("Failed to deserialize KeyFile")
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_thread_memory_map() {
+        let adp = Allocatedp::default();
+        let mem = adp.get();
+
+        let current_thread = thread::current();
+        let name = current_thread.name().unwrap();
+
+        let mut map: HashMap<String, u64> = HashMap::new();
+        map.insert(name.to_string(), mem);
+
+        let file = MapFile(map);
+        let path = Path::new("file.txt");
+        file.write_to_file(&path);
+
+        let new_file = MapFile::from_file(&path);
+
+        assert_eq!(file, new_file);
     }
 }
