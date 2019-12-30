c0ae5f7b6e8403d9c1964dc4c404b5e023b9fb1e
Tyera Eulberg <tyera@solana.com>
2019-12-30 11:29:55 -0700

    Make sol-to-lamport const name more clear

--- bench-tps/src/cli.rs
+++ bench-tps/src/cli.rs
@@ -4,7 +4,7 @@ use solana_sdk::fee_calculator::FeeCalculator;
 use solana_sdk::signature::{read_keypair_file, Keypair, KeypairUtil};
 use std::{net::SocketAddr, process::exit, time::Duration};
 
-const NUM_LAMPORTS_PER_ACCOUNT_DEFAULT: u64 = solana_sdk::native_token::SOL_LAMPORTS;
+const NUM_LAMPORTS_PER_ACCOUNT_DEFAULT: u64 = solana_sdk::native_token::LAMPORTS_PER_SOL;
 
 /// Holds the configuration for a single run of the benchmark
 pub struct Config {
