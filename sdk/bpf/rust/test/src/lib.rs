6c544708e1f976e3bc167cdec2b0ba4785d8da8f
Jack May <jack@solana.com>
2020-01-03 09:14:28 -0800

    Add safety docs (#7665)

--- sdk/bpf/rust/test/src/lib.rs
+++ sdk/bpf/rust/test/src/lib.rs
@@ -1,6 +1,7 @@
 //! @brief Solana Rust-based BPF program utility functions and types
 
 #[no_mangle]
+/// # Safety
 pub unsafe fn sol_log_(message: *const u8, length: u64) {
     let slice = std::slice::from_raw_parts(message, length as usize);
     let string = std::str::from_utf8(&slice).unwrap();
