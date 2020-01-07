517979a86778b87798696d9c4f75702187eb91cb
Carl <carl@solana.com>
2020-01-06 23:26:11 -0500

    Add test

--- ledger/src/rooted_slot_iterator.rs
+++ ledger/src/rooted_slot_iterator.rs
@@ -43,6 +43,7 @@ impl<'a> Iterator for RootedSlotIterator<'a> {
                         self.prev_root,
                     )
                     .expect("Database failure, couldn't fetch rooted slots iterator");
+                iter.next();
                 (iter.next(), true)
             });
 
@@ -86,7 +87,7 @@ mod tests {
 
                  slot 0
                    |
-                 slot 1  <-- set_root(true)
+                 slot 1  <-- set_root
                  /   \
             slot 2   |
                /     |
@@ -146,6 +147,84 @@ mod tests {
             .map(|(slot, _)| slot)
             .collect();
         let expected = vec![0, 1, 2, 3];
+        assert_eq!(result, expected);
+
+        drop(blocktree);
+        Blocktree::destroy(&blocktree_path).expect("Expected successful database destruction");
+    }
+
+    #[test]
+    fn test_skipping_rooted_slot_iterator() {
+        let blocktree_path = get_tmp_ledger_path!();
+        let blocktree = Blocktree::open(&blocktree_path).unwrap();
+        let ticks_per_slot = 5;
+        /*
+            Build a blocktree in the ledger with the following fork structure:
+                 slot 0
+                   |
+                 slot 1
+                   |
+                 slot 2
+                   |
+                 slot 3 <-- set_root
+                   |
+                 SKIP (caused by a snapshot)
+                   |
+                 slot 10 <-- set_root
+                   |
+                 slot 11 <-- set_root
+        */
+
+        // Create pre-skip slots
+        for slot in 0..=3 {
+            let parent = {
+                if slot == 0 {
+                    0
+                } else {
+                    slot - 1
+                }
+            };
+            fill_blocktree_slot_with_ticks(
+                &blocktree,
+                ticks_per_slot,
+                slot,
+                parent,
+                Hash::default(),
+            );
+        }
+
+        // Set roots
+        blocktree.set_roots(&[0, 1, 2, 3]).unwrap();
+
+        // Create one post-skip slot at 10, simulating starting from a snapshot
+        // at 10
+        blocktree.set_roots(&[10]).unwrap();
+        // Try to get an iterator from before the skip. The post-skip slot
+        // should not return a SlotMeta
+        let result: Vec<_> = RootedSlotIterator::new(3, &blocktree)
+            .unwrap()
+            .into_iter()
+            .map(|(slot, meta)| (slot, meta.is_some()))
+            .collect();
+        let expected = vec![(3, true), (10, false)];
+        assert_eq!(result, expected);
+
+        // Create one more post-skip slot at 11 with parent equal to 10
+        fill_blocktree_slot_with_ticks(&blocktree, ticks_per_slot, 11, 10, Hash::default());
+
+        let result: Vec<_> = RootedSlotIterator::new(0, &blocktree)
+            .unwrap()
+            .into_iter()
+            .map(|(slot, meta)| (slot, meta.is_some()))
+            .collect();
+        let expected = vec![
+            (0, true),
+            (1, true),
+            (2, true),
+            (3, true),
+            (10, false),
+            (11, true),
+        ];
         assert_eq!(result, expected);
 
         drop(blocktree);
