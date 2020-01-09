ea5e727b5fbea4fb69ff644f1c2b87bd99c3803d
Justin Starry <justin@solana.com>
2020-01-10 03:43:02 +0800

    Update http crate to fix security vulnerability (#7725)
    * Update http to fix security vulnerability
    
    * Ignore RUSTSEC because they incorrectly says http 0.1.21 is vulnerable
    
    (cherry picked from commit 719785a8d307b4269497aa82b148109e491019d5)
    
    # Conflicts:
    #	Cargo.lock
    #	ci/test-checks.sh

--- ci/test-checks.sh
+++ ci/test-checks.sh
@@ -19,7 +19,11 @@ _ cargo +"$rust_stable" clippy --all --exclude solana-sdk-c -- --deny=warnings
 _ cargo +"$rust_stable" clippy --manifest-path sdk-c/Cargo.toml -- --deny=warnings
 
 _ cargo +"$rust_stable" audit --version
+<<<<<<< HEAD
 _ cargo +"$rust_stable" audit --ignore RUSTSEC-2019-0013 --ignore RUSTSEC-2018-0015
+=======
+_ cargo +"$rust_stable" audit --ignore RUSTSEC-2019-0013 --ignore RUSTSEC-2018-0015 --ignore RUSTSEC-2019-0031 --ignore RUSTSEC-2019-0033 --ignore RUSTSEC-2019-0034
+>>>>>>> 719785a8d... Update http crate to fix security vulnerability (#7725)
 _ ci/nits.sh
 _ ci/order-crates-for-publishing.py
 _ book/build.sh
@@ -28,7 +32,11 @@ _ ci/check-ssh-keys.sh
 
 {
   cd programs/bpf
+<<<<<<< HEAD
   _ cargo +"$rust_stable" audit
+=======
+  _ cargo +"$rust_stable" audit --ignore RUSTSEC-2019-0031 --ignore RUSTSEC-2019-0033 --ignore RUSTSEC-2019-0034
+>>>>>>> 719785a8d... Update http crate to fix security vulnerability (#7725)
   for project in rust/*/ ; do
     echo "+++ do_bpf_checks $project"
     (
