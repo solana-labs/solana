ad7d9c9b5b55d5bf776cce2e49c4d3c57db08293
Rob Walker <rob@solana.com>
2020-01-13 14:50:59 -0800

    Remove create account bandaid now that  requires signature

--- sdk/src/system_instruction.rs
+++ sdk/src/system_instruction.rs
@@ -147,7 +147,7 @@ pub fn create_account(
     program_id: &Pubkey,
 ) -> Instruction {
     let account_metas = vec![
-        AccountMeta::new(*from_pubkey, true),
+        AccountMeta::new(*from_pubkey, lamports != 0), // no signature required if no transfer
         AccountMeta::new(*to_pubkey, true),
     ];
     Instruction::new(
@@ -173,7 +173,7 @@ pub fn create_account_with_seed(
     program_id: &Pubkey,
 ) -> Instruction {
     let account_metas = vec![
-        AccountMeta::new(*from_pubkey, true),
+        AccountMeta::new(*from_pubkey, lamports != 0),
         AccountMeta::new(*to_pubkey, false),
     ]
     .with_signer(base);
