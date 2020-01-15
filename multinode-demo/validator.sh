36631b5ffcb359bb0c8b7e83b6471c831d3a7f39
Michael Vines <mvines@gmail.com>
2020-01-14 23:29:41 -0700

    Set bootstrap leader and net/ validator vote account commission to 100%
    (cherry picked from commit 50c1c082352d094c98a2b3e8fa91583af47c24a0)

--- multinode-demo/validator.sh
+++ multinode-demo/validator.sh
@@ -257,7 +257,7 @@ setup_validator_accounts() {
     fi
 
     echo "Creating validator vote account"
-    wallet create-vote-account "$voting_keypair_path" "$identity_keypair_path" --commission 50 || return $?
+    wallet create-vote-account "$voting_keypair_path" "$identity_keypair_path" || return $?
   fi
   echo "Validator vote account configured"
 
