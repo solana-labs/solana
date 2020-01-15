5947ef77064aea9ee343a8dcebea106856510862
Dan Albert <dan@solana.com>
2020-01-15 13:50:37 -0500

    Remove word pair from address generator seed string (#7802)
    * Remove word pair from address generator seed string

--- genesis/src/address_generator.rs
+++ genesis/src/address_generator.rs
@@ -3,28 +3,21 @@ use solana_sdk::{pubkey::Pubkey, system_instruction::create_address_with_seed};
 #[derive(Default)]
 pub struct AddressGenerator {
     base_pubkey: Pubkey,
-    base_seed: String,
     program_id: Pubkey,
     nth: usize,
 }
 
 impl AddressGenerator {
-    pub fn new(base_pubkey: &Pubkey, base_seed: &str, program_id: &Pubkey) -> Self {
+    pub fn new(base_pubkey: &Pubkey, program_id: &Pubkey) -> Self {
         Self {
             base_pubkey: *base_pubkey,
-            base_seed: base_seed.to_string(),
             program_id: *program_id,
             nth: 0,
         }
     }
 
     pub fn nth(&self, nth: usize) -> Pubkey {
-        create_address_with_seed(
-            &self.base_pubkey,
-            &format!("{}:{}", self.base_seed, nth),
-            &self.program_id,
-        )
-        .unwrap()
+        create_address_with_seed(&self.base_pubkey, &format!("{}", nth), &self.program_id).unwrap()
     }
 
     #[allow(clippy::should_implement_trait)]
