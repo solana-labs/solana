a6f30e89c3f9c5a22efd761839f1dea3bb111dce
Sagar Dhawan <sagar@solana.com>
2020-01-21 10:41:02 -0800

    Revert "Generate MAX_DATA_SHREDS_PER_FEC_BLOCK coding shreds for each FEC block (#7474)"
    This reverts commit 7adc721d96b68b39252e8b3cc8a40910274c827b.

--- ledger/src/blockstore.rs
+++ ledger/src/blockstore.rs
@@ -5297,7 +5297,7 @@ pub mod tests {
                 .iter()
                 .cloned()
                 .chain(
-                    coding_shreds[coding_shreds.len() / 2 - 1..coding_shreds.len() / 2]
+                    coding_shreds[coding_shreds.len() / 2 - 1..data_shreds.len() / 2]
                         .iter()
                         .cloned(),
                 )
