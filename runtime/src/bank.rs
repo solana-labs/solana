b53336100ee162687b14f632b45f26534166ba1f
Rob Walker <rob@solana.com>
2020-01-21 16:19:50 -0800

    Move redeem_vote_credits into runtime

--- runtime/src/bank.rs
+++ runtime/src/bank.rs
@@ -31,6 +31,7 @@ use solana_metrics::{
 };
 use solana_sdk::{
     account::Account,
+    account_utils::State,
     clock::{get_segment_from_slot, Epoch, Slot, UnixTimestamp, MAX_RECENT_BLOCKHASHES},
     epoch_schedule::EpochSchedule,
     fee_calculator::FeeCalculator,
