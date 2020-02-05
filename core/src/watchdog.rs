e0f8d190cc85d5b475cdf64ebd22da6bb6492891
Anatoly Yakovenko <anatoly@solana.com>
2020-02-05 14:37:57 -0800

    progress

--- core/src/watchdog.rs
+++ core/src/watchdog.rs
@@ -10,6 +10,8 @@ use std::sync::{
     Arc, Mutex, RwLock,
 };
 
+pub const MAX_ROOT_DELTA: usize = VOTE_THRESHOLD_DEPTH;
+
 struct WatchdogService {
     t_dog: JoinHandle<Result<()>>,
 }
@@ -17,7 +19,8 @@ struct WatchdogService {
 struct Watchdog {
     cluster_info: Arc<RwLock<ClusterInfo>>,
     bank_forks: Arc<RwLock<BankForks>>,
-    since: u64,
+    since_votes: u64,
+    since_epochs: u64,
 }
 
 const MIN_CLUSTER_AGREEMENT: f64 = 0.75;
@@ -29,15 +32,6 @@ impl Watchdog {
         let anscestors: Vec<Slot> = self.bank_forks.read().unwrap().working_bank().ancestors();
 
         let acceptable_roots = self.acceptable_roots(root, ancestors);
-
-        let slot_history = self
-            .bank_forks
-            .read()
-            .unwrap()
-            .working_bank()
-            .get_sysvar_account(&sysvar::slot_history::id())
-            .map(|account| SlotHistory::from_account(&account).unwrap())
-            .unwrap_or_default();
 
         let slot_hashes = self
             .bank_forks
@@ -55,26 +49,52 @@ impl Watchdog {
             .working_bank()
             .epoch_vote_accounts();
 
-        let (votes, ns1) = self.cluster_info.read().unwrap().get_votes(&self.since);
+        let (votes, ns1) = self.cluster_info.read().unwrap().get_votes(&self.since_votes);
+        self.since_votes = ns1;
         let (epoch_slots, ns2) = self
             .cluster_info
             .read()
             .unwrap()
-            .get_all_epoch_states(&self.since);
-        self.since = std::min(ns1, ns2);
+            .get_all_epoch_states(&self.since_epochs);
+        self.since_epochs = ns2;
     }
 
+    fn compute_epoch_slot_agreement(
+        vote_accounts: HashMap<Pubkey, (u64, Account)>,
+        epoch_slots: HashMap<Pubkey, EpochSlot>)
+    -> u64 {
+        root_votes.iter().flat_map(|k,_| 
+            vote_accounts.get(k).unwrap_or_default()
+        ).sum()
+    }
+
+    fn find_root_epoch_slots(acceptable_roots: HashSet<Slot>, epoch_slots: Vec<EpochSlot>) -> HashMap<Pubkey, EpochSlot> {
+        epoch_slots.into_iter().filter(|e| acceptable_roots.contains(e.slot)).map(|e| (e.from, e)).collect()
+    }
+
+    fn compute_root_vote_slot_agreement(
+        vote_accounts: HashMap<Pubkey, (u64, Account)>,
+        root_votes: HashMap<Pubkey, Vec<Vote>>)
+    -> u64 {
+        root_votes.iter().flat_map(|k,_| 
+            vote_accounts.get(k).unwrap_or_default()
+        ).sum()
+    }
+
+    //network should be within root +/- threshold blocks
     fn find_acceptable_roots(root: Slot, ancestors: Vec<Slot>) -> HashSet<Slot> {
-        ancestors
-            .into_iter()
-            .filter(|x| x <= VOTE_THRESHOLD_DEPTH + 2)
-            .collect()
+        assert!(ancestors.is_empty() || ancestors.first() >= ancestors.last());
+        let root_pos = ancestors
+            .iter()
+            .take_while(|x| x == root)
+            .count();
+        ancestors.into_iter().skip(std::MAX(0, root_pos - MAX_ROOT_DELTA)).take(2*MAX_ROOT_DELTA).collect()
     }
 
     fn find_root_votes(
         votes: Vec<Transaction>,
         acceptable_roots: HashSet<Slot>,
-    ) -> HashMap<(Pubkey, Vec<Vote>)> {
+    ) -> HashMap<Pubkey, Vec<Vote>> {
         let mut root_votes = HashMap::new();
         votes.into_iter().for_each(|tx| {
             let decoded = Self::decode_votes(tx);
