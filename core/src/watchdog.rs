daa87edc0e4e9a2228252131e7f70ce6bf2ef21c
Anatoly Yakovenko <anatoly@solana.com>
2020-02-06 15:31:37 -0800

    progress

--- core/src/watchdog.rs
+++ core/src/watchdog.rs
@@ -10,7 +10,8 @@ use std::sync::{
     Arc, Mutex, RwLock,
 };
 
-pub const MAX_ROOT_DELTA: usize = VOTE_THRESHOLD_DEPTH;
+//8 hours
+pub const TIMEOUT_MS: usize = 1_000*60*60*8;
 
 struct WatchdogService {
     t_dog: JoinHandle<Result<()>>,
@@ -19,47 +20,85 @@ struct WatchdogService {
 struct Watchdog {
     cluster_info: Arc<RwLock<ClusterInfo>>,
     bank_forks: Arc<RwLock<BankForks>>,
+    slot_heat_map: HashMap<Slot, (u64, HashSet<Pubkey>)>,
+    hash_heat_map: HashMap<Hash, (u64, HashSet<Pubkey>)>,
+    observed_hashes: HashMap<Hash, (u64, HashSet<Slot>)>,
+    observed_slots: HashMap<Slot, (u64, HashSet<Hash>)>,
     since_votes: u64,
-    since_epochs: u64,
 }
 
-const MIN_CLUSTER_AGREEMENT: f64 = 0.75;
+const MIN_CLUSTER_AGREEMENT: f64 = 2f64/3f64;
 
 impl Watchdog {
     fn verify(&mut self) -> bool {
-        let slot_hashes = self
-            .bank_forks
-            .read()
-            .unwrap()
-            .working_bank()
-            .get_sysvar_account(&sysvar::slot_hashes::id())
-            .map(|account| SlotHashes::from_account(&account).unwrap())
-            .unwrap_or_default();
-        let slots_hashes: HashMap<Slot,Hash> = slot_hashes.iter().collect();
-        let hashes_slots: HashMap<Hash,Slot> = slot_hashes.iter().map(|(s,h)| (h,s)).collect();
+        self.read_bank_forks();
+        self.read_cluster_votes();
 
-        let vote_accounts: HashMap<Pubkey, (u64, Account)> = self
+       let vote_accounts: HashMap<Pubkey, (u64, Account)> = self
             .bank_forks
             .read()
             .unwrap()
             .working_bank()
             .epoch_vote_accounts();
 
-        let (votes, ns1) = self
+    }
+
+    fn gc(&mut self) {
+        let now = timestamp();
+        self.slot_heat_map.retain(|v| v.0 > now - TIMEOUT_MS);
+        self.hash_heat_map.retain(|v| v.0 > now - TIMEOUT_MS);
+        self.observed_hashes.retain(|v| v.0 > now - TIMEOUT_MS);
+        self.observed_slots.retain(|v| v.0 > now - TIMEOUT_MS);
+    }
+
+    fn read_bank_forks(&mut self) {
+        let now = timestamp();
+        let frozen = self.bank_forks
+                .read()
+                .unwrap()
+                .frozen_banks();
+        for b in frozen.iter() {
+            if self.observed_hashes.contains(b.hash()) {
+                continue;
+            }
+            let s = b.slot();
+            let h = b.hash();
+            self.observed_slots.entry(s).or_default().insert(h); 
+            self.observed_hashes.entry(h).or_default().insert(s); 
+            self.observed_slots.entry(s).or_default().0 = now;
+            self.observed_hashes.entry(h).or_default().0 = now;
+            if self.observed_hashes.contains(b.parent().hash()) {
+                continue;
+            }
+            let slot_hashes = b
+                .get_sysvar_account(&sysvar::slot_hashes::id())
+                .map(|account| SlotHashes::from_account(&account).unwrap())
+                .unwrap_or_default();
+            for (s,h) in slot_hashes {
+                self.observed_slots.entry(s).or_default().1.insert(h); 
+                self.observed_hashes.entry(h).or_default().1.insert(s); 
+                self.observed_slots.entry(s).or_default().0 = now;
+                self.observed_hashes.entry(h).or_default().0 = now;
+            }
+        }
+    }
+
+    fn read_cluster_votes(&mut self) {
+        let (votes, ts) = self
             .cluster_info
             .read()
             .unwrap()
             .get_votes(&self.since_votes);
-        self.since_votes = ns1;
+        self.since_votes = ts;
+        let new_votes = Self::collect_votes(votes);
+        self.update_slot_heat_map(&new_votes);
+        self.update_hash_heat_map(&new_votes);
     }
-
-    fn slot_hash_heat_map(
-        vote_accounts: &HashMap<Pubkey, (u64, Account)>,
-        hash_slots: &HashMap<Hash, Slot>,
-        slot_hashes: &HashMap<Slot, Hash>,
+ 
+    fn update_hash_heat_map(&mut self,
         votes: &HashMap<Pubkey, Vec<Vote>>,
     ) -> HashMap<Hash, u64> {
-        let mut heat_map = HashMap::new();
+        let now = timestamp();
         for (key, val) in votes {
             let hashes: HashSet<Hash> = val
                 .votes
@@ -67,32 +106,29 @@ impl Watchdog {
                 .flat_map(|v| {
                     let mut hss = vec![v.hash];
                     if hash_slots[v.hash] == v.slots[0] {
-                        hss.extend(v.slots.iter().filter_map(|s| slot_hashes.get(s)))
+                        hss.extend(v.slots.iter().flat_map(|s| self.observed_slots.get(s).flat_map(|h| h.1.iter())))
                     }
                     hss
                 })
                 .collect();
             for hash in hashes {
-                *heap_map.entry(hash).or_insert(0) +=
-                    vote_accounts.get(key).map(|v| v.0).unwrap_or_default();
+                self.hash_heat_map.entry(hash).or_default().1.insert(key);
+                self.hash_heat_map.entry(hash).or_default().0 = now;
             }
         }
-        heat_map
     }
 
-    fn vote_heat_map(
-        vote_accounts: &HashMap<Pubkey, (u64, Account)>,
+    fn update_vote_heat_map(
+        &mut self,
         votes: &HashMap<Pubkey, Vec<Vote>>,
     ) -> HashMap<Slot, u64> {
-        let mut heat_map = HashMap::new();
+        let now = timestamp();
         for (key, val) in votes {
-            let slots: HashSet<Slot> = val.votes.iter().flat_map(|v| v.slots).collect();
-            for slot in slots {
-                *heap_map.entry(slot).or_insert(0) +=
-                    vote_accounts.get(key).map(|v| v.0).unwrap_or(0);
-            }
+            val.votes.iter().flat_map(|v| v.slots).for_each(|slot| {
+                self.slot_heat_map.entry(slot).or_default().insert(key);
+                self.slot_heat_map.entry(slot).or_default().0 = now;
+            });
         }
-        heat_map
     }
 
     fn collect_votes(votes: Vec<Transaction>) -> HashMap<Pubkey, Vec<Vote>> {
