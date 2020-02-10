0e3ace31f45063e8136059a190c5d393a416d0e1
Ryo Onodera <ryoqun@solana.com>
2020-02-10 18:30:48 +0900

    Just define BnakSlotDelta type alias

--- core/tests/bank_forks.rs
+++ core/tests/bank_forks.rs
@@ -14,8 +14,8 @@ mod tests {
         snapshot_utils,
     };
     use solana_runtime::{
-        bank::Bank,
-        status_cache::{SlotDelta, MAX_CACHE_ENTRIES},
+        bank::{Bank, BankSlotDelta},
+        status_cache::MAX_CACHE_ENTRIES,
     };
     use solana_sdk::{
         clock::Slot,
@@ -312,7 +312,7 @@ mod tests {
         // before we compare, stick an empty status_cache in this dir so that the package comparision works
         // This is needed since the status_cache is added by the packager and is not collected from
         // the source dir for snapshots
-        let dummy_slot_deltas: Vec<SlotDelta<transaction::Result<()>>> = vec![];
+        let dummy_slot_deltas: Vec<BankSlotDelta> = vec![];
         snapshot_utils::serialize_snapshot_data_file(
             &saved_snapshots_dir
                 .path()
