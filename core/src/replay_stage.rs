a30214fe217dd8cbc15a24338aadc9ae81bdcdbe
carllin <wumu727@gmail.com>
2020-02-18 02:39:47 -0800

    Factor out creating genesis with vote accounts into a utility function (#8315)
    automerge
    
    (cherry picked from commit 73a278dc647d12cf43a6c14dccfd1edb42cc0ee5)
    
    # Conflicts:
    #	core/src/consensus.rs
    #	core/src/replay_stage.rs

--- core/src/replay_stage.rs
+++ core/src/replay_stage.rs
@@ -1179,6 +1179,10 @@ pub(crate) mod tests {
     use super::*;
     use crate::{
         commitment::BlockCommitment,
+<<<<<<< HEAD
+=======
+        consensus::test::{initialize_state, VoteResult, VoteSimulator},
+>>>>>>> 73a278dc6... Factor out creating genesis with vote accounts into a utility function (#8315)
         consensus::Tower,
         genesis_utils::{create_genesis_config, create_genesis_config_with_leader},
         replay_stage::ReplayStage,
@@ -1197,7 +1201,7 @@ pub(crate) mod tests {
             SIZE_OF_COMMON_SHRED_HEADER, SIZE_OF_DATA_SHRED_HEADER, SIZE_OF_DATA_SHRED_PAYLOAD,
         },
     };
-    use solana_runtime::genesis_utils::GenesisConfigInfo;
+    use solana_runtime::genesis_utils::{GenesisConfigInfo, ValidatorVoteKeypairs};
     use solana_sdk::{
         account::Account,
         hash::{hash, Hash},
@@ -2014,4 +2018,76 @@ pub(crate) mod tests {
         }
         Blockstore::destroy(&ledger_path).unwrap();
     }
+<<<<<<< HEAD
+=======
+
+    #[test]
+    fn test_child_bank_heavier() {
+        let node_keypair = Keypair::new();
+        let vote_keypair = Keypair::new();
+        let stake_keypair = Keypair::new();
+        let node_pubkey = node_keypair.pubkey();
+        let mut keypairs = HashMap::new();
+        keypairs.insert(
+            node_pubkey,
+            ValidatorVoteKeypairs::new(node_keypair, vote_keypair, stake_keypair),
+        );
+
+        let (bank_forks, mut progress) = initialize_state(&keypairs);
+        let bank_forks = Arc::new(RwLock::new(bank_forks));
+        let mut tower = Tower::new_with_key(&node_pubkey);
+
+        // Create the tree of banks in a BankForks object
+        let forks = tr(0) / (tr(1) / (tr(2) / (tr(3))));
+
+        let mut voting_simulator = VoteSimulator::new(&forks);
+        let mut cluster_votes: HashMap<Pubkey, Vec<Slot>> = HashMap::new();
+        let votes: Vec<Slot> = vec![0, 2];
+        for vote in &votes {
+            assert_eq!(
+                voting_simulator.simulate_vote(
+                    *vote,
+                    &bank_forks,
+                    &mut cluster_votes,
+                    &keypairs,
+                    keypairs.get(&node_pubkey).unwrap(),
+                    &mut progress,
+                    &mut tower,
+                ),
+                VoteResult::Ok
+            );
+        }
+
+        let mut frozen_banks: Vec<_> = bank_forks
+            .read()
+            .unwrap()
+            .frozen_banks()
+            .values()
+            .cloned()
+            .collect();
+
+        ReplayStage::compute_bank_stats(
+            &Pubkey::default(),
+            &bank_forks.read().unwrap().ancestors(),
+            &mut frozen_banks,
+            &tower,
+            &mut progress,
+        );
+
+        frozen_banks.sort_by_key(|bank| bank.slot());
+        for pair in frozen_banks.windows(2) {
+            let first = progress
+                .get(&pair[0].slot())
+                .unwrap()
+                .fork_stats
+                .fork_weight;
+            let second = progress
+                .get(&pair[1].slot())
+                .unwrap()
+                .fork_stats
+                .fork_weight;
+            assert!(second >= first);
+        }
+    }
+>>>>>>> 73a278dc6... Factor out creating genesis with vote accounts into a utility function (#8315)
 }
