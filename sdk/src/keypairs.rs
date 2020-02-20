09adbf327159115570bd7a4e6084dda5155eed9a
Greg Fitzgerald <greg@solana.com>
2020-02-20 10:58:16 -0700

    Dedup

--- sdk/src/keypairs.rs
+++ sdk/src/keypairs.rs
@@ -8,98 +8,50 @@ pub trait Keypairs {
     fn sign_message(&self, message: &[u8]) -> Vec<Signature>;
 }
 
-impl<T: KeypairUtil> Keypairs for [&T] {
-    fn pubkeys(&self) -> Vec<Pubkey> {
-        self.iter().map(|keypair| keypair.pubkey()).collect()
-    }
+macro_rules! default_keypairs_impl {
+    () => (
+            fn pubkeys(&self) -> Vec<Pubkey> {
+                self.iter().map(|keypair| keypair.pubkey()).collect()
+            }
 
-    fn sign_message(&self, message: &[u8]) -> Vec<Signature> {
-        self.iter()
-            .map(|keypair| keypair.sign_message(message))
-            .collect()
-    }
+            fn sign_message(&self, message: &[u8]) -> Vec<Signature> {
+                self.iter()
+                    .map(|keypair| keypair.sign_message(message))
+                    .collect()
+            }
+    );
+}
+
+impl<T: KeypairUtil> Keypairs for [&T] {
+    default_keypairs_impl!();
 }
 
 impl Keypairs for [Box<dyn KeypairUtil>] {
-    fn pubkeys(&self) -> Vec<Pubkey> {
-        self.iter().map(|keypair| keypair.pubkey()).collect()
-    }
-
-    fn sign_message(&self, message: &[u8]) -> Vec<Signature> {
-        self.iter()
-            .map(|keypair| keypair.sign_message(message))
-            .collect()
-    }
+    default_keypairs_impl!();
 }
 
 impl<T: KeypairUtil> Keypairs for [&T; 0] {
-    fn pubkeys(&self) -> Vec<Pubkey> {
-        vec![]
-    }
-
-    fn sign_message(&self, _message: &[u8]) -> Vec<Signature> {
-        vec![]
-    }
+    default_keypairs_impl!();
 }
 
 impl<T: KeypairUtil> Keypairs for [&T; 1] {
-    fn pubkeys(&self) -> Vec<Pubkey> {
-        self.iter().map(|keypair| keypair.pubkey()).collect()
-    }
-
-    fn sign_message(&self, message: &[u8]) -> Vec<Signature> {
-        self.iter()
-            .map(|keypair| keypair.sign_message(message))
-            .collect()
-    }
+    default_keypairs_impl!();
 }
 
 impl<T: KeypairUtil> Keypairs for [&T; 2] {
-    fn pubkeys(&self) -> Vec<Pubkey> {
-        self.iter().map(|keypair| keypair.pubkey()).collect()
-    }
-
-    fn sign_message(&self, message: &[u8]) -> Vec<Signature> {
-        self.iter()
-            .map(|keypair| keypair.sign_message(message))
-            .collect()
-    }
+    default_keypairs_impl!();
 }
 
 impl<T: KeypairUtil> Keypairs for [&T; 3] {
-    fn pubkeys(&self) -> Vec<Pubkey> {
-        self.iter().map(|keypair| keypair.pubkey()).collect()
-    }
-
-    fn sign_message(&self, message: &[u8]) -> Vec<Signature> {
-        self.iter()
-            .map(|keypair| keypair.sign_message(message))
-            .collect()
-    }
+    default_keypairs_impl!();
 }
 
 impl<T: KeypairUtil> Keypairs for [&T; 4] {
-    fn pubkeys(&self) -> Vec<Pubkey> {
-        self.iter().map(|keypair| keypair.pubkey()).collect()
-    }
-
-    fn sign_message(&self, message: &[u8]) -> Vec<Signature> {
-        self.iter()
-            .map(|keypair| keypair.sign_message(message))
-            .collect()
-    }
+    default_keypairs_impl!();
 }
 
 impl<T: KeypairUtil> Keypairs for Vec<&T> {
-    fn pubkeys(&self) -> Vec<Pubkey> {
-        self.iter().map(|keypair| keypair.pubkey()).collect()
-    }
-
-    fn sign_message(&self, message: &[u8]) -> Vec<Signature> {
-        self.iter()
-            .map(|keypair| keypair.sign_message(message))
-            .collect()
-    }
+    default_keypairs_impl!();
 }
 
 #[cfg(test)]
