e8007401661f31fbf505c23f4e0bd09b1e35dcd4
Michael Vines <mvines@gmail.com>
2020-03-01 15:17:23 -0700

    Avoid is_x86_feature_detected when not building for x86

--- core/src/validator.rs
+++ core/src/validator.rs
@@ -750,16 +750,19 @@ fn report_target_features() {
         }
     );
 
-    // Validator binaries built on a machine with AVX support will generate invalid opcodes
-    // when run on machines without AVX causing a non-obvious process abort.  Instead detect
-    // the mismatch and error cleanly.
-    #[target_feature(enable = "avx")]
+    #[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
     {
-        if is_x86_feature_detected!("avx") {
-            info!("AVX detected");
-        } else {
-            error!("Your machine does not have AVX support, please rebuild from source on your machine");
-            process::exit(1);
+        // Validator binaries built on a machine with AVX support will generate invalid opcodes
+        // when run on machines without AVX causing a non-obvious process abort.  Instead detect
+        // the mismatch and error cleanly.
+        #[target_feature(enable = "avx")]
+        {
+            if is_x86_feature_detected!("avx") {
+                info!("AVX detected");
+            } else {
+                error!("Your machine does not have AVX support, please rebuild from source on your machine");
+                process::exit(1);
+            }
         }
     }
 }
