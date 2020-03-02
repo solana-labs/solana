0f6f6bf10ddaa1b8132683842921e8aec8205b53
Ryo Onodera <ryoqun@gmail.com>
2020-03-02 22:49:09 +0900

    Hack to skip cleanup_dead_slots upon snapshot load
    (cherry picked from commit 5d9130a3c462e49bbdde2eee47cff6e3631e2d0c)

--- ledger/src/snapshot_utils.rs
+++ ledger/src/snapshot_utils.rs
@@ -453,16 +453,18 @@ pub fn bank_from_archive<P: AsRef<Path>>(
         String::from("0.22.3")
     };
 
-    let bank = rebuild_bank_from_snapshots(
+    let mut bank = rebuild_bank_from_snapshots(
         snapshot_version.trim(),
         account_paths,
         &unpacked_snapshots_dir,
         unpacked_accounts_dir,
     )?;
 
+    bank.work_around_dead_slots_cleaning_bug(true);
     if !bank.verify_snapshot_bank() {
         panic!("Snapshot bank for slot {} failed to verify", bank.slot());
     }
+    bank.work_around_dead_slots_cleaning_bug(false);
     measure.stop();
     info!("{}", measure);
 
