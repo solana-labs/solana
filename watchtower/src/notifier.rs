55dee2901ec47ff660c8ef3b9999eaadc0555a7d
HM <43654523+HM999@users.noreply.github.com>
2020-03-03 06:37:57 +0000

    watchtower: flag to suppress duplicate notifications (#8549)
    * watchtower: send error message as notification
    
    * watchtower: send all clear notification when ok again
    
    * watchtower: add twilio sms notifications
    
    * watchtower: flag to suppress duplicate notifications
    
    * remove trailing space character
    
    * changes as per suggestion on PR
    
    * all changes together
    
    * cargo fmt

--- watchtower/src/notifier.rs
+++ watchtower/src/notifier.rs
@@ -8,11 +8,60 @@ struct TelegramWebHook {
     chat_id: String,
 }
 
+#[derive(Debug, Default)]
+struct TwilioWebHook {
+    account: String,
+    token: String,
+    to: String,
+    from: String,
+}
+
+impl TwilioWebHook {
+    fn complete(&self) -> bool {
+        !(self.account.is_empty()
+            || self.token.is_empty()
+            || self.to.is_empty()
+            || self.from.is_empty())
+    }
+}
+
+fn get_twilio_config() -> Result<Option<TwilioWebHook>, String> {
+    let config_var = env::var("TWILIO_CONFIG");
+
+    if config_var.is_err() {
+        info!("Twilio notifications disabled");
+        return Ok(None);
+    }
+
+    let mut config = TwilioWebHook::default();
+
+    for pair in config_var.unwrap().split(',') {
+        let nv: Vec<_> = pair.split('=').collect();
+        if nv.len() != 2 {
+            return Err(format!("TWILIO_CONFIG is invalid: '{}'", pair));
+        }
+        let v = nv[1].to_string();
+        match nv[0] {
+            "ACCOUNT" => config.account = v,
+            "TOKEN" => config.token = v,
+            "TO" => config.to = v,
+            "FROM" => config.from = v,
+            _ => return Err(format!("TWILIO_CONFIG is invalid: '{}'", pair)),
+        }
+    }
+
+    if !config.complete() {
+        return Err("TWILIO_CONFIG is incomplete".to_string());
+    }
+    Ok(Some(config))
+}
+
 pub struct Notifier {
     client: Client,
     discord_webhook: Option<String>,
     slack_webhook: Option<String>,
     telegram_webhook: Option<TelegramWebHook>,
+    twilio_webhook: Option<TwilioWebHook>,
 }
 
 impl Notifier {
@@ -35,12 +84,16 @@ impl Notifier {
             info!("Telegram notifications disabled");
             None
         };
+        let twilio_webhook = get_twilio_config()
+            .map_err(|err| panic!("Twilio config error: {}", err))
+            .unwrap();
 
         Notifier {
             client: Client::new(),
             discord_webhook,
             slack_webhook,
             telegram_webhook,
+            twilio_webhook,
         }
     }
 
@@ -65,6 +118,23 @@ impl Notifier {
 
             if let Err(err) = self.client.post(&url).json(&data).send() {
                 warn!("Failed to send Telegram message: {:?}", err);
+            }
+        }
+
+        if let Some(TwilioWebHook {
+            account,
+            token,
+            to,
+            from,
+        }) = &self.twilio_webhook
+        {
+            let url = format!(
+                "https://{}:{}@api.twilio.com/2010-04-01/Accounts/{}/Messages.json",
+                account, token, account
+            );
+            let params = [("To", to), ("From", from), ("Body", &msg.to_string())];
+            if let Err(err) = self.client.post(&url).form(&params).send() {
+                warn!("Failed to send Twilio message: {:?}", err);
             }
         }
     }
