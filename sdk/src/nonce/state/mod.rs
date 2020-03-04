0b148b98644e2a2a1ba3fa86ac22aec9f15429ae
Trent Nelson <trent@solana.com>
2020-03-04 09:58:50 -0700

    Copy current state version to v0

--- sdk/src/nonce/state/mod.rs
+++ sdk/src/nonce/state/mod.rs
@@ -1,10 +1,13 @@
 mod current;
 pub use current::{Data, State};
 
+pub mod v0;
+
 use serde_derive::{Deserialize, Serialize};
 
 #[derive(Debug, Serialize, Deserialize, PartialEq, Clone)]
 pub enum Versions {
+    V0(Box<v0::State>),
     Current(Box<State>),
 }
 
@@ -16,6 +19,15 @@ impl Versions {
     pub fn convert_to_current(self) -> State {
         match self {
             Self::Current(state) => *state,
+            Self::V0(v0_state) => {
+                match *v0_state {
+                    v0::State::Uninitialized => State::Uninitialized,
+                    v0::State::Initialized(v0_data) => {
+                        let v0::Data { authority, blockhash} = v0_data;
+                        State::Initialized(Data { authority, blockhash })
+                    }
+                }
+            }
         }
     }
 }
---
+++ sdk/src/nonce/state/v0.rs
@@ -0,0 +1,38 @@
+use super::Versions;
+use crate::{hash::Hash, pubkey::Pubkey};
+use serde_derive::{Deserialize, Serialize};
+
+#[derive(Debug, Default, Serialize, Deserialize, PartialEq, Clone, Copy)]
+pub struct Data {
+    pub authority: Pubkey,
+    pub blockhash: Hash,
+}
+
+#[derive(Debug, Serialize, Deserialize, PartialEq, Clone, Copy)]
+pub enum State {
+    Uninitialized,
+    Initialized(Data),
+}
+
+impl Default for State {
+    fn default() -> Self {
+        State::Uninitialized
+    }
+}
+
+impl State {
+    pub fn size() -> usize {
+        let data = Versions::V0(Box::new(State::Initialized(Data::default())));
+        bincode::serialized_size(&data).unwrap() as usize
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn default_is_uninitialized() {
+        assert_eq!(State::default(), State::Uninitialized)
+    }
+}
