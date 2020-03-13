6aa797c05c48f69ba770f47acc94b34945ddd51a
Justin Starry <justin@solana.com>
2020-03-13 17:45:34 +0800

    Add placeholder main page

--- explorer/src/index.tsx
+++ explorer/src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './scss/theme.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
