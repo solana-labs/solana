e6cc249957e9d6a38906881f00c0ced67ba0816b
Dan Albert <dan@solana.com>
2020-03-16 16:21:54 -0700

    Refactor automation framework to accept arbitary scripts after cluster launch

--- system-test/stake-operations-testcases/stake_test_automation.sh
+++ system-test/stake-operations-testcases/stake_test_automation.sh
@@ -1,19 +1,14 @@
 #!/usr/bin/env bash
 
-set -e
-set -x
+set -ex
 
+# shellcheck disable=SC1090
 # shellcheck disable=SC1091
 source "$(dirname "$0")"/../automation_utils.sh
 
-curl -sSf https://raw.githubusercontent.com/solana-labs/solana/v1.0.5/install/solana-install-init.sh | sh -s - 1.0.5
-
-# Create a single node cluster on colo, then call offline_stake_operations.sh against that cluster
-"${REPO_ROOT}"/net/colo.sh delete --reclaim-preemptible-reservations
-"${REPO_ROOT}"/net/colo.sh create -n 1 -c 0 -p stake-ops-testnet --dedicated
-"${REPO_ROOT}"/net/net.sh start -t edge
+# Runs offline stake operations tests against a running cluster launched from the automation framework
 
 bootstrapper_ip_address="$(get_bootstrap_validator_ip_address)"
 entrypoint=http://"${bootstrapper_ip_address}":8899
 
-"${REPO_ROOT}"/system-test/stake-operations-testcases/offline_stake_operations.sh "$entrypoint"
+PATH="$REPO_ROOT"/solana-release/bin:$PATH "$REPO_ROOT"/system-test/stake-operations-testcases/offline_stake_operations.sh "$entrypoint"
