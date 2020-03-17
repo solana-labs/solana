d087ed5bf681ebf753e946ebf547a3ebc6351bf6
Jack May <jack@solana.com>
2020-03-17 15:59:09 -0700

    Remove copypasta (#8912)

--- programs/bpf/tests/programs.rs
+++ programs/bpf/tests/programs.rs
@@ -4,10 +4,21 @@ mod bpf {
         bank::Bank,
         bank_client::BankClient,
         genesis_utils::{create_genesis_config, GenesisConfigInfo},
-        loader_utils::load_program,
+        loader_utils::{load_program},
     };
-    use solana_sdk::{bpf_loader, pubkey::Pubkey, signature::Keypair};
-    use std::{env, fs::File, io::Read, path::PathBuf};
+    use solana_sdk::{
+        account::Account,
+        bpf_loader,
+        client::SyncClient,
+        clock::DEFAULT_SLOTS_PER_EPOCH,
+        instruction::{AccountMeta, Instruction, InstructionError},
+        pubkey::Pubkey,
+        signature::Keypair,
+        signature::Signer,
+        sysvar::{clock, fees, rent, rewards, slot_hashes, stake_history},
+        transaction::TransactionError,
+    };
+    use std::{env, fs::File, io::Read, path::PathBuf, sync::Arc};
 
     /// BPF program file extension
     const PLATFORM_FILE_EXTENSION_BPF: &str = "so";
@@ -33,25 +44,15 @@ mod bpf {
         load_program(bank_client, payer_keypair, &bpf_loader::id(), elf)
     }
 
-    #[cfg(feature = "bpf_c")]
-    mod bpf_c {
-        use super::*;
-        use solana_runtime::loader_utils::create_invoke_instruction;
-        use solana_sdk::{
-            account::Account,
-            client::SyncClient,
-            instruction::{AccountMeta, Instruction, InstructionError},
-            pubkey::Pubkey,
-            signature::Signer,
-            transaction::TransactionError,
-        };
-        use std::sync::Arc;
-
         #[test]
-        fn test_program_bpf_c() {
+#[cfg(any(feature = "bpf_c", feature = "bpf_rust"))]
+    fn test_program_bpf_sanity() {
             solana_logger::setup();
 
-            let programs = [
+        let mut programs = Vec::new();
+        #[cfg(feature = "bpf_c")]
+        {
+            programs.extend_from_slice(&[
                 ("bpf_to_bpf", true),
                 ("multiple_static", true),
                 ("noop", true),
@@ -60,176 +61,11 @@ mod bpf {
                 ("relative_call", true),
                 ("struct_pass", true),
                 ("struct_ret", true),
-            ];
-            for program in programs.iter() {
-                println!("Test program: {:?}", program.0);
-
-                let GenesisConfigInfo {
-                    genesis_config,
-                    mint_keypair,
-                    ..
-                } = create_genesis_config(50);
-                let bank = Bank::new(&genesis_config);
-                let bank_client = BankClient::new(bank);
-
-                // Call user program
-                let program_id = load_bpf_program(&bank_client, &mint_keypair, program.0);
-                let instruction =
-                    create_invoke_instruction(mint_keypair.pubkey(), program_id, &1u8);
-                let result = bank_client.send_instruction(&mint_keypair, instruction);
-                if program.1 {
-                    assert!(result.is_ok());
-                } else {
-                    assert!(result.is_err());
-                }
-            }
+            ]);
         }
-
-        #[test]
-        fn test_program_bpf_c_duplicate_accounts() {
-            solana_logger::setup();
-
-            let GenesisConfigInfo {
-                genesis_config,
-                mint_keypair,
-                ..
-            } = create_genesis_config(50);
-
-            let bank = Arc::new(Bank::new(&genesis_config));
-            let bank_client = BankClient::new_shared(&bank);
-            let program_id = load_bpf_program(&bank_client, &mint_keypair, "dup_accounts");
-            let payee_account = Account::new(10, 1, &program_id);
-            let payee_pubkey = Pubkey::new_rand();
-            bank.store_account(&payee_pubkey, &payee_account);
-
-            let account = Account::new(10, 1, &program_id);
-            let pubkey = Pubkey::new_rand();
-            let account_metas = vec![
-                AccountMeta::new(mint_keypair.pubkey(), true),
-                AccountMeta::new(payee_pubkey, false),
-                AccountMeta::new(pubkey, false),
-                AccountMeta::new(pubkey, false),
-            ];
-
-            bank.store_account(&pubkey, &account);
-            let instruction = Instruction::new(program_id, &1u8, account_metas.clone());
-            let result = bank_client.send_instruction(&mint_keypair, instruction);
-            let data = bank_client.get_account_data(&pubkey).unwrap().unwrap();
-            assert!(result.is_ok());
-            assert_eq!(data[0], 1);
-
-            bank.store_account(&pubkey, &account);
-            let instruction = Instruction::new(program_id, &2u8, account_metas.clone());
-            let result = bank_client.send_instruction(&mint_keypair, instruction);
-            let data = bank_client.get_account_data(&pubkey).unwrap().unwrap();
-            assert!(result.is_ok());
-            assert_eq!(data[0], 2);
-
-            bank.store_account(&pubkey, &account);
-            let instruction = Instruction::new(program_id, &3u8, account_metas.clone());
-            let result = bank_client.send_instruction(&mint_keypair, instruction);
-            let data = bank_client.get_account_data(&pubkey).unwrap().unwrap();
-            assert!(result.is_ok());
-            assert_eq!(data[0], 3);
-
-            bank.store_account(&pubkey, &account);
-            let instruction = Instruction::new(program_id, &4u8, account_metas.clone());
-            let result = bank_client.send_instruction(&mint_keypair, instruction);
-            let lamports = bank_client.get_balance(&pubkey).unwrap();
-            assert!(result.is_ok());
-            assert_eq!(lamports, 11);
-
-            bank.store_account(&pubkey, &account);
-            let instruction = Instruction::new(program_id, &5u8, account_metas.clone());
-            let result = bank_client.send_instruction(&mint_keypair, instruction);
-            let lamports = bank_client.get_balance(&pubkey).unwrap();
-            assert!(result.is_ok());
-            assert_eq!(lamports, 12);
-
-            bank.store_account(&pubkey, &account);
-            let instruction = Instruction::new(program_id, &6u8, account_metas.clone());
-            let result = bank_client.send_instruction(&mint_keypair, instruction);
-            let lamports = bank_client.get_balance(&pubkey).unwrap();
-            assert!(result.is_ok());
-            assert_eq!(lamports, 13);
-        }
-
-        #[test]
-        fn test_program_bpf_c_error_handling() {
-            solana_logger::setup();
-
-            let GenesisConfigInfo {
-                genesis_config,
-                mint_keypair,
-                ..
-            } = create_genesis_config(50);
-
-            let bank = Bank::new(&genesis_config);
-            let bank_client = BankClient::new(bank);
-            let program_id = load_bpf_program(&bank_client, &mint_keypair, "error_handling");
-            let account_metas = vec![AccountMeta::new(mint_keypair.pubkey(), true)];
-
-            let instruction = Instruction::new(program_id, &1u8, account_metas.clone());
-            let result = bank_client.send_instruction(&mint_keypair, instruction);
-            assert!(result.is_ok());
-
-            let instruction = Instruction::new(program_id, &2u8, account_metas.clone());
-            let result = bank_client.send_instruction(&mint_keypair, instruction);
-            assert_eq!(
-                result.unwrap_err().unwrap(),
-                TransactionError::InstructionError(0, InstructionError::InvalidAccountData)
-            );
-
-            let instruction = Instruction::new(program_id, &3u8, account_metas.clone());
-            let result = bank_client.send_instruction(&mint_keypair, instruction);
-            assert_eq!(
-                result.unwrap_err().unwrap(),
-                TransactionError::InstructionError(0, InstructionError::CustomError(42))
-            );
-
-            let instruction = Instruction::new(program_id, &4u8, account_metas.clone());
-            let result = bank_client.send_instruction(&mint_keypair, instruction);
-            assert_eq!(
-                result.unwrap_err().unwrap(),
-                TransactionError::InstructionError(0, InstructionError::InvalidError)
-            );
-
-            let instruction = Instruction::new(program_id, &5u8, account_metas.clone());
-            let result = bank_client.send_instruction(&mint_keypair, instruction);
-            assert_eq!(
-                result.unwrap_err().unwrap(),
-                TransactionError::InstructionError(0, InstructionError::InvalidError)
-            );
-
-            let instruction = Instruction::new(program_id, &6u8, account_metas.clone());
-            let result = bank_client.send_instruction(&mint_keypair, instruction);
-            assert_eq!(
-                result.unwrap_err().unwrap(),
-                TransactionError::InstructionError(0, InstructionError::InvalidInstructionData)
-            );
-        }
-    }
-
     #[cfg(feature = "bpf_rust")]
-    mod bpf_rust {
-        use super::*;
-        use solana_sdk::{
-            account::Account,
-            client::SyncClient,
-            clock::DEFAULT_SLOTS_PER_EPOCH,
-            instruction::{AccountMeta, Instruction, InstructionError},
-            pubkey::Pubkey,
-            signature::{Keypair, Signer},
-            sysvar::{clock, fees, rent, rewards, slot_hashes, stake_history},
-            transaction::TransactionError,
-        };
-        use std::sync::Arc;
-
-        #[test]
-        fn test_program_bpf_rust() {
-            solana_logger::setup();
-
-            let programs = [
+        {
+            programs.extend_from_slice(&[
                 ("solana_bpf_rust_128bit", true),
                 ("solana_bpf_rust_alloc", true),
                 ("solana_bpf_rust_dep_crate", true),
@@ -240,7 +76,9 @@ mod bpf {
                 ("solana_bpf_rust_panic", false),
                 ("solana_bpf_rust_param_passing", true),
                 ("solana_bpf_rust_sysval", true),
-            ];
+            ]);
+        }
+
             for program in programs.iter() {
                 println!("Test program: {:?}", program.0);
 
@@ -278,8 +116,21 @@ mod bpf {
         }
 
         #[test]
-        fn test_program_bpf_rust_duplicate_accounts() {
+    fn test_program_bpf_duplicate_accounts() {
             solana_logger::setup();
+
+        let mut programs = Vec::new();
+        #[cfg(feature = "bpf_c")]
+        {
+            programs.extend_from_slice(&[("dup_accounts")]);
+        }
+        #[cfg(feature = "bpf_rust")]
+        {
+            programs.extend_from_slice(&[("solana_bpf_rust_dup_accounts")]);
+        }
+
+        for program in programs.iter() {
+            println!("Test program: {:?}", program);
 
             let GenesisConfigInfo {
                 genesis_config,
@@ -288,8 +139,7 @@ mod bpf {
             } = create_genesis_config(50);
             let bank = Arc::new(Bank::new(&genesis_config));
             let bank_client = BankClient::new_shared(&bank);
-            let program_id =
-                load_bpf_program(&bank_client, &mint_keypair, "solana_bpf_rust_dup_accounts");
+            let program_id = load_bpf_program(&bank_client, &mint_keypair, program);
             let payee_account = Account::new(10, 1, &program_id);
             let payee_pubkey = Pubkey::new_rand();
             bank.store_account(&payee_pubkey, &payee_account);
@@ -345,10 +195,24 @@ mod bpf {
             assert!(result.is_ok());
             assert_eq!(lamports, 13);
         }
+    }
 
         #[test]
-        fn test_program_bpf_rust_error_handling() {
+    fn test_program_bpf_error_handling() {
             solana_logger::setup();
+
+        let mut programs = Vec::new();
+        #[cfg(feature = "bpf_c")]
+        {
+            programs.extend_from_slice(&[("error_handling")]);
+        }
+        #[cfg(feature = "bpf_rust")]
+        {
+            programs.extend_from_slice(&[("solana_bpf_rust_error_handling")]);
+        }
+
+        for program in programs.iter() {
+            println!("Test program: {:?}", program);
 
             let GenesisConfigInfo {
                 genesis_config,
@@ -357,11 +221,7 @@ mod bpf {
             } = create_genesis_config(50);
             let bank = Bank::new(&genesis_config);
             let bank_client = BankClient::new(bank);
-            let program_id = load_bpf_program(
-                &bank_client,
-                &mint_keypair,
-                "solana_bpf_rust_error_handling",
-            );
+            let program_id = load_bpf_program(&bank_client, &mint_keypair, program);
             let account_metas = vec![AccountMeta::new(mint_keypair.pubkey(), true)];
 
             let instruction = Instruction::new(program_id, &1u8, account_metas.clone());
@@ -389,14 +249,43 @@ mod bpf {
                 TransactionError::InstructionError(0, InstructionError::CustomError(42))
             );
 
+            let instruction = Instruction::new(program_id, &5u8, account_metas.clone());
+            let result = bank_client.send_instruction(&mint_keypair, instruction);
+            let result = result.unwrap_err().unwrap();
+            if TransactionError::InstructionError(0, InstructionError::InvalidInstructionData)
+                != result
+            {
+                assert_eq!(
+                    result,
+                    TransactionError::InstructionError(0, InstructionError::InvalidError)
+                );
+            }
+
             let instruction = Instruction::new(program_id, &6u8, account_metas.clone());
             let result = bank_client.send_instruction(&mint_keypair, instruction);
-            assert_eq!(
-                result.unwrap_err().unwrap(),
-                TransactionError::InstructionError(0, InstructionError::AccountBorrowFailed)
-            );
+            let result = result.unwrap_err().unwrap();
+            if TransactionError::InstructionError(0, InstructionError::InvalidInstructionData)
+                != result
+            {
+                assert_eq!(
+                    result,
+                    TransactionError::InstructionError(0, InstructionError::InvalidError)
+                );
+            }
 
             let instruction = Instruction::new(program_id, &7u8, account_metas.clone());
+            let result = bank_client.send_instruction(&mint_keypair, instruction);
+            let result = result.unwrap_err().unwrap();
+            if TransactionError::InstructionError(0, InstructionError::InvalidInstructionData)
+                != result
+            {
+            assert_eq!(
+                    result,
+                TransactionError::InstructionError(0, InstructionError::AccountBorrowFailed)
+            );
+            }
+
+            let instruction = Instruction::new(program_id, &8u8, account_metas.clone());
             let result = bank_client.send_instruction(&mint_keypair, instruction);
             assert_eq!(
                 result.unwrap_err().unwrap(),
