6d81b026b91a9e8ee463f95b7b9979d93b0482cb
Greg Fitzgerald <greg@solana.com>
2020-04-06 11:55:56 -0600

    Default to mainnet-beta

--- cli-config/src/config.rs
+++ cli-config/src/config.rs
@@ -26,7 +26,7 @@ impl Default for Config {
             keypair_path.extend(&[".config", "solana", "id.json"]);
             keypair_path.to_str().unwrap().to_string()
         };
-        let json_rpc_url = "http://127.0.0.1:8899".to_string();
+        let json_rpc_url = "https://api.mainnet-beta.solana.com".to_string();
 
         // Empty websocket_url string indicates the client should
         // `Config::compute_websocket_url(&json_rpc_url)`
