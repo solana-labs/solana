1eff7fa39c16a8b81aa3b489564f3fe746874bdb
Michael Vines <mvines@gmail.com>
2020-04-15 17:07:47 -0700

    Use $rust_stable
    (cherry picked from commit d567799d43f8d074debc7022b8b67b05c22e451e)
    
    # Conflicts:
    #	docs/build-cli-usage.sh

--- docs/build-cli-usage.sh
+++ docs/build-cli-usage.sh
@@ -3,7 +3,11 @@ set -e
 
 cd "$(dirname "$0")"
 
+<<<<<<< HEAD
 usage=$(cargo -q run -p solana-cli -- -C ~/.foo --help | sed 's|'"$HOME"'|~|g')
+=======
+usage=$(cargo +"$rust_stable" -q run -p solana-cli -- -C ~/.foo --help | sed -e 's|'"$HOME"'|~|g' -e 's/[[:space:]]\+$//')
+>>>>>>> d567799d4... Use $rust_stable
 
 out=${1:-src/cli/usage.md}
 
@@ -29,6 +33,10 @@ in_subcommands=0
 while read -r subcommand rest; do
   [[ $subcommand == "SUBCOMMANDS:" ]] && in_subcommands=1 && continue
   if ((in_subcommands)); then
+<<<<<<< HEAD
       section "$(cargo -q run -p solana-cli -- help "$subcommand" | sed 's|'"$HOME"'|~|g')" "####" >> "$out"
+=======
+      section "$(cargo +"$rust_stable" -q run -p solana-cli -- help "$subcommand" | sed -e 's|'"$HOME"'|~|g' -e 's/[[:space:]]\+$//')" "####" >> "$out"
+>>>>>>> d567799d4... Use $rust_stable
   fi
 done <<<"$usage">>"$out"
