06d6905df1b6f1a1b77417bfd2773914270d250e
Michael Vines <mvines@gmail.com>
2020-04-15 17:18:36 -0700

    Update build-cli-usage.sh

--- docs/build-cli-usage.sh
+++ docs/build-cli-usage.sh
@@ -3,11 +3,7 @@ set -e
 
 cd "$(dirname "$0")"
 
-<<<<<<< HEAD
-usage=$(cargo -q run -p solana-cli -- -C ~/.foo --help | sed 's|'"$HOME"'|~|g')
-=======
-usage=$(cargo +"$rust_stable" -q run -p solana-cli -- -C ~/.foo --help | sed -e 's|'"$HOME"'|~|g' -e 's/[[:space:]]\+$//')
->>>>>>> d567799d4... Use $rust_stable
+usage=$(cargo +"$rust_stable" -q run -p solana-cli -- -C ~/.foo --help | sed 's|'"$HOME"'|~|g')
 
 out=${1:-src/cli/usage.md}
 
@@ -33,10 +29,6 @@ in_subcommands=0
 while read -r subcommand rest; do
   [[ $subcommand == "SUBCOMMANDS:" ]] && in_subcommands=1 && continue
   if ((in_subcommands)); then
-<<<<<<< HEAD
-      section "$(cargo -q run -p solana-cli -- help "$subcommand" | sed 's|'"$HOME"'|~|g')" "####" >> "$out"
-=======
-      section "$(cargo +"$rust_stable" -q run -p solana-cli -- help "$subcommand" | sed -e 's|'"$HOME"'|~|g' -e 's/[[:space:]]\+$//')" "####" >> "$out"
->>>>>>> d567799d4... Use $rust_stable
+      section "$(cargo +"$rust_stable" -q run -p solana-cli -- help "$subcommand" | sed 's|'"$HOME"'|~|g')" "####" >> "$out"
   fi
 done <<<"$usage">>"$out"
