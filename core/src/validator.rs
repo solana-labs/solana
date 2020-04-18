8134213c1f7f12a849e551c8194a6a77b520e4f9
Michael Vines <mvines@gmail.com>
2020-04-18 10:16:19 -0700

    validator: Consider the activated stake of this node to be online again (#9573)
    automerge
    
    (cherry picked from commit a7aa7e172bae6b35903d3c28d2441447a8373fca)

--- core/src/validator.rs
+++ core/src/validator.rs
@@ -794,7 +794,8 @@ fn get_stake_percent_in_gossip(
 
     let mut total_activated_stake = 0;
     let all_tvu_peers = cluster_info.read().unwrap().all_tvu_peers();
-    let me = cluster_info.read().unwrap().my_data();
+    let my_shred_version = cluster_info.read().unwrap().my_data().shred_version;
+    let my_id = cluster_info.read().unwrap().id();
 
     for (activated_stake, vote_account) in bank.vote_accounts().values() {
         let vote_state =
@@ -809,7 +810,7 @@ fn get_stake_percent_in_gossip(
             .iter()
             .find(|peer| peer.id == vote_state.node_pubkey)
         {
-            if peer.shred_version == me.shred_version {
+            if peer.shred_version == my_shred_version {
                 trace!(
                     "observed {} in gossip, (activated_stake={})",
                     vote_state.node_pubkey,
@@ -820,6 +821,8 @@ fn get_stake_percent_in_gossip(
                 wrong_shred_stake += activated_stake;
                 wrong_shred_nodes.push((*activated_stake, vote_state.node_pubkey));
             }
+        } else if vote_state.node_pubkey == my_id {
+            online_stake += activated_stake; // This node is online
         } else {
             offline_stake += activated_stake;
             offline_nodes.push((*activated_stake, vote_state.node_pubkey));
