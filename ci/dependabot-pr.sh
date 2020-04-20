a092c3f4395b36266de440a051a76e7e5264d022
Ryo Onodera <ryoqun@gmail.com>
2020-04-20 19:08:56 +0900

    Clean up shells and stop obscure API...

--- ci/dependabot-pr.sh
+++ ci/dependabot-pr.sh
@@ -13,24 +13,24 @@ source ci/rust-version.sh stable
 
 ci/docker-run.sh $rust_nightly_docker_image ci/dependabot-updater.sh
 
-if [[ $(git status --short :**/Cargo.lock | wc -l) -gt 0 ]]; then
-  (
-    echo --- "(FAILING) Backpropagating dependabot-triggered Cargo.lock updates"
-
-    git add :**/Cargo.lock
-    NAME="dependabot-buildkite"
-    GIT_AUTHOR_NAME="$NAME" \
-      GIT_COMMITTER_NAME="$NAME" \
-      EMAIL="dependabot-buildkite@noreply.solana.com" \
-      git commit -m "[auto-commit] Update all Cargo lock files"
-    api_base="https://api.github.com/repos/solana-labs/solana/pulls"
-    pr_num=$(echo "$BUILDKITE_BRANCH" | grep -Eo '[0-9]+')
-    branch=$(curl -s "$api_base/$pr_num" | python -c 'import json,sys;print json.load(sys.stdin)["head"]["ref"]')
-    git push origin "HEAD:$branch"
-
-    echo "Source branch is updated; failing this build for the next"
-    exit 1
-  )
+if [[ $(git status --short :**/Cargo.lock | wc -l) -eq 0 ]]; then
+  echo --- ok
+  exit 0
 fi
 
-echo --- ok
+echo --- "(FAILING) Backpropagating dependabot-triggered Cargo.lock updates"
+
+name="dependabot-buildkite"
+api_base="https://api.github.com/repos/solana-labs/solana/pulls"
+pr_num=$(echo "$BUILDKITE_BRANCH" | grep -Eo '[0-9]+')
+branch=$(curl -s "$api_base/$pr_num" | python -c 'import json,sys;print json.load(sys.stdin)["head"]["ref"]')
+
+git add :**/Cargo.lock
+EMAIL="dependabot-buildkite@noreply.solana.com" \
+  GIT_AUTHOR_NAME="$name" \
+  GIT_COMMITTER_NAME="$name" \
+  git commit -m "[auto-commit] Update all Cargo lock files"
+git push origin "HEAD:$branch"
+
+echo "Source branch is updated; failing this build for the next"
+exit 1
