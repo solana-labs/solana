e37755ae2c5329ce11ba7ddf7f4d1e6e897a975d
Ryo Onodera <ryoqun@gmail.com>
2020-04-20 19:51:08 +0900

    Clean up shells and stop obscure API...

--- ci/dependabot-updater.sh
+++ ci/dependabot-updater.sh
@@ -13,8 +13,9 @@ parsed_update_args="$(
 package=$(echo "$parsed_update_args" | awk '{print $2}')
 if [[ -n $parsed_update_args ]]; then
   # shellcheck disable=SC2086
-  _TARGET_LOCK_FILES=$(git grep --files-with-matches "$package" :**/Cargo.lock) \
-    _ scripts/cargo-for-all-lock-files.sh update $parsed_update_args
+  _ scripts/cargo-for-all-lock-files.sh \
+    "$(git grep --files-with-matches "$package" :**/Cargo.lock)" -- \
+    update $parsed_update_args
 fi
 
 echo --- ok
