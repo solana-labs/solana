122f3b92bfee694c6cb0d5b1519ad4be5d3cc167
Michael Vines <mvines@gmail.com>
2020-04-20 13:31:16 -0700

    Fail gracefully if older AddressSignatures rocksdb keys are present

--- ledger/src/blockstore_db.rs
+++ ledger/src/blockstore_db.rs
@@ -368,11 +368,15 @@ impl Column for columns::AddressSignatures {
     }
 
     fn index(key: &[u8]) -> (u64, Pubkey, Slot, Signature) {
-        let index = BigEndian::read_u64(&key[0..8]);
-        let pubkey = Pubkey::new(&key[8..40]);
-        let slot = BigEndian::read_u64(&key[40..48]);
-        let signature = Signature::new(&key[48..112]);
-        (index, pubkey, slot, signature)
+        if key.len() != 112 {
+            (0, Pubkey::default(), 0, Signature::default())
+        } else {
+            let index = BigEndian::read_u64(&key[0..8]);
+            let pubkey = Pubkey::new(&key[8..40]);
+            let slot = BigEndian::read_u64(&key[40..48]);
+            let signature = Signature::new(&key[48..112]);
+            (index, pubkey, slot, signature)
+        }
     }
 
     fn primary_index(index: Self::Index) -> u64 {
