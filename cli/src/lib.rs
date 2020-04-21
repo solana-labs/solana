1fbf279c5c625497d925f522452f1dce6d04c20f
Greg Fitzgerald <greg@solana.com>
2020-04-21 17:09:34 -0600

    Cleanup CLI help message (#9632)
    automerge
    
    (cherry picked from commit cbc7b3b0b727511b71884529a967d2b7060b8706)
    
    # Conflicts:
    #	cli/src/lib.rs

--- cli/src/lib.rs
+++ cli/src/lib.rs
@@ -1,3 +1,29 @@
+<<<<<<< HEAD
+=======
+macro_rules! ACCOUNT_STRING {
+    () => {
+        r#", one of:
+  * a base58-encoded public key
+  * a path to a keypair file
+  * a hyphen; signals a JSON-encoded keypair on stdin
+  * the 'ASK' keyword; to recover a keypair via its seed phrase
+  * a hardware wallet keypair URL (i.e. usb://ledger)"#
+    };
+}
+
+#[macro_use]
+macro_rules! pubkey {
+    ($arg:expr, $help:expr) => {
+        $arg.takes_value(true)
+            .validator(is_valid_pubkey)
+            .help(concat!($help, ACCOUNT_STRING!()))
+    };
+}
+
+#[macro_use]
+extern crate serde_derive;
+
+>>>>>>> cbc7b3b0b... Cleanup CLI help message (#9632)
 pub mod cli;
 pub mod cluster_query;
 pub mod display;
