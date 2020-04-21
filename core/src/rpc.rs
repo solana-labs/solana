18cba86f7706b9222c1abf08af3f0c196ed43402
Tyera Eulberg <teulberg@gmail.com>
2020-04-20 23:25:49 -0600

    Wait for supermajority of cluster to have rooted a transaction to consider it finalized (#9618)
    * Add rooted stake to BlockCommitment
    
    * Use rooted stake to include cluster check

--- core/src/rpc.rs
+++ core/src/rpc.rs
@@ -1,8 +1,11 @@
 //! The `rpc` module implements the Solana RPC interface.
 
 use crate::{
-    cluster_info::ClusterInfo, commitment::BlockCommitmentCache, contact_info::ContactInfo,
-    storage_stage::StorageState, validator::ValidatorExit,
+    cluster_info::ClusterInfo,
+    commitment::{BlockCommitmentArray, BlockCommitmentCache},
+    contact_info::ContactInfo,
+    storage_stage::StorageState,
+    validator::ValidatorExit,
 };
 use bincode::serialize;
 use jsonrpc_core::{Error, Metadata, Result};
@@ -218,7 +221,7 @@ impl JsonRpcRequestProcessor {
         }
     }
 
-    fn get_block_commitment(&self, block: Slot) -> RpcBlockCommitment<[u64; MAX_LOCKOUT_HISTORY]> {
+    fn get_block_commitment(&self, block: Slot) -> RpcBlockCommitment<BlockCommitmentArray> {
         let r_block_commitment = self.block_commitment_cache.read().unwrap();
         RpcBlockCommitment {
             commitment: r_block_commitment
@@ -486,7 +489,9 @@ impl JsonRpcRequestProcessor {
             .map(|(slot, status)| {
                 let r_block_commitment_cache = self.block_commitment_cache.read().unwrap();
 
-                let confirmations = if r_block_commitment_cache.root() >= slot {
+                let confirmations = if r_block_commitment_cache.root() >= slot
+                    && r_block_commitment_cache.is_confirmed_rooted(slot)
+                {
                     None
                 } else {
                     r_block_commitment_cache
@@ -644,7 +649,7 @@ pub trait RpcSol {
         &self,
         meta: Self::Metadata,
         block: Slot,
-    ) -> Result<RpcBlockCommitment<[u64; MAX_LOCKOUT_HISTORY]>>;
+    ) -> Result<RpcBlockCommitment<BlockCommitmentArray>>;
 
     #[rpc(meta, name = "getGenesisHash")]
     fn get_genesis_hash(&self, meta: Self::Metadata) -> Result<String>;
@@ -948,7 +953,7 @@ impl RpcSol for RpcSolImpl {
         &self,
         meta: Self::Metadata,
         block: Slot,
-    ) -> Result<RpcBlockCommitment<[u64; MAX_LOCKOUT_HISTORY]>> {
+    ) -> Result<RpcBlockCommitment<BlockCommitmentArray>> {
         Ok(meta
             .request_processor
             .read()
@@ -2070,7 +2075,7 @@ pub mod tests {
                 .expect("actual response deserialization");
         let result = result.as_ref().unwrap();
         assert_eq!(expected_res, result.status);
-        assert_eq!(None, result.confirmations);
+        assert_eq!(Some(2), result.confirmations);
 
         // Test getSignatureStatus request on unprocessed tx
         let tx = system_transaction::transfer(&alice, &bob_pubkey, 10, blockhash);
@@ -2421,8 +2426,8 @@ pub mod tests {
         let validator_exit = create_validator_exit(&exit);
         let bank_forks = new_bank_forks().0;
 
-        let commitment_slot0 = BlockCommitment::new([8; MAX_LOCKOUT_HISTORY]);
-        let commitment_slot1 = BlockCommitment::new([9; MAX_LOCKOUT_HISTORY]);
+        let commitment_slot0 = BlockCommitment::new([8; MAX_LOCKOUT_HISTORY + 1]);
+        let commitment_slot1 = BlockCommitment::new([9; MAX_LOCKOUT_HISTORY + 1]);
         let mut block_commitment: HashMap<u64, BlockCommitment> = HashMap::new();
         block_commitment
             .entry(0)
@@ -2514,7 +2519,7 @@ pub mod tests {
         let res = io.handle_request_sync(&req, meta);
         let result: Response = serde_json::from_str(&res.expect("actual response"))
             .expect("actual response deserialization");
-        let commitment_response: RpcBlockCommitment<[u64; MAX_LOCKOUT_HISTORY]> =
+        let commitment_response: RpcBlockCommitment<BlockCommitmentArray> =
             if let Response::Single(res) = result {
                 if let Output::Success(res) = res {
                     serde_json::from_value(res.result).unwrap()
