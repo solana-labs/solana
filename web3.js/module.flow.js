8142aa6c1da89772640f02fb15f577f2b40217cf
Trent Nelson <trent@solana.com>
2020-04-23 15:44:19 -0600

    feat: allow external signatures on `Transactions`
    Adds a helper for adding externally created signature/pubkey pairs
    to `Transactions`

--- web3.js/module.flow.js
+++ web3.js/module.flow.js
@@ -415,6 +415,7 @@ declare module '@solana/web3.js' {
     sign(...signers: Array<Account>): void;
     signPartial(...partialSigners: Array<PublicKey | Account>): void;
     addSigner(signer: Account): void;
+    addSignature(pubkey: PublicKey, signature: Buffer): void;
     verifySignatures(): boolean;
     serialize(): Buffer;
   }
