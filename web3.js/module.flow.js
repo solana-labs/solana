7e642d5456e94070e2120471a82c528c87b6df60
Trent Nelson <trent@solana.com>
2020-04-24 10:20:27 -0600

    fix: rename `signData` to `serializeMessage()`

--- web3.js/module.flow.js
+++ web3.js/module.flow.js
@@ -411,7 +411,7 @@ declare module '@solana/web3.js' {
         Transaction | TransactionInstruction | TransactionInstructionCtorFields,
       >
     ): Transaction;
-    signData: Buffer;
+    serializeMessage(): Buffer;
     sign(...signers: Array<Account>): void;
     signPartial(...partialSigners: Array<PublicKey | Account>): void;
     addSigner(signer: Account): void;
