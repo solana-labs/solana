23de27c9d624d93a6cc09e9e351590d75d48e93d
Stephen Akridge <sakridge@gmail.com>
2020-04-25 22:01:42 -0700

    add test

--- runtime/src/bank.rs
+++ runtime/src/bank.rs
@@ -5758,4 +5758,44 @@ mod tests {
         let result = bank.process_transaction(&tx);
         assert_eq!(result, Ok(()));
     }
+
+    #[test]
+    fn test_account_ids_after_program_ids() {
+        solana_logger::setup();
+        let (genesis_config, mint_keypair) = create_genesis_config(500);
+        let mut bank = Bank::new(&genesis_config);
+
+        let from_pubkey = Pubkey::new_rand();
+        let to_pubkey = Pubkey::new_rand();
+
+        let account_metas = vec![
+            AccountMeta::new(from_pubkey, false),
+            AccountMeta::new(to_pubkey, false),
+        ];
+
+        let instruction = Instruction::new(solana_vote_program::id(), &10, account_metas);
+        let mut tx = Transaction::new_signed_with_payer(
+            &[instruction],
+            Some(&mint_keypair.pubkey()),
+            &[&mint_keypair],
+            bank.last_blockhash(),
+        );
+
+        tx.message.account_keys.push(Pubkey::new_rand());
+
+        fn mock_vote_processor(
+            _pubkey: &Pubkey,
+            _ka: &[KeyedAccount],
+            _data: &[u8],
+        ) -> std::result::Result<(), InstructionError> {
+            Ok(())
+        }
+
+        bank.add_instruction_processor(solana_vote_program::id(), mock_vote_processor);
+        let result = bank.process_transaction(&tx);
+        assert_eq!(result, Ok(()));
+        let account = bank.get_account(&solana_vote_program::id()).unwrap();
+        info!("account: {:?}", account);
+        assert!(account.executable);
+    }
 }
