0336f02d7f1ef0dc8b2f558cf15894d39857c240
Ryo Onodera <ryoqun@gmail.com>
2020-04-28 21:58:16 +0900

    Fix shell scripts..

--- multinode-demo/validator.sh
+++ multinode-demo/validator.sh
@@ -149,6 +149,9 @@ while [[ -n $1 ]]; do
     elif [[ $1 = --halt-on-trusted-validators-accounts-hash-mismatch ]]; then
       args+=("$1")
       shift
+    elif [[ $1 = --max-genesis-archive-unpacked-size ]]; then
+      args+=("$1" "$2")
+      shift 2
     elif [[ $1 = -h ]]; then
       usage "$@"
     else
