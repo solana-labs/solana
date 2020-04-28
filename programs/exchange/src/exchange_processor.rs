a7e27c616484cff02f79706c9ca19b729b95f867
Tyera Eulberg <tyera@solana.com>
2020-04-28 13:55:26 -0600

    typo

--- programs/exchange/src/exchange_processor.rs
+++ programs/exchange/src/exchange_processor.rs
@@ -577,7 +577,7 @@ mod test {
     fn create_bank(lamports: u64) -> (Bank, Keypair) {
         let (genesis_config, mint_keypair) = create_genesis_config(lamports);
         let mut bank = Bank::new(&genesis_config);
-        bank.add_static_program("excahnge_program", id(), process_instruction);
+        bank.add_static_program("exchange_program", id(), process_instruction);
         (bank, mint_keypair)
     }
 
