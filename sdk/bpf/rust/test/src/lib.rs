2cd0fbf4d52c24c1b70d18debdd0cb63829122d2
Jack May <jack@solana.com>
2020-04-28 16:17:17 -0700

    Fix bpf unit test linkage

--- sdk/bpf/rust/test/src/lib.rs
+++ sdk/bpf/rust/test/src/lib.rs
@@ -13,6 +13,11 @@ pub fn sol_log_64_(arg1: u64, arg2: u64, arg3: u64, arg4: u64, arg5: u64) {
     std::println!("{} {} {} {} {}", arg1, arg2, arg3, arg4, arg5);
 }
 
+#[no_mangle]
+pub fn sol_invoke_signed_rust() {
+    std::println!("sol_invoke_signed_rust()");
+}
+
 #[macro_export]
 macro_rules! stubs {
     () => {
@@ -21,6 +26,7 @@ macro_rules! stubs {
             use $crate::*;
             unsafe { sol_log_("sol_log_".as_ptr(), 8) };
             sol_log_64_(1, 2, 3, 4, 5);
+            sol_invoke_signed_rust();
         }
     };
 }
