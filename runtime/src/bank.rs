56c0482130200267a894877153c30898bdcc1afc
Stephen Akridge <sakridge@gmail.com>
2020-04-27 22:17:22 -0700

    fix test compilation from add_static_program change

--- runtime/src/bank.rs
+++ runtime/src/bank.rs
@@ -5836,7 +5836,7 @@ mod tests {
             Ok(())
         }
 
-        bank.add_instruction_processor(solana_vote_program::id(), mock_vote_processor);
+        bank.add_static_program("mock_vote", solana_vote_program::id(), mock_vote_processor);
         let result = bank.process_transaction(&tx);
         assert_eq!(result, Ok(()));
         let account = bank.get_account(&solana_vote_program::id()).unwrap();
