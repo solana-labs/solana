0d3bb3235e6c28d9afcfff07e494b8474c1931eb
Justin Starry <justin@solana.com>
2020-05-09 18:28:58 +0800

    Add support for more system instructions

--- explorer/src/components/instruction/system/AllocateWithSeedDetailsCard.tsx
+++ explorer/src/components/instruction/system/AllocateWithSeedDetailsCard.tsx
@@ -10,7 +10,7 @@ import { InstructionCard } from "../InstructionCard";
 import Copyable from "components/Copyable";
 import { UnknownDetailsCard } from "../UnknownDetailsCard";
 
-export function AssignDetailsCard(props: {
+export function AllocateWithSeedDetailsCard(props: {
   ix: TransactionInstruction;
   index: number;
   result: SignatureResult;
@@ -19,20 +19,21 @@ export function AssignDetailsCard(props: {
 
   let params;
   try {
-    params = SystemInstruction.decodeAssign(ix);
+    params = SystemInstruction.decodeAllocateWithSeed(ix);
   } catch (err) {
     console.error(err);
     return <UnknownDetailsCard {...props} />;
   }
 
   const accountKey = params.accountPubkey.toBase58();
+  const baseKey = params.basePubkey.toBase58();
 
   return (
     <InstructionCard
       ix={ix}
       index={index}
       result={result}
-      title="Assign Account"
+      title="Allocate Account w/ Seed"
     >
       <tr>
         <td>Program</td>
@@ -50,6 +51,29 @@ export function AssignDetailsCard(props: {
             <code>{accountKey}</code>
           </Copyable>
         </td>
+      </tr>
+
+      <tr>
+        <td>Base Address</td>
+        <td className="text-right">
+          <Copyable text={baseKey}>
+            <code>{baseKey}</code>
+          </Copyable>
+        </td>
+      </tr>
+
+      <tr>
+        <td>Seed</td>
+        <td className="text-right">
+          <Copyable text={params.seed}>
+            <code>{params.seed}</code>
+          </Copyable>
+        </td>
+      </tr>
+
+      <tr>
+        <td>Allocated Space (Bytes)</td>
+        <td className="text-right">{params.space}</td>
       </tr>
 
       <tr>
--- explorer/src/components/instruction/system/AssignWithSeedDetailsCard.tsx
+++ explorer/src/components/instruction/system/AssignWithSeedDetailsCard.tsx
@@ -10,7 +10,7 @@ import { InstructionCard } from "../InstructionCard";
 import Copyable from "components/Copyable";
 import { UnknownDetailsCard } from "../UnknownDetailsCard";
 
-export function AssignDetailsCard(props: {
+export function AssignWithSeedDetailsCard(props: {
   ix: TransactionInstruction;
   index: number;
   result: SignatureResult;
@@ -19,20 +19,21 @@ export function AssignDetailsCard(props: {
 
   let params;
   try {
-    params = SystemInstruction.decodeAssign(ix);
+    params = SystemInstruction.decodeAssignWithSeed(ix);
   } catch (err) {
     console.error(err);
     return <UnknownDetailsCard {...props} />;
   }
 
   const accountKey = params.accountPubkey.toBase58();
+  const baseKey = params.basePubkey.toBase58();
 
   return (
     <InstructionCard
       ix={ix}
       index={index}
       result={result}
-      title="Assign Account"
+      title="Assign Account w/ Seed"
     >
       <tr>
         <td>Program</td>
@@ -48,6 +49,24 @@ export function AssignDetailsCard(props: {
         <td className="text-right">
           <Copyable text={accountKey}>
             <code>{accountKey}</code>
+          </Copyable>
+        </td>
+      </tr>
+
+      <tr>
+        <td>Base Address</td>
+        <td className="text-right">
+          <Copyable text={baseKey}>
+            <code>{baseKey}</code>
+          </Copyable>
+        </td>
+      </tr>
+
+      <tr>
+        <td>Seed</td>
+        <td className="text-right">
+          <Copyable text={params.seed}>
+            <code>{params.seed}</code>
           </Copyable>
         </td>
       </tr>
