3ff02dd405f265b24c40274b4b185190a6707a3d
Tyera Eulberg <tyera@solana.com>
2020-05-11 14:04:05 -0600

    Check slot cleaned up for rpc blockstore/slot queries

--- core/src/rpc.rs
+++ core/src/rpc.rs
@@ -19,7 +19,9 @@ use solana_client::{
     rpc_response::*,
 };
 use solana_faucet::faucet::request_airdrop_transaction;
-use solana_ledger::{bank_forks::BankForks, blockstore::Blockstore};
+use solana_ledger::{
+    bank_forks::BankForks, blockstore::Blockstore, blockstore_db::BlockstoreError,
+};
 use solana_perf::packet::PACKET_DATA_SIZE;
 use solana_runtime::{accounts::AccountAddressFilter, bank::Bank};
 use solana_sdk::{
@@ -416,6 +418,29 @@ impl JsonRpcRequestProcessor {
         }
     }
 
+    fn check_slot_cleaned_up<T>(
+        &self,
+        result: &std::result::Result<T, BlockstoreError>,
+        slot: Slot,
+    ) -> Result<()>
+    where
+        T: std::fmt::Debug,
+    {
+        if result.is_err() {
+            if let BlockstoreError::SlotCleanedUp = result.as_ref().unwrap_err() {
+                return Err(RpcCustomError::BlockCleanedUp {
+                    slot,
+                    first_available_block: self
+                        .blockstore
+                        .get_first_available_block()
+                        .unwrap_or_default(),
+                }
+                .into());
+            }
+        }
+        Ok(())
+    }
+
     pub fn get_confirmed_block(
         &self,
         slot: Slot,
@@ -429,7 +454,9 @@ impl JsonRpcRequestProcessor {
                     .unwrap()
                     .largest_confirmed_root()
         {
-            Ok(self.blockstore.get_confirmed_block(slot, encoding).ok())
+            let result = self.blockstore.get_confirmed_block(slot, encoding);
+            self.check_slot_cleaned_up(&result, slot)?;
+            Ok(result.ok())
         } else {
             Ok(None)
         }
@@ -477,11 +504,9 @@ impl JsonRpcRequestProcessor {
             let stakes = HashMap::new();
             let stakes = bank.epoch_vote_accounts(epoch).unwrap_or(&stakes);
 
-            Ok(self
-                .blockstore
-                .get_block_time(slot, slot_duration, stakes)
-                .ok()
-                .unwrap_or(None))
+            let result = self.blockstore.get_block_time(slot, slot_duration, stakes);
+            self.check_slot_cleaned_up(&result, slot)?;
+            Ok(result.ok().unwrap_or(None))
         } else {
             Ok(None)
         }
--- core/src/rpc_error.rs
+++ core/src/rpc_error.rs
@@ -2,9 +2,17 @@ use jsonrpc_core::{Error, ErrorCode};
 use solana_sdk::clock::Slot;
 
 const JSON_RPC_SERVER_ERROR_0: i64 = -32000;
+const JSON_RPC_SERVER_ERROR_1: i64 = -32001;
 
 pub enum RpcCustomError {
-    NonexistentClusterRoot { cluster_root: Slot, node_root: Slot },
+    NonexistentClusterRoot {
+        cluster_root: Slot,
+        node_root: Slot,
+    },
+    BlockCleanedUp {
+        slot: Slot,
+        first_available_block: Slot,
+    },
 }
 
 impl From<RpcCustomError> for Error {
@@ -18,6 +26,17 @@ impl From<RpcCustomError> for Error {
                 message: format!(
                     "Cluster largest_confirmed_root {} does not exist on node. Node root: {}",
                     cluster_root, node_root,
+                ),
+                data: None,
+            },
+            RpcCustomError::BlockCleanedUp {
+                slot,
+                first_available_block,
+            } => Self {
+                code: ErrorCode::ServerError(JSON_RPC_SERVER_ERROR_1),
+                message: format!(
+                    "Block {} cleaned up, does not exist on node. First available block: {}",
+                    slot, first_available_block,
                 ),
                 data: None,
             },
