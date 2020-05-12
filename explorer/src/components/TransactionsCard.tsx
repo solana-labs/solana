8f11dc0fdc2436d41a1fcafa4baa3f53e2cd673a
Justin Starry <justin@solana.com>
2020-05-12 18:10:48 +0800

    Remove source tracking for addresses and signatures

--- explorer/src/components/TransactionsCard.tsx
+++ explorer/src/components/TransactionsCard.tsx
@@ -3,7 +3,6 @@ import { Link } from "react-router-dom";
 import {
   useTransactions,
   TransactionStatus,
-  Source,
   FetchStatus,
   useFetchTransactionStatus
 } from "../providers/transactions";
@@ -33,7 +32,7 @@ function TransactionsCard() {
       return;
     }
 
-    fetchTransaction(signature, Source.Input);
+    fetchTransaction(signature);
     const inputEl = signatureInput.current;
     if (inputEl) {
       inputEl.value = "";
