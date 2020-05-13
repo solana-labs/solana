2d1b69ea128a5a79b17d4217a59590b73456b004
Ryo Onodera <ryoqun@solana.com>
2020-05-13 16:22:14 +0900

    Introduce eager rent collection (#9527)
    * Switch AccountsIndex.account_maps from HashMap to BTreeMap
    
    * Introduce eager rent collection
    
    * Start to add tests
    
    * Avoid too short eager rent collection cycles
    
    * Add more tests
    
    * Add more tests...
    
    * Refacotr!!!!!!
    
    * Refactoring follow up
    
    * More tiny cleanups
    
    * Don't rewrite 0-lamport accounts to be deterministic
    
    * Refactor a bit
    
    * Do hard fork, restore tests, and perf. mitigation
    
    * Fix build...
    
    * Refactor and add switch over for testnet (TdS)
    
    * Use to_be_bytes
    
    * cleanup
    
    * More tiny cleanup
    
    * Rebase cleanup
    
    * Set Bank::genesis_hash when resuming from snapshot
    
    * Reorder fns and clean ups
    
    * Better naming and commenting
    
    * Yet more naming clarifications
    
    * Make prefix width strictly uniform for 2-base partition_count
    
    * Fix typo...
    
    * Revert cluster-dependent gate
    
    * kick ci?
    
    * kick ci?
    
    * kick ci?
    
    (cherry picked from commit 1eb40c3fe0141f3d2d76419a0fdedeb59be787c3)
    
    # Conflicts:
    #	core/tests/bank_forks.rs
    #	ledger/src/bank_forks_utils.rs
    #	ledger/src/snapshot_utils.rs
    #	runtime/src/accounts_index.rs
    #	runtime/src/bank.rs

--- ledger/src/snapshot_utils.rs
+++ ledger/src/snapshot_utils.rs
@@ -13,7 +13,7 @@ use solana_runtime::{
         MAX_SNAPSHOT_DATA_FILE_SIZE,
     },
 };
-use solana_sdk::{clock::Slot, hash::Hash, pubkey::Pubkey};
+use solana_sdk::{clock::Slot, genesis_config::GenesisConfig, hash::Hash, pubkey::Pubkey};
 use std::{
     cmp::Ordering,
     env,
@@ -439,6 +439,11 @@ pub fn bank_from_archive<P: AsRef<Path>>(
     frozen_account_pubkeys: &[Pubkey],
     snapshot_path: &PathBuf,
     snapshot_tar: P,
+<<<<<<< HEAD
+=======
+    compression: CompressionType,
+    genesis_config: &GenesisConfig,
+>>>>>>> 1eb40c3fe... Introduce eager rent collection (#9527)
 ) -> Result<Bank> {
     // Untar the snapshot into a temp directory under `snapshot_config.snapshot_path()`
     let unpack_dir = tempfile::tempdir_in(snapshot_path)?;
@@ -458,6 +463,7 @@ pub fn bank_from_archive<P: AsRef<Path>>(
         frozen_account_pubkeys,
         &unpacked_snapshots_dir,
         unpacked_accounts_dir,
+        genesis_config,
     )?;
 
     if !bank.verify_snapshot_bank() {
@@ -584,6 +590,7 @@ fn rebuild_bank_from_snapshots<P>(
     frozen_account_pubkeys: &[Pubkey],
     unpacked_snapshots_dir: &PathBuf,
     append_vecs_path: P,
+    genesis_config: &GenesisConfig,
 ) -> Result<Bank>
 where
     P: AsRef<Path>,
@@ -612,6 +619,7 @@ where
                     )));
                 }
             };
+            bank.operating_mode = Some(genesis_config.operating_mode);
             info!("Rebuilding accounts...");
             let rc = bank::BankRc::from_stream(
                 account_paths,
