42aaacf5208188f9a28f9a4e99fac703e7b56b0a
carllin <wumu727@gmail.com>
2020-05-22 23:23:17 -0700

    Factor out LockedPubkeyReferences (#10198)
    Co-authored-by: Carl <carl@solana.com>

--- core/src/pubkey_references.rs
+++ core/src/pubkey_references.rs
@@ -1,5 +1,9 @@
 use solana_sdk::pubkey::Pubkey;
-use std::{collections::HashSet, rc::Rc};
+use std::{
+    collections::HashSet,
+    rc::Rc,
+    sync::{Arc, RwLock},
+};
 
 #[derive(Default)]
 pub struct PubkeyReferences(HashSet<Rc<Pubkey>>);
@@ -17,5 +21,24 @@ impl PubkeyReferences {
 
     pub fn purge(&mut self) {
         self.0.retain(|x| Rc::strong_count(x) > 1);
+    }
+}
+
+#[derive(Default)]
+pub struct LockedPubkeyReferences(pub RwLock<HashSet<Arc<Pubkey>>>);
+
+impl LockedPubkeyReferences {
+    pub fn get_or_insert(&self, pubkey: &Pubkey) -> Arc<Pubkey> {
+        let mut cached_pubkey = self.0.read().unwrap().get(pubkey).cloned();
+        if cached_pubkey.is_none() {
+            let new_pubkey = Arc::new(*pubkey);
+            self.0.write().unwrap().insert(new_pubkey.clone());
+            cached_pubkey = Some(new_pubkey);
+        }
+        cached_pubkey.unwrap()
+    }
+
+    pub fn purge(&self) {
+        self.0.write().unwrap().retain(|x| Arc::strong_count(x) > 1);
     }
 }
