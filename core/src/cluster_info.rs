e961d3d9ce5a44403d598c859000ab309fcbc47c
Anatoly Yakovenko <anatoly@solana.com>
2020-05-29 20:06:17 -0700

    wait to start filtering responses, and push stats to influx

--- core/src/cluster_info.rs
+++ core/src/cluster_info.rs
@@ -1500,7 +1500,7 @@ impl ClusterInfo {
                     }
                     //TODO: possibly tune this parameter
                     //we saw a deadlock passing an obj.read().unwrap().timeout into sleep
-                    if start - last_push > CRDS_GOSSIP_PULL_CRDS_TIMEOUT_MS / 2 {
+                    if start - last_push > CRDS_GOSSIP_PUSH_MSG_TIMEOUT_MS * 2 {
                         obj.push_self(&stakes);
                         last_push = timestamp();
                     }
