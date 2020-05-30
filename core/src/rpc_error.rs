e438d7f68d3e5b3c6ce4fd1d6eec2ae5d81c78c2
Michael Vines <mvines@gmail.com>
2020-05-29 23:16:35 -0700

    Add preflight checks to sendTransaction RPC method

--- core/src/rpc_error.rs
+++ core/src/rpc_error.rs
@@ -3,6 +3,7 @@ use solana_sdk::clock::Slot;
 
 const JSON_RPC_SERVER_ERROR_0: i64 = -32000;
 const JSON_RPC_SERVER_ERROR_1: i64 = -32001;
+const JSON_RPC_SERVER_ERROR_2: i64 = -32002;
 
 pub enum RpcCustomError {
     NonexistentClusterRoot {
@@ -12,6 +13,9 @@ pub enum RpcCustomError {
     BlockCleanedUp {
         slot: Slot,
         first_available_block: Slot,
+    },
+    SendTransactionPreflightFailure {
+        message: String,
     },
 }
 
@@ -38,6 +42,11 @@ impl From<RpcCustomError> for Error {
                     "Block {} cleaned up, does not exist on node. First available block: {}",
                     slot, first_available_block,
                 ),
+                data: None,
+            },
+            RpcCustomError::SendTransactionPreflightFailure { message } => Self {
+                code: ErrorCode::ServerError(JSON_RPC_SERVER_ERROR_2),
+                message,
                 data: None,
             },
         }
