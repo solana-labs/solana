5c5f9e48495afeaf236d19f3b79f33fb6d0f5717
Michael Vines <mvines@gmail.com>
2020-05-31 21:54:12 -0700

    Add send_transaction_with_config
    (cherry picked from commit f1733ab12531392ab10fa6fadf81531872cb6395)
    
    # Conflicts:
    #	client/src/rpc_client.rs

--- client/src/rpc_client.rs
+++ client/src/rpc_client.rs
@@ -1,9 +1,15 @@
 use crate::{
     client_error::{ClientError, ClientErrorKind, Result as ClientResult},
+<<<<<<< HEAD
     generic_rpc_client_request::GenericRpcClientRequest,
     mock_rpc_client_request::{MockRpcClientRequest, Mocks},
     rpc_client_request::RpcClientRequest,
     rpc_config::RpcLargestAccountsConfig,
+=======
+    http_sender::HttpSender,
+    mock_sender::{MockSender, Mocks},
+    rpc_config::{RpcLargestAccountsConfig, RpcSendTransactionConfig},
+>>>>>>> f1733ab12... Add send_transaction_with_config
     rpc_request::{RpcError, RpcRequest},
     rpc_response::*,
 };
@@ -95,10 +101,20 @@ impl RpcClient {
     }
 
     pub fn send_transaction(&self, transaction: &Transaction) -> ClientResult<Signature> {
+        self.send_transaction_with_config(transaction, RpcSendTransactionConfig::default())
+    }
+
+    pub fn send_transaction_with_config(
+        &self,
+        transaction: &Transaction,
+        config: RpcSendTransactionConfig,
+    ) -> ClientResult<Signature> {
         let serialized_encoded = bs58::encode(serialize(transaction).unwrap()).into_string();
 
-        let signature_base58_str: String =
-            self.send(RpcRequest::SendTransaction, json!([serialized_encoded]))?;
+        let signature_base58_str: String = self.send(
+            RpcRequest::SendTransaction,
+            json!([serialized_encoded, config]),
+        )?;
 
         let signature = signature_base58_str
             .parse::<Signature>()
