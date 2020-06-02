059442a41e4ce269cd9473ea90d6db21f325703a
Ryo Onodera <ryoqun@gmail.com>
2020-06-02 13:17:21 +0900

    Simplify source credit
    Co-authored-by: Michael Vines <mvines@gmail.com>

--- sdk/build.rs
+++ sdk/build.rs
@@ -4,7 +4,7 @@ use rustc_version::{version_meta, Channel};
 fn main() {
     // Copied and adapted from
     // https://github.com/Kimundi/rustc-version-rs/blob/1d692a965f4e48a8cb72e82cda953107c0d22f47/README.md#example
-    // Licensed under Apache-2.0 + MIT (compatible because we're Apache-2.0)
+    // Licensed under Apache-2.0 + MIT
     match version_meta().unwrap().channel {
         Channel::Stable => {
             println!("cargo:rustc-cfg=RUSTC_WITHOUT_SPECIALIZATION");
