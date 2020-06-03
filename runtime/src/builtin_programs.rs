9c631a01c13fa6e765d8c401c0a1f21eee351d1c
Michael Vines <mvines@gmail.com>
2020-06-02 17:34:12 -0700

    Enable rolling upgrade of system_instruction_processor

--- runtime/src/builtin_programs.rs
+++ runtime/src/builtin_programs.rs
@@ -1,5 +1,8 @@
-use crate::system_instruction_processor;
-use solana_sdk::{entrypoint_native::ProcessInstruction, pubkey::Pubkey, system_program};
+use crate::{legacy_system_instruction_processor0, system_instruction_processor};
+use solana_sdk::{
+    clock::Epoch, entrypoint_native::ProcessInstruction, genesis_config::OperatingMode,
+    pubkey::Pubkey, system_program,
+};
 
 pub struct BuiltinProgram {
     pub name: String,
@@ -16,13 +19,30 @@ impl BuiltinProgram {
     }
 }
 
-pub fn get_builtin_programs() -> Vec<BuiltinProgram> {
+fn new_system_program_activation_epoch(operating_mode: OperatingMode) -> Epoch {
+    match operating_mode {
+        OperatingMode::Development => 0,
+        OperatingMode::Preview => std::u64::MAX / 2,
+        OperatingMode::Stable => std::u64::MAX / 2,
+    }
+}
+
+/// All builtin programs that should be active at the given (operating_mode, epoch)
+pub fn get_builtin_programs(operating_mode: OperatingMode, epoch: Epoch) -> Vec<BuiltinProgram> {
     vec![
-        BuiltinProgram::new(
-            "system_program",
-            system_program::id(),
-            system_instruction_processor::process_instruction,
-        ),
+        if epoch < new_system_program_activation_epoch(operating_mode) {
+            BuiltinProgram::new(
+                "system_program",
+                system_program::id(),
+                legacy_system_instruction_processor0::process_instruction,
+            )
+        } else {
+            BuiltinProgram::new(
+                "system_program",
+                system_program::id(),
+                system_instruction_processor::process_instruction,
+            )
+        },
         BuiltinProgram::new(
             "config_program",
             solana_config_program::id(),
@@ -39,4 +59,20 @@ pub fn get_builtin_programs() -> Vec<BuiltinProgram> {
             solana_vote_program::vote_instruction::process_instruction,
         ),
     ]
+}
+
+/// Builtin programs that activate at the given (operating_mode, epoch)
+pub fn get_epoch_activated_builtin_programs(
+    operating_mode: OperatingMode,
+    epoch: Epoch,
+) -> Option<Vec<BuiltinProgram>> {
+    if epoch == new_system_program_activation_epoch(operating_mode) {
+        Some(vec![BuiltinProgram::new(
+            "system_program",
+            system_program::id(),
+            system_instruction_processor::process_instruction,
+        )])
+    } else {
+        None
+    }
 }
