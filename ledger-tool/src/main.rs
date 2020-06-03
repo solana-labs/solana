e31b2c023e2a612b4a2a360892b040a76e027485
Ryo Onodera <ryoqun@gmail.com>
2020-06-03 20:48:19 +0900

    Really fix conflict...

--- ledger-tool/src/main.rs
+++ ledger-tool/src/main.rs
@@ -545,7 +545,6 @@ fn load_bank_forks(
             snapshot_interval_slots: 0, // Value doesn't matter
             snapshot_package_output_path: ledger_path.clone(),
             snapshot_path,
-            compression: CompressionType::Bzip2,
         })
     };
     let account_paths = if let Some(account_paths) = arg_matches.value_of("account_paths") {
