775340c6f3ab1c3fe85139f9da4f5afab1d84616
Trent Nelson <trent@solana.com>
2020-06-03 18:50:57 -0600

    net.sh: Support multiple validators with active stake from the start

--- multinode-demo/bootstrap-validator.sh
+++ multinode-demo/bootstrap-validator.sh
@@ -63,6 +63,9 @@ while [[ -n $1 ]]; do
     elif [[ $1 == --wait-for-supermajority ]]; then
       args+=("$1" "$2")
       shift 2
+    elif [[ $1 == --expected-bank-hash ]]; then
+      args+=("$1" "$2")
+      shift 2
     else
       echo "Unknown argument: $1"
       $program --help
