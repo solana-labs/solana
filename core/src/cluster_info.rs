8734289759f860ade7b95bad36a40adea3d5371d
sakridge <sakridge@gmail.com>
2020-06-05 09:36:31 -0700

    Add pull request count metrics (#10421)
    (cherry picked from commit 3d2230f1a94c45ae55c2c3c7474d4a75ff039d59)

--- core/src/cluster_info.rs
+++ core/src/cluster_info.rs
@@ -231,6 +231,7 @@ struct GossipStats {
     epoch_slots_push: Counter,
     push_message: Counter,
     new_pull_requests: Counter,
+    new_pull_requests_count: Counter,
     mark_pull_request: Counter,
     skip_pull_response_shred_version: Counter,
     skip_pull_shred_version: Counter,
@@ -1401,6 +1402,9 @@ impl ClusterInfo {
                 .collect()
         };
         self.append_entrypoint_to_pulls(&mut pulls);
+        self.stats
+            .new_pull_requests_count
+            .add_relaxed(pulls.len() as u64);
         pulls
             .into_iter()
             .map(|(peer, filter, gossip, self_info)| {
@@ -2141,7 +2145,7 @@ impl ClusterInfo {
                 ),
             );
             datapoint_info!(
-                "cluster_info_shred_version_skip",
+                "cluster_info_stats4",
                 (
                     "skip_push_message_shred_version",
                     self.stats.skip_push_message_shred_version.clear(),
@@ -2165,6 +2169,11 @@ impl ClusterInfo {
                 (
                     "push_message_value_count",
                     self.stats.push_message_value_count.clear(),
+                    i64
+                ),
+                (
+                    "new_pull_requests_count",
+                    self.stats.new_pull_requests_count.clear(),
                     i64
                 ),
             );
