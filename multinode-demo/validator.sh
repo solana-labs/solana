103e6403128cd6e2ebf688cac3a26494445466f3
Trent Nelson <trent@solana.com>
2020-06-12 15:29:56 -0600

    Plumb --wait-for-supermajority through scripts

--- multinode-demo/validator.sh
+++ multinode-demo/validator.sh
@@ -148,6 +148,9 @@ while [[ -n $1 ]]; do
     elif [[ $1 = --max-genesis-archive-unpacked-size ]]; then
       args+=("$1" "$2")
       shift 2
+    elif [[ $1 == --wait-for-supermajority ]]; then
+      args+=("$1" "$2")
+      shift 2
     elif [[ $1 = -h ]]; then
       usage "$@"
     else
