f1131dfbbb859750cc2b718f79577079c048b411
Ryo Onodera <ryoqun@gmail.com>
2020-06-14 18:15:46 +0900

    Check the whole range of commits in the topic branch (#10560)
    * Check the whole range of commits in the topic branch
    
    * Fix shellcheck
    
    * Debug...
    
    * Ensure to update to the latest of the base branch...

--- ci/test-sanity.sh
+++ ci/test-sanity.sh
@@ -5,8 +5,20 @@ cd "$(dirname "$0")/.."
 
 source ci/_
 
-# Look for failed mergify.io backports
-_ git show HEAD --check --oneline
+echo --- prepare git show --check
+
+(
+  set -x
+  # Look for failed mergify.io backports by searching leftover conflict markers
+  # Also check for any trailing whitespaces!
+  if [[ -n $BUILDKITE_PULL_REQUEST_BASE_BRANCH ]]; then
+    base_branch=$BUILDKITE_PULL_REQUEST_BASE_BRANCH
+  else
+    base_branch=$BUILDKITE_BRANCH
+  fi
+  _ git fetch origin "$base_branch"
+  _ git show "$(git merge-base HEAD "origin/$base_branch")..HEAD" --check --oneline
+)
 
 _ ci/nits.sh
 _ ci/check-ssh-keys.sh
