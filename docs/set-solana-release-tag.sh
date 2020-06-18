8287ce745d918c24cf9c2d0646f5db53089f2dae
Michael Vines <mvines@gmail.com>
2020-06-18 16:41:14 -0700

    Don't bother `api.github.com` on pull requests to avoid getting rate limited

--- docs/set-solana-release-tag.sh
+++ docs/set-solana-release-tag.sh
@@ -5,12 +5,16 @@ cd "$(dirname "$0")"
 
 if [[ -n $CI_TAG ]]; then
   LATEST_SOLANA_RELEASE_VERSION=$CI_TAG
-else
+elif [[ -z $CI_PULL_REQUEST ]]; then
   LATEST_SOLANA_RELEASE_VERSION=$(\
     curl -sSfL https://api.github.com/repos/solana-labs/solana/releases/latest \
     | grep -m 1 tag_name \
     | sed -ne 's/^ *"tag_name": "\([^"]*\)",$/\1/p' \
   )
+else
+  # Don't bother the `api.github.com` on pull requests to avoid getting rate
+  # limited
+  LATEST_SOLANA_RELEASE_VERSION=unknown-version
 fi
 
 if [[ -z "$LATEST_SOLANA_RELEASE_VERSION" ]]; then
