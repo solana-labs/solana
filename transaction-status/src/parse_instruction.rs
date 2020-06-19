e8e37c5a7497b9460e34a6ea3af9577571666d6f
Tyera Eulberg <tyera@solana.com>
2020-06-19 11:51:10 -0600

    Add parse test

--- transaction-status/src/parse_instruction.rs
+++ transaction-status/src/parse_instruction.rs
@@ -33,3 +33,27 @@ pub fn parse(program_id: &Pubkey, instruction: &CompiledInstruction) -> Option<V
 fn parse_memo(instruction: &CompiledInstruction) -> Value {
     Value::String(from_utf8(&instruction.data).unwrap().to_string())
 }
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_parse() {
+        let memo_instruction = CompiledInstruction {
+            program_id_index: 0,
+            accounts: vec![],
+            data: vec![240, 159, 166, 150],
+        };
+        let expected_json = json!({
+            "spl-memo": "🦖"
+        });
+        assert_eq!(
+            parse(&MEMO_PROGRAM_ID, &memo_instruction),
+            Some(expected_json)
+        );
+
+        let non_parsable_program_id = Pubkey::new(&[1; 32]);
+        assert_eq!(parse(&non_parsable_program_id, &memo_instruction), None);
+    }
+}
