afd9550ad7bd7e1912311c2b9101ccf8c01f1c1a
Ryo Onodera <ryoqun@gmail.com>
2020-06-25 12:48:28 +0900

    Fix race in ci/run-sanity.sh (#10796)
    (cherry picked from commit 4dc9f378b884f3716b5c7cf1eb577c425befc479)

--- ci/run-sanity.sh
+++ ci/run-sanity.sh
@@ -19,13 +19,16 @@ while [[ ! -f config/run/init-completed ]]; do
   fi
 done
 
-while [[ $($solana_cli --url http://localhost:8899 slot --commitment recent) -eq 0 ]]; do
+snapshot_slot=1
+
+# wait a bit longer than snapshot_slot
+while [[ $($solana_cli --url http://localhost:8899 slot --commitment recent) -le $((snapshot_slot + 1)) ]]; do
   sleep 1
 done
 curl -X POST -H 'Content-Type: application/json' -d '{"jsonrpc":"2.0","id":1, "method":"validatorExit"}' http://localhost:8899
 
 wait $pid
 
-$solana_ledger_tool create-snapshot --ledger config/ledger 1 config/snapshot-ledger
+$solana_ledger_tool create-snapshot --ledger config/ledger "$snapshot_slot" config/snapshot-ledger
 cp config/ledger/genesis.tar.bz2 config/snapshot-ledger
 $solana_ledger_tool verify --ledger config/snapshot-ledger
