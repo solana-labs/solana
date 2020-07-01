c988a72cca1091c6884d35259914cfedb4e2c3ce
Tyera Eulberg <tyera@solana.com>
2020-06-30 18:37:45 -0600

    s/Rpc/Ui

--- account-decoder/src/parse_nonce.rs
+++ account-decoder/src/parse_nonce.rs
@@ -5,13 +5,13 @@ use solana_sdk::{
     nonce::{state::Versions, State},
 };
 
-pub fn parse_nonce(data: &[u8]) -> Result<RpcNonceState, ParseAccountError> {
+pub fn parse_nonce(data: &[u8]) -> Result<UiNonceState, ParseAccountError> {
     let nonce_state: Versions = bincode::deserialize(data)
         .map_err(|_| ParseAccountError::from(InstructionError::InvalidAccountData))?;
     let nonce_state = nonce_state.convert_to_current();
     match nonce_state {
-        State::Uninitialized => Ok(RpcNonceState::Uninitialized),
-        State::Initialized(data) => Ok(RpcNonceState::Initialized(RpcNonceData {
+        State::Uninitialized => Ok(UiNonceState::Uninitialized),
+        State::Initialized(data) => Ok(UiNonceState::Initialized(UiNonceData {
             authority: data.authority.to_string(),
             blockhash: data.blockhash.to_string(),
             fee_calculator: data.fee_calculator,
@@ -21,14 +21,14 @@ pub fn parse_nonce(data: &[u8]) -> Result<RpcNonceState, ParseAccountError> {
 
 #[derive(Debug, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
-pub enum RpcNonceState {
+pub enum UiNonceState {
     Uninitialized,
-    Initialized(RpcNonceData),
+    Initialized(UiNonceData),
 }
 
 #[derive(Debug, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
-pub struct RpcNonceData {
+pub struct UiNonceData {
     pub authority: String,
     pub blockhash: String,
     pub fee_calculator: FeeCalculator,
