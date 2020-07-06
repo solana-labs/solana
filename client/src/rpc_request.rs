01987f8f8973f2155c6f9146ecf9e18cdba0c1e1
Michael Vines <mvines@gmail.com>
2020-07-06 08:37:04 -0700

    getConfirmedBlocks now has an upper limit on slot range
    (cherry picked from commit aef6bf272ef830117c46ed8b0fe96d44ac2a1c6a)
    
    # Conflicts:
    #	core/src/rpc.rs

--- client/src/rpc_request.rs
+++ client/src/rpc_request.rs
@@ -102,6 +102,7 @@ impl fmt::Display for RpcRequest {
 pub const NUM_LARGEST_ACCOUNTS: usize = 20;
 pub const MAX_GET_SIGNATURE_STATUSES_QUERY_ITEMS: usize = 256;
 pub const MAX_GET_CONFIRMED_SIGNATURES_FOR_ADDRESS_SLOT_RANGE: u64 = 10_000;
+pub const MAX_GET_CONFIRMED_BLOCKS_RANGE: u64 = 500_000;
 
 impl RpcRequest {
     pub(crate) fn build_request_json(self, id: u64, params: Value) -> Value {
