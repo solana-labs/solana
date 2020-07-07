5e7d7a2bfac6c8268eb9806c96d85b5ef5117158
Michael Vines <mvines@gmail.com>
2020-07-07 15:02:39 -0700

    Add UiTransactionEncoding alias

--- transaction-status/src/lib.rs
+++ transaction-status/src/lib.rs
@@ -133,6 +133,7 @@ pub enum TransactionEncoding {
     Binary,
     Json,
 }
+pub type UiTransactionEncoding = TransactionEncoding;
 
 #[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase", untagged)]
