a26709a908822eb436111b88c3587014d5e39cce
publish-docs.sh <maintainers@solana.com>
2020-07-08 10:35:30 -0600

    Setup deployment infrastructure

---
+++ docs/build.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+set -ex
+
+cd "$(dirname "$0")"
+PATH=/usr/bin:"$PATH"
+
+# md check
+find src -name '*.md' -a \! -name SUMMARY.md |
+ while read -r file; do
+   if ! grep -q '('"${file#src/}"')' src/SUMMARY.md; then
+       echo "Error: $file missing from SUMMARY.md"
+       exit 1
+   fi
+ done
+
+# auto-generate src/cli/usage.md
+./build-cli-usage.sh
+./set-solana-release-tag.sh
+
+# Build from /src into /build
+npm install
+npm run build
+
+# Deploy the /build content using vercel
+npm i -g vercel
+if [[ -d .vercel ]]; then
+  rm -r .vercel
+fi
+
+PROD=
+if [[ -n $CI ]]; then
+  if [[ -n $TOKEN ]]; then
+    TOKEN_OPT="--token $TOKEN"
+  else
+    echo "TOKEN is undefined.  Needed for Vercel authentication."
+    exit 1
+  fi
+
+  # Only push to production domains for non-PR jobs, otherwise just staging
+  if [[ -z $CI_PULL_REQUEST ]]; then
+    PROD="--prod"
+  fi
+fi
+
+./set-vercel-project-name.sh
+
+vercel deploy . --local-config=vercel.json --confirm "$TOKEN_OPT" "$PROD"
