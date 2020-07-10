272cbbce4d5fa44a5fcecb65e0de472330730a8a
publish-docs.sh <maintainers@solana.com>
2020-07-10 16:25:59 -0600

    shellcheck

--- docs/convert-ascii-to-svg.sh
+++ docs/convert-ascii-to-svg.sh
@@ -11,11 +11,11 @@ output_dir=static/img
 mkdir -p "$output_dir"
 
 while read -r bob_file; do
-  svg_file=$(basename ${bob_file%.*}.svg)
+  svg_file=$(basename "${bob_file%.*}".svg)
   svgbob "$bob_file" --output "$output_dir/$svg_file"
 done < <(find art/*.bob)
 
 while read -r msc_file; do
-  svg_file=$(basename ${msc_file%.*}.svg)
+  svg_file=$(basename "${msc_file%.*}".svg)
   mscgen -T svg -o "$output_dir/$svg_file" -i "$msc_file"
 done < <(find art/*.msc)
