271abd544c622bcbab28239ecc6dfef2110c201e
Ryo Onodera <ryoqun@gmail.com>
2020-07-15 16:28:11 +0900

    Use match and proper type aliases

--- genesis-programs/src/lib.rs
+++ genesis-programs/src/lib.rs
@@ -17,41 +17,29 @@ use solana_runtime::bank::{Bank, EnteredEpochCallback};
 
 pub fn get_inflation(operating_mode: OperatingMode, epoch: Epoch) -> Option<Inflation> {
     match operating_mode {
-        OperatingMode::Development => {
-            if epoch == 0 {
-                Some(Inflation::default())
-            } else {
-                None
-            }
-        }
-        OperatingMode::Preview => {
-            if epoch == 0 {
-                // No inflation at epoch 0
-                Some(Inflation::new_disabled())
-            } else if epoch == 44 {
-                // testnet enabled inflation at epoch 44:
-                // https://github.com/solana-labs/solana/blob/d8e885f4259e6c7db420cce513cb34ebf961073d
-                Some(Inflation::default())
-            } else if epoch == 68 {
-                // Completely disable inflation prior to ship the inflation fix at epoch 68
-                Some(Inflation::new_disabled())
-            } else {
-                None
-            }
-        }
-        OperatingMode::Stable => {
-            if epoch == 0 {
-                // No inflation at epoch 0
-                Some(Inflation::new_disabled())
-            } else if epoch == std::u64::MAX {
-                // Inflation starts
-                // The epoch of std::u64::MAX is a placeholder and is expected to be reduced in
-                // a future hard fork.
-                Some(Inflation::default())
-            } else {
-                None
-            }
-        }
+        OperatingMode::Development => match epoch {
+            0 => Some(Inflation::default()),
+            _ => None,
+        },
+        OperatingMode::Preview => match epoch {
+            // No inflation at epoch 0
+            0 => Some(Inflation::new_disabled()),
+            // testnet enabled inflation at epoch 44:
+            // https://github.com/solana-labs/solana/blob/d8e885f4259e6c7db420cce513cb34ebf961073d
+            44 => Some(Inflation::default()),
+            // Completely disable inflation prior to ship the inflation fix at epoch 68
+            68 => Some(Inflation::new_disabled()),
+            _ => None,
+        },
+        OperatingMode::Stable => match epoch {
+            // No inflation at epoch 0
+            0 => Some(Inflation::new_disabled()),
+            // Inflation starts
+            // The epoch of Epoch::MAX is a placeholder and is expected to be reduced in
+            // a future hard fork.
+            Epoch::MAX => Some(Inflation::default()),
+            _ => None,
+        },
     }
 }
 
