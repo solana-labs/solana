a53eeed7f5d2148fc7dbfdde41030c0b14cab533
Greg Fitzgerald <greg@solana.com>
2020-07-16 14:05:03 -0600

    Update debug messages

--- core/src/rpc.rs
+++ core/src/rpc.rs
@@ -121,9 +121,11 @@ impl JsonRpcRequestProcessor {
         match commitment_level {
             CommitmentLevel::Recent => {
                 if let Some(descendant_slot) = r_bank_forks.highest_descendant(slot) {
+                    debug!("RPC using highest descendant of the last-voted-on slot: {:?}, {:?}", descendant_slot, slot);
                     slot = descendant_slot;
+                } else {
+                    debug!("RPC using the last-voted-on slot: {:?}", slot);
                 }
-                debug!("RPC using working_bank: {:?}", slot);
             }
             CommitmentLevel::Root => {
                 debug!("RPC using node root: {:?}", slot);
