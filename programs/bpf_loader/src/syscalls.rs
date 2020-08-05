4210b1e1398eb75a5003de8142aa9f0ac84f937e
Jack May <jack@solana.com>
2020-08-05 10:09:51 -0700

    Gate create_program_address syscall on CPI support

--- programs/bpf_loader/src/syscalls.rs
+++ programs/bpf_loader/src/syscalls.rs
@@ -90,7 +90,9 @@ pub fn register_syscalls<'a>(
             logger: invoke_context.get_logger(),
         }),
     )?;
-    vm.register_syscall_ex("sol_create_program_address", syscall_create_program_address)?;
+    if invoke_context.is_cross_program_supported() {
+        vm.register_syscall_ex("sol_create_program_address", syscall_create_program_address)?;
+    }
 
     // Cross-program invocation syscalls
 
