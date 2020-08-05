6ec5949d77c6e358d6e818c50494dcd3d0aaa2ce
Tyera Eulberg <tyera@solana.com>
2020-08-04 19:26:32 -0600

    Rework parsed account format

--- account-decoder/src/parse_token.rs
+++ account-decoder/src/parse_token.rs
@@ -67,7 +67,7 @@ pub fn parse_token(data: &[u8]) -> Result<TokenAccountType, ParseAccountError> {
 }
 
 #[derive(Debug, Serialize, Deserialize, PartialEq)]
-#[serde(rename_all = "camelCase")]
+#[serde(rename_all = "camelCase", tag = "accountType", content = "info")]
 pub enum TokenAccountType {
     Account(UiTokenAccount),
     Mint(UiMint),
