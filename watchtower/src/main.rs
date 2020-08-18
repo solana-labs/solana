ca6d4ad6cd9f88425b325a936221c167ebc989c6
Michael Vines <mvines@gmail.com>
2020-08-18 08:40:23 -0700

    Revert "Revert "rpc: rework binary encoding.  BREAKING CHANGE (bp #11646) (#11673)""
    This reverts commit fb90fb3feb059fcc4a8409dfd6084b95a0927d11.

--- watchtower/src/main.rs
+++ watchtower/src/main.rs
@@ -229,7 +229,7 @@ fn load_blocks(
     let mut blocks = vec![];
     for slot in slots.into_iter() {
         let block =
-            rpc_client.get_confirmed_block_with_encoding(slot, UiTransactionEncoding::Binary)?;
+            rpc_client.get_confirmed_block_with_encoding(slot, UiTransactionEncoding::Base64)?;
         blocks.push((slot, block));
     }
     Ok(blocks)
