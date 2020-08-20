5eae5f7e2b03894f37c55dfacdd5446c36693dc5
Ryo Onodera <ryoqun@gmail.com>
2020-08-21 00:34:16 +0900

    Clippy

--- genesis-programs/src/lib.rs
+++ genesis-programs/src/lib.rs
@@ -64,6 +64,7 @@ fn get_programs(operating_mode: OperatingMode, epoch: Epoch) -> Vec<Program> {
                 Program::Native(solana_exchange_program!()),
             ]);
 
+            #[allow(clippy::absurd_extreme_comparisons)]
             if epoch >= std::u64::MAX {
                 // The epoch of std::u64::MAX is a placeholder and is expected
                 // to be reduced in a future network update.
@@ -71,6 +72,7 @@ fn get_programs(operating_mode: OperatingMode, epoch: Epoch) -> Vec<Program> {
             }
         }
         OperatingMode::Preview => {
+            #[allow(clippy::absurd_extreme_comparisons)]
             if epoch >= std::u64::MAX {
                 // The epoch of std::u64::MAX is a placeholder and is expected
                 // to be reduced in a future network update.
@@ -82,7 +84,7 @@ fn get_programs(operating_mode: OperatingMode, epoch: Epoch) -> Vec<Program> {
         }
         OperatingMode::Stable => {
             // at which epoch, bpf_loader_program is enabled??
-
+            #[allow(clippy::absurd_extreme_comparisons)]
             if epoch >= std::u64::MAX {
                 // The epoch of std::u64::MAX is a placeholder and is expected
                 // to be reduced in a future network update.
