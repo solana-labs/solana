f5b89c28b261a29bf3296bd96b32b5245e8e6ce6
Justin Starry <justin@solana.com>
2020-08-20 16:01:35 +0800

    Revert "Hotfix for enabling CPI (#11728)"
    This reverts commit a0190b410588714c511485c9ef785eaa6c06cfc9.

--- runtime/src/bank.rs
+++ runtime/src/bank.rs
@@ -472,11 +472,6 @@ impl Bank {
             }
         }
 
-        // HOTFIX
-        if new.epoch() >= 63 {
-            new.set_cross_program_support(true)
-        }
-
         new.update_epoch_stakes(leader_schedule_epoch);
         new.ancestors.insert(new.slot(), 0);
         new.parents().iter().enumerate().for_each(|(i, p)| {
