c61808a9ce735d034988d4d2c545868e43dc27fd
Justin Starry <justin@solana.com>
2020-09-08 16:27:58 +0800

    Update rpc.rs

--- core/src/rpc.rs
+++ core/src/rpc.rs
@@ -2150,14 +2150,10 @@ impl RpcSol for RpcSolImpl {
                 .into());
             }
 
-<<<<<<< HEAD
-            let preflight_bank = &*meta.bank(config.preflight_commitment)?;
-=======
             let preflight_commitment = config
                 .preflight_commitment
                 .map(|commitment| CommitmentConfig { commitment });
-            let preflight_bank = &*meta.bank(preflight_commitment);
->>>>>>> 9940870c8... Fix RPC transaction method configs serialization (#12100)
+            let preflight_bank = &*meta.bank(preflight_commitment)?;
             if let (Err(err), _log_output) =
                 run_transaction_simulation(&preflight_bank, transaction.clone())
             {
