d00453f7472c5c8f534a94cd2af25488010c15fd
Jack May <jack@solana.com>
2020-09-25 11:08:10 -0700

    Drain the entire compute budget (#12478)

--- programs/bpf_loader/src/syscalls.rs
+++ programs/bpf_loader/src/syscalls.rs
@@ -1269,7 +1269,7 @@ mod tests {
         let addr = string.as_ptr() as *const _ as u64;
 
         let compute_meter: Rc<RefCell<dyn ComputeMeter>> =
-            Rc::new(RefCell::new(MockComputeMeter { remaining: 4 }));
+            Rc::new(RefCell::new(MockComputeMeter { remaining: 3 }));
         let log = Rc::new(RefCell::new(vec![]));
         let logger: Rc<RefCell<dyn Logger>> =
             Rc::new(RefCell::new(MockLogger { log: log.clone() }));
