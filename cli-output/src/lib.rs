ccb48f5ce9dd813d4e8ebfad62b788cd4c1be214
Trent Nelson <trent@solana.com>
2020-09-28 14:18:31 -0600

    cli-output: Add a path to handling `--verbose` and `--quiet` display

--- cli-output/src/lib.rs
+++ cli-output/src/lib.rs
@@ -1,3 +1,15 @@
 mod cli_output;
 pub mod display;
 pub use cli_output::*;
+
+pub trait QuietDisplay: std::fmt::Display {
+    fn write_str(&self, w: &mut dyn std::fmt::Write) -> std::fmt::Result {
+        write!(w, "{}", self)
+    }
+}
+
+pub trait VerboseDisplay: std::fmt::Display {
+    fn write_str(&self, w: &mut dyn std::fmt::Write) -> std::fmt::Result {
+        write!(w, "{}", self)
+    }
+}
