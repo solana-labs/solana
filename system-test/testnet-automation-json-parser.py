1698769290608534c1e612a8874870ac67a89979
Trent Nelson <trent@solana.com>
2020-09-29 22:30:48 -0600

    Modernize python scripts
    (cherry picked from commit fce3c70b72e9624c2d9fa7ab9d8e89ed4d8b9d89)

--- system-test/testnet-automation-json-parser.py
+++ system-test/testnet-automation-json-parser.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 import sys, json
 
 data=json.load(sys.stdin)
@@ -6,8 +6,8 @@ data=json.load(sys.stdin)
 if 'results' in data:
    for result in data['results']:
       if 'series' in result:
-         print result['series'][0]['columns'][1].encode() + ': ' + str(result['series'][0]['values'][0][1])
+         print(result['series'][0]['columns'][1].encode() + ': ' + str(result['series'][0]['values'][0][1]))
       else:
-         print "An expected result from CURL request is missing"
+         print("An expected result from CURL request is missing")
 else:
-   print "No results returned from CURL request"
+   print("No results returned from CURL request")
