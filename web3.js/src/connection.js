337da184f34855bf362d2409157dfff7fde46890
Ryo Onodera <ryoqun@gmail.com>
2020-10-02 13:49:27 +0900

    doc: Add a comment to clarify port selection

--- web3.js/src/connection.js
+++ web3.js/src/connection.js
@@ -1462,6 +1462,12 @@ export class Connection {
 
     url.protocol = url.protocol === 'https:' ? 'wss:' : 'ws:';
     url.host = '';
+    // Only shift the port by +1 as a convention for ws(s) only if given endpoint
+    // is explictly specifying the endpoint port (HTTP-based RPC), assuming
+    // we're directly trying to connect to solana-validator's ws listening port.
+    // When the endpoint omits the port, we're connecting to the protocol
+    // default ports: http(80) or https(443) and it's assumed we're behind a reverse
+    // proxy which manages WebSocket upgrade and backend port redirection.
     if (url.port !== null) {
       url.port = String(Number(url.port) + 1);
     }
