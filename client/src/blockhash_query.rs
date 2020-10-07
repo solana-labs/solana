7e15dedf196bad80752d59bfb18d357703346c3d
Trent Nelson <trent@solana.com>
2020-10-07 13:18:25 -0600

    clap-utils: Allow fine-tuning offline args

--- client/src/blockhash_query.rs
+++ client/src/blockhash_query.rs
@@ -179,7 +179,7 @@ mod tests {
     fn test_blockhash_query_new_from_matches_ok() {
         let test_commands = App::new("blockhash_query_test")
             .nonce_args(false)
-            .offline_args(false);
+            .offline_args();
         let blockhash = hash(&[1u8]);
         let blockhash_string = blockhash.to_string();
 
