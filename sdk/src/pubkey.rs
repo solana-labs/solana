5ed50c2c5d7687a74f64c9d32813b8d599c322ad
Michael Vines <mvines@gmail.com>
2020-10-09 22:39:17 -0700

    Update pubkey.rs

--- sdk/src/pubkey.rs
+++ sdk/src/pubkey.rs
@@ -179,7 +179,6 @@ impl Pubkey {
     /// Find a valid program address and its corresponding nonce which must be passed
     /// as an additional seed when calling `create_program_address`
     // #[cfg(not(feature = "program"))]
-    #[allow(clippy::same_item_push)]
     pub fn find_program_address(seeds: &[&[u8]], program_id: &Pubkey) -> (Pubkey, u8) {
         let mut nonce = [255];
         for _ in 0..std::u8::MAX {
