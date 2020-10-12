b8a2f03c2f5a93778f972fb9b90f85015d3d5906
Jack May <jack@solana.com>
2020-10-12 11:59:18 -0700

    Feedback

--- runtime/src/message_processor.rs
+++ runtime/src/message_processor.rs
@@ -454,9 +454,9 @@ impl MessageProcessor {
         instruction_data: &[u8],
         invoke_context: &mut dyn InvokeContext,
     ) -> Result<(), InstructionError> {
-        if !keyed_accounts.is_empty() {
-            if native_loader::check_id(&keyed_accounts[0].owner()?) {
-                let root_id = keyed_accounts[0].unsigned_key();
+        if let Some(root_account) = keyed_accounts.iter().next() {
+            if native_loader::check_id(&root_account.owner()?) {
+                let root_id = root_account.unsigned_key();
                 for (id, process_instruction) in &self.loaders {
                     if id == root_id {
                         // Call the program via a builtin loader
@@ -486,7 +486,7 @@ impl MessageProcessor {
                     invoke_context,
                 );
             } else {
-                let owner_id = &keyed_accounts[0].owner()?;
+                let owner_id = &root_account.owner()?;
                 for (id, process_instruction) in &self.loaders {
                     if id == owner_id {
                         // Call the program via a builtin loader
@@ -512,26 +512,29 @@ impl MessageProcessor {
         accounts: &[Rc<RefCell<Account>>],
         invoke_context: &mut dyn InvokeContext,
     ) -> Result<(), InstructionError> {
-        let instruction = &message.instructions[0];
+        if let Some(instruction) = message.instructions.iter().next() {
+            // Verify the calling program hasn't misbehaved
+            invoke_context.verify_and_update(message, instruction, accounts)?;
 
-        // Verify the calling program hasn't misbehaved
-        invoke_context.verify_and_update(message, instruction, accounts)?;
+            // Construct keyed accounts
+            let keyed_accounts =
+                Self::create_keyed_accounts(message, instruction, executable_accounts, accounts)?;
 
-        // Construct keyed accounts
-        let keyed_accounts =
-            Self::create_keyed_accounts(message, instruction, executable_accounts, accounts)?;
+            // Invoke callee
+            invoke_context.push(instruction.program_id(&message.account_keys))?;
+            let mut result =
+                self.process_instruction(&keyed_accounts, &instruction.data, invoke_context);
+            if result.is_ok() {
+                // Verify the called program has not misbehaved
+                result = invoke_context.verify_and_update(message, instruction, accounts);
+            }
+            invoke_context.pop();
 
-        // Invoke callee
-        invoke_context.push(instruction.program_id(&message.account_keys))?;
-        let mut result =
-            self.process_instruction(&keyed_accounts, &instruction.data, invoke_context);
-        if result.is_ok() {
-            // Verify the called program has not misbehaved
-            result = invoke_context.verify_and_update(message, instruction, accounts);
+            result
+        } else {
+            // This function is always called with a valid instruction, if that changes return an error
+            Err(InstructionError::GenericError)
         }
-        invoke_context.pop();
-
-        result
     }
 
     /// Record the initial state of the accounts so that they can be compared
