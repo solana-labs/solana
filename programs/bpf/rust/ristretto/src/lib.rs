64f3d762cc3f7de30c039b52a5d45c5413d5c1c7
Michael Vines <mvines@gmail.com>
2020-10-19 12:32:20 -0700

    Remove program_stubs!()
    (cherry picked from commit 6d5889bdb5e8741b5cfbae5a3d04c9bb0118eeb1)

--- programs/bpf/rust/ristretto/src/lib.rs
+++ programs/bpf/rust/ristretto/src/lib.rs
@@ -38,8 +38,6 @@ fn process_instruction(
 #[cfg(test)]
 mod test {
     use super::*;
-    // Pull in syscall stubs when building for non-BPF targets
-    solana_sdk::program_stubs!();
 
     #[test]
     fn test_ristretto() {
