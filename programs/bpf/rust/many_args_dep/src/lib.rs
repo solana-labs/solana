6d5889bdb5e8741b5cfbae5a3d04c9bb0118eeb1
Michael Vines <mvines@gmail.com>
2020-10-19 12:32:20 -0700

    Remove program_stubs!()

--- programs/bpf/rust/many_args_dep/src/lib.rs
+++ programs/bpf/rust/many_args_dep/src/lib.rs
@@ -51,8 +51,6 @@ pub fn many_args_sret(
 #[cfg(test)]
 mod test {
     use super::*;
-    // Pull in syscall stubs when building for non-BPF targets
-    solana_sdk::program_stubs!();
 
     #[test]
     fn test_many_args() {
