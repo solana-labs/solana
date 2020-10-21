21aea94f5a07cd423ab3b60cd2ff5eb5d226422e
Michael Vines <mvines@gmail.com>
2020-10-20 17:50:20 -0700

    Add cargo-build-bpf
    (cherry picked from commit 07a853d6cc7605e90dc725d9dd736b85476eb294)

--- sdk/bpf/rust/build.sh
+++ sdk/bpf/rust/build.sh
@@ -1,49 +1,21 @@
 #!/usr/bin/env bash
 
-if [ "$#" -ne 1 ]; then
+if [[ "$#" -ne 1 ]]; then
     echo "Error: Must provide the full path to the project to build"
     exit 1
 fi
-if [ ! -f "$1/Cargo.toml" ]; then
+if [[ ! -f "$1/Cargo.toml" ]]; then
       echo "Error: Cannot find project: $1"
     exit 1
 fi
+bpf_sdk=$(cd "$(dirname "$0")/.." && pwd)
 
 echo "Building $1"
 set -e
-
-bpf_sdk=$(cd "$(dirname "$0")/.." && pwd)
-
-# Ensure the sdk is installed
-"$bpf_sdk"/scripts/install.sh
-
-# Use the SDK's version of llvm to build the compiler-builtins for BPF
-export CC="$bpf_sdk/dependencies/llvm-native/bin/clang"
-export AR="$bpf_sdk/dependencies/llvm-native/bin/llvm-ar"
-
-# Use the SDK's version of Rust to build for BPF
-export RUSTUP_TOOLCHAIN=bpf
-export RUSTFLAGS="
-    -C lto=no \
-    -C opt-level=2 \
-    -C link-arg=-z -C link-arg=notext \
-    -C link-arg=-T$bpf_sdk/rust/bpf.ld \
-    -C link-arg=--Bdynamic \
-    -C link-arg=-shared \
-    -C link-arg=--entry=entrypoint \
-    -C link-arg=-no-threads \
-    -C linker=$bpf_sdk/dependencies/llvm-native/bin/ld.lld"
-
-# CARGO may be set if build.sh is run from within cargo, causing
-# incompatibilities between cargo and xargo versions
-unset CARGO
-
-# Setup xargo
-export XARGO_HOME="$bpf_sdk/dependencies/xargo"
-export XARGO_RUST_SRC="$bpf_sdk/dependencies/rust-bpf-sysroot/src"
-export RUST_COMPILER_RT_ROOT="$bpf_sdk/dependencies/rust-bpf-sysroot/src/compiler-rt"
+# shellcheck source=sdk/bpf/env.sh
+source "$bpf_sdk"/env.sh
 
 cd "$1"
-xargo build --target bpfel-unknown-unknown --release --no-default-features --features program
+"$XARGO" build --target "$XARGO_TARGET" --release --no-default-features --features program
 
 { { set +x; } 2>/dev/null; echo Success; }
