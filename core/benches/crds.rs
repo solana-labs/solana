5d0125aa786c6d209b8910c89928f36547e30c30
behzad nouri <behzadnouri@gmail.com>
2020-10-22 08:47:46 -0400

    updates benchmark code

--- core/benches/crds.rs
+++ core/benches/crds.rs
@@ -16,26 +16,16 @@ fn bench_find_old_labels(bencher: &mut Bencher) {
     let thread_pool = ThreadPoolBuilder::new().build().unwrap();
     let mut rng = thread_rng();
     let mut crds = Crds::default();
-    let mut num_inserts = 0;
     let now = CRDS_GOSSIP_PULL_CRDS_TIMEOUT_MS + CRDS_GOSSIP_PULL_CRDS_TIMEOUT_MS / 1000;
-    for _ in 0..50_000 {
-        if crds
-            .insert(CrdsValue::new_rand(&mut rng), rng.gen_range(0, now))
-            .is_ok()
-        {
-            num_inserts += 1;
-        }
-    }
-    assert_eq!(num_inserts, 50_000);
+    std::iter::repeat_with(|| (CrdsValue::new_rand(&mut rng), rng.gen_range(0, now)))
+        .take(50_000)
+        .for_each(|(v, ts)| assert!(crds.insert(v, ts).is_ok()));
     let mut timeouts = HashMap::new();
     timeouts.insert(Pubkey::default(), CRDS_GOSSIP_PULL_CRDS_TIMEOUT_MS);
-    let mut fail = 0;
     bencher.iter(|| {
-        let out = crds.find_old_labels(&thread_pool, now, &timeouts).len();
-        if out < 10 || out > 250 {
-            fail += 1;
-        }
+        let out = crds.find_old_labels(&thread_pool, now, &timeouts);
+        assert!(out.len() > 10);
+        assert!(out.len() < 250);
         out
     });
-    assert_eq!(fail, 0);
 }
