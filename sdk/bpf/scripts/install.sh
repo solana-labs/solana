358a0ea8da5ca4054a98822993f875e8dd9a87a9
Jack May <jack@solana.com>
2020-10-23 11:47:23 -0700

    more portable install.sh

--- sdk/bpf/scripts/install.sh
+++ sdk/bpf/scripts/install.sh
@@ -11,43 +11,34 @@ fi
 download() {
   set -e
   declare url="$1/$2/$3"
-  declare version=$2
   declare filename=$3
-  declare dirname=$4
-  declare progress=$5
-  declare cache_directory=~/.cache/"$version"
-  declare cache_dirname=$cache_directory/${dirname//:\//_}
-
-  link() {
-    set -e
-    ln -sf "$cache_dirname" "$dirname"
-    ln -sf "$cache_directory/$dirname-$version.md" "$dirname-$version.md"
-  }
-
-  if [[ -r $cache_dirname ]]; then
-    link
-    return 0
-  fi
-
-  declare args=(
-    "$url" -O "$filename"
-    "--progress=dot:$progress"
+  declare wget_args=(
+    "$url -O $filename"
+    "--progress=dot:giga"
     "--retry-connrefused"
     "--read-timeout=30"
   )
+  declare curl_args=(
+    "-L $url -o $filename"
+  )
+  wget_or_curl=$(
+    if hash wget 2>/dev/null; then
+      echo "wget ${wget_args[*]}"
+    elif hash curl 2>/dev/null; then
+      echo "curl ${curl_args[*]}"
+    fi
+  )
+  if [ -z "$wget_or_curl" ]; then
+    echo "Error: Neither curl nor wget were found" >&2
+    return 1
+  fi
   set -x
-  mkdir -p "$cache_dirname"
-  pushd "$cache_dirname"
-  if wget "${args[@]}"; then
+  if $wget_or_curl; then
     tar --strip-components 1 -jxf "$filename"
+    { set +x; } 2>/dev/null
     rm -rf "$filename"
-    echo "$url" >"../$dirname-$version.md"
-    popd
-    link
     return 0
   fi
-  popd
-  rm -rf "$cache_dirname"
   return 1
 }
 
@@ -55,31 +46,43 @@ clone() {
   set -e
   declare url=$1
   declare version=$2
-  declare dirname=$3
-  declare cache_directory=~/.cache/"$version"
-  declare cache_dirname=$cache_directory/${dirname//:\//_}
 
-  link() {
-    set -e
-    ln -sf "$cache_dirname" "$dirname"
-    ln -sf "$cache_directory/$dirname-$version.md" "$dirname-$version.md"
-  }
+  cmd="git clone --recursive --depth 1 --single-branch --branch $version $url temp"
+  set -x
+  if $cmd; then
+    { set +x; } 2>/dev/null
+    shopt -s dotglob nullglob
+    mv temp/* .
+    return 0
+  fi
+  return 1
+}
+
+get() {
+  set -e
+  declare version=$1
+  declare dirname=$2
+  declare job=$3
+  declare cache_root=~/.cache/"$version"
+  declare cache_dirname="$cache_root/$dirname"
+  declare cache_partial_dirname="$cache_dirname"_partial
 
   if [[ -r $cache_dirname ]]; then
-    link
+    ln -sf "$cache_dirname" "$dirname"
     return 0
   fi
 
-  set -x
-  mkdir -p "$cache_directory"
-  pushd "$cache_directory"
-  cmd="git clone --recursive --depth 1 --single-branch --branch $version $url"
-  if $cmd; then
-    echo "$cmd" >"$dirname-$version.md"
+  rm -rf "$cache_partial_dirname"
+  mkdir -p "$cache_partial_dirname"
+  pushd "$cache_partial_dirname"
+
+  if $job; then
     popd
-    link
+    mv "$cache_partial_dirname" "$cache_dirname"
+    ln -sf "$cache_dirname" "$dirname"
     return 0
   fi
+  popd
   return 1
 }
 
@@ -106,56 +109,59 @@ fi
 
 # Install Criterion
 version=v2.3.2
-if [[ ! -e criterion-$version.md ]]; then
+if [[ ! -e criterion-$version.md || ! -e criterion ]]; then
   (
     set -e
     rm -rf criterion*
-    download "https://github.com/Snaipe/Criterion/releases/download" \
-      $version \
-      "criterion-$version-$machine-x86_64.tar.bz2" \
-      criterion \
-      mega
+    job="download \
+           https://github.com/Snaipe/Criterion/releases/download \
+           $version \
+           criterion-$version-$machine-x86_64.tar.bz2 \
+           criterion"
+    get $version criterion "$job"
   )
   exitcode=$?
   if [[ $exitcode -ne 0 ]]; then
-    rm -rf criterion-$version.md
     exit 1
   fi
+  touch criterion-$version.md
 fi
 
 # Install LLVM
 version=v0.0.15
-if [[ ! -e llvm-native-$version.md ]]; then
+if [[ ! -e llvm-native-$version.md || ! -e llvm-native ]]; then
   (
     set -e
     rm -rf llvm-native*
     rm -rf xargo
-    download "https://github.com/solana-labs/llvm-builder/releases/download" \
-      $version \
-      "solana-llvm-$machine.tar.bz2" \
-      llvm-native \
-      giga
+    job="download \
+           https://github.com/solana-labs/llvm-builder/releases/download \
+           $version \
+           solana-llvm-$machine.tar.bz2 \
+           llvm-native"
+    get $version llvm-native "$job"
   )
   exitcode=$?
   if [[ $exitcode -ne 0 ]]; then
-    rm -rf llvm-native-$version.md
     exit 1
   fi
+  touch llvm-native-$version.md
 fi
 
 # Install Rust-BPF
 version=v0.2.5
-if [[ ! -e rust-bpf-$version.md ]]; then
+if [[ ! -e rust-bpf-$version.md || ! -e rust-bpf ]]; then
   (
     set -e
     rm -rf rust-bpf
     rm -rf rust-bpf-$machine-*
     rm -rf xargo
-    download "https://github.com/solana-labs/rust-bpf-builder/releases/download" \
-      $version \
-      "solana-rust-bpf-$machine.tar.bz2" \
-      rust-bpf \
-      giga
+    job="download \
+           https://github.com/solana-labs/rust-bpf-builder/releases/download \
+           $version \
+           solana-rust-bpf-$machine.tar.bz2 \
+           rust-bpf"
+    get $version rust-bpf "$job"
 
     set -ex
     ./rust-bpf/bin/rustc --print sysroot
@@ -166,27 +172,28 @@ if [[ ! -e rust-bpf-$version.md ]]; then
   )
   exitcode=$?
   if [[ $exitcode -ne 0 ]]; then
-    rm -rf rust-bpf-$version.md
     exit 1
   fi
+  touch rust-bpf-$version.md
 fi
 
 # Install Rust-BPF Sysroot sources
 version=v0.12
-if [[ ! -e rust-bpf-sysroot-$version.md ]]; then
+if [[ ! -e rust-bpf-sysroot-$version.md || ! -e rust-bpf-sysroot ]]; then
   (
     set -e
     rm -rf rust-bpf-sysroot*
     rm -rf xargo
-    clone "https://github.com/solana-labs/rust-bpf-sysroot.git" \
-      $version \
-      rust-bpf-sysroot
+    job="clone \
+           https://github.com/solana-labs/rust-bpf-sysroot.git \
+           $version"
+    get $version rust-bpf-sysroot "$job"
   )
   exitcode=$?
   if [[ $exitcode -ne 0 ]]; then
-    rm -rf rust-bpf-sysroot-$version.md
     exit 1
   fi
+  touch rust-bpf-sysroot-$version.md
 fi
 
 exit 0
