9c1803a06c2b3d33c73589cc16f5e9bc7af9aba2
Alexander Meißner <AlexanderMeissner@gmx.net>
2020-10-29 12:48:00 +0100

    Updates rbpf to v0.2.0, which unifies the interfaces of the interpreter and the JIT. However, the JIT is not enabled yet.

--- cli/src/cli.rs
+++ cli/src/cli.rs
@@ -9,7 +9,7 @@ use log::*;
 use num_traits::FromPrimitive;
 use serde_json::{self, json, Value};
 use solana_account_decoder::{UiAccount, UiAccountEncoding};
-use solana_bpf_loader_program::bpf_verifier;
+use solana_bpf_loader_program::{bpf_verifier, BPFError};
 use solana_clap_utils::{
     self,
     commitment::commitment_arg_with_default,
@@ -39,7 +39,7 @@ use solana_client::{
 use solana_faucet::faucet::request_airdrop_transaction;
 #[cfg(test)]
 use solana_faucet::faucet_mock::request_airdrop_transaction;
-use solana_rbpf::vm::EbpfVm;
+use solana_rbpf::vm::Executable;
 use solana_remote_wallet::remote_wallet::RemoteWalletManager;
 use solana_sdk::{
     bpf_loader, bpf_loader_deprecated,
@@ -1217,7 +1217,7 @@ fn do_process_deploy(
         CliError::DynamicProgramError(format!("Unable to read program file: {}", err))
     })?;
 
-    EbpfVm::create_executable_from_elf(&program_data, Some(|x| bpf_verifier::check(x, true)))
+    Executable::<BPFError>::from_elf(&program_data, Some(|x| bpf_verifier::check(x, true)))
         .map_err(|err| CliError::DynamicProgramError(format!("ELF error: {}", err)))?;
 
     let loader_id = if use_deprecated_loader {
