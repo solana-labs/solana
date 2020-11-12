f24ffe489e6cc2259e4f80e48c87a9dda62cdff4
dr497 <anand.ratiney@gmail.com>
2020-11-12 11:59:53 +0800

    Add NFTs to the explorer

--- explorer/src/tokenRegistry.ts
+++ explorer/src/tokenRegistry.ts
@@ -115,4 +115,124 @@ const MAINNET_TOKENS: { [key: string]: TokenDetails } = {
     symbol: "SUSHI",
     name: "Wrapped Sushi",
   },
+  AcstFzGGawvvdVhYV9bftr7fmBHbePUjhv53YK1W3dZo: {
+    symbol: "LSD",
+    name: "LSD",
+    website: "https://solible.com",
+  },
+  "91fSFQsPzMLat9DHwLdQacW3i3EGnWds5tA5mt7yLiT9": {
+    symbol: "Unlimited Energy",
+    name: "Unlimited Energy",
+    website: "https://solible.com",
+  },
+  "29PEpZeuqWf9tS2gwCjpeXNdXLkaZSMR2s1ibkvGsfnP": {
+    symbol: "Need for Speed",
+    name: "Need for Speed",
+    website: "https://solible.com",
+  },
+  HsY8PNar8VExU335ZRYzg89fX7qa4upYu6vPMPFyCDdK: {
+    symbol: "ADOR OPENS",
+    name: "ADOR OPENS",
+    website: "https://solible.com",
+  },
+  EDP8TpLJ77M3KiDgFkZW4v4mhmKJHZi9gehYXenfFZuL: {
+    symbol: "CMS - Rare",
+    name: "CMS - Rare",
+    website: "https://solible.com",
+  },
+  BrUKFwAABkExb1xzYU4NkRWzjBihVQdZ3PBz4m5S8if3: {
+    symbol: "Tesla",
+    name: "Tesla",
+    website: "https://solible.com",
+  },
+  "9CmQwpvVXRyixjiE3LrbSyyopPZohNDN1RZiTk8rnXsQ": {
+    symbol: "DeceFi",
+    name: "DeceFi",
+    website: "https://solible.com",
+  },
+  F6ST1wWkx2PeH45sKmRxo1boyuzzWCfpnvyKL4BGeLxF: {
+    symbol: "Power User",
+    name: "Power User",
+    website: "https://solible.com",
+  },
+  dZytJ7iPDcCu9mKe3srL7bpUeaR3zzkcVqbtqsmxtXZ: {
+    symbol: "VIP Member",
+    name: "VIP Member",
+    website: "https://solible.com",
+  },
+  "8T4vXgwZUWwsbCDiptHFHjdfexvLG9UP8oy1psJWEQdS": {
+    symbol: "Uni Christmas",
+    name: "Uni Christmas",
+    website: "https://solible.com",
+  },
+  EjFGGJSyp9UDS8aqafET5LX49nsG326MeNezYzpiwgpQ: {
+    symbol: "BNB",
+    name: "BNB",
+    website: "https://solible.com",
+  },
+  FkmkTr4en8CXkfo9jAwEMov6PVNLpYMzWr3Udqf9so8Z: {
+    symbol: "Seldom",
+    name: "Seldom",
+    website: "https://solible.com",
+  },
+  "2gn1PJdMAU92SU5inLSp4Xp16ZC5iLF6ScEi7UBvp8ZD": {
+    symbol: "Satoshi Closeup",
+    name: "Satoshi Closeup",
+    website: "https://solible.com",
+  },
+  "7mhZHtPL4GFkquQR4Y6h34Q8hNkQvGc1FaNtyE43NvUR": {
+    symbol: "Satoshi GB",
+    name: "Satoshi GB",
+    website: "https://solible.com",
+  },
+  "8RoKfLx5RCscbtVh8kYb81TF7ngFJ38RPomXtUREKsT2": {
+    symbol: "Satoshi OG",
+    name: "Satoshi OG",
+    website: "https://solible.com",
+  },
+  "9rw5hyDngBQ3yDsCRHqgzGHERpU2zaLh1BXBUjree48J": {
+    symbol: "Satoshi BTC",
+    name: "Satoshi BTC",
+    website: "https://solible.com",
+  },
+  AiD7J6D5Hny5DJB1MrYBc2ePQqy2Yh4NoxWwYfR7PzxH: {
+    symbol: "Satoshi GB",
+    name: "Satoshi GB",
+    website: "https://solible.com",
+  },
+  bxiA13fpU1utDmYuUvxvyMT8odew5FEm96MRv7ij3eb: {
+    symbol: "Satoshi",
+    name: "Satoshi",
+    website: "https://solible.com",
+  },
+  GoC24kpj6TkvjzspXrjSJC2CVb5zMWhLyRcHJh9yKjRF: {
+    symbol: "Satoshi Closeup",
+    name: "Satoshi Closeup",
+    website: "https://solible.com",
+  },
+  oCUduD44ETuZ65bpWdPzPDSnAdreg1sJrugfwyFZVHV: {
+    symbol: "Satoshi BTC",
+    name: "Satoshi BTC",
+    website: "https://solible.com",
+  },
+  "9Vvre2DxBB9onibwYDHeMsY1cj6BDKtEDccBPWRN215E": {
+    symbol: "Satoshi Nakamoto",
+    name: "Satoshi Nakamoto",
+    website: "https://solible.com",
+  },
+  "7RpFk44cMTAUt9CcjEMWnZMypE9bYQsjBiSNLn5qBvhP": {
+    symbol: "Charles Hoskinson",
+    name: "Charles Hoskinson",
+    website: "https://solible.com",
+  },
+  GyRkPAxpd9XrMHcBF6fYHVRSZQvQBwAGKAGQeBPSKzMq: {
+    symbol: "SBF",
+    name: "SBF",
+    website: "https://solible.com",
+  },
+  AgdBQN2Sy2abiZ2KToWeUsQ9PHdCv95wt6kVWRf5zDkx: {
+    symbol: "Bitcoin Tram",
+    name: "Bitcoin Tram",
+    website: "https://solible.com",
+  },
 };
