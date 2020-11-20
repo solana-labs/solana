f36f3514ba35f767dbe1404c31659307d4bff09d
Alexander Meißner <AlexanderMeissner@gmx.net>
2020-11-20 17:02:21 +0100

    Replaces Default::default() by explicit values

--- programs/bpf/tests/programs.rs
+++ programs/bpf/tests/programs.rs
@@ -87,8 +87,10 @@ fn run_program(
     let mut instruction_meter = ThisInstructionMeter { compute_meter };
 
     let config = Config {
+        max_call_depth: 20,
+        stack_frame_size: 4096,
+        enable_instruction_meter: true,
         enable_instruction_tracing: true,
-        ..Default::default()
     };
     let mut executable = Executable::from_elf(&data, None, config).unwrap();
     executable.set_syscall_registry(register_syscalls(&mut invoke_context).unwrap());
