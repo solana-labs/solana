0d7bb9627765d25657f94e86e773f88321c64546
Jack May <jack@solana.com>
2020-12-01 08:46:28 -0800

    Resolve conflicts

--- programs/bpf/rust/caller_access/src/lib.rs
+++ programs/bpf/rust/caller_access/src/lib.rs
@@ -1,4 +1,4 @@
-use solana_program::{
+use solana_sdk::{
     account_info::AccountInfo,
     entrypoint,
     entrypoint::ProgramResult,
--- programs/bpf/tests/programs.rs
+++ programs/bpf/tests/programs.rs
@@ -523,11 +523,7 @@ fn test_program_bpf_invoke() {
             &[TEST_SUCCESS, bump_seed1, bump_seed2, bump_seed3],
             account_metas.clone(),
         );
-        let noop_instruction = Instruction::new(
-            noop_program_id,
-            &(),
-            vec![]
-        );
+        let noop_instruction = Instruction::new(noop_program_id, &(), vec![]);
         let message = Message::new(&[instruction, noop_instruction], Some(&mint_pubkey));
         let tx = Transaction::new(
             &[
@@ -699,59 +695,6 @@ fn test_program_bpf_invoke() {
             assert_eq!(i as u8, account.data[i]);
         }
     }
-<<<<<<< HEAD
-=======
-
-    // Check for program id spoofing
-    {
-        let GenesisConfigInfo {
-            genesis_config,
-            mint_keypair,
-            ..
-        } = create_genesis_config(50);
-        let mut bank = Bank::new(&genesis_config);
-        let (name, id, entrypoint) = solana_bpf_loader_program!();
-        bank.add_builtin(&name, id, entrypoint);
-        let bank = Arc::new(bank);
-        let bank_client = BankClient::new_shared(&bank);
-
-        let malicious_swap_pubkey = load_bpf_program(
-            &bank_client,
-            &bpf_loader::id(),
-            &mint_keypair,
-            "solana_bpf_rust_spoof1",
-        );
-        let malicious_system_pubkey = load_bpf_program(
-            &bank_client,
-            &bpf_loader::id(),
-            &mint_keypair,
-            "solana_bpf_rust_spoof1_system",
-        );
-
-        let from_pubkey = Pubkey::new_unique();
-        let account = Account::new(10, 0, &solana_sdk::system_program::id());
-        bank.store_account(&from_pubkey, &account);
-
-        let to_pubkey = Pubkey::new_unique();
-        let account = Account::new(0, 0, &solana_sdk::system_program::id());
-        bank.store_account(&to_pubkey, &account);
-
-        let account_metas = vec![
-            AccountMeta::new_readonly(solana_sdk::system_program::id(), false),
-            AccountMeta::new_readonly(malicious_system_pubkey, false),
-            AccountMeta::new(from_pubkey, false),
-            AccountMeta::new(to_pubkey, false),
-        ];
-
-        let instruction = Instruction::new(malicious_swap_pubkey, &(), account_metas.clone());
-        let result = bank_client.send_and_confirm_instruction(&mint_keypair, instruction);
-        assert_eq!(
-            result.unwrap_err().unwrap(),
-            TransactionError::InstructionError(0, InstructionError::ModifiedProgramId)
-        );
-        assert_eq!(10, bank.get_balance(&from_pubkey));
-        assert_eq!(0, bank.get_balance(&to_pubkey));
-    }
 
     // Check the caller has access to cpi program
     {
@@ -789,7 +732,6 @@ fn test_program_bpf_invoke() {
             TransactionError::InstructionError(0, InstructionError::MissingAccount)
         );
     }
->>>>>>> 733fcbaa6... Check that the program was granted access to program_id (#13890)
 }
 
 #[cfg(feature = "bpf_rust")]
