aa2751e61413a0e6decd201a5d1d5086d024599f
sakridge <sakridge@gmail.com>
2020-12-10 18:20:08 -0800

    Check shred type in is_duplicate (#14050)

--- ledger/src/blockstore.rs
+++ ledger/src/blockstore.rs
@@ -2714,10 +2714,20 @@ impl Blockstore {
     // Returns the existing shred if `new_shred` is not equal to the existing shred at the
     // given slot and index as this implies the leader generated two different shreds with
     // the same slot and index
-    pub fn is_shred_duplicate(&self, slot: u64, index: u32, new_shred: &[u8]) -> Option<Vec<u8>> {
-        let res = self
-            .get_data_shred(slot, index as u64)
-            .expect("fetch from DuplicateSlots column family failed");
+    pub fn is_shred_duplicate(
+        &self,
+        slot: u64,
+        index: u32,
+        new_shred: &[u8],
+        is_data: bool,
+    ) -> Option<Vec<u8>> {
+        let res = if is_data {
+            self.get_data_shred(slot, index as u64)
+                .expect("fetch from DuplicateSlots column family failed")
+        } else {
+            self.get_coding_shred(slot, index as u64)
+                .expect("fetch from DuplicateSlots column family failed")
+        };
 
         res.map(|existing_shred| {
             if existing_shred != new_shred {
@@ -7279,11 +7289,21 @@ pub mod tests {
 
             // Check if shreds are duplicated
             assert_eq!(
-                blockstore.is_shred_duplicate(slot, 0, &duplicate_shred.payload),
+                blockstore.is_shred_duplicate(
+                    slot,
+                    0,
+                    &duplicate_shred.payload,
+                    duplicate_shred.is_data()
+                ),
                 Some(shred.payload.clone())
             );
             assert!(blockstore
-                .is_shred_duplicate(slot, 0, &non_duplicate_shred.payload)
+                .is_shred_duplicate(
+                    slot,
+                    0,
+                    &non_duplicate_shred.payload,
+                    duplicate_shred.is_data()
+                )
                 .is_none());
 
             // Store a duplicate shred
