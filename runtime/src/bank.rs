cd8ab3bc62bb6840c805b5e39aa3b4e2e1dd1acd
Carl Lin <carl@solana.com>
2020-12-15 00:33:18 -0800

    Revert "Add check frozen slots are not stored to"
    This reverts commit e7c70965cdd279f75d7e7d78a9e34bcd718d135b.

--- runtime/src/bank.rs
+++ runtime/src/bank.rs
@@ -1952,7 +1952,6 @@ impl Bank {
             self.run_incinerator();
 
             // freeze is a one-way trip, idempotent
-            self.rc.accounts.accounts_db.mark_slot_frozen(self.slot());
             *hash = self.hash_internal_state();
         }
     }
