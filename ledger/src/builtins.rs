501fd83afda1c97e699941f57a2b2c9310fac0df
Michael Vines <mvines@gmail.com>
2020-12-15 20:29:09 -0800

    Fix up upgradeable bpf loader activation

--- ledger/src/builtins.rs
+++ ledger/src/builtins.rs
@@ -2,7 +2,7 @@ use solana_runtime::{
     bank::{Builtin, Builtins},
     builtins::ActivationType,
 };
-use solana_sdk::{feature_set, genesis_config::ClusterType, pubkey::Pubkey};
+use solana_sdk::{feature_set, pubkey::Pubkey};
 
 macro_rules! to_builtin {
     ($b:expr) => {
@@ -11,37 +11,33 @@ macro_rules! to_builtin {
 }
 
 /// Builtin programs that are always available
-fn genesis_builtins(cluster_type: ClusterType, bpf_jit: bool) -> Vec<Builtin> {
-    let mut builtins = vec![
+fn genesis_builtins(bpf_jit: bool) -> Vec<Builtin> {
+    vec![
         to_builtin!(solana_bpf_loader_deprecated_program!()),
         if bpf_jit {
             to_builtin!(solana_bpf_loader_program_with_jit!())
         } else {
             to_builtin!(solana_bpf_loader_program!())
         },
-    ];
-    if cluster_type != ClusterType::MainnetBeta {
-        builtins.push(if bpf_jit {
-            to_builtin!(solana_bpf_loader_upgradeable_program_with_jit!())
-        } else {
-            to_builtin!(solana_bpf_loader_upgradeable_program!())
-        });
-    }
-    builtins
+    ]
 }
 
 /// Builtin programs activated dynamically by feature
-fn feature_builtins() -> Vec<(Builtin, Pubkey, ActivationType)> {
+fn feature_builtins(bpf_jit: bool) -> Vec<(Builtin, Pubkey, ActivationType)> {
     vec![(
-        to_builtin!(solana_bpf_loader_upgradeable_program!()),
+        if bpf_jit {
+            to_builtin!(solana_bpf_loader_upgradeable_program_with_jit!())
+        } else {
+            to_builtin!(solana_bpf_loader_upgradeable_program!())
+        },
         feature_set::bpf_loader_upgradeable_program::id(),
         ActivationType::NewProgram,
     )]
 }
 
-pub(crate) fn get(cluster_type: ClusterType, bpf_jit: bool) -> Builtins {
+pub(crate) fn get(bpf_jit: bool) -> Builtins {
     Builtins {
-        genesis_builtins: genesis_builtins(cluster_type, bpf_jit),
-        feature_builtins: feature_builtins(),
+        genesis_builtins: genesis_builtins(bpf_jit),
+        feature_builtins: feature_builtins(bpf_jit),
     }
 }
