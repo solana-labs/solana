3d077fb656c8a729ee86c27716c0f53f6a07c804
Michael Vines <mvines@gmail.com>
2020-12-30 20:55:41 -0800

    Revert "Upgrade in-tree tokio 0.2 usage to tokio 0.3"
    This reverts commit 444ed768dcacb073607a43401a3674c7b6061552.

--- core/src/rpc_service.rs
+++ core/src/rpc_service.rs
@@ -32,6 +32,7 @@ use std::{
     sync::{mpsc::channel, Arc, Mutex, RwLock},
     thread::{self, Builder, JoinHandle},
 };
+use tokio::runtime;
 
 pub struct JsonRpcService {
     thread_hdl: JoinHandle<()>,
@@ -40,6 +41,7 @@ pub struct JsonRpcService {
     pub request_processor: JsonRpcRequestProcessor, // Used only by test_rpc_new()...
 
     close_handle: Option<CloseHandle>,
+    runtime: runtime::Runtime,
 }
 
 struct RpcRequestMiddleware {
@@ -280,13 +282,12 @@ impl JsonRpcService {
         ));
 
         let tpu_address = cluster_info.my_contact_info().tpu;
-        let runtime = Arc::new(
-            tokio::runtime::Builder::new_multi_thread()
-                .thread_name("rpc-runtime")
-                .enable_all()
-                .build()
-                .expect("Runtime"),
-        );
+        let mut runtime = runtime::Builder::new()
+            .threaded_scheduler()
+            .thread_name("rpc-runtime")
+            .enable_all()
+            .build()
+            .expect("Runtime");
 
         let exit_bigtable_ledger_upload_service = Arc::new(AtomicBool::new(false));
 
@@ -300,7 +301,7 @@ impl JsonRpcService {
                         info!("BigTable ledger storage initialized");
 
                         let bigtable_ledger_upload_service = Arc::new(BigTableUploadService::new(
-                            runtime.clone(),
+                            runtime.handle().clone(),
                             bigtable_ledger_storage.clone(),
                             blockstore.clone(),
                             block_commitment_cache.clone(),
@@ -329,7 +330,7 @@ impl JsonRpcService {
             health.clone(),
             cluster_info.clone(),
             genesis_hash,
-            runtime,
+            &runtime,
             bigtable_ledger_storage,
             optimistically_confirmed_bank,
         );
@@ -403,6 +404,7 @@ impl JsonRpcService {
             .register_exit(Box::new(move || close_handle_.close()));
         Self {
             thread_hdl,
+            runtime,
             #[cfg(test)]
             request_processor: test_request_processor,
             close_handle: Some(close_handle),
@@ -416,6 +418,7 @@ impl JsonRpcService {
     }
 
     pub fn join(self) -> thread::Result<()> {
+        self.runtime.shutdown_background();
         self.thread_hdl.join()
     }
 }
