ed76de17c434b1c26adcaa0d106098680846ab88
Tyera Eulberg <tyera@solana.com>
2021-01-20 11:20:19 -0700

    Fix conflicts

--- cli/src/program.rs
+++ cli/src/program.rs
@@ -11,16 +11,8 @@ use bip39::{Language, Mnemonic, MnemonicType, Seed};
 use clap::{App, AppSettings, Arg, ArgMatches, SubCommand};
 use log::*;
 use serde_json::{self, json};
-<<<<<<< HEAD
 use solana_bpf_loader_program::bpf_verifier;
-use solana_clap_utils::{
-    self, commitment::commitment_arg_with_default, input_parsers::*, input_validators::*,
-    keypair::*,
-};
-=======
-use solana_bpf_loader_program::{bpf_verifier, BPFError, ThisInstructionMeter};
 use solana_clap_utils::{self, input_parsers::*, input_validators::*, keypair::*};
->>>>>>> a7086a0f8... Cli: promote commitment to a global arg + config.yml (#14684)
 use solana_cli_output::display::new_spinner_progress_bar;
 use solana_client::{
     rpc_client::RpcClient, rpc_config::RpcSendTransactionConfig,
@@ -226,7 +218,6 @@ impl ProgramSubCommands for App<'_, '_> {
                                 .conflicts_with("new_buffer_authority")
                                 .help("The buffer will be immutable")
                         )
-                        .arg(commitment_arg_with_default("singleGossip")),
                 )
                 .subcommand(
                     SubCommand::with_name("set-upgrade-authority")
@@ -260,22 +251,6 @@ impl ProgramSubCommands for App<'_, '_> {
                                 .conflicts_with("new_upgrade_authority")
                                 .help("The program will not be upgradeable")
                         )
-<<<<<<< HEAD
-                        .arg(commitment_arg_with_default("singleGossip")),
-=======
-                )
-                .subcommand(
-                    SubCommand::with_name("get-authority")
-                        .about("Gets a buffer or program account's authority")
-                        .arg(
-                            Arg::with_name("account")
-                                .index(1)
-                                .value_name("ACCOUNT_ADDRESS")
-                                .takes_value(true)
-                                .required(true)
-                                .help("Public key of the account to query")
-                        ),
->>>>>>> a7086a0f8... Cli: promote commitment to a global arg + config.yml (#14684)
                 )
         )
     }
