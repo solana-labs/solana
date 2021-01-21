27f7a3e8a261f1094bff2afa3e9c86a973836dee
Josh Hundley <josh.hundley@gmail.com>
2021-01-20 23:34:31 -0800

    explorer: introduce transfer with seed instruction card

---
+++ explorer/src/components/instruction/system/TransferWithSeedDetailsCard.tsx
@@ -0,0 +1,82 @@
+import React from "react";
+import {
+  SystemProgram,
+  SignatureResult,
+  ParsedInstruction,
+} from "@solana/web3.js";
+import { lamportsToSolString } from "utils";
+import { InstructionCard } from "../InstructionCard";
+import { Copyable } from "components/common/Copyable";
+import { Address } from "components/common/Address";
+import { TransferWithSeedInfo } from "./types";
+
+export function TransferWithSeedDetailsCard(props: {
+  ix: ParsedInstruction;
+  index: number;
+  result: SignatureResult;
+  info: TransferWithSeedInfo;
+  innerCards?: JSX.Element[];
+  childIndex?: number;
+}) {
+  const { ix, index, result, info, innerCards, childIndex } = props;
+
+  return (
+    <InstructionCard
+      ix={ix}
+      index={index}
+      result={result}
+      title="Transfer w/ Seed"
+      innerCards={innerCards}
+      childIndex={childIndex}
+    >
+      <tr>
+        <td>Program</td>
+        <td className="text-lg-right">
+          <Address pubkey={SystemProgram.programId} alignRight link />
+        </td>
+      </tr>
+
+      <tr>
+        <td>From Address</td>
+        <td className="text-lg-right">
+          <Address pubkey={info.source} alignRight link />
+        </td>
+      </tr>
+
+      <tr>
+        <td>Destination Address</td>
+        <td className="text-lg-right">
+          <Address pubkey={info.destination} alignRight link />
+        </td>
+      </tr>
+
+      <tr>
+        <td>Base Address</td>
+        <td className="text-lg-right">
+          <Address pubkey={info.sourceBase} alignRight link />
+        </td>
+      </tr>
+
+      <tr>
+        <td>Transfer Amount (SOL)</td>
+        <td className="text-lg-right">{lamportsToSolString(info.lamports)}</td>
+      </tr>
+
+      <tr>
+        <td>Seed</td>
+        <td className="text-lg-right">
+          <Copyable right text={info.sourceSeed}>
+            <code>{info.sourceSeed}</code>
+          </Copyable>
+        </td>
+      </tr>
+
+      <tr>
+        <td>Source Owner</td>
+        <td className="text-lg-right">
+          <Address pubkey={info.sourceOwner} alignRight link />
+        </td>
+      </tr>
+    </InstructionCard>
+  );
+}
