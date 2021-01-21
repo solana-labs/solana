15baf43d1e5859205e4f3829630032fb917f146b
Michael Vines <mvines@gmail.com>
2021-01-20 21:52:01 -0800

    Template for an Inflation Candidate nomination
    To submit your nomination:
    1. Replace all instances of "my_name" with a suitable alternative then address the "TODO" code comments
    2. Submit a new Github pull request and work with the project contributors to merge your pull request

--- sdk/src/feature_set.rs
+++ sdk/src/feature_set.rs
@@ -49,6 +49,25 @@ pub mod full_inflation {
         }
     }
 
+    pub mod my_name {
+        pub mod vote {
+            // TODO: Contact the Solana Foundation by emailing nominations@solana.foundation and
+            // replace "MyVoteAddress111111111111111111111111111111" with the pubkey provided to
+            // you.
+            //
+            // Once completed remove this TODO comment
+            solana_sdk::declare_id!("MyVoteAddress111111111111111111111111111111");
+        }
+        pub mod enable {
+            // TODO: Run `solana-keygen new -o inflation.json` and replace
+            // "MyEnab1eAddress1111111111111111111111111111" with the pubkey for the keypair you
+            // just generated, as output by solana-keygen.  Keep `inflation.json` in a safe place.
+            //
+            // Once completed remove this TODO comment
+            solana_sdk::declare_id!("MyEnab1eAddress1111111111111111111111111111");
+        }
+    }
+
     pub mod nam {
         pub mod vote {
             solana_sdk::declare_id!("Hb6tvjY81EmgapxNS4dos1v8Q2RSjQABphu7cnzM4ELa");
@@ -230,6 +249,8 @@ lazy_static! {
         (track_writable_deescalation::id(), "track account writable deescalation"),
         (spl_token_v2_self_transfer_fix::id(), "spl-token self-transfer fix"),
         (matching_buffer_upgrade_authorities::id(), "Upgradeable buffer and program authorities must match"),
+        (full_inflation::my_name::vote::id(), "Community vote allowing my_name to enable full inflation"),
+        (full_inflation::my_name::enable::id(), "Full inflation enabled by my_name"),
         (full_inflation::nam::vote::id(), "community vote allowing Nam to enable full inflation"),
         (full_inflation::nam::enable::id(), "full inflation enabled by Nam"),
         /*************** ADD NEW FEATURES HERE ***************/
@@ -262,6 +283,10 @@ lazy_static! {
         FullInflationFeaturePair {
             vote_id: full_inflation::candidate_example::vote::id(),
             enable_id: full_inflation::candidate_example::enable::id(),
+        },
+        FullInflationFeaturePair {
+            vote_id: full_inflation::my_name::vote::id(),
+            enable_id: full_inflation::my_name::enable::id(),
         },
         FullInflationFeaturePair {
             vote_id: full_inflation::nam::vote::id(),
