62bcbb8431dc45b64f3a2adac9efbf84fb9ec070
Trent Nelson <trent@solana.com>
2021-01-21 23:58:55 -0700

    CLI: Don't scare the users
    (cherry picked from commit e9c98f241648683fcf040e00f1d3b45c204b0b91)

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -242,7 +242,6 @@ pub fn parse_args<'a>(
 }
 
 fn main() -> Result<(), Box<dyn error::Error>> {
-    solana_logger::setup();
     let matches = app(
         crate_name!(),
         crate_description!(),
