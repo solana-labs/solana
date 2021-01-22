82bb0039cacc92a56663cfc192feea2da060f7a5
Jeff Washington (jwash) <wash678@gmail.com>
2021-01-22 12:04:14 -0600

    typo

--- multinode-demo/bench-tps.sh
+++ multinode-demo/bench-tps.sh
@@ -14,7 +14,7 @@ usage() {
   echo
   echo " Run bench-tps "
   echo
-  echo "   extra args: additional arguments are pass along to solana-bench-tps"
+  echo "   extra args: additional arguments are passed along to solana-bench-tps"
   echo
   exit 1
 }
