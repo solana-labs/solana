6dcf73c47ba9458b39f4cc37791ce0d40d11d0ba
Josh Hundley <josh.hundley@gmail.com>
2021-01-22 14:35:56 -0800

    explorer: add block rewards

--- explorer/src/components/block/BlockOverviewCard.tsx
+++ explorer/src/components/block/BlockOverviewCard.tsx
@@ -1,14 +1,14 @@
-import bs58 from "bs58";
 import React from "react";
 import { TableCardBody } from "components/common/TableCardBody";
 import { useBlock, useFetchBlock, FetchStatus } from "providers/block";
-import { Signature } from "components/common/Signature";
 import { ErrorCard } from "components/common/ErrorCard";
 import { LoadingCard } from "components/common/LoadingCard";
 import { Slot } from "components/common/Slot";
 import { ClusterStatus, useCluster } from "providers/cluster";
+import { BlockHistoryCard } from "./BlockHistoryCard";
+import { BlockRewardsCard } from "./BlockRewardsCard";
 
-export function BlockHistoryCard({ slot }: { slot: number }) {
+export function BlockOverviewCard({ slot }: { slot: number }) {
   const confirmedBlock = useBlock(slot);
   const fetchBlock = useFetchBlock();
   const { status } = useCluster();
@@ -67,61 +67,8 @@ export function BlockHistoryCard({ slot }: { slot: number }) {
         </TableCardBody>
       </div>
 
-      {block.transactions.length === 0 ? (
-        <ErrorCard text="This block has no transactions" />
-      ) : (
-        <div className="card">
-          <div className="card-header align-items-center">
-            <h3 className="card-header-title">Block Transactions</h3>
-          </div>
-
-          <div className="table-responsive mb-0">
-            <table className="table table-sm table-nowrap card-table">
-              <thead>
-                <tr>
-                  <th className="text-muted">Result</th>
-                  <th className="text-muted">Transaction Signature</th>
-                </tr>
-              </thead>
-              <tbody className="list">
-                {block.transactions.map((tx, i) => {
-                  let statusText;
-                  let statusClass;
-                  let signature: React.ReactNode;
-                  if (tx.meta?.err || !tx.transaction.signature) {
-                    statusClass = "warning";
-                    statusText = "Failed";
-                  } else {
-                    statusClass = "success";
-                    statusText = "Success";
-                  }
-
-                  if (tx.transaction.signature) {
-                    signature = (
-                      <Signature
-                        signature={bs58.encode(tx.transaction.signature)}
-                        link
-                      />
-                    );
-                  }
-
-                  return (
-                    <tr key={i}>
-                      <td>
-                        <span className={`badge badge-soft-${statusClass}`}>
-                          {statusText}
-                        </span>
-                      </td>
-
-                      <td>{signature}</td>
-                    </tr>
-                  );
-                })}
-              </tbody>
-            </table>
-          </div>
-        </div>
-      )}
+      <BlockRewardsCard block={block} />
+      <BlockHistoryCard block={block} />
     </>
   );
 }
