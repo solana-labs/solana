8c77898fc60a15b2095724111c18dc18f65b1325
Josh Hundley <josh.hundley@gmail.com>
2021-01-27 16:12:54 -0800

    explorer: update flagged account message

--- explorer/src/pages/AccountDetailsPage.tsx
+++ explorer/src/pages/AccountDetailsPage.tsx
@@ -161,7 +161,7 @@ function DetailsSections({ pubkey, tab }: { pubkey: PublicKey; tab?: string }) {
     <>
       {isScam && (
         <div className="alert alert-danger alert-scam" role="alert">
-          Warning! This account has been flagged as a scam account. Please be
+          Warning! This account has been flagged by the Solana community as a scam account. Please be
           cautious sending SOL to this account.
         </div>
       )}
