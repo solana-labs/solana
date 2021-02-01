22e8723a40e4816eb83ff4dcfcfcd80b11e369a0
Trent Nelson <trent@solana.com>
2021-01-31 23:42:35 -0700

    cli: Add sigverify status to `decode-transaction` output
    (cherry picked from commit d547585041a73518135f3c9ec376fc902c35cbbe)

--- cli-output/src/cli_output.rs
+++ cli-output/src/cli_output.rs
@@ -1571,7 +1571,7 @@ pub enum CliSignatureVerificationStatus {
 }
 
 impl CliSignatureVerificationStatus {
-    pub fn get(tx: &Transaction) -> Vec<Self> {
+    pub fn verify_transaction(tx: &Transaction) -> Vec<Self> {
         tx.verify_with_results()
             .iter()
             .zip(&tx.signatures)
