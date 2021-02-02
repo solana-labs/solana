894b412aef6520a79e3554e10c6d740137250117
Trent Nelson <trent@solana.com>
2021-02-01 21:30:15 -0700

    docs: bump nofiles recommendations to match maps

--- ledger/src/blockstore.rs
+++ ledger/src/blockstore.rs
@@ -3550,7 +3550,7 @@ fn adjust_ulimit_nofile(_enforce_ulimit_nofile: bool) -> Result<()> {
 fn adjust_ulimit_nofile(enforce_ulimit_nofile: bool) -> Result<()> {
     // Rocks DB likes to have many open files.  The default open file descriptor limit is
     // usually not enough
-    let desired_nofile = 500000;
+    let desired_nofile = 700000;
 
     fn get_nofile() -> libc::rlimit {
         let mut nofile = libc::rlimit {
