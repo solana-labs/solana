d89a5aa46abf64695b9c1b983ddd3bdb1f0d002b
behzad nouri <behzadnouri@gmail.com>
2021-02-02 09:43:36 -0500

    removes pubkey references

--- core/src/pubkey_references.rs
+++ core/src/pubkey_references.rs
@@ -1,28 +1,8 @@
 use solana_sdk::pubkey::Pubkey;
 use std::{
     collections::HashSet,
-    rc::Rc,
     sync::{Arc, RwLock},
 };
-
-#[derive(Default)]
-pub struct PubkeyReferences(HashSet<Rc<Pubkey>>);
-
-impl PubkeyReferences {
-    pub fn get_or_insert(&mut self, pubkey: &Pubkey) -> Rc<Pubkey> {
-        let mut cached_pubkey: Option<Rc<Pubkey>> = self.0.get(pubkey).cloned();
-        if cached_pubkey.is_none() {
-            let new_pubkey = Rc::new(*pubkey);
-            self.0.insert(new_pubkey.clone());
-            cached_pubkey = Some(new_pubkey);
-        }
-        cached_pubkey.unwrap()
-    }
-
-    pub fn purge(&mut self) {
-        self.0.retain(|x| Rc::strong_count(x) > 1);
-    }
-}
 
 #[derive(Default)]
 pub struct LockedPubkeyReferences(pub RwLock<HashSet<Arc<Pubkey>>>);
