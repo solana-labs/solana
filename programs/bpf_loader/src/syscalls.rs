0e447daaae2e16c0d74d6a1c51b3296acc195fd9
Mrmaxmeier <Mrmaxmeier@gmail.com>
2021-02-03 16:20:03 +0100

    syscalls: fix integer overflow in check_instruction_size
    This will reject overflowing inputs with
    SyscallError::InstructionTooLarge instead of an AccessViolation.

--- programs/bpf_loader/src/syscalls.rs
+++ programs/bpf_loader/src/syscalls.rs
@@ -1471,7 +1471,9 @@ fn check_instruction_size(
     data_len: usize,
     invoke_context: &Ref<&mut dyn InvokeContext>,
 ) -> Result<(), EbpfError<BPFError>> {
-    let size = num_accounts * size_of::<AccountMeta>() + data_len;
+    let size = num_accounts
+        .saturating_mul(size_of::<AccountMeta>())
+        .saturating_add(data_len);
     let max_size = invoke_context
         .get_bpf_compute_budget()
         .max_cpi_instruction_size;
