e7f53182e223bb24bc89432e22f3d8ad64ac58ef
Jeff Washington (jwash) <75863576+jeffwashington@users.noreply.github.com>
2021-02-04 09:20:50 -0600

    fix merge conflict (#15085)

--- runtime/src/accounts_db.rs
+++ runtime/src/accounts_db.rs
@@ -313,16 +313,6 @@ impl<'a> LoadedAccount<'a> {
             },
         }
     }
-
-    pub fn lamports(&self) -> u64 {
-        match self {
-            LoadedAccount::Stored(stored_account_meta) => stored_account_meta.account_meta.lamports,
-            LoadedAccount::Cached((_, cached_account)) => match cached_account {
-                Cow::Owned(cached_account) => cached_account.account.lamports,
-                Cow::Borrowed(cached_account) => cached_account.account.lamports,
-            },
-        }
-    }
 }
 
 #[derive(Clone, Default, Debug)]
