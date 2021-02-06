819d829c41c05686dd172c8a9cc9348c053bb892
Tyera Eulberg <teulberg@gmail.com>
2021-02-05 23:39:04 -0700

    Only publish release-tag docs on beta channel (#15158)

--- docs/build.sh
+++ docs/build.sh
@@ -18,9 +18,15 @@ source ../ci/rust-version.sh
 npm run build
 echo $?
 
-# Publish only from merge commits and release tags
+eval "$(../ci/channel-info.sh)"
+
+# Publish only from merge commits and beta release tags
 if [[ -n $CI ]]; then
   if [[ -z $CI_PULL_REQUEST ]]; then
+    if [[ -n $CI_TAG ]] && [[ $CI_TAG != $BETA_CHANNEL* ]]; then
+      echo "not a beta tag"
+      exit 0
+    fi
     ./publish-docs.sh
   fi
 fi
