2849496fd489351af16b7336d339fbe0259ca88f
FairExchange <36345983+Fair-Exchange@users.noreply.github.com>
2021-02-09 16:56:43 -0700

    bankstatuscache

--- runtime/src/bank.rs
+++ runtime/src/bank.rs
@@ -110,7 +110,7 @@ impl ExecuteTimings {
 }
 
 type BankStatusCache = StatusCache<Result<()>>;
-#[frozen_abi(digest = "MUmkgPsCRrWL2HEsMEvpkWMis35kbBnaEZtrph5P6bk")]
+#[frozen_abi(digest = "23JgvLbfn8yrCoD57Et49T38HpbUmAL8eb7ePmMFmgff")]
 pub type BankSlotDelta = SlotDelta<Result<()>>;
 type TransactionAccountRefCells = Vec<Rc<RefCell<Account>>>;
 type TransactionAccountDepRefCells = Vec<(Pubkey, RefCell<Account>)>;
