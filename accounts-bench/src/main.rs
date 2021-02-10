7aea9eec57b05247252f7e3bc86c491ae048111b
Jeff Washington (jwash) <75863576+jeffwashington@users.noreply.github.com>
2021-02-10 14:38:00 -0600

    pass expected capitalization to hash calculation to improve assert msg (#15191)
    * cleanup if
    
    * pass expected capitalization to hash calculation to improve assert message
    
    * fix bank function
    
    * one more level
    
    * calculate_accounts_hash_helper
    
    * add slot to error message
    
    * success

--- accounts-bench/src/main.rs
+++ accounts-bench/src/main.rs
@@ -108,11 +108,12 @@ fn main() {
             time.stop();
             let mut time_store = Measure::start("hash using store");
             let results_store = accounts.accounts_db.update_accounts_hash_with_index_option(
-                true,
+                false,
                 false,
                 solana_sdk::clock::Slot::default(),
                 &ancestors,
                 true,
+                None,
             );
             time_store.stop();
             if results != results_store {
