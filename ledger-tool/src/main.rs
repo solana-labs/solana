2ec07a5beef93e942395790c5d1016e69c553c83
Ryo Onodera <ryoqun@gmail.com>
2021-02-12 15:24:23 +0900

    More failure codepath tracing (#15246)
    (cherry picked from commit 4e99aa5fa6676bc92ca89a83bd2b8cf5f80ce93d)

--- ledger-tool/src/main.rs
+++ ledger-tool/src/main.rs
@@ -2255,6 +2255,7 @@ fn main() {
                             point_value: Option<PointValue>,
                             old_credits_observed: Option<u64>,
                             new_credits_observed: Option<u64>,
+                            skipped_reasons: String,
                         }
                         use solana_stake_program::stake_state::InflationPointCalculationEvent;
                         let mut stake_calcuration_details: HashMap<Pubkey, CalculationDetail> =
@@ -2312,7 +2313,7 @@ fn main() {
                                         new_credits_observed,
                                     ) => {
                                         detail.old_credits_observed = Some(*old_credits_observed);
-                                        detail.new_credits_observed = Some(*new_credits_observed);
+                                        detail.new_credits_observed = *new_credits_observed;
                                     }
                                     InflationPointCalculationEvent::Delegation(
                                         delegation,
@@ -2325,6 +2326,13 @@ fn main() {
                                         if delegation.deactivation_epoch < Epoch::max_value() {
                                             detail.deactivation_epoch =
                                                 Some(delegation.deactivation_epoch);
+                                        }
+                                    }
+                                    InflationPointCalculationEvent::Skipped(skipped_reason) => {
+                                        if detail.skipped_reasons.is_empty() {
+                                            detail.skipped_reasons = format!("{:?}", skipped_reason);
+                                        } else {
+                                            detail.skipped_reasons += &format!("/{:?}", skipped_reason);
                                         }
                                     }
                                 }
