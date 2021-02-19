4e84869c8e108b7da8beac91b3e2c3010ffd3bbe
Tyera Eulberg <teulberg@gmail.com>
2021-02-18 20:14:48 -0700

    Send program deploy txs to up to 2 leaders (#15421)

--- cli/src/send_tpu.rs
+++ cli/src/send_tpu.rs
@@ -1,21 +1,38 @@
 use log::*;
 use solana_client::rpc_response::{RpcContactInfo, RpcLeaderSchedule};
+use solana_sdk::clock::NUM_CONSECUTIVE_LEADER_SLOTS;
 use std::net::{SocketAddr, UdpSocket};
 
-pub fn get_leader_tpu(
+pub fn get_leader_tpus(
     slot_index: u64,
+    num_leaders: u64,
     leader_schedule: Option<&RpcLeaderSchedule>,
     cluster_nodes: Option<&Vec<RpcContactInfo>>,
-) -> Option<SocketAddr> {
-    leader_schedule?
-        .iter()
-        .find(|(_pubkey, slots)| slots.iter().any(|slot| *slot as u64 == slot_index))
-        .and_then(|(pubkey, _)| {
-            cluster_nodes?
+) -> Vec<SocketAddr> {
+    let leaders: Vec<_> = (0..num_leaders)
+        .filter_map(|i| {
+            leader_schedule?
                 .iter()
-                .find(|contact_info| contact_info.pubkey == *pubkey)
-                .and_then(|contact_info| contact_info.tpu)
+                .find(|(_pubkey, slots)| {
+                    slots.iter().any(|slot| {
+                        *slot as u64 == (slot_index + (i * NUM_CONSECUTIVE_LEADER_SLOTS))
+                    })
+                })
+                .and_then(|(pubkey, _)| {
+                    cluster_nodes?
+                        .iter()
+                        .find(|contact_info| contact_info.pubkey == *pubkey)
+                        .and_then(|contact_info| contact_info.tpu)
+                })
         })
+        .collect();
+    let mut unique_leaders = vec![];
+    for leader in leaders.into_iter() {
+        if !unique_leaders.contains(&leader) {
+            unique_leaders.push(leader);
+        }
+    }
+    unique_leaders
 }
 
 pub fn send_transaction_tpu(
