814b2389506992b0455d9eb942854aac74633218
Michael Vines <mvines@gmail.com>
2021-02-22 22:54:35 -0800

    Limit the number of getProgramAccounts filters
    (cherry picked from commit 4b0114b9910e2e5482ffb6e6d9c3419b2dffeaf8)

--- client/src/rpc_request.rs
+++ client/src/rpc_request.rs
@@ -127,6 +127,7 @@ pub const MAX_GET_CONFIRMED_BLOCKS_RANGE: u64 = 500_000;
 pub const MAX_GET_CONFIRMED_SIGNATURES_FOR_ADDRESS2_LIMIT: usize = 1_000;
 pub const MAX_MULTIPLE_ACCOUNTS: usize = 100;
 pub const NUM_LARGEST_ACCOUNTS: usize = 20;
+pub const MAX_GET_PROGRAM_ACCOUNT_FILTERS: usize = 4;
 
 // Validators that are this number of slots behind are considered delinquent
 pub const DELINQUENT_VALIDATOR_SLOT_DISTANCE: u64 = 128;
