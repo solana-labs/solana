9987d0d573834d5f9c7ff0fbe3ceb26d3f4f0de0
Josh Hundley <josh.hundley@gmail.com>
2021-02-23 21:17:22 -0800

    explorer: add fetch polyfill and clean up / message change

--- explorer/src/pages/AccountDetailsPage.tsx
+++ explorer/src/pages/AccountDetailsPage.tsx
@@ -162,8 +162,8 @@ function DetailsSections({ pubkey, tab }: { pubkey: PublicKey; tab?: string }) {
     <>
       {flaggedAccounts.has(address) && (
         <div className="alert alert-danger alert-scam" role="alert">
-          Warning! This account has been flagged as a scam account. Please be
-          cautious sending SOL to this account.
+          Warning! This account has been flagged by the community as a scam
+          account. Please be cautious sending SOL to this account.
         </div>
       )}
       {<InfoSection account={account} />}
