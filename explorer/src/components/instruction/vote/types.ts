8d1217a7dfd255eae293f4f4dcd396fa4641e4f2
Josh Hundley <josh.hundley@gmail.com>
2021-03-01 12:13:20 -0800

    fix: expand vote details card to cover all instructions

--- explorer/src/components/instruction/vote/types.ts
+++ explorer/src/components/instruction/vote/types.ts
@@ -3,6 +3,26 @@
 import { array, number, optional, pick, string, StructType } from "superstruct";
 import { Pubkey } from "validators/pubkey";
 
+export type InitializeInfo = StructType<typeof InitializeInfo>;
+export const InitializeInfo = pick({
+  voteAccount: Pubkey,
+  rentSysvar: Pubkey,
+  clockSysvar: Pubkey,
+  node: Pubkey,
+  authorizedVoter: Pubkey,
+  authorizedWithdrawer: Pubkey,
+  commission: number(),
+});
+
+export type AuthorizeInfo = StructType<typeof AuthorizeInfo>;
+export const AuthorizeInfo = pick({
+  voteAccount: Pubkey,
+  clockSysvar: Pubkey,
+  authority: Pubkey,
+  newAuthority: Pubkey,
+  authorityType: number(),
+});
+
 export type VoteInfo = StructType<typeof VoteInfo>;
 export const VoteInfo = pick({
   clockSysvar: Pubkey,
@@ -14,4 +34,40 @@ export const VoteInfo = pick({
     slots: array(number()),
     timestamp: optional(number()),
   }),
+});
+
+export type WithdrawInfo = StructType<typeof WithdrawInfo>;
+export const WithdrawInfo = pick({
+  voteAccount: Pubkey,
+  destination: Pubkey,
+  withdrawAuthority: Pubkey,
+  lamports: number(),
+});
+
+export type UpdateValidatorInfo = StructType<typeof UpdateValidatorInfo>;
+export const UpdateValidatorInfo = pick({
+  voteAccount: Pubkey,
+  newValidatorIdentity: Pubkey,
+  withdrawAuthority: Pubkey,
+});
+
+export type UpdateCommissionInfo = StructType<typeof UpdateCommissionInfo>;
+export const UpdateCommissionInfo = pick({
+  voteAccount: Pubkey,
+  withdrawAuthority: Pubkey,
+  commission: number(),
+});
+
+export type VoteSwitchInfo = StructType<typeof VoteSwitchInfo>;
+export const VoteSwitchInfo = pick({
+  voteAccount: Pubkey,
+  slotHashesSysvar: Pubkey,
+  clockSysvar: Pubkey,
+  voteAuthority: Pubkey,
+  vote: pick({
+    hash: string(),
+    slots: array(number()),
+    timestamp: number(),
+  }),
+  hash: string(),
 });
