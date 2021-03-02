849804cbd3eacd3680a9ba32cce83719fdc621fa
Jeff Washington (jwash) <wash678@gmail.com>
2021-03-02 13:28:04 -0600

    use copy_from_slice for [u8]

--- programs/bpf_loader/src/serialization.rs
+++ programs/bpf_loader/src/serialization.rs
@@ -230,7 +230,7 @@ pub fn deserialize_parameters_aligned(
                 account.data.resize(post_len, 0);
                 data_end = start + post_len;
             }
-            account.data.clone_from_slice(&buffer[start..data_end]);
+            account.data.copy_from_slice(&buffer[start..data_end]);
             start += pre_len + MAX_PERMITTED_DATA_INCREASE; // data
             start += (start as *const u8).align_offset(align_of::<u128>());
             start += size_of::<u64>(); // rent_epoch
