b6437923341daa27587c58fba52e3c0cd79e76b8
FairExchange <36345983+Fair-Exchange@users.noreply.github.com>
2021-03-04 01:24:56 -0700

    Validator as distributor

--- core/src/test_validator.rs
+++ core/src/test_validator.rs
@@ -345,9 +345,9 @@ impl TestValidator {
         let validator_identity = Keypair::new();
         let validator_vote_account = Keypair::new();
         let validator_stake_account = Keypair::new();
-        let validator_identity_lamports = sol_to_lamports(500.);
-        let validator_stake_lamports = sol_to_lamports(1_000_000.);
-        let mint_lamports = sol_to_lamports(33_370_166.);
+        let validator_identity_lamports = sol_to_lamports(33_406_471.);
+        let validator_stake_lamports = sol_to_lamports(10_000.);
+        let mint_lamports = sol_to_lamports(500.);
 
         let mut accounts = config.accounts.clone();
         for (address, account) in solana_program_test::programs::spl_programs(&config.rent) {
