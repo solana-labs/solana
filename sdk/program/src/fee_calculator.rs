f8aad4de4edce12cf03d3c969e6adf63f316a390
FairExchange <36345983+Fair-Exchange@users.noreply.github.com>
2021-03-03 23:18:22 -0700

    SAFE fees

--- sdk/program/src/fee_calculator.rs
+++ sdk/program/src/fee_calculator.rs
@@ -86,8 +86,8 @@ pub struct FeeRateGovernor {
     pub burn_percent: u8,
 }
 
-pub const DEFAULT_TARGET_LAMPORTS_PER_SIGNATURE: u64 = 10_000;
-pub const DEFAULT_TARGET_SIGNATURES_PER_SLOT: u64 = 50 * DEFAULT_MS_PER_SLOT;
+pub const DEFAULT_TARGET_LAMPORTS_PER_SIGNATURE: u64 = 1_000_000;
+pub const DEFAULT_TARGET_SIGNATURES_PER_SLOT: u64 = 10 * DEFAULT_MS_PER_SLOT;
 
 // Percentage of tx fees to burn
 pub const DEFAULT_BURN_PERCENT: u8 = 50;
