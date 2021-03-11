c391523859f1187da4709940d04496ea8bbcb019
behzad nouri <behzadnouri@gmail.com>
2021-03-11 14:43:53 -0500

    increases min-stake to one sol

--- core/src/cluster_info.rs
+++ core/src/cluster_info.rs
@@ -118,7 +118,7 @@ const PULL_RESPONSE_MIN_SERIALIZED_SIZE: usize = 161;
 const CRDS_UNIQUE_PUBKEY_CAPACITY: usize = 4096;
 /// Minimum stake that a node should have so that its CRDS values are
 /// propagated through gossip (few types are exempted).
-const MIN_STAKE_FOR_GOSSIP: u64 = 1;
+const MIN_STAKE_FOR_GOSSIP: u64 = solana_sdk::native_token::LAMPORTS_PER_SOL;
 /// Minimum number of staked nodes for enforcing stakes in gossip.
 const MIN_NUM_STAKED_NODES: usize = 500;
 
