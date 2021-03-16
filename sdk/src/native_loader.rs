c09ea2c314b83a42eff304f0d2f674f1c5577ed4
Jeff Washington (jwash) <75863576+jeffwashington@users.noreply.github.com>
2021-03-15 19:27:17 -0500

    More AccountSharedData construction (#15844)
    * one more AccountSharedData construction
    
    * one more construct

--- sdk/src/native_loader.rs
+++ sdk/src/native_loader.rs
@@ -1,14 +1,14 @@
-use crate::account::AccountSharedData;
+use crate::account::{Account, AccountSharedData};
 
 crate::declare_id!("NativeLoader1111111111111111111111111111111");
 
 /// Create an executable account with the given shared object name.
 pub fn create_loadable_account(name: &str, lamports: u64) -> AccountSharedData {
-    AccountSharedData {
+    AccountSharedData::from(Account {
         lamports,
         owner: id(),
         data: name.as_bytes().to_vec(),
         executable: true,
         rent_epoch: 0,
-    }
+    })
 }
