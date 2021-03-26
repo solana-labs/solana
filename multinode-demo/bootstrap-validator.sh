4e262669dd072e941f1db9a5828b9d32992c4c4c
sakridge <sakridge@gmail.com>
2021-03-25 18:54:51 -0700

    Skip leader slots until a vote lands (#15607)
    (cherry picked from commit b99ae8f3349db7c8cdfaf3da8194dc1d7a405d7f)

--- multinode-demo/bootstrap-validator.sh
+++ multinode-demo/bootstrap-validator.sh
@@ -105,6 +105,7 @@ args+=(
   --vote-account "$vote_account"
   --rpc-faucet-address 127.0.0.1:9900
   --no-poh-speed-test
+  --no-wait-for-vote-to-start-leader
 )
 default_arg --gossip-port 8001
 default_arg --log -
