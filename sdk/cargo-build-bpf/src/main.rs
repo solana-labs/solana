1359bceb5d6c73a7b4852156d4c8676034e2e446
Dmitri Makarov <dmakarov@users.noreply.github.com>
2021-04-04 15:04:22 +0200

    Bump bpf-tools to version v1.5 (#16331)
    The new version of bpf-tools eliminates the separate
    rust-bpf-sysroot. The Rust standard libraries for the BPF target are
    built in tree when the compiler is built.  The standard libraries code
    is slightly more optimized and some reduction of compute budget can be
    expected with this version of bpf-tools.

--- sdk/cargo-build-bpf/src/main.rs
+++ sdk/cargo-build-bpf/src/main.rs
@@ -260,7 +260,7 @@ fn build_bpf_package(config: &Config, target_directory: &Path, package: &cargo_m
     install_if_missing(
         &config,
         "bpf-tools",
-        "v1.4",
+        "v1.5",
         "https://github.com/solana-labs/bpf-tools/releases/download",
         &PathBuf::from(bpf_tools_filename),
     )
