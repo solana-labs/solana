8350430028240dc1363fd63fee308f8a2e0a4351
Christian Drappi <christiandrappi@gmail.com>
2021-04-04 12:10:11 -0400

    toy around with #cfg(...) options

--- measure/src/lib.rs
+++ measure/src/lib.rs
@@ -2,10 +2,11 @@
 pub mod measure;
 pub mod thread_mem_usage;
 
-#[cfg(unix)]
+// #[cfg(all(unix, not(all(target_os = "macos", target_arch="aarch64-app-darwin"))))]
+// #[cfg(unix)]
 extern crate jemallocator;
 
 #[cfg(not(feature = "no-jemalloc"))]
-#[cfg(unix)]
+#[cfg(all(unix, not(all(target_os = "macos", target_arch="aarch64-apple-darwin"))))]
 #[global_allocator]
 static ALLOC: jemallocator::Jemalloc = jemallocator::Jemalloc;
