0755aa9f2331a2fad1f0750cb33e347ccb69fe5f
Dmitri Makarov <dmakarov@users.noreply.github.com>
2021-04-13 09:20:18 +0200

    Add llvm feature option to compile for Solana BPF target (#16495)

--- sdk/bpf/scripts/install.sh
+++ sdk/bpf/scripts/install.sh
@@ -92,7 +92,7 @@ if [[ ! -e criterion-$version.md || ! -e criterion ]]; then
 fi
 
 # Install Rust-BPF
-version=v1.5
+version=v1.6
 if [[ ! -e bpf-tools-$version.md || ! -e bpf-tools ]]; then
   (
     set -e
