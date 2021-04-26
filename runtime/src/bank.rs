cb44161a564147edd686c741e32e1c081b8b49ba
Jeff Washington (jwash) <wash678@gmail.com>
2021-04-26 15:56:38 -0500

    bank.withdraw uses checked_sub_lamports

--- runtime/src/bank.rs
+++ runtime/src/bank.rs
@@ -4030,12 +4030,9 @@ impl Bank {
                     _ => 0,
                 };
 
-                lamports
-                    .checked_add(min_balance)
-                    .filter(|required_balance| *required_balance <= account.lamports())
-                    .ok_or(TransactionError::InsufficientFundsForFee)?;
-
-                account.lamports -= lamports;
+                account
+                    .checked_sub_lamports(lamports)
+                    .or_else(|_| Err(TransactionError::InsufficientFundsForFee))?;
                 self.store_account(pubkey, &account);
 
                 Ok(())
