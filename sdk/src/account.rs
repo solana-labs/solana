ef272550b022f9e9bb810619c7ebc7917183d047
Jeff Washington (jwash) <wash678@gmail.com>
2021-04-27 10:33:44 -0500

    accountshareddata.executable is private

--- sdk/src/account.rs
+++ sdk/src/account.rs
@@ -37,7 +37,7 @@ pub struct AccountSharedData {
     /// the program that owns this account. If executable, the program that loads this account.
     owner: Pubkey,
     /// this account's data contains a loaded program (and is now read-only)
-    pub executable: bool,
+    executable: bool,
     /// the epoch at which this account will next owe rent
     pub rent_epoch: Epoch,
 }
