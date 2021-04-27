fa74f7da5ed43fb009e7ba8771dd5af9a5d3d786
Jack May <jack@solana.com>
2021-04-27 13:36:42 -0700

    Retain alloc'd and updated data in cpi (#16850)
    (cherry picked from commit 9b3a59f0302ed324336aba7d0796913260aa0953)
    
    # Conflicts:
    #	programs/bpf_loader/src/syscalls.rs
    #	sdk/src/feature_set.rs

--- sdk/src/feature_set.rs
+++ sdk/src/feature_set.rs
@@ -126,6 +126,17 @@ pub mod check_duplicates_by_hash {
 pub mod enforce_aligned_host_addrs {
     solana_sdk::declare_id!("6Qob9Z4RwGdf599FDVCqsjuKjR8ZFR3oVs2ByRLWBsua");
 }
+<<<<<<< HEAD
+=======
+
+pub mod set_upgrade_authority_via_cpi_enabled {
+    solana_sdk::declare_id!("GQdjCCptpGECG7QfE35hKTAopB1umGoSrdKfax2VmZWy");
+}
+>>>>>>> 9b3a59f03... Retain alloc'd and updated data in cpi (#16850)
+
+pub mod update_data_on_realloc {
+    solana_sdk::declare_id!("BkPcYCrwHXBoTsv9vMhiRF9gteZmDj3Uwisz9CDjoMKp");
+}
 
 lazy_static! {
     /// Map of feature identifiers to user-visible description
@@ -159,6 +170,11 @@ lazy_static! {
         (sysvar_via_syscall::id(), "provide sysvars via syscalls"),
         (check_duplicates_by_hash::id(), "use transaction message hash for duplicate check"),
         (enforce_aligned_host_addrs::id(), "enforce aligned host addresses"),
+<<<<<<< HEAD
+=======
+        (set_upgrade_authority_via_cpi_enabled::id(), "set upgrade authority instruction via cpi calls for upgradable programs"),
+        (update_data_on_realloc::id(), "Retain updated data values modified after realloc via CPI"),
+>>>>>>> 9b3a59f03... Retain alloc'd and updated data in cpi (#16850)
         /*************** ADD NEW FEATURES HERE ***************/
     ]
     .iter()
