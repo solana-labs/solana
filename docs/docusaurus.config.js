e8e254f119ae2ac205b86f683ec6e09b5773cd7e
Justin Starry <justin@solana.com>
2021-04-29 22:07:50 +0800

    Run lint:fix on docs

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -1,135 +1,135 @@
-const math = require('remark-math');
-const katex = require('rehype-katex');
+const math = require("remark-math");
+const katex = require("rehype-katex");
 module.exports = {
-    title: "Solana Docs",
-    tagline:
+  title: "Solana Docs",
+  tagline:
     "Solana is an open source project implementing a new, high-performance, permissionless blockchain.",
-    url: "https://docs.solana.com",
-    baseUrl: "/",
-    favicon: "img/favicon.ico",
-    organizationName: "solana-labs", // Usually your GitHub org/user name.
-	projectName: "solana", // Usually your repo name.
-	onBrokenLinks: 'throw',
-    stylesheets: [
-	{
-	    href: '/katex/katex.min.css',
-	    type: 'text/css',
-	    integrity:
-            'sha384-AfEj0r4/OFrOo5t7NnNe46zW/tFgW6x/bCJG8FqQCEo3+Aro6EYUG4+cU+KJWu/X',
-	    crossorigin: 'anonymous',
-	},
-    ],
-    themeConfig: {
-	navbar: {
-	    logo: {
-		alt: "Solana Logo",
-		src: "img/logo-horizontal.svg",
-		srcDark: "img/logo-horizontal-dark.svg",
-	    },
-	    items: [
-		{
-		    href: "https://spl.solana.com",
-		    label: "Program Library »",
-		    position: "left",
-		},
-		{
-		    to: "developing/programming-model/overview",
-		    label: "Develop",
-		    position: "left",
-		},
-		{
-		    to: "running-validator",
-		    label: "Validate",
-		    position: "left",
-		},
-		{
-		    to: "integrations/exchange",
-		    label: "Integrate",
-		    position: "left",
-		},
-		{
-		    to: "cluster/overview",
-		    label: "Learn",
-		    position: "left",
-		},
-		{
-		    href: "https://discordapp.com/invite/pquxPsq",
-		    label: "Chat",
-		    position: "right",
-		},
-		{
-		    href: "https://github.com/solana-labs/solana",
-		    label: "GitHub",
-		    position: "right",
-		},
-	    ],
-	},
-	algolia: {
-	    // This API key is "search-only" and safe to be published
-	    apiKey: "d58e0d68c875346d52645d68b13f3ac0",
-	    indexName: "solana",
-	},
-	footer: {
-	    style: "dark",
-	    links: [
-		{
-		    title: "Docs",
-		    items: [
-			{
-			    label: "Introduction",
-			    to: "introduction",
-			},
-			{
-			    label: "Tour de SOL",
-			    to: "tour-de-sol",
-			},
-		    ],
-		},
-		{
-		    title: "Community",
-		    items: [
-			{
-			    label: "Discord",
-			    href: "https://discordapp.com/invite/pquxPsq",
-			},
-			{
-			    label: "Twitter",
-			    href: "https://twitter.com/solana",
-			},
-			{
-			    label: "Forums",
-			    href: "https://forums.solana.com",
-			},
-		    ],
-		},
-		{
-		    title: "More",
-		    items: [
-			{
-			    label: "GitHub",
-			    href: "https://github.com/solana-labs/solana",
-			},
-		    ],
-		},
-	    ],
-	    copyright: `Copyright © ${new Date().getFullYear()} Solana Foundation`,
-	},
+  url: "https://docs.solana.com",
+  baseUrl: "/",
+  favicon: "img/favicon.ico",
+  organizationName: "solana-labs", // Usually your GitHub org/user name.
+  projectName: "solana", // Usually your repo name.
+  onBrokenLinks: "throw",
+  stylesheets: [
+    {
+      href: "/katex/katex.min.css",
+      type: "text/css",
+      integrity:
+        "sha384-AfEj0r4/OFrOo5t7NnNe46zW/tFgW6x/bCJG8FqQCEo3+Aro6EYUG4+cU+KJWu/X",
+      crossorigin: "anonymous",
     },
-    presets: [
-	[
-	    "@docusaurus/preset-classic",
-	    {
-		docs: {
-		    path: "src",
-		    routeBasePath: "/",
-		    sidebarPath: require.resolve("./sidebars.js"),
-		    remarkPlugins: [math],
-		    rehypePlugins: [katex],
-		},
-		theme: {
-		    customCss: require.resolve("./src/css/custom.css"),
-		},
-	    },
-	],
+  ],
+  themeConfig: {
+    navbar: {
+      logo: {
+        alt: "Solana Logo",
+        src: "img/logo-horizontal.svg",
+        srcDark: "img/logo-horizontal-dark.svg",
+      },
+      items: [
+        {
+          href: "https://spl.solana.com",
+          label: "Program Library »",
+          position: "left",
+        },
+        {
+          to: "developing/programming-model/overview",
+          label: "Develop",
+          position: "left",
+        },
+        {
+          to: "running-validator",
+          label: "Validate",
+          position: "left",
+        },
+        {
+          to: "integrations/exchange",
+          label: "Integrate",
+          position: "left",
+        },
+        {
+          to: "cluster/overview",
+          label: "Learn",
+          position: "left",
+        },
+        {
+          href: "https://discordapp.com/invite/pquxPsq",
+          label: "Chat",
+          position: "right",
+        },
+        {
+          href: "https://github.com/solana-labs/solana",
+          label: "GitHub",
+          position: "right",
+        },
+      ],
+    },
+    algolia: {
+      // This API key is "search-only" and safe to be published
+      apiKey: "d58e0d68c875346d52645d68b13f3ac0",
+      indexName: "solana",
+    },
+    footer: {
+      style: "dark",
+      links: [
+        {
+          title: "Docs",
+          items: [
+            {
+              label: "Introduction",
+              to: "introduction",
+            },
+            {
+              label: "Tour de SOL",
+              to: "tour-de-sol",
+            },
+          ],
+        },
+        {
+          title: "Community",
+          items: [
+            {
+              label: "Discord",
+              href: "https://discordapp.com/invite/pquxPsq",
+            },
+            {
+              label: "Twitter",
+              href: "https://twitter.com/solana",
+            },
+            {
+              label: "Forums",
+              href: "https://forums.solana.com",
+            },
+          ],
+        },
+        {
+          title: "More",
+          items: [
+            {
+              label: "GitHub",
+              href: "https://github.com/solana-labs/solana",
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} Solana Foundation`,
+    },
+  },
+  presets: [
+    [
+      "@docusaurus/preset-classic",
+      {
+        docs: {
+          path: "src",
+          routeBasePath: "/",
+          sidebarPath: require.resolve("./sidebars.js"),
+          remarkPlugins: [math],
+          rehypePlugins: [katex],
+        },
+        theme: {
+          customCss: require.resolve("./src/css/custom.css"),
+        },
+      },
     ],
+  ],
 };
