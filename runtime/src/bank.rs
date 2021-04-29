93883f4b4350d51d41ff819f21aa4133841aa5d9
Jeff Washington (jwash) <wash678@gmail.com>
2021-04-29 15:17:35 -0500

    update message text

--- runtime/src/bank.rs
+++ runtime/src/bank.rs
@@ -3378,11 +3378,11 @@ impl Bank {
                         // overflow adding lamports
                         self.capitalization.fetch_sub(rent_to_be_paid, Relaxed);
                         error!(
-                            "Incinerated {} rent lamports instead of sending to {}",
+                            "Burned {} rent lamports instead of sending to {}",
                             rent_to_be_paid, pubkey
                         );
                         inc_new_counter_error!(
-                            "bank-incinerated_rent_lamports",
+                            "bank-burned_rent_lamports",
                             rent_to_be_paid as usize
                         );
                     }
