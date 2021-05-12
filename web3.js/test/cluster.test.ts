d9d46751f217046e496a1f4905e3164c90e9d538
Josh Hundley <josh.hundley@gmail.com>
2021-05-12 08:33:33 -0700

    fix: cluster test

--- web3.js/test/cluster.test.ts
+++ web3.js/test/cluster.test.ts
@@ -10,9 +10,13 @@ describe('Cluster Util', () => {
   });
 
   it('devnet', () => {
-    expect(clusterApiUrl()).to.eq('https://devnet.solana.com');
-    expect(clusterApiUrl('devnet')).to.eq('https://devnet.solana.com');
-    expect(clusterApiUrl('devnet', true)).to.eq('https://devnet.solana.com');
-    expect(clusterApiUrl('devnet', false)).to.eq('http://devnet.solana.com');
+    expect(clusterApiUrl()).to.eq('https://api.devnet.solana.com');
+    expect(clusterApiUrl('devnet')).to.eq('https://api.devnet.solana.com');
+    expect(clusterApiUrl('devnet', true)).to.eq(
+      'https://api.devnet.solana.com',
+    );
+    expect(clusterApiUrl('devnet', false)).to.eq(
+      'http://api.devnet.solana.com',
+    );
   });
 });
