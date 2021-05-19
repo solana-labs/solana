c58af22238f9b8d455b3b961b48aa0d5609c5d8e
Jeff Washington (jwash) <wash678@gmail.com>
2021-05-19 13:55:50 -0500

    simplify logic

--- runtime/src/accounts_db.rs
+++ runtime/src/accounts_db.rs
@@ -1060,17 +1060,14 @@ impl ShrinkStats {
         // last is initialized to 0 by ::default()
         // thus, the first 'report' call would always log.
         // Instead, the first call now initialializes 'last_report' to now.
-        let first_call = last == 0;
-        let time_elapsed = !first_call && now.saturating_sub(last) > 1000;
-        let should_report = (first_call || time_elapsed) && {
-            let we_exchanged =
-                self.last_report
-                    .compare_exchange(last, now, Ordering::Relaxed, Ordering::Relaxed)
-                    == Ok(last);
-            we_exchanged && !first_call
-        };
+        let is_first_call = last == 0;
+        let should_report = now.saturating_sub(last) > 1000
+            && self
+                .last_report
+                .compare_exchange(last, now, Ordering::Relaxed, Ordering::Relaxed)
+                == Ok(last);
 
-        if should_report {
+        if !is_first_call && should_report {
             datapoint_info!(
                 "shrink_stats",
                 (
