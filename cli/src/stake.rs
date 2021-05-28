cdce6a0e44bcb7d9a692c122d6a50375ca30fe00
Trent Nelson <trent@solana.com>
2021-05-28 01:42:55 -0600

    Improve missing default filepath signer error messaging
    (cherry picked from commit 06a926f2f496aa82632a494563419375b0f248a4)

--- cli/src/stake.rs
+++ cli/src/stake.rs
@@ -2117,10 +2117,7 @@ mod tests {
         let default_keypair = Keypair::new();
         let (default_keypair_file, mut tmp_file) = make_tmp_file();
         write_keypair(&default_keypair, tmp_file.as_file_mut()).unwrap();
-        let default_signer = DefaultSigner {
-            path: default_keypair_file.clone(),
-            arg_name: String::new(),
-        };
+        let default_signer = DefaultSigner::new("", &default_keypair_file);
         let (keypair_file, mut tmp_file) = make_tmp_file();
         let stake_account_keypair = Keypair::new();
         write_keypair(&stake_account_keypair, tmp_file.as_file_mut()).unwrap();
