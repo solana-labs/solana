0de91b4ebebac3899f076b3b78f99cff69ab57fc
Tao Zhu <82401714+taozhu-chicago@users.noreply.github.com>
2021-06-01 09:16:17 -0500

    Cost Model to limit transactions which are not parallelizeable (#16694)
    * * Add following to banking_stage:
      1. CostModel as immutable ref shared between threads, to provide estimated cost for transactions.
      2. CostTracker which is shared between threads, tracks transaction costs for each block.
    
    * replace hard coded program ID with id() calls
    
    * Add Account Access Cost as part of TransactionCost. Account Access cost are weighted differently between read and write, signed and non-signed.
    
    * Establish instruction_execution_cost_table, add function to update or insert instruction cost, unit tested. It is read-only for now; it allows Replay to insert realtime instruction execution costs to the table.
    
    * add test for cost_tracker atomically try_add operation, serves as safety guard for future changes
    
    * check cost against local copy of cost_tracker, return transactions that would exceed limit as unprocessed transaction to be buffered; only apply bank processed transactions cost to tracker;
    
    * bencher to new banking_stage with max cost limit to allow cost model being hit consistently during bench iterations

--- core/benches/banking_stage.rs
+++ core/benches/banking_stage.rs
@@ -7,7 +7,10 @@ use crossbeam_channel::unbounded;
 use log::*;
 use rand::{thread_rng, Rng};
 use rayon::prelude::*;
-use solana_core::banking_stage::{BankingStage, BankingStageStats};
+use solana_core::banking_stage::{create_test_recorder, BankingStage, BankingStageStats};
+use solana_core::cost_model::CostModel;
+use solana_core::cost_tracker::CostTracker;
+use solana_core::poh_recorder::WorkingBankEntry;
 use solana_gossip::cluster_info::ClusterInfo;
 use solana_gossip::cluster_info::Node;
 use solana_ledger::blockstore_processor::process_entries;
@@ -32,7 +35,7 @@ use solana_sdk::transaction::Transaction;
 use std::collections::VecDeque;
 use std::sync::atomic::Ordering;
 use std::sync::mpsc::Receiver;
-use std::sync::Arc;
+use std::sync::{Arc, Mutex};
 use std::time::{Duration, Instant};
 use test::Bencher;
 
@@ -91,6 +94,8 @@ fn bench_consume_buffered(bencher: &mut Bencher) {
                 None::<Box<dyn Fn()>>,
                 &BankingStageStats::default(),
                 &recorder,
+                &Arc::new(CostModel::default()),
+                &Arc::new(Mutex::new(CostTracker::new(std::u32::MAX, std::u32::MAX))),
             );
         });
 
@@ -204,13 +209,15 @@ fn bench_banking(bencher: &mut Bencher, tx_type: TransactionType) {
         let cluster_info = ClusterInfo::new_with_invalid_keypair(Node::new_localhost().info);
         let cluster_info = Arc::new(cluster_info);
         let (s, _r) = unbounded();
-        let _banking_stage = BankingStage::new(
+        let _banking_stage = BankingStage::new_with_cost_limit(
             &cluster_info,
             &poh_recorder,
             verified_receiver,
             vote_receiver,
             None,
             s,
+            std::u32::MAX,
+            std::u32::MAX,
         );
         poh_recorder.lock().unwrap().set_bank(&bank);
 
