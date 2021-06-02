e6f4243cbcb1b4f4babda1a92fde7d13d311664f
Sidhartha Chatterjee <me@sidharthachatterjee.com>
2021-06-02 21:38:34 +0530

    Move abbreviatedNumber to utils

--- explorer/src/utils/index.tsx
+++ explorer/src/utils/index.tsx
@@ -143,3 +143,11 @@ export function formatDollarValue(
 export function normalizePercentage(value: number, decimals: number): string {
   return value ? value.toFixed(decimals) + "%" : "";
 }
+
+export const abbreviatedNumber = (value: number, fixed = 1) => {
+  if (value < 1e3) return value;
+  if (value >= 1e3 && value < 1e6) return +(value / 1e3).toFixed(fixed) + "K";
+  if (value >= 1e6 && value < 1e9) return +(value / 1e6).toFixed(fixed) + "M";
+  if (value >= 1e9 && value < 1e12) return +(value / 1e9).toFixed(fixed) + "B";
+  if (value >= 1e12) return +(value / 1e12).toFixed(fixed) + "T";
+};
