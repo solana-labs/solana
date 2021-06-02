7db7f429677e1598229c5d99240c11b79e880d27
Jeff Washington (jwash) <wash678@gmail.com>
2021-06-02 13:25:29 -0500

    add tests

--- runtime/src/pubkey_bins.rs
+++ runtime/src/pubkey_bins.rs
@@ -1,6 +1,7 @@
 use solana_sdk::pubkey::Pubkey;
 
 pub(crate) struct PubkeyBinCalculator16 {
+    // how many bits from the first 2 bytes to shift away to ignore when calculating bin
     shift_bits: u32,
 }
 
@@ -21,13 +22,11 @@ impl PubkeyBinCalculator16 {
         assert!(bins <= max_plus_1);
         assert!(bins.is_power_of_two());
         let bits = Self::log_2(bins as u32);
-        // how many bits to shift away to ignore when calculating bin
         Self {
             shift_bits: MAX_BITS - bits,
         }
     }
 
-    // this function is coupled to get_shift_bits_from_bins
     pub fn bin_from_pubkey(&self, pubkey: &Pubkey) -> usize {
         let as_ref = pubkey.as_ref();
         ((as_ref[0] as usize * 256 + as_ref[1] as usize) as usize) >> self.shift_bits
@@ -44,6 +43,84 @@ pub mod tests {
         assert_eq!(PubkeyBinCalculator16::num_bits::<u32>(), 32);
         for i in 0..32 {
             assert_eq!(PubkeyBinCalculator16::log_2(2u32.pow(i)), i);
+        }
+    }
+
+    #[test]
+    fn test_pubkey_bins() {
+        for i in 0..=16 {
+            let bins = 2u32.pow(i);
+            let calc = PubkeyBinCalculator16::new(bins as usize);
+            assert_eq!(calc.shift_bits, 16 - i, "i: {}", i);
+        }
+    }
+
+    #[test]
+    fn test_pubkey_bins_pubkeys() {
+        let mut pk = Pubkey::new(&[0; 32]);
+        for i in 0..=8 {
+            let bins = 2usize.pow(i);
+            let calc = PubkeyBinCalculator16::new(bins);
+
+            let shift_bits = calc.shift_bits - 8; // we are only dealing with first byte
+
+            pk.as_mut()[0] = 0;
+            assert_eq!(0, calc.bin_from_pubkey(&pk));
+            pk.as_mut()[0] = 0xff;
+            assert_eq!(bins - 1, calc.bin_from_pubkey(&pk));
+
+            for bin in 0..bins {
+                pk.as_mut()[0] = (bin << shift_bits) as u8;
+                assert_eq!(
+                    bin,
+                    calc.bin_from_pubkey(&pk),
+                    "bin: {}/{}, bits: {}, val: {}",
+                    bin,
+                    bins,
+                    shift_bits,
+                    pk.as_ref()[0]
+                );
+                if bin > 0 {
+                    pk.as_mut()[0] = ((bin << shift_bits) - 1) as u8;
+                    assert_eq!(bin - 1, calc.bin_from_pubkey(&pk));
+                }
+            }
+        }
+
+        for i in 9..=16 {
+            let mut pk = Pubkey::new(&[0; 32]);
+            let bins = 2usize.pow(i);
+            let calc = PubkeyBinCalculator16::new(bins);
+
+            let shift_bits = calc.shift_bits;
+
+            pk.as_mut()[1] = 0;
+            assert_eq!(0, calc.bin_from_pubkey(&pk));
+            pk.as_mut()[0] = 0xff;
+            pk.as_mut()[1] = 0xff;
+            assert_eq!(bins - 1, calc.bin_from_pubkey(&pk));
+
+            let mut pk = Pubkey::new(&[0; 32]);
+            for bin in 0..bins {
+                let mut target = (bin << shift_bits) as u16;
+                pk.as_mut()[0] = (target / 256) as u8;
+                pk.as_mut()[1] = (target % 256) as u8;
+                assert_eq!(
+                    bin,
+                    calc.bin_from_pubkey(&pk),
+                    "bin: {}/{}, bits: {}, val: {}",
+                    bin,
+                    bins,
+                    shift_bits,
+                    pk.as_ref()[0]
+                );
+                if bin > 0 {
+                    target -= 1;
+                    pk.as_mut()[0] = (target / 256) as u8;
+                    pk.as_mut()[1] = (target % 256) as u8;
+                    assert_eq!(bin - 1, calc.bin_from_pubkey(&pk));
+                }
+            }
         }
     }
 
