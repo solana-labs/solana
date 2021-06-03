e8eb03124b84550955568903ee46269b0ce1b744
Anatoly Yakovenko <anatoly@solana.com>
2021-06-03 09:06:18 -0400

    updates

---
+++ runtime/src/bucket_map.rs
@@ -0,0 +1,168 @@
+//! BucketMap is a mostly contention free concurrent map backed by MmapMut
+
+use crate::accounts_db::AccountInfo;
+use crate::data_bucket::DataBucket;
+use std::path::PathBuf;
+use core::sync::atomic::AtomicU64;
+use core::sync::atomic::Ordering;
+use core::sync::atomic::AtomicBool;
+use solana_sdk::pubkey::Pubkey;
+use solana_sdk::slot_history::Slot;
+use std::collections::HashMap;
+use std::convert::TryInto;
+use std::sync::Arc;
+use std::sync::RwLock;
+
+pub struct BucketMap {
+    // power of 2 size masks point to the same bucket
+    masks: RwLock<Vec<Arc<RwLock<Bucket>>>>,
+    drives: Arc<Vec<PathBuf>>,
+    key_locks: RwLock<HashMap<Pubkey, Arc<SpinLock>>>,
+}
+
+struct SpinLock {
+    lock: AtomicBool,
+}
+
+impl SpinLock {
+    fn lock(&self) {
+        while Ok(false)
+            != self
+                .lock
+                .compare_exchange(false, true, Ordering::Relaxed, Ordering::Relaxed)
+        {}
+    }
+    fn unlock(&self) {
+        self.lock.store(false, Ordering::Relaxed)
+    }
+}
+
+impl BucketMap {
+    pub fn scan<T>(&self, accum: &mut T, scanfn: fn(&Bucket, accum: &mut T)) {
+        let masks = self.masks.write().unwrap();
+        for i in 0..masks.len() {
+            if i > 1 && masks[i].ptr_eq(masks[i - 1]) {
+                continue;
+            }
+            scanfn(&masks[i].read().unwrap(), accum)
+        }
+    }
+
+    pub fn read(&self, pubkey: &Pubkey) -> Option<&SlotSlice> {
+        let ix = self.bucket_ix(pubkey);
+        self.check_lock(pubkey);
+        let spinlock = self.key_locks.read().unwrap().get(pubkey).unwrap();
+        spinlock.lock();
+        let rv = self.masks[ix].bucket.read(pubkey);
+        spinlock.unlock();
+        rv
+    }
+
+    pub fn delete(&self, pubkey: &Pubkey) {
+        let ix = self.bucket_ix(pubkey);
+        self.check_lock(pubkey);
+        let spinlock = self.key_locks.read().unwrap().get(pubkey).unwrap();
+        spinlock.lock();
+        self.masks[ix].bucket.delete(pubkey);
+        spinlock.unlock();
+    }
+
+    //updatefn must be re-entrant
+    pub fn update(
+        &self,
+        pubkey: &Pubkey,
+        updatefn: fn(Option<&SlotSlice>) -> &SlotSlice,
+    ) -> Result<(), BucketMapError> {
+        loop {
+            let e = self.try_update(pubkey, updatefn);
+            if let Err(BucketMapError::IndexNoSpace) = e {
+                let ix = self.bucket_ix(pubkey);
+                let mut masks = self.masks.write();
+                let new_bucket = masks[ix].split(ix);
+                let mut new_masks = Vec::new();
+                for i in 0..masks.len() {
+                    new_masks[i] = masks[i];
+                    new_masks[i + 1] = masks[i];
+                    if i as u64 == ix {
+                        new_masks[i + 1] = new_bucket;
+                    }
+                }
+                *masks = new_masks;
+                continue;
+            }
+            return e;
+        }
+    }
+
+    fn try_update(
+        &self,
+        pubkey: &Pubkey,
+        updatefn: fn(Option<&SlotSlice>) -> &SlotSlice,
+    ) -> Result<(), BucketMapError> {
+        let ix = self.bucket_ix(pubkey);
+        self.check_lock(pubkey);
+        let spinlock = self.key_locks.read().unwrap().get(pubkey).unwrap();
+        spinlock.lock();
+        let current = self.masks[ix].bucket.read(pubkey);
+        if current.is_none() {
+            self.new_key_locked(pubkey);
+        }
+        let new = updatefn(current);
+        let mut rv = Ok(());
+        loop {
+            rv = self.masks[ix].bucket.read().unrwap().try_write(pubkey, new);
+            if let Err(BucketMapError::DataNoSpace(ix)) = rv {
+                self.masks[ix].bucket.write().unwrap().grow(new.len());
+                continue;
+            }
+            break;
+        }
+        spinlock.unlock();
+        return rv;
+    }
+
+    fn check_lock(&self, pubkey: &Pubkey) {
+        let key_locks = self.key_locks.read().unwrap();
+        if key_locks.get(pubkey).is_none() {
+            drop(key_locks);
+            let key_locks = self.key_locks.write().unwrap();
+            key_locks.insert(pubkey, Arc::new(SpinLock::new()));
+        }
+    }
+    fn bucket_ix(&self, pubkey: &Pubkey) -> u64 {
+        let location = read_be_u64(pubkey.as_ref());
+        let bits = (self.masks.len() as f64).log2() as u64;
+        location >> (64 - bits)
+    }
+}
+
+struct Bucket {
+    version: AtomicU64,
+    //index
+    index: DataBucket,
+    //data buckets to store SlotSlice up to a power of 2 in len
+    data: Vec<Arc<DataBucket>>,
+}
+
+#[repr(C)]
+struct IndexEntry {
+    key: Pubkey,
+    data_bucket: u64,
+    data_location: u64,
+    //if the bucket doubled, the index can be recomputed
+    create_bucket_capacity: u64,
+    num_slots: u64,
+}
+
+pub type SlotInfo = (Slot, AccountInfo);
+
+enum BucketMapError {
+    DataNoSpace(u64),
+    IndexNoSpace,
+}
+
+pub type SlotSlice = [SlotInfo];
+
+fn read_be_u64(input: &[u8]) -> u64 {
+    u64::from_be_bytes(input[0..std::mem::size_of::<u64>()].try_into().unwrap())
+}
---
+++ runtime/src/data_bucket.rs
@@ -0,0 +1,223 @@
+use log::*;
+use memmap2::MmapMut;
+use rand::{thread_rng, Rng};
+use rayon::iter::IntoParallelIterator;
+use rayon::iter::ParallelIterator;
+use std::fs::{remove_file, OpenOptions};
+use std::io::Seek;
+use std::io::SeekFrom;
+use std::io::Write;
+use std::path::PathBuf;
+use std::sync::atomic::{AtomicU64, Ordering};
+use std::sync::Arc;
+
+const DEFAULT_CAPACITY: u64 = 16;
+
+#[repr(C)]
+struct Header {
+    lock: AtomicU64,
+}
+
+impl Header {
+    fn try_lock(&self, uid: u64) -> bool {
+        Ok(0)
+            == self
+                .lock
+                .compare_exchange(0, uid, Ordering::Relaxed, Ordering::Relaxed)
+    }
+    fn unlock(&self, uid: u64) -> bool {
+        Ok(uid)
+            == self
+                .lock
+                .compare_exchange(uid, 0, Ordering::Relaxed, Ordering::Relaxed)
+    }
+    fn uid(&self) -> u64 {
+        self.lock.load(Ordering::Relaxed)
+    }
+}
+
+pub struct DataBucket {
+    drives: Arc<Vec<PathBuf>>,
+    path: PathBuf,
+    mmap: MmapMut,
+    pub cell_size: usize,
+    pub capacity: u64,
+    pub used: AtomicU64,
+}
+
+#[derive(Debug)]
+pub enum DataBucketError {
+    AlreadyAllocated,
+}
+
+impl Drop for DataBucket {
+    fn drop(&mut self) {
+        if let Err(e) = remove_file(&self.path) {
+            error!("failed to remove {:?}: {:?}", &self.path, e);
+        }
+    }
+}
+
+impl DataBucket {
+    pub fn new(drives: Arc<Vec<PathBuf>>, elem_size: usize) -> Self {
+        let cell_size = elem_size + std::mem::size_of::<Header>();
+        let (mmap, path) = Self::new_map(&drives, cell_size, DEFAULT_CAPACITY);
+        Self {
+            path,
+            mmap,
+            drives,
+            cell_size,
+            used: AtomicU64::new(0),
+            capacity: DEFAULT_CAPACITY,
+        }
+    }
+
+    pub fn uid(&self, ix: u64) -> u64 {
+        if ix >= self.capacity {
+            panic!("bad index size");
+        }
+        let ix = ix as usize * self.cell_size;
+        let hdr_slice = &self.mmap[ix..ix + std::mem::size_of::<Header>()];
+        unsafe {
+            let hdr = hdr_slice.as_ptr() as *const Header;
+            return hdr.as_ref().unwrap().uid();
+        }
+    }
+
+    pub fn allocate(&self, ix: u64, uid: u64) -> Result<(), DataBucketError> {
+        if ix >= self.capacity {
+            panic!("bad index size");
+        }
+        let mut e = Err(DataBucketError::AlreadyAllocated);
+        let ix = ix as usize * self.cell_size;
+        let hdr_slice = &self.mmap[ix..ix + std::mem::size_of::<Header>()];
+        unsafe {
+            let hdr = hdr_slice.as_ptr() as *const Header;
+            if hdr.as_ref().unwrap().try_lock(uid) {
+                e = Ok(());
+                self.used.fetch_add(1, Ordering::Relaxed);
+            }
+        };
+        e
+    }
+
+    pub fn free(&self, ix: u64, uid: u64) {
+        if ix >= self.capacity {
+            panic!("bad index size");
+        }
+        let ix = ix as usize * self.cell_size;
+        let hdr_slice = &self.mmap[ix..ix + std::mem::size_of::<Header>()];
+        unsafe {
+            let hdr = hdr_slice.as_ptr() as *const Header;
+            if hdr.as_ref().unwrap().unlock(uid) {
+                self.used.fetch_sub(1, Ordering::Relaxed);
+            }
+        };
+    }
+
+    pub fn get<T: Sized + AsRef<T>>(&self, ix: u64) -> &T {
+        if ix >= self.capacity {
+            panic!("bad index size");
+        }
+        let start = ix as usize * self.cell_size + std::mem::size_of::<Header>();
+        let end = start + std::mem::size_of::<T>();
+        let item_slice = &self.mmap[start..end];
+        unsafe {
+            let item = item_slice.as_ptr() as *const T;
+            return item.as_ref().unwrap();
+        };
+    }
+
+    pub fn get_mut<T: Sized + AsMut<T>>(&self, ix: u64) -> &mut T {
+        if ix >= self.capacity {
+            panic!("bad index size");
+        }
+        let start = ix as usize * self.cell_size + std::mem::size_of::<Header>();
+        let end = start + std::mem::size_of::<T>();
+        let item_slice = &self.mmap[start..end];
+        unsafe {
+            let item = item_slice.as_ptr() as *mut T;
+            return item.as_mut().unwrap();
+        };
+    }
+
+    fn new_map(drives: &[PathBuf], cell_size: usize, capacity: u64) -> (MmapMut, PathBuf) {
+        let r = thread_rng().gen_range(0, drives.len());
+        let drive = &drives[r];
+        let pos = format!("{}", thread_rng().gen_range(0, u128::MAX),);
+        let file = drive.join(pos);
+        let mut data = OpenOptions::new()
+            .read(true)
+            .write(true)
+            .create(true)
+            .open(file.clone())
+            .map_err(|e| {
+                panic!(
+                    "Unable to create data file {} in current dir({:?}): {:?}",
+                    file.display(),
+                    std::env::current_dir(),
+                    e
+                );
+            })
+            .unwrap();
+
+        // Theoretical performance optimization: write a zero to the end of
+        // the file so that we won't have to resize it later, which may be
+        // expensive.
+        data.seek(SeekFrom::Start(capacity * cell_size as u64 - 1))
+            .unwrap();
+        data.write_all(&[0]).unwrap();
+        data.seek(SeekFrom::Start(0)).unwrap();
+        data.flush().unwrap();
+        (unsafe { MmapMut::map_mut(&data).unwrap() }, file)
+    }
+
+    pub fn grow(&mut self) {
+        let old_cap = self.capacity;
+        let old_map = &self.mmap;
+        let old_file = self.path.clone();
+        let (new_map, new_file) = Self::new_map(&self.drives, self.cell_size, self.capacity * 2);
+        (0..old_cap as usize).into_par_iter().for_each(|i| {
+            let old_ix = i * self.cell_size;
+            let new_ix = old_ix * 2;
+            let dst_slice = &new_map[new_ix..new_ix + self.cell_size];
+            let src_slice = &old_map[old_ix..old_ix + self.cell_size];
+
+            unsafe {
+                let dst = dst_slice.as_ptr() as *mut u8;
+                let src = src_slice.as_ptr() as *const u8;
+                std::ptr::copy(src, dst, self.cell_size);
+            };
+        });
+        self.mmap = new_map;
+        self.path = new_file;
+        self.capacity = self.capacity * 2;
+        remove_file(old_file).unwrap();
+    }
+
+    pub fn split<T: Sized + AsRef<T> + AsMut<T> + Copy>(&mut self, testfn: fn(&T) -> bool) -> Self {
+        let (mmap, path) = Self::new_map(&self.drives, self.cell_size, self.capacity);
+        let new_bucket = Self {
+            mmap,
+            path,
+            cell_size: self.cell_size,
+            capacity: self.capacity,
+            drives: self.drives.clone(),
+            used: AtomicU64::new(0),
+        };
+        (0..self.capacity as usize).into_par_iter().for_each(|i| {
+            let ix = i as u64;
+            let uid = self.uid(ix);
+            if uid != 0 {
+                let item = self.get(ix);
+                if testfn(item) {
+                    new_bucket.allocate(ix, uid).unwrap();
+                    let new: &mut T = new_bucket.get_mut(ix);
+                    *new = *item;
+                }
+                self.free(ix, uid);
+            }
+        });
+        new_bucket
+    }
+}
