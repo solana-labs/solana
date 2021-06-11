ed37cfed1cb8f41ae2c318667f754c3c2830fcc8
Dmitri Makarov <dmakarov@alumni.stanford.edu>
2021-06-11 10:23:15 -0700

    Update RUSTFLAGS if it is set, instead of overriding in build-bpf

--- sdk/bpf/env.sh
+++ sdk/bpf/env.sh
@@ -15,4 +15,4 @@ export AR="$bpf_sdk/dependencies/bpf-tools/llvm/bin/llvm-ar"
 export OBJDUMP="$bpf_sdk/dependencies/bpf-tools/llvm/bin/llvm-objdump"
 export OBJCOPY="$bpf_sdk/dependencies/bpf-tools/llvm/bin/llvm-objcopy"
 
-export RUSTFLAGS="-C lto=no"
+export RUSTFLAGS="${RUSTFLAGS} -C lto=no"
