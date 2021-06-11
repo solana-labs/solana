14fdbd55f10b5b792d9d8656f010c06ea5603394
carllin <carl@solana.com>
2021-06-10 22:28:23 -0700

    Account for duplicate before a bank is frozen or replayed (#17866)
    (cherry picked from commit afafa624a3de9964045cda81e7127a975883f6f5)

--- core/src/consensus.rs
+++ core/src/consensus.rs
@@ -1302,7 +1302,7 @@ pub mod test {
     use super::*;
     use crate::{
         cluster_info_vote_listener::VoteTracker,
-        cluster_slot_state_verifier::GossipDuplicateConfirmedSlots,
+        cluster_slot_state_verifier::{DuplicateSlotsTracker, GossipDuplicateConfirmedSlots},
         cluster_slots::ClusterSlots,
         fork_choice::SelectVoteAndResetForkResult,
         heaviest_subtree_fork_choice::{HeaviestSubtreeForkChoice, SlotHashKey},
@@ -1497,6 +1497,7 @@ pub mod test {
                 &AbsRequestSender::default(),
                 None,
                 &mut self.heaviest_subtree_fork_choice,
+                &mut DuplicateSlotsTracker::default(),
                 &mut GossipDuplicateConfirmedSlots::default(),
                 &mut UnfrozenGossipVerifiedVoteHashes::default(),
                 &mut true,
