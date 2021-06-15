be2bb719c4a3895261763f275c81721e5df5758a
carllin <carl@solana.com>
2021-06-14 21:04:01 -0700

    Handle removing slots during account scans (#17471)
    (cherry picked from commit ccc013e134e5f1112e4af279de58e7fc65f8c96d)

--- ledger-tool/src/main.rs
+++ ledger-tool/src/main.rs
@@ -1972,8 +1972,10 @@ fn main() {
                     }
 
                     if remove_stake_accounts {
-                        for (address, mut account) in
-                            bank.get_program_accounts(&stake::program::id()).into_iter()
+                        for (address, mut account) in bank
+                            .get_program_accounts(&stake::program::id())
+                            .unwrap()
+                            .into_iter()
                         {
                             account.set_lamports(0);
                             bank.store_account(&address, &account);
@@ -2006,6 +2008,7 @@ fn main() {
                         // Delete existing vote accounts
                         for (address, mut account) in bank
                             .get_program_accounts(&solana_vote_program::id())
+                            .unwrap()
                             .into_iter()
                         {
                             account.set_lamports(0);
@@ -2167,6 +2170,7 @@ fn main() {
 
                     let accounts: BTreeMap<_, _> = bank
                         .get_all_accounts_with_modified_slots()
+                        .unwrap()
                         .into_iter()
                         .filter(|(pubkey, _account, _slot)| {
                             include_sysvars || !solana_sdk::sysvar::is_sysvar_id(pubkey)
