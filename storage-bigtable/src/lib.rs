9da600ae165c7ca81713b425126a8ef804f923bd
Tyera Eulberg <tyera@solana.com>
2021-06-15 14:57:58 -0600

    Add comment to prevent future errors

--- storage-bigtable/src/lib.rs
+++ storage-bigtable/src/lib.rs
@@ -82,6 +82,9 @@ fn key_to_slot(key: &str) -> Option<Slot> {
 // StoredConfirmedBlock holds the same contents as ConfirmedBlock, but is slightly compressed and avoids
 // some serde JSON directives that cause issues with bincode
 //
+// Note: in order to continue to support old bincode-serialized bigtable entries, if new fields are
+// added to ConfirmedBlock, they must either be excluded or set to `default_on_eof` here
+//
 #[derive(Serialize, Deserialize)]
 struct StoredConfirmedBlock {
     previous_blockhash: String,
