5bc6c89adc2c2cced25b79e361b9a9e2a351297a
Trent Nelson <trent@solana.com>
2021-06-16 10:52:27 -0600

    validator: run poh speed test earlier in start up

--- core/src/validator.rs
+++ core/src/validator.rs
@@ -385,6 +385,7 @@ impl Validator {
             &exit,
             config.enforce_ulimit_nofile,
             &start_progress,
+            config.no_poh_speed_test,
         );
 
         *start_progress.write().unwrap() = ValidatorStartProgress::StartingServices;
@@ -615,10 +616,6 @@ impl Validator {
             } else {
                 (None, None)
             };
-
-        if !config.no_poh_speed_test {
-            check_poh_speed(&genesis_config, None);
-        }
 
         let waited_for_supermajority = if let Ok(waited) = wait_for_supermajority(
             config,
@@ -1021,6 +1018,7 @@ fn new_banks_from_ledger(
     exit: &Arc<AtomicBool>,
     enforce_ulimit_nofile: bool,
     start_progress: &Arc<RwLock<ValidatorStartProgress>>,
+    no_poh_speed_test: bool,
 ) -> (
     GenesisConfig,
     BankForks,
@@ -1052,6 +1050,10 @@ fn new_banks_from_ledger(
             error!("Delete the ledger directory to continue: {:?}", ledger_path);
             abort();
         }
+    }
+
+    if !no_poh_speed_test {
+        check_poh_speed(&genesis_config, None);
     }
 
     let BlockstoreSignals {
