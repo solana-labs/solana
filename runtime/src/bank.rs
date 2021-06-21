90dd099fff776cbfb76abb8614ed14cea01d5c08
Tyera Eulberg <teulberg@gmail.com>
2021-06-21 16:14:54 -0600

    Move stake_weighted_timestamp module (#18114)
    * Move timestamp module into runtime
    
    * Less public
    
    * Remove unused enum
    
    (cherry picked from commit 19fe1dd46327717e9a95f94db567fed96705624f)
    
    # Conflicts:
    #	runtime/src/bank.rs
    #	runtime/src/lib.rs

--- runtime/src/bank.rs
+++ runtime/src/bank.rs
@@ -18,6 +18,10 @@ use crate::{
     log_collector::LogCollector,
     message_processor::{ExecuteDetailsTimings, Executors, MessageProcessor},
     rent_collector::RentCollector,
+    stake_weighted_timestamp::{
+        calculate_stake_weighted_timestamp, MaxAllowableDrift, MAX_ALLOWABLE_DRIFT_PERCENTAGE,
+        MAX_ALLOWABLE_DRIFT_PERCENTAGE_FAST, MAX_ALLOWABLE_DRIFT_PERCENTAGE_SLOW,
+    },
     stakes::Stakes,
     status_cache::{SlotDelta, StatusCache},
     system_instruction_processor::{get_system_account_kind, SystemAccountKind},
@@ -63,10 +67,14 @@ use solana_sdk::{
     signature::{Keypair, Signature},
     slot_hashes::SlotHashes,
     slot_history::SlotHistory,
+<<<<<<< HEAD
     stake_weighted_timestamp::{
         calculate_stake_weighted_timestamp, MaxAllowableDrift, MAX_ALLOWABLE_DRIFT_PERCENTAGE,
         MAX_ALLOWABLE_DRIFT_PERCENTAGE_FAST, MAX_ALLOWABLE_DRIFT_PERCENTAGE_SLOW,
     },
+=======
+    stake::{self, state::Delegation},
+>>>>>>> 19fe1dd46 (Move stake_weighted_timestamp module (#18114))
     system_transaction,
     sysvar::{self},
     timing::years_as_slots,
