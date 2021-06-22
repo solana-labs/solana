9ea8fb23bc0b93132f34af4ef980a6d3c1fde32e
Ashwin Sekar <ashwin@solana.com>
2021-06-21 17:31:43 -0700

    Enable previously flaky optimistic detection local cluster test

--- local-cluster/tests/local_cluster.rs
+++ local-cluster/tests/local_cluster.rs
@@ -2084,7 +2084,6 @@ fn test_no_voting() {
 
 #[test]
 #[serial]
-#[ignore]
 fn test_optimistic_confirmation_violation_detection() {
     solana_logger::setup_with_default(RUST_LOG_FILTER);
     // First set up the cluster with 2 nodes
