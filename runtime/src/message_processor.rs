c583ec3b62b11109ec8b6de39a00c1c1de77a785
jonch <9093549+jon-chuang@users.noreply.github.com>
2021-07-02 17:36:08 +0800

    fix minor errors

--- runtime/src/message_processor.rs
+++ runtime/src/message_processor.rs
@@ -254,7 +254,7 @@ impl PreAccount {
     }
 
     pub fn executable(&self) -> bool {
-        self.account.borrow().executable()
+        self.account().borrow().executable()
     }
 
     pub fn is_zeroed(buf: &[u8]) -> bool {
@@ -1215,7 +1215,7 @@ impl MessageProcessor {
             Err(InstructionError::MissingAccount)
         };
         instruction.visit_each_account(&mut work)?;
-        // work(0, instruction.program_id_index as usize)?;
+        work(0, instruction.program_id_index as usize)?;
 
         // Verify that the total sum of all the lamports did not change
         if pre_sum != post_sum {
