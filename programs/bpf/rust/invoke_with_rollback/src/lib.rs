0b908b3aa13d8abd0779f41bec083f02e1dbc988
jonch <9093549+jon-chuang@users.noreply.github.com>
2021-07-03 23:21:18 +0800

    unified budget and rollback example program

---
+++ programs/bpf/rust/invoke_with_rollback/src/lib.rs
@@ -0,0 +1,56 @@
+//! @brief Example Rust-based BPF program that exercises instruction introspection
+
+extern crate solana_program;
+use solana_program::{
+    account_info::next_account_info,
+    account_info::AccountInfo,
+    entrypoint,
+    entrypoint::ProgramResult,
+    instruction::Instruction,
+    log::sol_log_compute_units,
+    msg,
+    program::{invoke_signed_with_budget, invoke_with_rollback},
+    program_error::{InvokeError, ProgramError},
+    pubkey::Pubkey,
+};
+
+entrypoint!(process_instruction);
+pub fn process_instruction(
+    _program_id: &Pubkey,
+    accounts: &[AccountInfo],
+    _instruction_data: &[u8],
+) -> ProgramResult {
+    let account_info_iter = &mut accounts.iter();
+    let error_program_info = next_account_info(account_info_iter)?;
+    let inner_program_info = next_account_info(account_info_iter)?;
+    // 7 * 25_000 = 175_000
+    for _ in 0..7 {
+        sol_log_compute_units();
+        {
+            let ix = Instruction::new_with_bincode(*error_program_info.key, &[0], vec![]);
+            match invoke_with_rollback(
+                &ix,
+                Some(12_500),
+                &[error_program_info.clone()],
+                &vec![][..],
+            ) {
+                Err(InvokeError(ProgramError::Custom(0x01))) => {
+                    msg!("invoke_with_rollback: error 1, rolled back")
+                }
+                ret => ret?,
+            }
+        }
+        {
+            let ix = Instruction::new_with_bincode(*inner_program_info.key, &[0], vec![]);
+            let ret =
+                invoke_signed_with_budget(&ix, 25_000, &[inner_program_info.clone()], &vec![][..]);
+            if let Err(ProgramError::ComputationalBudgetExceeded) = ret {
+                msg!("invoke_with_budget: inner CPI exceeded computational budget, rolled back");
+            } else {
+                ret?;
+            }
+        }
+    }
+    sol_log_compute_units();
+    Ok(())
+}
--- programs/bpf/rust/invoke_with_rollback/tests/test.rs
+++ programs/bpf/rust/invoke_with_rollback/tests/test.rs
@@ -1,5 +1,5 @@
 use {
-    cpi_budgeted_early_termination::process_instruction,
+    invoke_with_rollback::process_instruction,
     solana_program::{
         instruction::{AccountMeta, Instruction},
         pubkey::Pubkey,
@@ -7,18 +7,25 @@ use {
     solana_program_test::*,
     solana_sdk::{
         account_info::AccountInfo, entrypoint::ProgramResult, log::sol_log_compute_units,
-        signature::Signer, transaction::Transaction,
+        signature::Signer, transaction::Transaction, program_error::ProgramError,
     },
     std::str::FromStr,
 };
 
-pub fn process_instruction_inner(
+pub fn process_instruction_error(
+    _program_id: &Pubkey,
+    _accounts: &[AccountInfo],
+    _instruction_data: &[u8],
+) -> ProgramResult {
+    Err(ProgramError::Custom(0x01))
+}
+pub fn process_instruction_exceeds_budget(
     _program_id: &Pubkey,
     _accounts: &[AccountInfo],
     _instruction_data: &[u8],
 ) -> ProgramResult {
     let mut i = 0u32;
-    for _ in 0..10_000 {
+    for _ in 0..3_000 {
         if i % 500 == 0 {
             sol_log_compute_units();
         }
@@ -29,26 +36,31 @@ pub fn process_instruction_inner(
 
 #[tokio::test]
 async fn test_cpi_budgeted_early_termination() {
-    let program_id_inner = Pubkey::from_str("inner11111111111111111111111111111111111111").unwrap();
-    let mut prog_test = ProgramTest::new(
-        "inner",
-        program_id_inner,
-        processor!(process_instruction_inner),
-    );
     let program_id = Pubkey::from_str("outer11111111111111111111111111111111111111").unwrap();
-    prog_test.add_program(
-        "cpi_budgeted_early_termination",
+    let mut prog_test = ProgramTest::new(
+        "outer",
         program_id,
         processor!(process_instruction),
     );
-
+    let program_id_error = Pubkey::from_str("error11111111111111111111111111111111111111").unwrap();
+    prog_test.add_program(
+        "error",
+        program_id_error,
+        processor!(process_instruction_error),
+    );
+    let program_id_inner = Pubkey::from_str("inner11111111111111111111111111111111111111").unwrap();
+    prog_test.add_program(
+        "inner",
+        program_id_inner,
+        processor!(process_instruction_exceeds_budget),
+    );
     let mut context = prog_test.start_with_context().await;
 
     let transaction = Transaction::new_signed_with_payer(
         &[Instruction::new_with_bincode(
             program_id,
             &[0],
-            vec![AccountMeta::new_readonly(program_id_inner, false)],
+            vec![AccountMeta::new_readonly(program_id_error, false), AccountMeta::new_readonly(program_id_inner, false)],
         )],
         Some(&context.payer.pubkey()),
         &[&context.payer],
