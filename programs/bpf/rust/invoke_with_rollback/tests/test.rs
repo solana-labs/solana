6066e9879ba9dbc0c147123e1dbf5cfe398a23a6
jonch <9093549+jon-chuang@users.noreply.github.com>
2021-07-03 23:38:27 +0800

    fmt

--- programs/bpf/rust/invoke_with_rollback/tests/test.rs
+++ programs/bpf/rust/invoke_with_rollback/tests/test.rs
@@ -7,7 +7,7 @@ use {
     solana_program_test::*,
     solana_sdk::{
         account_info::AccountInfo, entrypoint::ProgramResult, log::sol_log_compute_units,
-        signature::Signer, transaction::Transaction, program_error::ProgramError,
+        program_error::ProgramError, signature::Signer, transaction::Transaction,
     },
     std::str::FromStr,
 };
@@ -37,11 +37,7 @@ pub fn process_instruction_exceeds_budget(
 #[tokio::test]
 async fn test_invoke_with_budget_and_rollback() {
     let program_id = Pubkey::from_str("outer11111111111111111111111111111111111111").unwrap();
-    let mut prog_test = ProgramTest::new(
-        "outer",
-        program_id,
-        processor!(process_instruction),
-    );
+    let mut prog_test = ProgramTest::new("outer", program_id, processor!(process_instruction));
     let program_id_error = Pubkey::from_str("error11111111111111111111111111111111111111").unwrap();
     prog_test.add_program(
         "error",
@@ -60,7 +56,10 @@ async fn test_invoke_with_budget_and_rollback() {
         &[Instruction::new_with_bincode(
             program_id,
             &[0],
-            vec![AccountMeta::new_readonly(program_id_error, false), AccountMeta::new_readonly(program_id_inner, false)],
+            vec![
+                AccountMeta::new_readonly(program_id_error, false),
+                AccountMeta::new_readonly(program_id_inner, false),
+            ],
         )],
         Some(&context.payer.pubkey()),
         &[&context.payer],
