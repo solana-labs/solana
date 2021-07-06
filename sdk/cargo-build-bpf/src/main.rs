24f0fe463133f21cb8d5c5721c72103dac1f76cb
Dmitri Makarov <dmakarov@alumni.stanford.edu>
2021-07-05 17:15:39 -0700

    Bump bpf-tools version
    Update Rust BPF to version 1.53

--- sdk/cargo-build-bpf/src/main.rs
+++ sdk/cargo-build-bpf/src/main.rs
@@ -415,7 +415,7 @@ fn build_bpf_package(config: &Config, target_directory: &Path, package: &cargo_m
     install_if_missing(
         config,
         "bpf-tools",
-        "v1.12",
+        "v1.13",
         "https://github.com/solana-labs/bpf-tools/releases/download",
         &PathBuf::from(bpf_tools_filename),
     )
