e433930ab5e88c924d891c51ab60346c209c843c
Jack May <jack@solana.com>
2021-07-07 09:09:52 -0700

    Update verify policy (#18459)
    (cherry picked from commit 44289e6728b293ff4f9be95eb0d6a381df008b5c)
    
    # Conflicts:
    #	runtime/src/message_processor.rs

--- runtime/src/message_processor.rs
+++ runtime/src/message_processor.rs
@@ -174,8 +174,13 @@ impl PreAccount {
                 return Err(InstructionError::ExecutableAccountNotRentExempt);
             }
             if !is_writable // line coverage used to get branch coverage
+<<<<<<< HEAD
                 || pre.executable
                 || *program_id != pre.owner
+=======
+                || pre.executable()
+                || program_id != post.owner()
+>>>>>>> 44289e672 (Update verify policy (#18459))
             {
                 return Err(InstructionError::ExecutableModified);
             }
@@ -1733,6 +1738,21 @@ mod tests {
                 .verify(),
             Ok(()),
             "system program should be able to change account data size"
+        );
+    }
+
+    #[test]
+    fn test_verify_account_changes_owner_executable() {
+        let alice_program_id = solana_sdk::pubkey::new_rand();
+        let bob_program_id = solana_sdk::pubkey::new_rand();
+
+        assert_eq!(
+            Change::new(&alice_program_id, &alice_program_id)
+                .owner(&bob_program_id)
+                .executable(false, true)
+                .verify(),
+            Err(InstructionError::ExecutableModified),
+            "Program should not be able to change owner and executable at the same time"
         );
     }
 
