64683a5b61150831117436c218e39e6c5e64a03b
Steven Czabaniuk <steven@solana.com>
2021-07-11 17:45:56 -0500

    Add benchmark for ShredIndex read/write

---
+++ ledger/benches/shred_index.rs
@@ -0,0 +1,37 @@
+#![feature(test)]
+
+extern crate test;
+use rand::seq::SliceRandom;
+use solana_ledger::blockstore_meta::ShredIndex;
+use test::Bencher;
+
+fn get_shuffled_vector(size: usize) -> Vec<u64> {
+    let mut values: Vec<u64> = Vec::with_capacity(size);
+    for i in 0..size {
+        values.push(i as u64);
+    }
+    values.shuffle(&mut rand::thread_rng());
+    values
+}
+
+#[bench]
+fn bench_shred_index_is_present(bencher: &mut Bencher) {
+    let values = get_shuffled_vector(500);
+    bencher.iter(|| {
+        let index = ShredIndex::default();
+        for value in values.iter() {
+            index.is_present(*value);
+        }
+    })
+}
+
+#[bench]
+fn bench_shred_index_set_present(bencher: &mut Bencher) {
+    let values = get_shuffled_vector(500);
+    bencher.iter(|| {
+        let mut index = ShredIndex::default();
+        for value in values.iter() {
+            index.set_present(*value, true);
+        }
+    })
+}
