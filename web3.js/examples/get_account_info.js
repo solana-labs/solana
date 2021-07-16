e8086fcc806dfc5f7778fdff024a231544901698
chaseeb <chaseeb@gmail.com>
2021-07-16 00:09:35 -0400

    chore: add get_account_info example

---
+++ web3.js/examples/get_account_info.js
@@ -0,0 +1,23 @@
+import * as web3 from '@solana/web3.js';
+
+(async () => {
+  // Connect to cluster
+  var connection = new web3.Connection(
+    web3.clusterApiUrl('devnet'),
+    'confirmed',
+  );
+
+  // Generate a new wallet keypair and airdrop SOL
+  var wallet = web3.Keypair.generate();
+  var airdropSignature = await connection.requestAirdrop(
+    wallet.publicKey,
+    web3.LAMPORTS_PER_SOL,
+  );
+
+  //wait for airdrop confirmation
+  await connection.confirmTransaction(airdropSignature);
+
+  //get account info
+  let account = await connection.getAccountInfo(wallet.publicKey);
+  console.log(account);
+})();
