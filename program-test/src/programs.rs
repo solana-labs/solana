073178496a94c19537f403f0c60e885baa042331
Jon Cinque <jon.cinque@gmail.com>
2021-07-21 00:24:26 +0200

    Update to spl-token v3.2.0

--- program-test/src/programs.rs
+++ program-test/src/programs.rs
@@ -18,7 +18,7 @@ mod spl_associated_token_account {
 }
 
 static SPL_PROGRAMS: &[(Pubkey, &[u8])] = &[
-    (spl_token::ID, include_bytes!("programs/spl_token-3.1.0.so")),
+    (spl_token::ID, include_bytes!("programs/spl_token-3.2.0.so")),
     (
         spl_memo_1_0::ID,
         include_bytes!("programs/spl_memo-1.0.0.so"),
