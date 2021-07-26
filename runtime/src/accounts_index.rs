825009310f87caf8a3e8a4b2b56d33b981322ce9
Jeff Washington (jwash) <wash678@gmail.com>
2021-07-26 13:45:13 -0500

    panic on empty range metric

--- runtime/src/accounts_index.rs
+++ runtime/src/accounts_index.rs
@@ -1169,6 +1169,9 @@ impl<T: 'static + Clone + IsCached + ZeroLamport + std::marker::Sync + std::mark
                 ("num_keys_iterated", num_keys_iterated, i64),
             )
         }
+        else {
+            panic!("empty metric name");
+        }
     }
 
     fn do_scan_secondary_index<
