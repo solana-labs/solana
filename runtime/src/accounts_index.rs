ac5b9a52b93c12382d5148179bd822f10de0db35
Jeff Washington (jwash) <wash678@gmail.com>
2021-07-26 15:43:48 -0500

    ce

--- runtime/src/accounts_index.rs
+++ runtime/src/accounts_index.rs
@@ -3539,7 +3539,7 @@ pub mod tests {
         index
             .account_maps
             .iter()
-            .map(|bin_map| bin_map.read().unwrap().len())
+            .map(|bin_map| bin_map.read().unwrap().len_inaccurate())
             .sum()
     }
 
