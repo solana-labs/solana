a895186524bbfb9bd0fffffa7eca99f70f202728
Tyera Eulberg <tyera@solana.com>
2021-08-02 09:42:13 -0600

    Exit stable-bpf CI runs before localnet-sanity

--- ci/test-stable.sh
+++ ci/test-stable.sh
@@ -75,6 +75,7 @@ test-stable-bpf)
   bpf_dump_archive="bpf-dumps.tar.bz2"
   rm -f "$bpf_dump_archive"
   tar cjvf "$bpf_dump_archive" "${bpf_target_path}"/{deploy/*.txt,bpfel-unknown-unknown/release/*.so}
+  exit 0
   ;;
 test-stable-perf)
   if [[ $(uname) = Linux ]]; then
