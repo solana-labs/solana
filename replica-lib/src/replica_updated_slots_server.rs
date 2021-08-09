5c9b6aa0f05e089b923d889745549bdcedf67238
Lijun Wang <83639177+lijunwangs@users.noreply.github.com>
2021-08-09 00:50:17 -0700

    Get the updated slots.

--- replica-lib/src/replica_updated_slots_server.rs
+++ replica-lib/src/replica_updated_slots_server.rs
@@ -1,20 +1,48 @@
 use tonic;
 // tonic::include_proto!("accountsdb_repl");
-use crate::accountsdb_repl_server::{self, ReplicaUpdatedSlotsServer};
+use {
+    crate::accountsdb_repl_server::{self, ReplicaUpdatedSlotsServer},
+    solana_sdk::{clock::Slot, commitment_config::CommitmentLevel},
+    std::{collections::HashMap, sync::RwLock},
+};
 
-pub(crate) struct ReplicaUpdatedSlotsServerImpl {}
+/// The structure modelling the slots eligible for replication and
+/// their states.
+#[derive(Default)]
+struct ReplicaEligibleSlotSet {
+    slot_set: RwLock<HashMap<Slot, CommitmentLevel>>,
+}
+
+pub(crate) struct ReplicaUpdatedSlotsServerImpl {
+    eligible_slot_set: ReplicaEligibleSlotSet,
+}
 
 impl ReplicaUpdatedSlotsServer for ReplicaUpdatedSlotsServerImpl {
     fn get_updated_slots(
         &self,
-        _request: &accountsdb_repl_server::ReplicaUpdatedSlotsRequest,
+        request: &accountsdb_repl_server::ReplicaUpdatedSlotsRequest,
     ) -> Result<accountsdb_repl_server::ReplicaUpdatedSlotsResponse, tonic::Status> {
-        Err(tonic::Status::unimplemented("Not implemented yet"))
+        let slot_set = self.eligible_slot_set.slot_set.read().unwrap();
+        let updated_slots: Vec<u64> = slot_set
+            .iter()
+            .filter(|(slot, _)| **slot > request.last_replicated_slot)
+            .map(|(slot, _)| *slot)
+            .collect();
+
+        Ok(accountsdb_repl_server::ReplicaUpdatedSlotsResponse { updated_slots })
     }
 }
 
 impl ReplicaUpdatedSlotsServerImpl {
     pub fn new() -> Self {
-        Self {}
+        Self {
+            eligible_slot_set: ReplicaEligibleSlotSet::default(),
+        }
+    }
+
+    /// Notify the slot has been optimistically confirmed
+    pub fn notify_optimistically_confirmed_slot(&self, slot: Slot) {
+        let mut slot_set = self.eligible_slot_set.slot_set.write().unwrap();
+        slot_set.insert(slot, CommitmentLevel::Confirmed);
     }
 }
