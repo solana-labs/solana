8d9090552728e5c16d31e1d9d7ee9086d671ef55
Lijun Wang <83639177+lijunwangs@users.noreply.github.com>
2021-08-21 19:02:50 -0700

    Refactor code for transformation from internal accounts data to protocol ReplicaAccountInfo

--- replica-lib/src/replica_accounts_server.rs
+++ replica-lib/src/replica_accounts_server.rs
@@ -2,7 +2,7 @@ use {
     crate::accountsdb_repl_server::{
         self, ReplicaAccountData, ReplicaAccountInfo, ReplicaAccountMeta, ReplicaAccountsServer,
     },
-    solana_runtime::{accounts_db::LoadedAccount, bank_forks::BankForks},
+    solana_runtime::{accounts_cache::CachedAccount, accounts_db::LoadedAccount, append_vec::StoredAccountMeta, bank_forks::BankForks},
     solana_sdk::account::Account,
     std::{
         cmp::Eq,
@@ -17,6 +17,45 @@ pub(crate) struct ReplicaAccountsServerImpl {
 
 impl Eq for ReplicaAccountInfo {}
 
+impl ReplicaAccountInfo {
+    fn from_stored_account_meta(stored_account_meta: &StoredAccountMeta) -> Self {
+        let account_meta = Some(ReplicaAccountMeta {
+            pubkey: stored_account_meta.meta.pubkey.to_bytes().to_vec(),
+            lamports: stored_account_meta.account_meta.lamports,
+            owner: stored_account_meta.account_meta.owner.to_bytes().to_vec(),
+            executable: stored_account_meta.account_meta.executable,
+            rent_epoch: stored_account_meta.account_meta.rent_epoch,
+        });
+        let data = Some(ReplicaAccountData {
+            data: stored_account_meta.data.to_vec(),
+        });
+        ReplicaAccountInfo {
+            account_meta,
+            hash: stored_account_meta.hash.0.to_vec(),
+            data,
+        }
+    }
+
+    fn from_cached_account(cached_account: &CachedAccount) -> Self {
+        let account = Account::from(cached_account.account.clone());
+        let account_meta = Some(ReplicaAccountMeta {
+            pubkey: cached_account.pubkey().to_bytes().to_vec(),
+            lamports: account.lamports,
+            owner: account.owner.to_bytes().to_vec(),
+            executable: account.executable,
+            rent_epoch: account.rent_epoch,
+        });
+        let data = Some(ReplicaAccountData {
+            data: account.data.to_vec(),
+        });
+        ReplicaAccountInfo {
+            account_meta,
+            hash: cached_account.hash().0.to_vec(),
+            data,
+        }
+    }
+}
+
 impl ReplicaAccountsServer for ReplicaAccountsServerImpl {
     fn get_slot_accounts(
         &self,
@@ -29,41 +68,10 @@ impl ReplicaAccountsServer for ReplicaAccountsServerImpl {
             Some(bank) => {
                 let accounts = bank.rc.accounts.scan_slot(slot, |account| match account {
                     LoadedAccount::Stored(stored_account_meta) => {
-                        let account_meta = Some(ReplicaAccountMeta {
-                            pubkey: stored_account_meta.meta.pubkey.to_bytes().to_vec(),
-                            lamports: stored_account_meta.account_meta.lamports,
-                            owner: stored_account_meta.account_meta.owner.to_bytes().to_vec(),
-                            executable: stored_account_meta.account_meta.executable,
-                            rent_epoch: stored_account_meta.account_meta.rent_epoch,
-                        });
-                        let data = Some(ReplicaAccountData {
-                            data: stored_account_meta.data.to_vec(),
-                        });
-                        let replica_account_info = ReplicaAccountInfo {
-                            account_meta,
-                            hash: stored_account_meta.hash.0.to_vec(),
-                            data,
-                        };
-                        Some(replica_account_info)
+                        Some(ReplicaAccountInfo::from_stored_account_meta(&stored_account_meta))
                     }
-                    LoadedAccount::Cached((pubkey, cached_account)) => {
-                        let account = Account::from(cached_account.account.clone());
-                        let account_meta = Some(ReplicaAccountMeta {
-                            pubkey: pubkey.to_bytes().to_vec(),
-                            lamports: account.lamports,
-                            owner: account.owner.to_bytes().to_vec(),
-                            executable: account.executable,
-                            rent_epoch: account.rent_epoch,
-                        });
-                        let data = Some(ReplicaAccountData {
-                            data: account.data.to_vec(),
-                        });
-                        let replica_account_info = ReplicaAccountInfo {
-                            account_meta,
-                            hash: cached_account.hash().0.to_vec(),
-                            data,
-                        };
-                        Some(replica_account_info)
+                    LoadedAccount::Cached((_pubkey, cached_account)) => {
+                        Some(ReplicaAccountInfo::from_cached_account(&cached_account))
                     }
                 });
 
