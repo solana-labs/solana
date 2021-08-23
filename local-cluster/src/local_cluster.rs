31ce198352fc5adea13525b9feaf2e26035a306c
Carl Lin <carl@solana.com>
2021-08-22 22:37:15 -0700

    Fix duplicate broadcast

--- local-cluster/src/local_cluster.rs
+++ local-cluster/src/local_cluster.rs
@@ -148,6 +148,11 @@ impl LocalCluster {
                 .iter()
                 .zip(&config.node_stakes)
                 .filter_map(|((node_keypair, in_genesis), stake)| {
+                    info!(
+                        "STARTING LOCAL CLUSTER: key {} has {} stake",
+                        node_keypair.pubkey(),
+                        stake
+                    );
                     if *in_genesis {
                         Some((
                             ValidatorVoteKeypairs {
