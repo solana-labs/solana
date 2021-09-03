bbd2fc1a70436ca9dd56d6e08c9676ceffc6abc2
Brooks Prumo <brooks@prumo.org>
2021-09-02 19:14:21 -0500

    Revert "Make startup aware of Incremental Snapshots (#19550)"
    This reverts commit d45ced0a5d1b67657e643b735849f2e5bbb2ceef.

--- core/src/validator.rs
+++ core/src/validator.rs
@@ -69,7 +69,7 @@ use {
         hardened_unpack::{open_genesis_config, MAX_GENESIS_ARCHIVE_UNPACKED_SIZE},
         snapshot_archive_info::SnapshotArchiveInfoGetter,
         snapshot_config::SnapshotConfig,
-        snapshot_package::{AccountsPackageSender, PendingSnapshotPackage},
+        snapshot_package::PendingSnapshotPackage,
         snapshot_utils,
     },
     solana_sdk::{
@@ -92,7 +92,7 @@ use {
         path::{Path, PathBuf},
         sync::{
             atomic::{AtomicBool, AtomicU64, Ordering},
-            mpsc::{channel, Receiver},
+            mpsc::Receiver,
             Arc, Mutex, RwLock,
         },
         thread::{sleep, Builder, JoinHandle},
@@ -379,7 +379,7 @@ impl Validator {
                 .register_exit(Box::new(move || exit.store(true, Ordering::Relaxed)));
         }
 
-        let accounts_package_channel = channel();
+        let (replay_vote_sender, replay_vote_receiver) = unbounded();
         let (
             genesis_config,
             bank_forks,
@@ -387,7 +387,6 @@ impl Validator {
             ledger_signal_receiver,
             completed_slots_receiver,
             leader_schedule_cache,
-            last_full_snapshot_slot,
             snapshot_hash,
             TransactionHistoryServices {
                 transaction_status_sender,
@@ -409,7 +408,6 @@ impl Validator {
             config.enforce_ulimit_nofile,
             &start_progress,
             config.no_poh_speed_test,
-            accounts_package_channel.0.clone(),
         );
 
         *start_progress.write().unwrap() = ValidatorStartProgress::StartingServices;
@@ -709,7 +707,6 @@ impl Validator {
         let rpc_completed_slots_service =
             RpcCompletedSlotsService::spawn(completed_slots_receiver, rpc_subscriptions.clone());
 
-        let (replay_vote_sender, replay_vote_receiver) = unbounded();
         let tvu = Tvu::new(
             vote_account,
             authorized_voter_keypairs,
@@ -780,8 +777,6 @@ impl Validator {
             },
             &max_slots,
             &cost_model,
-            accounts_package_channel,
-            last_full_snapshot_slot,
         );
 
         let tpu = Tpu::new(
@@ -1074,7 +1069,7 @@ fn post_process_restored_tower(
         })
 }
 
-#[allow(clippy::type_complexity, clippy::too_many_arguments)]
+#[allow(clippy::type_complexity)]
 fn new_banks_from_ledger(
     validator_identity: &Pubkey,
     vote_account: &Pubkey,
@@ -1085,7 +1080,6 @@ fn new_banks_from_ledger(
     enforce_ulimit_nofile: bool,
     start_progress: &Arc<RwLock<ValidatorStartProgress>>,
     no_poh_speed_test: bool,
-    accounts_package_sender: AccountsPackageSender,
 ) -> (
     GenesisConfig,
     BankForks,
@@ -1093,7 +1087,6 @@ fn new_banks_from_ledger(
     Receiver<bool>,
     CompletedSlotsReceiver,
     LeaderScheduleCache,
-    Option<Slot>,
     Option<(Slot, Hash)>,
     TransactionHistoryServices,
     Tower,
@@ -1189,26 +1182,24 @@ fn new_banks_from_ledger(
             TransactionHistoryServices::default()
         };
 
-    let (mut bank_forks, mut leader_schedule_cache, last_full_snapshot_slot, snapshot_hash) =
-        bank_forks_utils::load(
-            &genesis_config,
-            &blockstore,
-            config.account_paths.clone(),
-            config.account_shrink_paths.clone(),
-            config.snapshot_config.as_ref(),
-            process_options,
-            transaction_history_services
-                .transaction_status_sender
-                .as_ref(),
-            transaction_history_services
-                .cache_block_meta_sender
-                .as_ref(),
-            accounts_package_sender,
-        )
-        .unwrap_or_else(|err| {
-            error!("Failed to load ledger: {:?}", err);
-            abort()
-        });
+    let (mut bank_forks, mut leader_schedule_cache, snapshot_hash) = bank_forks_utils::load(
+        &genesis_config,
+        &blockstore,
+        config.account_paths.clone(),
+        config.account_shrink_paths.clone(),
+        config.snapshot_config.as_ref(),
+        process_options,
+        transaction_history_services
+            .transaction_status_sender
+            .as_ref(),
+        transaction_history_services
+            .cache_block_meta_sender
+            .as_ref(),
+    )
+    .unwrap_or_else(|err| {
+        error!("Failed to load ledger: {:?}", err);
+        abort()
+    });
 
     if let Some(warp_slot) = config.warp_slot {
         let snapshot_config = config.snapshot_config.as_ref().unwrap_or_else(|| {
@@ -1286,7 +1277,6 @@ fn new_banks_from_ledger(
         ledger_signal_receiver,
         completed_slots_receiver,
         leader_schedule_cache,
-        last_full_snapshot_slot,
         snapshot_hash,
         transaction_history_services,
         tower,
--- ledger/src/bank_forks_utils.rs
+++ ledger/src/bank_forks_utils.rs
@@ -8,20 +8,12 @@ use crate::{
 };
 use log::*;
 use solana_entry::entry::VerifyRecyclers;
-use solana_runtime::{
-    bank_forks::BankForks, snapshot_archive_info::SnapshotArchiveInfoGetter,
-    snapshot_config::SnapshotConfig, snapshot_package::AccountsPackageSender, snapshot_utils,
-};
+use solana_runtime::{bank_forks::BankForks, snapshot_config::SnapshotConfig, snapshot_utils};
 use solana_sdk::{clock::Slot, genesis_config::GenesisConfig, hash::Hash};
 use std::{fs, path::PathBuf, process, result};
 
 pub type LoadResult = result::Result<
-    (
-        BankForks,
-        LeaderScheduleCache,
-        Option<Slot>,
-        Option<(Slot, Hash)>,
-    ),
+    (BankForks, LeaderScheduleCache, Option<(Slot, Hash)>),
     BlockstoreProcessorError,
 >;
 
@@ -29,16 +21,9 @@ fn to_loadresult(
     bpr: BlockstoreProcessorResult,
     snapshot_slot_and_hash: Option<(Slot, Hash)>,
 ) -> LoadResult {
-    bpr.map(
-        |(bank_forks, leader_schedule_cache, last_full_snapshot_slot)| {
-            (
-                bank_forks,
-                leader_schedule_cache,
-                last_full_snapshot_slot,
-                snapshot_slot_and_hash,
-            )
-        },
-    )
+    bpr.map(|(bank_forks, leader_schedule_cache)| {
+        (bank_forks, leader_schedule_cache, snapshot_slot_and_hash)
+    })
 }
 
 /// Load the banks and accounts
@@ -54,7 +39,6 @@ pub fn load(
     process_options: ProcessOptions,
     transaction_status_sender: Option<&TransactionStatusSender>,
     cache_block_meta_sender: Option<&CacheBlockMetaSender>,
-    accounts_package_sender: AccountsPackageSender,
 ) -> LoadResult {
     if let Some(snapshot_config) = snapshot_config {
         info!(
@@ -79,7 +63,6 @@ pub fn load(
                 process_options,
                 transaction_status_sender,
                 cache_block_meta_sender,
-                accounts_package_sender,
             );
         } else {
             info!("No snapshot package available; will load from genesis");
@@ -94,8 +77,6 @@ pub fn load(
         account_paths,
         process_options,
         cache_block_meta_sender,
-        snapshot_config,
-        accounts_package_sender,
     )
 }
 
@@ -105,8 +86,6 @@ fn load_from_genesis(
     account_paths: Vec<PathBuf>,
     process_options: ProcessOptions,
     cache_block_meta_sender: Option<&CacheBlockMetaSender>,
-    snapshot_config: Option<&SnapshotConfig>,
-    accounts_package_sender: AccountsPackageSender,
 ) -> LoadResult {
     info!("Processing ledger from genesis");
     to_loadresult(
@@ -116,8 +95,6 @@ fn load_from_genesis(
             account_paths,
             process_options,
             cache_block_meta_sender,
-            snapshot_config,
-            accounts_package_sender,
         ),
         None,
     )
@@ -133,7 +110,6 @@ fn load_from_snapshot(
     process_options: ProcessOptions,
     transaction_status_sender: Option<&TransactionStatusSender>,
     cache_block_meta_sender: Option<&CacheBlockMetaSender>,
-    accounts_package_sender: AccountsPackageSender,
 ) -> LoadResult {
     // Fail hard here if snapshot fails to load, don't silently continue
     if account_paths.is_empty() {
@@ -141,25 +117,24 @@ fn load_from_snapshot(
         process::exit(1);
     }
 
-    let (deserialized_bank, timings, full_snapshot_archive_info, _) =
-        snapshot_utils::bank_from_latest_snapshot_archives(
-            &snapshot_config.bank_snapshots_dir,
-            &snapshot_config.snapshot_archives_dir,
-            &account_paths,
-            &process_options.frozen_accounts,
-            genesis_config,
-            process_options.debug_keys.clone(),
-            Some(&crate::builtins::get(process_options.bpf_jit)),
-            process_options.account_indexes.clone(),
-            process_options.accounts_db_caching_enabled,
-            process_options.limit_load_slot_count_from_snapshot,
-            process_options.shrink_ratio,
-            process_options.accounts_db_test_hash_calculation,
-            process_options.accounts_db_skip_shrink,
-            process_options.verify_index,
-            process_options.accounts_index_config,
-        )
-        .expect("Load from snapshot failed");
+    let (deserialized_bank, timings) = snapshot_utils::bank_from_latest_snapshot_archives(
+        &snapshot_config.bank_snapshots_dir,
+        &snapshot_config.snapshot_archives_dir,
+        &account_paths,
+        &process_options.frozen_accounts,
+        genesis_config,
+        process_options.debug_keys.clone(),
+        Some(&crate::builtins::get(process_options.bpf_jit)),
+        process_options.account_indexes.clone(),
+        process_options.accounts_db_caching_enabled,
+        process_options.limit_load_slot_count_from_snapshot,
+        process_options.shrink_ratio,
+        process_options.accounts_db_test_hash_calculation,
+        process_options.accounts_db_skip_shrink,
+        process_options.verify_index,
+        process_options.accounts_index_config,
+    )
+    .expect("Load from snapshot failed");
 
     let deserialized_bank_slot_and_hash = (
         deserialized_bank.slot(),
@@ -178,10 +153,7 @@ fn load_from_snapshot(
             &VerifyRecyclers::default(),
             transaction_status_sender,
             cache_block_meta_sender,
-            Some(snapshot_config),
-            accounts_package_sender,
             timings,
-            full_snapshot_archive_info.slot(),
         ),
         Some(deserialized_bank_slot_and_hash),
     )
