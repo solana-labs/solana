e2179b2bd778a909ca1b8d08c3fc6f7839202b41
Tyera Eulberg <tyera@solana.com>
2021-09-03 00:59:29 -0600

    Fix conflicts

--- transaction-status/src/parse_instruction.rs
+++ transaction-status/src/parse_instruction.rs
@@ -10,31 +10,20 @@ use crate::{
 use inflector::Inflector;
 use serde_json::Value;
 use solana_account_decoder::parse_token::spl_token_id_v2_0;
-<<<<<<< HEAD
 use solana_sdk::{instruction::CompiledInstruction, pubkey::Pubkey, system_program};
-use std::{collections::HashMap, str::from_utf8};
-=======
-use solana_sdk::{instruction::CompiledInstruction, pubkey::Pubkey, stake, system_program};
 use std::{
     collections::HashMap,
     str::{from_utf8, Utf8Error},
 };
->>>>>>> f4ae450f3 (Populate memo in bigtable transaction structs (#19512))
 use thiserror::Error;
 
 lazy_static! {
     static ref ASSOCIATED_TOKEN_PROGRAM_ID: Pubkey = spl_associated_token_id_v1_0();
     static ref BPF_LOADER_PROGRAM_ID: Pubkey = solana_sdk::bpf_loader::id();
     static ref BPF_UPGRADEABLE_LOADER_PROGRAM_ID: Pubkey = solana_sdk::bpf_loader_upgradeable::id();
-<<<<<<< HEAD
-    static ref MEMO_V1_PROGRAM_ID: Pubkey = Pubkey::new_from_array(spl_memo::v1::id().to_bytes());
-    static ref MEMO_V3_PROGRAM_ID: Pubkey = Pubkey::new_from_array(spl_memo::id().to_bytes());
-    static ref STAKE_PROGRAM_ID: Pubkey = solana_stake_program::id();
-=======
     static ref MEMO_V1_PROGRAM_ID: Pubkey = spl_memo_id_v1();
     static ref MEMO_V3_PROGRAM_ID: Pubkey = spl_memo_id_v3();
-    static ref STAKE_PROGRAM_ID: Pubkey = stake::program::id();
->>>>>>> f4ae450f3 (Populate memo in bigtable transaction structs (#19512))
+    static ref STAKE_PROGRAM_ID: Pubkey = solana_stake_program::id();
     static ref SYSTEM_PROGRAM_ID: Pubkey = system_program::id();
     static ref TOKEN_PROGRAM_ID: Pubkey = spl_token_id_v2_0();
     static ref VOTE_PROGRAM_ID: Pubkey = solana_vote_program::id();
