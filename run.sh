c933e821141995f5082f166035a2c065cbab3631
Trent Nelson <trent@solana.com>
2021-09-07 11:13:57 -0500

    add some guidance in place of `./run.sh`

---
+++ run.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+cat <<'EOF'
+
+  WARNING! LEGACY SHELL SCRIPT
+
+  You almost certainly do not want to run this script!
+
+  If you are a dapp developer and looking for a way to run a local validator, please
+  see https://docs.solana.com/developing/test-validator
+
+  If you are a prospective validator, please see https://docs.solana.com/running-validator
+
+  If you are a core developer, many apologies for what you're about to endure, but
+  you may be in the right place.  This script is now located at `./scripts/run.sh`.
+  Please update whatever docs lead you here to reflect this change
+
+EOF
