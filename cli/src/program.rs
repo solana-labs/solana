dae412d2ced401813e5acc62c1161d59e09f95dc
Jack May <jack@solana.com>
2021-09-07 22:10:57 -0700

    resolve conflicts

--- cli/src/program.rs
+++ cli/src/program.rs
@@ -1533,7 +1533,6 @@ fn process_close(
                             &account_pubkey,
                             &recipient_pubkey,
                             authority_signer,
-                            None,
                         )?;
                     }
                     Ok(config
@@ -1577,7 +1576,6 @@ fn process_close(
                                     &programdata_pubkey,
                                     &recipient_pubkey,
                                     authority_signer,
-                                    Some(&account_pubkey),
                                 )?;
                                 Ok(config.output_format.formatted_string(
                                     &CliUpgradeableProgramClosed {
