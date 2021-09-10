58f15e37daab04a0be7b1182fa8a625069230409
Jeff Washington (jwash) <wash678@gmail.com>
2021-09-10 17:54:55 -0500

    push use of AccountMapEntry one level deeper

--- runtime/src/in_mem_accounts_index.rs
+++ runtime/src/in_mem_accounts_index.rs
@@ -1,3 +1,4 @@
+use crate::accounts_index::{AccountMapEntry, IsCached};
 use solana_sdk::pubkey::Pubkey;
 use std::collections::{
     hash_map::{Entry, Iter, Keys},
@@ -9,31 +10,31 @@ type K = Pubkey;
 
 // one instance of this represents one bin of the accounts index.
 #[derive(Debug, Default)]
-pub struct InMemAccountsIndex<V> {
+pub struct InMemAccountsIndex<V: IsCached> {
     // backing store
-    map: HashMap<Pubkey, V>,
+    map: HashMap<Pubkey, AccountMapEntry<V>>,
 }
 
-impl<V> InMemAccountsIndex<V> {
+impl<V: IsCached> InMemAccountsIndex<V> {
     pub fn new() -> Self {
         Self {
             map: HashMap::new(),
         }
     }
 
-    pub fn entry(&mut self, pubkey: Pubkey) -> Entry<K, V> {
+    pub fn entry(&mut self, pubkey: Pubkey) -> Entry<K, AccountMapEntry<V>> {
         self.map.entry(pubkey)
     }
 
-    pub fn iter(&self) -> Iter<K, V> {
+    pub fn iter(&self) -> Iter<K, AccountMapEntry<V>> {
         self.map.iter()
     }
 
-    pub fn keys(&self) -> Keys<K, V> {
+    pub fn keys(&self) -> Keys<K, AccountMapEntry<V>> {
         self.map.keys()
     }
 
-    pub fn get(&self, key: &K) -> Option<&V> {
+    pub fn get(&self, key: &K) -> Option<&AccountMapEntry<V>> {
         self.map.get(key)
     }
 
