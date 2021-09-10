a7fd0aac8a602281aed5e5645a8cfa9b5eba08d3
Brooks Prumo <brooks@solana.com>
2021-09-10 12:10:08 -0500

    fixup

--- validator/src/main.rs
+++ validator/src/main.rs
@@ -2726,20 +2726,21 @@ pub fn main() {
                 })
             });
 
+    let incremental_snapshot_interval_slots =
+        value_t_or_exit!(matches, "incremental_snapshot_interval_slots", u64);
     let (full_snapshot_archive_interval_slots, incremental_snapshot_archive_interval_slots) =
-        let incremental_snapshot_interval_slots = value_t_or_exit!(matches, "incremental_snapshot_interval_slots", u64);
-    if incremental_snapshot_interval_slots > 0 {
-        if matches.is_present("incremental_snapshots") {
-            (
-                value_t_or_exit!(matches, "full_snapshot_interval_slots", u64),
-                incremental_snapshot_interval_slots,
-            )
+        if incremental_snapshot_interval_slots > 0 {
+            if matches.is_present("incremental_snapshots") {
+                (
+                    value_t_or_exit!(matches, "full_snapshot_interval_slots", u64),
+                    incremental_snapshot_interval_slots,
+                )
+            } else {
+                (incremental_snapshot_interval_slots, Slot::MAX)
+            }
         } else {
-            (incremental_snapshot_interval_slots, Slot::MAX)
-        }
-    } else {
-        (Slot::MAX, Slot::MAX)
-    };
+            (Slot::MAX, Slot::MAX)
+        };
 
     validator_config.snapshot_config = Some(SnapshotConfig {
         full_snapshot_archive_interval_slots,
