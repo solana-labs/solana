9812a32dd57a92085c0242725da15beeb5e35968
Stephen Akridge <sakridge@gmail.com>
2021-09-15 11:21:02 -0700

    Add transaction dos

--- transaction-executor/src/lib.rs
+++ transaction-executor/src/lib.rs
@@ -1,6 +1,6 @@
 #![allow(clippy::integer_arithmetic)]
 use log::*;
-use solana_client::{rpc_client::RpcClient, rpc_config::RpcSendTransactionConfig};
+use solana_client::{tpu_client::{TpuClientConfig, TpuClient}, rpc_client::RpcClient, rpc_config::RpcSendTransactionConfig};
 use solana_measure::measure::Measure;
 use solana_sdk::{
     commitment_config::CommitmentConfig, signature::Signature, timing::timestamp,
@@ -12,6 +12,7 @@ use std::{
         atomic::{AtomicBool, AtomicU64, Ordering},
         Arc, RwLock,
     },
+    collections::VecDeque,
     thread::{sleep, Builder, JoinHandle},
     time::{Duration, Instant},
 };
@@ -21,30 +22,33 @@ type PendingQueue = Vec<(Signature, u64, u64)>;
 
 pub struct TransactionExecutor {
     sig_clear_t: JoinHandle<()>,
+    transfer_t: JoinHandle<()>,
+
+    pending_transactions: Arc<RwLock<VecDeque<Vec<(u64, Transaction)>>>>,
+
     sigs: Arc<RwLock<PendingQueue>>,
     cleared: Arc<RwLock<Vec<u64>>>,
+
     exit: Arc<AtomicBool>,
     counter: AtomicU64,
-    client: RpcClient,
-    skip_preflight: bool,
 }
 
 impl TransactionExecutor {
-    pub fn new(entrypoint_addr: SocketAddr, skip_preflight: bool) -> Self {
+    pub fn new(entrypoint_addr: SocketAddr, skip_preflight: bool, use_tpu_client: bool, tpu_client_fanout_slots: Option<u64>) -> Self {
         let sigs = Arc::new(RwLock::new(Vec::new()));
         let cleared = Arc::new(RwLock::new(Vec::new()));
         let exit = Arc::new(AtomicBool::new(false));
+        let pending_transactions = Arc::new(RwLock::new(VecDeque::new()));
         let sig_clear_t = Self::start_sig_clear_thread(&exit, &sigs, &cleared, entrypoint_addr);
-        let client =
-            RpcClient::new_socket_with_commitment(entrypoint_addr, CommitmentConfig::confirmed());
+        let transfer_t = Self::start_transfer_thread(&exit, &sigs, &pending_transactions, skip_preflight, &cleared, entrypoint_addr, use_tpu_client, tpu_client_fanout_slots);
         Self {
             sigs,
             cleared,
             sig_clear_t,
             exit,
             counter: AtomicU64::new(0),
-            client,
-            skip_preflight,
+            pending_transactions,
+            transfer_t,
         }
     }
 
@@ -52,28 +56,93 @@ impl TransactionExecutor {
         self.sigs.read().unwrap().len()
     }
 
+    fn start_transfer_thread(
+        exit: &Arc<AtomicBool>,
+        sigs: &Arc<RwLock<PendingQueue>>,
+        pending_transactions: &Arc<RwLock<VecDeque<Vec<(u64, Transaction)>>>>,
+        skip_preflight: bool,
+        cleared: &Arc<RwLock<Vec<u64>>>,
+        entrypoint_addr: SocketAddr,
+        use_tpu_client: bool,
+        tpu_client_fanout_slots: Option<u64>,
+    ) -> JoinHandle<()> {
+        let exit = exit.clone();
+        let sigs = sigs.clone();
+        let pending_transactions = pending_transactions.clone();
+        let cleared = cleared.clone();
+        Builder::new()
+            .name("transfer".to_string())
+            .spawn(move || {
+                let client =
+                    Arc::new(RpcClient::new_socket_with_commitment(entrypoint_addr, CommitmentConfig::confirmed()));
+                let tpu_client = if use_tpu_client {
+                    let mut tpu_config = TpuClientConfig::default();
+                    if let Some(fanout_slots) = tpu_client_fanout_slots {
+                        tpu_config.fanout_slots = fanout_slots;
+                    }
+                    Some(TpuClient::new(client.clone(), "", tpu_config).unwrap())
+                } else {
+                    None
+                };
+
+                while !exit.load(Ordering::Relaxed) {
+                    let txs = {
+                        let mut pending_transactions = pending_transactions.write().unwrap();
+                        pending_transactions.pop_front()
+                    };
+                    if let Some(txs) = txs {
+                        let mut errored_ids = Vec::new();
+                        let new_sigs = txs.into_iter().filter_map(|(id, tx)| {
+                            match tpu_client.as_ref() {
+                                Some(tpu_client) => {
+                                    if tpu_client.send_transaction(&tx) {
+                                        return Some((tx.signatures[0], timestamp(), id));
+                                    }
+                                },
+                                None => {
+                                    let config = RpcSendTransactionConfig {
+                                        skip_preflight,
+                                        ..RpcSendTransactionConfig::default()
+                                    };
+                                    match client.send_transaction_with_config(&tx, config) {
+                                        Ok(sig) => {
+                                            return Some((sig, timestamp(), id));
+                                        }
+                                        Err(e) => {
+                                            info!("error: {:#?}", e);
+                                            errored_ids.push(id);
+                                        }
+                                    }
+                                }
+                            }
+                            None
+                        });
+                        {
+                            let mut sigs_w = sigs.write().unwrap();
+                            sigs_w.extend(new_sigs);
+                        }
+                        {
+                            let mut cleared = cleared.write().unwrap();
+                            cleared.extend(errored_ids);
+                        }
+                    } else {
+                        sleep(Duration::from_millis(10));
+                    }
+                }
+            }).unwrap()
+    }
+
     pub fn push_transactions(&self, txs: Vec<Transaction>) -> Vec<u64> {
-        let mut ids = vec![];
-        let new_sigs = txs.into_iter().filter_map(|tx| {
-            let id = self.counter.fetch_add(1, Ordering::Relaxed);
-            ids.push(id);
-            let config = RpcSendTransactionConfig {
-                skip_preflight: self.skip_preflight,
-                ..RpcSendTransactionConfig::default()
-            };
-            match self.client.send_transaction_with_config(&tx, config) {
-                Ok(sig) => {
-                    return Some((sig, timestamp(), id));
-                }
-                Err(e) => {
-                    info!("error: {:#?}", e);
-                }
-            }
-            None
-        });
-        let mut sigs_w = self.sigs.write().unwrap();
-        sigs_w.extend(new_sigs);
-        ids
+        let txs_len = txs.len() as u64;
+        let latest_id = self.counter.fetch_add(txs_len, Ordering::Relaxed);
+        let new_ids: Vec<_> = (latest_id..latest_id + txs_len).into_iter().collect();
+        assert_eq!(new_ids.len(), txs.len());
+        let txs: Vec<_> = new_ids.clone().into_iter().zip(txs.into_iter()).collect();
+
+        let mut pending_transactions = self.pending_transactions.write().unwrap();
+        pending_transactions.push_back(txs);
+
+        new_ids
     }
 
     pub fn drain_cleared(&self) -> Vec<u64> {
@@ -83,6 +152,7 @@ impl TransactionExecutor {
     pub fn close(self) {
         self.exit.store(true, Ordering::Relaxed);
         self.sig_clear_t.join().unwrap();
+        self.transfer_t.join().unwrap();
     }
 
     fn start_sig_clear_thread(
