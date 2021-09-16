b22f78e072cf0f7107851b08e58c2e3fead3f64d
Brooks Prumo <brooks@solana.com>
2021-09-16 16:19:44 -0500

    remove specializations (i don't think they are needed)

--- bucket_map/src/lib.rs
+++ bucket_map/src/lib.rs
@@ -1,6 +1,3 @@
-#![cfg_attr(RUSTC_WITH_SPECIALIZATION, feature(min_specialization))]
-#![allow(clippy::integer_arithmetic)]
-#![allow(clippy::mut_from_ref)]
 mod bucket;
 pub mod bucket_map;
 mod bucket_stats;
