a450bdfc6bd4e2a1ba05a839dda01f3310919570
Brooks Prumo <brooks@solana.com>
2021-09-17 21:11:51 -0500

    Fixup atomic ordering on BucketStorage Header
    Lock-type operations should be atomic::acquire, and unlock-type
    operations should be atomic::release, so setting that here.
    
    Additionally, change `unlock()` so it cannot fail.

--- bucket_map/src/bucket.rs
+++ bucket_map/src/bucket.rs
@@ -240,7 +240,7 @@ impl<T: Clone + Copy> Bucket<T> {
                 if best_bucket.uid(ix) == 0 {
                     let elem_loc = elem.data_loc(current_bucket);
                     if elem.num_slots > 0 {
-                        current_bucket.free(elem_loc, elem_uid).unwrap();
+                        current_bucket.free(elem_loc, elem_uid);
                     }
                     // elem: &mut IndexEntry = self.index.get_mut(elem_ix);
                     elem.storage_offset = ix;
@@ -266,10 +266,10 @@ impl<T: Clone + Copy> Bucket<T> {
                 let data_bucket = &self.data[elem.data_bucket_ix() as usize];
                 let loc = elem.data_loc(data_bucket);
                 //debug!(                    "DATA FREE {:?} {} {} {}",                    key, elem.data_location, data_bucket.capacity, elem_uid                );
-                data_bucket.free(loc, elem_uid).unwrap();
+                data_bucket.free(loc, elem_uid);
             }
             //debug!("INDEX FREE {:?} {}", key, elem_uid);
-            self.index.free(elem_ix, elem_uid).unwrap();
+            self.index.free(elem_ix, elem_uid);
         }
     }
 
