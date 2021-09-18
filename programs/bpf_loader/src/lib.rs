df349782c9cdf6a494d031cc3a2e370bd0dfd6f0
Justin Starry <justin@solana.com>
2021-09-17 20:08:47 -0500

    Update feature switch for reduced required deploy balance

--- programs/bpf_loader/src/lib.rs
+++ programs/bpf_loader/src/lib.rs
@@ -32,7 +32,7 @@ use solana_sdk::{
     entrypoint::{HEAP_LENGTH, SUCCESS},
     feature_set::{
         add_missing_program_error_mappings, close_upgradeable_program_accounts, fix_write_privs,
-        stop_verify_mul64_imm_nonzero,
+        reduce_required_deploy_balance, stop_verify_mul64_imm_nonzero,
     },
     ic_logger_msg, ic_msg,
     instruction::{AccountMeta, InstructionError},
@@ -390,7 +390,10 @@ fn process_loader_upgradeable_instruction(
                 return Err(InstructionError::InvalidArgument);
             }
 
-            if invoke_context.is_feature_active(&fix_write_privs::id()) {
+            let predrain_buffer = invoke_context
+                .is_feature_active(&reduce_required_deploy_balance::id())
+                && invoke_context.is_feature_active(&fix_write_privs::id());
+            if predrain_buffer {
                 // Drain the Buffer account to payer before paying for programdata account
                 payer
                     .try_account_ref_mut()?
@@ -448,7 +451,7 @@ fn process_loader_upgradeable_instruction(
             })?;
             program.try_account_ref_mut()?.set_executable(true);
 
-            if !invoke_context.is_feature_active(&fix_write_privs::id()) {
+            if !predrain_buffer {
                 // Drain the Buffer account back to the payer
                 payer
                     .try_account_ref_mut()?
