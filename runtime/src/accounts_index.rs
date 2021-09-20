0c0fb13b51dfe328e9aa79ce413b0048304e936f
Jeff Washington (jwash) <wash678@gmail.com>
2021-09-19 20:25:47 -0500

    AcctIdx: accessors for age

--- runtime/src/accounts_index.rs
+++ runtime/src/accounts_index.rs
@@ -174,6 +174,14 @@ impl<T: IndexValue> AccountMapEntryInner<T> {
     pub fn set_dirty(&self, value: bool) -> bool {
         self.meta.dirty.swap(value, Ordering::Acquire)
     }
+
+    pub fn age(&self) -> Age {
+        self.meta.age.load(Ordering::Relaxed) // todo: check ordering
+    }
+
+    pub fn set_age(&self, value: Age) {
+        self.meta.age.store(value, Ordering::Relaxed) // todo: check ordering
+    }
 }
 
 pub enum AccountIndexGetResult<'a, T: IndexValue> {
