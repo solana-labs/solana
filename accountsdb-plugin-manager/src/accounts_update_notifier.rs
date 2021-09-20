30e4058228ef471ed587b04672822dbfb5acfecb
Lijun Wang <83639177+lijunwangs@users.noreply.github.com>
2021-09-19 18:21:06 -0700

    Accounts Select for accountsdb plugin

--- accountsdb-plugin-manager/src/accounts_update_notifier.rs
+++ accountsdb-plugin-manager/src/accounts_update_notifier.rs
@@ -1,3 +1,5 @@
+use std::str::FromStr;
+
 /// Module responsible for notifying the plugins of accounts update
 use {
     crate::accountsdb_plugin_manager::AccountsDbPluginManager,
@@ -9,19 +11,78 @@ use {
         accounts_cache::CachedAccount, accounts_db::LoadedAccount, append_vec::StoredAccountMeta,
         bank_forks::BankForks,
     },
-    solana_sdk::account::Account,
     solana_sdk::clock::Slot,
-    std::sync::{Arc, RwLock},
+    solana_sdk::{
+        account::{Account, ReadableAccount},
+        pubkey::Pubkey,
+    },
+    std::{
+        collections::HashSet,
+        sync::{Arc, RwLock},
+    },
 };
 
 pub(crate) struct AccountsUpdateNotifier {
     plugin_manager: Arc<RwLock<AccountsDbPluginManager>>,
     bank_forks: Arc<RwLock<BankForks>>,
+    accounts_selector: AccountsSelector,
+}
+
+pub(crate) struct AccountsSelector {
+    pub accounts: HashSet<Pubkey>,
+    pub owners: HashSet<Pubkey>,
+    pub select_all_accounts: bool,
+}
+
+impl AccountsSelector {
+    pub fn default() -> Self {
+        AccountsSelector {
+            accounts: HashSet::default(),
+            owners: HashSet::default(),
+            select_all_accounts: true,
+        }
+    }
+
+    pub fn new(accounts: &[String], owners: &[String]) -> Self {
+        let select_all_accounts = accounts.iter().any(|key| key == "*");
+        if select_all_accounts {
+            return AccountsSelector {
+                accounts: HashSet::default(),
+                owners: HashSet::default(),
+                select_all_accounts,
+            };
+        }
+        let accounts = accounts
+            .iter()
+            .map(|pubkey| Pubkey::from_str(pubkey).unwrap())
+            .collect();
+        let owners = owners
+            .iter()
+            .map(|pubkey| Pubkey::from_str(pubkey).unwrap())
+            .collect();
+        AccountsSelector {
+            accounts,
+            owners,
+            select_all_accounts,
+        }
+    }
+
+    pub fn is_account_selected(&self, account: &Pubkey, owner: &Pubkey) -> bool {
+        self.select_all_accounts || self.accounts.contains(account) || self.owners.contains(owner)
+    }
 }
 
 fn accountinfo_from_stored_account_meta(
+    accounts_selector: &AccountsSelector,
     stored_account_meta: &StoredAccountMeta,
-) -> ReplicaAccountInfo {
+) -> Option<ReplicaAccountInfo> {
+    if !accounts_selector.is_account_selected(
+        &stored_account_meta.meta.pubkey,
+        &stored_account_meta.account_meta.owner,
+    ) {
+        return None;
+    }
+
     let account_meta = ReplicaAccountMeta {
         pubkey: bs58::encode(stored_account_meta.meta.pubkey).into_string(),
         lamports: stored_account_meta.account_meta.lamports,
@@ -30,14 +91,23 @@ fn accountinfo_from_stored_account_meta(
         rent_epoch: stored_account_meta.account_meta.rent_epoch,
     };
     let data = stored_account_meta.data.to_vec();
-    ReplicaAccountInfo {
+    Some(ReplicaAccountInfo {
         account_meta,
         hash: bs58::encode(stored_account_meta.hash.0).into_string(),
         data,
-    }
+    })
 }
 
-fn accountinfo_from_cached_account(cached_account: &CachedAccount) -> ReplicaAccountInfo {
+fn accountinfo_from_cached_account(
+    accounts_selector: &AccountsSelector,
+    cached_account: &CachedAccount,
+) -> Option<ReplicaAccountInfo> {
+    if !accounts_selector
+        .is_account_selected(&cached_account.pubkey(), cached_account.account.owner())
+    {
+        return None;
+    }
+
     let account = Account::from(cached_account.account.clone());
     let account_meta = ReplicaAccountMeta {
         pubkey: bs58::encode(cached_account.pubkey()).into_string(),
@@ -47,21 +117,23 @@ fn accountinfo_from_cached_account(cached_account: &CachedAccount) -> ReplicaAcc
         rent_epoch: account.rent_epoch,
     };
     let data = account.data.to_vec();
-    ReplicaAccountInfo {
+    Some(ReplicaAccountInfo {
         account_meta,
         hash: bs58::encode(cached_account.hash().0).into_string(),
         data,
-    }
+    })
 }
 
 impl AccountsUpdateNotifier {
     pub fn new(
         plugin_manager: Arc<RwLock<AccountsDbPluginManager>>,
         bank_forks: Arc<RwLock<BankForks>>,
+        accounts_selector: AccountsSelector,
     ) -> Self {
         AccountsUpdateNotifier {
             plugin_manager,
             bank_forks,
+            accounts_selector,
         }
     }
     pub fn notify_slot_confirmed(&self, slot: Slot) {
@@ -77,10 +149,13 @@ impl AccountsUpdateNotifier {
             Some(bank) => {
                 let accounts = bank.rc.accounts.scan_slot(slot, |account| match account {
                     LoadedAccount::Stored(stored_account_meta) => {
-                        Some(accountinfo_from_stored_account_meta(&stored_account_meta))
+                        accountinfo_from_stored_account_meta(
+                            &self.accounts_selector,
+                            &stored_account_meta,
+                        )
                     }
                     LoadedAccount::Cached((_pubkey, cached_account)) => {
-                        Some(accountinfo_from_cached_account(&cached_account))
+                        accountinfo_from_cached_account(&self.accounts_selector, &cached_account)
                     }
                 });
 
--- accountsdb-plugin-manager/src/accountsdb_plugin_service.rs
+++ accountsdb-plugin-manager/src/accountsdb_plugin_service.rs
@@ -1,6 +1,6 @@
 use {
     crate::{
-        accounts_update_notifier::AccountsUpdateNotifier,
+        accounts_update_notifier::{AccountsSelector, AccountsUpdateNotifier},
         accountsdb_plugin_manager::AccountsDbPluginManager,
         confirmed_slots_observer::SlotConfirmationObserver,
     },
@@ -18,12 +18,41 @@ use {
     },
 };
 
+/// The service managing the AccountsDb plugin workflow.
 pub struct AccountsDbPluginService {
     confirmed_slots_observer: SlotConfirmationObserver,
     plugin_manager: Arc<RwLock<AccountsDbPluginManager>>,
 }
 
 impl AccountsDbPluginService {
+    /// Creates and returns the AccountsDbPluginService.
+    /// # Arguments
+    /// * `confirmed_bank_receiver` - The receiver for confirmed bank notification
+    /// * `bank_forks` - The bank forks.
+    /// * `accountsdb_plugin_config_file` - The config file path for the plugin. The
+    ///    config file controls what accounts to stream and the plugin responsible
+    ///    for transporting the data to external data stores. It is defined in JSON format.
+    ///    The `libpath` field should be pointed to the full path of the dynamic shared library
+    ///    (.so file) to be loaded. The shared library must implement the `AccountsDbPlugin`
+    ///    trait. And the shared library shall export a `C` function `_create_plugin` which
+    ///    shall create the implementation of `AccountsDbPlugin` and returns to the caller.
+    ///    The `accounts_selector` section allows the user to controls accounts selections.
+    ///    accounts_selector = {
+    ///         accounts = \['pubkey-1', 'pubkey-2', ..., 'pubkey-n'\],
+    ///    }
+    ///    or:
+    ///    accounts_selector = {
+    ///         owners = \['pubkey-1', 'pubkey-2', ..., 'pubkey-m'\]
+    ///    }
+    ///    When accounts and owners are specified, the program only filters only using accounts
+    ///    as it is more selective. And owners field is ignored. When only owners is specified,
+    ///    all accounts belonging to the owners will be streamed.
+    ///    The rest of the JSON fields's definition is upto to the concrete plugin implementation
+    ///    It is usually used to configure the connection information for the external data store.
+    ///    The accounts field support wildcard to select all accounts:
+    ///    accounts_selector = {
+    ///         accounts = \['*'\],
+    ///    }
     pub fn new(
         confirmed_bank_receiver: Receiver<Slot>,
         bank_forks: Arc<RwLock<BankForks>>,
@@ -36,16 +65,45 @@ impl AccountsDbPluginService {
         let plugin_manager = AccountsDbPluginManager::new();
         let plugin_manager = Arc::new(RwLock::new(plugin_manager));
 
-        let accounts_update_notifier =
-            AccountsUpdateNotifier::new(plugin_manager.clone(), bank_forks);
-        let confirmed_slots_observer =
-            SlotConfirmationObserver::new(confirmed_bank_receiver, accounts_update_notifier);
-
         let mut file = File::open(accountsdb_plugin_config_file).unwrap();
         let mut contents = String::new();
         file.read_to_string(&mut contents).unwrap();
 
         let result: serde_json::Value = serde_json::from_str(&contents).unwrap();
+        let accounts_selector = &result["accounts_selector"];
+
+        let accounts_selector = if accounts_selector.is_null() {
+            AccountsSelector::default()
+        } else {
+            let accounts = &accounts_selector["accounts"];
+            let accounts: Vec<String> = if accounts.is_array() {
+                accounts
+                    .as_array()
+                    .unwrap()
+                    .iter()
+                    .map(|val| val.to_string())
+                    .collect()
+            } else {
+                Vec::default()
+            };
+            let owners = &accounts_selector["owners"];
+            let owners: Vec<String> = if owners.is_array() {
+                owners
+                    .as_array()
+                    .unwrap()
+                    .iter()
+                    .map(|val| val.to_string())
+                    .collect()
+            } else {
+                Vec::default()
+            };
+            AccountsSelector::new(&accounts, &owners)
+        };
+
+        let accounts_update_notifier =
+            AccountsUpdateNotifier::new(plugin_manager.clone(), bank_forks, accounts_selector);
+        let confirmed_slots_observer =
+            SlotConfirmationObserver::new(confirmed_bank_receiver, accounts_update_notifier);
 
         unsafe {
             plugin_manager
