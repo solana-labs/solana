c17c74021c3d9ce8cb759a6b29df5a87e7007a29
Tao Zhu <82401714+taozhu-chicago@users.noreply.github.com>
2021-09-20 23:00:33 -0500

    sigverify to identify and mark simple vote transaction (#20021)
    * sigverify to identify and mark simple vote transaction
    
    * check vote tx at get_packet_offsets to cover both cpu and gpu paths
    
    * add pubkey_len to PacketOffsets to reduce the redundant bytes counting

--- perf/src/test_tx.rs
+++ perf/src/test_tx.rs
@@ -5,6 +5,7 @@ use solana_sdk::system_instruction::SystemInstruction;
 use solana_sdk::system_program;
 use solana_sdk::system_transaction;
 use solana_sdk::transaction::Transaction;
+use solana_vote_program::vote_transaction;
 
 pub fn test_tx() -> Transaction {
     let keypair1 = Keypair::new();
@@ -36,5 +37,18 @@ pub fn test_multisig_tx() -> Transaction {
         blockhash,
         program_ids,
         instructions,
+    )
+}
+
+pub fn vote_tx() -> Transaction {
+    let keypair = Keypair::new();
+    vote_transaction::new_vote_transaction(
+        vec![2],
+        Hash::default(),
+        Hash::default(),
+        &keypair,
+        &keypair,
+        &keypair,
+        None,
     )
 }
