cc7fc220d11c5c0c449c7db2f64247f99fdb1420
Michael Vines <mvines@gmail.com>
2021-09-21 19:58:14 -0700

    Add CloseAccountData proof program plumbing

--- programs/zk-token-proof/src/lib.rs
+++ programs/zk-token-proof/src/lib.rs
@@ -1,12 +1,28 @@
 #![forbid(unsafe_code)]
 
 use {
+    bytemuck::Pod,
     solana_sdk::{
         ic_msg, instruction::InstructionError, process_instruction::InvokeContext, pubkey::Pubkey,
     },
     spl_zk_token_crypto::instruction::*,
     std::result::Result,
 };
+
+fn verify<T: Pod + Verifiable>(
+    input: &[u8],
+    invoke_context: &mut dyn InvokeContext,
+) -> Result<(), InstructionError> {
+    let proof = ProofInstruction::decode_data::<T>(input).ok_or_else(|| {
+        ic_msg!(invoke_context, "invalid proof data");
+        InstructionError::InvalidInstructionData
+    })?;
+
+    proof.verify().map_err(|err| {
+        ic_msg!(invoke_context, "proof verification failed: {:?}", err);
+        InstructionError::InvalidInstructionData
+    })
+}
 
 pub fn process_instruction(
     program_id: &Pubkey,
@@ -16,15 +32,13 @@ pub fn process_instruction(
     match ProofInstruction::decode_type(program_id, input)
         .ok_or(InstructionError::InvalidInstructionData)?
     {
-        ProofInstruction::VerifyUpdateAccountPkData => {
-            ic_msg!(invoke_context, "VerifyUpdateAccountPkData");
-            let proof = ProofInstruction::decode_data::<UpdateAccountPkData>(input)
-                .ok_or(InstructionError::InvalidInstructionData)?;
-
-            proof.verify().map_err(|err| {
-                ic_msg!(invoke_context, "proof verification failed: {:?}", err);
-                InstructionError::InvalidInstructionData
-            })
+        ProofInstruction::VerifyUpdateAccountPk => {
+            ic_msg!(invoke_context, "VerifyUpdateAccountPk");
+            verify::<UpdateAccountPkData>(input, invoke_context)
+        }
+        ProofInstruction::VerifyCloseAccount => {
+            ic_msg!(invoke_context, "VerifyCloseAccount");
+            verify::<CloseAccountData>(input, invoke_context)
         }
     }
 }
