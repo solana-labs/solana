ad7ad7cb2743701da94c225ed145b5f702da024d
Lijun Wang <83639177+lijunwangs@users.noreply.github.com>
2021-09-28 16:16:29 -0700

    Addressed some code review from Trent -- do direct notify of slot status change

--- accountsdb-plugin-manager/src/slot_status_observer.rs
+++ accountsdb-plugin-manager/src/slot_status_observer.rs
@@ -1,37 +1,19 @@
 use {
     crossbeam_channel::Receiver,
-    solana_accountsdb_plugin_interface::accountsdb_plugin_interface::SlotStatus,
     solana_rpc::optimistically_confirmed_bank_tracker::BankNotification,
     solana_runtime::accounts_update_notifier_interface::AccountsUpdateNotifier,
-    solana_sdk::clock::Slot,
     std::{
-        collections::VecDeque,
         sync::{
             atomic::{AtomicBool, Ordering},
-            Arc, RwLock,
+            Arc,
         },
-        thread::{self, sleep, Builder, JoinHandle},
-        time::Duration,
+        thread::{self, Builder, JoinHandle},
     },
 };
-
-struct SlotSetEntry {
-    slot: Slot,
-    parent: Option<Slot>,
-    status: SlotStatus,
-}
-
-/// The structure modelling the slots eligible for replication and
-/// their states.
-#[derive(Default, Clone)]
-struct EligibleSlotSet {
-    slot_set: Arc<RwLock<VecDeque<SlotSetEntry>>>,
-}
 
 #[derive(Debug)]
 pub(crate) struct SlotStatusObserver {
     confirmed_bank_receiver_service: Option<JoinHandle<()>>,
-    plugin_notify_service: Option<JoinHandle<()>>,
     exit_updated_slot_server: Arc<AtomicBool>,
 }
 
@@ -40,17 +22,11 @@ impl SlotStatusObserver {
         confirmed_bank_receiver: Receiver<BankNotification>,
         accounts_update_notifier: AccountsUpdateNotifier,
     ) -> Self {
-        let eligible_slot_set = EligibleSlotSet::default();
         let exit_updated_slot_server = Arc::new(AtomicBool::new(false));
 
         Self {
             confirmed_bank_receiver_service: Some(Self::run_confirmed_bank_receiver(
                 confirmed_bank_receiver,
-                eligible_slot_set.clone(),
-                exit_updated_slot_server.clone(),
-            )),
-            plugin_notify_service: Some(Self::run_plugin_notify_service(
-                eligible_slot_set,
                 exit_updated_slot_server.clone(),
                 accounts_update_notifier,
             )),
@@ -64,95 +40,39 @@ impl SlotStatusObserver {
             .take()
             .map(JoinHandle::join)
             .unwrap()
-            .expect("confirmed_bank_receiver_service");
-
-        self.plugin_notify_service
-            .take()
-            .map(JoinHandle::join)
-            .unwrap()
     }
 
     fn run_confirmed_bank_receiver(
         confirmed_bank_receiver: Receiver<BankNotification>,
-        eligible_slot_set: EligibleSlotSet,
         exit: Arc<AtomicBool>,
+        accounts_update_notifier: AccountsUpdateNotifier,
     ) -> JoinHandle<()> {
         Builder::new()
             .name("confirmed_bank_receiver".to_string())
             .spawn(move || {
                 while !exit.load(Ordering::Relaxed) {
                     if let Ok(slot) = confirmed_bank_receiver.recv() {
-                        let mut slot_set = eligible_slot_set.slot_set.write().unwrap();
                         match slot {
                             BankNotification::OptimisticallyConfirmed(slot) => {
-                                slot_set.push_back(SlotSetEntry {
-                                    slot,
-                                    parent: None,
-                                    status: SlotStatus::Confirmed,
-                                });
+                                accounts_update_notifier
+                                    .read()
+                                    .unwrap()
+                                    .notify_slot_confirmed(slot, None);
                             }
                             BankNotification::Frozen(bank) => {
-                                slot_set.push_back(SlotSetEntry {
-                                    slot: bank.slot(),
-                                    parent: Some(bank.parent_slot()),
-                                    status: SlotStatus::Processed,
-                                });
+                                accounts_update_notifier
+                                    .read()
+                                    .unwrap()
+                                    .notify_slot_processed(bank.slot(), Some(bank.parent_slot()));
                             }
                             BankNotification::Root(bank) => {
-                                slot_set.push_back(SlotSetEntry {
-                                    slot: bank.slot(),
-                                    parent: Some(bank.parent_slot()),
-                                    status: SlotStatus::Rooted,
-                                });
+                                accounts_update_notifier
+                                    .read()
+                                    .unwrap()
+                                    .notify_slot_rooted(bank.slot(), Some(bank.parent_slot()));
                             }
                         }
                     }
-                }
-            })
-            .unwrap()
-    }
-
-    fn run_plugin_notify_service(
-        eligible_slot_set: EligibleSlotSet,
-        exit: Arc<AtomicBool>,
-        accounts_update_notifier: AccountsUpdateNotifier,
-    ) -> JoinHandle<()> {
-        Builder::new()
-            .name("plugin_notify_service".to_string())
-            .spawn(move || {
-                while !exit.load(Ordering::Relaxed) {
-                    let mut slot_set = eligible_slot_set.slot_set.write().unwrap();
-
-                    loop {
-                        let slot_status = slot_set.pop_front();
-                        if slot_status.is_none() {
-                            break;
-                        }
-
-                        let slot_status = slot_status.unwrap();
-                        match slot_status.status {
-                            SlotStatus::Confirmed => {
-                                accounts_update_notifier
-                                    .read()
-                                    .unwrap()
-                                    .notify_slot_confirmed(slot_status.slot, slot_status.parent);
-                            }
-                            SlotStatus::Processed => {
-                                accounts_update_notifier
-                                    .read()
-                                    .unwrap()
-                                    .notify_slot_processed(slot_status.slot, slot_status.parent);
-                            }
-                            SlotStatus::Rooted => {
-                                accounts_update_notifier
-                                    .read()
-                                    .unwrap()
-                                    .notify_slot_rooted(slot_status.slot, slot_status.parent);
-                            }
-                        }
-                    }
-                    drop(slot_set);
-                    sleep(Duration::from_millis(200));
                 }
             })
             .unwrap()
