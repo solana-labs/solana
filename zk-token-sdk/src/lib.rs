6202b3c1d2a0900f456d89901cf46352f4400f5a
Michael Vines <mvines@gmail.com>
2021-10-01 09:49:33 -0700

    Update lib.rs

--- zk-token-sdk/src/lib.rs
+++ zk-token-sdk/src/lib.rs
@@ -1,13 +1,10 @@
 #[cfg(not(target_arch = "bpf"))]
 #[macro_use]
 pub(crate) mod macros;
-
 #[cfg(not(target_arch = "bpf"))]
 pub mod encryption;
-
 #[cfg(not(target_arch = "bpf"))]
 mod errors;
-
 #[cfg(not(target_arch = "bpf"))]
 mod range_proof;
 #[cfg(not(target_arch = "bpf"))]
