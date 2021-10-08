42f6369faf8e6020a7f6b68e114df3332bf47c66
Tyera Eulberg <tyera@solana.com>
2021-10-08 14:33:11 -0600

    Use statement cleanup

--- install/src/stop_process.rs
+++ install/src/stop_process.rs
@@ -1,5 +1,4 @@
-use std::io;
-use std::process::Child;
+use std::{io, process::Child};
 
 fn kill_process(process: &mut Child) -> Result<(), io::Error> {
     if let Ok(()) = process.kill() {
@@ -17,12 +16,18 @@ pub fn stop_process(process: &mut Child) -> Result<(), io::Error> {
 
 #[cfg(not(windows))]
 pub fn stop_process(process: &mut Child) -> Result<(), io::Error> {
-    use nix::errno::Errno::{EINVAL, EPERM, ESRCH};
-    use nix::sys::signal::{kill, Signal};
-    use nix::unistd::Pid;
-    use std::io::ErrorKind;
-    use std::thread;
-    use std::time::{Duration, Instant};
+    use {
+        nix::{
+            errno::Errno::{EINVAL, EPERM, ESRCH},
+            sys::signal::{kill, Signal},
+            unistd::Pid,
+        },
+        std::{
+            io::ErrorKind,
+            thread,
+            time::{Duration, Instant},
+        },
+    };
 
     let nice_wait = Duration::from_secs(5);
     let pid = Pid::from_raw(process.id() as i32);
