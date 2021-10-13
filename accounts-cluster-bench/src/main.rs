137201f87c4e4f908a20fbdea4f6b2b915ae8664
Jack May <jack@solana.com>
2021-10-12 17:36:35 -0700

    feedback

--- accounts-cluster-bench/src/main.rs
+++ accounts-cluster-bench/src/main.rs
@@ -375,7 +375,7 @@ fn run_accounts_bench(
     let mut latest_blockhash = Instant::now();
     let mut last_log = Instant::now();
     let mut count = 0;
-    let blockhash = client.get_latest_blockhash().expect("blockhash");
+    let mut blockhash = client.get_latest_blockhash().expect("blockhash");
     let mut tx_sent_count = 0;
     let mut total_accounts_created = 0;
     let mut total_accounts_closed = 0;
@@ -422,6 +422,7 @@ fn run_accounts_bench(
 
     loop {
         if latest_blockhash.elapsed().as_millis() > 10_000 {
+            blockhash = client.get_latest_blockhash().expect("blockhash");
             latest_blockhash = Instant::now();
         }
 
