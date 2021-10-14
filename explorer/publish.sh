47ef12f19bf543244675d6dba87b8e34b060676c
joeaba <77398477+joeaba@users.noreply.github.com>
2021-10-14 17:20:10 +0530

    adding publish.sh
    it will trigger a build on vercel

---
+++ explorer/publish.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+set -ex
+
+if [[ -d .vercel ]]; then
+  rm -r .vercel
+fi
+
+CONFIG_FILE=vercel.json
+PROJECT_NAME=explorer
+
+cat > "$CONFIG_FILE" <<EOF
+{
+  "name": "$PROJECT_NAME",
+  "scope": "solana-lab"
+}
+EOF
+
+[[ -n $VERCEL_TOKEN ]] || {
+  echo "VERCEL_TOKEN is undefined.  Needed for Vercel authentication."
+  exit 1
+}
+vercel deploy . --local-config="$CONFIG_FILE" --confirm --token "$VERCEL_TOKEN"
