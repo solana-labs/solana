acab0e909763acf07725d02f8f92335f15086e64
Lijun Wang <83639177+lijunwangs@users.noreply.github.com>
2021-10-16 17:12:11 -0700

    hunting

--- runtime/src/accounts_db.rs
+++ runtime/src/accounts_db.rs
@@ -7131,6 +7131,7 @@ impl AccountsDb {
                     let mut pure_notify: usize = 0;
                     let mut pure_bookeeping: usize = 0;
                     for account in accounts_to_stream.values() {
+                        /*
                         let mut measure = Measure::start("accountsdb-plugin-notifying-accounts");
                         notifier.notify_account_restore_from_snapshot(slot, account);
                         measure.stop();
@@ -7155,6 +7156,7 @@ impl AccountsDb {
                         );
                         pure_bookeeping += measure.as_ms() as usize;
                         total_pure_bookeeping += measure.as_ms() as usize;
+                        */
                     }
                     measure.stop();
                     inc_new_counter_info!(
