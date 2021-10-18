a6e6bc6dc096b70c482e952106742470b8f13523
Brooks Prumo <brooks@solana.com>
2021-10-18 15:29:12 -0500

    Ignore RUSTSEC-2020-0159
    (cherry picked from commit 7baeb04f26b0c9df3eefe0e4d5b4a0a23888bd38)
    
    # Conflicts:
    #	ci/do-audit.sh

--- ci/do-audit.sh
+++ ci/do-audit.sh
@@ -45,5 +45,15 @@ cargo_audit_ignores=(
   # Blocked on jsonrpc removing dependency on unmaintained `websocket`
   # https://github.com/paritytech/jsonrpc/issues/605
   --ignore RUSTSEC-2021-0079
+<<<<<<< HEAD
+=======
+
+  # chrono: Potential segfault in `localtime_r` invocations
+  #
+  # Blocked due to no safe upgrade
+  # https://github.com/chronotope/chrono/issues/499
+  --ignore RUSTSEC-2020-0159
+
+>>>>>>> 7baeb04f2 (Ignore RUSTSEC-2020-0159)
 )
 scripts/cargo-for-all-lock-files.sh stable audit "${cargo_audit_ignores[@]}"
