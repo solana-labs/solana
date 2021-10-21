f2e7f0c32be235c4ad34ac1bd9c35e76b71152fc
Michael Vines <mvines@gmail.com>
2021-10-21 10:49:29 -0700

    Adapt to changes to native program entrypoint

--- programs/zk-token-proof/src/lib.rs
+++ programs/zk-token-proof/src/lib.rs
@@ -2,16 +2,15 @@
 
 use {
     bytemuck::Pod,
-    solana_sdk::{
-        ic_msg, instruction::InstructionError, process_instruction::InvokeContext, pubkey::Pubkey,
-    },
+    solana_program_runtime::{ic_msg, invoke_context::InvokeContext},
+    solana_sdk::instruction::InstructionError,
     spl_zk_token_sdk::zk_token_proof_instruction::*,
     std::result::Result,
 };
 
 fn verify<T: Pod + Verifiable>(
     input: &[u8],
-    invoke_context: &mut dyn InvokeContext,
+    invoke_context: &mut InvokeContext,
 ) -> Result<(), InstructionError> {
     let proof = ProofInstruction::decode_data::<T>(input).ok_or_else(|| {
         ic_msg!(invoke_context, "invalid proof data");
@@ -25,9 +24,9 @@ fn verify<T: Pod + Verifiable>(
 }
 
 pub fn process_instruction(
-    program_id: &Pubkey,
+    _first_instruction_account: usize,
     input: &[u8],
-    invoke_context: &mut dyn InvokeContext,
+    invoke_context: &mut InvokeContext,
 ) -> Result<(), InstructionError> {
     if invoke_context.invoke_depth() != 1 {
         // Not supported as an inner instruction
@@ -40,9 +39,7 @@ pub fn process_instruction(
         compute_meter.borrow_mut().consume(25_000)?; // TODO: Tune the number of units consumed?
     }
 
-    match ProofInstruction::decode_type(program_id, input)
-        .ok_or(InstructionError::InvalidInstructionData)?
-    {
+    match ProofInstruction::decode_type(input).ok_or(InstructionError::InvalidInstructionData)? {
         ProofInstruction::VerifyCloseAccount => {
             ic_msg!(invoke_context, "VerifyCloseAccount");
             verify::<CloseAccountData>(input, invoke_context)
