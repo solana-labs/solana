43168e6365c86fd7d8e6ddf4e0953e76ecfa2b6e
behzad nouri <behzadnouri@gmail.com>
2021-10-26 13:06:55 +0000

    doubles crds unique pubkey capacity (#20947)

--- gossip/src/cluster_info.rs
+++ gossip/src/cluster_info.rs
@@ -131,7 +131,7 @@ pub const DEFAULT_CONTACT_SAVE_INTERVAL_MILLIS: u64 = 60_000;
 /// Minimum serialized size of a Protocol::PullResponse packet.
 const PULL_RESPONSE_MIN_SERIALIZED_SIZE: usize = 161;
 // Limit number of unique pubkeys in the crds table.
-pub(crate) const CRDS_UNIQUE_PUBKEY_CAPACITY: usize = 4096;
+pub(crate) const CRDS_UNIQUE_PUBKEY_CAPACITY: usize = 8192;
 /// Minimum stake that a node should have so that its CRDS values are
 /// propagated through gossip (few types are exempted).
 const MIN_STAKE_FOR_GOSSIP: u64 = solana_sdk::native_token::LAMPORTS_PER_SOL;
