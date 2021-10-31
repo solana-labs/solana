6161a01de2be0b4936d1cad9da7fda7f71b241f8
Colin Ogoo <ogoo.colin@gmail.com>
2021-10-31 21:52:54 +0000

    fix(vote-account): rangeError [ERR_OUT_OF_RANGE] error
    The web3 buffer layout is out-of-date with the current `VoteState` implementation. The buffer layout
    is updated to match the structure in
    https://github.com/solana-labs/solana/blob/master/account-decoder/src/parse_vote.rs
    
    fix #20786

--- web3.js/src/vote-account.ts
+++ web3.js/src/vote-account.ts
@@ -23,6 +23,21 @@ export type EpochCredits = {
   prevCredits: number;
 };
 
+export type AuthorizedVoters = {
+  [key: string]: PublicKey;
+};
+
+export type PriorVoters = {
+  authorizedPubkey: PublicKey;
+  epochOfLastAuthorizedSwitch: number;
+  targetEpoch: number;
+};
+
+export type Timestamp = {
+  slot: number;
+  timetamp: number;
+};
+
 /**
  * See https://github.com/solana-labs/solana/blob/8a12ed029cfa38d4a45400916c2463fb82bbec8c/programs/vote_api/src/vote_state.rs#L68-L88
  *
@@ -30,8 +45,7 @@ export type EpochCredits = {
  */
 const VoteAccountLayout = BufferLayout.struct([
   Layout.publicKey('nodePubkey'),
-  Layout.publicKey('authorizedVoterPubkey'),
-  Layout.publicKey('authorizedWithdrawerPubkey'),
+  Layout.publicKey('authorizedWithdrawer'),
   BufferLayout.u8('commission'),
   BufferLayout.nu64(), // votes.length
   BufferLayout.seq(
@@ -44,9 +58,25 @@ const VoteAccountLayout = BufferLayout.struct([
   ),
   BufferLayout.u8('rootSlotValid'),
   BufferLayout.nu64('rootSlot'),
-  BufferLayout.nu64('epoch'),
-  BufferLayout.nu64('credits'),
-  BufferLayout.nu64('lastEpochCredits'),
+  BufferLayout.nu64(), // authorizedVoters.length
+  BufferLayout.seq(
+    BufferLayout.struct([
+      BufferLayout.nu64('epoch'),
+      Layout.publicKey('authorizedVoter'),
+    ]),
+    BufferLayout.offset(BufferLayout.u32(), -8),
+    'authorizedVoters',
+  ),
+  BufferLayout.nu64(), // priorVoters.length
+  BufferLayout.seq(
+    BufferLayout.struct([
+      Layout.publicKey('authorizedPubkey'),
+      BufferLayout.nu64('epochOfLastAuthorizedSwitch'),
+      BufferLayout.nu64('targetEpoch'),
+    ]),
+    BufferLayout.offset(BufferLayout.u32(), -8),
+    'priorVoters',
+  ),
   BufferLayout.nu64(), // epochCredits.length
   BufferLayout.seq(
     BufferLayout.struct([
@@ -57,19 +87,22 @@ const VoteAccountLayout = BufferLayout.struct([
     BufferLayout.offset(BufferLayout.u32(), -8),
     'epochCredits',
   ),
+  BufferLayout.struct(
+    [BufferLayout.nu64('slot'), BufferLayout.nu64('timestamp')],
+    'lastTimestamp',
+  ),
 ]);
 
 type VoteAccountArgs = {
   nodePubkey: PublicKey;
-  authorizedVoterPubkey: PublicKey;
-  authorizedWithdrawerPubkey: PublicKey;
+  authorizedWithdrawer: PublicKey;
   commission: number;
-  votes: Array<Lockout>;
   rootSlot: number | null;
-  epoch: number;
-  credits: number;
-  lastEpochCredits: number;
-  epochCredits: Array<EpochCredits>;
+  votes: Lockout[];
+  authorizedVoters: AuthorizedVoters[];
+  priorVoters: PriorVoters[];
+  epochCredits: EpochCredits[];
+  lastTimestamp: Timestamp;
 };
 
 /**
@@ -77,30 +110,28 @@ type VoteAccountArgs = {
  */
 export class VoteAccount {
   nodePubkey: PublicKey;
-  authorizedVoterPubkey: PublicKey;
-  authorizedWithdrawerPubkey: PublicKey;
+  authorizedWithdrawer: PublicKey;
   commission: number;
-  votes: Array<Lockout>;
   rootSlot: number | null;
-  epoch: number;
-  credits: number;
-  lastEpochCredits: number;
-  epochCredits: Array<EpochCredits>;
+  votes: Lockout[];
+  authorizedVoters: AuthorizedVoters[];
+  priorVoters: PriorVoters[];
+  epochCredits: EpochCredits[];
+  lastTimestamp: Timestamp;
 
   /**
    * @internal
    */
   constructor(args: VoteAccountArgs) {
     this.nodePubkey = args.nodePubkey;
-    this.authorizedVoterPubkey = args.authorizedVoterPubkey;
-    this.authorizedWithdrawerPubkey = args.authorizedWithdrawerPubkey;
+    this.authorizedWithdrawer = args.authorizedWithdrawer;
     this.commission = args.commission;
-    this.votes = args.votes;
     this.rootSlot = args.rootSlot;
-    this.epoch = args.epoch;
-    this.credits = args.credits;
-    this.lastEpochCredits = args.lastEpochCredits;
+    this.votes = args.votes;
+    this.authorizedVoters = args.authorizedVoters;
+    this.priorVoters = args.priorVoters;
     this.epochCredits = args.epochCredits;
+    this.lastTimestamp = args.lastTimestamp;
   }
 
   /**
@@ -112,7 +143,8 @@ export class VoteAccount {
   static fromAccountData(
     buffer: Buffer | Uint8Array | Array<number>,
   ): VoteAccount {
-    const va = VoteAccountLayout.decode(toBuffer(buffer), 0);
+    const versionOffset = 4;
+    const va = VoteAccountLayout.decode(toBuffer(buffer), versionOffset);
 
     let rootSlot: number | null = va.rootSlot;
     if (!va.rootSlotValid) {
@@ -121,15 +153,14 @@ export class VoteAccount {
 
     return new VoteAccount({
       nodePubkey: new PublicKey(va.nodePubkey),
-      authorizedVoterPubkey: new PublicKey(va.authorizedVoterPubkey),
-      authorizedWithdrawerPubkey: new PublicKey(va.authorizedWithdrawerPubkey),
+      authorizedWithdrawer: new PublicKey(va.authorizedWithdrawer),
       commission: va.commission,
       votes: va.votes,
       rootSlot,
-      epoch: va.epoch,
-      credits: va.credits,
-      lastEpochCredits: va.lastEpochCredits,
+      authorizedVoters: va.authorizedVoters,
+      priorVoters: va.priorVoters,
       epochCredits: va.epochCredits,
+      lastTimestamp: va.lastTimestamp,
     });
   }
 }
