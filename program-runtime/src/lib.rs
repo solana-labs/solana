067e29ae0b3b3a7a0af51f7414f7e4262537f0c8
Alexander Meißner <AlexanderMeissner@gmx.net>
2021-11-09 13:35:49 +0100

    Replaces unchecked integer arithmetic by guarded versions. (#21186)

--- program-runtime/src/lib.rs
+++ program-runtime/src/lib.rs
@@ -1,5 +1,4 @@
 #![cfg_attr(RUSTC_WITH_SPECIALIZATION, feature(min_specialization))]
-#![allow(clippy::integer_arithmetic)] // TODO: Remove
 
 pub mod instruction_processor;
 pub mod instruction_recorder;
