6cdfd08edf4d8d3d50582bb24a6bd758a209b2ba
Nil Puig <hello@nilpuig.co>
2021-11-11 17:29:38 +0800

    refactoring from var to const

--- web3.js/examples/get_account_info.js
+++ web3.js/examples/get_account_info.js
@@ -2,14 +2,14 @@ const web3 = require("@solana/web3.js");
 
 (async () => {
   // Connect to cluster
-  var connection = new web3.Connection(
+  const connection = new web3.Connection(
     web3.clusterApiUrl('devnet'),
     'confirmed',
   );
 
   // Generate a new wallet keypair and airdrop SOL
-  var wallet = web3.Keypair.generate();
-  var airdropSignature = await connection.requestAirdrop(
+  const wallet = web3.Keypair.generate();
+  const airdropSignature = await connection.requestAirdrop(
     wallet.publicKey,
     web3.LAMPORTS_PER_SOL,
   );
@@ -20,6 +20,6 @@ const web3 = require("@solana/web3.js");
   // get account info
   // account data is bytecode that needs to be deserialized
   // serialization and deserialization is program specic
-  let account = await connection.getAccountInfo(wallet.publicKey);
+  const account = await connection.getAccountInfo(wallet.publicKey);
   console.log(account);
 })();
