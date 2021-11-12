0bb37f417902b244520289058d86fb59bbdb6b84
Lijun Wang <83639177+lijunwangs@users.noreply.github.com>
2021-11-12 02:19:14 -0800

    Added unit tests for transaction selector

--- accountsdb-plugin-postgres/src/transaction_selector.rs
+++ accountsdb-plugin-postgres/src/transaction_selector.rs
@@ -55,6 +55,10 @@ impl TransactionSelector {
         is_vote: bool,
         accounts: Box<dyn Iterator<Item = &Pubkey> + '_>,
     ) -> bool {
+        if !self.is_interested_in_any_transaction() {
+            return false;
+        }
+
         if self.select_all_transactions || (self.select_all_vote_transactions && is_vote) {
             return true;
         }
@@ -68,6 +72,113 @@ impl TransactionSelector {
 
     /// Check if any transaction is of interested at all
     pub fn is_interested_in_any_transaction(&self) -> bool {
-        self.select_all_transactions || self.select_all_vote_transactions || !self.mentions.is_empty()
+        self.select_all_transactions
+            || self.select_all_vote_transactions
+            || !self.mentions.is_empty()
+    }
+}
+
+#[cfg(test)]
+pub(crate) mod tests {
+    use super::*;
+
+    #[test]
+    fn test_select_transaction() {
+        let pubkey1 = Pubkey::new_unique();
+        let pubkey2 = Pubkey::new_unique();
+
+        let selector = TransactionSelector::new(&[pubkey1.to_string()]);
+
+        assert!(selector.is_interested_in_any_transaction());
+
+        let accounts = [pubkey1];
+
+        assert!(selector.is_transaction_selected(false, Box::new(accounts.iter())));
+
+        let accounts = [pubkey2];
+        assert!(!selector.is_transaction_selected(false, Box::new(accounts.iter())));
+
+        let accounts = [pubkey1, pubkey2];
+        assert!(selector.is_transaction_selected(false, Box::new(accounts.iter())));
+    }
+
+    #[test]
+    fn test_select_all_transaction_using_wildcard() {
+        let pubkey1 = Pubkey::new_unique();
+        let pubkey2 = Pubkey::new_unique();
+
+        let selector = TransactionSelector::new(&["*".to_string()]);
+
+        assert!(selector.is_interested_in_any_transaction());
+
+        let accounts = [pubkey1];
+
+        assert!(selector.is_transaction_selected(false, Box::new(accounts.iter())));
+
+        let accounts = [pubkey2];
+        assert!(selector.is_transaction_selected(false, Box::new(accounts.iter())));
+
+        let accounts = [pubkey1, pubkey2];
+        assert!(selector.is_transaction_selected(false, Box::new(accounts.iter())));
+    }
+
+    #[test]
+    fn test_select_all_transaction_all() {
+        let pubkey1 = Pubkey::new_unique();
+        let pubkey2 = Pubkey::new_unique();
+
+        let selector = TransactionSelector::new(&["all".to_string()]);
+
+        assert!(selector.is_interested_in_any_transaction());
+
+        let accounts = [pubkey1];
+
+        assert!(selector.is_transaction_selected(false, Box::new(accounts.iter())));
+
+        let accounts = [pubkey2];
+        assert!(selector.is_transaction_selected(false, Box::new(accounts.iter())));
+
+        let accounts = [pubkey1, pubkey2];
+        assert!(selector.is_transaction_selected(false, Box::new(accounts.iter())));
+    }
+
+    #[test]
+    fn test_select_all_vote_transaction() {
+        let pubkey1 = Pubkey::new_unique();
+        let pubkey2 = Pubkey::new_unique();
+
+        let selector = TransactionSelector::new(&["all_votes".to_string()]);
+
+        assert!(selector.is_interested_in_any_transaction());
+
+        let accounts = [pubkey1];
+
+        assert!(!selector.is_transaction_selected(false, Box::new(accounts.iter())));
+
+        let accounts = [pubkey2];
+        assert!(selector.is_transaction_selected(true, Box::new(accounts.iter())));
+
+        let accounts = [pubkey1, pubkey2];
+        assert!(selector.is_transaction_selected(true, Box::new(accounts.iter())));
+    }
+
+    #[test]
+    fn test_select_no_transaction() {
+        let pubkey1 = Pubkey::new_unique();
+        let pubkey2 = Pubkey::new_unique();
+
+        let selector = TransactionSelector::new(&[]);
+
+        assert!(!selector.is_interested_in_any_transaction());
+
+        let accounts = [pubkey1];
+
+        assert!(!selector.is_transaction_selected(false, Box::new(accounts.iter())));
+
+        let accounts = [pubkey2];
+        assert!(!selector.is_transaction_selected(true, Box::new(accounts.iter())));
+
+        let accounts = [pubkey1, pubkey2];
+        assert!(!selector.is_transaction_selected(true, Box::new(accounts.iter())));
     }
 }
