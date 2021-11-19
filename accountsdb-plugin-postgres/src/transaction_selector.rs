db931f4482239de78ef27e7fa44aac0d72a11894
Lijun Wang <83639177+lijunwangs@users.noreply.github.com>
2021-11-19 12:15:54 -0800

    Addressed some comments

--- accountsdb-plugin-postgres/src/transaction_selector.rs
+++ accountsdb-plugin-postgres/src/transaction_selector.rs
@@ -3,7 +3,7 @@
 use {log::*, solana_sdk::pubkey::Pubkey, std::collections::HashSet};
 
 pub(crate) struct TransactionSelector {
-    pub mentions: HashSet<Vec<u8>>,
+    pub mentioned_addresses: HashSet<Vec<u8>>,
     pub select_all_transactions: bool,
     pub select_all_vote_transactions: bool,
 }
@@ -12,39 +12,48 @@ pub(crate) struct TransactionSelector {
 impl TransactionSelector {
     pub fn default() -> Self {
         Self {
-            mentions: HashSet::default(),
+            mentioned_addresses: HashSet::default(),
             select_all_transactions: false,
             select_all_vote_transactions: false,
         }
     }
 
-    pub fn new(accounts: &[String]) -> Self {
-        info!("Creating TransactionSelector from accounts: {:?}", accounts);
+    /// Create a selector based on the mentioned addresses
+    /// To select all transactions use ["*"] or ["all"]
+    /// To select all vote transactions, use ["all_votes"]
+    /// To select transactions mentioning specific addresses use ["<pubkey1>", "<pubkey2>", ...]
+    pub fn new(mentioned_addresses: &[String]) -> Self {
+        info!(
+            "Creating TransactionSelector from addresses: {:?}",
+            mentioned_addresses
+        );
 
-        let select_all_transactions = accounts.iter().any(|key| key == "*" || key == "all");
+        let select_all_transactions = mentioned_addresses
+            .iter()
+            .any(|key| key == "*" || key == "all");
         if select_all_transactions {
             return Self {
-                mentions: HashSet::default(),
+                mentioned_addresses: HashSet::default(),
                 select_all_transactions,
                 select_all_vote_transactions: true,
             };
         }
-        let select_all_vote_transactions = accounts.iter().any(|key| key == "all_votes");
+        let select_all_vote_transactions = mentioned_addresses.iter().any(|key| key == "all_votes");
         if select_all_vote_transactions {
             return Self {
-                mentions: HashSet::default(),
+                mentioned_addresses: HashSet::default(),
                 select_all_transactions,
                 select_all_vote_transactions: true,
             };
         }
 
-        let accounts = accounts
+        let mentioned_addresses = mentioned_addresses
             .iter()
             .map(|key| bs58::decode(key).into_vec().unwrap())
             .collect();
 
         Self {
-            mentions: accounts,
+            mentioned_addresses,
             select_all_transactions: false,
             select_all_vote_transactions: false,
         }
@@ -54,7 +63,7 @@ impl TransactionSelector {
     pub fn is_transaction_selected(
         &self,
         is_vote: bool,
-        accounts: Box<dyn Iterator<Item = &Pubkey> + '_>,
+        mentioned_addresses: Box<dyn Iterator<Item = &Pubkey> + '_>,
     ) -> bool {
         if !self.is_enabled() {
             return false;
@@ -63,19 +72,19 @@ impl TransactionSelector {
         if self.select_all_transactions || (self.select_all_vote_transactions && is_vote) {
             return true;
         }
-        for account in accounts {
-            if self.mentions.contains(account.as_ref()) {
+        for address in mentioned_addresses {
+            if self.mentioned_addresses.contains(address.as_ref()) {
                 return true;
             }
         }
         false
     }
 
-    /// Check if any transaction is of interested at all
+    /// Check if any transaction is of interest at all
     pub fn is_enabled(&self) -> bool {
         self.select_all_transactions
             || self.select_all_vote_transactions
-            || !self.mentions.is_empty()
+            || !self.mentioned_addresses.is_empty()
     }
 }
 
@@ -92,15 +101,15 @@ pub(crate) mod tests {
 
         assert!(selector.is_enabled());
 
-        let accounts = [pubkey1];
+        let addresses = [pubkey1];
 
-        assert!(selector.is_transaction_selected(false, Box::new(accounts.iter())));
+        assert!(selector.is_transaction_selected(false, Box::new(addresses.iter())));
 
-        let accounts = [pubkey2];
-        assert!(!selector.is_transaction_selected(false, Box::new(accounts.iter())));
+        let addresses = [pubkey2];
+        assert!(!selector.is_transaction_selected(false, Box::new(addresses.iter())));
 
-        let accounts = [pubkey1, pubkey2];
-        assert!(selector.is_transaction_selected(false, Box::new(accounts.iter())));
+        let addresses = [pubkey1, pubkey2];
+        assert!(selector.is_transaction_selected(false, Box::new(addresses.iter())));
     }
 
     #[test]
@@ -112,15 +121,15 @@ pub(crate) mod tests {
 
         assert!(selector.is_enabled());
 
-        let accounts = [pubkey1];
+        let addresses = [pubkey1];
 
-        assert!(selector.is_transaction_selected(false, Box::new(accounts.iter())));
+        assert!(selector.is_transaction_selected(false, Box::new(addresses.iter())));
 
-        let accounts = [pubkey2];
-        assert!(selector.is_transaction_selected(false, Box::new(accounts.iter())));
+        let addresses = [pubkey2];
+        assert!(selector.is_transaction_selected(false, Box::new(addresses.iter())));
 
-        let accounts = [pubkey1, pubkey2];
-        assert!(selector.is_transaction_selected(false, Box::new(accounts.iter())));
+        let addresses = [pubkey1, pubkey2];
+        assert!(selector.is_transaction_selected(false, Box::new(addresses.iter())));
     }
 
     #[test]
@@ -132,15 +141,15 @@ pub(crate) mod tests {
 
         assert!(selector.is_enabled());
 
-        let accounts = [pubkey1];
+        let addresses = [pubkey1];
 
-        assert!(selector.is_transaction_selected(false, Box::new(accounts.iter())));
+        assert!(selector.is_transaction_selected(false, Box::new(addresses.iter())));
 
-        let accounts = [pubkey2];
-        assert!(selector.is_transaction_selected(false, Box::new(accounts.iter())));
+        let addresses = [pubkey2];
+        assert!(selector.is_transaction_selected(false, Box::new(addresses.iter())));
 
-        let accounts = [pubkey1, pubkey2];
-        assert!(selector.is_transaction_selected(false, Box::new(accounts.iter())));
+        let addresses = [pubkey1, pubkey2];
+        assert!(selector.is_transaction_selected(false, Box::new(addresses.iter())));
     }
 
     #[test]
@@ -152,15 +161,15 @@ pub(crate) mod tests {
 
         assert!(selector.is_enabled());
 
-        let accounts = [pubkey1];
+        let addresses = [pubkey1];
 
-        assert!(!selector.is_transaction_selected(false, Box::new(accounts.iter())));
+        assert!(!selector.is_transaction_selected(false, Box::new(addresses.iter())));
 
-        let accounts = [pubkey2];
-        assert!(selector.is_transaction_selected(true, Box::new(accounts.iter())));
+        let addresses = [pubkey2];
+        assert!(selector.is_transaction_selected(true, Box::new(addresses.iter())));
 
-        let accounts = [pubkey1, pubkey2];
-        assert!(selector.is_transaction_selected(true, Box::new(accounts.iter())));
+        let addresses = [pubkey1, pubkey2];
+        assert!(selector.is_transaction_selected(true, Box::new(addresses.iter())));
     }
 
     #[test]
@@ -172,14 +181,14 @@ pub(crate) mod tests {
 
         assert!(!selector.is_enabled());
 
-        let accounts = [pubkey1];
+        let addresses = [pubkey1];
 
-        assert!(!selector.is_transaction_selected(false, Box::new(accounts.iter())));
+        assert!(!selector.is_transaction_selected(false, Box::new(addresses.iter())));
 
-        let accounts = [pubkey2];
-        assert!(!selector.is_transaction_selected(true, Box::new(accounts.iter())));
+        let addresses = [pubkey2];
+        assert!(!selector.is_transaction_selected(true, Box::new(addresses.iter())));
 
-        let accounts = [pubkey1, pubkey2];
-        assert!(!selector.is_transaction_selected(true, Box::new(accounts.iter())));
+        let addresses = [pubkey1, pubkey2];
+        assert!(!selector.is_transaction_selected(true, Box::new(addresses.iter())));
     }
 }
