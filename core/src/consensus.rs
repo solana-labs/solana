362ff933cc05075cf506e0b9c8274e0c1efeec18
sakridge <sakridge@gmail.com>
2021-11-19 16:20:18 +0000

    Add bank drop service (#21322)
    (cherry picked from commit 0bda0c3e0cf46ee44de2cad366afa3593f76fc6d)

--- core/src/consensus.rs
+++ core/src/consensus.rs
@@ -1548,6 +1548,7 @@ pub mod test {
         }
 
         pub fn set_root(&mut self, new_root: Slot) {
+            let (bank_drop_sender, _bank_drop_receiver) = std::sync::mpsc::channel();
             ReplayStage::handle_new_root(
                 new_root,
                 &self.bank_forks,
@@ -1560,6 +1561,7 @@ pub mod test {
                 &mut UnfrozenGossipVerifiedVoteHashes::default(),
                 &mut true,
                 &mut Vec::new(),
+                &bank_drop_sender,
             )
         }
 
