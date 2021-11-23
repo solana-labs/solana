9edfc5936d2df7fc920a9ffd920beed5e7148b47
Tao Zhu <82401714+taozhu-chicago@users.noreply.github.com>
2021-11-23 15:17:55 -0600

    Refactor accounts.rs with Justin's comments to improve lock accounts (#21406)
    with results code path.
    - fix a bug that could unlock accounts that weren't locked
    - add test to the refactored function
    - skip enumerating transaction accounts if qos results is an error
    - add #[must_use] annotation
    - avoid clone error in results
    - add qos error code to unlock_accounts match statement
    - remove unnecessary AbiExample

--- core/src/banking_stage.rs
+++ core/src/banking_stage.rs
@@ -940,7 +940,8 @@ impl BankingStage {
         // Once accounts are locked, other threads cannot encode transactions that will modify the
         // same account state
         let mut lock_time = Measure::start("lock_time");
-        let batch = bank.prepare_sanitized_batch_with_results(txs, transactions_qos_results.iter());
+        let batch =
+            bank.prepare_sanitized_batch_with_results(txs, transactions_qos_results.into_iter());
         lock_time.stop();
 
         // retryable_txs includes AccountInUse, WouldExceedMaxBlockCostLimit and
