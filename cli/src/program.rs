3ea108927b5b90c067a985cdc9fe8b9fd3a91c24
Alexander Meißner <AlexanderMeissner@gmx.net>
2021-12-03 23:44:31 +0100

    Bumps solana_rbpf to v0.2.16 (#21492)

--- cli/src/program.rs
+++ cli/src/program.rs
@@ -24,10 +24,7 @@ use {
         rpc_filter::{Memcmp, MemcmpEncodedBytes, RpcFilterType},
         tpu_client::{TpuClient, TpuClientConfig},
     },
-    solana_rbpf::{
-        verifier,
-        vm::{Config, Executable},
-    },
+    solana_rbpf::{elf::Executable, verifier, vm::Config},
     solana_remote_wallet::remote_wallet::RemoteWalletManager,
     solana_sdk::{
         account::Account,
@@ -1989,7 +1986,7 @@ fn read_and_verify_elf(program_location: &str) -> Result<Vec<u8>, Box<dyn std::e
     let mut invoke_context = MockInvokeContext::new(vec![]);
 
     // Verify the program
-    <dyn Executable<BpfError, ThisInstructionMeter>>::from_elf(
+    Executable::<BpfError, ThisInstructionMeter>::from_elf(
         &program_data,
         Some(verifier::check),
         Config {
