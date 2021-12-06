a303a817113b2e66e0e20eb9e7be086ec4dbb912
Tyera Eulberg <teulberg@gmail.com>
2021-12-06 15:54:50 -0700

    Add offline and fee-payer utilities to CLI vote module (#21579)
    * create-vote-account: add offline, nonce, fee_payer capabilities
    
    * vote-authorize: add offline, nonce, fee-payer
    
    * vote-update-things: add offline, nonce, fee-payer
    
    * withdraw-vote: add offline, nonce, fee-payer
    
    * close-vote-acct: add fee-payer
    
    * Allow WithdrawVoteAccount to empty account, since offline operations cannot perform account state queries as in CloseVoteAccount
    
    * Fix lint
    
    * Update offline-signing docs
    
    * Add some parse unit tests
    
    * Add offline integration test
    
    (cherry picked from commit 873fe81bc048599b540cf511872380d77be38f56)

--- cli/tests/vote.rs
+++ cli/tests/vote.rs
@@ -4,6 +4,7 @@ use {
         spend_utils::SpendAmount,
         test_utils::check_recent_balance,
     },
+    solana_cli_output::{parse_sign_only_reply_string, OutputFormat},
     solana_client::{
         blockhash_query::{self, BlockhashQuery},
         rpc_client::RpcClient,
@@ -12,7 +13,7 @@ use {
     solana_sdk::{
         account_utils::StateMut,
         commitment_config::CommitmentConfig,
-        signature::{Keypair, Signer},
+        signature::{Keypair, NullSigner, Signer},
     },
     solana_streamer::socket::SocketAddrSpace,
     solana_test_validator::TestValidator,
@@ -49,7 +50,13 @@ fn test_vote_authorize_and_withdraw() {
         authorized_voter: None,
         authorized_withdrawer: config.signers[0].pubkey(),
         commission: 0,
+        sign_only: false,
+        dump_transaction_message: false,
+        blockhash_query: BlockhashQuery::All(blockhash_query::Source::Cluster),
+        nonce_account: None,
+        nonce_authority: 0,
         memo: None,
+        fee_payer: 0,
     };
     process_command(&config).unwrap();
     let vote_account = rpc_client
@@ -93,7 +100,13 @@ fn test_vote_authorize_and_withdraw() {
         vote_account_pubkey,
         new_authorized_pubkey: first_withdraw_authority.pubkey(),
         vote_authorize: VoteAuthorize::Withdrawer,
+        sign_only: false,
+        dump_transaction_message: false,
+        blockhash_query: BlockhashQuery::All(blockhash_query::Source::Cluster),
+        nonce_account: None,
+        nonce_authority: 0,
         memo: None,
+        fee_payer: 0,
         authorized: 0,
         new_authorized: None,
     };
@@ -112,7 +125,13 @@ fn test_vote_authorize_and_withdraw() {
         vote_account_pubkey,
         new_authorized_pubkey: withdraw_authority.pubkey(),
         vote_authorize: VoteAuthorize::Withdrawer,
+        sign_only: false,
+        dump_transaction_message: false,
+        blockhash_query: BlockhashQuery::All(blockhash_query::Source::Cluster),
+        nonce_account: None,
+        nonce_authority: 0,
         memo: None,
+        fee_payer: 0,
         authorized: 1,
         new_authorized: Some(1),
     };
@@ -126,7 +145,13 @@ fn test_vote_authorize_and_withdraw() {
         vote_account_pubkey,
         new_authorized_pubkey: withdraw_authority.pubkey(),
         vote_authorize: VoteAuthorize::Withdrawer,
+        sign_only: false,
+        dump_transaction_message: false,
+        blockhash_query: BlockhashQuery::All(blockhash_query::Source::Cluster),
+        nonce_account: None,
+        nonce_authority: 0,
         memo: None,
+        fee_payer: 0,
         authorized: 1,
         new_authorized: Some(2),
     };
@@ -146,7 +171,13 @@ fn test_vote_authorize_and_withdraw() {
         withdraw_authority: 1,
         withdraw_amount: SpendAmount::Some(100),
         destination_account_pubkey: destination_account,
+        sign_only: false,
+        dump_transaction_message: false,
+        blockhash_query: BlockhashQuery::All(blockhash_query::Source::Cluster),
+        nonce_account: None,
+        nonce_authority: 0,
         memo: None,
+        fee_payer: 0,
     };
     process_command(&config).unwrap();
     let expected_balance = expected_balance - 100;
@@ -160,7 +191,13 @@ fn test_vote_authorize_and_withdraw() {
         vote_account_pubkey,
         new_identity_account: 2,
         withdraw_authority: 1,
+        sign_only: false,
+        dump_transaction_message: false,
+        blockhash_query: BlockhashQuery::All(blockhash_query::Source::Cluster),
+        nonce_account: None,
+        nonce_authority: 0,
         memo: None,
+        fee_payer: 0,
     };
     process_command(&config).unwrap();
 
@@ -172,8 +209,283 @@ fn test_vote_authorize_and_withdraw() {
         withdraw_authority: 1,
         destination_account_pubkey: destination_account,
         memo: None,
+        fee_payer: 0,
     };
     process_command(&config).unwrap();
     check_recent_balance(0, &rpc_client, &vote_account_pubkey);
+    check_recent_balance(expected_balance, &rpc_client, &destination_account);
+}
+
+#[test]
+fn test_offline_vote_authorize_and_withdraw() {
+    let mint_keypair = Keypair::new();
+    let mint_pubkey = mint_keypair.pubkey();
+    let faucet_addr = run_local_faucet(mint_keypair, None);
+    let test_validator =
+        TestValidator::with_no_fees(mint_pubkey, Some(faucet_addr), SocketAddrSpace::Unspecified);
+
+    let rpc_client =
+        RpcClient::new_with_commitment(test_validator.rpc_url(), CommitmentConfig::processed());
+    let default_signer = Keypair::new();
+
+    let mut config_payer = CliConfig::recent_for_tests();
+    config_payer.json_rpc_url = test_validator.rpc_url();
+    config_payer.signers = vec![&default_signer];
+
+    let mut config_offline = CliConfig::recent_for_tests();
+    config_offline.json_rpc_url = String::default();
+    config_offline.command = CliCommand::ClusterVersion;
+    let offline_keypair = Keypair::new();
+    config_offline.signers = vec![&offline_keypair];
+    // Verify that we cannot reach the cluster
+    process_command(&config_offline).unwrap_err();
+
+    request_and_confirm_airdrop(
+        &rpc_client,
+        &config_payer,
+        &config_payer.signers[0].pubkey(),
+        100_000,
+    )
+    .unwrap();
+    check_recent_balance(100_000, &rpc_client, &config_payer.signers[0].pubkey());
+
+    request_and_confirm_airdrop(
+        &rpc_client,
+        &config_offline,
+        &config_offline.signers[0].pubkey(),
+        100_000,
+    )
+    .unwrap();
+    check_recent_balance(100_000, &rpc_client, &config_offline.signers[0].pubkey());
+
+    // Create vote account with specific withdrawer
+    let vote_account_keypair = Keypair::new();
+    let vote_account_pubkey = vote_account_keypair.pubkey();
+    config_payer.signers = vec![&default_signer, &vote_account_keypair];
+    config_payer.command = CliCommand::CreateVoteAccount {
+        vote_account: 1,
+        seed: None,
+        identity_account: 0,
+        authorized_voter: None,
+        authorized_withdrawer: offline_keypair.pubkey(),
+        commission: 0,
+        sign_only: false,
+        dump_transaction_message: false,
+        blockhash_query: BlockhashQuery::All(blockhash_query::Source::Cluster),
+        nonce_account: None,
+        nonce_authority: 0,
+        memo: None,
+        fee_payer: 0,
+    };
+    process_command(&config_payer).unwrap();
+    let vote_account = rpc_client
+        .get_account(&vote_account_keypair.pubkey())
+        .unwrap();
+    let vote_state: VoteStateVersions = vote_account.state().unwrap();
+    let authorized_withdrawer = vote_state.convert_to_current().authorized_withdrawer;
+    assert_eq!(authorized_withdrawer, offline_keypair.pubkey());
+    let expected_balance = rpc_client
+        .get_minimum_balance_for_rent_exemption(VoteState::size_of())
+        .unwrap()
+        .max(1);
+    check_recent_balance(expected_balance, &rpc_client, &vote_account_pubkey);
+
+    // Transfer in some more SOL
+    config_payer.signers = vec![&default_signer];
+    config_payer.command = CliCommand::Transfer {
+        amount: SpendAmount::Some(1_000),
+        to: vote_account_pubkey,
+        from: 0,
+        sign_only: false,
+        dump_transaction_message: false,
+        allow_unfunded_recipient: true,
+        no_wait: false,
+        blockhash_query: BlockhashQuery::All(blockhash_query::Source::Cluster),
+        nonce_account: None,
+        nonce_authority: 0,
+        memo: None,
+        fee_payer: 0,
+        derived_address_seed: None,
+        derived_address_program_id: None,
+    };
+    process_command(&config_payer).unwrap();
+    let expected_balance = expected_balance + 1_000;
+    check_recent_balance(expected_balance, &rpc_client, &vote_account_pubkey);
+
+    // Authorize vote account withdrawal to another signer, offline
+    let withdraw_authority = Keypair::new();
+    let blockhash = rpc_client.get_latest_blockhash().unwrap();
+    config_offline.command = CliCommand::VoteAuthorize {
+        vote_account_pubkey,
+        new_authorized_pubkey: withdraw_authority.pubkey(),
+        vote_authorize: VoteAuthorize::Withdrawer,
+        sign_only: true,
+        dump_transaction_message: false,
+        blockhash_query: BlockhashQuery::None(blockhash),
+        nonce_account: None,
+        nonce_authority: 0,
+        memo: None,
+        fee_payer: 0,
+        authorized: 0,
+        new_authorized: None,
+    };
+    config_offline.output_format = OutputFormat::JsonCompact;
+    let sig_response = process_command(&config_offline).unwrap();
+    let sign_only = parse_sign_only_reply_string(&sig_response);
+    assert!(sign_only.has_all_signers());
+    let offline_presigner = sign_only
+        .presigner_of(&config_offline.signers[0].pubkey())
+        .unwrap();
+    config_payer.signers = vec![&offline_presigner];
+    config_payer.command = CliCommand::VoteAuthorize {
+        vote_account_pubkey,
+        new_authorized_pubkey: withdraw_authority.pubkey(),
+        vote_authorize: VoteAuthorize::Withdrawer,
+        sign_only: false,
+        dump_transaction_message: false,
+        blockhash_query: BlockhashQuery::FeeCalculator(blockhash_query::Source::Cluster, blockhash),
+        nonce_account: None,
+        nonce_authority: 0,
+        memo: None,
+        fee_payer: 0,
+        authorized: 0,
+        new_authorized: None,
+    };
+    process_command(&config_payer).unwrap();
+    let vote_account = rpc_client
+        .get_account(&vote_account_keypair.pubkey())
+        .unwrap();
+    let vote_state: VoteStateVersions = vote_account.state().unwrap();
+    let authorized_withdrawer = vote_state.convert_to_current().authorized_withdrawer;
+    assert_eq!(authorized_withdrawer, withdraw_authority.pubkey());
+
+    // Withdraw from vote account offline
+    let destination_account = solana_sdk::pubkey::new_rand(); // Send withdrawal to new account to make balance check easy
+    let blockhash = rpc_client.get_latest_blockhash().unwrap();
+    let fee_payer_null_signer = NullSigner::new(&default_signer.pubkey());
+    config_offline.signers = vec![&fee_payer_null_signer, &withdraw_authority];
+    config_offline.command = CliCommand::WithdrawFromVoteAccount {
+        vote_account_pubkey,
+        withdraw_authority: 1,
+        withdraw_amount: SpendAmount::Some(100),
+        destination_account_pubkey: destination_account,
+        sign_only: true,
+        dump_transaction_message: false,
+        blockhash_query: BlockhashQuery::None(blockhash),
+        nonce_account: None,
+        nonce_authority: 0,
+        memo: None,
+        fee_payer: 0,
+    };
+    config_offline.output_format = OutputFormat::JsonCompact;
+    let sig_response = process_command(&config_offline).unwrap();
+    let sign_only = parse_sign_only_reply_string(&sig_response);
+    let offline_presigner = sign_only
+        .presigner_of(&config_offline.signers[1].pubkey())
+        .unwrap();
+    config_payer.signers = vec![&default_signer, &offline_presigner];
+    config_payer.command = CliCommand::WithdrawFromVoteAccount {
+        vote_account_pubkey,
+        withdraw_authority: 1,
+        withdraw_amount: SpendAmount::Some(100),
+        destination_account_pubkey: destination_account,
+        sign_only: false,
+        dump_transaction_message: false,
+        blockhash_query: BlockhashQuery::FeeCalculator(blockhash_query::Source::Cluster, blockhash),
+        nonce_account: None,
+        nonce_authority: 0,
+        memo: None,
+        fee_payer: 0,
+    };
+    process_command(&config_payer).unwrap();
+    let expected_balance = expected_balance - 100;
+    check_recent_balance(expected_balance, &rpc_client, &vote_account_pubkey);
+    check_recent_balance(100, &rpc_client, &destination_account);
+
+    // Re-assign validator identity offline
+    let blockhash = rpc_client.get_latest_blockhash().unwrap();
+    let new_identity_keypair = Keypair::new();
+    let new_identity_null_signer = NullSigner::new(&new_identity_keypair.pubkey());
+    config_offline.signers = vec![
+        &fee_payer_null_signer,
+        &withdraw_authority,
+        &new_identity_null_signer,
+    ];
+    config_offline.command = CliCommand::VoteUpdateValidator {
+        vote_account_pubkey,
+        new_identity_account: 2,
+        withdraw_authority: 1,
+        sign_only: true,
+        dump_transaction_message: false,
+        blockhash_query: BlockhashQuery::None(blockhash),
+        nonce_account: None,
+        nonce_authority: 0,
+        memo: None,
+        fee_payer: 0,
+    };
+    process_command(&config_offline).unwrap();
+    config_offline.output_format = OutputFormat::JsonCompact;
+    let sig_response = process_command(&config_offline).unwrap();
+    let sign_only = parse_sign_only_reply_string(&sig_response);
+    let offline_presigner = sign_only
+        .presigner_of(&config_offline.signers[1].pubkey())
+        .unwrap();
+    config_payer.signers = vec![&default_signer, &offline_presigner, &new_identity_keypair];
+    config_payer.command = CliCommand::VoteUpdateValidator {
+        vote_account_pubkey,
+        new_identity_account: 2,
+        withdraw_authority: 1,
+        sign_only: false,
+        dump_transaction_message: false,
+        blockhash_query: BlockhashQuery::FeeCalculator(blockhash_query::Source::Cluster, blockhash),
+        nonce_account: None,
+        nonce_authority: 0,
+        memo: None,
+        fee_payer: 0,
+    };
+    process_command(&config_payer).unwrap();
+
+    // Close vote account offline. Must use WithdrawFromVoteAccount and specify amount, since
+    // CloseVoteAccount requires RpcClient
+    let destination_account = solana_sdk::pubkey::new_rand(); // Send withdrawal to new account to make balance check easy
+    config_offline.signers = vec![&fee_payer_null_signer, &withdraw_authority];
+    config_offline.command = CliCommand::WithdrawFromVoteAccount {
+        vote_account_pubkey,
+        withdraw_authority: 1,
+        withdraw_amount: SpendAmount::Some(expected_balance),
+        destination_account_pubkey: destination_account,
+        sign_only: true,
+        dump_transaction_message: false,
+        blockhash_query: BlockhashQuery::None(blockhash),
+        nonce_account: None,
+        nonce_authority: 0,
+        memo: None,
+        fee_payer: 0,
+    };
+    process_command(&config_offline).unwrap();
+    config_offline.output_format = OutputFormat::JsonCompact;
+    let sig_response = process_command(&config_offline).unwrap();
+    let sign_only = parse_sign_only_reply_string(&sig_response);
+    let offline_presigner = sign_only
+        .presigner_of(&config_offline.signers[1].pubkey())
+        .unwrap();
+    config_payer.signers = vec![&default_signer, &offline_presigner];
+    config_payer.command = CliCommand::WithdrawFromVoteAccount {
+        vote_account_pubkey,
+        withdraw_authority: 1,
+        withdraw_amount: SpendAmount::Some(expected_balance),
+        destination_account_pubkey: destination_account,
+        sign_only: false,
+        dump_transaction_message: false,
+        blockhash_query: BlockhashQuery::FeeCalculator(blockhash_query::Source::Cluster, blockhash),
+        nonce_account: None,
+        nonce_authority: 0,
+        memo: None,
+        fee_payer: 0,
+    };
+    let result = process_command(&config_payer).unwrap();
+    println!("{:?}", result);
+    check_recent_balance(0, &rpc_client, &vote_account_pubkey);
+    println!("what");
     check_recent_balance(expected_balance, &rpc_client, &destination_account);
 }
