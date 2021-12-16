98a0d39ac9bc93b29e27af34ff10a6f7b0c8c2e7
Tyera Eulberg <tyera@solana.com>
2021-12-16 14:28:09 -0700

    Revert "Revert "Add more reporting for invalid stake cache members and prune them (backport #21654) (#21740)""
    This reverts commit 8028f218a4783276cf9669c6a0b40bc6ac18b920.

--- sdk/src/feature_set.rs
+++ sdk/src/feature_set.rs
@@ -281,6 +281,10 @@ pub mod reject_non_rent_exempt_vote_withdraws {
     solana_sdk::declare_id!("7txXZZD6Um59YoLMF7XUNimbMjsqsWhc7g2EniiTrmp1");
 }
 
+pub mod evict_invalid_stakes_cache_entries {
+    solana_sdk::declare_id!("EMX9Q7TVFAmQ9V1CggAkhMzhXSg8ECp7fHrWQX2G1chf");
+}
+
 lazy_static! {
     /// Map of feature identifiers to user-visible description
     pub static ref FEATURE_NAMES: HashMap<Pubkey, &'static str> = [
@@ -351,6 +355,7 @@ lazy_static! {
         (reject_all_elf_rw::id(), "reject all read-write data in program elfs"),
         (spl_token_v3_3_0_release::id(), "spl-token v3.3.0 release"),
         (reject_non_rent_exempt_vote_withdraws::id(), "fail vote withdraw instructions which leave the account non-rent-exempt"),
+        (evict_invalid_stakes_cache_entries::id(), "evict invalid stakes cache entries on epoch boundaries"),
         /*************** ADD NEW FEATURES HERE ***************/
     ]
     .iter()
