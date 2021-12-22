de82ec348e0adf8a99c48747d6406f595c80300e
carllin <carl@solana.com>
2021-12-22 15:07:07 -0500

    Increment execution timings on errors as well (#22053)
    (cherry picked from commit 37f6777ceb0b55f851ba0f0f9adf44b3cd40d68a)
    
    # Conflicts:
    #	programs/bpf_loader/src/lib.rs

--- programs/bpf_loader/src/lib.rs
+++ programs/bpf_loader/src/lib.rs
@@ -912,8 +912,12 @@ impl Executor for BpfExecutor {
         serialize_time.stop();
         let mut create_vm_time = Measure::start("create_vm");
         let mut execute_time;
+<<<<<<< HEAD
         {
             let compute_meter = invoke_context.get_compute_meter();
+=======
+        let execution_result = {
+>>>>>>> 37f6777ce (Increment execution timings on errors as well (#22053))
             let mut vm = match create_vm(
                 loader_id,
                 &self.executable,
@@ -958,6 +962,7 @@ impl Executor for BpfExecutor {
                 stable_log::program_return(&logger, program_id, return_data);
             }
             match result {
+<<<<<<< HEAD
                 Ok(status) => {
                     if status != SUCCESS {
                         let error: InstructionError = if !add_missing_program_error_mappings
@@ -971,6 +976,12 @@ impl Executor for BpfExecutor {
                         stable_log::program_failure(&logger, program_id, &error);
                         return Err(error);
                     }
+=======
+                Ok(status) if status != SUCCESS => {
+                    let error: InstructionError = status.into();
+                    stable_log::program_failure(&log_collector, &program_id, &error);
+                    Err(error)
+>>>>>>> 37f6777ce (Increment execution timings on errors as well (#22053))
                 }
                 Err(error) => {
                     let error = match error {
@@ -982,13 +993,21 @@ impl Executor for BpfExecutor {
                             InstructionError::ProgramFailedToComplete
                         }
                     };
+<<<<<<< HEAD
                     stable_log::program_failure(&logger, program_id, &error);
                     return Err(error);
+=======
+                    stable_log::program_failure(&log_collector, &program_id, &error);
+                    Err(error)
+>>>>>>> 37f6777ce (Increment execution timings on errors as well (#22053))
                 }
+                _ => Ok(()),
             }
-            execute_time.stop();
-        }
+        };
+        execute_time.stop();
+
         let mut deserialize_time = Measure::start("deserialize");
+<<<<<<< HEAD
         let keyed_accounts = invoke_context.get_keyed_accounts()?;
         deserialize_parameters(loader_id, keyed_accounts, parameter_bytes.as_slice())?;
         deserialize_time.stop();
@@ -1000,6 +1019,35 @@ impl Executor for BpfExecutor {
         );
         stable_log::program_success(&logger, program_id);
         Ok(())
+=======
+        let execute_or_deserialize_result = execution_result.and_then(|_| {
+            let keyed_accounts = invoke_context.get_keyed_accounts()?;
+            deserialize_parameters(
+                &loader_id,
+                &keyed_accounts[first_instruction_account + 1..],
+                parameter_bytes.as_slice(),
+                &account_lengths,
+                invoke_context
+                    .feature_set
+                    .is_active(&do_support_realloc::id()),
+            )
+        });
+        deserialize_time.stop();
+
+        // Update the timings
+        let timings = &mut invoke_context.timings;
+        timings.serialize_us = timings.serialize_us.saturating_add(serialize_time.as_us());
+        timings.create_vm_us = timings.create_vm_us.saturating_add(create_vm_time.as_us());
+        timings.execute_us = timings.execute_us.saturating_add(execute_time.as_us());
+        timings.deserialize_us = timings
+            .deserialize_us
+            .saturating_add(deserialize_time.as_us());
+
+        if execute_or_deserialize_result.is_ok() {
+            stable_log::program_success(&log_collector, &program_id);
+        }
+        execute_or_deserialize_result
+>>>>>>> 37f6777ce (Increment execution timings on errors as well (#22053))
     }
 }
 
