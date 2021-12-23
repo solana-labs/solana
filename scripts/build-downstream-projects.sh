78892af4db63efb80fb1def3bb8254685e4d03cc
Jon Cinque <jon.cinque@gmail.com>
2021-12-23 15:19:55 -0500

    ci: Add Anchor and Anchor projects to the downstream build

--- scripts/build-downstream-projects.sh
+++ scripts/build-downstream-projects.sh
@@ -27,15 +27,35 @@ update_solana_dependencies() {
   sed -i -e "s#\(solana-sdk = { version = \"\)[^\"]*\(\"\)#\1=$solana_ver\2#g" "${tomls[@]}" || return $?
   sed -i -e "s#\(solana-client = \"\)[^\"]*\(\"\)#\1=$solana_ver\2#g" "${tomls[@]}" || return $?
   sed -i -e "s#\(solana-client = { version = \"\)[^\"]*\(\"\)#\1=$solana_ver\2#g" "${tomls[@]}" || return $?
+  sed -i -e "s#\(solana-clap-utils = \"\)[^\"]*\(\"\)#\1=$solana_ver\2#g" "${tomls[@]}" || return $?
+  sed -i -e "s#\(solana-clap-utils = { version = \"\)[^\"]*\(\"\)#\1=$solana_ver\2#g" "${tomls[@]}" || return $?
 }
 
-patch_crates_io() {
+update_anchor_dependencies() {
+  declare tomls=()
+  while IFS='' read -r line; do tomls+=("$line"); done < <(find "$1" -name Cargo.toml)
+
+  sed -i -e "s#\(anchor-lang = \"\)[^\"]*\(\"\)#\1=$anchor_ver\2#g" "${tomls[@]}" || return $?
+  sed -i -e "s#\(anchor-spl = \"\)[^\"]*\(\"\)#\1=$anchor_ver\2#g" "${tomls[@]}" || return $?
+  sed -i -e "s#\(anchor-lang = { version = \"\)[^\"]*\(\"\)#\1=$anchor_ver\2#g" "${tomls[@]}" || return $?
+  sed -i -e "s#\(anchor-spl = { version = \"\)[^\"]*\(\"\)#\1=$anchor_ver\2#g" "${tomls[@]}" || return $?
+}
+
+patch_crates_io_solana() {
   cat >> "$1" <<EOF
 [patch.crates-io]
+solana-clap-utils = { path = "$solana_dir/clap-utils" }
 solana-client = { path = "$solana_dir/client" }
 solana-program = { path = "$solana_dir/sdk/program" }
 solana-program-test = { path = "$solana_dir/program-test" }
 solana-sdk = { path = "$solana_dir/sdk" }
+EOF
+}
+
+patch_crates_io_anchor() {
+  cat >> "$1" <<EOF
+anchor-lang = { path = "$anchor_dir/lang" }
+anchor-spl = { path = "$anchor_dir/spl" }
 EOF
 }
 
@@ -47,7 +67,7 @@ example_helloworld() {
     cd example-helloworld
 
     update_solana_dependencies src/program-rust
-    patch_crates_io src/program-rust/Cargo.toml
+    patch_crates_io_solana src/program-rust/Cargo.toml
     echo "[workspace]" >> src/program-rust/Cargo.toml
 
     $cargo_build_bpf \
@@ -81,8 +101,8 @@ serum_dex() {
     cd serum-dex
 
     update_solana_dependencies .
-    patch_crates_io Cargo.toml
-    patch_crates_io dex/Cargo.toml
+    patch_crates_io_solana Cargo.toml
+    patch_crates_io_solana dex/Cargo.toml
     cat >> dex/Cargo.toml <<EOF
 [workspace]
 exclude = [
@@ -100,7 +120,66 @@ EOF
   )
 }
 
+# NOTE This isn't run in a subshell to get $anchor_dir and $anchor_ver
+anchor() {
+  set -x
+  rm -rf anchor
+  git clone https://github.com/project-serum/anchor.git
+  cd anchor
 
+  update_solana_dependencies .
+  patch_crates_io_solana Cargo.toml
+
+  $cargo build
+  $cargo test
+
+  anchor_dir=$PWD
+  anchor_ver=$(readCargoVariable version "$anchor_dir"/lang/Cargo.toml)
+
+  cd "$solana_dir"/target/downstream-projects
+}
+
+mango() {
+  (
+    set -x
+    rm -rf mango-v3
+    git clone https://github.com/blockworks-foundation/mango-v3
+    cd mango-v3
+
+    update_solana_dependencies .
+    update_anchor_dependencies .
+    patch_crates_io_solana Cargo.toml
+    patch_crates_io_anchor Cargo.toml
+
+    $cargo build
+    $cargo test
+    $cargo_build_bpf
+    $cargo_test_bpf
+  )
+}
+
+metaplex() {
+  (
+    set -x
+    rm -rf metaplex
+    git clone https://github.com/metaplex-foundation/metaplex
+    cd metaplex/rust
+
+    update_solana_dependencies .
+    update_anchor_dependencies .
+    patch_crates_io_solana Cargo.toml
+    patch_crates_io_anchor Cargo.toml
+
+    $cargo build
+    $cargo test
+    $cargo_build_bpf
+    $cargo_test_bpf
+  )
+}
+
+_ anchor
 _ example_helloworld
 _ spl
 _ serum_dex
+#_ metaplex
+_ mango
