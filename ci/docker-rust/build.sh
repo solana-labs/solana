ca80a085dc4bbd92f96d14830bb7ca91086ed9be
lijunwangs <lijun@solana.com>
2022-01-05 21:03:34 +0000

    For postgres plugin testingwq

--- ci/docker-rust/build.sh
+++ ci/docker-rust/build.sh
@@ -8,5 +8,5 @@ docker build -t solanalabs/rust .
 read -r rustc version _ < <(docker run solanalabs/rust rustc --version)
 [[ $rustc = rustc ]]
 docker tag solanalabs/rust:latest solanalabs/rust:"$version"
-docker push solanalabs/rust:"$version"
-docker push solanalabs/rust:latest
+# docker push solanalabs/rust:"$version"
+# docker push solanalabs/rust:latest
