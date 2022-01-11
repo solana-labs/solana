f6f16e253d1370795e725ec3c5271005d38d2bd5
behzad nouri <behzadnouri@gmail.com>
2022-01-11 09:06:06 -0500

    adds more comments and test coverage

--- runtime/src/read_only_accounts_cache.rs
+++ runtime/src/read_only_accounts_cache.rs
@@ -31,7 +31,7 @@ pub(crate) struct ReadOnlyAccountsCache {
     // When an item is first entered into the cache, it is added to the end of
     // the queue. Also each time an entry is looked up from the cache it is
     // moved to the end of the queue. As a result, items in the queue are
-    // always sorted in the order that they last have been accessed. When doing
+    // always sorted in the order that they have last been accessed. When doing
     // LRU eviction, cache entries are evicted from the front of the queue.
     queue: Mutex<IndexList<ReadOnlyCacheKey>>,
     max_data_size: usize,
@@ -63,6 +63,8 @@ impl ReadOnlyAccountsCache {
         };
         self.hits.fetch_add(1, Ordering::Relaxed);
         // Move the entry to the end of the queue.
+        // self.queue is modified while holding a reference to the cache entry;
+        // so that another thread cannot write to the same key.
         {
             let mut queue = self.queue.lock().unwrap();
             queue.remove(entry.index);
@@ -79,6 +81,8 @@ impl ReadOnlyAccountsCache {
         let key = (pubkey, slot);
         let account_size = self.account_size(&account);
         self.data_size.fetch_add(account_size, Ordering::Relaxed);
+        // self.queue is modified while holding a reference to the cache entry;
+        // so that another thread cannot write to the same key.
         match self.cache.entry(key) {
             Entry::Vacant(entry) => {
                 // Insert the entry at the end of the queue.
@@ -109,6 +113,9 @@ impl ReadOnlyAccountsCache {
 
     pub(crate) fn remove(&self, pubkey: Pubkey, slot: Slot) -> Option<AccountSharedData> {
         let (_, entry) = self.cache.remove(&(pubkey, slot))?;
+        // self.queue should be modified only after removing the entry from the
+        // cache, so that this is still safe if another thread writes to the
+        // same key.
         self.queue.lock().unwrap().remove(entry.index);
         let account_size = self.account_size(&entry.account);
         self.data_size.fetch_sub(account_size, Ordering::Relaxed);
@@ -134,8 +141,13 @@ impl ReadOnlyAccountsCache {
 mod tests {
     use {
         super::*,
+        rand::{
+            seq::{IteratorRandom, SliceRandom},
+            Rng, SeedableRng,
+        },
+        rand_chacha::ChaChaRng,
         solana_sdk::account::{accounts_equal, Account, WritableAccount},
-        std::sync::Arc,
+        std::{collections::HashMap, iter::repeat_with, sync::Arc},
     };
     #[test]
     fn test_accountsdb_sizeof() {
@@ -206,5 +218,63 @@ mod tests {
         assert!(accounts_equal(&cache.load(key2, slot).unwrap(), &account1));
         assert!(accounts_equal(&cache.load(key3, slot).unwrap(), &account3));
         assert_eq!(2, cache.cache_len());
+    }
+
+    #[test]
+    fn test_read_only_accounts_cache_random() {
+        const SEED: [u8; 32] = [0xdb; 32];
+        const DATA_SIZE: usize = 19;
+        const MAX_CACHE_SIZE: usize = 17 * (CACHE_ENTRY_SIZE + DATA_SIZE);
+        let mut rng = ChaChaRng::from_seed(SEED);
+        let cache = ReadOnlyAccountsCache::new(MAX_CACHE_SIZE);
+        let slots: Vec<Slot> = repeat_with(|| rng.gen_range(0, 1000)).take(5).collect();
+        let pubkeys: Vec<Pubkey> = repeat_with(|| {
+            let mut arr = [0u8; 32];
+            rng.fill(&mut arr[..]);
+            Pubkey::new_from_array(arr)
+        })
+        .take(7)
+        .collect();
+        let mut hash_map = HashMap::<ReadOnlyCacheKey, (AccountSharedData, usize)>::new();
+        for ix in 0..1000 {
+            if rng.gen_bool(0.1) {
+                let key = *cache.cache.iter().choose(&mut rng).unwrap().key();
+                let (pubkey, slot) = key;
+                let account = cache.load(pubkey, slot).unwrap();
+                let (other, index) = hash_map.get_mut(&key).unwrap();
+                assert_eq!(account, *other);
+                *index = ix;
+            } else {
+                let mut data = vec![0u8; DATA_SIZE];
+                rng.fill(&mut data[..]);
+                let account = AccountSharedData::from(Account {
+                    lamports: rng.gen(),
+                    data,
+                    executable: rng.gen(),
+                    rent_epoch: rng.gen(),
+                    owner: Pubkey::default(),
+                });
+                let slot = *slots.choose(&mut rng).unwrap();
+                let pubkey = *pubkeys.choose(&mut rng).unwrap();
+                let key = (pubkey, slot);
+                hash_map.insert(key, (account.clone(), ix));
+                cache.store(pubkey, slot, account);
+            }
+        }
+        assert_eq!(cache.cache_len(), 17);
+        assert_eq!(hash_map.len(), 35);
+        let index = hash_map
+            .iter()
+            .filter(|(k, _)| cache.cache.contains_key(k))
+            .map(|(_, (_, ix))| *ix)
+            .min()
+            .unwrap();
+        for (key, (account, ix)) in hash_map {
+            let (pubkey, slot) = key;
+            assert_eq!(
+                cache.load(pubkey, slot),
+                if ix < index { None } else { Some(account) }
+            );
+        }
     }
 }
