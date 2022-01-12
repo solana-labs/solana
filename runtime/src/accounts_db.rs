f801cae121202109c4024788543d4fc5e33651cc
Jeff Washington (jwash) <wash678@gmail.com>
2022-01-12 14:38:29 -0600

    9/10 of old slots go into ancient append vecs

--- runtime/src/accounts_db.rs
+++ runtime/src/accounts_db.rs
@@ -3043,7 +3043,7 @@ impl AccountsDb {
     fn shrink_ancient_slots(&self) {
         let max_root = self.accounts_index.max_root();
         use solana_sdk::clock::DEFAULT_SLOTS_PER_EPOCH;
-        let epoch_width = DEFAULT_SLOTS_PER_EPOCH * 9 / 10; // todo - put some 'in-this-epoch' slots into an ancient append vec
+        let epoch_width = DEFAULT_SLOTS_PER_EPOCH * 1 / 10; // todo - put some 'in-this-epoch' slots into an ancient append vec
         let old_root = max_root.saturating_sub(epoch_width);
 
         let mut m = Measure::start("get slots");
