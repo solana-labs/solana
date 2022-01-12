54ab28b1b43fbb2c97304e56e083a13da026288e
Jack May <jack@solana.com>
2022-01-12 11:34:15 -0800

    nit: Traceable balance checks

--- cli/src/test_utils.rs
+++ cli/src/test_utils.rs
@@ -1,23 +1,29 @@
 use {
     solana_client::rpc_client::RpcClient,
-    solana_sdk::{clock::DEFAULT_MS_PER_SLOT, commitment_config::CommitmentConfig, pubkey::Pubkey},
+    solana_sdk::{clock::DEFAULT_MS_PER_SLOT, commitment_config::CommitmentConfig},
     std::{thread::sleep, time::Duration},
 };
 
-pub fn check_recent_balance(expected_balance: u64, client: &RpcClient, pubkey: &Pubkey) {
-    (0..5).for_each(|tries| {
-        let balance = client
-            .get_balance_with_commitment(pubkey, CommitmentConfig::processed())
-            .unwrap()
-            .value;
-        if balance == expected_balance {
-            return;
-        }
-        if tries == 4 {
-            assert_eq!(balance, expected_balance);
-        }
-        sleep(Duration::from_millis(500));
-    });
+#[macro_export]
+macro_rules! check_balance {
+    ($expected_balance:expr, $client:expr, $pubkey:expr) => {
+        (0..5).for_each(|tries| {
+            let balance = $client
+                .get_balance_with_commitment($pubkey, CommitmentConfig::processed())
+                .unwrap()
+                .value;
+            if balance == $expected_balance {
+                return;
+            }
+            if tries == 4 {
+                assert_eq!(balance, $expected_balance);
+            }
+            std::thread::sleep(std::time::Duration::from_millis(500));
+        });
+    };
+    ($expected_balance:expr, $client:expr, $pubkey:expr,) => {
+        check_balance!($expected_balance, $client, $pubkey)
+    };
 }
 
 pub fn check_ready(rpc_client: &RpcClient) {
