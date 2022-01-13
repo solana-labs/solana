216493d21056783ba7dc17746bee050722eadaf7
Anatoly Yakovenko <anatoly@solana.com>
2022-01-13 11:57:53 -0800

    metrics for generate new bank forks

--- core/src/replay_stage.rs
+++ core/src/replay_stage.rs
@@ -164,6 +164,10 @@ pub struct ReplayTiming {
     process_unfrozen_gossip_verified_vote_hashes_elapsed: u64,
     repair_correct_slots_elapsed: u64,
     retransmit_not_propagated_elapsed: u64,
+    generate_new_bank_forks_read_lock: u64,
+    generate_new_bank_forks_get_slots_since: u64,
+    generate_new_bank_forks_loop: u64,
+    generate_new_bank_forks_write_lock: u64,
 }
 impl ReplayTiming {
     #[allow(clippy::too_many_arguments)]
@@ -303,8 +307,27 @@ impl ReplayTiming {
                     self.retransmit_not_propagated_elapsed as i64,
                     i64
                 ),
+                (
+                    "generate_new_bank_forks_read_lock",
+                    self.generate_new_bank_forks_read_lock as i64,
+                    i64
+                ),
+                (
+                    "generate_new_bank_forks_get_slots_since",
+                    self.generate_new_bank_forks_get_slots_since as i64,
+                    i64
+                ),
+                (
+                    "generate_new_bank_forks_loop",
+                    self.generate_new_bank_forks_loop as i64,
+                    i64
+                ),
+                (
+                    "generate_new_bank_forks_write_lock",
+                    self.generate_new_bank_forks_write_lock as i64,
+                    i64
+                ),
             );
-
             *self = ReplayTiming::default();
             self.last_print = now;
         }
@@ -413,6 +436,7 @@ impl ReplayStage {
                         &leader_schedule_cache,
                         &rpc_subscriptions,
                         &mut progress,
+                        &mut replay_timing,
                     );
                     generate_new_bank_forks_time.stop();
 
@@ -2852,24 +2876,34 @@ impl ReplayStage {
         leader_schedule_cache: &Arc<LeaderScheduleCache>,
         rpc_subscriptions: &Arc<RpcSubscriptions>,
         progress: &mut ProgressMap,
+        replay_timing: &mut ReplayTiming,
     ) {
         // Find the next slot that chains to the old slot
+        let mut generate_new_bank_forks_read_lock =
+            Measure::start("generate_new_bank_forks_read_lock");
         let forks = bank_forks.read().unwrap();
+        generate_new_bank_forks_read_lock.stop();
+
         let frozen_banks = forks.frozen_banks();
         let frozen_bank_slots: Vec<u64> = frozen_banks
             .keys()
             .cloned()
             .filter(|s| *s >= forks.root())
             .collect();
+        let mut generate_new_bank_forks_get_slots_since =
+            Measure::start("generate_new_bank_forks_get_slots_since");
         let next_slots = blockstore
             .get_slots_since(&frozen_bank_slots)
             .expect("Db error");
+        generate_new_bank_forks_get_slots_since.stop();
+
         // Filter out what we've already seen
         trace!("generate new forks {:?}", {
             let mut next_slots = next_slots.iter().collect::<Vec<_>>();
             next_slots.sort();
             next_slots
         });
+        let mut generate_new_bank_forks_loop = Measure::start("generate_new_bank_forks_loop");
         let mut new_banks = HashMap::new();
         for (parent_slot, children) in next_slots {
             let parent_bank = frozen_banks
@@ -2910,11 +2944,20 @@ impl ReplayStage {
             }
         }
         drop(forks);
+        generate_new_bank_forks_loop.stop();
 
+        let mut generate_new_bank_forks_write_lock =
+            Measure::start("generate_new_bank_forks_write_lock");
         let mut forks = bank_forks.write().unwrap();
         for (_, bank) in new_banks {
             forks.insert(bank);
         }
+        generate_new_bank_forks_write_lock.stop();
+        replay_timing.generate_new_bank_forks_read_lock += generate_new_bank_forks_read_lock;
+        replay_timing.generate_new_bank_forks_get_slots_since +=
+            generate_new_bank_forks_get_slots_since;
+        replay_timing.generate_new_bank_forks_loop += generate_new_bank_forks_loop;
+        replay_timing.generate_new_bank_forks_write_lock += generate_new_bank_forks_write_lock;
     }
 
     fn new_bank_from_parent_with_notify(
