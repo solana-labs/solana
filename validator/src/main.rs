43bcf81aca9638c7acc125ecda486e146d4ee7f7
Jeff Biseda <jbiseda@gmail.com>
2022-01-20 14:02:47 -0800

    require --no-os-network-limits-test to bypass failed network limit checks

--- validator/src/main.rs
+++ validator/src/main.rs
@@ -2441,6 +2441,7 @@ pub fn main() {
             info!("OS network limits test passed.");
         } else {
             eprintln!("OS network limit test failed. solana-sys-tuner may be used to configure OS network limits. Bypass check with --no-os-network-limits-test.");
+            exit(1);
         }
     }
 
