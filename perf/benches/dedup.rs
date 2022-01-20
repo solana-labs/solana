48d841bbcfaf568a5672522d7abf9b050f951b60
Anatoly Yakovenko <anatoly@solana.com>
2022-01-19 22:38:33 -0800

    perf tests

--- perf/benches/dedup.rs
+++ perf/benches/dedup.rs
@@ -10,36 +10,54 @@ use {
 
 #[bench]
 fn bench_dedup_same(bencher: &mut Bencher) {
-    let tx = test_tx();
-
     // generate packet vector
-    let mut batches = to_packet_batches(
-        &std::iter::repeat(tx).take(64 * 1024).collect::<Vec<_>>(),
-        128,
-    );
-    let packet_count = sigverify::count_packets_in_batches(&batches);
     let bloom: AtomicBloom<&[u8]> = Bloom::random(1_000_000, 0.0001, 8 << 22).into();
-
-    println!("packet_count {} {}", packet_count, batches.len());
-
+    let mut count = 0;
     // verify packets
     bencher.iter(|| {
+        let tx = test_tx();
+        let mut batches =
+            to_packet_batches(&std::iter::repeat(tx).take(4096).collect::<Vec<_>>(), 128);
         let _ans = sigverify::dedup_packets(&bloom, &mut batches);
-    })
+        count += 4096;
+    });
+    println!("total {}", count);
+}
+
+#[bench]
+fn bench_dedup_same_baseline(bencher: &mut Bencher) {
+    let mut count = 0;
+    bencher.iter(|| {
+        let tx = test_tx();
+        let mut batches =
+            to_packet_batches(&std::iter::repeat(tx).take(4096).collect::<Vec<_>>(), 128);
+        count += 4096;
+    });
+    println!("total {}", count);
 }
 
 #[bench]
 fn bench_dedup_diff(bencher: &mut Bencher) {
     // generate packet vector
-    let mut batches =
-        to_packet_batches(&(0..64 * 1024).map(|_| test_tx()).collect::<Vec<_>>(), 128);
-    let packet_count = sigverify::count_packets_in_batches(&batches);
     let bloom: AtomicBloom<&[u8]> = Bloom::random(1_000_000, 0.0001, 8 << 22).into();
 
-    println!("packet_count {} {}", packet_count, batches.len());
-
+    let mut count = 0;
     // verify packets
     bencher.iter(|| {
+        let mut batches = to_packet_batches(&(0..4096).map(|_| test_tx()).collect::<Vec<_>>(), 128);
         let _ans = sigverify::dedup_packets(&bloom, &mut batches);
-    })
+        count += 4096;
+    });
+    println!("total {}", count);
+}
+
+#[bench]
+fn bench_dedup_diff_baseline(bencher: &mut Bencher) {
+    let mut count = 0;
+    bencher.iter(|| {
+        let mut _batches =
+            to_packet_batches(&(0..4096).map(|_| test_tx()).collect::<Vec<_>>(), 128);
+        count += 4096;
+    });
+    println!("total {}", count);
 }
