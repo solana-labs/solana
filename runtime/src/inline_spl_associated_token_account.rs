0fda228e0570c29bb695489a247ecdee00d10ef5
Jon Cinque <jon.cinque@gmail.com>
2022-01-21 20:35:33 +0100

    spl-associated-token-account: Add feature for new program

---
+++ runtime/src/inline_spl_associated_token_account.rs
@@ -0,0 +1,6 @@
+// Partial SPL Associated Token Account declarations inlined to avoid an external dependency on the spl-associated-token-account crate
+solana_sdk::declare_id!("ATokenGPvbdGVxr1b2hvZbsiqW5xWH25efTNsLJA8knL");
+
+pub(crate) mod new_program {
+    solana_sdk::declare_id!("nata4apBRD9S9256v3X8RxDQ7jmK7wLEsGNHhRNWFq3");
+}
