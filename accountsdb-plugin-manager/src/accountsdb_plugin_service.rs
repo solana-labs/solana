23dd9c462a222cf1a8594e7899fc7b60e246fc36
Kirill Fomichev <fanatid@ya.ru>
2022-01-21 10:30:59 +0300

    accountsdb: support config in json5

--- accountsdb-plugin-manager/src/accountsdb_plugin_service.rs
+++ accountsdb-plugin-manager/src/accountsdb_plugin_service.rs
@@ -9,7 +9,6 @@ use {
     },
     crossbeam_channel::Receiver,
     log::*,
-    serde_json,
     solana_rpc::{
         optimistically_confirmed_bank_tracker::BankNotification,
         transaction_notifier_interface::TransactionNotifierLock,
@@ -156,12 +155,12 @@ impl AccountsDbPluginService {
             )));
         }
 
-        let result: serde_json::Value = match serde_json::from_str(&contents) {
+        let result: serde_json::Value = match json5::from_str(&contents) {
             Ok(value) => value,
             Err(err) => {
                 return Err(AccountsdbPluginServiceError::InvalidConfigFileFormat(
                     format!(
-                        "The config file {:?} is not in a valid Json format, error: {:?}",
+                        "The config file {:?} is not in a valid Json5 format, error: {:?}",
                         accountsdb_plugin_config_file, err
                     ),
                 ));
