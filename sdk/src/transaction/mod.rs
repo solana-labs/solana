bebe0d1121273638377a3328927a9cfab288991c
Tyera Eulberg <tyera@solana.com>
2022-01-21 18:35:13 -0700

    Fix conflicts

--- sdk/src/transaction/mod.rs
+++ sdk/src/transaction/mod.rs
@@ -979,24 +979,16 @@ impl Transaction {
         }
     }
 
-<<<<<<< HEAD
-    pub fn get_invalid_signature() -> Signature {
-        Signature::default()
-    }
-
     /// Verify the length of signatures matches the value in the message header
     pub fn verify_signatures_len(&self) -> bool {
         self.signatures.len() == self.message.header.num_required_signatures as usize
     }
 
-    /// Verify the transaction and hash its message
-=======
     /// Verify the transaction and hash its message.
     ///
     /// # Errors
     ///
     /// Returns [`TransactionError::SignatureFailure`] on error.
->>>>>>> 8dd62854f (Document transaction module (#22440))
     pub fn verify_and_hash_message(&self) -> Result<Hash> {
         let message_bytes = self.message_data();
         if !self
