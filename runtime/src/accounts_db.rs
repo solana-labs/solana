8dcce632120b03b53fbbbb1592363973c55e6b4a
Jeff Washington (jwash) <wash678@gmail.com>
2022-01-24 10:52:18 -0600

    re-enable shrink

--- runtime/src/accounts_db.rs
+++ runtime/src/accounts_db.rs
@@ -3054,8 +3054,7 @@ impl AccountsDb {
     }
 
     fn shrink_ancient_slots(&self) {
-        return;
-                let max_root = self.accounts_index.max_root();
+        let max_root = self.accounts_index.max_root();
         use solana_sdk::clock::DEFAULT_SLOTS_PER_EPOCH;
         let epoch_width = DEFAULT_SLOTS_PER_EPOCH * 99 / 100; // todo - put some 'in-this-epoch' slots into an ancient append vec
         let old_root = max_root.saturating_sub(epoch_width);
