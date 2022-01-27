25ad45a58c06f4f6a903a367484e725dec100107
Michael Vines <mvines@gmail.com>
2022-01-26 22:03:14 -0800

    solana-test-validator now supports the `--rpc-pubsub-enable-vote-subscription` flag
    (cherry picked from commit 75658e2a96846c6afea95bc0e41dbabceef2c700)

--- validator/src/bin/solana-test-validator.rs
+++ validator/src/bin/solana-test-validator.rs
@@ -11,7 +11,7 @@ use {
     solana_client::rpc_client::RpcClient,
     solana_core::tower_storage::FileTowerStorage,
     solana_faucet::faucet::{run_local_faucet_with_port, FAUCET_PORT},
-    solana_rpc::rpc::JsonRpcConfig,
+    solana_rpc::{rpc::JsonRpcConfig, rpc_pubsub_service::PubSubConfig},
     solana_sdk::{
         account::AccountSharedData,
         clock::Slot,
@@ -153,6 +153,12 @@ fn main() {
                 .default_value(&default_rpc_port)
                 .validator(solana_validator::port_validator)
                 .help("Enable JSON RPC on this port, and the next port for the RPC websocket"),
+        )
+        .arg(
+            Arg::with_name("rpc_pubsub_enable_vote_subscription")
+                .long("rpc-pubsub-enable-vote-subscription")
+                .takes_value(false)
+                .help("Enable the unstable RPC PubSub `voteSubscribe` subscription"),
         )
         .arg(
             Arg::with_name("bpf_program")
@@ -395,6 +401,7 @@ fn main() {
         });
 
     let rpc_port = value_t_or_exit!(matches, "rpc_port", u16);
+    let enable_vote_subscription = matches.is_present("rpc_pubsub_enable_vote_subscription");
     let faucet_port = value_t_or_exit!(matches, "faucet_port", u16);
     let slots_per_epoch = value_t!(matches, "slots_per_epoch", Slot).ok();
     let gossip_host = matches.value_of("gossip_host").map(|gossip_host| {
@@ -600,6 +607,10 @@ fn main() {
             enable_cpi_and_log_storage: true,
             faucet_addr,
             ..JsonRpcConfig::default()
+        })
+        .pubsub_config(PubSubConfig {
+            enable_vote_subscription,
+            ..PubSubConfig::default()
         })
         .bpf_jit(!matches.is_present("no_bpf_jit"))
         .rpc_port(rpc_port)
