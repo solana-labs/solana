668cd92efed6d2a753c4e90f52dfe8b63f2a84b3
Will Hickey <will.hickey@solana.com>
2022-02-03 21:57:47 -0600

    Update test_abi_digest hash

--- runtime/src/bank.rs
+++ runtime/src/bank.rs
@@ -200,7 +200,7 @@ impl RentDebits {
 }
 
 type BankStatusCache = StatusCache<Result<()>>;
-#[frozen_abi(digest = "6XkxpmzmKZguLZMS1KmU7N2dAcv8MmNhyobJCwRLkTdi")]
+#[frozen_abi(digest = "2gyFzcvK5sXe1NgcGWnd4ATf3pCBaS43sNVPSDdjd7Rj")]
 pub type BankSlotDelta = SlotDelta<Result<()>>;
 pub(crate) type TransactionAccountRefCell = (Pubkey, Rc<RefCell<AccountSharedData>>);
 type TransactionAccountRefCells = Vec<TransactionAccountRefCell>;
