2f6490978bb8756f9b7d67f4f08fa3f465810f67
Michael Vines <mvines@gmail.com>
2022-02-07 23:17:24 -0800

    monitor: Remove getMaxRetransmitSlot RPC method usage
    (cherry picked from commit dcd4ea91110d2c0676b6c86782f2e04508837f20)
    
    # Conflicts:
    #	validator/src/dashboard.rs

--- validator/src/dashboard.rs
+++ validator/src/dashboard.rs
@@ -124,7 +124,6 @@ impl Dashboard {
 
                 match get_validator_stats(&rpc_client, &identity) {
                     Ok((
-                        max_retransmit_slot,
                         processed_slot,
                         confirmed_slot,
                         finalized_slot,
@@ -144,8 +143,13 @@ impl Dashboard {
                             )
                         };
 
+<<<<<<< HEAD
                         progress_bar.set_message(&format!(
                             "{}{}{}| \
+=======
+                        progress_bar.set_message(format!(
+                            "{}{}| \
+>>>>>>> dcd4ea911 (monitor: Remove getMaxRetransmitSlot RPC method usage)
                                     Processed Slot: {} | Confirmed Slot: {} | Finalized Slot: {} | \
                                     Snapshot Slot: {} | \
                                     Transactions: {} | {}",
@@ -154,11 +158,6 @@ impl Dashboard {
                                 "".to_string()
                             } else {
                                 format!("| {} ", style(health).bold().red())
-                            },
-                            if max_retransmit_slot == 0 {
-                                "".to_string()
-                            } else {
-                                format!("| Max Slot: {} ", max_retransmit_slot)
                             },
                             processed_slot,
                             confirmed_slot,
@@ -251,11 +250,10 @@ fn get_contact_info(rpc_client: &RpcClient, identity: &Pubkey) -> Option<RpcCont
 fn get_validator_stats(
     rpc_client: &RpcClient,
     identity: &Pubkey,
-) -> client_error::Result<(Slot, Slot, Slot, Slot, u64, Sol, String)> {
+) -> client_error::Result<(Slot, Slot, Slot, u64, Sol, String)> {
     let finalized_slot = rpc_client.get_slot_with_commitment(CommitmentConfig::finalized())?;
     let confirmed_slot = rpc_client.get_slot_with_commitment(CommitmentConfig::confirmed())?;
     let processed_slot = rpc_client.get_slot_with_commitment(CommitmentConfig::processed())?;
-    let max_retransmit_slot = rpc_client.get_max_retransmit_slot()?;
     let transaction_count =
         rpc_client.get_transaction_count_with_commitment(CommitmentConfig::processed())?;
     let identity_balance = rpc_client
@@ -284,7 +282,6 @@ fn get_validator_stats(
     };
 
     Ok((
-        max_retransmit_slot,
         processed_slot,
         confirmed_slot,
         finalized_slot,
