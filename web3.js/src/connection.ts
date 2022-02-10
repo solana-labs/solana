05bcac9dbf06567617d4339c0bd1c308184e18a5
Arrowana <8245419+Arrowana@users.noreply.github.com>
2022-02-10 13:22:17 +1100

    web3.js Remove deprecated commitments

--- web3.js/src/connection.ts
+++ web3.js/src/connection.ts
@@ -224,24 +224,6 @@ function notificationResultAndContext<T, U>(value: Struct<T, U>) {
 }
 
 /**
- * The level of commitment desired when querying state
- * <pre>
- *   'processed': Query the most recent block which has reached 1 confirmation by the connected node
- *   'confirmed': Query the most recent block which has reached 1 confirmation by the cluster
- *   'finalized': Query the most recent block which has been finalized by the cluster
- * </pre>
- */
-export type Commitment =
-  | 'processed'
-  | 'confirmed'
-  | 'finalized'
-  | 'recent' // Deprecated as of v1.5.5
-  | 'single' // Deprecated as of v1.5.5
-  | 'singleGossip' // Deprecated as of v1.5.5
-  | 'root' // Deprecated as of v1.5.5
-  | 'max'; // Deprecated as of v1.5.5
-
-/**
  * A subset of Commitment levels, which are at least optimistically confirmed
  * <pre>
  *   'confirmed': Query the most recent block which has reached 1 confirmation by the cluster
@@ -249,6 +231,16 @@ export type Commitment =
  * </pre>
  */
 export type Finality = 'confirmed' | 'finalized';
+
+/**
+ * The level of commitment desired when querying state
+ * <pre>
+ *   'processed': Query the most recent block which has reached 1 confirmation by the connected node
+ *   'confirmed': Query the most recent block which has reached 1 confirmation by the cluster
+ *   'finalized': Query the most recent block which has been finalized by the cluster
+ * </pre>
+ */
+export type Commitment = 'processed' | Finality;
 
 /**
  * Filter for largest accounts query
