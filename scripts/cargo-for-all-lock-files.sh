50e36d75ce7711386b3558c4a958f596a11cbe9e
Will Hickey <will.hickey@solana.com>
2022-02-12 10:06:22 -0600

    lint

--- scripts/cargo-for-all-lock-files.sh
+++ scripts/cargo-for-all-lock-files.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
 here="$(dirname "$0")"
-cargo="$(readlink -f "${here}/../cargo")" 
+cargo="$(readlink -f "${here}/../cargo")"
 
 if [[ -z $cargo ]]; then
-  >&2 echo "Failed to find cargo. Mac readlink doesn't support -f. Consider switching 
-  to gnu readlink with 'brew install coreutils' and then symlink greadlink as 
+  >&2 echo "Failed to find cargo. Mac readlink doesn't support -f. Consider switching
+  to gnu readlink with 'brew install coreutils' and then symlink greadlink as
   /usr/local/bin/readlink."
   exit 1
 fi
