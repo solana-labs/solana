24fc39c63b1cd787c255c50f75b0a4e81c5255d9
frolovdev <frolovandrey752@gmail.com>
2022-02-15 18:40:38 +0300

    fix: update bash script with ok

--- web3.js/.travis/script.sh
+++ web3.js/.travis/script.sh
@@ -9,7 +9,6 @@ ls -l lib
 test -r lib/index.iife.js
 test -r lib/index.cjs.js
 test -r lib/index.esm.js
-npm run doc
-npm run lint
+npm run ok
 npm run codecov
 npm run test:live-with-test-validator
