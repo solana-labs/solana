cf919e7eb6e239cf8d99865bddae74b25603709f
Will Hickey <will.hickey@solana.com>
2022-02-17 22:02:05 -0600

    Add --locked to spl-token-cli install

--- scripts/cargo-install-all.sh
+++ scripts/cargo-install-all.sh
@@ -133,7 +133,7 @@ mkdir -p "$installDir/bin"
   # Exclude `spl-token` binary for net.sh builds
   if [[ -z "$validatorOnly" ]]; then
     # shellcheck disable=SC2086 # Don't want to double quote $rust_version
-    "$cargo" $maybeRustVersion install spl-token-cli --root "$installDir"
+    "$cargo" $maybeRustVersion install --locked spl-token-cli --root "$installDir"
   fi
 )
 
