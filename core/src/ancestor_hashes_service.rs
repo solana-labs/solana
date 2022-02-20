6a0b9cfbfc02bfb2567739e97e790fa9b2b15713
Haoran Yi <haoran.yi@gmail.com>
2022-02-19 19:36:20 -0600

    share packet threshold with repair service (credit to carl)

--- core/src/ancestor_hashes_service.rs
+++ core/src/ancestor_hashes_service.rs
@@ -3,6 +3,7 @@ use {
         cluster_slots::ClusterSlots,
         duplicate_repair_status::{DeadSlotAncestorRequestStatus, DuplicateAncestorDecision},
         outstanding_requests::OutstandingRequests,
+        packet_threshold::DynamicPacketToProcessThreshold,
         repair_response::{self},
         repair_service::{DuplicateSlotsResetSender, RepairInfo, RepairStatsGroup},
         replay_stage::DUPLICATE_THRESHOLD,
@@ -133,58 +134,6 @@ impl AncestorRepairRequestsStats {
 
 pub struct AncestorHashesService {
     thread_hdls: Vec<JoinHandle<()>>,
-}
-
-enum PacketThresholdUpdate {
-    Increase,
-    Decrease,
-}
-
-impl PacketThresholdUpdate {
-    const PERCENTAGE: usize = 90;
-
-    fn calculate(&self, current: usize) -> usize {
-        match *self {
-            PacketThresholdUpdate::Increase => current
-                .saturating_mul(100)
-                .saturating_div(Self::PERCENTAGE),
-            PacketThresholdUpdate::Decrease => current
-                .saturating_mul(Self::PERCENTAGE)
-                .saturating_div(100),
-        }
-    }
-}
-
-struct DynamicPacketToProcessThreshold {
-    max_packets: usize,
-}
-
-impl Default for DynamicPacketToProcessThreshold {
-    fn default() -> Self {
-        Self {
-            max_packets: Self::DEFAULT_MAX_PACKETS,
-        }
-    }
-}
-
-impl DynamicPacketToProcessThreshold {
-    const DEFAULT_MAX_PACKETS: usize = 1024;
-    const TIME_THRESHOLD: Duration = Duration::from_secs(1);
-
-    fn update(&mut self, total_packets: usize, compute_time: Duration) {
-        if total_packets >= self.max_packets {
-            let threshold_update = if compute_time > Self::TIME_THRESHOLD {
-                PacketThresholdUpdate::Decrease
-            } else {
-                PacketThresholdUpdate::Increase
-            };
-            self.max_packets = threshold_update.calculate(self.max_packets);
-        }
-    }
-
-    fn should_drop(&self, total: usize) -> bool {
-        total >= self.max_packets
-    }
 }
 
 impl AncestorHashesService {
@@ -1605,30 +1554,5 @@ mod test {
 
         assert!(dead_slot_pool.is_empty());
         assert!(repairable_dead_slot_pool.contains(&request_slot));
-    }
-
-    #[test]
-    fn test_dynamic_packet_threshold() {
-        let mut threshold = DynamicPacketToProcessThreshold::default();
-        assert_eq!(
-            threshold.max_packets,
-            DynamicPacketToProcessThreshold::DEFAULT_MAX_PACKETS
-        );
-
-        assert!(!threshold.should_drop(10));
-        assert!(threshold.should_drop(2000));
-
-        let old = threshold.max_packets;
-
-        // Increase
-        let total = 2000;
-        let compute_time = Duration::from_millis(500);
-        threshold.update(total, compute_time);
-        assert!(threshold.max_packets > old);
-
-        // Decrease
-        let compute_time = Duration::from_millis(2000);
-        threshold.update(total, compute_time);
-        assert_eq!(threshold.max_packets, old - 1); // due to rounding error, there is a difference of 1
     }
 }
