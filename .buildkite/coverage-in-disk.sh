0e5a58b88315514146d2b90a3de44386cf2e9588
axleiro <83293196+axleiro@users.noreply.github.com>
2022-02-25 15:25:37 +0530

    adding a copy of "pipeline-upload.sh"
    for the new pipeline

---
+++ .buildkite/coverage-in-disk.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+#
+# This script is used to upload the full buildkite pipeline. The steps defined
+# in the buildkite UI should simply be:
+#
+#   steps:
+#    - command: ".buildkite/pipeline-upload.sh"
+#
+
+set -e
+cd "$(dirname "$0")"/..
+source ci/_
+
+_ ci/buildkite-pipeline-in-disk.sh pipeline.yml
+echo +++ pipeline
+cat pipeline.yml
+
+_ buildkite-agent pipeline upload pipeline.yml
