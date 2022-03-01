7943e8a1c376f571ace9c42fa69069b1cb7c7057
Josh <josh.hundley@gmail.com>
2022-03-01 11:41:11 -0800

    fix(explorer): retry should trigger loading state (#23417)
    * fix(explorer): retry should trigger loading state
    
    * fix(explorer): Solana ping add refreshing state

--- explorer/src/providers/stats/SolanaPingProvider.tsx
+++ explorer/src/providers/stats/SolanaPingProvider.tsx
@@ -33,6 +33,7 @@ export type PingInfo = {
 
 export enum PingStatus {
   Loading,
+  Refreshing,
   Ready,
   Error,
 }
@@ -124,15 +125,24 @@ export function SolanaPingProvider({ children }: Props) {
         .catch((error) => {
           setRollup({
             status: PingStatus.Error,
-            retry: fetchPingMetrics,
+            retry: () => {
+              setRollup({
+                status: PingStatus.Loading,
+              });
+
+              fetchPingMetrics();
+            },
           });
         });
     };
 
-    const fetchPingInterval = setInterval(
-      fetchPingMetrics,
-      FETCH_PING_INTERVAL
-    );
+    const fetchPingInterval = setInterval(() => {
+      setRollup({
+        status: PingStatus.Refreshing,
+      });
+
+      fetchPingMetrics();
+    }, FETCH_PING_INTERVAL);
     fetchPingMetrics();
     return () => {
       clearInterval(fetchPingInterval);
