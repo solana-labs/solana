82cb61dc362035089cefb00b08e9896287d4675f
Kirill Fomichev <fanatid@ya.ru>
2022-03-02 23:49:11 +0300

    Add Copy/Eq derive traits to SlotStatus in accountsdb-plugin (#23100)
    * Add copy/eq derive traits to SlotStatus in accountsdb-plugin
    
    * move Eq to derive
    
    * remove not required clone

--- accountsdb-plugin-interface/src/accountsdb_plugin_interface.rs
+++ accountsdb-plugin-interface/src/accountsdb_plugin_interface.rs
@@ -9,9 +9,7 @@ use {
     thiserror::Error,
 };
 
-impl Eq for ReplicaAccountInfo<'_> {}
-
-#[derive(Clone, PartialEq, Debug)]
+#[derive(Debug, Clone, PartialEq, Eq)]
 /// Information about an account being updated
 pub struct ReplicaAccountInfo<'a> {
     /// The Pubkey for the account
@@ -112,7 +110,7 @@ pub enum AccountsDbPluginError {
 }
 
 /// The current status of a slot
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub enum SlotStatus {
     /// The highest slot of the heaviest fork processed by the node. Ledger state at this slot is
     /// not derived from a confirmed or finalized block, but if multiple forks are present, is from
