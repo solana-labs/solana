6b2018b6667deab508480ffa6ad7c99a58587169
Jack May <jack@solana.com>
2022-03-11 15:47:24 -0800

    deny slice indexing

--- program-runtime/src/lib.rs
+++ program-runtime/src/lib.rs
@@ -1,4 +1,6 @@
 #![cfg_attr(RUSTC_WITH_SPECIALIZATION, feature(min_specialization))]
+#![deny(clippy::integer_arithmetic)]
+#![deny(clippy::indexing_slicing)]
 
 pub mod accounts_data_meter;
 pub mod compute_budget;
