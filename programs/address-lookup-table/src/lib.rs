c6f0d0759edeead8e89234c98ae6c3c17e672f8a
Justin Starry <justin@solana.com>
2022-03-16 15:56:43 +0800

    Make solana-address-lookup-table-program crate bpf compatible

--- programs/address-lookup-table/src/lib.rs
+++ programs/address-lookup-table/src/lib.rs
@@ -2,9 +2,11 @@
 #![cfg_attr(RUSTC_WITH_SPECIALIZATION, feature(specialization))]
 #![cfg_attr(RUSTC_NEEDS_PROC_MACRO_HYGIENE, feature(proc_macro_hygiene))]
 
-use solana_sdk::declare_id;
+use solana_program::declare_id;
 
+pub mod error;
 pub mod instruction;
+#[cfg(not(target_arch = "bpf"))]
 pub mod processor;
 pub mod state;
 
--- sdk/src/transaction/error.rs
+++ sdk/src/transaction/error.rs
@@ -150,33 +150,3 @@ impl From<SanitizeMessageError> for TransactionError {
         Self::SanitizeFailure
     }
 }
-
-#[derive(Debug, Error, PartialEq, Eq, Clone)]
-pub enum AddressLookupError {
-    /// Attempted to lookup addresses from a table that does not exist
-    #[error("Attempted to lookup addresses from a table that does not exist")]
-    LookupTableAccountNotFound,
-
-    /// Attempted to lookup addresses from an account owned by the wrong program
-    #[error("Attempted to lookup addresses from an account owned by the wrong program")]
-    InvalidAccountOwner,
-
-    /// Attempted to lookup addresses from an invalid account
-    #[error("Attempted to lookup addresses from an invalid account")]
-    InvalidAccountData,
-
-    /// Address lookup contains an invalid index
-    #[error("Address lookup contains an invalid index")]
-    InvalidLookupIndex,
-}
-
-impl From<AddressLookupError> for TransactionError {
-    fn from(err: AddressLookupError) -> Self {
-        match err {
-            AddressLookupError::LookupTableAccountNotFound => Self::AddressLookupTableNotFound,
-            AddressLookupError::InvalidAccountOwner => Self::InvalidAddressLookupTableOwner,
-            AddressLookupError::InvalidAccountData => Self::InvalidAddressLookupTableData,
-            AddressLookupError::InvalidLookupIndex => Self::InvalidAddressLookupTableIndex,
-        }
-    }
-}
