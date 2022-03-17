9fd8212657919159c391857c70de7563d7c3f95f
Pankaj Garg <pankaj@solana.com>
2022-03-17 09:01:41 -0700

    move time measure outside if block

--- core/src/voting_service.rs
+++ core/src/voting_service.rs
@@ -91,16 +91,16 @@ impl VotingService {
             crate::banking_stage::next_leader_tpu(cluster_info, poh_recorder)
         };
 
+        let mut measure = Measure::start("vote_tx_send-ms");
         if use_quic_client {
-            let mut measure = Measure::start("vote_using_quic-ms");
             let target_address =
                 target_address.unwrap_or_else(|| cluster_info.my_contact_info().tpu);
             let _ = get_connection(&target_address).send_transaction(vote_op.tx());
-            measure.stop();
-            inc_new_counter_info!("vote_using_quic-ms", measure.as_ms() as usize);
         } else {
             let _ = cluster_info.send_transaction(vote_op.tx(), target_address);
         }
+        measure.stop();
+        inc_new_counter_info!("vote_tx_send-ms", measure.as_ms() as usize);
 
         match vote_op {
             VoteOp::PushVote {
