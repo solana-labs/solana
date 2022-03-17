74d84598db37f6f18dfa804cd6484b73e3629693
Brian Anderson <andersrb@gmail.com>
2022-03-17 00:52:44 -0600

    Change asserts to debug asserts in syscall.rs

--- programs/bpf_loader/src/syscalls.rs
+++ programs/bpf_loader/src/syscalls.rs
@@ -2345,12 +2345,12 @@ impl<'a, 'b> SyscallInvokeSigned<'a, 'b> for SyscallInvokeSignedC<'a, 'b> {
     ) -> Result<Instruction, EbpfError<BpfError>> {
         let ix_c = translate_type::<SolInstruction>(memory_mapping, addr, loader_id)?;
 
-        assert_eq!(
+        debug_assert_eq!(
             std::mem::size_of_val(&ix_c.accounts_len),
             std::mem::size_of::<usize>(),
             "non-64-bit host"
         );
-        assert_eq!(
+        debug_assert_eq!(
             std::mem::size_of_val(&ix_c.data_len),
             std::mem::size_of::<usize>(),
             "non-64-bit host"
