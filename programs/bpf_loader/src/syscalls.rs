7e358c654f695c67dd80d32a412a6bf81ea627d6
Jack May <jack@solana.com>
2022-03-18 14:15:59 -0700

    add test to assert type assumption (#23769)

--- programs/bpf_loader/src/syscalls.rs
+++ programs/bpf_loader/src/syscalls.rs
@@ -4662,4 +4662,10 @@ mod tests {
             Err(SyscallError::BadSeeds(PubkeyError::MaxSeedLengthExceeded).into())
         );
     }
+
+    #[test]
+    fn test_check_type_assumptions() {
+        // Code in this file assumes that u64 and usize are the same
+        assert_eq!(size_of::<u64>(), size_of::<usize>());
+    }
 }
