f2c99745586db3e87aaa84e34787cfbcfb03de96
Sam Kim <skim13@cs.stanford.edu>
2022-03-18 17:29:17 -0400

    zk-token-sdk: cargo fmt

--- zk-token-sdk/src/lib.rs
+++ zk-token-sdk/src/lib.rs
@@ -32,8 +32,8 @@ mod sigma_proofs;
 mod transcript;
 
 // TODO: re-organize visibility
+pub mod curve25519;
 pub mod instruction;
 pub mod zk_token_elgamal;
 pub mod zk_token_proof_instruction;
 pub mod zk_token_proof_program;
-pub mod curve25519;
