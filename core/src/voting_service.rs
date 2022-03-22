ce467da3999d7b99046a1f595372c8d099884f27
Justin Starry <justin@solana.com>
2022-03-22 13:15:03 +0800

    Manually serialize vote tx before sending to TPU

--- core/src/voting_service.rs
+++ core/src/voting_service.rs
@@ -90,7 +90,8 @@ impl VotingService {
 
         let mut measure = Measure::start("vote_tx_send-ms");
         let target_address = target_address.unwrap_or_else(|| cluster_info.my_contact_info().tpu);
-        let _ = get_connection(&target_address).send_transaction(vote_op.tx());
+        let wire_vote_tx = bincode::serialize(vote_op.tx()).expect("vote serialization failure");
+        let _ = get_connection(&target_address).send_wire_transaction(&wire_vote_tx);
         measure.stop();
         inc_new_counter_info!("vote_tx_send-ms", measure.as_ms() as usize);
 
