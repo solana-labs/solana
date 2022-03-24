90009f330b51afb1f8e3009d6851ccbac0090b65
HaoranYi <haoran.yi@gmail.com>
2022-03-24 08:20:56 -0500

    small refactor to shorten the lock on slot_under_contention hashset (#23891)
    * small refactor to shorten the lock on slot_under_contention hashset
    
    * adding comments
    
    * comments

--- runtime/src/accounts_db.rs
+++ runtime/src/accounts_db.rs
@@ -4853,22 +4853,14 @@ impl AccountsDb {
         slot: Slot,
         should_flush_f: Option<&mut impl FnMut(&Pubkey, &AccountSharedData) -> bool>,
     ) -> Option<FlushStats> {
-        let is_being_purged = {
-            let mut slots_under_contention = self
-                .remove_unrooted_slots_synchronization
-                .slots_under_contention
-                .lock()
-                .unwrap();
-            // If we're purging this slot, don't flush it here
-            if slots_under_contention.contains(&slot) {
-                true
-            } else {
-                slots_under_contention.insert(slot);
-                false
-            }
-        };
-
-        if !is_being_purged {
+        if self
+            .remove_unrooted_slots_synchronization
+            .slots_under_contention
+            .lock()
+            .unwrap()
+            .insert(slot)
+        {
+            // We have not see this slot, flush it.
             let flush_stats = self.accounts_cache.slot_cache(slot).map(|slot_cache| {
                 #[cfg(test)]
                 {
@@ -4898,6 +4890,7 @@ impl AccountsDb {
                 .notify_all();
             flush_stats
         } else {
+            // We have already seen this slot. It is already under flushing. Skip.
             None
         }
     }
