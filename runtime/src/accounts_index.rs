adc07809b5e5639026fb22946d98ce07c8342e60
Jeff Washington (jwash) <jeff.washington@solana.com>
2022-03-24 09:52:33 -0500

    Update runtime/src/accounts_index.rs
    Co-authored-by: Michael Vines <mvines@gmail.com>

--- runtime/src/accounts_index.rs
+++ runtime/src/accounts_index.rs
@@ -869,7 +869,7 @@ pub struct AccountsIndex<T: IndexValue> {
     program_id_index: SecondaryIndex<DashMapSecondaryIndexEntry>,
     spl_token_mint_index: SecondaryIndex<DashMapSecondaryIndexEntry>,
     spl_token_owner_index: SecondaryIndex<RwLockSecondaryIndexEntry>,
-    pub roots_tracker: RwLock<RootsTracker>,
+    pub(crate) roots_tracker: RwLock<RootsTracker>,
     ongoing_scan_roots: RwLock<BTreeMap<Slot, u64>>,
     // Each scan has some latest slot `S` that is the tip of the fork the scan
     // is iterating over. The unique id of that slot `S` is recorded here (note we don't use
