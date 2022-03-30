dff5adffc2802aeddae8a84cc33b25c3995ded08
Dmitri Makarov <dmakarov@alumni.stanford.edu>
2022-03-30 10:19:04 -0700

    Bump sbf-tools version to v1.24
    (cherry picked from commit 689064a4f4d0a7122c9a24bef061712548663600)

--- sdk/bpf/scripts/install.sh
+++ sdk/bpf/scripts/install.sh
@@ -102,7 +102,7 @@ if [[ ! -e criterion-$version.md || ! -e criterion ]]; then
 fi
 
 # Install Rust-BPF
-version=v1.23
+version=v1.24
 if [[ ! -e bpf-tools-$version.md || ! -e bpf-tools ]]; then
   (
     set -e
