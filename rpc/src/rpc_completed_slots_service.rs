51b37f0184b15c02306d3047770d00755ab9c764
HaoranYi <haoran.yi@gmail.com>
2022-03-31 16:44:23 -0500

    Modify rpc_completed_slot_service to be non-blocking (#24007)
    * timeout for validator exits
    
    * clippy
    
    * print backtrace when panic
    
    * add backtrace package
    
    * increase time out to 30s
    
    * debug logging
    
    * make rpc complete service non blocking
    
    * reduce log level
    
    * remove logging
    
    * recv_timeout
    
    * remove backtrace
    
    * remove sleep
    
    * remove unused variable
    
    * add comments
    
    * Update core/src/validator.rs
    
    Co-authored-by: Trent Nelson <trent.a.b.nelson@gmail.com>
    
    * Update core/src/validator.rs
    
    Co-authored-by: Trent Nelson <trent.a.b.nelson@gmail.com>
    
    * whitespace
    
    * more whitespace
    
    * fix build
    
    Co-authored-by: Trent Nelson <trent.a.b.nelson@gmail.com>

--- rpc/src/rpc_completed_slots_service.rs
+++ rpc/src/rpc_completed_slots_service.rs
@@ -4,21 +4,35 @@ use {
     solana_ledger::blockstore::CompletedSlotsReceiver,
     solana_sdk::timing::timestamp,
     std::{
-        sync::Arc,
+        sync::{
+            atomic::{AtomicBool, Ordering},
+            Arc,
+        },
         thread::{Builder, JoinHandle},
+        time::Duration,
     },
 };
+
+pub const COMPLETE_SLOT_REPORT_SLEEP_MS: u64 = 100;
 
 pub struct RpcCompletedSlotsService;
 impl RpcCompletedSlotsService {
     pub fn spawn(
         completed_slots_receiver: CompletedSlotsReceiver,
         rpc_subscriptions: Arc<RpcSubscriptions>,
+        exit: Arc<AtomicBool>,
     ) -> JoinHandle<()> {
         Builder::new()
             .name("solana-rpc-completed-slots-service".to_string())
-            .spawn(move || {
-                for slots in completed_slots_receiver.iter() {
+            .spawn(move || loop {
+                // shutdown the service
+                if exit.load(Ordering::Relaxed) {
+                    break;
+                }
+
+                if let Ok(slots) = completed_slots_receiver
+                    .recv_timeout(Duration::from_millis(COMPLETE_SLOT_REPORT_SLEEP_MS))
+                {
                     for slot in slots {
                         rpc_subscriptions.notify_slot_update(SlotUpdate::Completed {
                             slot,
