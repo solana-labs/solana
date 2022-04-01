543bdf7ee5d26b706d69b228275b4f9ecd526c97
Alexander Meißner <AlexanderMeissner@gmx.net>
2022-04-01 18:53:26 +0200

    Migrates test_behavior_withdrawal_then_redelegate_with_less_than_minimum_stake_delegation.

--- programs/stake/src/stake_instruction.rs
+++ programs/stake/src/stake_instruction.rs
@@ -4025,66 +4025,170 @@ mod tests {
     fn test_behavior_withdrawal_then_redelegate_with_less_than_minimum_stake_delegation() {
         let rent = Rent::default();
         let rent_exempt_reserve = rent.minimum_balance(std::mem::size_of::<StakeState>());
-        let stake_pubkey = Pubkey::new_unique();
-        let signers = HashSet::from([stake_pubkey]);
-        let stake_account = AccountSharedData::new_ref(
+        let stake_address = solana_sdk::pubkey::new_rand();
+        let stake_account = AccountSharedData::new(
             rent_exempt_reserve + MINIMUM_STAKE_DELEGATION,
             std::mem::size_of::<StakeState>(),
             &id(),
         );
-        let stake_keyed_account = KeyedAccount::new(&stake_pubkey, true, &stake_account);
-        stake_keyed_account
-            .initialize(&Authorized::auto(&stake_pubkey), &Lockup::default(), &rent)
-            .unwrap();
-
-        let vote_pubkey = Pubkey::new_unique();
-        let vote_account = RefCell::new(vote_state::create_account(
-            &vote_pubkey,
-            &Pubkey::new_unique(),
-            0,
-            100,
-        ));
-        let vote_keyed_account = KeyedAccount::new(&vote_pubkey, false, &vote_account);
+        let vote_address = solana_sdk::pubkey::new_rand();
+        let vote_account =
+            vote_state::create_account(&vote_address, &solana_sdk::pubkey::new_rand(), 0, 100);
+        let recipient_address = solana_sdk::pubkey::new_rand();
         let mut clock = Clock::default();
-        stake_keyed_account
-            .delegate(
-                &vote_keyed_account,
-                &clock,
-                &StakeHistory::default(),
-                &Config::default(),
-                &signers,
-            )
-            .unwrap();
+        let mut transaction_accounts = vec![
+            (stake_address, stake_account),
+            (vote_address, vote_account),
+            (
+                recipient_address,
+                AccountSharedData::new(rent_exempt_reserve, 0, &system_program::id()),
+            ),
+            (
+                sysvar::clock::id(),
+                account::create_account_shared_data_for_test(&clock),
+            ),
+            (
+                sysvar::stake_history::id(),
+                account::create_account_shared_data_for_test(&StakeHistory::default()),
+            ),
+            (
+                stake_config::id(),
+                config::create_account(0, &stake_config::Config::default()),
+            ),
+            (
+                sysvar::rent::id(),
+                account::create_account_shared_data_for_test(&rent),
+            ),
+        ];
+        let instruction_accounts = vec![
+            AccountMeta {
+                pubkey: stake_address,
+                is_signer: true,
+                is_writable: false,
+            },
+            AccountMeta {
+                pubkey: vote_address,
+                is_signer: false,
+                is_writable: false,
+            },
+            AccountMeta {
+                pubkey: sysvar::clock::id(),
+                is_signer: false,
+                is_writable: false,
+            },
+            AccountMeta {
+                pubkey: sysvar::stake_history::id(),
+                is_signer: false,
+                is_writable: false,
+            },
+            AccountMeta {
+                pubkey: stake_config::id(),
+                is_signer: false,
+                is_writable: false,
+            },
+        ];
+
+        let accounts = process_instruction(
+            &serialize(&StakeInstruction::Initialize(
+                Authorized::auto(&stake_address),
+                Lockup::default(),
+            ))
+            .unwrap(),
+            transaction_accounts.clone(),
+            vec![
+                AccountMeta {
+                    pubkey: stake_address,
+                    is_signer: true,
+                    is_writable: false,
+                },
+                AccountMeta {
+                    pubkey: sysvar::rent::id(),
+                    is_signer: false,
+                    is_writable: false,
+                },
+            ],
+            Ok(()),
+        );
+        transaction_accounts[0] = (stake_address, accounts[0].clone());
+
+        let accounts = process_instruction(
+            &serialize(&StakeInstruction::DelegateStake).unwrap(),
+            transaction_accounts.clone(),
+            instruction_accounts.clone(),
+            Ok(()),
+        );
+        transaction_accounts[0] = (stake_address, accounts[0].clone());
+        transaction_accounts[1] = (vote_address, accounts[1].clone());
 
         clock.epoch += 1;
-        stake_keyed_account.deactivate(&clock, &signers).unwrap();
+        transaction_accounts[3] = (
+            sysvar::clock::id(),
+            account::create_account_shared_data_for_test(&clock),
+        );
+        let accounts = process_instruction(
+            &serialize(&StakeInstruction::Deactivate).unwrap(),
+            transaction_accounts.clone(),
+            vec![
+                AccountMeta {
+                    pubkey: stake_address,
+                    is_signer: true,
+                    is_writable: false,
+                },
+                AccountMeta {
+                    pubkey: sysvar::clock::id(),
+                    is_signer: false,
+                    is_writable: false,
+                },
+            ],
+            Ok(()),
+        );
+        transaction_accounts[0] = (stake_address, accounts[0].clone());
 
         clock.epoch += 1;
-        let withdraw_amount = stake_keyed_account.lamports().unwrap()
-            - (rent_exempt_reserve + MINIMUM_STAKE_DELEGATION - 1);
-        let withdraw_pubkey = Pubkey::new_unique();
-        let withdraw_account =
-            AccountSharedData::new_ref(rent_exempt_reserve, 0, &system_program::id());
-        let withdraw_keyed_account = KeyedAccount::new(&withdraw_pubkey, false, &withdraw_account);
-        stake_keyed_account
-            .withdraw(
-                withdraw_amount,
-                &withdraw_keyed_account,
-                &clock,
-                &StakeHistory::default(),
-                &stake_keyed_account,
-                None,
-            )
-            .unwrap();
+        transaction_accounts[3] = (
+            sysvar::clock::id(),
+            account::create_account_shared_data_for_test(&clock),
+        );
+        let withdraw_amount =
+            accounts[0].lamports() - (rent_exempt_reserve + MINIMUM_STAKE_DELEGATION - 1);
+        process_instruction(
+            &serialize(&StakeInstruction::Withdraw(withdraw_amount)).unwrap(),
+            transaction_accounts.clone(),
+            vec![
+                AccountMeta {
+                    pubkey: stake_address,
+                    is_signer: false,
+                    is_writable: false,
+                },
+                AccountMeta {
+                    pubkey: recipient_address,
+                    is_signer: false,
+                    is_writable: false,
+                },
+                AccountMeta {
+                    pubkey: sysvar::clock::id(),
+                    is_signer: false,
+                    is_writable: false,
+                },
+                AccountMeta {
+                    pubkey: sysvar::stake_history::id(),
+                    is_signer: false,
+                    is_writable: false,
+                },
+                AccountMeta {
+                    pubkey: stake_address,
+                    is_signer: true,
+                    is_writable: false,
+                },
+            ],
+            Ok(()),
+        );
 
-        assert!(stake_keyed_account
-            .delegate(
-                &vote_keyed_account,
-                &clock,
-                &StakeHistory::default(),
-                &Config::default(),
-                &signers,
-            )
-            .is_ok());
+        process_instruction(
+            &serialize(&StakeInstruction::DelegateStake).unwrap(),
+            transaction_accounts,
+            instruction_accounts,
+            Ok(()),
+        );
     }
 }
