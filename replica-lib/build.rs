7d8250f148d3a771192e2da530fab0fce3e312b0
Tyera Eulberg <tyera@solana.com>
2022-04-06 13:37:21 -0600

    Remove replica-node crates

--- replica-lib/build.rs
+++
@@ -1,5 +0,0 @@
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    // compiling protos using path on build time
-    tonic_build::configure().compile(&["proto/accountsdb_repl.proto"], &["proto"])?;
-    Ok(())
-}
