6d3316f3612b0d2a435fe476ed73ee857003b26e
HaoranYi <haoran.yi@gmail.com>
2022-04-07 08:58:06 -0500

    Update core/src/tvu.rs
    Co-authored-by: Trent Nelson <trent.a.b.nelson@gmail.com>

--- core/src/tvu.rs
+++ core/src/tvu.rs
@@ -372,7 +372,7 @@ impl Tvu {
             sender.send(()).unwrap();
         });
 
-        // timeout of 10s for closing tvu
+        // exit can deadlock. put an upper-bound on how long we wait for it
         let timeout = Duration::from_secs(TVU_THREADS_JOIN_TIMEOUT_SECONDS);
         if let Err(RecvTimeoutError::Timeout) = receiver.recv_timeout(timeout) {
             error!("timeout for closing tvu");
