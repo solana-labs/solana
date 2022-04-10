29ca21ed786a5fc969fa15831ca53df521a8739b
Tao Zhu <tao@solana.com>
2022-04-09 23:07:18 -0500

    undo transaction cost from cost_tracker if it was not executed successfully

--- core/src/banking_stage.rs
+++ core/src/banking_stage.rs
@@ -117,6 +117,8 @@ pub struct ExecuteAndCommitTransactionsOutput {
     // how many such transactions were committed
     commit_transactions_result: Result<(), PohRecorderError>,
     execute_and_commit_timings: LeaderExecuteAndCommitTimings,
+    // Transaction execution detail results
+    execution_results: Vec<TransactionExecutionResult>,
 }
 
 #[derive(Debug, Default)]
@@ -1247,6 +1249,7 @@ impl BankingStage {
                 retryable_transaction_indexes,
                 commit_transactions_result: Err(e),
                 execute_and_commit_timings,
+                execution_results,
             };
         }
 
@@ -1261,7 +1264,7 @@ impl BankingStage {
                     bank.commit_transactions(
                         sanitized_txs,
                         &mut loaded_transactions,
-                        execution_results,
+                        execution_results.clone(),
                         executed_transactions_count as u64,
                         executed_transactions_count
                             .saturating_sub(executed_with_successful_result_count)
@@ -1333,6 +1336,7 @@ impl BankingStage {
             retryable_transaction_indexes,
             commit_transactions_result: Ok(()),
             execute_and_commit_timings,
+            execution_results,
         }
     }
 
@@ -1389,6 +1393,7 @@ impl BankingStage {
         let ExecuteAndCommitTransactionsOutput {
             ref mut retryable_transaction_indexes,
             ref execute_and_commit_timings,
+            ref execution_results,
             ..
         } = execute_and_commit_transactions_output;
 
@@ -1398,7 +1403,7 @@ impl BankingStage {
         QosService::update_or_remove_transaction_costs(
             transaction_costs.iter(),
             transactions_qos_results.iter(),
-            retryable_transaction_indexes,
+            execution_results.iter(),
             bank,
         );
 
