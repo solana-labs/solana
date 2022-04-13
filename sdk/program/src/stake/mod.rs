3e22ae12efe782456b2ed9138257b225802e5d31
Brooks Prumo <brooks@solana.com>
2022-04-13 17:16:10 -0500

    Deprecate `MINIMUM_STAKE_DELEGATION`

--- sdk/program/src/stake/mod.rs
+++ sdk/program/src/stake/mod.rs
@@ -3,13 +3,12 @@ pub mod instruction;
 pub mod state;
 pub mod tools;
 
+mod deprecated;
+pub use deprecated::*;
+
 pub mod program {
     crate::declare_id!("Stake11111111111111111111111111111111111111");
 }
-
-// NOTE: This constant will be deprecated soon; if possible, use
-// `solana_stake_program::get_minimum_delegation()` instead.
-pub const MINIMUM_STAKE_DELEGATION: u64 = 1;
 
 /// The minimum number of epochs before stake account that is delegated to a delinquent vote
 /// account may be unstaked with `StakeInstruction::DeactivateDelinquent`
