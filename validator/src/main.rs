8c6cf723600704ffcc97028f0d2b09ce97105114
Tyera Eulberg <tyera@solana.com>
2022-04-13 14:17:10 -0400

    Support quic in bench-tps (#24295)
    * Update comment
    
    * Use connection_cache in tpu_client
    
    * Add --tpu-use-quic to bench-tps
    
    * Use connection_cache async send
    
    (cherry picked from commit 26899359d196e33992f34d83138872c3a9154ab9)

--- validator/src/main.rs
+++ validator/src/main.rs
@@ -1149,7 +1149,7 @@ pub fn main() {
             Arg::with_name("tpu_use_quic")
                 .long("tpu-use-quic")
                 .takes_value(false)
-                .help("When this is set to true, the system will use QUIC to send transactions."),
+                .help("Use QUIC to send transactions."),
         )
         .arg(
             Arg::with_name("rocksdb_max_compaction_jitter")
