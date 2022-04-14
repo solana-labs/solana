59525edf4d25821c25dc39d3c6834225ae6791bd
Brian Anderson <andersrb@gmail.com>
2022-04-14 15:15:38 -0500

    Update sdk/program/src/program_memory.rs
    Co-authored-by: Tyera Eulberg <teulberg@gmail.com>

--- sdk/program/src/program_memory.rs
+++ sdk/program/src/program_memory.rs
@@ -135,7 +135,7 @@ pub fn sol_memcmp(s1: &[u8], s2: &[u8], n: usize) -> i32 {
 /// # Errors
 ///
 /// When executed within a BPF program, the memory region spanning `n` bytes
-/// from from the start of `s` must be mapped program memory. If not the program
+/// from from the start of `s` must be mapped program memory. If not, the program
 /// will abort.
 ///
 /// # Safety
