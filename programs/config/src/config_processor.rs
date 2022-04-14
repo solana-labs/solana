3c0c9cf5f559fd8b81d4d8e41c2dc95823278cc3
Alexander Meißner <AlexanderMeissner@gmx.net>
2022-04-14 19:43:37 +0200

    Replaces KeyedAccount by BorrowedAccount in config.

--- programs/config/src/config_processor.rs
+++ programs/config/src/config_processor.rs
@@ -25,17 +25,20 @@ pub fn process_instruction(
     let keyed_accounts = invoke_context.get_keyed_accounts()?;
 
     let key_list: ConfigKeys = limited_deserialize(data)?;
-    let config_keyed_account =
-        &mut keyed_account_at_index(keyed_accounts, first_instruction_account)?;
-    let config_account_key = *config_keyed_account.unsigned_key();
-    let is_config_account_signer = config_keyed_account.signer_key().is_some();
+    let config_account_key =
+        transaction_context
+            .get_key_of_account_at_index(instruction_context.get_index_in_transaction(
+                instruction_context.get_number_of_program_accounts(),
+            )?)?;
+    let config_account =
+        instruction_context.try_borrow_instruction_account(transaction_context, 0)?;
+    let is_config_account_signer = config_account.is_signer();
     let current_data: ConfigKeys = {
-        let config_account = config_keyed_account.try_account_ref_mut()?;
-        if config_account.owner() != &crate::id() {
+        if config_account.get_owner() != &crate::id() {
             return Err(InstructionError::InvalidAccountOwner);
         }
 
-        deserialize(config_account.data()).map_err(|err| {
+        deserialize(config_account.get_data()).map_err(|err| {
             ic_msg!(
                 invoke_context,
                 "Unable to deserialize config account: {}",
@@ -44,7 +47,7 @@ pub fn process_instruction(
             InstructionError::InvalidAccountData
         })?
     };
-    drop(config_keyed_account);
+    drop(config_account);
 
     let current_signer_keys: Vec<Pubkey> = current_data
         .keys
@@ -63,9 +66,10 @@ pub fn process_instruction(
     let mut counter = 0;
     for (signer, _) in key_list.keys.iter().filter(|(_, is_signer)| *is_signer) {
         counter += 1;
-        if signer != &config_account_key {
-            let signer_account =
-                keyed_account_at_index(keyed_accounts, counter + 1).map_err(|_| {
+        if signer != config_account_key {
+            let signer_account = instruction_context
+                .try_borrow_instruction_account(transaction_context, counter)
+                .map_err(|_| {
                     ic_msg!(
                         invoke_context,
                         "account {:?} is not in account list",
@@ -73,8 +77,7 @@ pub fn process_instruction(
                     );
                     InstructionError::MissingRequiredSignature
                 })?;
-            let signer_key = signer_account.signer_key();
-            if signer_key.is_none() {
+            if !signer_account.is_signer() {
                 ic_msg!(
                     invoke_context,
                     "account {:?} signer_key().is_none()",
@@ -82,7 +85,7 @@ pub fn process_instruction(
                 );
                 return Err(InstructionError::MissingRequiredSignature);
             }
-            if signer_key.unwrap() != signer {
+            if signer_account.get_key() != signer {
                 ic_msg!(
                     invoke_context,
                     "account[{:?}].signer_key() does not match Config data)",
@@ -130,16 +133,13 @@ pub fn process_instruction(
         return Err(InstructionError::MissingRequiredSignature);
     }
 
-    let config_keyed_account =
-        &mut keyed_account_at_index(keyed_accounts, first_instruction_account)?;
-    if config_keyed_account.data_len()? < data.len() {
+    let mut config_account =
+        instruction_context.try_borrow_instruction_account(transaction_context, 0)?;
+    if config_account.get_data().len() < data.len() {
         ic_msg!(invoke_context, "instruction data too large");
         return Err(InstructionError::InvalidInstructionData);
     }
-    config_keyed_account
-        .try_account_ref_mut()?
-        .data_as_mut_slice()[..data.len()]
-        .copy_from_slice(data);
+    config_account.get_data_mut()[..data.len()].copy_from_slice(data);
     Ok(())
 }
 
