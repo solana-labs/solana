d0713ba24a851019be24dc3dda0127d82b9b5fe3
Justin Starry <justin@solana.com>
2022-04-19 18:36:44 +0800

    fix conflict

--- runtime/src/bank.rs
+++ runtime/src/bank.rs
@@ -15932,6 +15932,7 @@ pub(crate) mod tests {
 
         fn mock_ix_processor(
             _first_instruction_account: usize,
+            _data: &[u8],
             invoke_context: &mut InvokeContext,
         ) -> std::result::Result<(), InstructionError> {
             let compute_budget = invoke_context.get_compute_budget();
