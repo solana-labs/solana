0e5e37e0f8cbd2097988c0241bdd87b677720fd8
Michael Vines <mvines@gmail.com>
2022-04-19 15:06:30 -0700

    Remove halt_at_slot from RuntimeConfig, it's not a runtime concern

--- runtime/src/runtime_config.rs
+++ runtime/src/runtime_config.rs
@@ -1,9 +1,8 @@
-use {solana_program_runtime::compute_budget::ComputeBudget, solana_sdk::clock::Slot};
+use solana_program_runtime::compute_budget::ComputeBudget;
 
 /// Encapsulates flags that can be used to tweak the runtime behavior.
 #[derive(Default, Clone)]
 pub struct RuntimeConfig {
     pub bpf_jit: bool,
-    pub dev_halt_at_slot: Option<Slot>,
     pub compute_budget: Option<ComputeBudget>,
 }
