c477685ed57048413b85ed68cff4698860c67e15
Jeff Washington (jwash) <wash678@gmail.com>
2022-04-20 16:13:09 -0500

    90

--- runtime/src/accounts_db.rs
+++ runtime/src/accounts_db.rs
@@ -3083,9 +3083,10 @@ impl AccountsDb {
         use solana_sdk::clock::DEFAULT_SLOTS_PER_EPOCH;
         /*
         60 155875 fails
-        80
+        80 77396 fails
+        90
         */
-        let epoch_width = DEFAULT_SLOTS_PER_EPOCH * 80 / 100; // put some 'in-this-epoch' slots into an ancient append vec
+        let epoch_width = DEFAULT_SLOTS_PER_EPOCH * 90 / 100; // put some 'in-this-epoch' slots into an ancient append vec
         let old_root = max_root.saturating_sub(epoch_width + 1000);
 
         let mut m = Measure::start("get slots");
