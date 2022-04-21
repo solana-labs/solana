e24aafc5affdb081f30b1f922ac457e34e96f07d
Jeff Washington (jwash) <wash678@gmail.com>
2022-04-21 12:21:06 -0500

    c

--- runtime/src/accounts_db.rs
+++ runtime/src/accounts_db.rs
@@ -3112,7 +3112,7 @@ impl AccountsDb {
 99635 succeed
 99633 
 99632 passed
-99631 
+99631 passed
 99630
 9963 failed, succeeded, failed
 9962 failed
@@ -3123,7 +3123,7 @@ impl AccountsDb {
 9951 failed
 9950 failed
         */
-        let epoch_width = DEFAULT_SLOTS_PER_EPOCH * 99631 / 100000; // put some 'in-this-epoch' slots into an ancient append vec
+        let epoch_width = DEFAULT_SLOTS_PER_EPOCH * 99630 / 100000; // put some 'in-this-epoch' slots into an ancient append vec
         let old_root = max_root.saturating_sub(epoch_width + 1000);// + 16096 + 6096 -1000);
 
         let mut m = Measure::start("get slots");
