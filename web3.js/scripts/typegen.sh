2a3c0c9e81686ac86c6e6c390fc63bef766559a6
steveluscher <me+github@steveluscher.com>
2022-04-21 14:32:04 -0700

    chore: yarn type:gen now works on all platforms without hacks

--- web3.js/scripts/typegen.sh
+++ web3.js/scripts/typegen.sh
@@ -7,16 +7,13 @@ npx tsc -p tsconfig.d.json -d
 npx rollup -c rollup.config.types.js
 
 # Replace export with closing brace for module declaration
-sed -i.bak '$s/export {.*};/}/' lib/index.d.ts
+sed -i='' '$s/export {.*};/}/' lib/index.d.ts
 
 # Replace declare's with export's
-sed -i.bak 's/declare/export/g' lib/index.d.ts
+sed -i='' 's/declare/export/g' lib/index.d.ts
 
 # Prepend declare module line
-sed -i.bak '2s;^;declare module "@solana/web3.js" {\n;' lib/index.d.ts
-
-# Remove backup file from `sed` above
-rm lib/index.d.ts.bak
+sed -i='' '2s;^;declare module "@solana/web3.js" {\n;' lib/index.d.ts
 
 # Run prettier
 npx prettier --write lib/index.d.ts
