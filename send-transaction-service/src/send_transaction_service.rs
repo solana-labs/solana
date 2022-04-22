d9c0cc5af2744d634206cb0d5e309670fcd93309
Lijun Wang <83639177+lijunwangs@users.noreply.github.com>
2022-04-22 01:56:49 -0700

    Initial development for leader info refresher

--- send-transaction-service/src/send_transaction_service.rs
+++ send-transaction-service/src/send_transaction_service.rs
@@ -128,6 +128,48 @@ impl Default for Config {
     }
 }
 
+/// A struct responsible for providing the up-to-date leader information
+/// used for sending transactions.
+pub const LEADER_INFO_REFRESH_RATE_IN_MS: u64 = 1000;
+
+pub struct LeaderInfoProvider<T: TpuInfo + std::marker::Send + Clone + 'static> {
+    /// The last time the leader info is refreshed
+    last_leader_refresh: Option<Instant>,
+
+    /// The leader info
+    leader_info: Option<T>,
+
+    /// Refresh the leader info every duration set by refresh_rate
+    refresh_rate: Duration,
+}
+
+impl<T: TpuInfo + std::marker::Send + Clone + 'static> LeaderInfoProvider<T> {
+    /// Get the leader info, refresh if expired
+    pub fn get_leader_info(&mut self) -> &Option<T> {
+        if let Some(leader_info) = self.leader_info.as_mut() {
+            let now = Instant::now();
+            let expired = self
+                .last_leader_refresh
+                .map(|last| now.duration_since(last) >= self.refresh_rate)
+                .unwrap_or(false);
+
+            if expired {
+                leader_info.refresh_recent_peers();
+                self.last_leader_refresh = Some(now);
+            }
+        }
+        &self.leader_info
+    }
+
+    pub fn new(leader_info: Option<T>) -> Self {
+        Self {
+            last_leader_refresh: None,
+            leader_info,
+            refresh_rate: Duration::from_millis(LEADER_INFO_REFRESH_RATE_IN_MS),
+        }
+    }
+}
+
 impl SendTransactionService {
     pub fn new<T: TpuInfo + std::marker::Send + Clone + 'static>(
         tpu_address: SocketAddr,
@@ -155,11 +197,14 @@ impl SendTransactionService {
         config: Config,
     ) -> Self {
         let retry_transactions = Arc::new(Mutex::new(HashMap::new()));
+
+        let leader_info_provider = Arc::new(Mutex::new(LeaderInfoProvider::new(leader_info)));
+
         let exit = Arc::new(AtomicBool::new(false));
         let receive_txn_thread = Self::receive_txn_thread(
             tpu_address,
             receiver,
-            leader_info.clone(),
+            leader_info_provider.clone(),
             config.clone(),
             retry_transactions.clone(),
             exit.clone(),
@@ -168,7 +213,7 @@ impl SendTransactionService {
         let retry_thread = Self::retry_thread(
             tpu_address,
             bank_forks.clone(),
-            leader_info,
+            leader_info_provider,
             config,
             retry_transactions,
             exit.clone(),
@@ -181,25 +226,21 @@ impl SendTransactionService {
     }
 
     /// Thread responsible for receiving transactions from RPC clients.
-    fn receive_txn_thread<T: TpuInfo + std::marker::Send + 'static>(
+    fn receive_txn_thread<T: TpuInfo + std::marker::Send + Clone + 'static>(
         tpu_address: SocketAddr,
         receiver: Receiver<TransactionInfo>,
-        mut leader_info: Option<T>,
+        leader_info: Arc<Mutex<LeaderInfoProvider<T>>>,
         config: Config,
         retry_transactions: Arc<Mutex<HashMap<Signature, TransactionInfo>>>,
         exit: Arc<AtomicBool>,
     ) -> JoinHandle<()> {
         let mut last_batch_sent = Instant::now();
-        let mut last_leader_refresh = Instant::now();
         let mut transactions = HashMap::new();
 
         info!(
             "Starting send-transaction-service::receive_txn_thread with config {:?}",
             config
         );
-        if let Some(leader_info) = leader_info.as_mut() {
-            leader_info.refresh_recent_peers();
-        }
         connection_cache::set_use_quic(config.use_quic);
         Builder::new()
             .name("send-tx-receive".to_string())
@@ -243,7 +284,7 @@ impl SendTransactionService {
                     let _result = Self::send_transactions_in_batch(
                         &tpu_address,
                         &mut transactions,
-                        &leader_info,
+                        leader_info.lock().unwrap().get_leader_info(),
                         &config,
                     );
                     let last_sent_time = Instant::now();
@@ -266,35 +307,24 @@ impl SendTransactionService {
                     }
 
                     last_batch_sent = Instant::now();
-                    if last_leader_refresh.elapsed().as_millis() > 1000 {
-                        if let Some(leader_info) = leader_info.as_mut() {
-                            leader_info.refresh_recent_peers();
-                        }
-                        last_leader_refresh = Instant::now();
-                    }
                 }
             })
             .unwrap()
     }
 
     /// Thread responsible for retrying transactions
-    fn retry_thread<T: TpuInfo + std::marker::Send + 'static>(
+    fn retry_thread<T: TpuInfo + std::marker::Send + Clone + 'static>(
         tpu_address: SocketAddr,
         bank_forks: Arc<RwLock<BankForks>>,
-        mut leader_info: Option<T>,
+        leader_info: Arc<Mutex<LeaderInfoProvider<T>>>,
         config: Config,
         retry_transactions: Arc<Mutex<HashMap<Signature, TransactionInfo>>>,
         exit: Arc<AtomicBool>,
     ) -> JoinHandle<()> {
-        let mut last_leader_refresh = Instant::now();
-
         info!(
             "Starting send-transaction-service::retry_thread with config {:?}",
             config
         );
-        if let Some(leader_info) = leader_info.as_mut() {
-            leader_info.refresh_recent_peers();
-        }
         connection_cache::set_use_quic(config.use_quic);
         Builder::new()
             .name("send-tx-retry".to_string())
@@ -323,15 +353,9 @@ impl SendTransactionService {
                         &root_bank,
                         &tpu_address,
                         &mut transactions,
-                        &leader_info,
+                        leader_info.clone(),
                         &config,
                     );
-                }
-                if last_leader_refresh.elapsed().as_millis() > 1000 {
-                    if let Some(leader_info) = leader_info.as_mut() {
-                        leader_info.refresh_recent_peers();
-                    }
-                    last_leader_refresh = Instant::now();
                 }
             })
             .unwrap()
@@ -367,12 +391,12 @@ impl SendTransactionService {
     }
 
     /// Retry transactions sent before.
-    fn process_transactions<T: TpuInfo>(
+    fn process_transactions<T: TpuInfo + std::marker::Send + Clone + 'static>(
         working_bank: &Arc<Bank>,
         root_bank: &Arc<Bank>,
         tpu_address: &SocketAddr,
         transactions: &mut HashMap<Signature, TransactionInfo>,
-        leader_info: &Option<T>,
+        leader_info: Arc<Mutex<LeaderInfoProvider<T>>>,
         config: &Config,
     ) -> ProcessTransactionsResult {
         let mut result = ProcessTransactionsResult::default();
@@ -469,6 +493,8 @@ impl SendTransactionService {
 
         if !batched_transactions.is_empty() {
             // Processing the transactions in batch
+            let mut leader_info_provider = leader_info.lock().unwrap();
+            let leader_info = leader_info_provider.get_leader_info();
             let addresses = Self::get_tpu_addresses(tpu_address, leader_info, config);
 
             let wire_transactions = transactions
