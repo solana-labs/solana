fc3d6fb7e732f8f1863d506e6f48a4240e1ce122
Jeff Washington (jwash) <wash678@gmail.com>
2022-04-22 12:37:06 -0500

    log ancient_append_vecs

--- runtime/src/accounts_db.rs
+++ runtime/src/accounts_db.rs
@@ -5917,6 +5917,14 @@ failed:
             stats
                 .accounts_in_roots_older_than_epoch
                 .fetch_add(num_accounts, Ordering::Relaxed);
+                let ancients = sub_storages
+                        .iter()
+                        .map(|storage| if Self::is_ancient(&storage.accounts) { 1 } else { 0 })
+                        .sum();
+            stats
+                .ancient_append_vecs
+                .fetch_add(ancients, Ordering::Relaxed);
+
         }
     }
 
