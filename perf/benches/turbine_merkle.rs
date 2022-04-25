aba737dc2b99390a463bb9384f5e9303d42668bb
Jeff Biseda <jbiseda@gmail.com>
2022-04-25 15:50:29 -0700

    add parallel

--- perf/benches/turbine_merkle.rs
+++ perf/benches/turbine_merkle.rs
@@ -16,10 +16,8 @@ fn create_random_packets(npackets: usize) -> Vec<Vec<u8>> {
     packets
 }
 
-#[bench]
-fn bench_merkle_create_tree_64(b: &mut Bencher) {
-    let packets = create_random_packets(64);
-
+fn bench_merkle_create_tree_from_leaves(b: &mut Bencher, pkt_count: usize) {
+    let packets = create_random_packets(pkt_count);
     b.iter(|| {
         let leaves: Vec<TurbineMerkleHash> = packets
             .iter()
@@ -30,30 +28,18 @@ fn bench_merkle_create_tree_64(b: &mut Bencher) {
 }
 
 #[bench]
-fn bench_merkle_create_tree_96(b: &mut Bencher) {
-    let packets = create_random_packets(96);
-
-    b.iter(|| {
-        let mut leaves: Vec<TurbineMerkleHash> = packets
-            .iter()
-            .map(|p| TurbineMerkleHash::hash(&[p]))
-            .collect();
-        leaves.extend(std::iter::repeat(TurbineMerkleHash::default()).take(32));
-        let _tree = TurbineMerkleTree::new_from_leaves(&leaves);
-    });
+fn bench_merkle_create_tree_from_leaves_64(b: &mut Bencher) {
+    bench_merkle_create_tree_from_leaves(b, 64);
 }
 
 #[bench]
-fn bench_merkle_create_tree_128(b: &mut Bencher) {
-    let packets = create_random_packets(128);
+fn bench_merkle_create_tree_from_leaves_96(b: &mut Bencher) {
+    bench_merkle_create_tree_from_leaves(b, 64);
+}
 
-    b.iter(|| {
-        let leaves: Vec<TurbineMerkleHash> = packets
-            .iter()
-            .map(|p| TurbineMerkleHash::hash(&[p]))
-            .collect();
-        let _tree = TurbineMerkleTree::new_from_leaves(&leaves);
-    });
+#[bench]
+fn bench_merkle_create_tree_from_leaves_128(b: &mut Bencher) {
+    bench_merkle_create_tree_from_leaves(b, 64);
 }
 
 #[bench]
@@ -122,4 +108,63 @@ fn bench_merkle_verify_proofs_96_batch(b: &mut Bencher) {
             .map(|i| proofs[i].verify(&tree.root(), &tree.node(i), i))
             .collect();
     });
+}
+
+fn bench_merkle_create_tree_from_bufs(b: &mut Bencher, pkt_count: usize) {
+    let packets = create_random_packets(pkt_count);
+    b.iter(|| {
+        let _tree = TurbineMerkleTree::new_from_bufs(&packets);
+    });
+}
+
+fn bench_merkle_create_tree_from_bufs_par(b: &mut Bencher, pkt_count: usize, chunk: usize) {
+    let packets = create_random_packets(pkt_count);
+    b.iter(|| {
+        let _tree = TurbineMerkleTree::new_from_bufs_par(&packets, chunk);
+    });
+}
+
+#[bench]
+fn bench_merkle_create_tree_from_bufs_64(b: &mut Bencher) {
+    bench_merkle_create_tree_from_bufs(b, 64);
+}
+
+#[bench]
+fn bench_merkle_create_tree_from_bufs_128(b: &mut Bencher) {
+    bench_merkle_create_tree_from_bufs(b, 128);
+}
+
+#[bench]
+fn bench_merkle_create_tree_from_bufs_par_128_64(b: &mut Bencher) {
+    bench_merkle_create_tree_from_bufs_par(b, 128, 64);
+}
+
+#[bench]
+fn bench_merkle_create_tree_from_bufs_par_128_32(b: &mut Bencher) {
+    bench_merkle_create_tree_from_bufs_par(b, 128, 32);
+}
+
+#[bench]
+fn bench_merkle_create_tree_from_bufs_par_128_16(b: &mut Bencher) {
+    bench_merkle_create_tree_from_bufs_par(b, 128, 16);
+}
+
+#[bench]
+fn bench_merkle_create_tree_from_bufs_par_128_8(b: &mut Bencher) {
+    bench_merkle_create_tree_from_bufs_par(b, 128, 8);
+}
+
+#[bench]
+fn bench_merkle_create_tree_from_bufs_par_64_32(b: &mut Bencher) {
+    bench_merkle_create_tree_from_bufs_par(b, 64, 32);
+}
+
+#[bench]
+fn bench_merkle_create_tree_from_bufs_par_64_16(b: &mut Bencher) {
+    bench_merkle_create_tree_from_bufs_par(b, 64, 16);
+}
+
+#[bench]
+fn bench_merkle_create_tree_from_bufs_par_64_8(b: &mut Bencher) {
+    bench_merkle_create_tree_from_bufs_par(b, 64, 8);
 }
--- perf/src/turbine_merkle.rs
+++ perf/src/turbine_merkle.rs
@@ -1,4 +1,5 @@
 use {
+    rayon::{iter::ParallelIterator, prelude::*},
     serde::{Deserialize, Serialize},
     sha2::{Digest, Sha256},
 };
@@ -87,7 +88,7 @@ impl From<&[u8]> for TurbineMerkleProof {
 impl TurbineMerkleTree {
     #[allow(clippy::integer_arithmetic)]
     pub fn new_from_leaves(leaves: &[TurbineMerkleHash]) -> Self {
-        // TODO assert leaves.len() is a power of 2?
+        // TODO assert leaves.len() is a power of 2
         let tree_size = leaves.len() * 2 - 1;
         let mut tree = Vec::with_capacity(tree_size);
 
@@ -109,10 +110,75 @@ impl TurbineMerkleTree {
         Self { tree }
     }
 
-    pub fn new_from_bufs(bufs: &[&[u8]]) -> Self {
-        // TODO assert bufs.len() is power of 2 or pad to power of 2?
-        let leaves: Vec<_> = bufs.iter().map(|b| TurbineMerkleHash::hash(&[b])).collect();
+    pub fn new_from_bufs<T>(bufs: &[T]) -> Self
+    where
+        T: Sync + AsRef<[u8]>,
+    {
+        // TODO assert bufs.len() is power of 2 or pad to power of 2
+        let leaves: Vec<_> = bufs
+            .iter()
+            .map(|b| TurbineMerkleHash::hash(&[b.as_ref()]))
+            .collect();
         Self::new_from_leaves(&leaves)
+    }
+
+    #[allow(clippy::integer_arithmetic)]
+    pub fn new_from_bufs_par<T>(bufs: &[T], chunk: usize) -> Self
+    where
+        T: Sync + AsRef<[u8]>,
+    {
+        // compute subtrees of chunk width in parallel
+        let sub_trees: Vec<Vec<TurbineMerkleHash>> = bufs
+            .par_chunks(chunk)
+            .map(|slice| {
+                let mut tree = Vec::with_capacity(chunk * 2 - 1);
+                slice
+                    .iter()
+                    .for_each(|b| tree.push(TurbineMerkleHash::hash(&[b.as_ref()])));
+                let mut base = 0;
+                let mut level_leaves = slice.len();
+                while level_leaves > 1 {
+                    for i in (0..level_leaves).step_by(2) {
+                        let hash =
+                            TurbineMerkleHash::hash(&[&tree[base + i].0, &tree[base + i + 1].0]);
+                        tree.push(hash);
+                    }
+                    base += level_leaves;
+                    level_leaves >>= 1;
+                }
+                tree
+            })
+            .collect();
+
+        let tree_size = bufs.len() * 2 - 1;
+        let mut tree = Vec::with_capacity(tree_size);
+
+        // copy subtrees
+        let mut level_leaves = chunk;
+        let mut base = 0;
+        while level_leaves >= 1 {
+            for sub_tree in sub_trees.iter() {
+                for i in 0..level_leaves {
+                    tree.push(sub_tree[base + i]);
+                }
+            }
+            base += level_leaves;
+            level_leaves >>= 1;
+        }
+
+        // compute final levels of tree
+        level_leaves = bufs.len() / chunk;
+        base = (chunk * 2 - 1) * level_leaves - level_leaves;
+        while level_leaves > 1 {
+            for i in (0..level_leaves).step_by(2) {
+                let hash = TurbineMerkleHash::hash(&[&tree[base + i].0, &tree[base + i + 1].0]);
+                tree.push(hash);
+            }
+            base += level_leaves;
+            level_leaves >>= 1;
+        }
+
+        Self { tree }
     }
 
     #[allow(clippy::integer_arithmetic)]
@@ -187,5 +253,17 @@ mod tests {
 
         assert!(proof5.verify(&root, &leaves[5], 5));
         assert!(proof5.verify(&root, &tree.node(5), 5));
+    }
+
+    #[test]
+    fn test_merkle_from_buf_par() {
+        let packets = create_random_packets(128);
+        let ref_tree = TurbineMerkleTree::new_from_bufs(&packets[..]);
+        let mut chunk_width = 1;
+        while chunk_width <= 128 {
+            let tree = TurbineMerkleTree::new_from_bufs_par(&packets[..], chunk_width);
+            assert_eq!(&ref_tree, &tree);
+            chunk_width *= 2;
+        }
     }
 }
