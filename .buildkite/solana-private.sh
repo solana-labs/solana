bac372cad9af33c19fd6f3858ed7275a4542525b
axleiro <83293196+axleiro@users.noreply.github.com>
2022-05-03 17:10:38 +0530

    adding new file for solana-private CI

--- .buildkite/solana-private.sh
+++ .buildkite/solana-private.sh
@@ -11,9 +11,9 @@ set -e
 NAME=$(buildkite-agent meta-data get name)
 cd "$(dirname "$0")"/..
 source ci/_
-sudo chmod 0777 ci/buildkite-pipeline-in-disk.sh
+sudo chmod 0777 ci/buildkite-solana-private.sh
 
-_ ci/buildkite-pipeline-in-disk.sh pipeline.yml
+_ ci/buildkite-solana-private.sh pipeline.yml
 echo +++ pipeline
 cat pipeline.yml
 
