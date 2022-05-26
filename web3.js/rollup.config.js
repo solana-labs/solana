994ae755a9e2e0ddb1143991c4cb0a6431a60c7d
steveluscher <me+github@steveluscher.com>
2022-05-25 22:21:51 -0700

    chore: augment build script to build forked modules into correct bundles

--- web3.js/rollup.config.js
+++ web3.js/rollup.config.js
@@ -1,6 +1,9 @@
+import alias from '@rollup/plugin-alias';
 import babel from '@rollup/plugin-babel';
 import commonjs from '@rollup/plugin-commonjs';
+import * as fs from 'fs';
 import json from '@rollup/plugin-json';
+import path from 'path';
 import nodeResolve from '@rollup/plugin-node-resolve';
 import replace from '@rollup/plugin-replace';
 import {terser} from 'rollup-plugin-terser';
@@ -15,6 +18,41 @@ function generateConfig(configType, format) {
   const config = {
     input: 'src/index.ts',
     plugins: [
+      alias({
+        entries: [
+          {
+            find: /^\./, // Relative paths.
+            replacement: '.',
+            async customResolver(source, importer, options) {
+              const resolved = await this.resolve(source, importer, {
+                skipSelf: true,
+                ...options,
+              });
+              if (resolved == null) {
+                return;
+              }
+              const {id: resolvedId} = resolved;
+              const directory = path.dirname(resolvedId);
+              const moduleFilename = path.basename(resolvedId);
+              const forkPath = path.join(
+                directory,
+                '__forks__',
+                configType,
+                moduleFilename,
+              );
+              const hasForkCacheKey = `has_fork:${forkPath}`;
+              let hasFork = this.cache.get(hasForkCacheKey);
+              if (hasFork === undefined) {
+                hasFork = fs.existsSync(forkPath);
+                this.cache.set(hasForkCacheKey, hasFork);
+              }
+              if (hasFork) {
+                return forkPath;
+              }
+            },
+          },
+        ],
+      }),
       commonjs(),
       nodeResolve({
         browser,
