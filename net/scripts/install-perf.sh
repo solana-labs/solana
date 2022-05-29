16e18cdfe8a9571bb456f1943c7d21b743e3b3f0
klykov <lykov.kirill@gmail.com>
2022-05-29 19:27:10 +0200

    add comments on permissions setup

--- net/scripts/install-perf.sh
+++ net/scripts/install-perf.sh
@@ -11,5 +11,7 @@ set -ex
 apt-get --assume-yes install linux-tools-common linux-tools-generic "linux-tools-$(uname -r)"
 
 # setup permissions
+# Impose no scope and access restrictions on using perf_events performance monitoring
 echo -1 | tee /proc/sys/kernel/perf_event_paranoid
-sh -c "echo 0 > /proc/sys/kernel/kptr_restrict"
+# Allow recording kernel reference relocation symbol to avoid skewing symbol resolution if relocation was used
+echo 0 | tee /proc/sys/kernel/kptr_restrict
