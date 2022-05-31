3ff62b24feaf0cbe778b354b00083edf0973fb56
Tyera Eulberg <tyera@solana.com>
2022-05-31 16:53:06 -0600

    Add feature to increase timestamp bounding on the slow side to 150% of poh estimate (1s/slot) (#25666)
    (cherry picked from commit 8584a3a331e8bb22724059195d18504c5b87c256)

--- sdk/src/feature_set.rs
+++ sdk/src/feature_set.rs
@@ -363,6 +363,10 @@ pub mod add_shred_type_to_shred_seed {
     solana_sdk::declare_id!("Ds87KVeqhbv7Jw8W6avsS1mqz3Mw5J3pRTpPoDQ2QdiJ");
 }
 
+pub mod warp_timestamp_with_a_vengeance {
+    solana_sdk::declare_id!("3BX6SBeEBibHaVQXywdkcgyUk6evfYZkHdztXiDtEpFS");
+}
+
 lazy_static! {
     /// Map of feature identifiers to user-visible description
     pub static ref FEATURE_NAMES: HashMap<Pubkey, &'static str> = [
@@ -447,6 +451,7 @@ lazy_static! {
         (add_set_compute_unit_price_ix::id(), "add compute budget ix for setting a compute unit price"),
         (include_account_index_in_rent_error::id(), "include account index in rent tx error #25190"),
         (add_shred_type_to_shred_seed::id(), "add shred-type to shred seed #25556"),
+        (warp_timestamp_with_a_vengeance::id(), "warp timestamp again, adjust bounding to 150% slow #25666"),
         /*************** ADD NEW FEATURES HERE ***************/
     ]
     .iter()
