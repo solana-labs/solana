e9ef91110caefe2cf5810020611666a6f51bf44c
Jeff Washington (jwash) <wash678@gmail.com>
2022-05-31 19:29:47 -0500

    log better ancestors

--- runtime/src/serde_snapshot.rs
+++ runtime/src/serde_snapshot.rs
@@ -733,8 +733,8 @@ where
         solana_sdk::clock::DEFAULT_TICKS_PER_SLOT,
     );
 
-    info!("calc hash");
     let ancestors = crate::ancestors::Ancestors::from(slots.into_inner().unwrap());
+    info!("calc hash, slot: {}, ancestors: {:?}", snapshot_slot, ancestors);
     let _ = accounts_db.calculate_accounts_hash_helper(
         false,
         snapshot_slot,
