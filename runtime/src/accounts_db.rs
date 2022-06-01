379e2c96c93e216a276a5e0b0cbbd4137ac9453e
Jeff Washington (jwash) <wash678@gmail.com>
2022-05-31 19:47:07 -0500

    log

--- runtime/src/accounts_db.rs
+++ runtime/src/accounts_db.rs
@@ -7865,6 +7865,7 @@ impl AccountsDb {
                                         if !storages.is_empty() {
                                             Some((storages, *slot))
                                         } else {
+                                            error!("no accounts");
                                             None
                                         }
                                     },
