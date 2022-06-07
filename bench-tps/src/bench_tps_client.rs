f3dd509d8a0a5a463e9595ebac1e1be91c99ed8a
klykov <lykov.kirill@gmail.com>
2022-06-07 16:21:36 +0200

    add get_account to benchtpsclient

--- bench-tps/src/bench_tps_client.rs
+++ bench-tps/src/bench_tps_client.rs
@@ -1,8 +1,9 @@
 use {
     solana_client::{client_error::ClientError, tpu_client::TpuSenderError},
     solana_sdk::{
-        commitment_config::CommitmentConfig, epoch_info::EpochInfo, hash::Hash, message::Message,
-        pubkey::Pubkey, signature::Signature, transaction::Transaction, transport::TransportError,
+        account::Account, commitment_config::CommitmentConfig, epoch_info::EpochInfo, hash::Hash,
+        message::Message, pubkey::Pubkey, signature::Signature, transaction::Transaction,
+        transport::TransportError,
     },
     thiserror::Error,
 };
@@ -79,6 +80,9 @@ pub trait BenchTpsClient {
         lamports: u64,
         recent_blockhash: &Hash,
     ) -> Result<Signature>;
+
+    /// Returns all information associated with the account of the provided pubkey
+    fn get_account(&self, pubkey: &Pubkey) -> Result<Account>;
 }
 
 mod bank_client;
