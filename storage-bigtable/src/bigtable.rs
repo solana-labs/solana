426bbb7b58c4eaa9c133ad7c11b70ba982f434b1
Lawrence Wu <larry.wu@solana.com>
2022-06-11 20:04:05 -0500

    bigtable: cargo fmt

--- storage-bigtable/src/bigtable.rs
+++ storage-bigtable/src/bigtable.rs
@@ -183,17 +183,17 @@ impl BigTableConnection {
                     Ok(proxy_uri) => {
                         let proxy = hyper_proxy::Proxy::new(
                             hyper_proxy::Intercept::All,
-                            proxy_uri.parse::<http::Uri>()
+                            proxy_uri
+                                .parse::<http::Uri>()
                                 .map_err(|err| Error::InvalidUri(proxy_uri, err.to_string()))?,
                         );
-                        let mut proxy_connector = hyper_proxy::ProxyConnector::from_proxy(http, proxy)?;
+                        let mut proxy_connector =
+                            hyper_proxy::ProxyConnector::from_proxy(http, proxy)?;
                         // tonic handles TLS as a separate layer
                         proxy_connector.set_tls(None);
                         endpoint.connect_with_connector_lazy(proxy_connector)
                     }
-                    _ => {
-                        endpoint.connect_with_connector_lazy(http)
-                    }
+                    _ => endpoint.connect_with_connector_lazy(http),
                 };
 
                 Ok(Self {
