ce1a98121e278cbe6c1c772b975e1ebd3538b9bd
Andrew Fitzgerald <apfitzge@gmail.com>
2022-06-13 10:48:43 -0500

    static_ids.rs: use a lazy_static ref instead of a function

--- runtime/src/static_ids.rs
+++ runtime/src/static_ids.rs
@@ -3,12 +3,13 @@ use {
     solana_sdk::pubkey::Pubkey,
 };
 
-pub fn get_static_ids() -> Vec<Pubkey> {
-    vec![
+lazy_static! {
+    /// Vector of static token & mint IDs
+    pub static ref STATIC_IDS: Vec<Pubkey> = vec![
         inline_spl_associated_token_account::id(),
         inline_spl_associated_token_account::program_v1_1_0::id(),
         inline_spl_token::id(),
         inline_spl_token::native_mint::id(),
         inline_spl_token_2022::id(),
-    ]
+    ];
 }
