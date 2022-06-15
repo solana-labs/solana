4792d215a321e039eff69636f48f80124498c9df
Harshal0902 <harshalraikwar07@gmail.com>
2022-06-15 22:07:37 +0530

    💄 Update UI for side-navbar

--- explorer/src/components/Navbar.tsx
+++ explorer/src/components/Navbar.tsx
@@ -1,13 +1,14 @@
-import React, { useCallback } from "react";
+import React from "react";
 import Logo from "img/logos-solana/dark-explorer-logo.svg";
 import { clusterPath } from "utils/url";
 import { Link, NavLink } from "react-router-dom";
 import { ClusterStatusButton } from "components/ClusterStatusButton";
-import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
+import { WalletMultiButton, WalletDisconnectButton } from "@solana/wallet-adapter-react-ui";
 import { useWallet } from "@solana/wallet-adapter-react";
 
 export function Navbar() {
   // TODO: use `collapsing` to animate collapsible navbar
+  const [showSidebar, setShowSidebar] = React.useState(false);
   const [collapse, setCollapse] = React.useState(false);
   const { connected } = useWallet();
 
@@ -51,17 +52,34 @@ export function Navbar() {
             </li>
             <li className="nav-item">
               {connected ? (
-                <span className="fe fe-globe btn btn-primary globe"></span>
+                <span className="fe fe-globe btn btn-primary globe" onClick={() => setShowSidebar(true)}></span>
               ) : null}
+
+              {showSidebar ? (
+                <div className="sidenav">
+                  <div className="sidenav-header">
+                    <span className="fe fe-x close" onClick={() => setShowSidebar(false)}></span>
+                  </div>
+                  <div className="sidenav-body">
+                    <div className="sidenav-item">
+                      <button>
+                        <span className="fe fe-user"></span>Account
+                      </button>
+                    </div>
+                    <div className="sidenav-item">
+                      <button><span className="fe fe-repeat"></span>Swap Tokens</button>
+                    </div>
+                    <hr />
+                    <div className="sidenav-item-disconnect">
+                      <WalletDisconnectButton className="btn btn-primary"/>
+                    </div>
+                  </div>
+                </div>)
+                : null}
+
             </li>
           </ul>
         </div>
-
-        {/* <div>
-        {connected ? (
-                <span className="fe fe-globe btn btn-primary globe"></span>
-              ) : null}
-        </div> */}
 
         <div className="d-none d-md-block">
           <ClusterStatusButton />
