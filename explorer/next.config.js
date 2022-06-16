9f92c4fd91e7311d57e6ca51f930a66e67ef8af2
Bing <bing2609@gmail.com>
2022-06-17 02:08:48 +0800

    added path rewrites to match use-case of explorer
    Signed-off-by: Bing <bing2609@gmail.com>

--- explorer/next.config.js
+++ explorer/next.config.js
@@ -1,13 +1,32 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	reactStrictMode: true,
+
 	images: {
 		domains: ['raw.githubusercontent.com'],
 	},
+
 	webpack: (config) => {
 		config.resolve.fallback = { fs: false }
 		return config
 	},
+
+	async rewrites() {
+		return [
+			{
+				source: '/(accounts|accounts\/top)',
+				destination: '/supply'
+			},
+			{
+				source: '/(account|accounts|addresses)/:address',
+				destination: '/address/:address'
+			},
+			{
+				source: '/(txs|txn|txns|transaction|transactions)/:signature',
+				destination: '/tx/:signature'
+			}
+		]
+	}
 }
 
 module.exports = nextConfig
