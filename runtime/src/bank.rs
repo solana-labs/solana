50ceb060066f7ec858b0d792ee7e986ff3021da5
Haoran Yi <haoran.yi@gmail.com>
2022-06-16 09:26:06 -0500

    Revert "reduce threadpool for stake processing"
    This reverts commit 004a4f872ea7a3ef53e38d145b6350c3f57c680c.

--- runtime/src/bank.rs
+++ runtime/src/bank.rs
@@ -91,7 +91,6 @@ use {
         sysvar_cache::SysvarCache,
         timings::{ExecuteTimingType, ExecuteTimings},
     },
-    solana_rayon_threadlimit::get_thread_count,
     solana_sdk::{
         account::{
             create_account_shared_data_with_fields as create_account, from_account, Account,
@@ -1876,14 +1875,8 @@ impl Bank {
         let (_, update_epoch_time) = measure!(
             {
                 if parent_epoch < new.epoch() {
-                    let num_threads = get_thread_count();
                     let (thread_pool, thread_pool_time) = Measure::this(
-                        |_| {
-                            ThreadPoolBuilder::new()
-                                .num_threads(num_threads)
-                                .build()
-                                .unwrap()
-                        },
+                        |_| ThreadPoolBuilder::new().build().unwrap(),
                         (),
                         "thread_pool_creation",
                     );
