66afcd088652420d4fb28ef6d41691118b9772d5
Bing <bing2609@gmail.com>
2022-06-17 01:48:07 +0800

    whitelisted raw.githubusercontent.com as image domain
    Signed-off-by: Bing <bing2609@gmail.com>

--- explorer/next.config.js
+++ explorer/next.config.js
@@ -1,9 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	reactStrictMode: true,
+	images: {
+		domains: ['raw.githubusercontent.com'],
+	},
 	webpack: (config) => {
 		config.resolve.fallback = { fs: false }
-
 		return config
 	},
 }
