904802bbd4b1d2d7ba6728d56be593615d32f547
Bing <bing2609@gmail.com>
2022-06-17 22:06:11 +0800

    fixed navlink active class logic
    Signed-off-by: Bing <bing2609@gmail.com>

--- explorer/src/components/NavLink.tsx
+++ explorer/src/components/NavLink.tsx
@@ -2,6 +2,7 @@ import React, { Children } from "react";
 import { useRouter } from "next/router";
 import cx from "classnames";
 import Link, { LinkProps } from "next/link";
+import { useCluster, Cluster, clusterSlug } from "src/providers/cluster";
 
 type NavLinkProps = React.PropsWithChildren<LinkProps> & {
   activeClassName?: string;
@@ -13,10 +14,20 @@ export const NavLink = ({
   ...props
 }: NavLinkProps) => {
   const { asPath } = useRouter();
+  const { cluster } = useCluster();
+
   const child = Children.only(children) as React.ReactElement;
   const childClassName = child.props.className || "";
 
-  const isActive = asPath === props.href || asPath === props.as;
+  const [pathWithoutParams] = asPath.split('?');
+  let isActive: boolean
+
+  if (cluster === Cluster.MainnetBeta) {
+    isActive = pathWithoutParams === props.href || pathWithoutParams === props.as;
+  } else {
+    const pathWithCluster = `${pathWithoutParams}?cluster=${clusterSlug(cluster)}`;
+    isActive = pathWithCluster === props.href || pathWithCluster === props.as;
+  }
 
   const className = cx(childClassName, { [activeClassName]: isActive });
 
