84e236c93433871aa15f4e89767d48247937b0c1
Fynn <me@enriquefynn.com>
2022-06-17 09:15:56 -0300

    Limit `block_commitment_cache` read lock.
    As ruuda pointed out, there is a deadlock if we acquire a read lock for
    both `block_commitment_cache` and `bank_forks`, in ruuda words:
    - We take the read lock on block_commitment_cache.
    - Some other thread takes a write lock on bank_forks.
    - We want to take the read lock on bank_forks, but this blocks because
      the other thread holds a write lock.
    - The other thread wants to take a write lock on block_commitment_cache,
      but that blocks because we hold the read lock.
    - Deadlock!
    
    Instead, we guard the `block_commitment_cache` read lock so we don't
    hold two read locks concurrently. This might lead, as ruuda also
    pointed out, to an inconsistency that's known to Solana and dealt
    already in the code the same way it's dealt in `rpc/src/rpc.rs`.

--- prometheus/src/banks_with_commitments.rs
+++ prometheus/src/banks_with_commitments.rs
@@ -15,24 +15,26 @@ impl BanksWithCommitments {
         bank_forks: &Arc<RwLock<BankForks>>,
         block_commitment_cache: &Arc<RwLock<BlockCommitmentCache>>,
     ) -> Self {
-        let block_commitment_cache = block_commitment_cache.read().unwrap();
-        let finalized_slot = block_commitment_cache
-            .slot_with_commitment(solana_sdk::commitment_config::CommitmentLevel::Finalized);
-        let confirmed_slot = block_commitment_cache
-            .slot_with_commitment(solana_sdk::commitment_config::CommitmentLevel::Confirmed);
-        let processed_slot = block_commitment_cache
-            .slot_with_commitment(solana_sdk::commitment_config::CommitmentLevel::Processed);
-
+        let (finalized_slot, confirmed_slot, processed_slot) = {
+            let block_commitment_cache = block_commitment_cache.read().unwrap();
+            (
+                block_commitment_cache.slot_with_commitment(
+                    solana_sdk::commitment_config::CommitmentLevel::Finalized,
+                ),
+                block_commitment_cache.slot_with_commitment(
+                    solana_sdk::commitment_config::CommitmentLevel::Confirmed,
+                ),
+                block_commitment_cache.slot_with_commitment(
+                    solana_sdk::commitment_config::CommitmentLevel::Processed,
+                ),
+            )
+        };
         let r_bank_forks = bank_forks.read().unwrap();
 
         let default_closure = || {
             // From rpc/src/rpc.rs
-            // We log a warning instead of returning an error, because all known error cases
-            // are due to known bugs that should be fixed instead.
             //
-            // The slot may not be found as a result of a known bug in snapshot creation, where
-            // the bank at the given slot was not included in the snapshot.
-            // Also, it may occur after an old bank has been purged from BankForks and a new
+            // It may occur after an old bank has been purged from BankForks and a new
             // BlockCommitmentCache has not yet arrived. To make this case impossible,
             // BlockCommitmentCache should hold an `Arc<Bank>` everywhere it currently holds
             // a slot.
