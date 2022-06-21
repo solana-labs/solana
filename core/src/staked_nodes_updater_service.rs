61946a49c3a31667146e6fd14cf7bd79fb462635
Lijun Wang <83639177+lijunwangs@users.noreply.github.com>
2022-06-21 12:06:44 -0700

    Weight concurrent streams by stake (#25993)
    Weight concurrent streams by stake for staked nodes
    Ported changes from #25056 after address merge conflicts and some refactoring

--- core/src/staked_nodes_updater_service.rs
+++ core/src/staked_nodes_updater_service.rs
@@ -1,6 +1,7 @@
 use {
     solana_gossip::cluster_info::ClusterInfo,
     solana_runtime::bank_forks::BankForks,
+    solana_streamer::streamer::StakedNodes,
     std::{
         collections::HashMap,
         net::IpAddr,
@@ -24,7 +25,7 @@ impl StakedNodesUpdaterService {
         exit: Arc<AtomicBool>,
         cluster_info: Arc<ClusterInfo>,
         bank_forks: Arc<RwLock<BankForks>>,
-        shared_staked_nodes: Arc<RwLock<HashMap<IpAddr, u64>>>,
+        shared_staked_nodes: Arc<RwLock<StakedNodes>>,
     ) -> Self {
         let thread_hdl = Builder::new()
             .name("sol-sn-updater".to_string())
@@ -32,14 +33,17 @@ impl StakedNodesUpdaterService {
                 let mut last_stakes = Instant::now();
                 while !exit.load(Ordering::Relaxed) {
                     let mut new_ip_to_stake = HashMap::new();
+                    let mut total_stake = 0;
                     if Self::try_refresh_ip_to_stake(
                         &mut last_stakes,
                         &mut new_ip_to_stake,
+                        &mut total_stake,
                         &bank_forks,
                         &cluster_info,
                     ) {
                         let mut shared = shared_staked_nodes.write().unwrap();
-                        *shared = new_ip_to_stake;
+                        shared.total_stake = total_stake as f64;
+                        shared.stake_map = new_ip_to_stake;
                     }
                 }
             })
@@ -51,12 +55,17 @@ impl StakedNodesUpdaterService {
     fn try_refresh_ip_to_stake(
         last_stakes: &mut Instant,
         ip_to_stake: &mut HashMap<IpAddr, u64>,
+        total_stake: &mut u64,
         bank_forks: &RwLock<BankForks>,
         cluster_info: &ClusterInfo,
     ) -> bool {
         if last_stakes.elapsed() > IP_TO_STAKE_REFRESH_DURATION {
             let root_bank = bank_forks.read().unwrap().root_bank();
             let staked_nodes = root_bank.staked_nodes();
+            *total_stake = staked_nodes
+                .iter()
+                .map(|(_pubkey, stake)| stake)
+                .sum::<u64>();
             *ip_to_stake = cluster_info
                 .tvu_peers()
                 .into_iter()
