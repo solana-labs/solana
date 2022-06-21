4c03d26cdb1411a9f68a70b918fff28edb0f5740
Andrew Fitzgerald <apfitzge@gmail.com>
2022-06-21 14:32:24 -0400

    Add comment about load_to_collect_rent_eagerly

--- runtime/src/snapshot_minimizer.rs
+++ runtime/src/snapshot_minimizer.rs
@@ -144,6 +144,9 @@ impl<'a> SnapshotMinimizer<'a> {
 
         partitions.into_iter().for_each(|partition| {
             let subrange = Bank::pubkey_range_from_partition(partition);
+            // This may be overkill since we just need the pubkeys and don't need to actually load the accounts.
+            // Leaving it for now as this is only used by ledger-tool. If used in runtime, we will need to instead use
+            // some of the guts of `load_to_collect_rent_eagerly`.
             self.bank
                 .accounts()
                 .load_to_collect_rent_eagerly(&self.bank.ancestors, subrange)
