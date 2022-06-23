30420e60f8f10ed0ac9886a06e46a580abb47118
yihau <a122092487@gmail.com>
2022-06-23 11:11:54 +0800

    lint: use space

--- ci/publish-installer.sh
+++ ci/publish-installer.sh
@@ -5,22 +5,22 @@ cd "$(dirname "$0")/.."
 
 # check does it need to publish
 if [[ -n $DO_NOT_PUBLISH_TAR ]]; then
-	echo "Skipping publishing install wrapper"
-	exit 0
+  echo "Skipping publishing install wrapper"
+  exit 0
 fi
 
 # check channel and tag
 eval "$(ci/channel-info.sh)"
 
 if [[ -n "$CI_TAG" ]]; then
-	CHANNEL_OR_TAG=$CI_TAG
+  CHANNEL_OR_TAG=$CI_TAG
 else
-	CHANNEL_OR_TAG=$CHANNEL
+  CHANNEL_OR_TAG=$CHANNEL
 fi
 
 if [[ -z $CHANNEL_OR_TAG ]]; then
-	echo +++ Unable to determine channel or tag to publish into, exiting.
-	exit 0
+  echo +++ Unable to determine channel or tag to publish into, exiting.
+  exit 0
 fi
 
 # upload install script
