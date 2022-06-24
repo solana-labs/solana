edb6d4a1aedff113ad8c3363fbeb66e93edac2a7
Jeff Washington (jwash) <wash678@gmail.com>
2022-06-24 07:25:22 -0500

    increase mmap file limit to 1M to match published instructions

--- ledger/src/blockstore.rs
+++ ledger/src/blockstore.rs
@@ -4218,7 +4218,10 @@ fn adjust_ulimit_nofile(_enforce_ulimit_nofile: bool) -> Result<()> {
 fn adjust_ulimit_nofile(enforce_ulimit_nofile: bool) -> Result<()> {
     // Rocks DB likes to have many open files.  The default open file descriptor limit is
     // usually not enough
-    let desired_nofile = 500000;
+    // AppendVecs and disk Account Index are also heavy users of mmapped files.
+    // This should be kept in sync with published validator instructions.
+    // https://docs.solana.com/running-validator/validator-start#increased-memory-mapped-files-limit
+    let desired_nofile = 1_000_000;
 
     fn get_nofile() -> libc::rlimit {
         let mut nofile = libc::rlimit {
