02bdccbfa1ed12a0dd4f2b913b73c2c9a5f76f8b
DimAn <diman@diman.io>
2022-06-24 23:20:00 +0300

    Revert "adapt get_peer_snapshot_hashes"
    This reverts commit 3e905998633437dea0c1559a8225fed134550242.

--- validator/src/bootstrap.rs
+++ validator/src/bootstrap.rs
@@ -670,11 +670,9 @@ fn get_peer_snapshot_hashes(
             &mut peer_snapshot_hashes,
         );
     }
-    if incremental_snapshot_fetch {
-        retain_peer_snapshot_hashes_with_highest_incremental_snapshot_slot(
+    retain_peer_snapshot_hashes_with_highest_incremental_snapshot_slot(
             &mut peer_snapshot_hashes,
-        );
-    }
+    );
     retain_peer_snapshot_hashes_with_highest_full_snapshot_slot(&mut peer_snapshot_hashes);
 
     peer_snapshot_hashes
