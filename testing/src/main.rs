7b392d66069665bc78f46cc9f847ee51f9b6b6cb
greg <gregory.cusack@colorado.edu>
2022-06-28 12:15:49 -0600

    can read pubkeys from file

--- testing/src/main.rs
+++ testing/src/main.rs
@@ -1,5 +1,7 @@
 #![allow(clippy::integer_arithmetic)]
 
+use solana_sdk::pubkey::Pubkey;
+
 #[cfg(not(target_env = "msvc"))]
 use {
     log::*,
@@ -62,7 +64,7 @@ pub const DEFAULT_SHRED_VERSION: u16 = 44120;
 // }
 
 struct Accounts {
-    pubkey: String,
+    pubkey: Pubkey,
     stake: u64,
 
 }
@@ -89,27 +91,25 @@ fn parse_matches() -> ArgMatches<'static> {
         .get_matches()
 }
 
-/// Returns the first positional argument sent to this process. If there are no
-/// positional arguments, then this returns an error.
-fn get_first_arg() -> Result<OsString, Box<dyn Error>> {
-    match env::args_os().nth(1) {
-        None => Err(From::from("expected 1 argument, but got none")),
-        Some(file_path) => Ok(file_path),
-    }
-}
-
 fn parse_accounts_file(account_path: &OsString, account_vec: &mut Vec<Accounts>) -> Result<(), Box<dyn Error>> {
     let file = File::open(account_path)?;
     let mut rdr = csv::Reader::from_reader(file);
     for result in rdr.records() {
         let record = result?;
-        let key: String = record[0].to_string();
-        let stake: String = record[1].to_string();
-        println!("record: {}", stake);
-        // let stake = record[1].parse::<u64>().unwrap();
+        // let key: String = record[0].to_string();
+        let pubkey: Pubkey = Pubkey::from_str( &record[0]
+            .to_string())
+            .unwrap();
+        let stake: u64 = record[1]
+            .to_string()
+            .trim()
+            .parse::<u64>()
+            .unwrap();
 
-        // account_vec.push(Accounts { pubkey: key, stake: stake });
-        println!("{:?}", record);
+        account_vec.push(Accounts { 
+            pubkey: pubkey, 
+            stake: stake,
+        });
     }
     Ok(())
 }
@@ -134,7 +134,7 @@ pub fn main() {
     }
 
     for account in accounts {
-        println!("key: {}", account.pubkey);
+        println!("account: {}, {}", account.pubkey, account.stake);
     }
 
 
@@ -179,192 +179,192 @@ pub fn main() {
     let node_keypair_2 = Keypair::from_bytes(&priv_node_key_2)
         .map_err(|e| std::io::Error::new(std::io::ErrorKind::Other, e.to_string())).unwrap();
 
-    error!("keys: {}, {}, {}", node_keypair_0.pubkey(), node_keypair_1.pubkey(), node_keypair_2.pubkey());
+    println!("keys: {}, {}, {}", node_keypair_0.pubkey(), node_keypair_1.pubkey(), node_keypair_2.pubkey());
     println!("suhh");
 
 
-    let mut node_keys: Vec<Keypair> = Vec::new();
-    let mut stakes_in_genesis: Vec<u64> = Vec::new();
+    // let mut node_keys: Vec<Keypair> = Vec::new();
+    // let mut stakes_in_genesis: Vec<u64> = Vec::new();
 
-    node_keys.push(node_keypair_0);
-    node_keys.push(node_keypair_1);
-    node_keys.push(node_keypair_2);
+    // node_keys.push(node_keypair_0);
+    // node_keys.push(node_keypair_1);
+    // node_keys.push(node_keypair_2);
     
-    stakes_in_genesis.push(stake_0);
-    stakes_in_genesis.push(stake_1);
-    stakes_in_genesis.push(stake_2);
+    // stakes_in_genesis.push(stake_0);
+    // stakes_in_genesis.push(stake_1);
+    // stakes_in_genesis.push(stake_2);
 
 
 
-    // let leader_keypair = &keys_in_genesis[0].node_keypair;
-    let leader_keypair = &node_keys[0];
-    let leader_pubkey = leader_keypair.pubkey();
+    // // let leader_keypair = &keys_in_genesis[0].node_keypair;
+    // let leader_keypair = &node_keys[0];
+    // let leader_pubkey = leader_keypair.pubkey();
 
-    let num_nodes = node_keys.len();
+    // let num_nodes = node_keys.len();
 
-    let mut config = ClusterConfig::default();
+    // let mut config = ClusterConfig::default();
 
-    //Setup genesis config.
-    let genesis_config_info = &mut create_genesis_config_with_leader(
-        cluster_lamports,
-        &leader_pubkey,
-        stakes_in_genesis[0],
-    );
-    let genesis_config = &mut genesis_config_info.genesis_config;
+    // //Setup genesis config.
+    // let genesis_config_info = &mut create_genesis_config_with_leader(
+    //     cluster_lamports,
+    //     &leader_pubkey,
+    //     stakes_in_genesis[0],
+    // );
+    // let genesis_config = &mut genesis_config_info.genesis_config;
 
-    let mut gossip_thread_vector: Vec<GossipService> = Vec::new();
+    // let mut gossip_thread_vector: Vec<GossipService> = Vec::new();
 
-    for i in 0..num_nodes {
-        let mut entrypoint_addrs: Vec<SocketAddr> = Vec::new();
-        let gossip_host = IpAddr::from_str("127.0.0.1").unwrap();
-        if i == 0 {
-            //Entrypoint to join Gossip Cluster
-            let gossip_addr = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 8001);
+    // for i in 0..num_nodes {
+    //     let mut entrypoint_addrs: Vec<SocketAddr> = Vec::new();
+    //     let gossip_host = IpAddr::from_str("127.0.0.1").unwrap();
+    //     if i == 0 {
+    //         //Entrypoint to join Gossip Cluster
+    //         let gossip_addr = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 8001);
 
-            //create new node with external ip
-            let mut leader_node = Node::new_with_external_ip(   
-                &leader_pubkey,
-                &gossip_addr,
-                dynamic_port_range,
-                bind_address,
-                None,
-            );
-            let leader_keypair = Arc::new(Keypair::from_bytes(&leader_keypair.to_bytes()).unwrap());
-            leader_node.info.shred_version = shred_version;
+    //         //create new node with external ip
+    //         let mut leader_node = Node::new_with_external_ip(   
+    //             &leader_pubkey,
+    //             &gossip_addr,
+    //             dynamic_port_range,
+    //             bind_address,
+    //             None,
+    //         );
+    //         let leader_keypair = Arc::new(Keypair::from_bytes(&leader_keypair.to_bytes()).unwrap());
+    //         leader_node.info.shred_version = shred_version;
         
 
-            config.node_stakes = stakes_in_genesis.clone();
-            config.cluster_lamports = cluster_lamports;
-            config.validator_configs = make_identical_validator_configs(&ValidatorConfig::default_for_test(), 1);
+    //         config.node_stakes = stakes_in_genesis.clone();
+    //         config.cluster_lamports = cluster_lamports;
+    //         config.validator_configs = make_identical_validator_configs(&ValidatorConfig::default_for_test(), 1);
 
-            let cluster_info = ClusterInfo::new(
-                leader_node.info.clone(),
-                leader_keypair.clone(),
-                socket_addr_space,
-            );
+    //         let cluster_info = ClusterInfo::new(
+    //             leader_node.info.clone(),
+    //             leader_keypair.clone(),
+    //             socket_addr_space,
+    //         );
 
-            //set entrypoints for gossip
-            let cluster_entrypoints = entrypoint_addrs
-                .iter()
-                .map(ContactInfo::new_gossip_entry_point)
-                .collect::<Vec<_>>();
+    //         //set entrypoints for gossip
+    //         let cluster_entrypoints = entrypoint_addrs
+    //             .iter()
+    //             .map(ContactInfo::new_gossip_entry_point)
+    //             .collect::<Vec<_>>();
 
-            cluster_info.set_entrypoints(cluster_entrypoints);
-            let cluster_info = Arc::new(cluster_info);
+    //         cluster_info.set_entrypoints(cluster_entrypoints);
+    //         let cluster_info = Arc::new(cluster_info);
 
-                //Generate new bank and bank forks
-            let accounts_dir = TempDir::new().unwrap();
-            let bank0 = Bank::new_with_paths_for_tests(
-                genesis_config,
-                vec![accounts_dir.path().to_path_buf()],
-                None,
-                None,
-                AccountSecondaryIndexes::default(),
-                false,
-                accounts_db::AccountShrinkThreshold::default(),
-                false,
-            );
-            bank0.freeze();
-            let bank_forks = BankForks::new(bank0);
-            let bank_forks = Arc::new(RwLock::new(bank_forks));
+    //             //Generate new bank and bank forks
+    //         let accounts_dir = TempDir::new().unwrap();
+    //         let bank0 = Bank::new_with_paths_for_tests(
+    //             genesis_config,
+    //             vec![accounts_dir.path().to_path_buf()],
+    //             None,
+    //             None,
+    //             AccountSecondaryIndexes::default(),
+    //             false,
+    //             accounts_db::AccountShrinkThreshold::default(),
+    //             false,
+    //         );
+    //         bank0.freeze();
+    //         let bank_forks = BankForks::new(bank0);
+    //         let bank_forks = Arc::new(RwLock::new(bank_forks));
 
-            let (stats_reporter_sender, _stats_reporter_receiver) = unbounded();
+    //         let (stats_reporter_sender, _stats_reporter_receiver) = unbounded();
 
-            // Run Gossip
-            let exit_gossip = Arc::new(AtomicBool::new(false));
-            let gossip_service = GossipService::new(
-                &cluster_info,
-                Some(Arc::clone(&bank_forks.clone())),
-                leader_node.sockets.gossip,
-                None,
-                true,   //should check dup instance
-                Some(stats_reporter_sender.clone()),
+    //         // Run Gossip
+    //         let exit_gossip = Arc::new(AtomicBool::new(false));
+    //         let gossip_service = GossipService::new(
+    //             &cluster_info,
+    //             Some(Arc::clone(&bank_forks.clone())),
+    //             leader_node.sockets.gossip,
+    //             None,
+    //             true,   //should check dup instance
+    //             Some(stats_reporter_sender.clone()),
 
-                &exit_gossip,
-            );
+    //             &exit_gossip,
+    //         );
 
-            gossip_thread_vector.push(gossip_service);
-        } 
-        else {
-            let entrypoint = "127.0.0.1:8001";
-            entrypoint_addrs.push(
-                entrypoint
-                .parse()
-                .expect("Unable to parse socket address")
-            );
-            let gossip_addr = SocketAddr::new(
-                gossip_host,
-                solana_net_utils::find_available_port_in_range(bind_address, (0, 1)).unwrap_or_else(
-                    |err| {
-                        eprintln!("Unable to find an available gossip port: {}", err);
-                        exit(1);
-                    }
-                )
-            );
+    //         gossip_thread_vector.push(gossip_service);
+    //     } 
+    //     else {
+    //         let entrypoint = "127.0.0.1:8001";
+    //         entrypoint_addrs.push(
+    //             entrypoint
+    //             .parse()
+    //             .expect("Unable to parse socket address")
+    //         );
+    //         let gossip_addr = SocketAddr::new(
+    //             gossip_host,
+    //             solana_net_utils::find_available_port_in_range(bind_address, (0, 1)).unwrap_or_else(
+    //                 |err| {
+    //                     eprintln!("Unable to find an available gossip port: {}", err);
+    //                     exit(1);
+    //                 }
+    //             )
+    //         );
 
-            //create new node with external ip
-            let mut my_node = Node::new_with_external_ip( //run this with pubkey[i]
-                &node_keys[i].pubkey(),
-                &gossip_addr,
-                dynamic_port_range,
-                bind_address,
-                None,
-            );
-            let my_keypair = Arc::new(Keypair::from_bytes(&node_keys[i].to_bytes()).unwrap());
-            my_node.info.shred_version = shred_version;
+    //         //create new node with external ip
+    //         let mut my_node = Node::new_with_external_ip( //run this with pubkey[i]
+    //             &node_keys[i].pubkey(),
+    //             &gossip_addr,
+    //             dynamic_port_range,
+    //             bind_address,
+    //             None,
+    //         );
+    //         let my_keypair = Arc::new(Keypair::from_bytes(&node_keys[i].to_bytes()).unwrap());
+    //         my_node.info.shred_version = shred_version;
 
-            let cluster_info = ClusterInfo::new(
-                my_node.info.clone(),
-                my_keypair.clone(),
-                socket_addr_space,
-            );
+    //         let cluster_info = ClusterInfo::new(
+    //             my_node.info.clone(),
+    //             my_keypair.clone(),
+    //             socket_addr_space,
+    //         );
 
-            //set entrypoints for gossip
-            let cluster_entrypoints = entrypoint_addrs
-                .iter()
-                .map(ContactInfo::new_gossip_entry_point)
-                .collect::<Vec<_>>();
+    //         //set entrypoints for gossip
+    //         let cluster_entrypoints = entrypoint_addrs
+    //             .iter()
+    //             .map(ContactInfo::new_gossip_entry_point)
+    //             .collect::<Vec<_>>();
 
-            cluster_info.set_entrypoints(cluster_entrypoints);
-            // cluster_info.my_contact_info().
-            let cluster_info = Arc::new(cluster_info);
+    //         cluster_info.set_entrypoints(cluster_entrypoints);
+    //         // cluster_info.my_contact_info().
+    //         let cluster_info = Arc::new(cluster_info);
 
-            //Generate new bank and bank forks
-            let accounts_dir = TempDir::new().unwrap();
-            let bank0 = Bank::new_with_paths_for_tests(
-                genesis_config,
-                vec![accounts_dir.path().to_path_buf()],
-                None,
-                None,
-                AccountSecondaryIndexes::default(),
-                false,
-                accounts_db::AccountShrinkThreshold::default(),
-                false,
-            );
-            bank0.freeze();
-            let bank_forks = BankForks::new(bank0);
-            let bank_forks = Arc::new(RwLock::new(bank_forks));
+    //         //Generate new bank and bank forks
+    //         let accounts_dir = TempDir::new().unwrap();
+    //         let bank0 = Bank::new_with_paths_for_tests(
+    //             genesis_config,
+    //             vec![accounts_dir.path().to_path_buf()],
+    //             None,
+    //             None,
+    //             AccountSecondaryIndexes::default(),
+    //             false,
+    //             accounts_db::AccountShrinkThreshold::default(),
+    //             false,
+    //         );
+    //         bank0.freeze();
+    //         let bank_forks = BankForks::new(bank0);
+    //         let bank_forks = Arc::new(RwLock::new(bank_forks));
 
-            let (stats_reporter_sender, _stats_reporter_receiver) = unbounded();
+    //         let (stats_reporter_sender, _stats_reporter_receiver) = unbounded();
 
-            // Run Gossip
-            let exit_gossip = Arc::new(AtomicBool::new(false));
-            let gossip_service = GossipService::new(
-                &cluster_info,
-                Some(bank_forks.clone()),
-                my_node.sockets.gossip,
-                None,
-                true,   //should check dup instance
-                Some(stats_reporter_sender.clone()),
+    //         // Run Gossip
+    //         let exit_gossip = Arc::new(AtomicBool::new(false));
+    //         let gossip_service = GossipService::new(
+    //             &cluster_info,
+    //             Some(bank_forks.clone()),
+    //             my_node.sockets.gossip,
+    //             None,
+    //             true,   //should check dup instance
+    //             Some(stats_reporter_sender.clone()),
 
-                &exit_gossip,
-            );
+    //             &exit_gossip,
+    //         );
 
-            gossip_thread_vector.push(gossip_service);
-        }
-    }
-    for thread in gossip_thread_vector {
-        thread.join().unwrap();
-    }
+    //         gossip_thread_vector.push(gossip_service);
+    //     }
+    // }
+    // for thread in gossip_thread_vector {
+    //     thread.join().unwrap();
+    // }
 
 }
