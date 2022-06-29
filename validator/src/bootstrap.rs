ccd5541cebd1f401a814cc7fc9ca6209ed823e06
DimAn <diman@diman.io>
2022-06-30 02:18:43 +0300

    Revert "retain_peer_snapshot_hashes_with_highest_incremental_snapshot_slot: add check before retaining"
    This reverts commit ea70a7061c67cc49145b4afc830ee44ed94ce2d9.

--- validator/src/bootstrap.rs
+++ validator/src/bootstrap.rs
@@ -949,11 +949,9 @@ fn retain_peer_snapshot_hashes_with_highest_incremental_snapshot_slot(
         };
     });
 
-    if highest_incremental_snapshot_hash.is_some() {
-        peer_snapshot_hashes.retain(|peer_snapshot_hash| {
-            peer_snapshot_hash.snapshot_hash.incr == highest_incremental_snapshot_hash
-        });
-    }
+    peer_snapshot_hashes.retain(|peer_snapshot_hash| {
+        peer_snapshot_hash.snapshot_hash.incr == highest_incremental_snapshot_hash
+    });
 
     trace!(
         "retain peer snapshot hashes with highest incremental snapshot slot: {:?}",
