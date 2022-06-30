36d13af015c2830530dd68faae770a558aee0839
Brooks Prumo <brooks@solana.com>
2022-06-29 21:21:30 -0500

    Add stats for Accounts Background Service (#26298)
    * Add stats for Accounts Background Service
    
    * pr

--- runtime/src/accounts_background_service.rs
+++ runtime/src/accounts_background_service.rs
@@ -2,6 +2,7 @@
 //
 // This can be expensive since we have to walk the append vecs being cleaned up.
 
+mod stats;
 use {
     crate::{
         accounts_hash::CalcAccountsHashConfig,
@@ -19,6 +20,7 @@ use {
         clock::{BankId, Slot},
         hash::Hash,
     },
+    stats::StatsManager,
     std::{
         boxed::Box,
         fmt::{Debug, Formatter},
@@ -460,11 +462,13 @@ impl AccountsBackgroundService {
         let t_background = Builder::new()
             .name("solana-bg-accounts".to_string())
             .spawn(move || {
+                let mut stats = StatsManager::new();
                 let mut last_snapshot_end_time = None;
                 loop {
                     if exit.load(Ordering::Relaxed) {
                         break;
                     }
+                    let start_time = Instant::now();
 
                     // Grab the current root bank
                     let bank = bank_forks.read().unwrap().root_bank().clone();
@@ -559,6 +563,7 @@ impl AccountsBackgroundService {
                             last_cleaned_block_height = bank.block_height();
                         }
                     }
+                    stats.record_and_maybe_submit(start_time.elapsed());
                     sleep(Duration::from_millis(INTERVAL_MS));
                 }
             })
