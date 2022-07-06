4d4f97b820be771f04c780abe0b1914066d863f3
Xiang Zhu <xzhu70@gmail.com>
2022-07-05 22:48:17 -0700

    cargo fmt --all fixes

--- runtime/src/accounts_db.rs
+++ runtime/src/accounts_db.rs
@@ -2537,9 +2537,8 @@ impl AccountsDb {
                         let mut not_found_on_fork = 0;
                         let mut missing = 0;
                         let mut useful = 0;
-                        self.accounts_index.scan(
-                            pubkeys.iter(),
-                            |pubkey, slots_refs| {
+                        self.accounts_index
+                            .scan(pubkeys.iter(), |pubkey, slots_refs| {
                                 let mut useless = true;
                                 if let Some((slot_list, ref_count)) = slots_refs {
                                     let index_in_slot_list = self.accounts_index.latest_slot(
@@ -2603,8 +2602,7 @@ impl AccountsDb {
                                 } else {
                                     AccountsIndexScanResult::KeepInMemory
                                 }
-                            },
-                        );
+                            });
                         found_not_zero_accum.fetch_add(found_not_zero, Ordering::Relaxed);
                         not_found_on_fork_accum.fetch_add(not_found_on_fork, Ordering::Relaxed);
                         missing_accum.fetch_add(missing, Ordering::Relaxed);
