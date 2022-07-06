ab164fc975089fcf2108c20cad9870518894680a
Jeff Washington (jwash) <wash678@gmail.com>
2022-07-06 11:22:26 -0500

    simplify code (#26443)

--- runtime/src/accounts_db.rs
+++ runtime/src/accounts_db.rs
@@ -2518,7 +2518,6 @@ impl AccountsDb {
         let total_keys_count = pubkeys.len();
         let mut accounts_scan = Measure::start("accounts_scan");
         let uncleaned_roots = self.accounts_index.clone_uncleaned_roots();
-        let uncleaned_roots_len = self.accounts_index.uncleaned_roots_len();
         let found_not_zero_accum = AtomicU64::new(0);
         let not_found_on_fork_accum = AtomicU64::new(0);
         let missing_accum = AtomicU64::new(0);
@@ -2569,13 +2568,11 @@ impl AccountsDb {
                                             } else {
                                                 found_not_zero += 1;
                                             }
-                                            let slot = *slot;
-
-                                            if uncleaned_roots.contains(&slot) {
+                                            if uncleaned_roots.contains(slot) {
                                                 // Assertion enforced by `accounts_index.get()`, the latest slot
                                                 // will not be greater than the given `max_clean_root`
                                                 if let Some(max_clean_root) = max_clean_root {
-                                                    assert!(slot <= max_clean_root);
+                                                    assert!(slot <= &max_clean_root);
                                                 }
                                                 purges_old_accounts.push(*pubkey);
                                                 useless = false;
@@ -2776,7 +2773,7 @@ impl AccountsDb {
                 i64
             ),
             ("scan_missing", missing_accum.load(Ordering::Relaxed), i64),
-            ("uncleaned_roots_len", uncleaned_roots_len, i64),
+            ("uncleaned_roots_len", uncleaned_roots.len(), i64),
             (
                 "clean_old_root_us",
                 self.clean_accounts_stats
