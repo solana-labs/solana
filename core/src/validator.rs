1d4a4ed7c03a1eed5dea81ae6d187e049ef3ccff
Trent Nelson <trent@solana.com>
2022-07-19 23:50:04 -0600

    Enable QUIC servers by default
    (cherry picked from commit a603c8b0bc761fec86cd9010a416c87e922afe3f)

--- core/src/validator.rs
+++ core/src/validator.rs
@@ -87,7 +87,7 @@ use {
         clock::Slot,
         epoch_schedule::MAX_LEADER_SCHEDULE_EPOCH_OFFSET,
         exit::Exit,
-        genesis_config::{ClusterType, GenesisConfig},
+        genesis_config::GenesisConfig,
         hash::Hash,
         pubkey::Pubkey,
         shred_version::compute_shred_version,
@@ -236,7 +236,7 @@ impl Default for ValidatorConfig {
             accounts_db_config: None,
             wait_to_vote_slot: None,
             ledger_column_options: LedgerColumnOptions::default(),
-            enable_quic_servers: false,
+            enable_quic_servers: true,
         }
     }
 }
@@ -958,18 +958,6 @@ impl Validator {
             &connection_cache,
         );
 
-        let enable_quic_servers = if genesis_config.cluster_type == ClusterType::MainnetBeta {
-            config.enable_quic_servers
-        } else {
-            if config.enable_quic_servers {
-                warn!(
-                    "ignoring --enable-quic-servers. QUIC is always enabled for cluster type: {:?}",
-                    genesis_config.cluster_type
-                );
-            }
-            true
-        };
-
         let tpu = Tpu::new(
             &cluster_info,
             &poh_recorder,
@@ -1001,7 +989,7 @@ impl Validator {
             &cost_model,
             &connection_cache,
             &identity_keypair,
-            enable_quic_servers,
+            config.enable_quic_servers,
         );
 
         datapoint_info!(
