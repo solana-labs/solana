9219d9932ae48290c09dc7ecb33b1826197aa517
Andrew Fitzgerald <apfitzge@gmail.com>
2022-07-20 12:56:29 -0500

    Add snapshot_storage_lengths_from_fields

--- runtime/src/serde_snapshot.rs
+++ runtime/src/serde_snapshot.rs
@@ -230,6 +230,25 @@ pub(crate) fn compare_two_serialized_banks(
     Ok(fields1 == fields2)
 }
 
+/// Get snapshot storage lengths from accounts_db_fields
+pub(crate) fn snapshot_storage_lengths_from_fields(
+    accounts_db_fields: &AccountsDbFields<SerializableAccountStorageEntry>,
+) -> HashMap<Slot, HashMap<SerializedAppendVecId, usize>> {
+    let AccountsDbFields(snapshot_storage, ..) = &accounts_db_fields;
+    snapshot_storage
+        .iter()
+        .map(|(slot, slot_storage)| {
+            (
+                *slot,
+                slot_storage
+                    .iter()
+                    .map(|storage_entry| (storage_entry.id(), storage_entry.current_len()))
+                    .collect(),
+            )
+        })
+        .collect()
+}
+
 pub(crate) fn fields_from_stream<R: Read>(
     serde_style: SerdeStyle,
     snapshot_stream: &mut BufReader<R>,
