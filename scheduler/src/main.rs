5a4e115241d55807b8d92e82715c478e13da82a8
Ryo Onodera <ryoqun@gmail.com>
2022-07-23 21:44:47 +0900

    save

--- scheduler/src/main.rs
+++ scheduler/src/main.rs
@@ -16,7 +16,7 @@ fn main() {
         let s = s.clone();
         std::thread::spawn(move || {
             let mut i = 0;
-            for _ in 0..6 {
+            for _ in 0..6000000 {
                 let ss = (thx, i, std::time::Instant::now(), ExecutionEnvironment::default());
                 s.send(ss).unwrap();
                 i += 1;
@@ -28,14 +28,15 @@ fn main() {
         let mut count = 0;
         let start = std::time::Instant::now();
         let mut rrr = Vec::with_capacity(10);
-        for _ in 0..10 {
+        //for _ in 0..10 {
+        loop {
             let rr = r.recv().unwrap();
             rrr.push((rr.2.elapsed(), rr));
         }
 
         for rr in rrr {
             count += 1;
-            error!("recv-ed: {:?}", &rr);
+            //error!("recv-ed: {:?}", &rr);
             if count % 100_000 == 0 {
                 error!("recv-ed: {}", count / start.elapsed().as_secs().max(1));
                 break
