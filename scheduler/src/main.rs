b39a0902a5dad78bc106a8ef8599d11dc4734df3
Ryo Onodera <ryoqun@gmail.com>
2022-07-23 21:35:47 +0900

    save

--- scheduler/src/main.rs
+++ scheduler/src/main.rs
@@ -27,8 +27,8 @@ fn main() {
     joins.push(std::thread::spawn(move || {
         let mut count = 0;
         let start = std::time::Instant::now();
-        let mut rrr = Vec::new();
-        for _ in 0..8 {
+        let mut rrr = Vec::new_with_capacity(10);
+        for _ in 0..10 {
             rrr.push(r.recv().unwrap());
         }
 
