8ec8dd4cbd01113b90693b42f5f592f6f2caa64d
Ryo Onodera <ryoqun@gmail.com>
2022-07-23 21:51:42 +0900

    save

--- scheduler/src/main.rs
+++ scheduler/src/main.rs
@@ -11,7 +11,7 @@ fn main() {
     solana_logger::setup();
     error!("hello");
     let (s, r) = bounded(1000);
-    let (s2, &r2) = bounded(20);
+    let (s2, r2) = &bounded(20);
 
     let mut joins = (0..3).map(|thx| {
         let s = s.clone();
