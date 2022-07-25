e1319c9b5b48c32acbad066d6d5a6d17d077185f
Ryo Onodera <ryoqun@gmail.com>
2022-07-25 22:39:10 +0900

    save

--- scheduler/src/main.rs
+++ scheduler/src/main.rs
@@ -193,7 +193,7 @@ fn try_lock_for_tx<'a>(
     writable_attempts
 }
 
-fn ensure_unlock_for_tx(address_book: &mut AddressBook, Vec<LockAttempt>) {
+fn ensure_unlock_for_tx(address_book: &mut AddressBook, lock_attempts: Vec<LockAttempt>) {
     for l in lock_attempts {
         ensure_unlock(address_book, &l)
     }
