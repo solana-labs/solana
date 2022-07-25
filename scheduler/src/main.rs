bcaa0df9664208a512e700abee442b23b9be4359
Ryo Onodera <ryoqun@gmail.com>
2022-07-25 15:04:44 +0900

    save

--- scheduler/src/main.rs
+++ scheduler/src/main.rs
@@ -53,7 +53,7 @@ impl AddressBook {
 
         match self.map.entry(address) {
             Entry::Occupied(entry) => {
-                match &entry.get().usage.load() {
+                match &entry.get().usage.load(std::atomics::Ordering::Relaxed) {
                     Usage::Writable => return Err(())
                 }
             }
