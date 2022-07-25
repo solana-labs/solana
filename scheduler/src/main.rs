09f677526160d0dc8618474023aec7b16733b271
Ryo Onodera <ryoqun@gmail.com>
2022-07-25 13:05:44 +0900

    save

--- scheduler/src/main.rs
+++ scheduler/src/main.rs
@@ -22,8 +22,8 @@ impl ExecutionEnvironment {
 fn schedule(entry: Entry, bank: solana_runtime::bank::Bank) {
     let mut tx_queue = std::collections::BTreeMap::default();
 
-    for (ix, tx) in entry.transactions.iter().enumerate() {
-        let tx = bank.verify_transaction(*tx, solana_sdk::transaction::TransactionVerificationMode::FullVerification).unwrap();
+    for (ix, tx) in entry.transactions.into_iter().enumerate() {
+        let tx = bank.verify_transaction(tx, solana_sdk::transaction::TransactionVerificationMode::FullVerification).unwrap();
         let sig = tx.signature();
         let locks = tx.get_account_locks();
         //tx.foo();
