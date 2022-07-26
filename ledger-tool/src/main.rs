882ad1c6ae1137eca1275d876dc185e3f5b062fa
Ryo Onodera <ryoqun@gmail.com>
2022-07-26 17:08:52 +0900

    save

--- ledger-tool/src/main.rs
+++ ledger-tool/src/main.rs
@@ -142,7 +142,7 @@ fn output_entry(
     slot: Slot,
     entry_index: usize,
     entry: Entry,
-    to_schedule_stage: &crossbeam_channel::Sender<SanitizedTransaction>,
+    to_schedule_stage: &crossbeam_channel::Sender<Weight, SanitizedTransaction>,
 ) {
     match method {
         LedgerOutputMethod::Print => {
