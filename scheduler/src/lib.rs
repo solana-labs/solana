1dad98b61274fe620a7ba5c06fa0b8e84cde8881
Ryo Onodera <ryoqun@gmail.com>
2022-07-26 22:02:39 +0900

    save

--- scheduler/src/lib.rs
+++ scheduler/src/lib.rs
@@ -520,7 +520,7 @@ impl ScheduleStage {
                 let weighted_tx = weighted_tx.unwrap();
                 Self::register_runnable_task(weighted_tx, runnable_queue)
             }
-            send(maybe_ee.map(|| to_execute_substage).unwrap_or(&never()), maybe_ee.unwrap()) -> res => {
+            send(maybe_ee.map(|_| to_execute_substage).unwrap_or(&never()), maybe_ee.unwrap()) -> res => {
                 res.unwrap();
             }
             recv(from_execute_substage) -> processed_execution_environment => {
