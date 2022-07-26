74e604cadc8722c0ee0f146bc64b6f4a8f724d35
Ryo Onodera <ryoqun@gmail.com>
2022-07-26 22:11:35 +0900

    save

--- scheduler/src/lib.rs
+++ scheduler/src/lib.rs
@@ -511,7 +511,7 @@ impl ScheduleStage {
         to_execute_substage: &crossbeam_channel::Sender<ExecutionEnvironment>, // ideally want to stop wrapping with Option<...>...
         from_execute_substage: &crossbeam_channel::Receiver<ExecutionEnvironment>,
         to_next_stage: &crossbeam_channel::Sender<ExecutionEnvironment>, // assume unbounded
-        to_full_channel: &crossbeam_channel::Sender<()>, // assume unbounded
+        to_full_channel: &crossbeam_channel::Sender<ExecutionEnvironment>, // assume unbounded
     ) {
         use crossbeam_channel::select;
         let maybe_ee = Self::schedule_next_execution(runnable_queue, contended_queue, address_book);
@@ -544,7 +544,7 @@ impl ScheduleStage {
         to_execute_substage: crossbeam_channel::Sender<ExecutionEnvironment>, // ideally want to stop wrapping with Option<...>...
         from_execute_substage: crossbeam_channel::Receiver<ExecutionEnvironment>,
         to_next_stage: crossbeam_channel::Sender<ExecutionEnvironment>, // assume unbounded
-        to_full_channel: &crossbeam_channel::Sender<()>, // assume unbounded
+        to_full_channel: &crossbeam_channel::Sender<ExecutionEnvironment>, // assume unbounded
     ) {
         let exit = true;
         while exit {
