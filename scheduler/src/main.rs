0c967af0b8f1f2977aae7b7f6d5f10e88dde547d
Ryo Onodera <ryoqun@gmail.com>
2022-07-26 15:13:03 +0900

    save

--- scheduler/src/main.rs
+++ scheduler/src/main.rs
@@ -259,7 +259,7 @@ impl TaskQueue {
     }
 
     fn next_task_unique_weight(&self) -> Option<UniqueWeight> {
-        self.map.last_key_value().map(|e| e.0)
+        self.map.last_key_value().map(|e| e.0.clone())
     }
 
     fn pop_next_task(&mut self) -> Option<(UniqueWeight, Task)> {
