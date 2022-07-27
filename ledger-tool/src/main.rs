bc1ee3f801774a4eb91c82e118c8d15a693e58dc
Ryo Onodera <ryoqun@gmail.com>
2022-07-27 13:17:04 +0900

    save

--- ledger-tool/src/main.rs
+++ ledger-tool/src/main.rs
@@ -280,7 +280,7 @@ fn output_slot(
 
         let mut weight = 10_000_000;
         for i in 0..1000 {
-            error!("started!");
+            error!("started!: {}", i);
             for tx in txes.clone() {
                 tx_sender.send((Weight { ix: weight }, tx)).unwrap();
                 weight -= 1;
