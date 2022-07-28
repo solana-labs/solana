fea03ec2f8a6e7f94163abda3f466f62509e5a44
Ryo Onodera <ryoqun@gmail.com>
2022-07-28 21:22:46 +0900

    save

--- ledger-tool/src/main.rs
+++ ledger-tool/src/main.rs
@@ -282,6 +282,7 @@ fn output_slot(
                     for step in 0.. {
                         let ee = pre_execute_env_receiver.recv().unwrap().unwrap();
 
+                        /*
                         let mut process_message_time = Measure::start("process_message_time");
 
                         let sig = ee.task.tx.signature().to_string();
@@ -301,6 +302,7 @@ fn output_slot(
                             ("duration", duration_with_overhead, i64),
                             ("compute_units", ee.cu, i64),
                         );
+                        */
                         post_execute_env_sender.send(ee).unwrap();
                     }
                 })
