a116d4dc6a52cb1cc2b528cd48c110308a5bb45b
Andrew Fitzgerald <apfitzge@gmail.com>
2022-07-28 12:01:53 -0500

    Add MetricsReporterInterval trait and derive

--- metrics_reporter/derive/src/lib.rs
+++ metrics_reporter/derive/src/lib.rs
@@ -53,6 +53,62 @@ pub fn metrics_reporter_derive(input: TokenStream) -> TokenStream {
     TokenStream::from(expanded)
 }
 
+#[proc_macro_derive(
+    MetricsReporterInterval,
+    attributes(report_name, report_ignore, report_with, report_no_reset,)
+)]
+pub fn metrics_reporter_interval_derive(input: TokenStream) -> TokenStream {
+    let input = parse_macro_input!(input as DeriveInput);
+
+    let report_name = find_metrics_reporter_name(&input.attrs)
+        .expect("Struct must have a report_name attribute to derive MetricsReporter");
+
+    let name = &input.ident;
+    let metric_fields = get_metrics_fields(&input.data);
+
+    let metric_reports = metric_fields.into_iter().map(|field| {
+        let field_name = field.ident.as_ref().expect("Field must have a name");
+        let field_name_str = field_name.to_string();
+        let field_type = get_field_type(&field);
+        let mapped_type = get_mapped_type(&field_type);
+
+        let loader = if let Some(report_with) =
+            field.attrs.iter().find(|a| a.path.is_ident("report_with"))
+        {
+            let expression: syn::Expr = report_with.parse_args().unwrap();
+            quote! { #expression }
+        } else if field
+            .attrs
+            .iter()
+            .any(|a| a.path.is_ident("report_no_reset"))
+        {
+            get_default_loader(field_name, &field_type, &mapped_type)
+        } else {
+            get_default_loader_with_reset(field_name, &field_type, &mapped_type)
+        };
+
+        quote! {
+            (#field_name_str, #loader, #mapped_type),
+        }
+    });
+
+    let expanded = quote! {
+        impl MetricsReporterInterval for #name {
+            fn report(&mut self) {
+                // Publish datapoints
+                datapoint_info!(
+                    &#report_name,
+                    #(
+                        #metric_reports
+                    )*
+                );
+            }
+        }
+    };
+
+    TokenStream::from(expanded)
+}
+
 fn find_metrics_reporter_name(attrs: &[Attribute]) -> Option<String> {
     attrs
         .iter()
@@ -115,6 +171,31 @@ fn get_default_loader(
         "AtomicI64" | "AtomicI32" | "AtomicI16" | "AtomicI8" | "AtomicUSize" | "AtomicU64"
         | "AtomicU32" | "AtomicU16" | "AtomicU8" | "AtomicF32" | "AtomicF64" | "AtomicBool" => {
             quote! { self.#field_name.load(std::sync::atomic::Ordering::Relaxed) as #mapped_type }
+        }
+        unexpected_type => panic!("Unexpected type: {unexpected_type}"),
+    }
+}
+
+fn get_default_loader_with_reset(
+    field_name: &Ident,
+    field_type: &Ident,
+    mapped_type: &proc_macro2::TokenStream,
+) -> proc_macro2::TokenStream {
+    match field_type.to_string().as_str() {
+        "i64" | "i32" | "i16" | "i8" | "usize" | "u64" | "u32" | "u16" | "u8" => {
+            quote! { std::mem::replace(&mut self.#field_name, 0) as #mapped_type }
+        }
+        "f64" | "f32" => quote! { std::mem::replace(&mut self.#field_name, 0.0) as #mapped_type },
+        "bool" => quote! { std::mem::replace(&mut self.#field_name, false) },
+        "AtomicI64" | "AtomicI32" | "AtomicI16" | "AtomicI8" | "AtomicUSize" | "AtomicU64"
+        | "AtomicU32" | "AtomicU16" | "AtomicU8" => {
+            quote! { self.#field_name.swap(0, std::sync::atomic::Ordering::Relaxed) as #mapped_type }
+        }
+        "AtomicF32" | "AtomicF64" => {
+            quote! { self.#field_name.swap(0.0, std::sync::atomic::Ordering::Relaxed) as #mapped_type }
+        }
+        "AtomicBool" => {
+            quote! { self.#field_name.swap(false, std::sync::atomic::Ordering::Relaxed) }
         }
         unexpected_type => panic!("Unexpected type: {unexpected_type}"),
     }
--- metrics_reporter/src/lib.rs
+++ metrics_reporter/src/lib.rs
@@ -1,9 +1,14 @@
 pub use {
     solana_metrics::datapoint_info,
-    solana_metrics_reporter_derive::MetricsReporter,
+    solana_metrics_reporter_derive::{MetricsReporter, MetricsReporterInterval},
 };
 
 pub trait MetricsReporter {
     /// Reports metrics using datapoint_info!
     fn report(&self);
+}
+
+pub trait MetricsReporterInterval {
+    /// Reports metrics using datapoint_info! and resets fields
+    fn report(&mut self);
 }
