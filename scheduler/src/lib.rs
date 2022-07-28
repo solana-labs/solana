97de045e2cc9a47764f35fbd822d4a672bd397f0
Ryo Onodera <ryoqun@gmail.com>
2022-07-28 15:20:48 +0900

    save

--- scheduler/src/lib.rs
+++ scheduler/src/lib.rs
@@ -407,7 +407,7 @@ impl ScheduleStage {
                     //Some((false, weight_from_contended, task))
                     match contended_queue.entry_to_execute(weight_from_contended) {
                         Entry::Occupied(mut entry) => {
-                            entry
+                            (false, entry)
                         }
                         _ => { panic!() }
                     }
