11ad3619fd6d583fa37d9f73cf9ab2b213d32bd5
Ryo Onodera <ryoqun@gmail.com>
2022-07-28 15:29:32 +0900

    save

--- scheduler/src/lib.rs
+++ scheduler/src/lib.rs
@@ -393,7 +393,7 @@ impl ScheduleStage {
         runnable_queue: &'a mut TaskQueue,
         contended_queue: &'a mut TaskQueue,
         address_book: &mut AddressBook,
-    ) -> Option<(&'a mut TaskQueue, std::collections::btree_map::OccupiedEntry<'a, UniqueWeight, Task>)> {
+    ) -> Option<(&'a mut TaskQueue, bool, std::collections::btree_map::OccupiedEntry<'a, UniqueWeight, Task>)> {
         match (
             Self::get_weight_from_contended(address_book),
             runnable_queue.next_task_unique_weight(),
