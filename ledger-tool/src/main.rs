b120c420a85e29811cebad2ad327f4785e20b16e
Ryo Onodera <ryoqun@gmail.com>
2022-07-28 09:06:07 +0900

    save

--- ledger-tool/src/main.rs
+++ ledger-tool/src/main.rs
@@ -275,8 +275,8 @@ fn output_slot(
 
                 let mut process_message_time = Measure::start("process_message_time");
 
+                let sig = ee.task.tx.signature().to_string();
                 {
-                    let sig = ee.task.tx.signature().to_string();
                     info!("execute substage: #{} {:#?}", step, &sig);
                     std::thread::sleep(std::time::Duration::from_micros(ee.cu.try_into().unwrap()));
                 }
