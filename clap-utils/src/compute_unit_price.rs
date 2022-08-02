67324b136a66303f6e056af1fa1a6dae49b82b4f
Tao Zhu <tao@solana.com>
2022-08-02 17:13:19 -0500

    spell

--- clap-utils/src/compute_unit_price.rs
+++ clap-utils/src/compute_unit_price.rs
@@ -3,7 +3,7 @@ use {crate::ArgConstant, clap::Arg};
 pub const COMPUTE_UNIT_PRICE_ARG: ArgConstant<'static> = ArgConstant {
     name: "compute_unit_price",
     long: "--with-compute-unit-price",
-    help: "Set compute unit price to transaction, in increments of 0.000001 lamports per compute unit.",
+    help: "Set compute unit price for transaction, in increments of 0.000001 lamports per compute unit.",
 };
 
 pub fn compute_unit_price_arg<'a, 'b>() -> Arg<'a, 'b> {
