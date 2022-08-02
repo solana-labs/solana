45818a10c95feadb743fa59055343b77d7d9a43c
Ryo Onodera <ryoqun@gmail.com>
2022-08-02 17:31:16 +0900

    save

--- ledger-tool/src/main.rs
+++ ledger-tool/src/main.rs
@@ -171,10 +171,12 @@ fn output_entry(
                     let locks = sanitized_tx.get_account_locks().unwrap();
                     let writable_lock_iter = locks
                         .writable
-                        .iter();
+                        .iter()
+                        .copied();
                     let readonly_lock_iter = locks
                         .readonly
-                        .iter();
+                        .iter()
+                        .copied();
                     let locks = writable_lock_iter.chain(readonly_lock_iter).collect::<Vec<_>>();
                     to_schedule_stage.push(Box::new((sanitized_tx, locks)));
                 }
