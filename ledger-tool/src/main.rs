458b3c01e5a657bd3cdae2f2ce39f3ee859c33b1
Ryo Onodera <ryoqun@gmail.com>
2022-08-02 17:30:31 +0900

    save

--- ledger-tool/src/main.rs
+++ ledger-tool/src/main.rs
@@ -168,7 +168,7 @@ fn output_entry(
                 if !skip_voting
                     || !solana_runtime::vote_parser::is_simple_vote_transaction(&sanitized_tx)
                 {
-                    let locks = next_task.tx.get_account_locks().unwrap();
+                    let locks = sanitized_tx.get_account_locks().unwrap();
                     let writable_lock_iter = locks
                         .writable
                         .iter();
