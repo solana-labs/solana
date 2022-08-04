b39767193b15398262c63605463290dd192dd4aa
Yihau Chen <a122092487@gmail.com>
2022-08-04 19:51:01 +0800

    Revert "chore: increase timeout limit for coverage test" (#26917)
    This reverts commit b6ae6c1fe17e4b64c5051c651ca2585e4f55468c.

--- ci/buildkite-solana-private.sh
+++ ci/buildkite-solana-private.sh
@@ -139,7 +139,7 @@ all_test_steps() {
              ^ci/test-coverage.sh \
              ^scripts/coverage.sh \
       ; then
-    command_step coverage ". ci/rust-version.sh; ci/docker-run.sh \$\$rust_nightly_docker_image ci/test-coverage.sh" 120
+    command_step coverage ". ci/rust-version.sh; ci/docker-run.sh \$\$rust_nightly_docker_image ci/test-coverage.sh" 80
     wait_step
   else
     annotate --style info --context test-coverage \
