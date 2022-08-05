06b5ce3519606cdcb31252cd495e159f29004b14
Pankaj Garg <pankaj@solana.com>
2022-08-05 05:48:34 -0700

    cleanup setup_connection

--- streamer/src/nonblocking/quic.rs
+++ streamer/src/nonblocking/quic.rs
@@ -238,82 +238,45 @@ async fn setup_connection(
             stats.total_connections.fetch_add(1, Ordering::Relaxed);
             stats.total_new_connections.fetch_add(1, Ordering::Relaxed);
 
-            let remote_addr = new_connection.connection.remote_address();
-            let mut remote_pubkey = None;
-
-            let table_and_stake = {
-                let (some_pubkey, stake, t) =
-                    get_connection_stake(&connection, staked_nodes.clone())
-                        .map_or((None, 0, 0), |(pubkey, stake, t)| (Some(pubkey), stake, t));
-
-                if stake > 0 {
-                    remote_pubkey = some_pubkey;
-                    let params = NewConnectionHandlerParams {
+            let params = get_connection_stake(&new_connection.connection, staked_nodes.clone())
+                .map_or(
+                    NewConnectionHandlerParams::new_unstaked(
+                        packet_sender.clone(),
+                        max_connections_per_peer,
+                        stats.clone(),
+                    ),
+                    |(pubkey, stake, total_stake)| NewConnectionHandlerParams {
                         packet_sender,
-                        remote_pubkey,
+                        remote_pubkey: Some(pubkey),
                         stake,
                         total_stake,
                         max_connections_per_peer,
                         stats: stats.clone(),
-                    };
-                    let mut connection_table_l = staked_connection_table.lock().unwrap();
-                    if connection_table_l.total_size >= max_staked_connections {
-                        let num_pruned = connection_table_l.prune_random(stake);
-                        if num_pruned == 0 {
-                            if max_unstaked_connections > 0 {
-                                // If we couldn't prune a connection in the staked connection table, let's
-                                // put this connection in the unstaked connection table. If needed, prune a
-                                // connection from the unstaked connection table.
-                                if let Ok(()) = prune_unstaked_connections_and_add_new_connection(
-                                    new_connection,
-                                    unstaked_connection_table.lock().unwrap(),
-                                    unstaked_connection_table.clone(),
-                                    max_unstaked_connections,
-                                    &params,
-                                ) {
-                                    stats
-                                        .connection_added_from_staked_peer
-                                        .fetch_add(1, Ordering::Relaxed);
-                                } else {
-                                    stats
-                                        .connection_add_failed_on_pruning
-                                        .fetch_add(1, Ordering::Relaxed);
-                                    stats
-                                        .connection_add_failed_staked_node
-                                        .fetch_add(1, Ordering::Relaxed);
-                                }
-                            }
-                        } else {
-                            stats.num_evictions.fetch_add(num_pruned, Ordering::Relaxed);
-                            if let Ok(()) = handle_and_cache_new_connection(
-                                new_connection,
-                                connection_table_l,
-                                staked_connection_table.clone(),
-                                &params,
-                            ) {
-                                stats
-                                    .connection_added_from_staked_peer
-                                    .fetch_add(1, Ordering::Relaxed);
-                            }
-                        }
-                    } else {
-                        if let Ok(()) = handle_and_cache_new_connection(
-                            new_connection,
-                            connection_table_l,
-                            staked_connection_table.clone(),
-                            &params,
-                        ) {
-                            stats
-                                .connection_added_from_staked_peer
-                                .fetch_add(1, Ordering::Relaxed);
-                        }
+                    },
+                );
+
+            if params.stake > 0 {
+                let mut connection_table_l = staked_connection_table.lock().unwrap();
+                if connection_table_l.total_size >= max_staked_connections {
+                    let num_pruned = connection_table_l.prune_random(params.stake);
+                    stats.num_evictions.fetch_add(num_pruned, Ordering::Relaxed);
+                }
+
+                if connection_table_l.total_size < max_staked_connections {
+                    if let Ok(()) = handle_and_cache_new_connection(
+                        new_connection,
+                        connection_table_l,
+                        staked_connection_table.clone(),
+                        &params,
+                    ) {
+                        stats
+                            .connection_added_from_staked_peer
+                            .fetch_add(1, Ordering::Relaxed);
                     }
-                } else if max_unstaked_connections > 0 {
-                    let params = NewConnectionHandlerParams::new_unstaked(
-                        packet_sender.clone(),
-                        max_connections_per_peer,
-                        stats.clone(),
-                    );
+                } else {
+                    // If we couldn't prune a connection in the staked connection table, let's
+                    // put this connection in the unstaked connection table. If needed, prune a
+                    // connection from the unstaked connection table.
                     if let Ok(()) = prune_unstaked_connections_and_add_new_connection(
                         new_connection,
                         unstaked_connection_table.lock().unwrap(),
@@ -322,19 +285,32 @@ async fn setup_connection(
                         &params,
                     ) {
                         stats
-                            .connection_added_from_unstaked_peer
+                            .connection_added_from_staked_peer
                             .fetch_add(1, Ordering::Relaxed);
                     } else {
                         stats
-                            .connection_add_failed_unstaked_node
+                            .connection_add_failed_on_pruning
+                            .fetch_add(1, Ordering::Relaxed);
+                        stats
+                            .connection_add_failed_staked_node
                             .fetch_add(1, Ordering::Relaxed);
                     }
-                } else {
-                    stats
-                        .connection_add_failed_unstaked_node
-                        .fetch_add(1, Ordering::Relaxed);
                 }
-            };
+            } else if let Ok(()) = prune_unstaked_connections_and_add_new_connection(
+                new_connection,
+                unstaked_connection_table.lock().unwrap(),
+                unstaked_connection_table.clone(),
+                max_unstaked_connections,
+                &params,
+            ) {
+                stats
+                    .connection_added_from_unstaked_peer
+                    .fetch_add(1, Ordering::Relaxed);
+            } else {
+                stats
+                    .connection_add_failed_unstaked_node
+                    .fetch_add(1, Ordering::Relaxed);
+            }
         } else {
             stats.connection_setup_error.fetch_add(1, Ordering::Relaxed);
         }
