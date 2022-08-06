83ba73fdc172c3646c78e1e778165cc9684f6adb
Ryo Onodera <ryoqun@gmail.com>
2022-08-06 22:14:22 +0900

    save

--- scheduler/src/lib.rs
+++ scheduler/src/lib.rs
@@ -136,7 +136,7 @@ impl AddressBook {
                 *is_success = true;
             }
             AddressMapEntry::Occupied(mut book_entry) => {
-                let mut page = &*book_entry.get_mut();
+                let mut page = book_entry.get_mut().get_mut();
 
                 match &mut page.current_usage {
                     CurrentUsage::Unused => {
