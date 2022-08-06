cf58640937d740951fb9c356e9043f630dc35d1f
Christian Kamm <mail@ckamm.de>
2022-08-06 19:54:38 +0200

    Keypair: implement clone() (#26248)
    * Keypair: implement clone()
    
    This was not implemented upstream in ed25519-dalek to force everyone to
    think twice before creating another copy of a potentially sensitive
    private key in memory.
    
    See https://github.com/dalek-cryptography/ed25519-dalek/issues/76
    
    However, there are now 9 instances of
      Keypair::from_bytes(&keypair.to_bytes())
    in the solana codebase and it would be preferable to have a function.
    
    In particular since this also comes up when writing programs and can
    cause users to either start messing with lifetimes or discover the
    from_bytes() workaround themselves.
    
    This patch opts to not implement the Clone trait. This avoids automatic
    use in order to preserve some of the original "let developers think
    twice about this" intention.
    
    * Use Keypair::clone

--- core/src/replay_stage.rs
+++ core/src/replay_stage.rs
@@ -3522,7 +3522,7 @@ pub(crate) mod tests {
             hash::{hash, Hash},
             instruction::InstructionError,
             poh_config::PohConfig,
-            signature::{Keypair, Signer},
+            signature::{Keypair, KeypairInsecureClone, Signer},
             system_transaction,
             transaction::TransactionError,
         },
@@ -3602,7 +3602,7 @@ pub(crate) mod tests {
         let my_pubkey = my_keypairs.node_keypair.pubkey();
         let cluster_info = ClusterInfo::new(
             Node::new_localhost_with_pubkey(&my_pubkey).info,
-            Arc::new(Keypair::from_bytes(&my_keypairs.node_keypair.to_bytes()).unwrap()),
+            Arc::new(my_keypairs.node_keypair.clone()),
             SocketAddrSpace::Unspecified,
         );
         assert_eq!(my_pubkey, cluster_info.id());
