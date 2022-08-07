f85f24f720f18c3ec22710d7f90546e09c04d8e8
Ryo Onodera <ryoqun@gmail.com>
2022-08-07 21:44:13 +0900

    save

--- scheduler/src/lib.rs
+++ scheduler/src/lib.rs
@@ -567,7 +567,7 @@ impl ScheduleStage {
         for mut l in lock_attempts {
             let newly_uncontended_while_queued = address_book.unlock(&mut l);
             if newly_uncontended_while_queued {
-                address_book.newly_uncontended_addresses.insert(*(l.status.address2()), MyRc::clone(l.status.page_rc()));
+                address_book.newly_uncontended_addresses.insert(ByAddress(MyRc::clone(l.status.page_rc())));
             }
 
             // todo: mem::forget and panic in LockAttempt::drop()
