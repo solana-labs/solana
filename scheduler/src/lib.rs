1523a693e110f716907f919ee8e866af3a584516
Ryo Onodera <ryoqun@gmail.com>
2022-08-08 09:43:54 +0900

    save

--- scheduler/src/lib.rs
+++ scheduler/src/lib.rs
@@ -178,8 +178,8 @@ impl AddressBook {
                     AddressMapEntry::Occupied(mut book_entry) => {
                         let page = book_entry.get_mut();
                         let cloned_page = MyRc::clone(&page);
-                        dbg!(&(page, cloned_page));
-                        //assert_eq!(&*page, &cloned_page);
+                        dbg!((&page, &cloned_page));
+                        assert_eq!(&*page, &cloned_page);
                         *status = LockAttemptStatus::AfterLookup(cloned_page);
                         let mut page = unsafe { MyRcInner::get_mut_unchecked(&mut page.0) };
 
