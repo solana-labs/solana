53a3d6a4dafb3d9d7c8bc96188a8728120c4a99c
Brian Anderson <andersrb@gmail.com>
2022-08-08 17:22:10 -0500

    Add API docs for secp256k1_instruction and secp256k1_recover (#26065)
    * Add API docs for secp256k1_instruction and secp256k1_recover
    
    * typo
    
    * Remove unused variable from secp256k1 program test
    
    * Bump solana_bpf_rust_secp256k1_recover ix count
    
    Co-authored-by: Tyera Eulberg <tyera@solana.com>
    (cherry picked from commit ebe25fdb561f31488450c9a49501345d0831058f)
    
    # Conflicts:
    #	programs/bpf/Cargo.lock
    #	programs/bpf/rust/secp256k1_recover/Cargo.toml
    #	programs/bpf/tests/programs.rs

--- programs/bpf/tests/programs.rs
+++ programs/bpf/tests/programs.rs
@@ -1651,10 +1651,17 @@ fn assert_instruction_count() {
             ("solana_bpf_rust_membuiltins", 1539),
             ("solana_bpf_rust_noop", 326),
             ("solana_bpf_rust_param_passing", 146),
+<<<<<<< HEAD
             ("solana_bpf_rust_rand", 429),
             ("solana_bpf_rust_sanity", 52290),
             ("solana_bpf_rust_secp256k1_recover", 25707),
             ("solana_bpf_rust_sha", 25265),
+=======
+            ("solana_bpf_rust_rand", 469),
+            ("solana_bpf_rust_sanity", 52054),
+            ("solana_bpf_rust_secp256k1_recover", 91195),
+            ("solana_bpf_rust_sha", 24081),
+>>>>>>> ebe25fdb5 (Add API docs for secp256k1_instruction and secp256k1_recover (#26065))
         ]);
     }
 
