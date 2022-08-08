4683a391834d3a77b49474762ec0aa2ec0bd0123
Ryo Onodera <ryoqun@gmail.com>
2022-08-08 10:13:02 +0900

    save

--- scheduler/src/lib.rs
+++ scheduler/src/lib.rs
@@ -146,7 +146,7 @@ impl Page {
 
 type AddressMap = std::collections::HashMap<Pubkey, PageRc>;
 use by_address::ByAddress;
-type AddressSet = std::collections::HashSet<ByAddress<MyRcInner<Page>>>;
+type AddressSet = std::collections::HashSet<PageRc>;
 type AddressMapEntry<'a, K, V> = std::collections::hash_map::Entry<'a, K, V>;
 
 // needs ttl mechanism and prune
