e53eb8f6ef2be7b37045a8aff88473c7ebd8e558
Ryo Onodera <ryoqun@gmail.com>
2022-08-08 10:47:49 +0900

    save

--- ledger-tool/src/main.rs
+++ ledger-tool/src/main.rs
@@ -173,11 +173,11 @@ fn output_entry(
                     let writable_lock_iter = locks
                         .writable
                         .iter()
-                        .map(|address| LockAttempt::new(**address, RequestedUsage::Writable));
+                        .map(|address| LockAttempt::new(preloader.load(**address), RequestedUsage::Writable));
                     let readonly_lock_iter = locks
                         .readonly
                         .iter()
-                        .map(|address| LockAttempt::new(**address, RequestedUsage::Readonly));
+                        .map(|address| LockAttempt::new(preloader.load(**address), RequestedUsage::Readonly));
                     let locks = writable_lock_iter.chain(readonly_lock_iter).collect::<Vec<_>>();
                     to_schedule_stage.push(Box::new((sanitized_tx, locks)));
                 }
