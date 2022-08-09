6d560b9b57338dd53b398c9da707ae0544848f32
Ryo Onodera <ryoqun@gmail.com>
2022-08-09 19:21:04 +0900

    save

--- scheduler/src/lib.rs
+++ scheduler/src/lib.rs
@@ -594,9 +594,9 @@ impl ScheduleStage {
     ) {
         for l in lock_attempts {
             address_book.ensure_unlock(l);
-            if let Some(uw) = l.target.page().contended_unique_weights.last() {
-                address_book.newly_uncontended_addresses.remove(uw);
-            }
+            //if let Some(uw) = l.target.page().contended_unique_weights.last() {
+            //    address_book.newly_uncontended_addresses.remove(uw);
+            //}
 
             // todo: mem::forget and panic in LockAttempt::drop()
         }
