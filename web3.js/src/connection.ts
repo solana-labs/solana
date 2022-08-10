9ab52e426072577774cdedc0221d3ca87e3c7d9f
Justin Starry <justin@solana.com>
2022-08-10 21:35:30 +0100

    chore: annotate more types as deprecated (#27067)

--- web3.js/src/connection.ts
+++ web3.js/src/connection.ts
@@ -884,6 +884,8 @@ export type TransactionResponse = {
 
 /**
  * A confirmed transaction on the ledger
+ *
+ * @deprecated Deprecated since Solana v1.8.0.
  */
 export type ConfirmedTransaction = {
   /** The slot during which the transaction was processed */
@@ -1013,7 +1015,9 @@ export type BlockResponse = {
 };
 
 /**
- * A ConfirmedBlock on the ledger
+ * A confirmed block on the ledger
+ *
+ * @deprecated Deprecated since Solana v1.8.0.
  */
 export type ConfirmedBlock = {
   /** Blockhash of this block */
