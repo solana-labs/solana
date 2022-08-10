c253576d063df7084d21e3e25a95dbaa26d88d9e
Jeff Biseda <jbiseda@gmail.com>
2022-08-09 21:39:55 -0700

    ancestor hashes socket ping/pong support (#26866)
    (cherry picked from commit 370de8129eb22c5e111a8102e5789fcc0b983ca1)
    
    # Conflicts:
    #	core/src/serve_repair.rs
    #	sdk/src/feature_set.rs

--- sdk/src/feature_set.rs
+++ sdk/src/feature_set.rs
@@ -415,6 +415,25 @@ pub mod sign_repair_requests {
     solana_sdk::declare_id!("sigrs6u1EWeHuoKFkY8RR7qcSsPmrAeBBPESyf5pnYe");
 }
 
+<<<<<<< HEAD
+=======
+pub mod concurrent_replay_of_forks {
+    solana_sdk::declare_id!("9F2Dcu8xkBPKxiiy65XKPZYdCG3VZDpjDTuSmeYLozJe");
+}
+
+pub mod check_ping_ancestor_requests {
+    solana_sdk::declare_id!("AXLB87anNaUQtqBSsxkm4gvNzYY985aLtNtpJC94uWLJ");
+}
+
+pub mod incremental_snapshot_only_incremental_hash_calculation {
+    solana_sdk::declare_id!("25vqsfjk7Nv1prsQJmA4Xu1bN61s8LXCBGUPp8Rfy1UF");
+}
+
+pub mod disable_cpi_setting_executable_and_rent_epoch {
+    solana_sdk::declare_id!("B9cdB55u4jQsDNsdTK525yE9dmSc5Ga7YBaBrDFvEhM9");
+}
+
+>>>>>>> 370de8129 (ancestor hashes socket ping/pong support (#26866))
 lazy_static! {
     /// Map of feature identifiers to user-visible description
     pub static ref FEATURE_NAMES: HashMap<Pubkey, &'static str> = [
@@ -513,6 +532,13 @@ lazy_static! {
         (preserve_rent_epoch_for_rent_exempt_accounts::id(), "preserve rent epoch for rent exempt accounts #26479"),
         (prevent_crediting_accounts_that_end_rent_paying::id(), "prevent crediting rent paying accounts #26606"),
         (sign_repair_requests::id(), "sign repair requests #26834"),
+<<<<<<< HEAD
+=======
+        (concurrent_replay_of_forks::id(), "Allow slots from different forks to be replayed concurrently #26465"),
+        (check_ping_ancestor_requests::id(), "ancestor hash repair socket ping/pong support #26963"),
+        (incremental_snapshot_only_incremental_hash_calculation::id(), "only hash accounts in incremental snapshot during incremental snapshot creation #26799"),
+        (disable_cpi_setting_executable_and_rent_epoch::id(), "disable setting is_executable and_rent_epoch in CPI #26987"),
+>>>>>>> 370de8129 (ancestor hashes socket ping/pong support (#26866))
         /*************** ADD NEW FEATURES HERE ***************/
     ]
     .iter()
