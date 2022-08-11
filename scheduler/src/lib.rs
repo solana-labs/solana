8c7bf46c21bfab8b7bf7a45ecf83a83bdac35253
Ryo Onodera <ryoqun@gmail.com>
2022-08-11 16:14:08 +0900

    save

--- scheduler/src/lib.rs
+++ scheduler/src/lib.rs
@@ -745,7 +745,7 @@ impl ScheduleStage {
                     *count -= 1;
                     if *count == 0 {
                         address_book.guaranteed_lock_counts.remove(&task_id);
-                        address_book.runnable_guaranteed_task_ids.insert(task_id);
+                        address_book.runnable_guaranteed_task_ids.insert(task_id, ());
                     }
                 }
             }
