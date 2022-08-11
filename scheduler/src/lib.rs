84088222753f9b07f5cdbecb869aabbd51646b9e
Ryo Onodera <ryoqun@gmail.com>
2022-08-11 22:45:20 +0900

    save

--- scheduler/src/lib.rs
+++ scheduler/src/lib.rs
@@ -749,7 +749,7 @@ impl ScheduleStage {
                 page.switch_to_next_usage();
                 for task_id in std::mem::take(&mut page.guaranteed_task_ids).keys() {
                     match address_book.guaranteed_lock_counts.entry(*task_id) {
-                        std::collections::hash_map::Entry::Occupied(entry) => {
+                        std::collections::hash_map::Entry::Occupied(mut entry) => {
                             let count = entry.get_mut();
                             trace!("guaranteed lock decrease: {} => {}", *count, *count -1);
                             *count -= 1;
