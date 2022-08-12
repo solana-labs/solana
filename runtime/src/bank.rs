35c87c38884672b9de5f28f0185d0adfaa8c1e05
Brooks Prumo <brooks@solana.com>
2022-08-12 13:06:54 -0400

    Fix test_accounts_data_size_and_resize_transactions (#27105)

--- runtime/src/bank.rs
+++ runtime/src/bank.rs
@@ -19333,7 +19333,10 @@ pub(crate) mod tests {
         {
             let account_pubkey = Pubkey::new_unique();
             let account_balance = LAMPORTS_PER_SOL;
-            let account_size = rng.gen_range(1, MAX_PERMITTED_DATA_LENGTH) as usize;
+            let account_size = rng.gen_range(
+                1,
+                MAX_PERMITTED_DATA_LENGTH as usize - MAX_PERMITTED_DATA_INCREASE,
+            );
             let account_data =
                 AccountSharedData::new(account_balance, account_size, &mock_program_id);
             bank.store_account(&account_pubkey, &account_data);
