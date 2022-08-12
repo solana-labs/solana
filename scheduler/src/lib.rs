0947c1fe439e8775509a1c742f1c7dac6583a51e
Ryo Onodera <ryoqun@gmail.com>
2022-08-12 22:54:34 +0900

    save

--- scheduler/src/lib.rs
+++ scheduler/src/lib.rs
@@ -665,7 +665,8 @@ impl ScheduleStage {
                 );
                 std::mem::swap(&mut next_task.tx.1, &mut populated_lock_attempts);
 
-                return None;
+                //return None;
+                continue;
             }
 
             trace!("successful lock: (from_runnable: {}) after {} contentions", from_runnable, next_task.contention_count);
