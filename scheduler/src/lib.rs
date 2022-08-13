05511d4b11aac3bbf0b3cb87a6456c735082f774
Ryo Onodera <ryoqun@gmail.com>
2022-08-13 22:10:26 +0900

    save

--- scheduler/src/lib.rs
+++ scheduler/src/lib.rs
@@ -510,7 +510,7 @@ impl ScheduleStage {
         //    )
         //    .unwrap();
         //tx.foo();
-        let unique_weight = (weight.ix << 32) | (*unique_key & 0x0000_0000_ffff_ffff);
+        let unique_weight = (weight << 32) | (*unique_key & 0x0000_0000_ffff_ffff);
 
         runnable_queue.add_to_schedule(
             /*
