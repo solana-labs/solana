07a5caaea5a106ea5e37f8159b2a0f1ff7fae235
Ryo Onodera <ryoqun@gmail.com>
2022-08-14 21:48:09 +0900

    save

--- transaction-scheduler-bench/src/main.rs
+++ transaction-scheduler-bench/src/main.rs
@@ -340,10 +340,10 @@ fn handle_transaction_batch(
         .priority_collected
         .fetch_add(priority_collected, Ordering::Relaxed);
 
-    dbg!("send: {:?}", transaction_batch.unique_weight);
     completed_transaction_sender
         .send(solana_scheduler::Multiplexed::FromExecute(transaction_batch))
         .unwrap();
+    dbg!("send: {:?}", transaction_batch.unique_weight);
 }
 
 const NUM_SENDERS: usize = 2;
