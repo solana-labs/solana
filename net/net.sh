4153f67f3f8a5620fb1e578afb37e1be56be7f70
greg <gregory.cusack@colorado.edu>
2022-08-15 11:07:46 -0500

    incr time between same node deploy instances by 2. from 3 to 5

--- net/net.sh
+++ net/net.sh
@@ -624,7 +624,7 @@ threadGossipDeploy() {
     echo "startGossipNode: instanceIndex: $i"
     startNode "$ipAddress" $nodeType $nodeIndex $i
     echo "################# Launched Gossip Node: $i ###################"
-    sleep 3
+    sleep 5
   done
 }
 
