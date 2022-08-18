bd465b2370f2864a1e0b32a4712d8febc1d1260d
Tao Zhu <tao@solana.com>
2022-08-18 09:31:50 -0500

    Remove unnecessary and stale comment

--- runtime/src/block_cost_limits.rs
+++ runtime/src/block_cost_limits.rs
@@ -39,7 +39,6 @@ lazy_static! {
         (solana_sdk::stake::program::id(), COMPUTE_UNIT_TO_US_RATIO * 25),
         (solana_config_program::id(), COMPUTE_UNIT_TO_US_RATIO * 15),
         (solana_vote_program::id(), COMPUTE_UNIT_TO_US_RATIO * 70),
-        // secp256k1 is executed in banking stage, it should cost similar to sigverify
         (secp256k1_program::id(), COMPUTE_UNIT_TO_US_RATIO * 24),
         (ed25519_program::id(), COMPUTE_UNIT_TO_US_RATIO * 24),
         (system_program::id(), COMPUTE_UNIT_TO_US_RATIO * 5),
