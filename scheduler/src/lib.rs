350540fea7f67c090afe72b48bf913add418a38d
Ryo Onodera <ryoqun@gmail.com>
2022-08-20 14:21:43 +0900

    save

--- scheduler/src/lib.rs
+++ scheduler/src/lib.rs
@@ -153,9 +153,9 @@ impl TaskIds {
     }
 
     #[inline(never)]
-    fn last(&self) -> Option<&UniqueWeight> {
+    fn last(&self) -> Option<UniqueWeight> {
         //self.task_ids.last()
-        self.task_ids.back().map(|e| e.value())
+        self.task_ids.back().map(|e| *(e.value()))
         //self.cached_heaviest.as_ref()
     }
 }
