f0fccf83e1534f532d910b255529a7ada76c70c9
Ryo Onodera <ryoqun@gmail.com>
2022-08-20 15:12:13 +0900

    save

--- scheduler/src/lib.rs
+++ scheduler/src/lib.rs
@@ -139,7 +139,7 @@ impl TaskIds {
     }
 
     #[inline(never)]
-    pub fn remove(&mut self, u: &UniqueWeight) {
+    pub fn remove(&self, u: &UniqueWeight) {
         let a = self.task_ids.remove(u);
         /*
         match self.cached_heaviest {
