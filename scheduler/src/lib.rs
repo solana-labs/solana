2ba70e749ab1e00312bf25744eab1c84cbacacd2
Ryo Onodera <ryoqun@gmail.com>
2022-08-20 14:21:03 +0900

    save

--- scheduler/src/lib.rs
+++ scheduler/src/lib.rs
@@ -117,17 +117,19 @@ pub enum RequestedUsage {
 pub struct TaskIds {
     //task_ids: std::collections::BTreeSet<UniqueWeight>,
     task_ids: crossbeam_skiplist::SkipSet<UniqueWeight>,
-    cached_heaviest: Option<UniqueWeight>,
+    //cached_heaviest: Option<UniqueWeight>,
 }
 
 impl TaskIds {
     #[inline(never)]
     fn insert(&mut self, u: UniqueWeight) {
+        /*
         match self.cached_heaviest {
             Some(c) if u > c => { self.cached_heaviest = Some(u) },
             None => { self.cached_heaviest = Some(u); }
             _ => {},
         }
+        */
              
         self.task_ids.insert(u);
     }
@@ -135,11 +137,13 @@ impl TaskIds {
     #[inline(never)]
     fn remove(&mut self, u: &UniqueWeight) {
         let a = self.task_ids.remove(u);
+        /*
         match self.cached_heaviest {
             //Some(ref c) if u == c => { self.cached_heaviest = self.task_ids.last().copied() },
             Some(ref c) if u == c => { self.cached_heaviest = self.task_ids.back().map(|e| *(e.value())) },
             _ => {},
         }
+        */
         a;
     }
 
@@ -149,9 +153,10 @@ impl TaskIds {
     }
 
     #[inline(never)]
-    fn last(&self) -> Option<&UniqueWeight> {
+    fn last(&self) -> Option<UniqueWeight> {
         //self.task_ids.last()
-        self.cached_heaviest.as_ref()
+        self.task_ids.back().map(|e| *(e.value()))
+        //self.cached_heaviest.as_ref()
     }
 }
 
