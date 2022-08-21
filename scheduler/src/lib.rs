082995b62479573d3a71ccfee999ee1d007edf61
Ryo Onodera <ryoqun@gmail.com>
2022-08-22 08:53:31 +0900

    save

--- scheduler/src/lib.rs
+++ scheduler/src/lib.rs
@@ -659,7 +659,7 @@ impl ScheduleStage {
             trace!("pop loop iteration");
             let from_runnable = reborrowed_contended_queue.is_some();
             let unique_weight = *queue_entry.key();
-            let next_task = queue_entry.get_mut();
+            let mut next_task = queue_entry.get_mut();
             let next_task = unsafe { TaskInQueue::get_mut_unchecked(&mut next_task) };
             let message_hash = next_task.tx.0.message_hash();
 
