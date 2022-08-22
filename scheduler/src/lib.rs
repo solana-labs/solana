73197f9fda2a9af16a0447f8d85bd9028c92cb51
Ryo Onodera <ryoqun@gmail.com>
2022-08-22 16:58:07 +0900

    save

--- scheduler/src/lib.rs
+++ scheduler/src/lib.rs
@@ -840,7 +840,7 @@ impl ScheduleStage {
             }
             if page.current_usage == Usage::Unused && page.next_usage != Usage::Unused {
                 page.switch_to_next_usage();
-                for task_id in std::mem::take(&mut page.provisional_task_ids).keys() {
+                for task_id in std::mem::take(&mut page.provisional_task_ids).keys().into_iter() {
                     match address_book.provisioning_trackers.entry(*task_id) {
                         std::collections::hash_map::Entry::Occupied(mut tracker_entry) => {
                             let tracker = tracker_entry.get_mut();
