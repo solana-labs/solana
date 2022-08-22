2e0f237a776fd47d76a9bcfa5d9389ed79d93db1
apfitzge <apfitzge@users.noreply.github.com>
2022-08-22 11:15:48 -0400

    slots_per_epoch shouldn't be optional (#27256)
    (cherry picked from commit 9d5029da15f8ed32aa4cafc746905f80e4545b10)
    
    # Conflicts:
    #	runtime/src/accounts_db.rs

--- runtime/src/accounts_db.rs
+++ runtime/src/accounts_db.rs
@@ -6646,20 +6646,18 @@ impl AccountsDb {
     }
 
     /// storages are sorted by slot and have range info.
-    /// if we know slots_per_epoch, then add all stores older than slots_per_epoch to dirty_stores so clean visits these slots
-    fn mark_old_slots_as_dirty(&self, storages: &SortedStorages, slots_per_epoch: Option<Slot>) {
-        if let Some(slots_per_epoch) = slots_per_epoch {
-            let max = storages.max_slot_inclusive();
-            let acceptable_straggler_slot_count = 100; // do nothing special for these old stores which will likely get cleaned up shortly
-            let sub = slots_per_epoch + acceptable_straggler_slot_count;
-            let in_epoch_range_start = max.saturating_sub(sub);
-            for (slot, storages) in storages.iter_range(..in_epoch_range_start) {
-                if let Some(storages) = storages {
-                    storages.iter().for_each(|store| {
-                        self.dirty_stores
-                            .insert((slot, store.append_vec_id()), store.clone());
-                    });
-                }
+    /// add all stores older than slots_per_epoch to dirty_stores so clean visits these slots
+    fn mark_old_slots_as_dirty(&self, storages: &SortedStorages, slots_per_epoch: Slot) {
+        let max = storages.max_slot_inclusive();
+        let acceptable_straggler_slot_count = 100; // do nothing special for these old stores which will likely get cleaned up shortly
+        let sub = slots_per_epoch + acceptable_straggler_slot_count;
+        let in_epoch_range_start = max.saturating_sub(sub);
+        for (slot, storages) in storages.iter_range(..in_epoch_range_start) {
+            if let Some(storages) = storages {
+                storages.iter().for_each(|store| {
+                    self.dirty_stores
+                        .insert((slot, store.append_vec_id()), store.clone());
+                });
             }
         }
     }
@@ -6888,7 +6886,16 @@ impl AccountsDb {
 
         stats.oldest_root = storages.range().start;
 
+<<<<<<< HEAD
         self.mark_old_slots_as_dirty(storages, Some(config.epoch_schedule.slots_per_epoch));
+=======
+        assert!(
+            !(config.store_detailed_debug_info_on_failure && config.use_write_cache),
+            "cannot accurately capture all data for debugging if accounts cache is being used"
+        );
+
+        self.mark_old_slots_as_dirty(storages, config.epoch_schedule.slots_per_epoch);
+>>>>>>> 9d5029da1 (slots_per_epoch shouldn't be optional (#27256))
 
         let (num_hash_scan_passes, bins_per_pass) = Self::bins_per_pass(self.num_hash_scan_passes);
         let use_bg_thread_pool = config.use_bg_thread_pool;
