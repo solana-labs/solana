50865f85e4b50d5748633cd4051f46d081437848
Ryo Onodera <ryoqun@gmail.com>
2022-08-23 10:45:36 +0900

    save

--- scheduler/src/lib.rs
+++ scheduler/src/lib.rs
@@ -527,10 +527,10 @@ impl TaskQueue {
         &mut self,
     ) -> Option<TaskQueueOccupiedEntry<'_>> {
         //panic!()//self.tasks.last_entry()
-        let k = self.tasks.get_max().map(|(k, _v)| *k);
+        //let k = self.tasks.get_max().map(|(k, _v)| *k);
         //panic!();
         //let k = self.tasks.iter().next().map(|(k, _v)| *k);
-        //let k = self.tasks.iter().next().map(|r| *r.key());
+        let k = self.tasks.iter().next().map(|r| *r.key());
 
         k.map(|k| self.entry_to_execute(k).unwrap())
     }
