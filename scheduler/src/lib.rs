0e1917cc71eb850c33dd38dcff82569ea87d9df2
Ryo Onodera <ryoqun@gmail.com>
2022-08-23 22:26:34 +0900

    save

--- scheduler/src/lib.rs
+++ scheduler/src/lib.rs
@@ -444,7 +444,7 @@ impl Task {
             tx: Box::new((self.tx.0.clone(), self.tx.1.iter().map(|l| l.clone_for_test()).collect::<Vec<_>>())),
             contention_count: Default::default(),
             uncontended: Default::default(),
-            queue_time: queue_time: std::sync::atomic::AtomicUsize::new(usize::max_value()),
+            queue_time: std::sync::atomic::AtomicUsize::new(usize::max_value()),
         }
     }
 
