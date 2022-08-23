5ea12264d9992e72d172961f25879c57a6d6a1a1
Ryo Onodera <ryoqun@gmail.com>
2022-08-23 12:37:34 +0900

    save

--- scheduler/src/lib.rs
+++ scheduler/src/lib.rs
@@ -480,7 +480,7 @@ impl Task {
     pub fn clone_for_test(&self) {
         Self {
             unique_weight: self.unique_weight,
-            tx: Box::new(self.tx.0.clone(), self.tx.1.clone_for_test()),
+            tx: Box::new(self.tx.0.clone(), self.tx.1.iter(|l| l.clone_for_test()).collect::<Vec<_>>()),
             contention_count: Default:default(),
             uncontended: Default::default(),
         }
