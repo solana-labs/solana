ca461bf93cccb33bcb43c85d4e4089ddb9eca26e
Ryo Onodera <ryoqun@gmail.com>
2022-08-23 10:58:33 +0900

    save

--- scheduler/src/lib.rs
+++ scheduler/src/lib.rs
@@ -951,7 +951,7 @@ impl ScheduleStage {
 
     #[inline(never)]
     fn register_runnable_task(
-        weighted_tx: (Weight, std::sync::Arc<(SanitizedTransaction, Vec<LockAttempt>)>),
+        weighted_tx: (Weight, TaskInQueue)>),
         runnable_queue: &mut TaskQueue,
         unique_key: &mut u64,
     ) {
