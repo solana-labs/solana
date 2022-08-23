b84cd60eceb83663717b9e2c68bebe6050153b63
Ryo Onodera <ryoqun@gmail.com>
2022-08-23 09:58:55 +0900

    save

--- scheduler/src/lib.rs
+++ scheduler/src/lib.rs
@@ -498,7 +498,11 @@ impl TaskQueue {
 
     #[inline(never)]
     pub fn has_task(&self, unique_weight: &UniqueWeight) -> bool {
-        self.tasks.contains_key(unique_weight)
+        let queue_entry = self.tasks.entry(unique_weight);
+        match queue_entry {
+            TaskQueueEntry::Occupied(queue_entry) => queue_entry.value().uncontended.load(std::sync::atomic::Ordering::SeqCst),
+            TaskQueueEntry::Vacant(_queue_entry) => false,
+        }
     }
 
     #[inline(never)]
