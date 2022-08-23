ec0a89b4bd02b6c8c08dd20251e47833f57ad387
Ryo Onodera <ryoqun@gmail.com>
2022-08-23 11:27:00 +0900

    save

--- transaction-scheduler-bench/src/main.rs
+++ transaction-scheduler-bench/src/main.rs
@@ -472,7 +472,7 @@ fn send_packets(
                 let p = solana_scheduler::get_transaction_priority_details(&v.0);
                 let p = (p << 32) | (rng.gen::<u64>() & 0x0000_0000_ffff_ffff);
                 let t = solana_scheduler::Task::new_for_queue(p, v);
-                for lock_attempt in v.1.iter() {
+                for lock_attempt in t.tx.1.iter() {
                     lock_attempt.target.page_ref().contended_unique_weights.insert_task_id(p, solana_scheduler::TaskInQueue::clone(&t));
                 }
                 packet_batch_sender.send(solana_scheduler::Multiplexed::FromPrevious((p, t))).unwrap();
