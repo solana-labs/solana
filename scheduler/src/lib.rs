849ce1f6234a418910e8a364dbcb61e693b50567
Ryo Onodera <ryoqun@gmail.com>
2022-08-23 12:38:18 +0900

    save

--- scheduler/src/lib.rs
+++ scheduler/src/lib.rs
@@ -481,7 +481,7 @@ impl Task {
         Self {
             unique_weight: self.unique_weight,
             tx: Box::new((self.tx.0.clone(), self.tx.1.iter().map(|l| l.clone_for_test()).collect::<Vec<_>>())),
-            contention_count: Default:default(),
+            contention_count: Default::default(),
             uncontended: Default::default(),
         }
     }
