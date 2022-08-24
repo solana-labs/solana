f2142b29296be9eb84811b39e2aadb9377f20954
Ryo Onodera <ryoqun@gmail.com>
2022-08-24 22:24:36 +0900

    save

--- transaction-scheduler-bench/src/main.rs
+++ transaction-scheduler-bench/src/main.rs
@@ -357,16 +357,19 @@ fn handle_transaction_batch(
     //transaction_batch.task.trace_timestamps("in_exec(self)");
     for mut lock_attempt in transaction_batch.lock_attempts.iter_mut() {
         let contended_unique_weights = lock_attempt.contended_unique_weights();
-        contended_unique_weights.remove_task(&uq);
         contended_unique_weights.heaviest_task_cursor().map(|mut task_cursor| {
             let mut found = true;
+            let mut removed = false;
             assert_ne!(task_cursor.key(), &uq);
             let mut task = task_cursor.value();
             //task.trace_timestamps("in_exec(initial list)");
             while !task.currently_contended() {
+                if task_cursor.key() == &uq {
+                    task_cursor.remove();
+                    removed = true;
+                }
                 if let Some(new_cursor) = task_cursor.prev() {
                     assert!(new_cursor.key() < task_cursor.key());
-                    assert_ne!(new_cursor.key(), &uq);
                     task_cursor = new_cursor;
                     task = task_cursor.value();
                     //task.trace_timestamps("in_exec(subsequent list)");
@@ -374,6 +377,9 @@ fn handle_transaction_batch(
                     found = false;
                     break;
                 }
+            }
+            if !removed {
+                contended_unique_weights.remove_task(&uq);
             }
             found.then(|| solana_scheduler::TaskInQueue::clone(task))
         }).flatten().map(|task| {
