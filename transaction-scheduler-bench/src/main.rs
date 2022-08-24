8ba4b7e97a401d51b07bcc8ce3bca0ea16924a89
Ryo Onodera <ryoqun@gmail.com>
2022-08-24 13:54:05 +0900

    save

--- transaction-scheduler-bench/src/main.rs
+++ transaction-scheduler-bench/src/main.rs
@@ -360,10 +360,10 @@ fn handle_transaction_batch(
                 }
             }
             found.then(|| solana_scheduler::TaskInQueue::clone(task_cursor.value()))
-        }).flatten().map(|task| {
+        }).flatten().then(|task| {
             lock_attempt.heaviest_uncontended = Some(task);
             ()
-        })
+        });
     }
     completed_transaction_sender.0
         .send(transaction_batch)
