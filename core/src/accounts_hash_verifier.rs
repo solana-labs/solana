46b4d933560d81f7558f6ca05d5d612712754ae5
Jeff Washington (jwash) <wash678@gmail.com>
2022-08-24 10:19:24 -0500

    add text to assert

--- core/src/accounts_hash_verifier.rs
+++ core/src/accounts_hash_verifier.rs
@@ -192,7 +192,10 @@ impl AccountsHashVerifier {
                 );
         }
 
-        assert_eq!(accounts_package.expected_capitalization, lamports);
+        assert_eq!(
+            accounts_package.expected_capitalization, lamports,
+            "accounts hash capitalization mismatch"
+        );
         if let Some(expected_hash) = accounts_package.accounts_hash_for_testing {
             assert_eq!(expected_hash, accounts_hash);
         };
