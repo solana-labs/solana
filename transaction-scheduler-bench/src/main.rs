e208134caf0bd193218e99530b857fdab1c31ca9
Ryo Onodera <ryoqun@gmail.com>
2022-08-24 22:28:54 +0900

    save

--- transaction-scheduler-bench/src/main.rs
+++ transaction-scheduler-bench/src/main.rs
@@ -354,7 +354,7 @@ fn handle_transaction_batch(
         .fetch_add(priority_collected, Ordering::Relaxed);
 
     let uq = transaction_batch.unique_weight;
-    //transaction_batch.task.trace_timestamps("in_exec(self)");
+    transaction_batch.task.trace_timestamps("in_exec(self)");
     for mut lock_attempt in transaction_batch.lock_attempts.iter_mut() {
         let contended_unique_weights = lock_attempt.contended_unique_weights();
         contended_unique_weights.heaviest_task_cursor().map(|mut task_cursor| {
