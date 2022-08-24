b41e6c4b2c5b0925dd89f5f064ae64ccd4ee6a8e
Brooks Prumo <brooks@solana.com>
2022-08-24 10:19:49 -0400

    Add feature gate to cap per-transaction accounts data allocations

--- sdk/src/feature_set.rs
+++ sdk/src/feature_set.rs
@@ -510,6 +510,10 @@ pub mod vote_state_update_root_fix {
     solana_sdk::declare_id!("G74BkWBzmsByZ1kxHy44H3wjwp5hp7JbrGRuDpco22tY");
 }
 
+pub mod cap_accounts_data_allocations_per_transaction {
+    solana_sdk::declare_id!("9gxu85LYRAcZL38We8MYJ4A9AwgBBPtVBAqebMcT1241");
+}
+
 lazy_static! {
     /// Map of feature identifiers to user-visible description
     pub static ref FEATURE_NAMES: HashMap<Pubkey, &'static str> = [
@@ -632,6 +636,7 @@ lazy_static! {
         (relax_authority_signer_check_for_lookup_table_creation::id(), "relax authority signer check for lookup table creation #27205"),
         (stop_sibling_instruction_search_at_parent::id(), "stop the search in get_processed_sibling_instruction when the parent instruction is reached #27289"),
         (vote_state_update_root_fix::id(), "fix root in vote state updates #27361"),
+        (cap_accounts_data_allocations_per_transaction::id(), "cap accounts data allocations per transaction #27375"),
         /*************** ADD NEW FEATURES HERE ***************/
     ]
     .iter()
