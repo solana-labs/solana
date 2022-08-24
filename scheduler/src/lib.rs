0c2e15c23880e9645b61e198aa4d4e879a39c3a9
Ryo Onodera <ryoqun@gmail.com>
2022-08-24 22:06:42 +0900

    save

--- scheduler/src/lib.rs
+++ scheduler/src/lib.rs
@@ -1055,7 +1055,7 @@ impl ScheduleStage {
                                 }
                             }
 
-                            while from_exec.len() > 0 {
+                            while false && from_exec.len() > 0 {
                                 let mut processed_execution_environment = from_exec.recv().unwrap();
                                 trace!("recv from execute: {:?}", processed_execution_environment.unique_weight);
                                 executing_queue_count -= 1;
@@ -1080,7 +1080,7 @@ impl ScheduleStage {
                         Self::commit_completed_execution(&mut processed_execution_environment, address_book, &mut execute_clock);
                         // async-ly propagate the result to rpc subsystems
                         to_next_stage.send(processed_execution_environment).unwrap();
-                        if from_exec.len() > 0 {
+                        if false && from_exec.len() > 0 {
                             processed_execution_environment = from_exec.recv().unwrap();
                         } else {
                             break;
