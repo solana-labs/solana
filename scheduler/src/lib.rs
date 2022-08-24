b823c17b97c85647bddee682d7f805a35cfbe490
Ryo Onodera <ryoqun@gmail.com>
2022-08-24 21:20:09 +0900

    save

--- scheduler/src/lib.rs
+++ scheduler/src/lib.rs
@@ -950,7 +950,7 @@ impl ScheduleStage {
     fn commit_completed_execution(ee: &mut ExecutionEnvironment, address_book: &mut AddressBook, commit_time: &mut usize) {
         // do par()-ly?
 
-        ee.task.record_commit_time(commit_time);
+        ee.task.record_commit_time(*commit_time);
         ee.task.trace_timestamps();
         //*commit_time = commit_time.checked_add(1).unwrap();
 
