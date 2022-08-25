8c81ed0203b16e0ab10e7b0e36830006679076e9
kirill lykov <kirill.lykov@solana.com>
2022-08-25 15:44:37 +0200

    add get_multiple_accounts to BenchTpsClient (#27379)

--- bench-tps/src/bench_tps_client.rs
+++ bench-tps/src/bench_tps_client.rs
@@ -91,6 +91,8 @@ pub trait BenchTpsClient {
         pubkey: &Pubkey,
         commitment_config: CommitmentConfig,
     ) -> Result<Account>;
+
+    fn get_multiple_accounts(&self, pubkeys: &[Pubkey]) -> Result<Vec<Option<Account>>>;
 }
 
 mod bank_client;
