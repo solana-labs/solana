13a66d3f91591e78b238e3805a5ac55e91a295cc
Ryo Onodera <ryoqun@gmail.com>
2022-08-25 21:37:55 +0900

    save

--- transaction-scheduler-bench/src/main.rs
+++ transaction-scheduler-bench/src/main.rs
@@ -131,7 +131,7 @@ fn spawn_unified_scheduler(
         let mut runnable_queue = solana_scheduler::TaskQueue::default();
 
         solana_scheduler::ScheduleStage::run(
-            num_execution_threads * 2,
+            num_execution_threads * 10,
             &mut runnable_queue,
             &mut address_book,
             &packet_batch_receiver.clone(),
@@ -491,22 +491,20 @@ fn send_packets(
         );
         let mut rng = rand::thread_rng();
 
-        loop {
-            for vv in packet_batches {
-                for v in vv.clone_for_test() {
-                    let p = solana_scheduler::get_transaction_priority_details(&v.0);
-                    let p = (p << 32) | (rng.gen::<u64>() & 0x0000_0000_ffff_ffff);
-                    let t = solana_scheduler::Task::new_for_queue(p, v);
-                    for lock_attempt in t.tx.1.iter() {
-                        lock_attempt.contended_unique_weights().insert_task(p, solana_scheduler::TaskInQueue::clone(&t));
-                    }
-                    packet_batch_sender.send(solana_scheduler::Multiplexed::FromPrevious((p, t))).unwrap();
+        for vv in packet_batches {
+            for v in vv {
+                let p = solana_scheduler::get_transaction_priority_details(&v.0);
+                let p = (p << 32) | (rng.gen::<u64>() & 0x0000_0000_ffff_ffff);
+                let t = solana_scheduler::Task::new_for_queue(p, v);
+                for lock_attempt in t.tx.1.iter() {
+                    lock_attempt.contended_unique_weights().insert_task(p, solana_scheduler::TaskInQueue::clone(&t));
                 }
+                packet_batch_sender.send(solana_scheduler::Multiplexed::FromPrevious((p, t))).unwrap();
             }
-            
-
-            std::thread::sleep(loop_duration.saturating_sub(packet_build_time.as_duration()));
         }
+        
+
+        std::thread::sleep(loop_duration.saturating_sub(packet_build_time.as_duration()));
     }
 }
 
