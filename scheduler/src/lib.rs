1bbdba1d68522246fe077785d77377ec19999d2f
Ryo Onodera <ryoqun@gmail.com>
2022-08-25 22:23:44 +0900

    save

--- scheduler/src/lib.rs
+++ scheduler/src/lib.rs
@@ -970,7 +970,7 @@ impl ScheduleStage {
 
     #[inline(never)]
     fn schedule_next_execution(
-        task_sender: crossbeam_channel::Sender<TaskInQueue>,
+        task_sender: &crossbeam_channel::Sender<TaskInQueue>,
         runnable_queue: &mut TaskQueue,
         address_book: &mut AddressBook,
         contended_count: &mut usize,
