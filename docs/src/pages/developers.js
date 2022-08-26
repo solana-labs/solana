5172e580be92796b7d69c1fb1c81eebc2f27761f
nickfrosty <nfrostbutter@gmail.com>
2022-08-26 15:35:21 -0400

    fix: updated labels for translateIds

--- docs/src/pages/developers.js
+++ docs/src/pages/developers.js
@@ -20,12 +20,12 @@ function Developers() {
                   to="developing/intro/programs"
                   header={{
                     label: "Getting Started",
-                    translateId: "start-building",
+                    translateId: "developer-programs",
                   }}
                   body={{
                     label:
                       "Discover what Solana programs are and how they work.",
-                    translateId: "start-building",
+                    translateId: "learn-programs",
                   }}
                 />
 
@@ -33,12 +33,12 @@ function Developers() {
                   to="developing/programming-model/transactions"
                   header={{
                     label: "Transactions",
-                    translateId: "start-building",
+                    translateId: "developer-transactions",
                   }}
                   body={{
                     label:
                       "Program execution begins with a transaction being submitted to the cluster.",
-                    translateId: "start-building",
+                    translateId: "learn-transactions",
                   }}
                 />
 
@@ -46,12 +46,12 @@ function Developers() {
                   to="developing/programming-model/accounts"
                   header={{
                     label: "Accounts",
-                    translateId: "run-validator",
+                    translateId: "developer-accounts",
                   }}
                   body={{
                     label:
                       "Understand how programs store data or state on the Solana blockchain.",
-                    translateId: "validate-transactions",
+                    translateId: "learn-accounts",
                   }}
                 />
               </div>
@@ -78,12 +78,12 @@ function Developers() {
                   to="developing/on-chain-programs/examples#helloworld"
                   header={{
                     label: "Hello World Example",
-                    translateId: "run-validator",
+                    translateId: "developer-hello-world",
                   }}
                   body={{
                     label:
                       "Example of how to use code to interact with the Solana blockchain.",
-                    translateId: "validate-transactions",
+                    translateId: "learn-hello-world",
                   }}
                 />
 
@@ -91,12 +91,12 @@ function Developers() {
                   to="developing/on-chain-programs/examples"
                   header={{
                     label: "Example Programs",
-                    translateId: "start-building",
+                    translateId: "developer-examples",
                   }}
                   body={{
                     label:
                       "Browse and clone working example programs from GitHub.",
-                    translateId: "start-building",
+                    translateId: "learn-by-example",
                   }}
                 />
               </div>
@@ -110,12 +110,12 @@ function Developers() {
                   to="developing/clients/jsonrpc-api"
                   header={{
                     label: "Essential Tools",
-                    translateId: "start-building",
+                    translateId: "developer-tools",
                   }}
                   body={{
                     label:
                       "Explore the essential developer tools for building and deploying Solana programs.",
-                    translateId: "start-building",
+                    translateId: "explore-tools",
                   }}
                 />
 
@@ -123,12 +123,12 @@ function Developers() {
                   to="developing/test-validator"
                   header={{
                     label: "Solana Test Validator",
-                    translateId: "run-validator",
+                    translateId: "developer-test-validator",
                   }}
                   body={{
                     label:
                       "Quickly setup and run a self contained local Solana blockchain for faster development.",
-                    translateId: "validate-transactions",
+                    translateId: "learn-test-validator",
                   }}
                 />
 
@@ -136,12 +136,12 @@ function Developers() {
                   to="developing/on-chain-programs/debugging"
                   header={{
                     label: "Debugging Programs",
-                    translateId: "start-building",
+                    translateId: "developer-debugging",
                   }}
                   body={{
                     label:
                       "Understand using unit test, logging. and error handling programs.",
-                    translateId: "start-building",
+                    translateId: "learn-debugging",
                   }}
                 />
               </div>
