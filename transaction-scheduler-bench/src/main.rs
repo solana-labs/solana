be540147fce6878b472b5f009b76f1f2e2392265
Ryo Onodera <ryoqun@gmail.com>
2022-08-26 17:23:52 +0900

    save

--- transaction-scheduler-bench/src/main.rs
+++ transaction-scheduler-bench/src/main.rs
@@ -457,6 +457,7 @@ fn spawn_packet_sender(
 ) -> JoinHandle<()> {
     std::thread::Builder::new().name(format!("sol-producer{:02}", i)).spawn(move || {
         send_packets(
+            unique_weight,
             producer_count,
             preloader,
             metrics,
@@ -471,6 +472,7 @@ fn spawn_packet_sender(
 }
 
 fn send_packets(
+    unique_weight: std::sync::Arc<std::sync::atomic::AtomicU64>,
     producer_count: usize,
     preloader: Arc<solana_scheduler::Preloader>,
     metrics: Arc<TransactionSchedulerBenchMetrics>,
@@ -516,8 +518,8 @@ fn send_packets(
             for v in vv {
                 let p = solana_scheduler::get_transaction_priority_details(&v.0);
                 //let p = (p << 32) | (rng.gen::<u64>() & 0x0000_0000_ffff_ffff);
-                let p = (p << 32) | (unique_weight & 0x0000_0000_ffff_ffff);
-                unique_weight -= 1;
+                let uw = unique_weight.fetch_sub(1, std::sync::atomic::Ordering::SeqCst);
+                let p = (p << 32) | (uw & 0x0000_0000_ffff_ffff);
                 let t = solana_scheduler::Task::new_for_queue(p, v);
                 //for lock_attempt in t.tx.1.iter() {
                 //    lock_attempt.contended_unique_weights().insert_task(p, solana_scheduler::TaskInQueue::clone(&t));
