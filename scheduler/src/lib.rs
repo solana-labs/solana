6463382fea4df5143012acb0215a15c172097d38
Ryo Onodera <ryoqun@gmail.com>
2022-08-26 15:32:50 +0900

    save

--- scheduler/src/lib.rs
+++ scheduler/src/lib.rs
@@ -1103,11 +1103,25 @@ impl ScheduleStage {
                         Self::commit_completed_execution(&mut processed_execution_environment, address_book, &mut execute_clock);
                         // async-ly propagate the result to rpc subsystems
                         to_next_stage.send(processed_execution_environment).unwrap();
+                       from_exec_len -= 1;
+                       if from_exec_len == 0 {
+                           from_exec_len = from_exec.len();
+                           if from_exec_len > 0 {
+                               trace!("select3: refill {} {}", from_len, from_exec_len);
+                           }
+                       }
                     }
                     if from_len > 0 {
                        trace!("select3: {} {}", from_len, from_exec_len);
                        let task = from.recv().unwrap();
                        Self::register_runnable_task(task, runnable_queue, &mut current_unique_key, &mut sequence_time);
+                       from_len -= 1;
+                       if from_len == 0 {
+                           from_len = from.len();
+                           if from_len > 0 {
+                               trace!("select3: refill {} {}", from_len, from_exec_len);
+                           }
+                       }
                     }
                 }
                 loop {
