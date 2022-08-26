900f8a3b2bdae18618483a500bb1843089155baf
Victor Pontis <victor@pont.is>
2022-08-26 12:49:37 -0400

    explorer: Add information about NFToken NFTs and Collection (#26716)
    This adds NFToken support to the Solana Explorer so that users can see NFToken NFTs and Collections and do some basic /exploring/. NFToken Docs: https://nftoken.so
    
    ## Feature Overview
    
    [Loom Video](https://www.loom.com/share/362e24aa66ac4db198b3e014a99235cb)
    
    <img width="1175" alt="CleanShot 2022-07-21 at 12 55 08@2x" src="https://user-images.githubusercontent.com/1319079/180270677-f02c0646-107e-4566-85b7-cd6a9f4b9eac.png">
    <img width="1177" alt="CleanShot 2022-07-21 at 12 55 00@2x" src="https://user-images.githubusercontent.com/1319079/180270686-d376bcba-3477-47ef-8cb0-972ad3b8d22c.png">
    
    ## Code Overview
    
    [Loom Video](https://www.loom.com/share/bdab68e55d73462a9222b105dd2316cb)

---
+++ explorer/src/components/account/nftoken/NFTokenAccountHeader.tsx
@@ -0,0 +1,120 @@
+import React, { Suspense } from "react";
+import { Account } from "../../../providers/accounts";
+import {
+  parseNFTokenCollectionAccount,
+  parseNFTokenNFTAccount,
+} from "./isNFTokenAccount";
+import { NftokenTypes } from "./nftoken-types";
+import { InfoTooltip } from "../../common/InfoTooltip";
+import { CachedImageContent } from "../../common/NFTArt";
+import { useNftokenMetadata } from "./nftoken-hooks";
+
+export function NFTokenAccountHeader({ account }: { account: Account }) {
+  const nft = parseNFTokenNFTAccount(account);
+
+  if (nft) {
+    return (
+      <Suspense fallback={<div>Loading...</div>}>
+        <NFTokenNFTHeader nft={nft} />
+      </Suspense>
+    );
+  }
+
+  const collection = parseNFTokenCollectionAccount(account);
+  if (collection) {
+    return (
+      <Suspense fallback={<div>Loading...</div>}>
+        <NFTokenCollectionHeader collection={collection} />
+      </Suspense>
+    );
+  }
+
+  return (
+    <>
+      <h6 className="header-pretitle">Details</h6>
+      <h2 className="header-title">Account</h2>
+    </>
+  );
+}
+
+export function NFTokenNFTHeader({ nft }: { nft: NftokenTypes.NftAccount }) {
+  const { data: metadata } = useNftokenMetadata(nft.metadata_url);
+
+  return (
+    <div className="row">
+      <div className="col-auto ms-2 d-flex align-items-center">
+        <CachedImageContent uri={metadata?.image} />
+      </div>
+
+      <div className="col mb-3 ms-0.5 mt-3">
+        {<h6 className="header-pretitle ms-1">NFToken NFT</h6>}
+        <div className="d-flex align-items-center">
+          <h2 className="header-title ms-1 align-items-center no-overflow-with-ellipsis">
+            {metadata ? metadata.name || "No NFT name was found" : "Loading..."}
+          </h2>
+        </div>
+
+        <div>
+          <div className={"d-inline-flex align-items-center mt-2"}>
+            <span className="badge badge-pill bg-dark">{`${
+              nft.authority_can_update ? "Mutable" : "Immutable"
+            }`}</span>
+
+            <InfoTooltip
+              bottom
+              text={
+                nft.authority_can_update
+                  ? "The authority of this NFT can update the Metadata."
+                  : "The Metadata cannot be updated by anyone."
+              }
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export function NFTokenCollectionHeader({
+  collection,
+}: {
+  collection: NftokenTypes.CollectionAccount;
+}) {
+  const { data: metadata } = useNftokenMetadata(collection.metadata_url);
+
+  return (
+    <div className="row">
+      <div className="col-auto ms-2 d-flex align-items-center">
+        <CachedImageContent uri={metadata?.image} />
+      </div>
+
+      <div className="col mb-3 ms-0.5 mt-3">
+        {<h6 className="header-pretitle ms-1">NFToken Collection</h6>}
+        <div className="d-flex align-items-center">
+          <h2 className="header-title ms-1 align-items-center no-overflow-with-ellipsis">
+            {metadata
+              ? metadata.name || "No collection name was found"
+              : "Loading..."}
+          </h2>
+        </div>
+
+        <div>
+          <div className={"d-inline-flex align-items-center mt-2"}>
+            <span className="badge badge-pill bg-dark">{`${
+              collection.authority_can_update ? "Mutable" : "Immutable"
+            }`}</span>
+
+            <InfoTooltip
+              bottom
+              text={
+                collection.authority_can_update
+                  ? "The authority of this Collection can update the Metadata and add NFTs."
+                  : "The Metadata cannot be updated by anyone."
+              }
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
