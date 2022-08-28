c06f986f1b4f9665de60f8107eb76627170af191
Ryo Onodera <ryoqun@gmail.com>
2022-08-28 22:12:41 +0900

    save

--- scheduler/src/lib.rs
+++ scheduler/src/lib.rs
@@ -794,10 +794,10 @@ impl ScheduleStage {
                     trace!("move to contended due to lock failure [{}/{}/{}]", unlockable_count, provisional_count, lock_count);
                     next_task.mark_as_contended();
                     *contended_count = contended_count.checked_add(1).unwrap();
-                    for lock_attempt in next_task.tx.1.iter() {
-                        lock_attempt.contended_unique_weights().insert_task(unique_weight, TaskInQueue::clone(&a2));
-                    }
-                    //task_sender.send((TaskInQueue::clone(&a2), std::mem::take(&mut next_task.for_indexer))).unwrap();
+                    //for lock_attempt in next_task.tx.1.iter() {
+                    //    lock_attempt.contended_unique_weights().insert_task(unique_weight, TaskInQueue::clone(&a2));
+                    //}
+                    task_sender.send((TaskInQueue::clone(&a2), std::mem::take(&mut next_task.for_indexer))).unwrap();
 
                     // maybe run lightweight prune logic on contended_queue here.
                 } else {
@@ -965,7 +965,7 @@ impl ScheduleStage {
     fn commit_completed_execution(ee: &mut ExecutionEnvironment, address_book: &mut AddressBook, commit_time: &mut usize, provisioning_tracker_count: &mut usize) {
         // do par()-ly?
 
-        ee.reindex();
+        //ee.reindex();
         ee.task.record_commit_time(*commit_time);
         ee.task.trace_timestamps("commit");
         //*commit_time = commit_time.checked_add(1).unwrap();
