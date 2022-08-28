55b9094c2491e59ab65238aa61c9314fd9228266
Ryo Onodera <ryoqun@gmail.com>
2022-08-28 21:39:48 +0900

    save

--- transaction-scheduler-bench/src/main.rs
+++ transaction-scheduler-bench/src/main.rs
@@ -308,7 +308,7 @@ fn execution_worker(
 ) {
     loop {
         if exit.load(Ordering::Relaxed) {
-            info("executor exit!");
+            info!("executor exit!");
             break;
         }
 
