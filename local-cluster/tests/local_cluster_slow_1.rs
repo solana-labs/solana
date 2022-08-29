3bac180ab2bf3fa9884524afa88caed25dca804f
Will Hickey <will.hickey@solana.com>
2022-08-29 13:36:16 -0500

    Remove #[ignore] annotations from local cluster tests (which are also fixed by this change)

--- local-cluster/tests/local_cluster_slow_1.rs
+++ local-cluster/tests/local_cluster_slow_1.rs
@@ -50,7 +50,6 @@ mod common;
 
 #[test]
 #[serial]
-#[ignore]
 // Steps in this test:
 // We want to create a situation like:
 /*
@@ -589,7 +588,6 @@ fn test_duplicate_shreds_broadcast_leader() {
 
 #[test]
 #[serial]
-#[ignore]
 fn test_switch_threshold_uses_gossip_votes() {
     solana_logger::setup_with_default(RUST_LOG_FILTER);
     let total_stake = 100 * DEFAULT_NODE_STAKE;
