5cf0b0a48d8cd30c4ad43af29dcdb82d4e12d19b
Brennan Watt <brennan.watt@solana.com>
2022-08-30 15:08:33 -0700

    PR feedback

--- ledger/src/shred/shred_code.rs
+++ ledger/src/shred/shred_code.rs
@@ -13,13 +13,9 @@ use {
     static_assertions::const_assert_eq,
 };
 
-const MAX_CODE_TO_DATA_SHRED_RATIO: usize = ERASURE_BATCH_SIZE[1] - 1;
-const_assert_eq!(
-    MAX_CODE_SHREDS_PER_SLOT,
-    32_768 * MAX_CODE_TO_DATA_SHRED_RATIO
-);
+const_assert_eq!(MAX_CODE_SHREDS_PER_SLOT, 32_768 * 17);
 pub(crate) const MAX_CODE_SHREDS_PER_SLOT: usize =
-    MAX_DATA_SHREDS_PER_SLOT * MAX_CODE_TO_DATA_SHRED_RATIO;
+    MAX_DATA_SHREDS_PER_SLOT * (ERASURE_BATCH_SIZE[1] - 1);
 
 const_assert_eq!(ShredCode::SIZE_OF_PAYLOAD, 1228);
 
