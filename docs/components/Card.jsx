5d95c7c40463b6eaec9ec429f9cd3702a0d7c8af
Nick Frostbutter <75431177+nickfrosty@users.noreply.github.com>
2022-08-31 13:32:36 -0400

    [docs] developers home page (#27436)
    * feat: developers landing page
    
    * fix: updated links
    
    * fix: updated labels for translateIds
    
    * refactor: updated index page to use Card component
    
    * style: removed whitespace
    
    * refactor: added static labels for developer sidebar items
    
    * feat: developer's sidebar and new layout file
    
    * chore: removed whitespace
    
    * fix: removed duplicate CLI sidebar item
    
    * fix: card shadows and RPC card text

---
+++ docs/components/Card.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import clsx from "clsx";
+import Link from "@docusaurus/Link";
+import styles from "../src/pages/styles.module.css";
+import Translate from "@docusaurus/Translate";
+
+function Card({ to, header, body }) {
+  /*
+  Both the `header` and `body` expect an object with the following type
+  header = {
+    label: String, //
+    translateId: String //
+  }
+  */
+
+  return (
+    <div className={clsx("col col--4 ", styles.feature)}>
+      <Link className="navbar__link card" to={to}>
+        <div className="card__header">
+          <h3>
+            <Translate description={header.translateId}>
+              {header.label}
+            </Translate>
+          </h3>
+        </div>
+        <div className="card__body">
+          <p>
+            <Translate description={body.translateId}>{body.label}</Translate>
+          </p>
+        </div>
+      </Link>
+    </div>
+  );
+}
+
+export default Card;
