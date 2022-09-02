69fd48a6970e4f61dbce53fe4baddafaba0e9b94
Ryo Onodera <ryoqun@gmail.com>
2022-09-02 22:38:11 +0900

    save

--- scheduler/src/lib.rs
+++ scheduler/src/lib.rs
@@ -1084,7 +1084,7 @@ impl ScheduleStage {
                 assert_eq!(task_receiver.len(), 0);
             }).unwrap();
         }
-                Self::commit_completed_execution2::<AST>(ast);
+                Self::commit_completed_execution2(ast);
 
         let (mut from_disconnected, mut from_exec_disconnected) = (false, false);
         loop {
