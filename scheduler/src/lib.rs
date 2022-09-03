b9749550829d127408da6082a037ed1c1e489650
Ryo Onodera <ryoqun@gmail.com>
2022-09-03 09:18:21 +0900

    save

--- scheduler/src/lib.rs
+++ scheduler/src/lib.rs
@@ -568,8 +568,8 @@ impl Task {
     pub fn clone_for_test(&self) -> Self {
         Self {
             unique_weight: self.unique_weight,
-            for_indexer: self.lock_attempts().iter().map(|a| a.clone_for_test()).collect(),
-            tx: (self.tx.0.clone(), self.lock_attempts().iter().map(|l| l.clone_for_test()).collect::<Vec<_>>()),
+            for_indexer: self.lock_attempts_not_mut().iter().map(|a| a.clone_for_test()).collect(),
+            tx: (self.tx.0.clone(), self.lock_attempts_not_mut().iter().map(|l| l.clone_for_test()).collect::<Vec<_>>()),
             contention_count: Default::default(),
             uncontended: Default::default(),
             sequence_time: std::sync::atomic::AtomicUsize::new(usize::max_value()),
