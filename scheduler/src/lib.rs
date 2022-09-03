12af3524df7defa231371b40538c0e8464608448
Ryo Onodera <ryoqun@gmail.com>
2022-09-03 13:26:50 +0900

    save

--- scheduler/src/lib.rs
+++ scheduler/src/lib.rs
@@ -906,7 +906,7 @@ impl ScheduleStage {
 
             assert!(!next_task.already_finished());
             if !from_runnable {
-                *contended_count = contended_count.checked_sub(1).unwrap_or_default();
+                *contended_count = contended_count.checked_sub(1).unwrap();
                 next_task.mark_as_uncontended();
             }
             let lock_attempts = std::mem::take(&mut *next_task.lock_attempts_mut(ast));
