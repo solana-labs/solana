4d651fc1b021cbddc2a0ed0ce2bf65899d04eb1a
Ryo Onodera <ryoqun@gmail.com>
2022-09-03 23:46:47 +0900

    save

--- runtime/src/bank.rs
+++ runtime/src/bank.rs
@@ -1231,6 +1231,7 @@ impl Default for Scheduler {
             );
             Ok(())
         }).unwrap();
+        drop(completed_ee_receiver);
 
         Self {
             scheduler_thread_handle: Some(scheduler_thread_handle),
