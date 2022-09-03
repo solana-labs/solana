72026aff06adeeaf729ed27d6b8a3e056d336b9a
Ryo Onodera <ryoqun@gmail.com>
2022-09-03 11:33:32 +0900

    save

--- scheduler/src/lib.rs
+++ scheduler/src/lib.rs
@@ -822,11 +822,11 @@ impl ScheduleStage {
                     trace!("move to contended due to lock failure [{}/{}/{}]", unlockable_count, provisional_count, lock_count);
                     next_task.mark_as_contended();
                     *contended_count = contended_count.checked_add(1).unwrap();
-                    //for lock_attempt in next_task.tx.1.iter() {
-                    //    lock_attempt.contended_unique_weights().insert_task(unique_weight, Task::clone_in_queue(&a2));
-                    //}
-                    let a = Task::clone_in_queue(&next_task);
-                    task_sender.send((a, std::mem::take(&mut *next_task.for_indexer.0.borrow_mut()))).unwrap();
+                    for lock_attempt in next_task.tx.1.iter() {
+                        lock_attempt.contended_unique_weights().insert_task(unique_weight, Task::clone_in_queue(&a2));
+                    }
+                    //let a = Task::clone_in_queue(&next_task);
+                    //task_sender.send((a, std::mem::take(&mut *next_task.for_indexer.0.borrow_mut()))).unwrap();
 
                     // maybe run lightweight prune logic on contended_queue here.
                 } else {
