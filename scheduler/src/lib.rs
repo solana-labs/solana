caa99c5240d6ac4d7e37c7cf99d80275f6ce7e4e
Ryo Onodera <ryoqun@gmail.com>
2022-09-03 13:49:30 +0900

    save

--- scheduler/src/lib.rs
+++ scheduler/src/lib.rs
@@ -793,6 +793,7 @@ impl ScheduleStage {
                 if runnable_queue.task_count() == 0 && /* *contended_count > 0 &&*/ address_book.stuck_tasks.len() > 0 {
                     error!("handling stuck...");
                     let (stuck_task_id, task) = address_book.stuck_tasks.pop_first().unwrap();
+                    // ensure proper rekeying
                     assert_eq!(task.stuck_task_id(), stuck_task_id);
 
                     if task.currently_contended() {
