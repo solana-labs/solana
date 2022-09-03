e7df46f3f87a5b18cec66242133152c5e516315f
Ryo Onodera <ryoqun@gmail.com>
2022-09-03 17:03:19 +0900

    save

--- runtime/src/bank.rs
+++ runtime/src/bank.rs
@@ -7869,7 +7869,7 @@ impl Bank {
     }
 
     fn wait_for_scheduler(&self) {
-        self.scheduler.scheduler_thread_handle.join()
+        self.scheduler.scheduler_thread_handle.join();
     }
 }
 
