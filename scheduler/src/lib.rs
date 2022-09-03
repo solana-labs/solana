3cf3049485d7a6ba3c1f07d9e6c318c30dd4f9b8
Ryo Onodera <ryoqun@gmail.com>
2022-09-03 13:15:01 +0900

    save

--- scheduler/src/lib.rs
+++ scheduler/src/lib.rs
@@ -789,7 +789,7 @@ impl ScheduleStage {
             (None, None) => {
                 trace!("select: none");
                 if runnable_queue.task_count() == 0 && *contended_count == 0 && address_book.stuck_tasks.len() > 0 {
-                    Some((false, address_book.stuck_tasks.pop_first().unwrap()))
+                    Some((false, address_book.stuck_tasks.pop_first().unwrap().1))
                 } else {
                     None
                 }
