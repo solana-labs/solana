f23fc6ec95631fc11a439e5a079a3a44d5254bb7
ananas-block <flow23@protonmail.com>
2022-09-04 20:46:11 +0100

    uncommented addition input check

--- sdk/program/src/alt_bn128.rs
+++ sdk/program/src/alt_bn128.rs
@@ -189,11 +189,11 @@ pub fn alt_bn128_addition(input: &[u8]) -> Result<Vec<u8>, AltBn128Error> {
 
     #[cfg(not(target_arch = "bpf"))]
     {
-        /*
+
         if input.len() > ALT_BN128_ADDITION_INPUT_LEN {
             return Err(AltBn128Error::InvalidInputData);
         }
-        */
+        
         let mut input = input.to_vec();
         input.resize(ALT_BN128_ADDITION_INPUT_LEN, 0);
 
