dbc0497a5126d12ffd13c013e34e2d625a4ea7dd
Ryo Onodera <ryoqun@gmail.com>
2022-09-04 21:38:39 +0900

    save

--- runtime/src/bank.rs
+++ runtime/src/bank.rs
@@ -7956,6 +7956,7 @@ impl Bank {
 
     pub fn wait_for_scheduler(&self) -> Result<()> {
         let mut scheduler = self.scheduler.write().unwrap();
+        scheduler.gracefully_stop()?;
 
         Ok(())
     }
