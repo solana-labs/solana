bf2ff3704299b73692377e598efb0a5694cabc83
Ryo Onodera <ryoqun@gmail.com>
2022-09-04 13:42:06 +0900

    save

--- scheduler/src/lib.rs
+++ scheduler/src/lib.rs
@@ -1438,8 +1438,8 @@ impl ScheduleStage {
                 }
             }
         }
-        drop(to_next_stage);
         drop(ee_sender);
+        drop(to_next_stage);
         info!("run finished...");
         info!("schedule_once (from: {}, to: {}, runnnable: {}, contended: {}, (immediate+provisional)/max: ({}+{})/{}) active from contended: {} stuck: {}!", from_prev.len(), to_execute_substage.len(), runnable_queue.task_count(), contended_count, executing_queue_count, provisioning_tracker_count, max_executing_queue_count, address_book.uncontended_task_ids.len(), address_book.stuck_tasks.len());
     }
