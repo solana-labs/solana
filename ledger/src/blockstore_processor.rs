2a77f22f84f0af7d0184ba118cea9721b80ae964
Ryo Onodera <ryoqun@gmail.com>
2022-09-05 22:24:08 +0900

    save

--- ledger/src/blockstore_processor.rs
+++ ledger/src/blockstore_processor.rs
@@ -1675,7 +1675,7 @@ fn process_single_slot(
         err
     })?;
 
-    bank.wait_for_scheduler().unwrap();
+    bank.wait_for_scheduler()?
     bank.freeze(); // all banks handled by this routine are created from complete slots
     if blockstore.is_primary_access() {
         blockstore.insert_bank_hash(bank.slot(), bank.hash(), false);
