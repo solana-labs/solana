29574287fe4748ffe20c3cb6e0b8d43bc5975771
Ryo Onodera <ryoqun@gmail.com>
2022-09-05 16:53:29 +0900

    save

--- scheduler/src/lib.rs
+++ scheduler/src/lib.rs
@@ -19,7 +19,7 @@ type PageRcInner<T> = std::rc::Rc<T>;
 unsafe impl Send for PageRc {}
 */
 
-type PageRcInner = triomphe::Arc<(std::cell::RefCell<Page>, TaskIds)>;
+type PageRcInner<T> = triomphe::Arc<(std::cell::RefCell<Page<T>>, TaskIds<T>)>;
 
 #[derive(Debug, Clone)]
 pub struct PageRc(PageRcInner);
