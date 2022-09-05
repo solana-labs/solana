b66c274179da6de84c08cca48173a5a20416cacd
Ryo Onodera <ryoqun@gmail.com>
2022-09-05 21:46:16 +0900

    save

--- runtime/src/bank.rs
+++ runtime/src/bank.rs
@@ -1245,6 +1245,7 @@ impl Default for Scheduler {
 
         let executing_thread_handles = (0..8).map(|thx| {
             let (scheduled_ee_receiver, completed_ee_sender) = (scheduled_ee_receiver.clone(), completed_ee_sender.clone());
+            let bank = bank.clone();
 
             std::thread::Builder::new().name(format!("solExec{:02}", thx)).spawn(move || {
             while let Ok(solana_scheduler::ExecutablePayload(mut ee)) = scheduled_ee_receiver.recv() {
