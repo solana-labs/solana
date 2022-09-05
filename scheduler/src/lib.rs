2f2e3bea0ed021efc5b7dd2241f3e7f262342ea0
Ryo Onodera <ryoqun@gmail.com>
2022-09-05 21:38:53 +0900

    save

--- scheduler/src/lib.rs
+++ scheduler/src/lib.rs
@@ -36,7 +36,7 @@ pub struct ExecutionEnvironment {
     pub task: TaskInQueue,
     pub finalized_lock_attempts: Vec<LockAttempt>,
     pub is_reindexed: bool,
-    pub execution_result: Option<Result<(), TransactionError>>,
+    pub execution_result: Option<Result<(), solana_sdk::transaction::TransactionError>>,
 }
 
 impl ExecutionEnvironment {
