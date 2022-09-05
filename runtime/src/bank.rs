35401eef6a7e271e5354f24c02b1837bbc6ffbac
Ryo Onodera <ryoqun@gmail.com>
2022-09-05 15:10:50 +0900

    save

--- runtime/src/bank.rs
+++ runtime/src/bank.rs
@@ -1249,12 +1249,12 @@ impl Default for Scheduler {
         }).unwrap();
 
         let errors = Arc::new(std::sync::Mutex::new(Vec::new()));
+        let errors_in_collector_thread = Arc::clone(errors);
 
         let error_collector_thread_handle = std::thread::Builder::new().name(format!("solErrorCol{:02}", 0)).spawn(move || {
-            let errors = errors.clone();
             while let Ok(solana_scheduler::DroppablePayload(ee)) = droppable_ee_receiver.recv() {
                 if false /* ee.is_aborted() */ {
-                    errors.lock().unwrap().push(Ok(()))
+                    errors_in_collector_thread.lock().unwrap().push(Ok(()))
                 }
             }
             Ok(())
