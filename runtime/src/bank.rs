d5538dd9d9e0b7809da1f6280e0a50377e57d734
Ryo Onodera <ryoqun@gmail.com>
2022-09-05 21:50:14 +0900

    save

--- runtime/src/bank.rs
+++ runtime/src/bank.rs
@@ -6384,6 +6384,8 @@ impl Bank {
 
             if r.bank.read().unwrap().is_none() {
                 drop(r);
+                let w = self.scheduler.write().unwrap();
+                w.bank.write().unwrap() = Some(Arc::clone(batch.bank));
                 panic!();
             } else {
                 r
