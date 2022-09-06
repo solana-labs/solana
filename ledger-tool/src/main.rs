ee2e04316c149b354322d8984393c4462bbd0a48
Ryo Onodera <ryoqun@gmail.com>
2022-09-06 22:06:46 +0900

    save

--- ledger-tool/src/main.rs
+++ ledger-tool/src/main.rs
@@ -378,7 +378,7 @@ fn output_slot(
                                 ()
                             });
                         }
-                        ee.task.reindex_with_address_book();
+                        ee.reindex_with_address_book();
                         post_execute_env_sender.send(solana_scheduler::UnlockablePayload(ee)).unwrap();
                     }
                 })
