921131a077424a831a6d473c9b47278574c2c881
Ryo Onodera <ryoqun@gmail.com>
2022-09-07 16:55:00 +0900

    save

--- scheduler/src/lib.rs
+++ scheduler/src/lib.rs
@@ -1433,7 +1433,7 @@ impl ScheduleStage {
                        Self::register_runnable_task(task, runnable_queue, &mut sequence_time);
                    } else {
                        assert_eq!(from_prev.len(), 0);
-                       from_prev = never();
+                       from_prev = crossbeam_channel::never();
                        from_disconnected |= true;
                        no_more_work |= runnable_queue.task_count() + contended_count + executing_queue_count + provisioning_tracker_count == 0;
                        info!("flushing2..: {} {} {} {} {} {}", from_disconnected, from_exec_disconnected, runnable_queue.task_count(), contended_count,  executing_queue_count, provisioning_tracker_count);
