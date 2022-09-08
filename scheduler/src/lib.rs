f1462150b9ec3fc795a5f7c9d0f4a7ae2530fabe
Ryo Onodera <ryoqun@gmail.com>
2022-09-08 21:23:36 +0900

    save

--- scheduler/src/lib.rs
+++ scheduler/src/lib.rs
@@ -104,7 +104,7 @@ impl ExecutionEnvironment {
                 });
 
             if lock_attempt.requested_usage == RequestedUsage::Writable {
-                let page = lock_attempt.target.page_mut(ast);
+                let mut page = lock_attempt.target.page_mut(ast);
                 page.contended_write_task_count = page.contended_write_task_count.checked_sub(1).unwrap();
             }
         }
