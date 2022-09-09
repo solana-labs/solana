b9a5ece98e1140d0173569ea2f0d001c87a30077
Ryo Onodera <ryoqun@gmail.com>
2022-09-09 22:00:29 +0900

    save

--- scheduler/src/lib.rs
+++ scheduler/src/lib.rs
@@ -1532,6 +1532,7 @@ impl ScheduleStage {
                         let elapsed = start.elapsed();
                         if elapsed > std::time::Duration::from_millis(150) {
                             let delta = processed_count - last_processed_count;
+                            let elapsed = elapsed.as_micros();
                             info!("schedule_once:interval id_{:016x} ch(prev: {}, exec: {}|{}), r: {}, u/c: {}/{}, (imm+provi)/max: ({}+{})/{} s: {} done: {} ({}txs/{}us={}tps)", random_id, from_prev.len(), to_execute_substage.len(), from_exec.len(), runnable_queue.task_count(), address_book.uncontended_task_ids.len(), contended_count, executing_queue_count, provisioning_tracker_count, max_executing_queue_count, address_book.stuck_tasks.len(), processed_count, delta, elapsed.as_micros(), 1000*delta/elapsed);
                             (start, last_processed_count) = (std::time::Instant::now(), processed_count);
                         }
@@ -1562,6 +1563,7 @@ impl ScheduleStage {
                         let elapsed = start.elapsed();
                         if elapsed > std::time::Duration::from_millis(150) {
                             let delta = processed_count - last_processed_count;
+                            let elapsed = elapsed.as_micros();
                             info!("schedule_once:interval id_{:016x} ch(prev: {}, exec: {}|{}), r: {}, u/c: {}/{}, (imm+provi)/max: ({}+{})/{} s: {} done: {} ({}txs/{}us={}tps)", random_id, from_prev.len(), to_execute_substage.len(), from_exec.len(), runnable_queue.task_count(), address_book.uncontended_task_ids.len(), contended_count, executing_queue_count, provisioning_tracker_count, max_executing_queue_count, address_book.stuck_tasks.len(), processed_count, delta, elapsed.as_micros(), 1000*delta/elapsed);
                             (start, last_processed_count) = (std::time::Instant::now(), processed_count);
                         }
