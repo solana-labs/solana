aa11214911fe3ad457d7dc4d28f49a16410f224c
Ryan Leung <ryan.leung@solana.com>
2022-09-10 01:19:37 +0800

    Small refactoring

--- runtime/src/cached_executors.rs
+++ runtime/src/cached_executors.rs
@@ -193,10 +193,8 @@ impl CachedExecutors {
 
     pub(crate) fn put(&mut self, executors: Vec<(Pubkey, Arc<dyn Executor>)>) {
         for (pubkey, executor) in executors {
-            self.entries.entry(pubkey).or_default();
-            self.entries.get(&pubkey).unwrap().hit_count.fetch_add(1, Relaxed);
-
-
+            self.entries.entry(pubkey).or_default().hit_count.fetch_add(1, Relaxed);
+            
             match self.executors.entry(pubkey) {
                 Entry::Vacant(entry) => {
                     self.stats.insertions.fetch_add(1, Relaxed);
