43f16ce4da44ff7ff0080cf15cbb08e4bbc843c7
Ryo Onodera <ryoqun@gmail.com>
2022-09-10 23:49:29 +0900

    save

--- scheduler/src/lib.rs
+++ scheduler/src/lib.rs
@@ -1002,7 +1002,7 @@ impl ScheduleStage {
         queue_clock: &mut usize,
         provisioning_tracker_count: &mut usize,
         runnable_exclusive: bool,
-        failed_lock_count: &mut usize,
+        &failed_lock_count: &mut usize,
     ) -> Option<(UniqueWeight, TaskInQueue, Vec<LockAttempt>)> {
         if let Some(mut a) = address_book.fulfilled_provisional_task_ids.pop_last() {
             trace!(
