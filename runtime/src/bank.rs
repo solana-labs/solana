5ab174325083c7a9a1766c43300e14a764896ac0
Ryo Onodera <ryoqun@gmail.com>
2022-09-12 13:26:23 +0900

    save

--- runtime/src/bank.rs
+++ runtime/src/bank.rs
@@ -1411,9 +1411,9 @@ impl Scheduler {
         info!("Scheduler::gracefully_stop(): waiting..");
         let transaction_sender = self.transaction_sender.take().unwrap();
         drop(transaction_sender);
-        let executing_thread_cpu_us: usize = self.executing_thread_handles.take().unwrap().iter().collect(|executing_thread_handle| {
+        let executing_thread_cpu_us: usize = self.executing_thread_handles.take().unwrap().iter().map(|executing_thread_handle| {
             executing_thread_handle.join().unwrap().map(|u| u.as_micros())
-        });
+        }).collect();
         let h = self.scheduler_thread_handle.take().unwrap();
         let scheduler_thread_cpu_us = h.join().unwrap()?.as_micros();
         let h = self.error_collector_thread_handle.take().unwrap();
