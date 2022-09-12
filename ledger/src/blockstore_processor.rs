971a5998a476a8e5a184b318c0db2fa47a3bead3
jeff washington <jeff.washington@solana.com>
2022-09-12 10:37:52 -0500

    fix ledger tool final hash calc

--- ledger/src/blockstore_processor.rs
+++ ledger/src/blockstore_processor.rs
@@ -1559,7 +1559,7 @@ fn load_frozen_forks(
 
 fn run_final_hash_calc(bank: &Bank, on_halt_store_hash_raw_data_for_debug: bool) {
     bank.force_flush_accounts_cache();
-    let can_cached_slot_be_unflushed = true;
+    let can_cached_slot_be_unflushed = false;
     // note that this slot may not be a root
     let _ = bank.verify_bank_hash(VerifyBankHash {
         test_hash_calculation: false,
