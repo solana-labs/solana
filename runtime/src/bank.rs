5cae160be47e2a54af80ea0bc59a8c1b64756183
Ryo Onodera <ryoqun@gmail.com>
2022-09-13 21:49:33 +0900

    save

--- runtime/src/bank.rs
+++ runtime/src/bank.rs
@@ -1199,8 +1199,8 @@ impl AbiExample for BuiltinPrograms {
 #[derive(Debug)]
 struct Scheduler {
     random_id: u64,
-    scheduler_thread_handle: Option<std::thread::JoinHandle<Result<Duration>>>,
-    executing_thread_handles: Option<Vec<std::thread::JoinHandle<Result<Duration>>>>,
+    scheduler_thread_handle: Option<std::thread::JoinHandle<Result<(Duration>>>,
+    executing_thread_handles: Option<Vec<std::thread::JoinHandle<Result<(Duration, Duration)>>>>,
     error_collector_thread_handle: Option<std::thread::JoinHandle<Result<Duration>>>,
     transaction_sender: Option<crossbeam_channel::Sender<solana_scheduler::SchedulablePayload>>,
     preloader: Arc<solana_scheduler::Preloader>,
