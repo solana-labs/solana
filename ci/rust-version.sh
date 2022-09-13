dd8feb8596d05108af54aad46f7828bcd26a628d
Will Hickey <will.hickey@solana.com>
2022-09-13 16:23:24 -0500

    Bump stable rust version to 1.60.0

--- ci/rust-version.sh
+++ ci/rust-version.sh
@@ -18,7 +18,7 @@
 if [[ -n $RUST_STABLE_VERSION ]]; then
   stable_version="$RUST_STABLE_VERSION"
 else
-  stable_version=1.59.0
+  stable_version=1.60.0
 fi
 
 if [[ -n $RUST_NIGHTLY_VERSION ]]; then
