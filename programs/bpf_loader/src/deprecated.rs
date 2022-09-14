54c0d4efada460fb81ec29abf20877186163a559
Ryo Onodera <ryoqun@gmail.com>
2022-09-14 13:04:27 +0900

    save

--- programs/bpf_loader/src/deprecated.rs
+++ programs/bpf_loader/src/deprecated.rs
@@ -1,6 +1,6 @@
 solana_sdk::declare_builtin!(
     solana_sdk::bpf_loader_deprecated::ID,
     solana_bpf_loader_deprecated_program,
-    solana_bpf_loader_program::process_instruction,
+    solana_bpf_loader_program::process_instruction_jit,
     deprecated::id
 );
