847ce7129bedd5604fea6c9fc1cc5f8b7258d441
Ryo Onodera <ryoqun@gmail.com>
2022-09-15 22:33:10 +0900

    save

--- runtime/src/bank.rs
+++ runtime/src/bank.rs
@@ -1208,7 +1208,7 @@ impl SchedulerPool {
     }
 
     fn create(&mut self) {
-        self.schedulers.push(Scheduler::default2());
+        self.schedulers.push(Arc::new(Scheduler::default2()));
     }
 
     fn take_from_pool(&mut self) -> Arc<Scheduler> {
