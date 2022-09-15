218b2180de52205dbd6cdb8f65c32ecb66ac7faf
Ryo Onodera <ryoqun@gmail.com>
2022-09-15 22:51:15 +0900

    save

--- runtime/src/bank.rs
+++ runtime/src/bank.rs
@@ -6575,7 +6575,7 @@ impl Bank {
                 drop(w);
                 self.scheduler.read().unwrap()
             } else {
-                assert_eq!(bank.slot(), r.as_ref().unwrap().slot.unwrap().load(SeqCst));
+                assert_eq!(bank.slot(), r.as_ref().unwrap().slot.unwrap().load(std::sync::atomic::Ordering::SeqCst));
                 r
             }
         };
