2b11ac8afe92d319d88ff0984552ea899dc352f3
Justin Starry <justin@solana.com>
2022-09-15 15:33:33 -0400

    Explorer: Support displaying and inspecting versioned transactions

--- explorer/src/pages/inspector/SimulatorCard.tsx
+++ explorer/src/pages/inspector/SimulatorCard.tsx
@@ -1,13 +1,14 @@
 import React from "react";
-import bs58 from "bs58";
-import { Connection, Message, Transaction } from "@solana/web3.js";
+import {
+  Connection,
+  VersionedMessage,
+  VersionedTransaction,
+} from "@solana/web3.js";
 import { useCluster } from "providers/cluster";
 import { InstructionLogs, parseProgramLogs } from "utils/program-logs";
 import { ProgramLogsCardBody } from "components/ProgramLogsCardBody";
 
-const DEFAULT_SIGNATURE = bs58.encode(Buffer.alloc(64).fill(0));
-
-export function SimulatorCard({ message }: { message: Message }) {
+export function SimulatorCard({ message }: { message: VersionedMessage }) {
   const { cluster, url } = useCluster();
   const {
     simulate,
@@ -77,7 +78,7 @@ export function SimulatorCard({ message }: { message: Message }) {
   );
 }
 
-function useSimulator(message: Message) {
+function useSimulator(message: VersionedMessage) {
   const { cluster, url } = useCluster();
   const [simulating, setSimulating] = React.useState(false);
   const [logs, setLogs] = React.useState<Array<InstructionLogs> | null>(null);
@@ -97,15 +98,10 @@ function useSimulator(message: Message) {
     const connection = new Connection(url, "confirmed");
     (async () => {
       try {
-        const tx = Transaction.populate(
-          message,
-          new Array(message.header.numRequiredSignatures).fill(
-            DEFAULT_SIGNATURE
-          )
-        );
-
         // Simulate without signers to skip signer verification
-        const resp = await connection.simulateTransaction(tx);
+        const resp = await connection.simulateTransaction(
+          new VersionedTransaction(message)
+        );
         if (resp.value.logs === null) {
           throw new Error("Expected to receive logs from simulation");
         }
