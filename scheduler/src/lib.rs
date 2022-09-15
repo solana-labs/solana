1110c711c171cb960f9c13da5d29b2dbe2b68ba0
Ryo Onodera <ryoqun@gmail.com>
2022-09-15 21:44:38 +0900

    save

--- scheduler/src/lib.rs
+++ scheduler/src/lib.rs
@@ -1756,7 +1756,7 @@ impl Checkpoint {
         }
     }
 
-    fn new(remaining_threads: usize) -> std::sync::Arc<Self> {
+    pub fn new(remaining_threads: usize) -> std::sync::Arc<Self> {
         std::sync::Arc::new(Self(std::sync::Mutex::new(remaining_threads), std::sync::Condvar::new()))
     }
 }
