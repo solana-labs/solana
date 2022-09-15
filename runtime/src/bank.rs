fece606578d4ff9873846a2b3318d709528cf948
Ryo Onodera <ryoqun@gmail.com>
2022-09-15 22:50:24 +0900

    save

--- runtime/src/bank.rs
+++ runtime/src/bank.rs
@@ -6571,7 +6571,7 @@ impl Bank {
                 // safe.
                 let mut w = self.scheduler.write().unwrap().unwrap();
                 *w.bank.write().unwrap() = Some(Arc::downgrade(&bank));
-                w.slot = Some(bank.slot);
+                w.slot = Some(AtomicU64::new(bank.slot));
                 drop(w);
                 self.scheduler.read().unwrap()
             } else {
