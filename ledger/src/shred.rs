b183e00dcfcc7e799f0ff1622e20c0becd8d3ab0
behzad nouri <behzadnouri@gmail.com>
2022-09-16 00:08:10 +0000

    patches range check in shred::layout::get_signed_message_range (#27822)

--- ledger/src/shred.rs
+++ ledger/src/shred.rs
@@ -623,7 +623,7 @@ pub mod layout {
                 merkle::ShredData::get_signed_message_range(proof_size)?
             }
         };
-        (shred.len() <= range.end).then_some(range)
+        (range.end <= shred.len()).then_some(range)
     }
 
     pub(crate) fn get_reference_tick(shred: &[u8]) -> Result<u8, Error> {
