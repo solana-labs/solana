93836e3b830d2b722407e6c08fdf3762c2d3e5ea
Ryo Onodera <ryoqun@gmail.com>
2022-09-16 15:59:57 +0900

    save

--- runtime/src/bank.rs
+++ runtime/src/bank.rs
@@ -3768,7 +3768,7 @@ impl Bank {
         // committed before this write lock can be obtained here.
         let mut hash = self.hash.write().unwrap();
         if *hash == Hash::default() {
-            assert!(self.scheduler2.read().unwrap().as_ref().unwrap().is_none());
+            assert!(self.scheduler2.read().unwrap().is_none());
             // finish up any deferred changes to account state
             self.collect_rent_eagerly(false);
             self.collect_fees();
