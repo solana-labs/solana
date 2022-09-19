ee657bc44d0f1d9c92cf600caf58a450e633001d
Ryo Onodera <ryoqun@gmail.com>
2022-09-19 14:04:48 +0900

    save

--- ledger/src/blockstore_processor.rs
+++ ledger/src/blockstore_processor.rs
@@ -329,6 +329,7 @@ fn rebatch_transactions<'a>(
 fn execute_batches(
     bank: &Arc<Bank>,
     batches: &[TransactionBatchWithIndexes],
+    transaction_indexes: impl Iter<Item = usize>,
     entry_callback: Option<&ProcessCallback>,
     transaction_status_sender: Option<&TransactionStatusSender>,
     replay_vote_sender: Option<&ReplayVoteSender>,
