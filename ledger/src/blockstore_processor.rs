eea9f838d740f0039807efc3d6dd6459eb99f633
Ryo Onodera <ryoqun@gmail.com>
2022-09-21 00:39:31 +0900

    save

--- ledger/src/blockstore_processor.rs
+++ ledger/src/blockstore_processor.rs
@@ -432,7 +432,7 @@ fn process_entries_with_callback(
                 // If it's a tick, save it for later
                 tick_hashes.push(hash);
                 if bank.is_block_boundary(bank.tick_height() + tick_hashes.len() as u64) {
-                    info!("process_entries_with_callback(): exeucted {} txs so far in {}us (still in loop)", tx_count, started.elapsed().as_micros());
+                    info!("process_entries_with_callback(): slot: {} idx: {} exeucted {} txs so far in {}us (still in loop)", bank.slot(), starting_index, tx_count, started.elapsed().as_micros());
                     started = std::time::Instant::now();
                     tx_count = 0;
 
@@ -461,7 +461,7 @@ fn process_entries_with_callback(
             }
         }
     }
-    info!("process_entries_with_callback(): exeucted {} txs so far in {}us (after loop)", tx_count, started.elapsed().as_micros());
+    info!("process_entries_with_callback(): slot: {} idx: {} exeucted {} txs so far in {}us (after loop)", bank.slot(), starting_index, tx_count, started.elapsed().as_micros());
     started = std::time::Instant::now();
     for hash in tick_hashes {
         bank.register_tick(hash);
