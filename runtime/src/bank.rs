ee226d2245abe497e3ea2d21d3e903ba338ebcaa
Ryo Onodera <ryoqun@gmail.com>
2022-09-21 23:53:02 +0900

    save

--- runtime/src/bank.rs
+++ runtime/src/bank.rs
@@ -1459,7 +1459,7 @@ impl Scheduler {
                             }
                             drop(ee);
                         },
-                        solana_scheduler::ExaminablePayload(solana_scheduler::Flushable::Flush(checkpoint) => { todo!() },
+                        solana_scheduler::ExaminablePayload(solana_scheduler::Flushable::Flush(checkpoint)) => { todo!() },
                     }
                 }
                 todo!();
