d6395bfc569dcb106d0bc2e14dad75d2d3dea9b9
Andrew Fitzgerald <apfitzge@gmail.com>
2022-09-22 16:46:55 -0500

    added some tests

--- runtime/src/root_bank_cache.rs
+++ runtime/src/root_bank_cache.rs
@@ -39,3 +39,46 @@ impl RootBankCache {
         self.cached_root_bank.clone()
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use {
+        super::*,
+        crate::{
+            accounts_background_service::AbsRequestSender,
+            bank_forks::BankForks,
+            genesis_utils::{create_genesis_config, GenesisConfigInfo},
+        },
+        solana_sdk::pubkey::Pubkey,
+    };
+
+    #[test]
+    fn test_root_bank_cache() {
+        let GenesisConfigInfo { genesis_config, .. } = create_genesis_config(10_000);
+        let bank = Bank::new_for_tests(&genesis_config);
+        let bank_forks = Arc::new(RwLock::new(BankForks::new(bank)));
+
+        let mut root_bank_cache = RootBankCache::new(bank_forks.clone());
+
+        let bank = bank_forks.read().unwrap().root_bank();
+        assert_eq!(bank, root_bank_cache.root_bank());
+
+        {
+            let child_bank = Bank::new_from_parent(&bank, &Pubkey::default(), 1);
+            bank_forks.write().unwrap().insert(child_bank);
+
+            // cached slot is still 0 since we have not set root
+            assert_eq!(bank.slot(), root_bank_cache.cached_root_bank.slot());
+        }
+        {
+            bank_forks
+                .write()
+                .unwrap()
+                .set_root(1, &AbsRequestSender::default(), None);
+            let bank = bank_forks.read().unwrap().root_bank();
+            assert!(bank.slot() != root_bank_cache.cached_root_bank.slot());
+            assert!(bank != root_bank_cache.cached_root_bank);
+            assert_eq!(bank, root_bank_cache.root_bank());
+        }
+    }
+}
