b3640b1a9a7edc33be93c08af1e9b5e83fb3492f
Will Hickey <will.hickey@solana.com>
2022-09-22 16:03:21 -0500

    Move channel version check to its own file and put it before nits

--- ci/test-sanity.sh
+++ ci/test-sanity.sh
@@ -22,36 +22,8 @@ source ci/_
 
 echo
 
+_ ci/check-channel-version.sh
 _ ci/nits.sh
 _ ci/check-ssh-keys.sh
-
-
-# Ensure the current channel version is not equal ("greater") than
-# the version of the latest tag
-if [[ -z $CI_TAG ]]; then
-  echo "--- channel version check"
-  (
-    eval "$(ci/channel-info.sh)"
-
-    if [[ -n $CHANNEL_LATEST_TAG ]]; then
-      source scripts/read-cargo-variable.sh
-
-      version=$(readCargoVariable version "version/Cargo.toml")
-      echo "latest channel tag: $CHANNEL_LATEST_TAG"
-      echo "current version: v$version"
-
-      if [[ $CHANNEL_LATEST_TAG = v$version ]]; then
-        echo "\033[31mError:\033[0m A release has been tagged since your feature branch was created. <current version> should be greater than <latest channel tag>.
-        Possible solutions (in the order they should be tried):
-        1. rebase your feature branch on the base branch
-        2. merge the PR: \"Bump Version to <$version+1>\" and then rebase
-        3. ask for help in #devops."
-        exit 1
-      fi
-    else
-      echo "Skipped. CHANNEL_LATEST_TAG (CHANNEL=$CHANNEL) unset"
-    fi
-  )
-fi
 
 echo --- ok
