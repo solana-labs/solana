9c50b4d3787c2bd22f234e1cbc6d82733a1b2040
Will Hickey <will.hickey@solana.com>
2022-09-22 21:36:31 -0500

    Format

--- ci/check-channel-version.sh
+++ ci/check-channel-version.sh
@@ -13,7 +13,7 @@ if [[ -z $CI_TAG ]]; then
       echo "current version: v$version"
 
       if [[ $CHANNEL_LATEST_TAG = v$version ]]; then
-        echo "\033[31mError:\033[0m A release has been tagged since your feature branch was created. <current version> should be greater than <latest channel tag>.
+        echo -e "\033[31mError:\033[0m A release has been tagged since your feature branch was created. <current version> should be greater than <latest channel tag>.
         Possible solutions (in the order they should be tried):
         1. rebase your feature branch on the base branch
         2. merge the PR: \"Bump Version to <$version+1>\" and then rebase
