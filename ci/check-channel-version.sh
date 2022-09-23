d6e4e96a6b7a1d6243c5dac3c02f5181847a1271
Will Hickey <will.hickey@solana.com>
2022-09-22 22:54:28 -0500

    lint

--- ci/check-channel-version.sh
+++ ci/check-channel-version.sh
@@ -16,7 +16,7 @@ if [[ -z $CI_TAG ]]; then
         echo -e "\033[31mError:\033[0m A release has been tagged since your feature branch was last rebased. <current version> must be greater than <latest channel tag>..
         Possible solutions (in the order they should be tried):
         1. rebase your feature branch on the base branch
-        2. merge the PR: \"Bump Version to <$version+0.0.1>\" once it has passed ci/checks, then rebase
+        2. merge the PR: \"Bump Version to ...\" once it has passed ci/checks, then rebase
         3. ask for help in #devops on discord"
         exit 1
       fi
