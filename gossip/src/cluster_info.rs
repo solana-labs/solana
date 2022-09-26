ed8e057a0fe2f19a1476a51a100c093882e0d288
Brooks Prumo <brooks@solana.com>
2022-09-26 12:26:37 -0400

    Update abi digest for cluster_info::Protocol

--- gossip/src/cluster_info.rs
+++ gossip/src/cluster_info.rs
@@ -266,7 +266,7 @@ pub fn make_accounts_hashes_message(
 pub(crate) type Ping = ping_pong::Ping<[u8; GOSSIP_PING_TOKEN_SIZE]>;
 
 // TODO These messages should go through the gpu pipeline for spam filtering
-#[frozen_abi(digest = "C1nR7B7CgMyUYo6h3z2KXcS38JSwF6y8jmZ6Y9Cz7XEd")]
+#[frozen_abi(digest = "9YNaHHwzwQaAaXDXW5A8k47HDuUzRYNjTAdARoAgVvPU")]
 #[derive(Serialize, Deserialize, Debug, AbiEnumVisitor, AbiExample)]
 #[allow(clippy::large_enum_variant)]
 pub(crate) enum Protocol {
