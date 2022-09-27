a89010cc89798fcde5984e60ad0454435dc3a2cc
Jeff Washington (jwash) <jeff.washington@solana.com>
2022-09-27 08:48:05 -0700

    fix duplicate local variable (#28093)

--- runtime/src/in_mem_accounts_index.rs
+++ runtime/src/in_mem_accounts_index.rs
@@ -1053,7 +1053,6 @@ impl<T: IndexValue> InMemAccountsIndex<T> {
 
         // merge all items into the disk index now
         let disk = self.bucket.as_ref().unwrap();
-        let mut duplicate = vec![];
         let mut count = 0;
         insert.into_iter().for_each(|(slot, k, v)| {
             let entry = (slot, v);
@@ -1066,7 +1065,7 @@ impl<T: IndexValue> InMemAccountsIndex<T> {
                         slot_list.extend_from_slice(current_slot_list);
                         slot_list.push(entry); // will never be from the same slot that already exists in the list
                         ref_count += new_ref_count;
-                        duplicate.push((slot, k));
+                        duplicates.push((slot, k));
                         Some((slot_list, ref_count))
                     }
                     None => {
