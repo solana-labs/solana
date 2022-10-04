cc378c15fc172edacd9609f642a61f4dacaf27a7
Steven Czabaniuk <steven@solana.com>
2022-10-03 23:47:49 -0500

    Update Node.js version to 16.x from deprecated 12.x

--- docs/.travis/before_install.sh
+++ docs/.travis/before_install.sh
@@ -1,6 +1,6 @@
 # |source| this file
 
-curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
+curl -sL https://deb.nodesource.com/setup_16.x | sudo -E bash -
 sudo apt install -y nodejs
 
 npm install --global docusaurus-init
