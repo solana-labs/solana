ba06a9501d8ce687c1955bbb97bfb1a8b89cc61b
brooks <brooks@solana.com>
2022-10-05 15:33:15 -0400

    pr: add tests

--- runtime/src/epoch_accounts_hash/manager.rs
+++ runtime/src/epoch_accounts_hash/manager.rs
@@ -46,7 +46,7 @@ impl Manager {
         *state = State::InFlight(slot);
     }
 
-    /// An epoch accounts hash calculation has complete; update our state
+    /// An epoch accounts hash calculation has completed; update our state
     pub fn set_valid(&self, epoch_accounts_hash: EpochAccountsHash, slot: Slot) {
         let mut state = self.state.lock().unwrap();
         if let State::Valid(old_epoch_accounts_hash, old_slot) = &*state {
@@ -68,11 +68,7 @@ impl Manager {
         loop {
             match &*state {
                 State::Valid(epoch_accounts_hash, _slot) => break *epoch_accounts_hash,
-                State::Invalid => {
-                    break EpochAccountsHash::new(Hash::new(
-                        "sentinel epoch accounts hash val".as_bytes(),
-                    ))
-                }
+                State::Invalid => break SENTINEL_EPOCH_ACCOUNTS_HASH,
                 State::InFlight(_slot) => state = self.cvar.wait(state).unwrap(),
             }
         }
@@ -111,4 +107,93 @@ enum State {
     InFlight(Slot),
     /// The EAH calculation is complete (for `Slot`) and the EAH value is valid to read/use.
     Valid(EpochAccountsHash, Slot),
+}
+
+/// Sentinel epoch accounts hash value; used when getting an Invalid EAH
+///
+/// Displays as "Sentine1EpochAccountsHash111111111111111111"
+const SENTINEL_EPOCH_ACCOUNTS_HASH: EpochAccountsHash =
+    EpochAccountsHash::new(Hash::new_from_array([
+        0x06, 0x92, 0x40, 0x3b, 0xee, 0xea, 0x7e, 0xe2, 0x7d, 0xf4, 0x90, 0x7f, 0xbd, 0x9e, 0xd0,
+        0xd2, 0x1c, 0x2b, 0x66, 0x9a, 0xc4, 0xda, 0xce, 0xd7, 0x23, 0x41, 0x69, 0xab, 0xb7, 0x80,
+        0x00, 0x00,
+    ]));
+
+#[cfg(test)]
+mod tests {
+    use {super::*, std::time::Duration};
+
+    #[test]
+    fn test_new_valid() {
+        let epoch_accounts_hash = EpochAccountsHash::new(Hash::new_unique());
+        let manager = Manager::new_valid(epoch_accounts_hash, 5678);
+        assert_eq!(
+            manager.try_get_epoch_accounts_hash(),
+            Some(epoch_accounts_hash),
+        );
+        assert_eq!(manager.wait_get_epoch_accounts_hash(), epoch_accounts_hash);
+    }
+
+    #[test]
+    fn test_new_invalid() {
+        let manager = Manager::new_invalid();
+        assert!(manager.try_get_epoch_accounts_hash().is_none());
+        assert_eq!(
+            manager.wait_get_epoch_accounts_hash(),
+            SENTINEL_EPOCH_ACCOUNTS_HASH,
+        );
+    }
+
+    #[test]
+    fn test_try_get_epoch_accounts_hash() {
+        let epoch_accounts_hash = EpochAccountsHash::new(Hash::new_unique());
+        for (state, expected) in [
+            (State::Invalid, None),
+            (State::InFlight(123), None),
+            (
+                State::Valid(epoch_accounts_hash, 5678),
+                Some(epoch_accounts_hash),
+            ),
+        ] {
+            let manager = Manager::_new(state);
+            let actual = manager.try_get_epoch_accounts_hash();
+            assert_eq!(actual, expected);
+        }
+    }
+
+    #[test]
+    fn test_wait_epoch_accounts_hash() {
+        // Test: State is Invalid, no need to wait
+        {
+            let manager = Manager::new_invalid();
+            assert_eq!(
+                manager.wait_get_epoch_accounts_hash(),
+                SENTINEL_EPOCH_ACCOUNTS_HASH,
+            );
+        }
+
+        // Test: State is Valid, no need to wait
+        {
+            let epoch_accounts_hash = EpochAccountsHash::new(Hash::new_unique());
+            let manager = Manager::new_valid(epoch_accounts_hash, 5678);
+            assert_eq!(manager.wait_get_epoch_accounts_hash(), epoch_accounts_hash);
+        }
+
+        // Test: State is InFlight, must wait
+        {
+            let epoch_accounts_hash = EpochAccountsHash::new(Hash::new_unique());
+            let manager = Manager::new_invalid();
+            manager.set_in_flight(123);
+
+            std::thread::scope(|s| {
+                s.spawn(|| {
+                    std::thread::sleep(Duration::from_secs(1));
+                    manager.set_valid(epoch_accounts_hash, 5678)
+                });
+                assert!(manager.try_get_epoch_accounts_hash().is_none());
+                assert_eq!(manager.wait_get_epoch_accounts_hash(), epoch_accounts_hash);
+                assert!(manager.try_get_epoch_accounts_hash().is_some());
+            });
+        }
+    }
 }
