9e7be1b8dfa3f2a81561f9aa94ce0ff47d8c3dbf
Haoran Yi <haoran.yi@gmail.com>
2022-10-12 17:10:56 -0500

    Revert "don't add ancestor for calc bank"
    This reverts commit 1482539c03d206b0ae37ebfe9be014a2607d4732.

--- runtime/src/bank.rs
+++ runtime/src/bank.rs
@@ -1820,7 +1820,6 @@ impl Bank {
         slot: Slot,
         //reward_calc_tracer: Option<impl Fn(&RewardCalculationEvent) + Send + Sync>,
         vote_only_bank: bool,
-        add_ancestor: bool,
     ) -> Self {
         //let mut time = Measure::start("bank::new_from_parent");
         //let NewBankOptions { vote_only_bank } = new_bank_options;
@@ -1997,19 +1996,17 @@ impl Bank {
             epoch_reward_calc_start: parent.epoch_reward_calc_start,
         };
 
-        if add_ancestor {
-            let (_, ancestors_time) = measure!(
-                {
-                    let mut ancestors = Vec::with_capacity(1 + new.parents().len());
-                    ancestors.push(new.slot());
-                    new.parents().iter().for_each(|p| {
-                        ancestors.push(p.slot());
-                    });
-                    new.ancestors = Ancestors::from(ancestors);
-                },
-                "ancestors_creation",
-            );
-        }
+        let (_, ancestors_time) = measure!(
+            {
+                let mut ancestors = Vec::with_capacity(1 + new.parents().len());
+                ancestors.push(new.slot());
+                new.parents().iter().for_each(|p| {
+                    ancestors.push(p.slot());
+                });
+                new.ancestors = Ancestors::from(ancestors);
+            },
+            "ancestors_creation",
+        );
         new
 
         //        // Following code may touch AccountsDb, requiring proper ancestors
@@ -2251,7 +2248,6 @@ impl Bank {
             slot,
             //reward_calc_tracer,
             vote_only_bank,
-            true,
         );
 
         //        let epoch_schedule = parent.epoch_schedule;
@@ -2481,7 +2477,6 @@ impl Bank {
                             slot,
                             //reward_calc_tracer,
                             vote_only_bank,
-                            false,
                         );
 
                         new2.apply_feature_activations(
