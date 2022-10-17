379b03db563f7ba2a95e853016a94c68bcc7f3a6
samuelmayna <samuelmayna@gmail.com>
2022-10-17 15:16:45 +0300

    Deleted the main.css. Was not part of the main application

--- explorer/src/components/NetworkStatusNotifier.tsx
+++ explorer/src/components/NetworkStatusNotifier.tsx
@@ -1,9 +1,11 @@
+import { useCluster } from "providers/cluster";
+import { Connection } from "@solana/web3.js";
 import React, { useState, useEffect } from "react";
 
 const STATUS_API_ENDPOINT: string =
   "https://status.solana.com/api/v2/status.json";
 
-const PING_PERIOD_IN_MS: number = 1000;
+const PING_PERIOD_IN_MS: number = 3000;
 
 const container = {
   display: "flex",
@@ -18,9 +20,29 @@ function NetworkStatusNotifier() {
   const [currentDownStatus, setCurrentErrorState] =
     useState<string>(healthyStatus);
   const [hasDownTime, setHasDownTime] = useState<boolean>(false);
+  const { cluster, url } = useCluster();
 
   useEffect(() => {
+    const connection = new Connection(url, "finalized");
+
     let timer = setInterval(async () => {
+      const nodes = await connection.getClusterNodes();
+
+      console.log(nodes[0].getHealth());
+
+      // try {
+      //   const response = await fetch("https://api.mainnet-beta.solana.com", {
+      //     // Adding method type
+      //     method: "POST",
+
+      //     // Adding body or contents to send
+      //     body: JSON.stringify({ jsonrpc: "2.0", id: 1, method: "getHealth" }),
+      //   });
+      //   console.log(response);
+      // } catch (error) {
+      //   console.log(error);
+      // }
+      console.log("This changed");
       const res = await makeGetRequest(STATUS_API_ENDPOINT);
       const statusDesc = res.status.description;
       if (currentDownStatus !== statusDesc) {
