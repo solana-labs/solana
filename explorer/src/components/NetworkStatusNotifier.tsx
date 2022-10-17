0a1045adb6e7c28501f9d41ccdc2bb157ccc3f26
samuelmayna <samuelmayna@gmail.com>
2022-10-17 13:22:52 +0300

    Added condition to prevent re-render when there are no changes in  the network condition.

--- explorer/src/components/NetworkStatusNotifier.tsx
+++ explorer/src/components/NetworkStatusNotifier.tsx
@@ -3,7 +3,7 @@ import React, { useState, useEffect } from "react";
 const STATUS_API_ENDPOINT: string =
   "https://status.solana.com/api/v2/status.json";
 
-const PING_PERIOD_IN_MS: number = 3000;
+const PING_PERIOD_IN_MS: number = 1000;
 
 const container = {
   display: "flex",
@@ -23,8 +23,10 @@ function NetworkStatusNotifier() {
     let timer = setInterval(async () => {
       const res = await makeGetRequest(STATUS_API_ENDPOINT);
       const statusDesc = res.status.description;
-      setHasDownTime(statusDesc !== healthyStatus);
-      setCurrentErrorState(statusDesc);
+      if (currentDownStatus !== statusDesc) {
+        setHasDownTime(statusDesc !== healthyStatus);
+        setCurrentErrorState(statusDesc);
+      }
     }, PING_PERIOD_IN_MS);
 
     return () => {
