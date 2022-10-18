f829f3583e8ec0a70695ba0979c39cb1e262cd82
Lijun Wang <83639177+lijunwangs@users.noreply.github.com>
2022-10-18 10:05:47 -0700

    FMT code

--- tpu-client/src/tpu_connection_cache.rs
+++ tpu-client/src/tpu_connection_cache.rs
@@ -341,7 +341,9 @@ mod tests {
     use {
         super::*,
         crate::{
-            nonblocking::tpu_connection::{SendTransactionCallbackOption, TpuConnection as NonblockingTpuConnection},
+            nonblocking::tpu_connection::{
+                SendTransactionCallbackOption, TpuConnection as NonblockingTpuConnection,
+            },
             tpu_connection::TpuConnection as BlockingTpuConnection,
         },
         async_trait::async_trait,
@@ -468,14 +470,21 @@ mod tests {
         fn tpu_addr(&self) -> &SocketAddr {
             &self.addr
         }
-        async fn send_wire_transaction<T>(&self, _wire_transaction: T, _callback: &mut SendTransactionCallbackOption,
+        async fn send_wire_transaction<T>(
+            &self,
+            _wire_transaction: T,
+            _callback: &mut SendTransactionCallbackOption,
         ) -> TransportResult<()>
         where
             T: AsRef<[u8]> + Send + Sync,
         {
             unimplemented!()
         }
-        async fn send_wire_transaction_batch<T>(&self, _buffers: &[T], _callback: &mut SendTransactionCallbackOption,) -> TransportResult<()>
+        async fn send_wire_transaction_batch<T>(
+            &self,
+            _buffers: &[T],
+            _callback: &mut SendTransactionCallbackOption,
+        ) -> TransportResult<()>
         where
             T: AsRef<[u8]> + Send + Sync,
         {
