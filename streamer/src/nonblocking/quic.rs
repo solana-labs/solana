ac14adec77feb4c14b85d8e656c44cf80980561b
Lijun Wang <83639177+lijunwangs@users.noreply.github.com>
2022-10-20 12:11:41 -0700

    Define WAIT_FOR_CHUNK_TIMEOUT_MS

--- streamer/src/nonblocking/quic.rs
+++ streamer/src/nonblocking/quic.rs
@@ -42,6 +42,7 @@ use {
 
 const QUIC_TOTAL_STAKED_CONCURRENT_STREAMS: f64 = 100_000f64;
 const WAIT_FOR_STREAM_TIMEOUT_MS: u64 = 1000;
+const WAIT_FOR_CHUNK_TIMEOUT_MS: u64 = 10000;
 
 pub const ALPN_TPU_PROTOCOL_ID: &[u8] = b"solana-tpu";
 
@@ -538,7 +539,7 @@ async fn handle_connection(
                             let mut maybe_batch = None;
                             while !stream_exit.load(Ordering::Relaxed) {
                                 if let Ok(chunk) = tokio::time::timeout(
-                                    Duration::from_millis(WAIT_FOR_STREAM_TIMEOUT_MS),
+                                    Duration::from_millis(WAIT_FOR_CHUNK_TIMEOUT_MS),
                                     stream.read_chunk(PACKET_DATA_SIZE, false),
                                 )
                                 .await
