9f01157e1e470d06c8891baddaade53c7a2717ce
samuelmayna <samuelmayna@gmail.com>
2022-11-08 15:45:29 +0300

    Added the active and deliquent validators to the list to be displayed

--- explorer/src/pages/StakeDelegationsPage.tsx
+++ explorer/src/pages/StakeDelegationsPage.tsx
@@ -1,10 +1,12 @@
-import { useEffect } from "react";
+import React, { useEffect } from "react";
 
 import { useVoteAccounts } from "providers/accounts/vote-accounts";
 import { ClusterStatus, useCluster } from "providers/cluster";
 import { LoadingCard } from "components/common/LoadingCard";
 import { Address } from "components/common/Address";
-import { PublicKey, VoteAccountInfo } from "@solana/web3.js";
+import { PublicKey } from "@solana/web3.js";
+
+const PAGINATION_COUNT = 7;
 
 export function StakeDelegationsPage() {
   const { status } = useCluster();
@@ -13,99 +15,24 @@ export function StakeDelegationsPage() {
   useEffect(() => {
     if (status === ClusterStatus.Connected) fetchVoteAccounts();
   }, [status]);
-  console.log(voteAccounts);
+
   if (voteAccounts)
     return (
       <>
-        <div className="card">
-          <div className="card-header">
-            <div className="row align-items-center">
-              <div className="col">
-                <h2 className="card-header-title"> Current Validator Data </h2>
-              </div>
-              <div className="col-auto">
-                Total Nodes: {voteAccounts?.current.length}
-              </div>
-            </div>
-          </div>
-          <div className="table-responsive mb-0">
-            <table className="table table-sm table-nowrap card-table">
-              <thead>
-                <tr>
-                  <th className="text-muted">#</th>
-                  <th className="text-muted">IDENTITY</th>
-                  <th className="text-muted text-end">ACTIVE STAKE</th>
-                  <th className="text-muted text-end">COMMISSION</th>
-                  <th className="text-muted text-end">LAST VOTE</th>
-                  <th className="text-muted text-start">VOTE IDENTITY</th>
-                </tr>
-              </thead>
-              <tbody className="list">
-                {voteAccounts.current.map((data: any, index: number) => {
-                  return renderRowData(index, data);
-                })}
-              </tbody>
-            </table>
-          </div>
-        </div>
-        <div className="card">
-          <div className="card-header">
-            <div className="row align-items-center">
-              <div className="col">
-                <h2 className="card-header-title centered ">Network Nodes</h2>
-              </div>
-              <div className="col-auto">Total Nodes:34</div>
-            </div>
-          </div>
-          <div className="table-responsive mb-0">
-            <table className="table table-sm table-nowrap card-table">
-              <thead>
-                <tr>
-                  <th className="text-muted">#</th>
-                  <th className="text-muted">IDENTITY</th>
-                  <th className="text-muted text-end">IP</th>
-                  <th className="text-muted text-end">Gossip</th>
-                  <th className="text-muted text-end">TPU</th>
-                  <th className="text-muted text-end">Version</th>
-                </tr>
-              </thead>
-              <tbody className="list">
-                <tr>
-                  <td>
-                    <span className="badge bg-gray-soft badge-pill">
-                      goog thing
-                    </span>
-                  </td>
-                  <td className="text-start">3439894343</td>
-                  <td className="text-end">good thing</td>
-                  <td className="text-end">NUll page</td>
-                  <td className="text-end">googd</td>
-                  <td className="text-end">dfdtere</td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
+        <div>
+          <RenderCardSection
+            title="Current Validators Data"
+            cardData={voteAccounts.current}
+          />
+          <RenderCardSection
+            title="Deliquent Validators Data"
+            cardData={voteAccounts.delinquent}
+          />
         </div>
       </>
-
-      // <div className="container mt-4">
-      //   <StakingComponent />
-      //   <div className="card">
-      //     <div className="card-header">
-      //       <div className="row align-items-center">
-      //         <div className="col">
-      //           <h4 className="card-header-title">
-      //             Staking Data for the Network
-      //           </h4>
-      //         </div>
-      //       </div>
-      //     </div>
-      //   </div>
-      // </div>
     );
   else return <LoadingCard message="Loading validators data" />;
 }
-
 interface rowData {
   activatedStake: number;
   commission: number;
@@ -113,6 +40,66 @@ interface rowData {
   nodePubkey: string;
   votePubkey: string;
 }
+function RenderCardSection({
+  title,
+  cardData,
+}: {
+  title: string;
+  cardData: rowData[];
+}): JSX.Element {
+  const [displayedCount, setDisplayedCount] =
+    React.useState<number>(PAGINATION_COUNT);
+  return (
+    <>
+      <div className="card">
+        <div className="card-header">
+          <div className="row align-items-center">
+            <div className="col">
+              <h2 className="card-header-title"> {title}</h2>
+            </div>
+            <div className="col-auto">Total Nodes: {cardData.length}</div>
+          </div>
+        </div>
+        <div className="table-responsive mb-0">
+          <table className="table table-sm table-nowrap card-table">
+            <thead>
+              <tr>
+                <th className="text-muted">#</th>
+                <th className="text-muted">IDENTITY</th>
+                <th className="text-muted text-end">ACTIVE STAKE</th>
+                <th className="text-muted text-end">COMMISSION</th>
+                <th className="text-muted text-end">LAST VOTE</th>
+                <th className="text-muted text-start">VOTE IDENTITY</th>
+              </tr>
+            </thead>
+            <tbody className="list">
+              {cardData
+                .slice(0, displayedCount)
+                .map((data: any, index: number) => {
+                  return renderRowData(index, data);
+                })}
+            </tbody>
+            {cardData.length > PAGINATION_COUNT && (
+              <div className="card-footer">
+                <button
+                  className="btn btn-primary"
+                  onClick={() =>
+                    setDisplayedCount(
+                      (displayed) => displayed + PAGINATION_COUNT
+                    )
+                  }
+                >
+                  Load More
+                </button>
+              </div>
+            )}
+          </table>
+        </div>
+      </div>
+    </>
+  );
+}
+
 function renderRowData(index: number, data: rowData): JSX.Element {
   const validatorPublicKey: PublicKey = new PublicKey(data.nodePubkey);
   const votePublicKey: PublicKey = new PublicKey(data.votePubkey);
