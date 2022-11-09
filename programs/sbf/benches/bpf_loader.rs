4d6653598b2e010d302217ab2e4fe9a6b1bbeed0
Brooks Prumo <brooks@solana.com>
2022-11-09 22:15:03 +0000

    Upgrades to Rust 1.65.0 (#28741)

--- programs/sbf/benches/bpf_loader.rs
+++ programs/sbf/benches/bpf_loader.rs
@@ -148,7 +148,7 @@ fn bench_program_alu(bencher: &mut Bencher) {
                 .unwrap();
         });
         let instructions = vm.get_total_instruction_count();
-        let summary = bencher.bench(|_bencher| {}).unwrap();
+        let summary = bencher.bench(|_bencher| Ok(())).unwrap().unwrap();
         println!("  {:?} instructions", instructions);
         println!("  {:?} ns/iter median", summary.median as u64);
         assert!(0f64 != summary.median);
@@ -168,7 +168,7 @@ fn bench_program_alu(bencher: &mut Bencher) {
         );
 
         bencher.iter(|| vm.execute_program_jit(&mut instruction_meter).unwrap());
-        let summary = bencher.bench(|_bencher| {}).unwrap();
+        let summary = bencher.bench(|_bencher| Ok(())).unwrap().unwrap();
         println!("  {:?} instructions", instructions);
         println!("  {:?} ns/iter median", summary.median as u64);
         assert!(0f64 != summary.median);
