IntokuSatori/kanonsolana

Get Index Key Size RPC Support

rpc-client-api/src/config.rs
@@ -137,6 +137,14 @@ pub struct RpcEpochConfig {
     pub min_context_slot: Option<Slot>,
 }
 
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq, Eq, Hash)]
+#[serde(rename_all = "camelCase")]
+pub enum JsonAccountIndex {
+    ProgramId,
+    SplTokenMint,
+    SplTokenOwner,
+}
+
 #[derive(Debug, Clone, Default, PartialEq, Eq, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct RpcAccountInfoConfig {

solana-labs/solana

rpc-client-api/src/config.rs
@@ -137,6 +137,14 @@ pub struct RpcEpochConfig {
     pub min_context_slot: Option<Slot>,
 }
 
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq, Eq, Hash)]
+#[serde(rename_all = "camelCase")]
+pub enum RpcAccountIndex {
+    ProgramId,
+    SplTokenMint,
+    SplTokenOwner,
+}
+
 #[derive(Debug, Clone, Default, PartialEq, Eq, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct RpcAccountInfoConfig {