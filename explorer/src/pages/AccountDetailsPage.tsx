8dbe895224a115d76756da5cf2aace6e4e6fa8a9
ngundotra <noah@gundotra.org>
2022-11-21 14:01:38 -0500

    explorer: add scam notice to serum deployments

--- explorer/src/pages/AccountDetailsPage.tsx
+++ explorer/src/pages/AccountDetailsPage.tsx
@@ -42,7 +42,7 @@ import { CacheEntry, FetchStatus } from "providers/cache";
 import { ClusterStatus, useCluster } from "providers/cluster";
 import { useTokenRegistry } from "providers/mints/token-registry";
 import React, { Suspense } from "react";
-import { NavLink, Redirect, useLocation } from "react-router-dom";
+import { Link, NavLink, Redirect, useLocation } from "react-router-dom";
 import { clusterPath } from "utils/url";
 import { NFTokenAccountHeader } from "../components/account/nftoken/NFTokenAccountHeader";
 import { NFTokenAccountSection } from "../components/account/nftoken/NFTokenAccountSection";
@@ -158,6 +158,20 @@ const TOKEN_TABS_HIDDEN = [
   "sysvar",
   "config",
 ];
+
+/**
+ * Flag accounts potentially at risk due to FTX hack
+ */
+const FTX_FLAGGED_ACCOUNTS = new Set<string>([
+  // Serum Swap
+  "22Y43yTVxuUkoRKdm9thyRhQ3SdgQS7c7kB6UNCiaczD",
+  // Serum Dex V3
+  "9xQeWvG816bUx9EPjHmaT23yvVM2ZWbrrpZb9PusVFin",
+  // Serum Dex V2
+  "EUqojwWA2rd19FZrzeBncJsm38Jm1hEhE3zsmX3bRc2o",
+  // Serum Dex V1
+  "BJ3jrUzddfuSrZHXSCxMUUQsjKEyLmuuyZebkcaFp2fg"
+])
 
 type Props = { address: string; tab?: string };
 export function AccountDetailsPage({ address, tab }: Props) {
@@ -333,6 +347,12 @@ function DetailsSections({
         <div className="alert alert-danger alert-scam" role="alert">
           Warning! This account has been flagged by the community as a scam
           account. Please be cautious sending SOL to this account.
+        </div>
+      )}
+      {FTX_FLAGGED_ACCOUNTS.has(address) && (
+        <div className="alert alert-danger alert-scam" role="alert">
+          Warning! This program's upgrade key may have been compromised by the FTX hack.
+          Please migrate to the community fork: <Link className="text-white" style={{ textDecoration: "underline" }} to="https://github.com/openbook-dex/program">https://github.com/openbook-dex/program</Link>
         </div>
       )}
       <InfoSection account={account} />
