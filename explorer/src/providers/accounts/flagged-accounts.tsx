5ec8abf249eb150513aebeb88d54cdcc1e7cef0f
ngundotra <noah@gundotra.org>
2022-11-21 15:26:03 -0500

    explorer: format:fix

--- explorer/src/providers/accounts/flagged-accounts.tsx
+++ explorer/src/providers/accounts/flagged-accounts.tsx
@@ -12,16 +12,16 @@ type IncidentId = "ftx-hack-november-2022" | "known-scam";
 type IncidentDescription = React.ReactElement;
 
 const FLAGGED_ACCOUNTS: Record<string, IncidentId> = {
-  "GACpXND1SSfTSQMmqGuFvGwXB3jGEYBDRGNzmLfTYwSP": "known-scam",
+  GACpXND1SSfTSQMmqGuFvGwXB3jGEYBDRGNzmLfTYwSP: "known-scam",
   "9tAViia54YAaL9gv92hBu8K4QGRBKbytCQ9TYsJ6F6or": "known-scam",
   // Serum Swap
   "22Y43yTVxuUkoRKdm9thyRhQ3SdgQS7c7kB6UNCiaczD": "ftx-hack-november-2022",
   // Serum Dex V3
   "9xQeWvG816bUx9EPjHmaT23yvVM2ZWbrrpZb9PusVFin": "ftx-hack-november-2022",
   // Serum Dex V2
-  "EUqojwWA2rd19FZrzeBncJsm38Jm1hEhE3zsmX3bRc2o": "ftx-hack-november-2022",
+  EUqojwWA2rd19FZrzeBncJsm38Jm1hEhE3zsmX3bRc2o: "ftx-hack-november-2022",
   // Serum Dex V1
-  "BJ3jrUzddfuSrZHXSCxMUUQsjKEyLmuuyZebkcaFp2fg": "ftx-hack-november-2022",
+  BJ3jrUzddfuSrZHXSCxMUUQsjKEyLmuuyZebkcaFp2fg: "ftx-hack-november-2022",
 };
 const INCIDENTS: Record<IncidentId, IncidentDescription> = {
   "known-scam": (
@@ -35,17 +35,18 @@ const INCIDENTS: Record<IncidentId, IncidentDescription> = {
   "ftx-hack-november-2022": (
     <>
       <div className="alert alert-danger alert-scam" role="alert">
-        Warning! This program's upgrade key may have been compromised by the FTX hack. Please migrate to the community fork:{" "}
-          <Link
-            className="text-white"
-            style={{ textDecoration: "underline" }}
-            to="https://github.com/openbook-dex/program"
-          >
+        Warning! This program's upgrade key may have been compromised by the FTX
+        hack. Please migrate to the community fork:{" "}
+        <Link
+          className="text-white"
+          style={{ textDecoration: "underline" }}
+          to="https://github.com/openbook-dex/program"
+        >
           https://github.com/openbook-dex/program
         </Link>
       </div>
     </>
-  )
+  ),
 } as const;
 
 const FlaggedContext = React.createContext<FlaggedMap>({});
