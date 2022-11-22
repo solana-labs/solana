015784ad07e7b71669cb4df7b39b8c9cdc38198a
hanako mumei <81144685+2501babe@users.noreply.github.com>
2022-11-22 15:20:05 -0800

    update to spl-token-2022 0.5.0
    * support CpiGuard and PermanentDelegate extensions in transaction-status and account-decoder
    * update transaction-status and account-decoder to new ConfidentialTransfer interfaces

--- transaction-status/src/parse_token/extension/mod.rs
+++ transaction-status/src/parse_token/extension/mod.rs
@@ -1,9 +1,11 @@
 use super::*;
 
 pub(super) mod confidential_transfer;
+pub(super) mod cpi_guard;
 pub(super) mod default_account_state;
 pub(super) mod interest_bearing_mint;
 pub(super) mod memo_transfer;
 pub(super) mod mint_close_authority;
+pub(super) mod permanent_delegate;
 pub(super) mod reallocate;
 pub(super) mod transfer_fee;
