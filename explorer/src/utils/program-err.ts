71dac94839aa8acc3f74c82d2fa1638c6403f8f0
samuelmayna <samuelmayna@gmail.com>
2022-11-24 14:55:29 +0300

    Added logs from the TransactionError key to the overall error string

--- explorer/src/utils/program-err.ts
+++ explorer/src/utils/program-err.ts
@@ -1,5 +1,131 @@
 import { TransactionError } from "@solana/web3.js";
 
+const transactionErrorMessage: Map<string, string> = new Map([
+  [
+    "AccountInUse",
+    "An account is already being processed in another transaction in a way that does not support parallelism",
+  ],
+  [
+    "AccountLoadedTwice",
+    "A Pubkey appears twice in the transaction’s account_keys. Instructions can reference Pubkeys more than once but the message must contain a list with no duplicate keys",
+  ],
+  [
+    "AccountNotFound",
+    "Attempt to debit an account but found no record of a prior credit.",
+  ],
+
+  ["ProgramAccountNotFound", "Attempt to load a program that does not exist"],
+
+  [
+    "InsufficientFundsForFee",
+    "The from Pubkey does not have sufficient balance to pay the fee to schedule the transaction",
+  ],
+
+  [
+    "InvalidAccountForFee",
+    "This account may not be used to pay transaction fees",
+  ],
+
+  [
+    "AlreadyProcessed",
+    "The bank has seen this transaction before. This can occur under normal operation when a UDP packet is duplicated, as a user error from a client not updating its recent_blockhash, or as a double-spend attack",
+  ],
+  [
+    "BlockhashNotFound",
+    "The bank has not seen the given recent_blockhash or the transaction is too old and the recent_blockhash has been discarded.",
+  ],
+
+  ["InstructionError", "Instruction at : 1"],
+
+  ["CallChainTooDeep", "Loader call chain is too deep"],
+
+  [
+    "MissingSignatureForFee",
+    "Transaction requires a fee but has no signature present",
+  ],
+
+  ["InvalidAccountIndex", "Transaction contains an invalid account reference"],
+
+  ["SignatureFailure", "Transaction did not pass signature verification"],
+
+  [
+    "InvalidProgramForExecution",
+    "This program may not be used for executing instructions",
+  ],
+
+  [
+    "SanitizeFailure",
+    "Transaction failed to sanitize accounts offsets correctly implies that account locks are not taken for this TX, and should not be unlocked.",
+  ],
+
+  [
+    "AccountBorrowOutstanding",
+    "Transaction processing left an account with an outstanding borrowed reference",
+  ],
+
+  [
+    "WouldExceedMaxBlockCostLimit",
+    "Transaction would exceed max Block Cost Limit",
+  ],
+
+  ["UnsupportedVersion", "Transaction version is unsupported"],
+
+  [
+    "InvalidWritableAccount",
+    "Transaction loads a writable account that cannot be written",
+  ],
+
+  [
+    "WouldExceedMaxAccountCostLimit",
+    "Transaction would exceed max account limit within the block",
+  ],
+
+  [
+    "WouldExceedAccountDataBlockLimit",
+    "Transaction would exceed account data limit within the block",
+  ],
+
+  ["TooManyAccountLocks", "Transaction locked too many accounts"],
+
+  ["AddressLookupTableNotFound", "Address lookup table not found"],
+
+  [
+    "InvalidAddressLookupTableOwner",
+    "Attempted to lookup addresses from an account owned by the wrong program",
+  ],
+
+  [
+    "InvalidAddressLookupTableData",
+    "Attempted to lookup addresses from an invalid account",
+  ],
+
+  [
+    "InvalidAddressLookupTableIndex",
+    "Address table lookup uses an invalid index",
+  ],
+
+  [
+    "InvalidRentPayingAccount",
+    "Transaction leaves an account with a lower balance than rent-exempt minimum",
+  ],
+
+  [
+    "WouldExceedMaxVoteCostLimit",
+    "Transaction would exceed max Vote Cost Limit",
+  ],
+  ["DuplicateInstruction", "Constains duplicate instructions: {0}"],
+
+  [
+    "WouldExceedAccountDataTotalLimit",
+    "Transaction would exceed total account data limit",
+  ],
+
+  [
+    "InsufficientFundsForRent",
+    "Transaction results in an account without insufficient funds for rent",
+  ],
+]);
+
 const instructionErrorMessage: Map<string, string> = new Map([
   ["GenericError", "generic instruction error"],
   ["InvalidArgument", "invalid program argument"],
@@ -107,14 +233,31 @@ export function getTransactionInstructionError(
     return;
   }
 
-  if (typeof error === "object" && "InstructionError" in error) {
-    const innerError = error["InstructionError"];
-    const index = innerError[0] as number;
-    const instructionError = innerError[1];
+  if (typeof error === "object") {
+    let instructionErrorKey = "InstructionError";
+    let transactionErrorKey = "TransactionError";
+
+    let index: number = 0;
+    let errors: string = "";
+    if (instructionErrorKey in error) {
+      //@ts-ignore
+      const innerError = error[instructionErrorKey];
+      index += innerError[0] as number;
+      const instructionError = innerError[1];
+      errors += getInstructionError(instructionError);
+    }
+
+    if (transactionErrorKey in error) {
+      //@ts-ignore
+      const innerError = error[transactionErrorKey];
+      index += (innerError[0] as number) + 1;
+      const instructionError = innerError[1];
+      errors += getTransactionError(instructionError);
+    }
 
     return {
       index,
-      message: getInstructionError(instructionError),
+      message: errors,
     };
   }
 }
@@ -141,4 +284,28 @@ function getInstructionError(error: any): string {
   }
 
   return "Unknown instruction error";
+}
+
+function getTransactionError(error: any): string {
+  let out;
+  let value;
+
+  if (typeof error === "string") {
+    const message = transactionErrorMessage.get(error);
+    if (message) {
+      return message;
+    }
+  } else if ("InstructionError" in error) {
+    out = transactionErrorMessage.get("InstructionError");
+    value = error["InstructionError"];
+  } else if ("DuplicateInstruction" in error) {
+    out = transactionErrorMessage.get("DuplicateInstruction");
+    value = error["DuplicateInstruction"];
+  }
+
+  if (out && value) {
+    return out.replace("{0}", value);
+  }
+
+  return "Unknown transaction error";
 }
