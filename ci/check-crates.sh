4631c092d256be46db07b1c74d558bdbc9e52678
yihau <yihau.chen@icloud.com>
2022-12-05 23:14:10 +0800

    v2

--- ci/check-crates.sh
+++ ci/check-crates.sh
@@ -1,69 +1,85 @@
 #!/usr/bin/env bash
 
-# env:
-#   - CRATE_TOKEN
+# input:
+# 	env:
+#  	 	- CRATE_TOKEN
+#   	- COMMIT_RANGE
+
+if [[ -z $COMMIT_RANGE ]]; then
+	echo "COMMIT_RANGE should be provided"
+	exit 1
+fi
+
+if ! command -v toml &>/dev/null; then
+	echo "not found toml-cli"
+	cargo install toml-cli
+fi
 
 declare skip_patterns=(
-    "./Cargo.toml"
-    "./programs/sbf/Cargo.toml"
+	"Cargo.toml"
+	"programs/sbf"
 )
 
 declare -A verified_crate_owners=(
-    ["solana-grimes"]=1
+	["solana-grimes"]=1
 )
 
-# search all Cargo.toml
-readarray -t files <<<"$(find . -name "Cargo.toml")"
+# get Cargo.toml from git diff
+readarray -t files <<<"$(git diff "$COMMIT_RANGE" --name-only | grep Cargo.toml)"
+printf "%s\n" "${files[@]}"
 
-declare -A including_unverified_owners=()
-declare -A unknown_errors=()
-
+error_count=0
 for file in "${files[@]}"; do
-    for skip_pattern in "${skip_patterns[@]}"; do
-        if [[ $file =~ ^$skip_pattern ]]; then
-            continue 2
-        fi
-    done
+	read -r crate_name package_publish workspace < <(toml get "$file" . | jq -r '(.package.name | tostring)+" "+(.package.publish | tostring)+" "+(.workspace | tostring)')
+	echo "=== $crate_name ($file) ==="
 
-    crate_name=$(grep -Poz '[\s\S]*?package\][\s\S]*?name = "\K([a-zA-Z0-9_\-]*)' "$file" | tr -d '\0')
-    echo "=== $crate_name ($file) ==="
+	if [[ $package_publish = 'false' ]]; then
+		echo -e "⏩ skip (package_publish: $package_publish)\n"
+		continue
+	fi
 
-    readarray -t owners <<<"$(cargo owner --list -q "$crate_name" --token "$CRATE_TOKEN" 2>&1)"
+	if [[ "$workspace" != "null" ]]; then
+		echo -e "⏩ skip (is a workspace root)\n"
+		continue
+	fi
 
-    for owner in "${owners[@]}"; do
-        if [[ -z $owner ]]; then
-            continue
-        fi
-        if [[ $owner =~ ^error ]]; then
-            unknown_errors+=(["$crate_name ($file)"]=1)
-            echo "❌ ERROR"
-            printf "%s\n" "${owners[@]}"
-            break
-        fi
+	for skip_pattern in "${skip_patterns[@]}"; do
+		if [[ $file =~ ^$skip_pattern ]]; then
+			echo -e "⏩ skip (match skip patterns)\n"
+			continue 2
+		fi
+	done
 
-        owner_id="$(echo "$owner" | awk '{print $1}')"
-        if [[ ${verified_crate_owners[$owner_id]} ]]; then
-            echo "✅ $owner"
-        else
-            including_unverified_owners+=(["$crate_name ($file)"]=1)
-            echo "❌ $owner"
-        fi
-    done
-
-    echo ""
+	result="$(cargo owner --list -q "$crate_name" --token "$CRATE_TOKEN" 2>&1)"
+	if [[ $result =~ ^error ]]; then
+		if [[ $result == *"Not Found"* ]]; then
+			((error_count++))
+			echo "❌ new crate $crate_name not found on crates.io. make a dummy publication (hint to instructions or mark it as not for publication with 'package.publish = false' in its Cargo.toml"
+		else
+			((error_count++))
+			echo "❌ $result"
+		fi
+	else
+		readarray -t owners <<<"$result"
+		for owner in "${owners[@]}"; do
+			if [[ -z $owner ]]; then
+				continue
+			fi
+			owner_id="$(echo "$owner" | awk '{print $1}')"
+			if [[ ${verified_crate_owners[$owner_id]} ]]; then
+				echo "✅ $owner"
+			else
+				((error_count++))
+				echo "❌ $owner"
+			fi
+		done
+	fi
+	echo ""
 done
 
-if [ ${#including_unverified_owners[@]} -eq 0 ] && [ "${#unknown_errors[@]}" -eq 0 ]; then
+if [ "$error_count" -eq 0 ]; then
     echo "success"
-    exit 0
+	exit 0
 else
-    echo "including unverified owner: ${#including_unverified_owners[@]}"
-    printf "%s\n" "${!including_unverified_owners[@]}"
-    echo ""
-
-    echo "unknown error: ${#unknown_errors[@]}"
-    printf "%s\n" "${!unknown_errors[@]}"
-    echo ""
-
-    exit 1
+	exit 1
 fi
