5e4d709154e2427ba6f2551cd586d6feec79fd3d
yihau <yihau.chen@icloud.com>
2022-12-06 19:19:03 +0800

    lint: ident with space 2

--- ci/check-crates.sh
+++ ci/check-crates.sh
@@ -6,22 +6,22 @@
 #   	- COMMIT_RANGE
 
 if [[ -z $COMMIT_RANGE ]]; then
-	echo "COMMIT_RANGE should be provided"
-	exit 1
+  echo "COMMIT_RANGE should be provided"
+  exit 1
 fi
 
 if ! command -v toml &>/dev/null; then
-	echo "not found toml-cli"
-	cargo install toml-cli
+  echo "not found toml-cli"
+  cargo install toml-cli
 fi
 
 declare skip_patterns=(
-	"Cargo.toml"
-	"programs/sbf"
+  "Cargo.toml"
+  "programs/sbf"
 )
 
 declare -A verified_crate_owners=(
-	["solana-grimes"]=1
+  ["solana-grimes"]=1
 )
 
 # get Cargo.toml from git diff
@@ -30,63 +30,63 @@ printf "%s\n" "${files[@]}"
 
 error_count=0
 for file in "${files[@]}"; do
-	read -r crate_name package_publish workspace < <(toml get "$file" . | jq -r '(.package.name | tostring)+" "+(.package.publish | tostring)+" "+(.workspace | tostring)')
-	echo "=== $crate_name ($file) ==="
+  read -r crate_name package_publish workspace < <(toml get "$file" . | jq -r '(.package.name | tostring)+" "+(.package.publish | tostring)+" "+(.workspace | tostring)')
+  echo "=== $crate_name ($file) ==="
 
-	if [[ $package_publish = 'false' ]]; then
-		echo -e "⏩ skip (package_publish: $package_publish)\n"
-		continue
-	fi
+  if [[ $package_publish = 'false' ]]; then
+    echo -e "⏩ skip (package_publish: $package_publish)\n"
+    continue
+  fi
 
-	if [[ "$workspace" != "null" ]]; then
-		echo -e "⏩ skip (is a workspace root)\n"
-		continue
-	fi
+  if [[ "$workspace" != "null" ]]; then
+    echo -e "⏩ skip (is a workspace root)\n"
+    continue
+  fi
 
-	for skip_pattern in "${skip_patterns[@]}"; do
-		if [[ $file =~ ^$skip_pattern ]]; then
-			echo -e "⏩ skip (match skip patterns)\n"
-			continue 2
-		fi
-	done
+  for skip_pattern in "${skip_patterns[@]}"; do
+    if [[ $file =~ ^$skip_pattern ]]; then
+      echo -e "⏩ skip (match skip patterns)\n"
+      continue 2
+    fi
+  done
 
-	result="$(cargo owner --list -q "$crate_name" --token "$CRATE_TOKEN" 2>&1)"
-	if [[ $result =~ ^error ]]; then
-		if [[ $result == *"Not Found"* ]]; then
-			((error_count++))
-			echo "❌ new crate $crate_name not found on crates.io. make a dummy publication (hint to instructions or mark it as not for publication with 'package.publish = false' in its Cargo.toml"
-		else
-			((error_count++))
-			echo "❌ $result"
-		fi
-	else
-		readarray -t owners <<<"$result"
+  result="$(cargo owner --list -q "$crate_name" --token "$CRATE_TOKEN" 2>&1)"
+  if [[ $result =~ ^error ]]; then
+    if [[ $result == *"Not Found"* ]]; then
+      ((error_count++))
+      echo "❌ new crate $crate_name not found on crates.io. make a dummy publication (hint to instructions or mark it as not for publication with 'package.publish = false' in its Cargo.toml"
+    else
+      ((error_count++))
+      echo "❌ $result"
+    fi
+  else
+    readarray -t owners <<<"$result"
 
-		verified_owner_count=0
-		unverified_owner_count=0
-		for owner in "${owners[@]}"; do
-			if [[ -z $owner ]]; then
-				continue
-			fi
-			owner_id="$(echo "$owner" | awk '{print $1}')"
-			if [[ ${verified_crate_owners[$owner_id]} ]]; then
-				((verified_owner_count++))
-				echo "✅ $owner"
-			else
-				((unverified_owner_count++))
-				echo "❌ $owner"
-			fi
-		done
-		if (("$unverified_owner_count" != 0)) || (("$verified_owner_count" == 0)); then
-			((error_count++))
-		fi
-	fi
-	echo ""
+    verified_owner_count=0
+    unverified_owner_count=0
+    for owner in "${owners[@]}"; do
+      if [[ -z $owner ]]; then
+        continue
+      fi
+      owner_id="$(echo "$owner" | awk '{print $1}')"
+      if [[ ${verified_crate_owners[$owner_id]} ]]; then
+        ((verified_owner_count++))
+        echo "✅ $owner"
+      else
+        ((unverified_owner_count++))
+        echo "❌ $owner"
+      fi
+    done
+    if (("$unverified_owner_count" != 0)) || (("$verified_owner_count" == 0)); then
+      ((error_count++))
+    fi
+  fi
+  echo ""
 done
 
 if [ "$error_count" -eq 0 ]; then
-    echo "success"
-	exit 0
+  echo "success"
+  exit 0
 else
-	exit 1
+  exit 1
 fi
