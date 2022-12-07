512f8166ca397abc7b48f5b4a01cd50b8bef63f3
Kirill Lykov <kirill.lykov@solana.com>
2022-12-07 13:49:43 +0000

    fix incorrect cargo existence check

--- scripts/cargo-for-all-lock-files.sh
+++ scripts/cargo-for-all-lock-files.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-if command -v cargo &> /dev/null ; then
+if ! command -v cargo &> /dev/null ; then
   >&2 echo "Failed to find cargo. Mac readlink doesn't support -f. Consider switching
   to gnu readlink with 'brew install coreutils' and then symlink greadlink as
   /usr/local/bin/readlink."
