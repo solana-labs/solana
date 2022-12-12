da49d2343736fafe63d3b9e9359425459299837f
Ryo Onodera <ryoqun@gmail.com>
2022-12-12 21:52:11 +0900

    save

--- core/src/banking_trace.rs
+++ core/src/banking_trace.rs
@@ -634,7 +634,7 @@ impl BankingSimulator {
                         let st = crate::packet_deserializer::PacketDeserializer::deserialize_and_collect_packets(0, &[batch.clone()]).deserialized_packets.iter().filter_map(|ip| ip.build_sanitized_transaction(&bank.feature_set, false, bank.as_ref())).filter_map(|s| bank.check_age_tx(&s).0.ok()).count();
                         format!("{}", st)
                     } else {
-                        ""
+                        "".into()
                     };
                     trace!("event parsed: {}: <{}: {}({}) = {:?}> {:?}", datetime.format("%Y-%m-%d %H:%M:%S.%f"), packet_count, sum, st_label, &batch.0.iter().map(|v| v.len()).collect::<Vec<_>>(), &event);
                 }
