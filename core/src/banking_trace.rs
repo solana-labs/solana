d835c574cdb993576ca8c2071f02d1fe5cf41adf
Ryo Onodera <ryoqun@gmail.com>
2022-12-12 22:36:14 +0900

    save

--- core/src/banking_trace.rs
+++ core/src/banking_trace.rs
@@ -679,7 +679,7 @@ impl BankingSimulator {
         std::thread::spawn(move || {
             let (adjusted_reference, range_iter) = if let Some((most_recent_past_leader_slot, start)) = bank_starts_by_slot.range(..bank_slot).next() {
                 (Some(({
-                    let datetime: chrono::DateTime<chrono::Utc> = start.into();
+                    let datetime: chrono::DateTime<chrono::Utc> = *start.into();
                     datetime.format("%Y-%m-%d %H:%M:%S.%f")
                 }, most_recent_past_leader_slot)), packet_batches_by_time.range(*start..))
             } else {
