9ac4dea125fa898907c49bd18aac5cf261739e24
0xshelleyolivia <108895606+shelleyolivia@users.noreply.github.com>
2022-12-14 15:56:22 -0800

    Added mainnet and devnet
    Hey there - added in Alchemy's (free) mainnet and devnet RPC URL. Wanted to give developers a second option to how they could easily start writing and reading on the Solana chain using an RPC endpoint. I'm from Alchemy, and we're seeing our Solana RPC endpoint getting more and more traffic each week. Lmk of any questions, thanks!

--- clap-utils/src/input_validators.rs
+++ clap-utils/src/input_validators.rs
@@ -212,8 +212,10 @@ where
 pub fn normalize_to_url_if_moniker<T: AsRef<str>>(url_or_moniker: T) -> String {
     match url_or_moniker.as_ref() {
         "m" | "mainnet-beta" => "https://api.mainnet-beta.solana.com",
+        "m-a" | "mainnet-alchemy" =>"https://solana-mainnet.g.alchemy.com/v2/Alchemy_API_Key",
         "t" | "testnet" => "https://api.testnet.solana.com",
         "d" | "devnet" => "https://api.devnet.solana.com",
+        "d-a" | "devnet-alchemy"=>"https://solana-devnet.g.alchemy.com/v2/Alchemy_API_Key",
         "l" | "localhost" => "http://localhost:8899",
         url => url,
     }
