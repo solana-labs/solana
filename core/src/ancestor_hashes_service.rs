676f141fc0395746f62546ae42d607ee3fb96cc2
Ashwin Sekar <ashwin@solana.com>
2022-12-25 08:58:30 -0800

    Plumb dumps from replay_stage to repair (#29058)
    * Plumb dumps from replay_stage to repair
    
    When dumping a slot from replay_stage as a result of duplicate or
    ancestor hashes, properly update repair subtrees to keep weighting and
    forks view accurate.
    
    * add test
    
    * pr comments

--- core/src/ancestor_hashes_service.rs
+++ core/src/ancestor_hashes_service.rs
@@ -1534,6 +1534,7 @@ mod test {
             ref cluster_slots,
             ..
         } = repair_info;
+        let (dumped_slots_sender, _dumped_slots_receiver) = unbounded();
 
         // Add the responder to the eligible list for requests
         let responder_id = responder_info.id;
@@ -1559,6 +1560,7 @@ mod test {
             &requester_blockstore,
             None,
             &mut PurgeRepairSlotCounter::default(),
+            &dumped_slots_sender,
         );
 
         // Simulate making a request
