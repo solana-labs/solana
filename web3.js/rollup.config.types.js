3df3cafe23c1b7500c3384c72fcf7e7348f4a98f
steveluscher <me+github@steveluscher.com>
2022-12-29 22:39:01 +0000

    Don't build typedefs for tests

--- web3.js/rollup.config.types.js
+++ web3.js/rollup.config.types.js
@@ -1,7 +1,7 @@
 import dts from 'rollup-plugin-dts';
 
 export default {
-  input: './declarations/src/index.d.ts',
+  input: './declarations/index.d.ts',
   output: [{file: 'lib/index.d.ts', format: 'es'}],
   plugins: [dts()],
   external: ['http', 'https'],
