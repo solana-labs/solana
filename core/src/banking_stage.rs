1f53fd46e66e5364760909f2556e476f06a7e29d
Ryo Onodera <ryoqun@gmail.com>
2023-01-10 16:31:48 +0900

    another guess....

--- core/src/banking_stage.rs
+++ core/src/banking_stage.rs
@@ -429,14 +429,7 @@ impl BankingStage {
             TOTAL_BUFFERED_PACKETS / ((num_threads - NUM_VOTE_PROCESSING_THREADS) as usize);
         // Keeps track of extraneous vote transactions for the vote threads
         let latest_unprocessed_votes = Arc::new(LatestUnprocessedVotes::new());
-        let mut should_split_voting_threads = bank_forks
-            .read()
-            .map(|bank_forks| {
-                let bank = bank_forks.root_bank();
-                bank.feature_set
-                    .is_active(&allow_votes_to_directly_update_vote_state::id())
-            })
-            .unwrap_or(false);
+        let mut should_split_voting_threads = false;
         info!("should_split_voting_threads: {}", should_split_voting_threads);
         //should_split_voting_threads = false;
         // Many banks that process transactions in parallel.
