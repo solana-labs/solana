36604b40b5f9b6c54e5e976298d406b949a4e1f4
Ryo Onodera <ryoqun@gmail.com>
2023-01-14 16:10:22 +0900

    Bump chrono to v0.4.23

--- tokens/src/db.rs
+++ tokens/src/db.rs
@@ -218,11 +218,11 @@ mod tests {
     #[test]
     fn test_sort_transaction_infos_finalized_first() {
         let info0 = TransactionInfo {
-            finalized_date: Some(Utc.ymd(2014, 7, 8).and_hms(9, 10, 11)),
+            finalized_date: Some(Utc.with_ymd_and_hms(2014, 7, 8, 9, 10, 11).unwrap()),
             ..TransactionInfo::default()
         };
         let info1 = TransactionInfo {
-            finalized_date: Some(Utc.ymd(2014, 7, 8).and_hms(9, 10, 42)),
+            finalized_date: Some(Utc.with_ymd_and_hms(2014, 7, 8, 9, 10, 42).unwrap()),
             ..TransactionInfo::default()
         };
         let info2 = TransactionInfo::default();
