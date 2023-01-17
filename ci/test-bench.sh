8daed75df9a0b5c942ec1cc745a48356db7f46ad
kirill lykov <kirill.lykov@solana.com>
2023-01-17 20:55:41 +0100

    Add toolchain file usage (#29370)
    * use cargo in cargo-for-all-lock-files.sh
    
    * replace ./cargo with cargo in ci shell scripts
    
    * remove ./cargo from test-stable.sh

--- ci/test-bench.sh
+++ ci/test-bench.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
+# To prevent usange of `./cargo` without `nightly`
+# Introduce cargoNighlty and disable warning to use word splitting
+# shellcheck disable=SC2086
 set -e
 cd "$(dirname "$0")/.."
 
@@ -7,7 +10,7 @@ source ci/upload-ci-artifact.sh
 
 eval "$(ci/channel-info.sh)"
 
-cargo="$(readlink -f "./cargo")"
+cargoNightly="$(readlink -f "./cargo") nightly"
 
 set -o pipefail
 export RUST_BACKTRACE=1
@@ -24,7 +27,7 @@ BENCH_FILE=bench_output.log
 BENCH_ARTIFACT=current_bench_results.log
 
 # solana-keygen required when building C programs
-_ "$cargo" build --manifest-path=keygen/Cargo.toml
+_ cargo build --manifest-path=keygen/Cargo.toml
 export PATH="$PWD/target/debug":$PATH
 
 # Clear the C dependency files, if dependency moves these files are not regenerated
@@ -32,43 +35,43 @@ test -d target/debug/sbf && find target/debug/sbf -name '*.d' -delete
 test -d target/release/sbf && find target/release/sbf -name '*.d' -delete
 
 # Ensure all dependencies are built
-_ "$cargo" nightly build --release
+_ $cargoNightly build --release
 
 # Remove "BENCH_FILE", if it exists so that the following commands can append
 rm -f "$BENCH_FILE"
 
 # Run sdk benches
-_ "$cargo" nightly bench --manifest-path sdk/Cargo.toml ${V:+--verbose} \
+_ $cargoNightly bench --manifest-path sdk/Cargo.toml ${V:+--verbose} \
   -- -Z unstable-options --format=json | tee -a "$BENCH_FILE"
 
 # Run runtime benches
-_ "$cargo" nightly bench --manifest-path runtime/Cargo.toml ${V:+--verbose} \
+_ $cargoNightly bench --manifest-path runtime/Cargo.toml ${V:+--verbose} \
   -- -Z unstable-options --format=json | tee -a "$BENCH_FILE"
 
 # Run gossip benches
-_ "$cargo" nightly bench --manifest-path gossip/Cargo.toml ${V:+--verbose} \
+_ $cargoNightly bench --manifest-path gossip/Cargo.toml ${V:+--verbose} \
   -- -Z unstable-options --format=json | tee -a "$BENCH_FILE"
 
 # Run poh benches
-_ "$cargo" nightly bench --manifest-path poh/Cargo.toml ${V:+--verbose} \
+_ $cargoNightly bench --manifest-path poh/Cargo.toml ${V:+--verbose} \
   -- -Z unstable-options --format=json | tee -a "$BENCH_FILE"
 
 # Run core benches
-_ "$cargo" nightly bench --manifest-path core/Cargo.toml ${V:+--verbose} \
+_ $cargoNightly bench --manifest-path core/Cargo.toml ${V:+--verbose} \
   -- -Z unstable-options --format=json | tee -a "$BENCH_FILE"
 
 # Run sbf benches
-_ "$cargo" nightly bench --manifest-path programs/sbf/Cargo.toml ${V:+--verbose} --features=sbf_c \
+_ $cargoNightly bench --manifest-path programs/sbf/Cargo.toml ${V:+--verbose} --features=sbf_c \
   -- -Z unstable-options --format=json --nocapture | tee -a "$BENCH_FILE"
 
 # Run banking/accounts bench. Doesn't require nightly, but use since it is already built.
-_ "$cargo" nightly run --release --manifest-path banking-bench/Cargo.toml ${V:+--verbose} | tee -a "$BENCH_FILE"
-_ "$cargo" nightly run --release --manifest-path accounts-bench/Cargo.toml ${V:+--verbose} -- --num_accounts 10000 --num_slots 4 | tee -a "$BENCH_FILE"
+_ $cargoNightly run --release --manifest-path banking-bench/Cargo.toml ${V:+--verbose} | tee -a "$BENCH_FILE"
+_ $cargoNightly run --release --manifest-path accounts-bench/Cargo.toml ${V:+--verbose} -- --num_accounts 10000 --num_slots 4 | tee -a "$BENCH_FILE"
 
 # `solana-upload-perf` disabled as it can take over 30 minutes to complete for some
 # reason
 exit 0
-_ "$cargo" nightly run --release --package solana-upload-perf \
+_ $cargoNightly run --release --package solana-upload-perf \
   -- "$BENCH_FILE" "$TARGET_BRANCH" "$UPLOAD_METRICS" | tee "$BENCH_ARTIFACT"
 
 upload-ci-artifact "$BENCH_FILE"
