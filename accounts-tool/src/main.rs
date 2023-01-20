0db6e78dd725ce1b219ef03c7bff1c6aea9d9b3c
Yueh-Hsuan Chiang <yueh-hsuan@solana.com>
2023-01-19 23:00:30 -0800

    Template implementaiton for accounts-tool

--- accounts-tool/src/main.rs
+++ accounts-tool/src/main.rs
@@ -1,1 +1,30 @@
-fn main() {}
+#![allow(clippy::integer_arithmetic)]
+use {
+    clap::{crate_description, crate_name, App, AppSettings},
+    log::*,
+    solana_measure::measure::Measure,
+    std::process::exit,
+};
+
+#[allow(clippy::cognitive_complexity)]
+fn main() {
+    solana_logger::setup_with_default("solana=info");
+
+    let mut measure_total_execution_time = Measure::start("accounts tool");
+    let matches = App::new(crate_name!())
+        .about(crate_description!())
+        .version(solana_version::version!())
+        .setting(AppSettings::InferSubcommands)
+        .setting(AppSettings::SubcommandRequiredElseHelp)
+        .setting(AppSettings::VersionlessSubcommands)
+        .get_matches();
+    match matches.subcommand() {
+        ("", _) => {
+            eprintln!("{}", matches.usage());
+            measure_total_execution_time.stop();
+            info!("{}", measure_total_execution_time);
+            exit(1);
+        }
+        _ => unreachable!(),
+    };
+}
