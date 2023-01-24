083e4460aeff72834a6f09e3ccdaba274382c604
Andrew Fitzgerald <apfitzge@gmail.com>
2023-01-24 17:45:25 +0000

    Check that halt_slot >= starting_slot

--- ledger-tool/src/main.rs
+++ ledger-tool/src/main.rs
@@ -1066,7 +1066,13 @@ fn load_bank_forks(
     };
 
     if let Some(halt_slot) = process_options.halt_at_slot {
-        // Check if we have the slot data necessary to replay from starting_slot to >= halt_slot.
+        if halt_slot < starting_slot {
+            eprintln!(
+                "Unable to load bank forks at slot {halt_slot} because it is less than the starting slot {starting_slot}."
+            );
+            exit(1);
+        }
+        // Check if we have the slot data necessary to replay from starting_slot to <= halt_slot.
         //  - This will not catch the case when loading from genesis without a full slot 0.
         if !blockstore.slot_range_connected(starting_slot, halt_slot) {
             eprintln!("Unable to load bank forks at slot {halt_slot} due to disconnected blocks.",);
