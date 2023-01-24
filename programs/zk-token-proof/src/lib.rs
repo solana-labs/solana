8ef0a021567ac7a301ddfbc5b9791a031ebac8ae
Sam Kim <skim13@cs.stanford.edu>
2023-01-24 21:48:49 +0900

    add proof context program to zk-token-proof program

--- programs/zk-token-proof/src/lib.rs
+++ programs/zk-token-proof/src/lib.rs
@@ -2,27 +2,55 @@
 
 use {
     bytemuck::Pod,
-    solana_program_runtime::{ic_msg, invoke_context::InvokeContext},
+    solana_program_runtime::{
+        ic_msg, invoke_context::InvokeContext, sysvar_cache::get_sysvar_with_account_check,
+    },
     solana_sdk::instruction::{InstructionError, TRANSACTION_LEVEL_STACK_HEIGHT},
-    solana_zk_token_sdk::zk_token_proof_instruction::*,
+    solana_zk_token_sdk::{zk_token_proof_instruction::*, zk_token_proof_program::id},
     std::result::Result,
 };
 
-fn verify<T: Pod + Verifiable>(invoke_context: &mut InvokeContext) -> Result<(), InstructionError> {
+fn verify<T: Pod + ZkProofData>(
+    invoke_context: &mut InvokeContext,
+) -> Result<(), InstructionError> {
     let transaction_context = &invoke_context.transaction_context;
     let instruction_context = transaction_context.get_current_instruction_context()?;
     let instruction_data = instruction_context.get_instruction_data();
     let instruction = ProofInstruction::decode_data::<T>(instruction_data);
 
-    let proof = instruction.ok_or_else(|| {
+    let proof_data = instruction.ok_or_else(|| {
         ic_msg!(invoke_context, "invalid proof data");
         InstructionError::InvalidInstructionData
     })?;
 
-    proof.verify().map_err(|err| {
+    proof_data.verify_proof().map_err(|err| {
         ic_msg!(invoke_context, "proof verification failed: {:?}", err);
         InstructionError::InvalidInstructionData
-    })
+    })?;
+
+    if proof_data.create_context_state() {
+        let mut proof_context_account =
+            instruction_context.try_borrow_instruction_account(transaction_context, 0)?;
+        if *proof_context_account.get_owner() != id() {
+            return Err(InstructionError::InvalidAccountOwner);
+        }
+        if proof_context_account.get_data().len() != proof_data.context_data().len() {
+            return Err(InstructionError::InvalidAccountData);
+        }
+
+        let rent = get_sysvar_with_account_check::rent(invoke_context, instruction_context, 1)?;
+
+        if !rent.is_exempt(
+            proof_context_account.get_lamports(),
+            proof_context_account.get_data().len(),
+        ) {
+            return Err(InstructionError::InsufficientFunds);
+        }
+
+        proof_context_account.set_data_from_slice(proof_data.context_data())?;
+    }
+
+    Ok(())
 }
 
 pub fn process_instruction(invoke_context: &mut InvokeContext) -> Result<(), InstructionError> {
@@ -67,5 +95,86 @@ pub fn process_instruction(invoke_context: &mut InvokeContext) -> Result<(), Ins
             ic_msg!(invoke_context, "VerifyPubkeyValidity");
             verify::<PubkeyValidityData>(invoke_context)
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use {
+        super::*,
+        solana_program_runtime::invoke_context::mock_process_instruction,
+        solana_sdk::{
+            account::{self, AccountSharedData},
+            instruction::AccountMeta,
+            pubkey::Pubkey,
+            rent::Rent,
+            sysvar,
+        },
+        solana_zk_token_sdk::{encryption::elgamal::ElGamalKeypair, instruction::CloseAccountData},
+    };
+
+    fn process_instruction(
+        instruction_data: &[u8],
+        transaction_accounts: Vec<(Pubkey, AccountSharedData)>,
+        instruction_accounts: Vec<AccountMeta>,
+        expected_result: Result<(), InstructionError>,
+    ) -> Vec<AccountSharedData> {
+        mock_process_instruction(
+            &id(),
+            Vec::new(),
+            instruction_data,
+            transaction_accounts,
+            instruction_accounts,
+            None,
+            None,
+            expected_result,
+            super::process_instruction,
+        )
+    }
+
+    fn create_default_rent_account() -> AccountSharedData {
+        account::create_account_shared_data_for_test(&Rent::default())
+    }
+
+    #[test]
+    fn test_close_account() {
+        let elgamal_keypair = ElGamalKeypair::new_rand();
+        let ciphertext = elgamal_keypair.public.encrypt(0_u64);
+        let close_account_data =
+            CloseAccountData::new(&elgamal_keypair, &ciphertext, true).unwrap();
+        let proof_context_length = close_account_data.context_data().len();
+
+        let rent = Rent::default();
+        let rent_exempt_reserve = rent.minimum_balance(proof_context_length);
+        let proof_context_address = solana_sdk::pubkey::new_rand();
+        let proof_context_account =
+            AccountSharedData::new(rent_exempt_reserve, proof_context_length, &id());
+
+        let instruction_accounts = vec![
+            AccountMeta {
+                pubkey: proof_context_address,
+                is_signer: false,
+                is_writable: true,
+            },
+            AccountMeta {
+                pubkey: sysvar::rent::id(),
+                is_signer: false,
+                is_writable: false,
+            },
+        ];
+
+        let instruction_data = ProofInstruction::VerifyCloseAccount
+            .encode(&close_account_data, Some(&proof_context_address))
+            .data;
+
+        process_instruction(
+            &instruction_data,
+            vec![
+                (proof_context_address, proof_context_account),
+                (sysvar::rent::id(), create_default_rent_account()),
+            ],
+            instruction_accounts,
+            Ok(()),
+        );
     }
 }
--- zk-token-sdk/src/zk_token_proof_instruction.rs
+++ zk-token-sdk/src/zk_token_proof_instruction.rs
@@ -4,7 +4,11 @@ use {
     bytemuck::{bytes_of, Pod},
     num_derive::{FromPrimitive, ToPrimitive},
     num_traits::{FromPrimitive, ToPrimitive},
-    solana_program::instruction::Instruction,
+    solana_program::{
+        instruction::{AccountMeta, Instruction},
+        pubkey::Pubkey,
+        sysvar,
+    },
 };
 
 #[derive(Clone, Copy, Debug, FromPrimitive, ToPrimitive, PartialEq, Eq)]
@@ -13,6 +17,12 @@ pub enum ProofInstruction {
     /// Verify a `CloseAccountData` struct
     ///
     /// Accounts expected by this instruction:
+    ///
+    ///   * Creating a proof context account
+    ///   0. `[WRITE]` Uninitialized
+    ///   1. `[]` Rent sysvar
+    ///
+    ///   * Otherwise
     ///   None
     ///
     /// Data expected by this instruction:
@@ -23,6 +33,12 @@ pub enum ProofInstruction {
     /// Verify a `WithdrawData` struct
     ///
     /// Accounts expected by this instruction:
+    ///
+    ///   * Creating a proof context account
+    ///   0. `[WRITE]` Uninitialized
+    ///   1. `[]` Rent sysvar
+    ///
+    ///   * Otherwise
     ///   None
     ///
     /// Data expected by this instruction:
@@ -33,6 +49,12 @@ pub enum ProofInstruction {
     /// Verify a `WithdrawWithheldTokensData` struct
     ///
     /// Accounts expected by this instruction:
+    ///
+    ///   * Creating a proof context account
+    ///   0. `[WRITE]` Uninitialized
+    ///   1. `[]` Rent sysvar
+    ///
+    ///   * Otherwise
     ///   None
     ///
     /// Data expected by this instruction:
@@ -43,6 +65,12 @@ pub enum ProofInstruction {
     /// Verify a `TransferData` struct
     ///
     /// Accounts expected by this instruction:
+    ///
+    ///   * Creating a proof context account
+    ///   0. `[WRITE]` Uninitialized
+    ///   1. `[]` Rent sysvar
+    ///
+    ///   * Otherwise
     ///   None
     ///
     /// Data expected by this instruction:
@@ -53,6 +81,12 @@ pub enum ProofInstruction {
     /// Verify a `TransferWithFeeData` struct
     ///
     /// Accounts expected by this instruction:
+    ///
+    ///   * Creating a proof context account
+    ///   0. `[WRITE]` Uninitialized
+    ///   1. `[]` Rent sysvar
+    ///
+    ///   * Otherwise
     ///   None
     ///
     /// Data expected by this instruction:
@@ -63,6 +97,12 @@ pub enum ProofInstruction {
     /// Verify a `PubkeyValidityData` struct
     ///
     /// Accounts expected by this instruction:
+    ///
+    ///   * Creating a proof context account
+    ///   0. `[WRITE]` Uninitialized
+    ///   1. `[]` Rent sysvar
+    ///
+    ///   * Otherwise
     ///   None
     ///
     /// Data expected by this instruction:
@@ -72,12 +112,26 @@ pub enum ProofInstruction {
 }
 
 impl ProofInstruction {
-    pub fn encode<T: Pod>(&self, proof: &T) -> Instruction {
+    pub fn encode<T: Pod + ZkProofData>(
+        &self,
+        proof_data: &T,
+        context_account: Option<&Pubkey>,
+    ) -> Instruction {
+        let accounts = if let Some(context_account) = context_account {
+            vec![
+                AccountMeta::new(*context_account, false),
+                AccountMeta::new_readonly(sysvar::rent::id(), false),
+            ]
+        } else {
+            vec![]
+        };
+
         let mut data = vec![ToPrimitive::to_u8(self).unwrap()];
-        data.extend_from_slice(bytes_of(proof));
+        data.extend_from_slice(bytes_of(proof_data));
+
         Instruction {
             program_id: crate::zk_token_proof_program::id(),
-            accounts: vec![],
+            accounts,
             data,
         }
     }
@@ -95,26 +149,38 @@ impl ProofInstruction {
     }
 }
 
-pub fn verify_close_account(proof_data: &CloseAccountData) -> Instruction {
-    ProofInstruction::VerifyCloseAccount.encode(proof_data)
+pub fn verify_close_account(
+    proof_data: &CloseAccountData,
+    context_account: Option<&Pubkey>,
+) -> Instruction {
+    ProofInstruction::VerifyCloseAccount.encode(proof_data, context_account)
 }
 
-pub fn verify_withdraw(proof_data: &WithdrawData) -> Instruction {
-    ProofInstruction::VerifyWithdraw.encode(proof_data)
+pub fn verify_withdraw(proof_data: &WithdrawData, context_account: Option<&Pubkey>) -> Instruction {
+    ProofInstruction::VerifyWithdraw.encode(proof_data, context_account)
 }
 
-pub fn verify_withdraw_withheld_tokens(proof_data: &WithdrawWithheldTokensData) -> Instruction {
-    ProofInstruction::VerifyWithdrawWithheldTokens.encode(proof_data)
+pub fn verify_withdraw_withheld_tokens(
+    proof_data: &WithdrawWithheldTokensData,
+    context_account: Option<&Pubkey>,
+) -> Instruction {
+    ProofInstruction::VerifyWithdrawWithheldTokens.encode(proof_data, context_account)
 }
 
-pub fn verify_transfer(proof_data: &TransferData) -> Instruction {
-    ProofInstruction::VerifyTransfer.encode(proof_data)
+pub fn verify_transfer(proof_data: &TransferData, context_account: Option<&Pubkey>) -> Instruction {
+    ProofInstruction::VerifyTransfer.encode(proof_data, context_account)
 }
 
-pub fn verify_transfer_with_fee(proof_data: &TransferWithFeeData) -> Instruction {
-    ProofInstruction::VerifyTransferWithFee.encode(proof_data)
+pub fn verify_transfer_with_fee(
+    proof_data: &TransferWithFeeData,
+    context_account: Option<&Pubkey>,
+) -> Instruction {
+    ProofInstruction::VerifyTransferWithFee.encode(proof_data, context_account)
 }
 
-pub fn verify_pubkey_validity(proof_data: &PubkeyValidityData) -> Instruction {
-    ProofInstruction::VerifyPubkeyValidity.encode(proof_data)
+pub fn verify_pubkey_validity(
+    proof_data: &PubkeyValidityData,
+    context_account: Option<&Pubkey>,
+) -> Instruction {
+    ProofInstruction::VerifyPubkeyValidity.encode(proof_data, context_account)
 }
