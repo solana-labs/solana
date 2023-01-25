a67d239dde1678709173a61c2f535447121927f0
Yihau Chen <a122092487@gmail.com>
2023-01-25 13:59:59 +0800

    chore: workspace inheritance (#29509)
    * introduce workspace.package
    
    * introduce workspace.dependencies
    
    * read version from root cargo.toml
    
    * pass check when version = { workspace = true }
    
    * don't bump version when version = { workspace = true }
    
    * including workspace Cargo.toml when bump version
    
    * programs/sbf use workspace inheritance
    
    * fix increasing cargo version ignore program/sbf/Cargo.toml

--- scripts/increment-cargo-version.sh
+++ scripts/increment-cargo-version.sh
@@ -32,7 +32,7 @@ for ignore in "${ignores[@]}"; do
 done
 
 # shellcheck disable=2207
-Cargo_tomls=($(find . -mindepth 2 -name Cargo.toml "${not_paths[@]}"))
+Cargo_tomls=($(find . -name Cargo.toml "${not_paths[@]}"))
 
 # Collect the name of all the internal crates
 crates=()
@@ -46,7 +46,7 @@ MINOR=0
 PATCH=0
 SPECIAL=""
 
-semverParseInto "$(readCargoVariable version "${Cargo_tomls[0]}")" MAJOR MINOR PATCH SPECIAL
+semverParseInto "$(readCargoVariable version Cargo.toml)" MAJOR MINOR PATCH SPECIAL
 [[ -n $MAJOR ]] || usage
 
 currentVersion="$MAJOR\.$MINOR\.$PATCH$SPECIAL"
@@ -80,6 +80,9 @@ dropspecial)
 check)
   badTomls=()
   for Cargo_toml in "${Cargo_tomls[@]}"; do
+    if grep "^version = { workspace = true }" "$Cargo_toml" &>/dev/null; then
+      continue
+    fi
     if ! grep "^version *= *\"$currentVersion\"$" "$Cargo_toml" &>/dev/null; then
       badTomls+=("$Cargo_toml")
     fi
@@ -119,6 +122,11 @@ newVersion="$MAJOR.$MINOR.$PATCH$SPECIAL"
 
 # Update all the Cargo.toml files
 for Cargo_toml in "${Cargo_tomls[@]}"; do
+  # ignore when version inheritant from workspace (exclude programs/sbf/Cargo.toml)
+  if grep "^version = { workspace = true }" "$Cargo_toml" &>/dev/null && Cargo_toml && ! [[ $Cargo_toml =~ programs/sbf/Cargo.toml ]]; then
+    continue
+  fi
+
   # Set new crate version
   (
     set -x
