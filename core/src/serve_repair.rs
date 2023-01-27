327d7ffd814442e5212d7cf2633ead0c263af901
Jeff Biseda <jbiseda@gmail.com>
2023-01-27 15:50:18 -0800

    track repair handle_requests time (#29940)
    (cherry picked from commit 7cacbdcca21976e69ece1c1d91a68c765a69e078)

--- core/src/serve_repair.rs
+++ core/src/serve_repair.rs
@@ -171,6 +171,7 @@ struct ServeRepairStats {
     ping_cache_check_failed: usize,
     pings_sent: usize,
     decode_time_us: u64,
+    handle_requests_time_us: u64,
     err_time_skew: usize,
     err_malformed: usize,
     err_sig_verify: usize,
@@ -524,7 +525,8 @@ impl ServeRepair {
             decoded_reqs.truncate(MAX_REQUESTS_PER_ITERATION);
         }
 
-        self.handle_packets(
+        let handle_requests_start = Instant::now();
+        self.handle_requests(
             ping_cache,
             recycler,
             blockstore,
@@ -534,6 +536,7 @@ impl ServeRepair {
             data_budget,
             cluster_type,
         );
+        stats.handle_requests_time_us += handle_requests_start.elapsed().as_micros() as u64;
 
         Ok(())
     }
@@ -610,6 +613,11 @@ impl ServeRepair {
             ),
             ("pings_sent", stats.pings_sent, i64),
             ("decode_time_us", stats.decode_time_us, i64),
+            (
+                "handle_requests_time_us",
+                stats.handle_requests_time_us,
+                i64
+            ),
             ("err_time_skew", stats.err_time_skew, i64),
             ("err_malformed", stats.err_malformed, i64),
             ("err_sig_verify", stats.err_sig_verify, i64),
@@ -775,7 +783,7 @@ impl ServeRepair {
         (check, ping_pkt)
     }
 
-    fn handle_packets(
+    fn handle_requests(
         &self,
         ping_cache: &mut PingCache,
         recycler: &PacketBatchRecycler,
