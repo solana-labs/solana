49042ac3ae1c62cc214ac9b1fef4a22bb468bc51
Sam Kim <skim13@cs.stanford.edu>
2023-01-31 09:33:11 +0900

    clean up pod

--- zk-token-sdk/src/zk_token_elgamal/pod.rs
+++ zk-token-sdk/src/zk_token_elgamal/pod.rs
@@ -1,33 +1,5 @@
 pub use bytemuck::{Pod, Zeroable};
-use {crate::errors::ProofError, solana_program::pubkey::Pubkey, std::fmt};
-
-#[derive(Clone, Copy, Debug, Default, PartialEq, Pod, Zeroable)]
-#[repr(transparent)]
-pub struct OptionalNonZeroPubkey(Pubkey);
-impl TryFrom<Option<Pubkey>> for OptionalNonZeroPubkey {
-    type Error = ProofError;
-    fn try_from(p: Option<Pubkey>) -> Result<Self, Self::Error> {
-        match p {
-            None => Ok(Self(Pubkey::default())),
-            Some(pubkey) => {
-                if pubkey == Pubkey::default() {
-                    Err(ProofError::PubkeyDeserialization)
-                } else {
-                    Ok(Self(pubkey))
-                }
-            }
-        }
-    }
-}
-impl From<OptionalNonZeroPubkey> for Option<Pubkey> {
-    fn from(p: OptionalNonZeroPubkey) -> Self {
-        if p.0 == Pubkey::default() {
-            None
-        } else {
-            Some(p.0)
-        }
-    }
-}
+use std::fmt;
 
 #[derive(Clone, Copy, Debug, Default, PartialEq, Eq, Pod, Zeroable)]
 #[repr(transparent)]
