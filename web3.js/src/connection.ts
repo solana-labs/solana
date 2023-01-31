b627b819648306adce6f4ffe12983f18a59dbdf2
steveluscher <me+github@steveluscher.com>
2023-01-31 06:33:04 +0000

    fix: correct return type of getFeeForMessage

--- web3.js/src/connection.ts
+++ web3.js/src/connection.ts
@@ -4452,7 +4452,7 @@ export class Connection {
   async getFeeForMessage(
     message: VersionedMessage,
     commitment?: Commitment,
-  ): Promise<RpcResponseAndContext<number>> {
+  ): Promise<RpcResponseAndContext<number | null>> {
     const wireMessage = toBuffer(message.serialize()).toString('base64');
     const args = this._buildArgs([wireMessage], commitment);
     const unsafeRes = await this._rpcRequest('getFeeForMessage', args);
