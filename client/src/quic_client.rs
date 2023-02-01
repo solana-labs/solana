d25bb1a3fa505ac1101a01802dd12b97499181bb
Lijun Wang <83639177+lijunwangs@users.noreply.github.com>
2023-02-01 15:41:21 -0800

    Give more explicit alternative
    Co-authored-by: Tyera <teulberg@gmail.com>

--- client/src/quic_client.rs
+++ client/src/quic_client.rs
@@ -1,2 +1,2 @@
-#[deprecated(since = "1.16.0", note = "Please use `QuicClientConnection` instead.")]
+#[deprecated(since = "1.16.0", note = "Please use `solana_quic_client::quic_client::QuicClientConnection` instead.")]
 pub use solana_quic_client::quic_client::QuicClientConnection as QuicTpuConnection;
