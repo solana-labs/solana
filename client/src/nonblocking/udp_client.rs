e2e366e97957cfb7ab7e7f40e661e56abdf18b39
Lijun Wang <83639177+lijunwangs@users.noreply.github.com>
2023-02-01 15:40:55 -0800

    Give more explicit alternative
    Co-authored-by: Tyera <teulberg@gmail.com>

--- client/src/nonblocking/udp_client.rs
+++ client/src/nonblocking/udp_client.rs
@@ -1,2 +1,2 @@
-#[deprecated(since = "1.16.0", note = "Please use `UdpClientConnection` instead.")]
+#[deprecated(since = "1.16.0", note = "Please use `solana_udp_client::nonblocking::udp_client::UdpClientConnection` instead.")]
 pub use solana_udp_client::nonblocking::udp_client::UdpClientConnection as UdpTpuConnection;
