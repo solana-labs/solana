52a2b106eedcb53594c766a42266842f89076270
Illia Bobyr <illia.bobyr@solana.com>
2023-01-31 21:19:11 -0800

    doc: rpc: Document `MaxSlots::shred_insert`
    Current field name is a bit short to understand that exactly does it
    store.

--- rpc/src/max_slots.rs
+++ rpc/src/max_slots.rs
@@ -3,5 +3,8 @@ use std::sync::atomic::AtomicU64;
 #[derive(Default)]
 pub struct MaxSlots {
     pub retransmit: AtomicU64,
+
+    /// Maximum observed `solana_core::Slot` index, seen during the shred insertion operation.
+    // TODO Should this be renamed to `max_shred_insert_slot`?
     pub shred_insert: AtomicU64,
 }
