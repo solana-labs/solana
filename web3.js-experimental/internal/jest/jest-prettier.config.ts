1cab61d3b7a640854c62f311d9ca54b85b972195
Steven Luscher <steveluscher@users.noreply.github.com>
2023-02-01 17:50:04 -0800

    Move experimental web3.js folder out of monorepo (#30072)

--- web3.js-experimental/internal/jest/jest-prettier.config.ts
+++
@@ -1,9 +0,0 @@
-import type { Config } from 'jest';
-
-export default {
-    displayName: 'Prettier',
-    moduleFileExtensions: ['js', 'ts', 'json', 'md'],
-    rootDir: '../../',
-    runner: 'prettier',
-    testMatch: ['<rootDir>README.md', '<rootDir>internal/**', '<rootDir>src/**', '<rootDir>*'],
-} as Config;
