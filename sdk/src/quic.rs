6e0636799a83f82baae9836d4b3bfac8b6d8c0ba
Ryo Onodera <ryoqun@gmail.com>
2023-02-05 22:22:59 +0900

    Revert "Revert "Revert "Tweak quic for mango bench"""
    This reverts commit 096e18b54fa2a361f711d64e1e23b0f77dd78b36.

--- sdk/src/quic.rs
+++ sdk/src/quic.rs
@@ -4,7 +4,7 @@ pub const QUIC_PORT_OFFSET: u16 = 6;
 // Empirically found max number of concurrent streams
 // that seems to maximize TPS on GCE (higher values don't seem to
 // give significant improvement or seem to impact stability)
-pub const QUIC_MAX_UNSTAKED_CONCURRENT_STREAMS: usize = 100_000;
+pub const QUIC_MAX_UNSTAKED_CONCURRENT_STREAMS: usize = 128;
 pub const QUIC_MIN_STAKED_CONCURRENT_STREAMS: usize = 128;
 
 pub const QUIC_TOTAL_STAKED_CONCURRENT_STREAMS: usize = 100_000;
@@ -22,7 +22,7 @@ pub const QUIC_CONNECTION_HANDSHAKE_TIMEOUT_MS: u64 = 60_000;
 
 /// The receive window for QUIC connection from unstaked nodes is
 /// set to this ratio times [`solana_sdk::packet::PACKET_DATA_SIZE`]
-pub const QUIC_UNSTAKED_RECEIVE_WINDOW_RATIO: u64 = 10;
+pub const QUIC_UNSTAKED_RECEIVE_WINDOW_RATIO: u64 = 1;
 
 /// The receive window for QUIC connection from minimum staked nodes is
 /// set to this ratio times [`solana_sdk::packet::PACKET_DATA_SIZE`]
