4551b0b596ac27c65c7053d0c05932278eccfc4a
Ryo Onodera <ryoqun@gmail.com>
2023-02-07 22:14:23 +0900

    save

--- runtime/src/bank.rs
+++ runtime/src/bank.rs
@@ -1034,7 +1034,7 @@ struct Scheduler<C> {
     bank: std::sync::Arc<std::sync::RwLock<std::option::Option<std::sync::Weak<Bank>>>>,
     slot: AtomicU64,
     commit_status: Arc<CommitStatus>,
-    last_checkpoint: Option<solana_scheduler::Checkpoint>,
+    last_checkpoint: Option<solana_scheduler::Checkpoint<ExecuteTimings>>,
 }
 
 impl<C> Scheduler<C> {
