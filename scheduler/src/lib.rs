a80496fde9b97407ba492c3167f78a22f9c018fc
Ryo Onodera <ryoqun@gmail.com>
2023-02-07 22:24:27 +0900

    save

--- scheduler/src/lib.rs
+++ scheduler/src/lib.rs
@@ -2111,7 +2111,7 @@ impl ScheduleStage {
         max_executing_queue_count: usize,
         runnable_queue: &mut TaskQueue,
         address_book: &mut AddressBook,
-        from: &crossbeam_channel::Receiver<SchedulablePayload<C>>,
+        from: &crossbeam_channel::Receiver<SchedulablePayload<C, B>>,
         to_execute_substage: &crossbeam_channel::Sender<ExecutablePayload>,
         to_high_execute_substage: Option<&crossbeam_channel::Sender<ExecutablePayload>>,
         from_execute_substage: &crossbeam_channel::Receiver<UnlockablePayload<T>>,
