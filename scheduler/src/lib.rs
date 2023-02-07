a8a3e76eef9f5deadaca694c452d863f76339058
Ryo Onodera <ryoqun@gmail.com>
2023-02-07 22:24:36 +0900

    save

--- scheduler/src/lib.rs
+++ scheduler/src/lib.rs
@@ -2115,7 +2115,7 @@ impl ScheduleStage {
         to_execute_substage: &crossbeam_channel::Sender<ExecutablePayload>,
         to_high_execute_substage: Option<&crossbeam_channel::Sender<ExecutablePayload>>,
         from_execute_substage: &crossbeam_channel::Receiver<UnlockablePayload<T>>,
-        maybe_to_next_stage: Option<&crossbeam_channel::Sender<ExaminablePayload<T, C>>>, // assume nonblocking
+        maybe_to_next_stage: Option<&crossbeam_channel::Sender<ExaminablePayload<T, C, B>>>, // assume nonblocking
     ) -> Option<std::sync::Arc<Checkpoint<C, B>>> {
         #[derive(Clone, Copy, Debug)]
         struct AtTopOfScheduleThread;
