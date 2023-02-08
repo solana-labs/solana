4a3fd7a6a9662545c7c837d10b5cd974b45fac61
Ryo Onodera <ryoqun@gmail.com>
2023-02-08 20:27:59 +0900

    save

--- runtime/src/bank.rs
+++ runtime/src/bank.rs
@@ -1411,7 +1411,7 @@ impl Scheduler<ExecuteTimings> {
                                     ("compute_units", ee.cu, i64),
                                     ("priority", ee.task.tx.0.get_transaction_priority_details().map(|d| d.priority).unwrap_or_default(), i64),
                                 );
-                                info!("execute_substage: slot: {} transaction_index: {} timings: {:?}", ee.task.slot, ee.task.transaction_index(), timings);
+                                info!("execute_substage: slot: {} transaction_index: {} timings: {:?}", latest_runner_context.as_ref().unwrap().slot(), ee.task.transaction_index(), timings);
                             }
 
                             if let Some(result) = ee.execution_result.take() {
