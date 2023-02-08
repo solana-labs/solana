f644b99dadbb16f7fffda0eac1f70cbd977e35e2
Ryo Onodera <ryoqun@gmail.com>
2023-02-08 14:13:29 +0900

    save

--- scheduler/src/lib.rs
+++ scheduler/src/lib.rs
@@ -1672,7 +1672,7 @@ impl ScheduleStage {
         from_exec: &crossbeam_channel::Receiver<UnlockablePayload<T>>,
         maybe_to_next_stage: Option<&crossbeam_channel::Sender<ExaminablePayload<T, C, B>>>, // assume nonblocking
         never: &'a crossbeam_channel::Receiver<SchedulablePayload<C, B>>,
-        log_prefix: impl Fn() -> String,
+        log_prefix: impl Fn(&Option<B>) -> String,
     ) -> Option<std::sync::Arc<Checkpoint<C, B>>> {
         let mut maybe_start_time = None;
         let mut slot = None;
