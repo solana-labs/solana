0a729dc8a0d2cce8f3e636a15a01480883a975cb
Ryo Onodera <ryoqun@gmail.com>
2023-02-08 20:36:00 +0900

    save

--- runtime/src/bank.rs
+++ runtime/src/bank.rs
@@ -1622,7 +1622,7 @@ impl<C> Scheduler<C> {
     }
 
     fn resume_commit_into_bank(&self, bank: Option<&Arc<Bank>>) {
-        self.current_checkpoint.replace_context_value(RunnerContext { bank: bank.cloned() });
+        self.current_checkpoint.replace_context_value(|c| c.bank = bank);
         self.commit_status.notify_as_resumed();
     }
 
