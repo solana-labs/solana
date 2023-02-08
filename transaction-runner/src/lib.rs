baf0d1dc89469c89b0139be5ecb1d51fcf6640f8
Ryo Onodera <ryoqun@gmail.com>
2023-02-08 13:44:47 +0900

    save

--- transaction-runner/src/lib.rs
+++ transaction-runner/src/lib.rs
@@ -1,4 +1,4 @@
 use std::sync::Arc;
 use solana_runtime::bank::Bank;
 
-struct TransactionRunner(Arc<Bank>, PohRecorder);
+struct TransactionRunner(Arc<Bank>, solana_poh::poh_recorder::PohRecorder);
