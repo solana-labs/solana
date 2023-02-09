6689b31a4ced483fa7f4d47002ce5c7bbb309c0f
Ryo Onodera <ryoqun@gmail.com>
2023-02-09 14:09:42 +0900

    save

--- runtime/src/bank_forks.rs
+++ runtime/src/bank_forks.rs
@@ -77,9 +77,9 @@ impl Index<u64> for BankForks {
 }
 
 impl BankForks {
-    pub fn new(bank: Bank) -> Self {
+    pub fn new(bank: Bank, a: usize) -> Self {
         let root = bank.slot();
-        Self::new_from_banks(&[Arc::new(bank)], root)
+        Self::new_from_banks(&[Arc::new(bank)], root, a: usize)
     }
 
     pub fn banks(&self) -> HashMap<Slot, Arc<Bank>> {
@@ -154,7 +154,7 @@ impl BankForks {
         self[self.root()].clone()
     }
 
-    pub fn new_from_banks(initial_forks: &[Arc<Bank>], root: Slot) -> Self {
+    pub fn new_from_banks(initial_forks: &[Arc<Bank>], root: Slot, a: usize) -> Self {
         let mut banks = HashMap::new();
 
         // Iterate through the heads of all the different forks
