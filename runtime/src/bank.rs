9e37c7e9209986a38132df995488f8172291a9ef
Ryo Onodera <ryoqun@gmail.com>
2023-02-09 14:05:26 +0900

    save

--- runtime/src/bank.rs
+++ runtime/src/bank.rs
@@ -1294,7 +1294,7 @@ impl Scheduler {
                         if !executed_transactions.is_empty() {
                             let hash = solana_entry::entry::hash_transactions(&executed_transactions);
                             let poh = transaction_runner.poh.read().unwrap();
-                            let res = poh.as_ref().unwrap()(bank.as_ref(), executed_transactions, hash);
+                            let res = poh.as_ref().unwrap().0(bank.as_ref(), executed_transactions, hash);
                             match res {
                                 Ok(aa) => aa,
                                 Err(e) => {
