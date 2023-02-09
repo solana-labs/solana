9e6e9d687ee9f3c6b4599ef236c3da8c6e88d7dc
brooks <brooks@solana.com>
2023-02-09 14:22:56 -0500

    fix ref

--- sdk/program/src/program.rs
+++ sdk/program/src/program.rs
@@ -311,7 +311,7 @@ pub fn invoke_signed_unchecked(
     }
 
     #[cfg(not(target_os = "solana"))]
-    crate::program_stubs::sol_invoke_signed(&instruction, account_infos, signers_seeds)
+    crate::program_stubs::sol_invoke_signed(instruction, account_infos, signers_seeds)
 }
 
 /// Maximum size that can be set using [`set_return_data`].
