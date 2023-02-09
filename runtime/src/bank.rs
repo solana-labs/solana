a27d5dec1ea238e4176424ce468d06887d05a092
Ryo Onodera <ryoqun@gmail.com>
2023-02-09 14:03:30 +0900

    save

--- runtime/src/bank.rs
+++ runtime/src/bank.rs
@@ -961,7 +961,7 @@ pub struct TransactionRunner {
 struct PohCallback(Box<dyn Fn(&Bank, Vec<VersionedTransaction>, solana_sdk::hash::Hash) -> std::result::Result<Option<usize>, ()> + Send + Sync>);
 
 impl Debug for PohCallback {
-    fn fmt(&self, f: &mut Formatter) -> std::fmt::Result {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
         write!(f, "PohCallback{self:p}")
     }
 }
