8a648ae1e9e0951716899e44854648487dedc457
Ryo Onodera <ryoqun@gmail.com>
2023-02-09 11:26:48 +0900

    save

--- runtime/src/bank.rs
+++ runtime/src/bank.rs
@@ -6890,10 +6890,10 @@ impl Bank {
         }
     }
 
-    pub fn sync_transaction_runner_context(self: &Arc<Self>, mode: solana_scheduler::Mode) {
+    pub fn sync_transaction_runner_context(self: &Arc<Self>, runner: Arc<Runner>, mode: solana_scheduler::Mode) {
         let s = self.scheduler2.read().unwrap();
         let scheduler = s.as_ref().unwrap();
-        scheduler.replace_transaction_runner_context(self.clone(), mode);
+        scheduler.replace_transaction_runner_context(runner.context(self.clone(), mode));
     }
 
     pub fn transaction_runner_mode(&self) -> solana_scheduler::Mode {
