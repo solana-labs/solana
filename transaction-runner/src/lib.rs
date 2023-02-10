536850ff00a90766590311164b29301f43a4ae86
Ryo Onodera <ryoqun@gmail.com>
2023-02-10 11:32:44 +0900

    save

--- transaction-runner/src/lib.rs
+++ transaction-runner/src/lib.rs
@@ -32,12 +32,12 @@ use solana_measure::measure::Measure;
 use std::sync::RwLock;
 use solana_poh::poh_recorder::PohRecorder;
 
-struct TransactionRunner(Arc<Bank>, solana_poh::poh_recorder::PohRecorder, solana_ledger::blockstore_processor::TransactionStatusSender);
+struct TransactionRunner(Arc<Bank>, PohRecorder, solana_ledger::blockstore_processor::TransactionStatusSender);
 
 #[derive(Debug)]
 pub struct SchedulerPool {
     schedulers: std::sync::Mutex<Vec<Box<dyn LikeScheduler>>>,
-    poh: Option<solana_poh::poh_recorder::PohRecorder>,
+    poh: Option<PohRecorder>,
 }
 
 impl SchedulerPool {
