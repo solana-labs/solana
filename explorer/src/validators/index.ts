7b2e1769f2a92b94a0020421a032922d54cb0cef
Noah Gundotra <ngundotra@users.noreply.github.com>
2023-02-10 18:37:27 -0500

    Explorer: Move code to `solana-labs/explorer` (#30264)

--- explorer/src/validators/index.ts
+++
@@ -1,9 +0,0 @@
-/* eslint-disable @typescript-eslint/no-redeclare */
-
-import { type, any, Infer, string } from "superstruct";
-
-export type ParsedInfo = Infer<typeof ParsedInfo>;
-export const ParsedInfo = type({
-  type: string(),
-  info: any(),
-});
