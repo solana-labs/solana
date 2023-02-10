00a8ed3b9cfe02d5f47a09626fedc2a11cdc87b8
Ryo Onodera <ryoqun@gmail.com>
2023-02-10 12:39:18 +0900

    save

--- transaction-runner/src/lib.rs
+++ transaction-runner/src/lib.rs
@@ -37,7 +37,7 @@ struct TransactionRunner(Arc<Bank>, PohRecorder, solana_ledger::blockstore_proce
 
 pub struct SchedulerPool {
     schedulers: std::sync::Mutex<Vec<Box<dyn LikeScheduler>>>,
-    poh: Option<Arc<RwLock<PohRecorder>>>,
+    transaction_recorder: Option<TransactionRecorder>,
 }
 
 impl std::fmt::Debug for SchedulerPool {
