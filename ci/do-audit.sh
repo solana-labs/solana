f265ea19a3bc9aa72936823ed295b1537dd8a65c
Trent Nelson <trent@solana.com>
2023-02-10 21:47:48 -0700

    ci: suppress dep trees by default in `ci/do-audit.sh` (#30255)
    get them back by passing `--display-dependency-trees`
    
    (cherry picked from commit 85af23613e434403116da8c7e5e1283f777c43d8)
    
    # Conflicts:
    #	ci/do-audit.sh

--- ci/do-audit.sh
+++ ci/do-audit.sh
@@ -7,6 +7,16 @@ src_root="$(readlink -f "${here}/..")"
 
 cd "${src_root}"
 
+# `cargo-audit` doesn't give us a way to do this nicely, so hammer it is...
+dep_tree_filter="grep -Ev '│|└|├|─'"
+
+while [[ -n $1 ]]; do
+  if [[ $1 = "--display-dependency-trees" ]]; then
+    dep_tree_filter="cat"
+    shift
+  fi
+done
+
 cargo_audit_ignores=(
   # `net2` crate has been deprecated; use `socket2` instead
   #
@@ -34,4 +44,4 @@ cargo_audit_ignores=(
   # Not worth upgrading tokio version on a stable branch
   --ignore RUSTSEC-2023-0001
 )
-scripts/cargo-for-all-lock-files.sh stable audit "${cargo_audit_ignores[@]}"
+scripts/cargo-for-all-lock-files.sh stable audit "${cargo_audit_ignores[@]}" | $dep_tree_filter
