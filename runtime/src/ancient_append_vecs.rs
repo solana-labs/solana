b3778d5044dee5f441d7be7d5926841696111727
Jeff Washington (jwash) <jeff.washington@solana.com>
2023-02-12 15:42:03 -0600

    cleanup (#30251)

--- runtime/src/ancient_append_vecs.rs
+++ runtime/src/ancient_append_vecs.rs
@@ -369,7 +369,7 @@ impl AccountsDb {
         } = packed;
         let accounts_to_write = StorableAccountsBySlot::new(
             target_slot,
-            &accounts_to_write[..],
+            accounts_to_write,
             INCLUDE_SLOT_IN_HASH_IRRELEVANT_APPEND_VEC_OPERATION,
         );
 
@@ -1975,7 +1975,7 @@ pub mod tests {
                         let target_slot = slots.clone().nth(combine_into).unwrap_or(slots.start);
                         let accounts_to_write = StorableAccountsBySlot::new(
                             target_slot,
-                            &accounts[..],
+                            &accounts,
                             INCLUDE_SLOT_IN_HASH_TESTS,
                         );
 
