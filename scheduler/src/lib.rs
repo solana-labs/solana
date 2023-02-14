acd302363ad6e9c68852fa6e229ff9e619015e19
Ryo Onodera <ryoqun@gmail.com>
2023-02-14 08:06:38 +0000

    save

--- scheduler/src/lib.rs
+++ scheduler/src/lib.rs
@@ -2277,6 +2277,7 @@ impl<T, B: WithMode> Checkpoint<T, B> {
         let (_, _, b) = &mut *g;
         let mut did_dropped = false;
         if let Some(sc) = b.take() {
+            drop(g);
             did_dropped = sc.drop_cyclically();
         }
         did_dropped
