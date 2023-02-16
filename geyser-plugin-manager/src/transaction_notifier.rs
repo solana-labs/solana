80cf7ed6d3aeed264b0fa5d2115d06bee1a67b52
Ubuntu <ubuntu@ip-172-31-84-110.ec2.internal>
2023-02-16 23:30:03 +0000

    Update gesyer plugin interface to not require write locks

--- geyser-plugin-manager/src/transaction_notifier.rs
+++ geyser-plugin-manager/src/transaction_notifier.rs
@@ -38,13 +38,13 @@ impl TransactionNotifier for TransactionNotifierImpl {
             transaction,
         );
 
-        let mut plugin_manager = self.plugin_manager.write().unwrap();
+        let plugin_manager = self.plugin_manager.read().unwrap();
 
         if plugin_manager.plugins.is_empty() {
             return;
         }
 
-        for plugin in plugin_manager.plugins.iter_mut() {
+        for plugin in plugin_manager.plugins.iter() {
             if !plugin.transaction_notifications_enabled() {
                 continue;
             }
