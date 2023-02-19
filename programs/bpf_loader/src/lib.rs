a9264be25ed49e738dc1499f76c3f1ce30fd5cbb
Ryo Onodera <ryoqun@gmail.com>
2023-02-19 21:28:18 +0900

    save

--- programs/bpf_loader/src/lib.rs
+++ programs/bpf_loader/src/lib.rs
@@ -71,7 +71,7 @@ use {
 solana_sdk::declare_builtin!(
     solana_sdk::bpf_loader::ID,
     solana_bpf_loader_program,
-    solana_bpf_loader_program::process_instruction_jit
+    solana_bpf_loader_program::process_instruction
 );
 
 /// Errors returned by functions the BPF Loader registers with the VM
@@ -337,15 +337,12 @@ pub fn create_vm<'a, 'b>(
     result
 }
 
-/*
 pub fn process_instruction(
     first_instruction_account: IndexOfAccount,
     invoke_context: &mut InvokeContext,
 ) -> Result<(), InstructionError> {
-    unreachable!();
     process_instruction_common(first_instruction_account, invoke_context, false)
 }
-*/
 
 pub fn process_instruction_jit(
     first_instruction_account: IndexOfAccount,
@@ -359,7 +356,6 @@ fn process_instruction_common(
     invoke_context: &mut InvokeContext,
     use_jit: bool,
 ) -> Result<(), InstructionError> {
-    assert!(use_jit);
     let log_collector = invoke_context.get_log_collector();
     let transaction_context = &invoke_context.transaction_context;
     let instruction_context = transaction_context.get_current_instruction_context()?;
