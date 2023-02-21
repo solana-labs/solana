368f78d532e5e6bae3cb0c4161dc3a94fa4d7b8e
behzad nouri <behzadnouri@gmail.com>
2023-02-21 09:52:12 -0500

    reworks gossip crds timeouts
    CrdsGossipPull::make_timeouts iterates over the stakes hashmap and
    creates a new hashmap which is unnecessary:
    https://github.com/solana-labs/solana/blob/c032dc275/gossip/src/crds_gossip_pull.rs#L517-L539
    
    The commit instead keeps a reference to the stakes hashmap.

--- gossip/benches/crds.rs
+++ gossip/benches/crds.rs
@@ -7,11 +7,11 @@ use {
     rayon::ThreadPoolBuilder,
     solana_gossip::{
         crds::{Crds, GossipRoute},
-        crds_gossip_pull::CRDS_GOSSIP_PULL_CRDS_TIMEOUT_MS,
+        crds_gossip_pull::{CrdsTimeouts, CRDS_GOSSIP_PULL_CRDS_TIMEOUT_MS},
         crds_value::CrdsValue,
     },
     solana_sdk::pubkey::Pubkey,
-    std::collections::HashMap,
+    std::{collections::HashMap, time::Duration},
     test::Bencher,
 };
 
@@ -24,8 +24,13 @@ fn bench_find_old_labels(bencher: &mut Bencher) {
     std::iter::repeat_with(|| (CrdsValue::new_rand(&mut rng, None), rng.gen_range(0, now)))
         .take(50_000)
         .for_each(|(v, ts)| assert!(crds.insert(v, ts, GossipRoute::LocalMessage).is_ok()));
-    let mut timeouts = HashMap::new();
-    timeouts.insert(Pubkey::default(), CRDS_GOSSIP_PULL_CRDS_TIMEOUT_MS);
+    let stakes = HashMap::from([(Pubkey::new_unique(), 1u64)]);
+    let timeouts = CrdsTimeouts::new(
+        Pubkey::new_unique(),
+        CRDS_GOSSIP_PULL_CRDS_TIMEOUT_MS, // default_timeout
+        Duration::from_secs(48 * 3600),   // epoch_duration
+        &stakes,
+    );
     bencher.iter(|| {
         let out = crds.find_old_labels(&thread_pool, now, &timeouts);
         assert!(out.len() > 10);
