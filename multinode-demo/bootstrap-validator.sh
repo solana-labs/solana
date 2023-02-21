123b627ab2e6e3a04d3819ca27592393d79be377
Andrew Fitzgerald <apfitzge@gmail.com>
2023-02-21 16:40:30 +0000

    Remove --allow-private-addr argument option since it is default

--- multinode-demo/bootstrap-validator.sh
+++ multinode-demo/bootstrap-validator.sh
@@ -97,9 +97,6 @@ while [[ -n $1 ]]; do
     elif [[ $1 == --no-snapshot-fetch ]]; then
       args+=("$1")
       shift
-    elif [[ $1 == --allow-private-addr ]]; then
-      args+=("$1")
-      shift
     elif [[ $1 == --accounts-db-skip-shrink ]]; then
       args+=("$1")
       shift
