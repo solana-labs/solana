b7c59a32c0c1153e2eacb3721002410d8d896207
Brennan Watt <brennan.watt@solana.com>
2023-02-23 14:24:27 -0800

    Revert "Adjust tower root after warp slot"
    This reverts commit 0bd5c8ab190a9736eb985797b811bce05ff454e9.

--- core/src/validator.rs
+++ core/src/validator.rs
@@ -1359,11 +1359,7 @@ fn post_process_restored_tower(
                 );
             }
 
-            let mut tower = Tower::new_from_bankforks(bank_forks, validator_identity, vote_account);
-            if let Some(warp_slot) = config.warp_slot {
-                tower.adjust_lockouts_after_warp(warp_slot);
-            }
-            tower
+            Tower::new_from_bankforks(bank_forks, validator_identity, vote_account)
         }
     };
 
