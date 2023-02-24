21f1b652a7f3def5c0d4869d9bf7cd232d24b445
Yihau Chen <a122092487@gmail.com>
2023-02-24 15:55:24 +0800

    ci: reorg downstream projects (#30463)
    reorg downstream projects

---
+++ .buildkite/scripts/build-downstream-projects.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+here=$(dirname "$0")
+
+# shellcheck source=.buildkite/scripts/common.sh
+source "$here"/common.sh
+
+agent="${1-solana}"
+
+group "downstream projects" \
+  '{ "name": "spl", "command": "./ci/downstream-projects/run-spl.sh", "timeout_in_minutes": 30, "agent": "'"$agent"'" }' \
+  '{ "name": "openbook-dex", "command": "./ci/downstream-projects/run-openbook-dex.sh", "timeout_in_minutes": 30, "agent": "'"$agent"'" }' \
+  '{ "name": "example-helloworld", "command": "./ci/downstream-projects/run-example-helloworld.sh", "timeout_in_minutes": 30, "agent": "'"$agent"'" }'
