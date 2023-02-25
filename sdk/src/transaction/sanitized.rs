7021afc715f9b110e843bb5dcb7d6a20004968be
Tao Zhu <tao@solana.com>
2023-02-24 20:53:12 -0600

    refactor to remove additional is_simple_vote check

--- sdk/src/transaction/sanitized.rs
+++ sdk/src/transaction/sanitized.rs
@@ -117,9 +117,13 @@ impl SanitizedTransaction {
         };
 
         let is_simple_vote_tx = is_simple_vote_tx.unwrap_or_else(|| {
-            // TODO: Move to `vote_parser` runtime module
-            let mut ix_iter = message.program_instructions_iter();
-            ix_iter.next().map(|(program_id, _ix)| program_id) == Some(&crate::vote::program::id())
+            if message.instructions().len() == 1 && matches!(message, SanitizedMessage::Legacy(_)) {
+                let mut ix_iter = message.program_instructions_iter();
+                ix_iter.next().map(|(program_id, _ix)| program_id)
+                    == Some(&crate::vote::program::id())
+            } else {
+                false
+            }
         });
 
         Ok(Self {
