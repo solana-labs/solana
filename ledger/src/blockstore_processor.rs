0f29d3e66528f8f7ca6d95b9ec39f4881be0108e
Ryo Onodera <ryoqun@gmail.com>
2023-02-27 22:08:58 +0900

    save

--- ledger/src/blockstore_processor.rs
+++ ledger/src/blockstore_processor.rs
@@ -459,7 +459,7 @@ fn process_entries_with_callback(
                     // execute the group and register the tick
                     execute_batches(
                         bank,
-                        &batches,
+                        &batches.retain(..),
                         transaction_status_sender,
                         replay_vote_sender,
                         confirmation_timing,
