fd666fa93f52dbcb1199c46382bfa197eaab9ef7
Ryo Onodera <ryoqun@gmail.com>
2023-02-28 13:22:44 +0900

    save

--- validator/src/main.rs
+++ validator/src/main.rs
@@ -1454,7 +1454,8 @@ pub fn main() {
     configure_banking_trace_dir_byte_limit(&mut validator_config, &matches);
     validator_config.replaying_backend = matches
         .value_of("replaying_backend")
-        .map(ReplayingBackend::from);
+        .map(ReplayingBackend::from)
+        .unwrap();
 
     validator_config.ledger_column_options = LedgerColumnOptions {
         compression_type: match matches.value_of("rocksdb_ledger_compression") {
