24d8b119ccf8f27f386bdcc8206a1b5dbf2dc2e0
Ryo Onodera <ryoqun@gmail.com>
2023-02-28 12:25:13 +0900

    save

--- core/src/validator.rs
+++ core/src/validator.rs
@@ -131,7 +131,7 @@ pub enum ReplayingBackend {
 
 impl ReplayingBackend {
     const fn const_default() -> Self {
-        Self::BlockstoreProcessor
+        Self::BlockstoreProcessor::default()
     }
 }
 
