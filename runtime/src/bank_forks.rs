9e999820e86028201e3619f15f633579abbe09da
Ryo Onodera <ryoqun@gmail.com>
2023-02-28 13:45:33 +0900

    save

--- runtime/src/bank_forks.rs
+++ runtime/src/bank_forks.rs
@@ -254,7 +254,7 @@ impl BankForks {
         match mode {
             solana_scheduler::Mode::Replaying => {
                 match &self.scheduler_pool {
-                    InstalledSchedulerPool::ReplayOnly(scheduler_pool) => Some(scheduler_pool)
+                    InstalledSchedulerPool::ReplayOnly(scheduler_pool) | InstalledSchedulerPool::Full(scheduler_pool) => Some(scheduler_pool)
                 }
             }
         }
