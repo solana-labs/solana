43b84eaf5f0db7b9e42edbe34146daaf65564451
Ryo Onodera <ryoqun@gmail.com>
2023-02-28 12:30:15 +0900

    save

--- core/src/validator.rs
+++ core/src/validator.rs
@@ -119,12 +119,10 @@ use {
 const MAX_COMPLETED_DATA_SETS_IN_CHANNEL: usize = 100_000;
 const WAIT_FOR_SUPERMAJORITY_THRESHOLD_PERCENT: u64 = 80;
 
-pub const DEFAULT_REPLAYING_BACKEND: &str = ReplayingBackend::const_default().to_str();
+pub const DEFAULT_REPLAYING_BACKEND: &str = "blockstore_processor";
 pub const DEFAULT_BANKING_BACKEND: &str = BankingBackend::default().to_str();
 
-#[derive(Default)]
 pub enum ReplayingBackend {
-    #[default]
     BlockstoreProcessor,
     UnifiedScheduler,
 }
