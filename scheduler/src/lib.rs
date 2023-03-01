26b94134e6146a17e98db8cbff20d024d0c2b43d
Ryo Onodera <ryoqun@gmail.com>
2023-03-01 21:07:20 +0900

    save

--- scheduler/src/lib.rs
+++ scheduler/src/lib.rs
@@ -2125,18 +2125,18 @@ impl ScheduleStage {
         max_executing_queue_count: usize,
         runnable_queue: &mut TaskQueue,
         address_book: &mut AddressBook,
-        from: &crossbeam_channel::Receiver<SchedulablePayload<C, B>>,
-        to_execute_substage: &crossbeam_channel::Sender<ExecutablePayload<C, B>>,
-        to_high_execute_substage: Option<&crossbeam_channel::Sender<ExecutablePayload<C, B>>>,
+        from: &crossbeam_channel::Receiver<SchedulablePayload>,
+        to_execute_substage: &crossbeam_channel::Sender<ExecutablePayload>,
+        to_high_execute_substage: Option<&crossbeam_channel::Sender<ExecutablePayload>>,
         from_execute_substage: &crossbeam_channel::Receiver<UnlockablePayload<T>>,
-        maybe_to_next_stage: Option<&crossbeam_channel::Sender<ExaminablePayload<T, C, B>>>, // assume nonblocking
+        maybe_to_next_stage: Option<&crossbeam_channel::Sender<ExaminablePayload<T>>>, // assume nonblocking
         log_prefix: impl Fn(&Option<B>) -> String,
-    ) -> Option<std::sync::Arc<Checkpoint<C, B>>> {
+    ) {
         #[derive(Clone, Copy, Debug)]
         struct AtTopOfScheduleThread;
         unsafe impl AtScheduleThread for AtTopOfScheduleThread {}
 
-        Self::_run::<AtTopOfScheduleThread, T, C, B>(
+        Self::_run::<AtTopOfScheduleThread, T>(
             AtTopOfScheduleThread,
             checkpoint,
             max_executing_queue_count,
