e8d1a9fd334874e6657a614d9bd6adbd7a0865b6
Ryo Onodera <ryoqun@gmail.com>
2023-03-01 15:51:22 +0900

    save

--- runtime/src/bank.rs
+++ runtime/src/bank.rs
@@ -966,9 +966,9 @@ impl solana_scheduler::WithMode for SchedulerContext {
 }
 
 impl SchedulerContext {
-    pub fn new(bank: Option<&Arc<Bank>>, mode: solana_scheduler::Mode) -> Self {
+    pub fn new(bank: Arc<Bank>, mode: solana_scheduler::Mode) -> Self {
         Self {
-            bank: bank.cloned(),
+            bank,
             mode,
         }
     }
