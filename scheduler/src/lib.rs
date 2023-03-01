544838f453f3b68798dd9b5c1574eeb9cee2e01a
Ryo Onodera <ryoqun@gmail.com>
2023-03-01 21:42:50 +0900

    save

--- scheduler/src/lib.rs
+++ scheduler/src/lib.rs
@@ -2148,26 +2148,22 @@ pub struct ExaminablePayload<T>(pub Flushable<(Box<ExecutionEnvironment>, T)>);
 pub struct Checkpoint<T, B>(std::sync::Mutex<(usize, Option<T>, Option<B>)>, std::sync::Condvar);
 
 impl<T, B> Checkpoint<T, B> {
-    fn wait_for_restart(&self, from_internal: bool) {
+    pub fn wait_for_restart(&self, maybe_given_restart_value: Option<T>) {
         let current_thread_name = std::thread::current().name().unwrap().to_string();
         let mut g = self.0.lock().unwrap();
         let (self_remaining_threads, self_return_value, _) = &mut *g;
+        info!(
+            "Checkpoint::wait_for_restart: {} is entering at {} -> {}",
+            current_thread_name,
+            *self_remaining_threads,
+            *self_remaining_threads - 1
+        );
 
-        if from_internal {
-            info!(
-                "Checkpoint::wait_for_restart: {} is entering at {} -> {}",
-                current_thread_name,
-                *self_remaining_threads,
-                *self_remaining_threads - 1
-            );
+        *self_remaining_threads = self_remaining_threads.checked_sub(1).unwrap();
 
-            *self_remaining_threads = self_remaining_threads.checked_sub(1).unwrap();
-        } else {
-            info!(
-                "Checkpoint::wait_for_restart: {} is entering at {}",
-                current_thread_name,
-                *self_remaining_threads,
-            );
+        if let Some(given_restart_value) = maybe_given_restart_value {
+            assert!(self_return_value.is_none());
+            *self_return_value = Some(given_restart_value);
         }
 
         if *self_remaining_threads == 0 {
@@ -2192,10 +2188,6 @@ impl<T, B> Checkpoint<T, B> {
                 current_thread_name
             );
         }
-    }
-
-    pub fn internally_wait_for_restart(&self) {
-        self.wait_for_restart(true);
     }
 
     pub fn register_return_value(&self, restart_value: T) {
