0761682d3d834e53e27844f501bd08c776faaa48
Ryo Onodera <ryoqun@gmail.com>
2023-03-02 16:51:42 +0900

    save

--- scheduler/src/lib.rs
+++ scheduler/src/lib.rs
@@ -2149,8 +2149,8 @@ pub struct Checkpoint<T, B>(std::sync::Mutex<(usize, Option<T>, Option<B>, usize
 
 impl<T, B> Checkpoint<T, B> {
     pub fn wait_for_restart(&self) {
+        let mut a = &mut None;
         let mut current_thread_name = || {
-            let mut a = None;
             a.get_or_insert_with(move || std::thread::current().name().unwrap().to_string()).clone()
         };
         let mut g = self.0.lock().unwrap();
