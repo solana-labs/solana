c55a17b9ac2f8c2f04bd22190c8a2e684e346921
Ryo Onodera <ryoqun@gmail.com>
2023-03-02 16:37:45 +0900

    save

--- scheduler/src/lib.rs
+++ scheduler/src/lib.rs
@@ -2151,7 +2151,7 @@ impl<T, B> Checkpoint<T, B> {
     pub fn wait_for_restart(&self) {
         let current_thread_name = std::thread::current().name().unwrap().to_string();
         let mut g = self.0.lock().unwrap();
-        let (self_remaining_threads, self_return_value, _) = &mut *g;
+        let (self_remaining_threads, self_return_value, ..) = &mut *g;
         info!(
             "Checkpoint::wait_for_restart: {} is entering at {} -> {}",
             current_thread_name,
