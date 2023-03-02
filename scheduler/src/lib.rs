226f1c460f9c5537be89a8aa8fac7ba964e88004
Ryo Onodera <ryoqun@gmail.com>
2023-03-02 16:36:05 +0900

    save

--- scheduler/src/lib.rs
+++ scheduler/src/lib.rs
@@ -2226,7 +2226,7 @@ impl<T, B> Checkpoint<T, B> {
 impl<T, B: Clone> Checkpoint<T, B> {
     pub fn replace_context_value(&self, new: B) {
         let mut g = self.0.lock().unwrap();
-        let (_self_remaining_threads, self_return_value, b) = &mut *g;
+        let (_self_remaining_threads, self_return_value, b, ..) = &mut *g;
         *b = Some((new, self.2));
     }
 
