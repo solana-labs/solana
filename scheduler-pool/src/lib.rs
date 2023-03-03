61bc3d41a829a20b38c9f2f6a7069987cbd249d0
Ryo Onodera <ryoqun@gmail.com>
2023-03-03 17:03:56 +0900

    save

--- scheduler-pool/src/lib.rs
+++ scheduler-pool/src/lib.rs
@@ -366,7 +366,7 @@ impl<T, B: Clone> Checkpoint<T, B> {
 }
 
 impl<T, B: Clone> solana_scheduler::WithContext<B> for Checkpoint<T, B> {
-    pub fn use_context_value(&self) -> Option<B> {
+    fn use_context_value(&self) -> Option<B> {
         let mut a = &mut None;
         let mut current_thread_name = || a.get_or_insert_with(|| std::thread::current().name().unwrap().to_string()).clone() ;
 
