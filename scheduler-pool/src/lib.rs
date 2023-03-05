a339ce0e491016b037321d6f6258b92ee93007e6
Ryo Onodera <ryoqun@gmail.com>
2023-03-05 10:25:18 +0900

    save

--- scheduler-pool/src/lib.rs
+++ scheduler-pool/src/lib.rs
@@ -255,14 +255,18 @@ impl Checkpoint {
             );
             let _ = *self
                 .1
-                .wait_while(g, |&mut ((threads_before_checkpoint, ..), ..)| threads_before_checkpoint > 0)
+                .wait_while(g, |&mut ((threads_before_checkpoint, threads_after_checkpoint), ..)| {
+                    if threads_before_checkpoint > 0 {
+                        true
+                    } else {
+                        *threads_after_checkpoint = threads_after_checkpoint.checked_add(1).unwrap();
+                        if *threads_after_checkpoint == self.thread_count() {
+                            self.2.notify_one();
+                        }
+                        false
+                    }
+                })
                 .unwrap();
-            g = self.0.lock().unwrap();
-            let ((_, threads_after_checkpoint), ..) = &mut *g;
-            *threads_after_checkpoint = threads_after_checkpoint.checked_add(1).unwrap();
-            if *threads_after_checkpoint == self.thread_count() {
-                self.2.notify_one();
-            }
             info!(
                 "Checkpoint::wait_for_restart: {} is started... {threads_after_checkpoint}",
                 current_thread_name()
