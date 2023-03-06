49f3c8c6656f3c31dc3c1f71c5db792277cf39e3
Dmitri Makarov <dmakarov@alumni.stanford.edu>
2023-03-06 17:00:12 -0500

    Bump sbf-tools version to v1.35
    - this release includes fixes in relocation handling necessary for
      source-level debugging of on-chain programs

--- sdk/cargo-build-sbf/src/main.rs
+++ sdk/cargo-build-sbf/src/main.rs
@@ -941,7 +941,7 @@ fn main() {
 
     // The following line is scanned by CI configuration script to
     // separate cargo caches according to the version of sbf-tools.
-    let sbf_tools_version = String::from("v1.34");
+    let sbf_tools_version = String::from("v1.35");
     let version = format!("{}\nsbf-tools {}", crate_version!(), sbf_tools_version);
     let matches = clap::Command::new(crate_name!())
         .about(crate_description!())
