902bd59134902fb2d7252ac81d9c33773f8b08f3
Ryo Onodera <ryoqun@gmail.com>
2023-03-09 23:01:22 +0900

    ci: silence ci test output while recording in full

---
+++ ci/upload-console.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+source ci/upload-ci-artifact.sh
+
+set -e
+
+if [[ -n $CI ]]; then
+  exit_code=0
+  console_out="console-$(date '+%Yy%mm%dd%Hh%Mm%Ss%Nns').log"
+  if "$@" -- --color=always 2>> "$console_out" 1>> >(tee -a "$console_out"); then
+    # noop
+    true
+  else
+    exit_code=$?
+  fi
+  gzip -f "$console_out"
+  upload-ci-artifact "$console_out.gz"
+  exit "$exit_code"
+else
+  "$@"
+fi
