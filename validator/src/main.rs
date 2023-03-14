4ace86d4f32af23c8744e31cf7bcd7d32c37f1bd
jeff washington <jeff.washington@solana.com>
2023-03-14 14:19:50 -0500

    adapt to master changes

--- validator/src/main.rs
+++ validator/src/main.rs
@@ -1668,8 +1668,8 @@ pub fn main() {
         .arg(
             Arg::with_name("accounts_db_skip_shrink")
                 .long("accounts-db-skip-shrink")
-                .help("Enables faster starting of validators by skipping shrink. \
-                      This option is for use during testing."),
+                .help("This is obsolete since it is now enabled by default. Enables faster starting of validators by skipping startup clean and shrink.")
+                .hidden(true),
         )
         .arg(
             Arg::with_name("accounts_db_skip_rewrites")
