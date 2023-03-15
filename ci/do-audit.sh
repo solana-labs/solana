9770f42f0be870ff5658da08830bb86a18f58fb3
yihau <yihau.chen@icloud.com>
2023-03-16 00:54:40 +0800

    redirect stderr to stdout

--- ci/do-audit.sh
+++ ci/do-audit.sh
@@ -29,6 +29,6 @@ cargo_audit_ignores=(
   # https://github.com/solana-labs/solana/issues/29586
   --ignore RUSTSEC-2023-0001
 )
-scripts/cargo-for-all-lock-files.sh audit "${cargo_audit_ignores[@]}" | $dep_tree_filter
+scripts/cargo-for-all-lock-files.sh audit "${cargo_audit_ignores[@]}" |& $dep_tree_filter
 # we want the `cargo audit` exit code, not `$dep_tree_filter`'s
 exit "${PIPESTATUS[0]}"
