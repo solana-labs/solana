9eae1966902a49e048196b5a08f70c2b1e23f55b
Ryo Onodera <ryoqun@gmail.com>
2023-03-16 20:34:19 +0900

    Stash actual command's exit_code, not echo's

--- ci/intercept.sh
+++ ci/intercept.sh
@@ -1,22 +1,18 @@
 #!/usr/bin/env bash
 
-source ci/upload-ci-artifact.sh
-
 set -e
 
 if [[ -n $CI && -z $NO_INTERCEPT ]]; then
-  exit_code=0
   console_log="./intercepted-console-$(date '+%Yy%mm%dd%Hh%Mm%Ss%Nns').log"
   echo "$0: Intercepting stderr into $console_log, along side tee-d stdout."
 
   # we don't care about being racy here as was before; so disable shellcheck
   # shellcheck disable=SC2094
   if "$@" 2>> "$console_log" 1>> >(tee -a "$console_log"); then
-    # noop
-    true
+    exit_code=0
   else
-    echo "$0: command failed; please see $console_log for details"
     exit_code=$?
+    echo "$0: command failed; please see $console_log in artifacts"
   fi
   exit "$exit_code"
 else
