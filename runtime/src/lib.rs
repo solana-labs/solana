6055e826770131a4001732821f18c410f1dc0cb5
Yueh-Hsuan Chiang <yueh-hsuan@solana.com>
2023-03-17 12:47:45 -0700

    Move alignment related consts from append_vec.rs to accounts_file.rs

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -12,6 +12,7 @@ pub mod accounts;
 pub mod accounts_background_service;
 pub mod accounts_cache;
 pub mod accounts_db;
+#[macro_use]
 pub mod accounts_file;
 pub mod accounts_hash;
 pub mod accounts_index;
