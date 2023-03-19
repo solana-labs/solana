59f589ff68910a587ce22548b5a811a7118623ce
Ryo Onodera <ryoqun@gmail.com>
2023-03-19 22:30:09 +0900

    save

--- scheduler-pool/src/lib.rs
+++ scheduler-pool/src/lib.rs
@@ -685,7 +685,7 @@ impl Scheduler {
                             if !propagate_tx_error {
                                 first_error = Ok(());
                             }
-                            checkpoint.register_return_value((std::mem::take(&mut cumulative_timings), std::mem::swap(&mut first_error, Ok(()))));
+                            checkpoint.register_return_value((std::mem::take(&mut cumulative_timings), std::mem::replace(&mut first_error, Ok(()))));
                             checkpoint.wait_for_restart_from_internal_thread(latest_scheduler_context.take());
                         },
                     }
