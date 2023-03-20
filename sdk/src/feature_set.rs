d77f0a22c7fd5746164e2789219eb48c065a18c1
bji <bryan@ischo.com>
2023-03-20 07:31:46 -0700

    Add new vote state version that replaces Lockout with LandedVote to a… (#29524)
    * Add new vote state version that replaces Lockout with LandedVote to allow vote latency to be tracked in a future change. Includes a feature to be enabled which will when enabled cause the vote state to be written in the new form.
    
    * Update feature set key to one owned by ashwin
    
    ---------
    
    Co-authored-by: Ashwin Sekar <ashwin@solana.com>

--- sdk/src/feature_set.rs
+++ sdk/src/feature_set.rs
@@ -630,6 +630,10 @@ pub mod round_up_heap_size {
     solana_sdk::declare_id!("CE2et8pqgyQMP2mQRg3CgvX8nJBKUArMu3wfiQiQKY1y");
 }
 
+pub mod vote_state_add_vote_latency {
+    solana_sdk::declare_id!("7axKe5BTYBDD87ftzWbk5DfzWMGyRvqmWTduuo22Yaqy");
+}
+
 lazy_static! {
     /// Map of feature identifiers to user-visible description
     pub static ref FEATURE_NAMES: HashMap<Pubkey, &'static str> = [
@@ -782,6 +786,7 @@ lazy_static! {
         (add_set_tx_loaded_accounts_data_size_instruction::id(), "add compute budget instruction for setting account data size per transaction #30366"),
         (switch_to_new_elf_parser::id(), "switch to new ELF parser #30497"),
         (round_up_heap_size::id(), "round up heap size when calculating heap cost #30679"),
+        (vote_state_add_vote_latency::id(), "replace Lockout with LandedVote (including vote latency) in vote state"),
         /*************** ADD NEW FEATURES HERE ***************/
     ]
     .iter()
