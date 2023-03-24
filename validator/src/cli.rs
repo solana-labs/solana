310e03bb32ec1ebb711dfcce453a0669fb226b57
Xiang Zhu <xzhu70@gmail.com>
2023-03-24 14:57:40 -0700

    update arg help

--- validator/src/cli.rs
+++ validator/src/cli.rs
@@ -306,7 +306,7 @@ pub fn app<'a>(version: &'a str, default_args: &'a DefaultArgs) -> App<'a, 'a> {
             // Hide it because it is for internal usage only at this point.  Once this path has been verified via
             // internal usage, this argument will be removed, and this path will be set as the default.
             .hidden(true)
-            .help("Construct initial bank state from the local snapshot dir instead of archives for much faster boot"),
+            .help("At boot, reuses account storage files already on disk instead of extracting them from a snapshot archive"),
         )
         .arg(
             Arg::with_name("tower")
