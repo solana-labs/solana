8d4b380de6854a7ef32b191236cd4b6537a9ec0a
Ryo Onodera <ryoqun@gmail.com>
2023-03-26 21:06:40 +0900

    save

--- runtime/src/bank.rs
+++ runtime/src/bank.rs
@@ -8168,7 +8168,7 @@ impl Bank {
         let ss = if IS_RESTART {
             s.as_mut().map(|a| Cow::Borrowed(&i))
         } else {
-            s.take().map(|a| Cow::Owned(i.clone()))
+            s.take().map(|a| Cow::Owned(a))
         };
         if let Some(mut scheduler) = s.take() {
             info!("wait_for_scheduler({VIA_DROP}): gracefully stopping bank ({})... from_internal: {FROM_INTERNAL} by {current_thread_name}", self.slot());
