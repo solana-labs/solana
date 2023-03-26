90d861df7c823f5e94cc6237c428779a625e23c3
Ryo Onodera <ryoqun@gmail.com>
2023-03-26 21:00:18 +0900

    save

--- runtime/src/bank.rs
+++ runtime/src/bank.rs
@@ -8165,7 +8165,7 @@ impl Bank {
         }
 
         let ss = if IS_RESTART {
-            Cow::Borrow(s.as_mut())
+            Cow::Borrowed(s.as_mut())
         } else {
             s.take()
         };
