6f061c91b1a1a3e57ad023ceff9341ad681087ab
Ryo Onodera <ryoqun@gmail.com>
2023-03-28 16:14:45 +0900

    save

--- core/src/replay_stage.rs
+++ core/src/replay_stage.rs
@@ -3595,7 +3595,7 @@ impl ReplayStage {
             Measure::start("generate_new_bank_forks_write_lock");
         let mut forks = bank_forks.write().unwrap();
         for (_, bank) in new_banks {
-            forks.add_new_bank_for_replaying(bank);
+            insert(bank);
         }
         generate_new_bank_forks_write_lock.stop();
         saturating_add_assign!(
