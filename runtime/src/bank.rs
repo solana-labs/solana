8f0e8f32297b71f5258ba10436e48dcb472c880b
Ryo Onodera <ryoqun@gmail.com>
2023-03-28 21:16:30 +0900

    save

--- runtime/src/bank.rs
+++ runtime/src/bank.rs
@@ -3708,8 +3708,6 @@ impl Bank {
         // readers can starve this write lock acquisition and ticks would be slowed down too
         // much if the write lock is acquired for each tick.
         let mut w_blockhash_queue = self.blockhash_queue.write().unwrap();
-
-        let mut blockhash = *blockhash;
         w_blockhash_queue.register_hash(&blockhash, self.fee_rate_governor.lamports_per_signature);
         self.update_recent_blockhashes_locked(&w_blockhash_queue);
     }
