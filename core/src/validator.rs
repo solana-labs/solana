d9be58d11b2562607fa88764c80bad6474d6d541
Ryo Onodera <ryoqun@gmail.com>
2023-03-28 21:32:42 +0900

    save

--- core/src/validator.rs
+++ core/src/validator.rs
@@ -758,7 +758,7 @@ impl Validator {
             use solana_scheduler_pool::{
                 SchedulerPool,
             };
-            let scheduler_pool = SchedulerPool::new_boxed(None, config.runtime_config.log_messages_bytes_limit, transaction_status_sender.clone(), Some(replay_vote_sender.clone()), prioritization_fee_cache);
+            let scheduler_pool = SchedulerPool::new_boxed(None, config.runtime_config.log_messages_bytes_limit, transaction_status_sender.clone(), Some(replay_vote_sender.clone()), prioritization_fee_cache.clone());
             bank_forks.write().unwrap().install_scheduler_pool(scheduler_pool);
         } else {
             info!("not installing scheduler pool...");
