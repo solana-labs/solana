d7466ba3812b227c906c6c1c16feb64ae3e632a0
Brennan Watt <brennan.watt@solana.com>
2023-03-28 23:56:48 +0000

    Reconfigure genesis hash download and check

--- genesis-utils/src/lib.rs
+++ genesis-utils/src/lib.rs
@@ -1,5 +1,8 @@
 use {
+    log::*,
+    solana_core::validator::ValidatorConfig,
     solana_download_utils::download_genesis_if_missing,
+    solana_rpc_client::rpc_client::RpcClient,
     solana_runtime::hardened_unpack::unpack_genesis_archive,
     solana_sdk::{
         genesis_config::{GenesisConfig, DEFAULT_GENESIS_ARCHIVE},
@@ -36,6 +39,51 @@ fn load_local_genesis(
     Ok(existing_genesis)
 }
 
+fn download_then_set_expected_genesis_hash(
+    rpc_addr: &SocketAddr,
+    ledger_path: &std::path::Path,
+    expected_genesis_hash: Option<Hash>,
+    max_genesis_archive_unpacked_size: u64,
+    no_genesis_fetch: bool,
+    use_progress_bar: bool,
+    validator_config: &mut ValidatorConfig,
+) -> Result<(), String> {
+    let genesis_config = if no_genesis_fetch {
+        load_local_genesis(ledger_path, expected_genesis_hash)?
+    } else {
+        let genesis_package = ledger_path.join(DEFAULT_GENESIS_ARCHIVE);
+        if let Ok(tmp_genesis_package) =
+            download_genesis_if_missing(rpc_addr, &genesis_package, use_progress_bar)
+        {
+            unpack_genesis_archive(
+                &tmp_genesis_package,
+                ledger_path,
+                max_genesis_archive_unpacked_size,
+            )
+            .map_err(|err| format!("Failed to unpack downloaded genesis config: {err}"))?;
+
+            let downloaded_genesis = GenesisConfig::load(ledger_path)
+                .map_err(|err| format!("Failed to load downloaded genesis config: {err}"))?;
+
+            check_genesis_hash(&downloaded_genesis, expected_genesis_hash)?;
+            std::fs::rename(tmp_genesis_package, genesis_package)
+                .map_err(|err| format!("Unable to rename: {err:?}"))?;
+
+            downloaded_genesis
+        } else {
+            load_local_genesis(ledger_path, expected_genesis_hash)?
+        }
+    };
+
+    let genesis_hash = genesis_config.hash();
+    if validator_config.expected_genesis_hash.is_none() {
+        info!("Expected genesis hash set to {}", genesis_hash);
+        validator_config.expected_genesis_hash = Some(genesis_hash);
+    }
+
+    Ok(())
+}
+
 pub fn download_then_check_genesis_hash(
     rpc_addr: &SocketAddr,
     ledger_path: &std::path::Path,
@@ -43,34 +91,34 @@ pub fn download_then_check_genesis_hash(
     max_genesis_archive_unpacked_size: u64,
     no_genesis_fetch: bool,
     use_progress_bar: bool,
-) -> Result<GenesisConfig, String> {
-    if no_genesis_fetch {
-        let genesis_config = load_local_genesis(ledger_path, expected_genesis_hash)?;
-        return Ok(genesis_config);
+    validator_config: &mut ValidatorConfig,
+    rpc_client: &RpcClient,
+) -> Result<(), String> {
+    if let Err(e) = download_then_set_expected_genesis_hash(
+        rpc_addr,
+        ledger_path,
+        expected_genesis_hash,
+        max_genesis_archive_unpacked_size,
+        no_genesis_fetch,
+        use_progress_bar,
+        validator_config,
+    ) {
+        error!("Failed to get genesis config: {e}");
     }
 
-    let genesis_package = ledger_path.join(DEFAULT_GENESIS_ARCHIVE);
-    let genesis_config = if let Ok(tmp_genesis_package) =
-        download_genesis_if_missing(rpc_addr, &genesis_package, use_progress_bar)
-    {
-        unpack_genesis_archive(
-            &tmp_genesis_package,
-            ledger_path,
-            max_genesis_archive_unpacked_size,
-        )
-        .map_err(|err| format!("Failed to unpack downloaded genesis config: {err}"))?;
+    if let Some(expected_genesis_hash) = validator_config.expected_genesis_hash {
+        // Sanity check that the RPC node is using the expected genesis hash before
+        // downloading a snapshot from it
+        let rpc_genesis_hash = rpc_client
+            .get_genesis_hash()
+            .map_err(|err| format!("Failed to get genesis hash: {err}"))?;
 
-        let downloaded_genesis = GenesisConfig::load(ledger_path)
-            .map_err(|err| format!("Failed to load downloaded genesis config: {err}"))?;
+        if expected_genesis_hash != rpc_genesis_hash {
+            return Err(format!(
+                "Genesis hash mismatch: expected {expected_genesis_hash} but RPC node genesis hash is {rpc_genesis_hash}"
+            ));
+        }
+    }
 
-        check_genesis_hash(&downloaded_genesis, expected_genesis_hash)?;
-        std::fs::rename(tmp_genesis_package, genesis_package)
-            .map_err(|err| format!("Unable to rename: {err:?}"))?;
-
-        downloaded_genesis
-    } else {
-        load_local_genesis(ledger_path, expected_genesis_hash)?
-    };
-
-    Ok(genesis_config)
+    Ok(())
 }
