c747480e8cd686a7c12e5821820a53a476bb3777
Ryo Onodera <ryoqun@gmail.com>
2023-03-28 21:38:32 +0900

    save

--- scheduler-pool/src/lib.rs
+++ scheduler-pool/src/lib.rs
@@ -650,7 +650,7 @@ impl Scheduler {
                                             1 as usize
                                         );
                                         inc_new_counter_info!("bank-process_transactions-sigs", ee.task.tx.0.signatures().len() as usize);
-                                        prioritization_fee_cache;
+                                        scheduler_pool.prioritization_fee_cache;
                                     },
                                     Err(e) => {
                                         transaction_error_counts.record(&e);
