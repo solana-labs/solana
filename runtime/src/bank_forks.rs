ffd1790b0d33ccab0673f0c38ac66b5e624e3804
Ryo Onodera <ryoqun@gmail.com>
2023-03-28 20:54:11 +0900

    save

--- runtime/src/bank_forks.rs
+++ runtime/src/bank_forks.rs
@@ -232,7 +232,7 @@ impl BankForks {
             self.descendants.entry(parent).or_default().insert(slot);
         }
         if let Some(scheduler_pool) = self.scheduler_pool {
-            let new_context = SchedulerContext::new(bank.clone(), mode);
+            let new_context = SchedulerContext::new(bank.clone(), Mode::Replaying);
             bank.install_scheduler(scheduler_pool.take_from_pool(new_context));
         }
         bank
