6f4805b81972b0a28d8e9a0203445ea8d3c3ec77
nickfrosty <nick.frostbutter@solana.org>
2023-04-05 14:37:51 -0400

    fix: checking for CI

--- docs/build-cli-usage.sh
+++ docs/build-cli-usage.sh
@@ -15,10 +15,10 @@ out=${1:-src/cli/usage.md}
 # load the usage file's header
 cat src/cli/.usage.md.header > "$out"
 
-# Only build the usage doc for doc deployment commits (e.g. to a select branch)
-if [[ ! -z $CI_BRANCH ]]; then
+# Skip generating the usage doc for non deployment commits of the docs 
+if [[ ! -z $CI ]]; then
   if [[ $CI_BRANCH != $EDGE_CHANNEL ]] && [[ $CI_BRANCH != $BETA_CHANNEL ]] && [[ $CI_BRANCH != $STABLE_CHANNEL ]]; then
-    echo "**NOTE:** This usage doc is auto-generated during deployments"
+    echo "**NOTE:** The usage doc is only auto-generated during full production deployments of the docs"
     echo "**NOTE:** This usage doc is auto-generated during deployments" >> "$out"
     exit
   fi
