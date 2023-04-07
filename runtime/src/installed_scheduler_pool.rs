3aade0a5d8fdadc1dc7bef6c2908f3cc98139a7b
Ryo Onodera <ryoqun@gmail.com>
2023-04-07 22:42:27 +0900

    save

--- runtime/src/installed_scheduler_pool.rs
+++ runtime/src/installed_scheduler_pool.rs
@@ -4,6 +4,7 @@ use solana_sdk::slot_history::Slot;
 use solana_sdk::transaction::SanitizedTransaction;
 use solana_program_runtime::timings::ExecuteTimings;
 use crate::installed_scheduler_pool::InstalledScheduler;
+use crate::bank_forks::InstalledSchedulerPool;
 
 pub trait InstalledScheduler: Send + Sync + std::fmt::Debug {
     fn random_id(&self) -> u64;
