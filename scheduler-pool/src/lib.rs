d0b2ac216bc52b34c79a250446811d314ed4ba41
Ryo Onodera <ryoqun@gmail.com>
2023-04-10 16:28:48 +0900

    save

--- scheduler-pool/src/lib.rs
+++ scheduler-pool/src/lib.rs
@@ -51,8 +51,8 @@ impl SchedulerPool {
         transaction_status_sender: Option<TransactionStatusSender>,
         replay_vote_sender: Option<ReplayVoteSender>,
         prioritization_fee_cache: Arc<PrioritizationFeeCache>,
-    ) -> Box<dyn InstalledSchedulerPool> {
-        Box::new(SchedulerPoolWrapper::new(
+    ) -> Arc<dyn InstalledSchedulerPool> {
+        Arc::new(SchedulerPool::new(
             poh_recorder,
             log_messages_bytes_limit,
             transaction_status_sender,
@@ -107,27 +107,6 @@ impl SchedulerPool {
         );
 
         schedulers.push(scheduler);
-    }
-}
-
-#[derive(Debug)]
-struct SchedulerPoolWrapper(Arc<SchedulerPool>);
-
-impl SchedulerPoolWrapper {
-    fn new(
-        poh_recorder: Option<&Arc<RwLock<PohRecorder>>>,
-        log_messages_bytes_limit: Option<usize>,
-        transaction_status_sender: Option<TransactionStatusSender>,
-        replay_vote_sender: Option<ReplayVoteSender>,
-        prioritization_fee_cache: Arc<PrioritizationFeeCache>,
-    ) -> Self {
-        Self(Arc::new(SchedulerPool::new(
-            poh_recorder,
-            log_messages_bytes_limit,
-            transaction_status_sender,
-            replay_vote_sender,
-            prioritization_fee_cache,
-        )))
     }
 }
 
