ca883d741875b149b0b61f771f7c144ad9188e9a
Ryo Onodera <ryoqun@gmail.com>
2023-04-10 16:59:48 +0900

    save

--- scheduler-pool/src/lib.rs
+++ scheduler-pool/src/lib.rs
@@ -127,7 +127,7 @@ impl InstalledScheduler for Scheduler {
         let b: TransactionBatchWithIndexes = {
             panic!();
         };
-        execute_batch();
+        execute_batch(b);
     }
     fn schedule_termination(&mut self) {
         todo!()
