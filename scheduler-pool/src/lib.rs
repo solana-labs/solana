0e38e151cca47518db54cc54a94fbb8d18117931
Ryo Onodera <ryoqun@gmail.com>
2023-04-11 10:57:32 +0900

    save

--- scheduler-pool/src/lib.rs
+++ scheduler-pool/src/lib.rs
@@ -8,7 +8,7 @@ use {
         vote_sender_types::ReplayVoteSender,
     },
     solana_sdk::transaction::{SanitizedTransaction, TransactionError},
-    std::sync::{Arc, RwLock, Weak},
+    std::sync::{Arc, RwLock, Weak, Mutex},
 };
 use solana_runtime::installed_scheduler_pool::SchedulerPoolArc;
 
