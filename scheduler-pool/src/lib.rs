ef3635a269ce9926b93dfcfd0d66c830514169ac
Ryo Onodera <ryoqun@gmail.com>
2023-04-11 13:53:04 +0900

    save

--- scheduler-pool/src/lib.rs
+++ scheduler-pool/src/lib.rs
@@ -127,6 +127,8 @@ impl InstalledScheduler for Scheduler {
 
     fn schedule_execution(&self, transaction: &SanitizedTransaction, index: usize) {
         let mut aa = self.1.lock().unwrap();
+        let mut a =
+            aa.1.get_or_insert_with(|| (ExecuteTimings::default(), Ok(())));
         let bank = aa.0.bank();
 
         let tt = [transaction.clone()];
@@ -134,8 +136,6 @@ impl InstalledScheduler for Scheduler {
             batch: bank.prepare_sanitized_batch(&tt),
             transaction_indexes: vec![index],
         };
-        let mut a =
-            aa.1.get_or_insert_with(|| (ExecuteTimings::default(), Ok(())));
 
         if a.1.is_ok() {
             a.1 = execute_batch(
