e967e99d0cac719c253919b69972f4e2d0fcdfde
Ryo Onodera <ryoqun@gmail.com>
2023-04-11 11:08:26 +0900

    save

--- scheduler-pool/src/lib.rs
+++ scheduler-pool/src/lib.rs
@@ -146,6 +146,6 @@ impl InstalledScheduler for Scheduler {
         &self,
         c: SchedulingContext,
     ) {
-        *self.1.lock().unwrap() = c;
+        *self.1.lock().unwrap() = (c, None);
     }
 }
