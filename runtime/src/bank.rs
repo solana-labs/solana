e235bea2e47d01f1f09747962a5e7ae2b4c2393d
haoran <haoran.yi@gmail.com>
2023-04-14 15:53:34 +0000

    clean up

--- runtime/src/bank.rs
+++ runtime/src/bank.rs
@@ -2871,8 +2871,8 @@ impl Bank {
         }
     }
 
-    /// Load, calculate and payout epoch rewards for stake and vote accounts
-    fn pay_validator_rewards_with_thread_pool2(
+    /// Load, calculate and payout epoch rewards for stake and vote accounts (no join of vote/stake accounts)
+    fn pay_validator_rewards_with_thread_pool_no_join(
         &mut self,
         rewarded_epoch: Epoch,
         rewards: u64,
@@ -2880,12 +2880,10 @@ impl Bank {
         credits_auto_rewind: bool,
         thread_pool: &ThreadPool,
         metrics: &mut RewardsMetrics,
-        _update_rewards_from_cached_accounts: bool,
     ) {
         let point_value = self.calculate_reward_points2(rewards, thread_pool, metrics);
 
         if let Some(point_value) = point_value {
-            info!("haha start redeem_rewards2");
             let (vote_account_rewards, stake_rewards) = self.redeem_rewards2(
                 rewarded_epoch,
                 point_value,
@@ -2898,19 +2896,6 @@ impl Bank {
             self.store_stake_accounts(&stake_rewards, metrics);
             let vote_rewards = self.store_vote_accounts(vote_account_rewards, metrics);
             self.update_reward_history(stake_rewards, vote_rewards);
-
-            // let mut rewards = self.redeem_rewards2(
-            //     rewarded_epoch,
-            //     point_value,
-            //     credits_auto_rewind,
-            //     thread_pool,
-            //     reward_calc_tracer.as_ref(),
-            //     metrics,
-            // );
-            info!("haha finish redeem_rewards2");
-
-            //info!("rewards_len: {}", rewards.len());
-            //self.update_reward_history2(&mut rewards);
         }
     }
 
@@ -2939,12 +2924,6 @@ impl Bank {
             metrics,
         );
 
-        // let point_value2 = self.calculate_reward_points2(rewards, thread_pool, metrics);
-
-        // assert!(point_value == point_value2);
-
-        // info!("calc_point_val: {:?}, {:?}", point_value, point_value2);
-
         if let Some(point_value) = point_value {
             let (vote_account_rewards, stake_rewards) = self.redeem_rewards(
                 vote_with_stake_delegations_map,
@@ -2955,40 +2934,6 @@ impl Bank {
                 reward_calc_tracer.as_ref(),
                 metrics,
             );
-
-            // let (vote_account_rewards2, stake_rewards2) = self.redeem_rewards2(
-            //     rewarded_epoch,
-            //     point_value2.unwrap(),
-            //     credits_auto_rewind,
-            //     thread_pool,
-            //     reward_calc_tracer.as_ref(),
-            //     metrics,
-            // );
-
-            // info!(
-            //     "vote_account_rewards_len: {}, {}",
-            //     vote_account_rewards.len(),
-            //     vote_account_rewards2.len()
-            // );
-
-            // info!(
-            //     "vote_account_rewards: {:?}, {:?}",
-            //     vote_account_rewards, vote_account_rewards2
-            // );
-
-            // info!(
-            //     "stake_rewards_len: {}, {}",
-            //     stake_rewards.len(),
-            //     stake_rewards2.len()
-            // );
-
-            // let vote_len1: usize = vote_account_rewards
-            //     .iter()
-            //     .map(|I| if I.3 { 1 } else { 0 })
-            //     .sum();
-
-            // assert!(vote_len1 == vote_account_rewards2.len());
-            // assert!(stake_rewards.len() == stake_rewards2.len());
 
             self.store_stake_accounts(&stake_rewards, metrics);
             let vote_rewards = self.store_vote_accounts(vote_account_rewards, metrics);
@@ -3044,7 +2989,6 @@ impl Bank {
         metrics: &mut RewardsMetrics,
     ) -> Option<PointValue> {
         let stake_history = self.stakes_cache.stakes().history().clone();
-
         let stakes = self.stakes_cache.stakes();
         let stake_delegations = self.filter_stake_delegations(&stakes);
 
@@ -3145,7 +3089,6 @@ impl Bank {
         thread_pool: &ThreadPool,
         reward_calc_tracer: Option<impl RewardCalcTracer>,
         metrics: &mut RewardsMetrics,
-        //) -> Vec<(Pubkey, RewardInfo)> {
     ) -> (VoteRewards, StakeRewards) {
         let stake_history = self.stakes_cache.stakes().history().clone();
 
@@ -3164,7 +3107,7 @@ impl Bank {
         };
 
         let vote_account_rewards: VoteRewards = DashMap::new();
-        let (mut stake_rewards, measure) = measure!(thread_pool.install(|| {
+        let (stake_rewards, measure) = measure!(thread_pool.install(|| {
             stake_delegations
                 .par_iter()
                 .filter_map(|(stake_pubkey, stake_account)| {
@@ -3232,18 +3175,6 @@ impl Bank {
                             },
                             stake_account,
                         });
-
-                        //self.store_account(&stake_pubkey, &stake_account);
-
-                        // (stakers_reward > 0).then_some((
-                        //     stake_pubkey,
-                        //     RewardInfo {
-                        //         reward_type: RewardType::Staking,
-                        //         lamports: i64::try_from(stakers_reward).unwrap(),
-                        //         post_balance,
-                        //         commission: Some(vote_state.commission),
-                        //     },
-                        // ));
                     } else {
                         debug!(
                             "stake_state::redeem_rewards() failed for {}: {:?}",
@@ -3253,14 +3184,9 @@ impl Bank {
                     None
                 })
                 .collect()
-            //.collect::<Vec<(Pubkey, RewardInfo)>>()
         }));
         metrics.redeem_rewards2_us += measure.as_us();
         (vote_account_rewards, stake_rewards)
-
-        //let mut rewards = self.store_vote_accounts(vote_account_rewards, metrics);
-        //rewards.append(&mut stake_rewards);
-        //rewards
     }
 
     fn redeem_rewards(
