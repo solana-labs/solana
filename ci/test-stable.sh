13fa1632f48675485386e049aa70a4a9e534bf7c
yihau <yihau.chen@icloud.com>
2023-04-14 14:08:27 +0800

    extract threads limit script

--- ci/test-stable.sh
+++ ci/test-stable.sh
@@ -27,16 +27,8 @@ export RUST_BACKTRACE=1
 export RUSTFLAGS="-D warnings"
 source scripts/ulimit-n.sh
 
-# limit jobs to 4gb/thread
-if [[ -f "/proc/meminfo" ]]; then
-  JOBS=$(grep MemTotal /proc/meminfo | awk '{printf "%.0f", ($2 / (4 * 1024 * 1024))}')
-else
-  JOBS=$(sysctl hw.memsize | awk '{printf "%.0f", ($2 / (4 * 1024**3))}')
-fi
-
-NPROC=$(nproc)
-JOBS=$((JOBS>NPROC ? NPROC : JOBS))
-
+#shellcheck source=ci/common/limit-threads.sh
+source ci/common/limit-threads.sh
 
 # get channel info
 eval "$(ci/channel-info.sh)"
