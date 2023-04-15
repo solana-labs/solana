4cef81c1413055459a22e767c10681058516213a
Ryo Onodera <ryoqun@gmail.com>
2023-04-15 14:08:05 +0900

    save

--- scheduler-pool/src/lib.rs
+++ scheduler-pool/src/lib.rs
@@ -198,7 +198,7 @@ mod tests {
     fn test_scheduler_pool_new() {
         let _ignored_prioritization_fee_cache = Arc::new(PrioritizationFeeCache::new(0u64));
         let pool = SchedulerPool::new_dyn(None, None, None, _ignored_prioritization_fee_cache);
-        let pool2: Arc<SchedulerPool> = unsafe { std::mem::transmute(pool) };
+        let pool2 = pool.downcast_unchecked();
         assert!(Arc::ptr_eq(&pool2, &pool2.weak_self.upgrade().unwrap()));
     }
 
