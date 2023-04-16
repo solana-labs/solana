b2cf096ce0cd36fe16bc14459155b45aed5ae9fb
Ryo Onodera <ryoqun@gmail.com>
2023-04-16 23:30:49 +0900

    save

--- scheduler-pool/src/lib.rs
+++ scheduler-pool/src/lib.rs
@@ -120,7 +120,7 @@ impl InstalledScheduler for Scheduler {
     }
 
     fn schedule_execution(&self, transaction: &SanitizedTransaction, index: usize) {
-        let &mut (result, timings) = &mut *self
+        let &mut (mut ref result, timings) = &mut *self
             .result_with_timing
             .lock()
             .expect("not poisoned")
