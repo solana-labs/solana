2db9a42970f69d369b2a7786bb82b40d1812f8f4
Ryo Onodera <ryoqun@gmail.com>
2023-04-17 21:24:49 +0900

    save

--- ledger/src/blockstore_processor.rs
+++ ledger/src/blockstore_processor.rs
@@ -4338,7 +4338,7 @@ pub mod tests {
         }
     }
 
-    fn create_test_transactions(mint_keypair: &Keypair) {
+    fn create_test_transactions(mint_keypair: &Keypair) -> Vec<SanitizedTransaction> {
         let pubkey = solana_sdk::pubkey::new_rand();
         let keypair2 = Keypair::new();
         let pubkey2 = solana_sdk::pubkey::new_rand();
