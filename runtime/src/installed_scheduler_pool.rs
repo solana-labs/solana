2b6a4f022bff90b8d133e6ea18f7801424e9fdb8
Ryo Onodera <ryoqun@gmail.com>
2023-04-17 14:49:34 +0900

    save

--- runtime/src/installed_scheduler_pool.rs
+++ runtime/src/installed_scheduler_pool.rs
@@ -402,5 +402,6 @@ mod tests {
         assert_eq!(bank.transaction_count(), 0);
         bank.schedule_transaction_executions(&[tx0], [0].iter());
         assert_eq!(bank.transaction_count(), 1);
+        std::mem::forget(bank);
     }
 }
