1ca4c6157ff3667c95e5c79418e65f51844f0a64
Ryo Onodera <ryoqun@gmail.com>
2023-04-17 14:48:14 +0900

    save

--- runtime/src/installed_scheduler_pool.rs
+++ runtime/src/installed_scheduler_pool.rs
@@ -400,7 +400,7 @@ mod tests {
         ));
 
         assert_eq!(bank.transaction_count(), 0);
-        bank.schedule_transaction_executions(&[tx0], &[0].into_iter());
+        bank.schedule_transaction_executions(&[tx0], [0].into_iter());
         assert_eq!(bank.transaction_count(), 1);
     }
 }
