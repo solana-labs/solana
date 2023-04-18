96efd590181da091f537e150b490f8d89be56efb
yihau <yihau.chen@icloud.com>
2023-04-18 20:17:28 +0800

    test common

---
+++ .buildkite/scripts/common.test.sh
@@ -0,0 +1,97 @@
+#!/usr/bin/env bash
+
+set -e
+here=$(dirname "$0")
+
+# shellcheck source=.buildkite/scripts/common.sh
+source "$here"/common.sh
+
+assert_eq() {
+  local test_name=$1
+  local want=$2
+  local got=$3
+
+  if [[ "$want" = "$got" ]]; then
+    echo "✅ $test_name"
+  else
+    cat <<EOF
+❌ $test_name
+want:
+$want
+got:
+$got
+EOF
+  fi
+}
+
+(
+  want=$(
+    cat <<EOF | indent
+- name: "test"
+  command: "start.sh"
+  timeout_in_minutes: 10
+  agents:
+    queue: "agent"
+EOF
+  )
+
+  got=$(step '{ "name": "test", "command": "start.sh", "timeout_in_minutes": 10, "agent": "agent"}')
+
+  assert_eq "basic setup" "$want" "$got"
+)
+
+(
+  want=$(
+    cat <<EOF | indent
+- name: "test"
+  command: "start.sh"
+  timeout_in_minutes: 10
+  agents:
+    queue: "agent"
+  parallelism: 3
+EOF
+  )
+
+  got=$(step '{ "name": "test", "command": "start.sh", "timeout_in_minutes": 10, "agent": "agent", "parallelism": 3}')
+
+  assert_eq "basic setup + parallelism" "$want" "$got"
+)
+
+(
+  want=$(
+    cat <<EOF | indent
+- name: "test"
+  command: "start.sh"
+  timeout_in_minutes: 10
+  agents:
+    queue: "agent"
+  retry:
+    automatic:
+      - limit: 3
+EOF
+  )
+
+  got=$(step '{ "name": "test", "command": "start.sh", "timeout_in_minutes": 10, "agent": "agent", "retry": 3}')
+
+  assert_eq "basic setup + retry" "$want" "$got"
+)
+
+(
+  want=$(
+    cat <<EOF | indent
+- name: "test"
+  command: "start.sh"
+  timeout_in_minutes: 10
+  agents:
+    queue: "agent"
+  parallelism: 3
+  retry:
+    automatic:
+      - limit: 3
+EOF
+  )
+
+  got=$(step '{ "name": "test", "command": "start.sh", "timeout_in_minutes": 10, "agent": "agent", "parallelism": 3, "retry": 3}')
+
+  assert_eq "basic setup + parallelism + retry" "$want" "$got"
+)
