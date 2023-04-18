39b6f4e8468398140c163ba727ed58a7bfc55e93
yihau <yihau.chen@icloud.com>
2023-04-18 21:06:54 +0800

    pretty format

--- .buildkite/scripts/build-stable.sh
+++ .buildkite/scripts/build-stable.sh
@@ -8,7 +8,27 @@ source "$here"/common.sh
 
 agent="${1-solana}"
 
+partitions=$(cat <<EOF
+{
+  "name": "partitions",
+  "command": ". ci/rust-version.sh; ci/docker-run.sh \$rust_stable_docker_image ci/stable/run-partition.sh",
+  "timeout_in_minutes": 30,
+  "agent": "$agent",
+  "parallelism": 3,
+  "retry": 3
+}
+EOF
+)
+
+localnet=$(cat <<EOF
+{
+  "name": "localnet",
+  "command": ". ci/rust-version.sh; ci/docker-run.sh \$rust_stable_docker_image ci/stable/run-localnet.sh",
+  "timeout_in_minutes": 30,
+  "agent": "$agent"
+}
+EOF
+)
+
 # shellcheck disable=SC2016
-group "stable" \
-  '{ "name": "partitions", "parallelism": 3, "command": ". ci/rust-version.sh; ci/docker-run.sh $$rust_stable_docker_image ci/stable/run-partition.sh", "timeout_in_minutes": 30, "agent": "'"$agent"'", "retry": 3 }' \
-  '{ "name": "localnet", "command": ". ci/rust-version.sh; ci/docker-run.sh $$rust_stable_docker_image ci/stable/run-localnet.sh", "timeout_in_minutes": 30, "agent": "'"$agent"'" }'
+group "stable" "$partitions" "$localnet"
