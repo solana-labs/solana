cece64daadffdfb8fd140daa45450b1127ccdb3e
Ryo Onodera <ryoqun@gmail.com>
2023-04-24 14:04:11 +0900

    ci: prefer clippy::default_trait_access over shell (#31318)
    Prefer clippy::default_trait_access over shell

--- ci/nits.sh
+++ ci/nits.sh
@@ -41,16 +41,6 @@ if _ git --no-pager grep -n "${prints[@]/#/-e}" -- "${print_free_tree[@]}"; then
     exit 1
 fi
 
-
-# Code readability: please be explicit about the type instead of using
-# Default::default()
-#
-# Ref: https://github.com/solana-labs/solana/issues/2630
-if _ git --no-pager grep -n 'Default::default()' -- '*.rs'; then
-    exit 1
-fi
-
-
 # Github Issues should be used to track outstanding work items instead of
 # marking up the code
 #
