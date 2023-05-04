93f47b9ac1f6612e4c4945a001a20f3a619bfbd6
haoran <haoran.yi@solana.com>
2023-05-04 15:03:25 +0000

    revert bench test

--- program-test/tests/warp.rs
+++ program-test/tests/warp.rs
@@ -82,7 +82,7 @@ async fn setup_vote(context: &mut ProgramTestContext) -> Pubkey {
     let vote_lamports = Rent::default().minimum_balance(VoteState::size_of());
     let vote_keypair = Keypair::new();
     let user_keypair = Keypair::new();
-    instructions.append(&mut vote_instruction::create_account(
+    instructions.append(&mut vote_instruction::create_account_with_config(
         &context.payer.pubkey(),
         &vote_keypair.pubkey(),
         &VoteInit {
@@ -91,6 +91,10 @@ async fn setup_vote(context: &mut ProgramTestContext) -> Pubkey {
             ..VoteInit::default()
         },
         vote_lamports,
+        vote_instruction::CreateVoteAccountConfig {
+            space: vote_state::VoteStateVersions::vote_state_size_of(true) as u64,
+            ..vote_instruction::CreateVoteAccountConfig::default()
+        },
     ));
 
     let transaction = Transaction::new_signed_with_payer(
@@ -281,17 +285,10 @@ async fn stake_rewards_from_warp() {
 
 #[tokio::test]
 async fn stake_rewards_filter_bench_100() {
-    stake_rewards_filter_bench_core(100, true).await;
+    stake_rewards_filter_bench_core(100).await;
 }
 
-#[tokio::test]
-async fn stake_rewards_bench_550_k() {
-    stake_rewards_filter_bench_core(550_000, false).await;
-}
-
-/// filter = true, to test that we exclude accounts with less than 1sol in reward
-/// filter = false, so that we can benchmark large number of stake accounts
-async fn stake_rewards_filter_bench_core(num_stake_accounts: u64, filter: bool) {
+async fn stake_rewards_filter_bench_core(num_stake_accounts: u64) {
     // Initialize and start the test network
     let mut program_test = ProgramTest::default();
 
@@ -304,26 +301,18 @@ async fn stake_rewards_filter_bench_core(num_stake_accounts: u64, filter: bool) 
 
     // create stake accounts with 0.9 sol to test min-stake filtering
     const TEST_FILTER_STAKE: u64 = 900_000_000; // 0.9 sol
-    const TEST_UNFILTER_STAKE: u64 = 1_100_000_000; // 1.1 sol
     let mut to_filter = vec![];
     for i in 0..num_stake_accounts {
         let stake_pubkey = Pubkey::new_unique();
-        let lamports = if filter {
-            TEST_FILTER_STAKE
-        } else {
-            TEST_UNFILTER_STAKE
-        };
         let stake_account = Account::from(stake_state::create_account(
             &stake_pubkey,
             &vote_address,
             &vote_account,
             &Rent::default(),
-            lamports,
+            TEST_FILTER_STAKE,
         ));
         program_test.add_account(stake_pubkey, stake_account);
-        if lamports == TEST_FILTER_STAKE {
-            to_filter.push(stake_pubkey);
-        }
+        to_filter.push(stake_pubkey);
         if i % 100 == 0 {
             debug!("create stake account {} {}", i, stake_pubkey);
         }
