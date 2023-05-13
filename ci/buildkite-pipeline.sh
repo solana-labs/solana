ed1692ba1afdc69f71093d5855b4a930cad55aab
Will Hickey <will.hickey@solana.com>
2023-05-13 14:03:12 -0500

    Add origin/ prefix for git diff when building CI pipeline.

--- ci/buildkite-pipeline.sh
+++ ci/buildkite-pipeline.sh
@@ -304,7 +304,7 @@ pull_or_push_steps() {
 
   # Version bump PRs are an edge case that can skip most of the CI steps
   if affects .toml$ && affects .lock$ && ! affects_other_than .toml$ .lock$; then
-    optional_old_version_number=$(git diff "$BUILDKITE_PULL_REQUEST_BASE_BRANCH"..HEAD validator/Cargo.toml | \
+    optional_old_version_number=$(git diff origin/"$BUILDKITE_PULL_REQUEST_BASE_BRANCH"..HEAD validator/Cargo.toml | \
       grep -e "^-version" | sed  's/-version = "\(.*\)"/\1/')
     echo "optional_old_version_number: ->$optional_old_version_number<-"
     new_version_number=$(grep -e  "^version = " validator/Cargo.toml | sed 's/version = "\(.*\)"/\1/')
@@ -314,7 +314,7 @@ pull_or_push_steps() {
     # lines that don't match. Any diff that produces output here is not a version bump.
     # | cat is a no-op. If this pull request is a version bump then grep will output no lines and have an exit code of 1.
     # Piping the output to cat prevents that non-zero exit code from exiting this script
-    diff_other_than_version_bump=$(git diff "$BUILDKITE_PULL_REQUEST_BASE_BRANCH"..HEAD | \
+    diff_other_than_version_bump=$(git diff origin/"$BUILDKITE_PULL_REQUEST_BASE_BRANCH"..HEAD | \
       grep -vE "^ |^@@ |^--- |^\+\+\+ |^index |^diff |^-( \")?solana.*$optional_old_version_number|^\+( \")?solana.*$new_version_number|^-version|^\+version"|cat)
     echo "diff_other_than_version_bump: ->$diff_other_than_version_bump<-"
 
