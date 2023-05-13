05c6bc62862c080838fcd4847220839c92dc7b1f
Maximilian Schneider <mail@maximilianschneider.net>
2023-05-13 15:58:27 +0200

    hold bank freeze lock in banks-server to prevent RC on slot change

--- banks-server/src/banks_server.rs
+++ banks-server/src/banks_server.rs
@@ -85,8 +85,21 @@ impl BanksServer {
                 .into_iter()
                 .map(|info| deserialize(&info.wire_transaction).unwrap())
                 .collect();
-            let bank = bank_forks.read().unwrap().working_bank();
-            let _ = bank.try_process_transactions(transactions.iter());
+            'inner: loop {
+                let bank = bank_forks.read().unwrap().working_bank();
+                // bank forks lock released, now verify bank hasn't been frozen yet
+                // in the mean-time the bank can not be frozen until this tx batch
+                // has been processed, this replicates BankingStage behaviour
+                let lock = bank.freeze_lock();
+                if *lock != Hash::default() {
+                    // retry reading working bank if bank is already frozen
+                    continue;
+                } else {
+                    let _ = bank.try_process_transactions(transactions.iter());
+                    // break out of inner loop and release bank freeze lock
+                    break 'inner;
+                }
+            }
         }
     }
 
