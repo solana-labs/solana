5c2ac80746b6b02d709ed3bddc66593c7fedb305
Tyera Eulberg <tyera@solana.com>
2023-05-16 22:34:15 -0600

    Remove redundant clone

--- rpc/src/rpc_service.rs
+++ rpc/src/rpc_service.rs
@@ -486,7 +486,7 @@ impl JsonRpcService {
             receiver,
             &connection_cache,
             send_transaction_service_config,
-            exit.clone(),
+            exit,
         ));
 
         #[cfg(test)]
