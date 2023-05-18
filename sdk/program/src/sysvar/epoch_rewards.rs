8f77bf908883c63fbf7f16639e47073a9ed78b52
HaoranYi <haoran.yi@gmail.com>
2023-05-17 21:14:20 -0500

    Update sdk/program/src/sysvar/epoch_rewards.rs
    Co-authored-by: Jon Cinque <joncinque@pm.me>

--- sdk/program/src/sysvar/epoch_rewards.rs
+++ sdk/program/src/sysvar/epoch_rewards.rs
@@ -132,7 +132,7 @@
 pub use crate::epoch_rewards::EpochRewards;
 use crate::{impl_sysvar_get, program_error::ProgramError, sysvar::Sysvar};
 
-crate::declare_sysvar_id!("SysvarEpochReward51111111111111111111111111", EpochRewards);
+crate::declare_sysvar_id!("SysvarEpochRewards1111111111111111111111111", EpochRewards);
 
 impl Sysvar for EpochRewards {
     impl_sysvar_get!(sol_get_epoch_rewards_sysvar);
