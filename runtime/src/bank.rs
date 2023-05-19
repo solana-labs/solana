a0d2722e952216d329d3879d43cc318a9814643f
HaoranYi <haoran.yi@solana.com>
2023-05-19 13:45:57 +0000

    clippy

--- runtime/src/bank.rs
+++ runtime/src/bank.rs
@@ -87,9 +87,9 @@ use {
     percentage::Percentage,
     rand::seq::SliceRandom,
     rand_chacha::{rand_core::SeedableRng, ChaChaRng},
-    rayon::slice::ParallelSliceMut,
     rayon::{
         iter::{IntoParallelIterator, IntoParallelRefIterator, ParallelIterator},
+        slice::ParallelSliceMut,
         ThreadPool, ThreadPoolBuilder,
     },
     solana_bpf_loader_program::syscalls::create_program_runtime_environment,
