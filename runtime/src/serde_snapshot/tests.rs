7a344566a431781caf1366fdfb16e7df866b8a6e
jeff washington <jeff.washington@solana.com>
2023-05-23 17:05:18 -0500

    change StartBlockHeightAndRewards from parent to current

--- runtime/src/serde_snapshot/tests.rs
+++ runtime/src/serde_snapshot/tests.rs
@@ -719,7 +719,7 @@ mod test_bank_serialize {
 
     // This some what long test harness is required to freeze the ABI of
     // Bank's serialization due to versioned nature
-    #[frozen_abi(digest = "8aBQnNxnfKDxGEmMwaq9uLeMh3fw5sKsMGZBsCp15Dmv")]
+    #[frozen_abi(digest = "9BucA5MtPMNNUjADyV27vNgzvDy1RqCLH2gRq5NEuDEF")]
     #[derive(Serialize, AbiExample)]
     pub struct BankAbiTestWrapperNewer {
         #[serde(serialize_with = "wrapper_newer")]
