174951478f11719c96483777f7eb6030eb6303ef
Yueh-Hsuan Chiang <93241502+yhchiang-sol@users.noreply.github.com>
2023-05-23 15:06:55 -0700

    Add new error type -- TieredStorageError (#31633)
    #### Summary of Changes
    This PR adds a new error type for the tiered storage.

--- runtime/src/tiered_storage.rs
+++ runtime/src/tiered_storage.rs
@@ -1,3 +1,8 @@
+pub mod error;
 pub mod file;
 pub mod footer;
 pub mod mmap_utils;
+
+use crate::tiered_storage::error::TieredStorageError;
+
+pub type TieredStorageResult<T> = Result<T, TieredStorageError>;
