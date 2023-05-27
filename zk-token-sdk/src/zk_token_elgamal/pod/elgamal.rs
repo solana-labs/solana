7a5259641f8a1ed76e0a4584517b5b9c692056ac
Sam Kim <skim13@cs.stanford.edu>
2023-05-27 10:59:20 +0900

    refactor `DecryptHandle` into `pod::elgamal` from `pod::pedersen`

--- zk-token-sdk/src/zk_token_elgamal/pod/elgamal.rs
+++ zk-token-sdk/src/zk_token_elgamal/pod/elgamal.rs
@@ -41,3 +41,13 @@ impl fmt::Display for ElGamalPubkey {
         write!(f, "{}", BASE64_STANDARD.encode(self.0))
     }
 }
+
+#[derive(Clone, Copy, Default, Pod, Zeroable, PartialEq, Eq)]
+#[repr(transparent)]
+pub struct DecryptHandle(pub [u8; 32]);
+
+impl fmt::Debug for DecryptHandle {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{:?}", self.0)
+    }
+}
--- zk-token-sdk/src/zk_token_elgamal/pod/pedersen.rs
+++ zk-token-sdk/src/zk_token_elgamal/pod/pedersen.rs
@@ -12,13 +12,3 @@ impl fmt::Debug for PedersenCommitment {
         write!(f, "{:?}", self.0)
     }
 }
-
-#[derive(Clone, Copy, Default, Pod, Zeroable, PartialEq, Eq)]
-#[repr(transparent)]
-pub struct DecryptHandle(pub [u8; 32]);
-
-impl fmt::Debug for DecryptHandle {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{:?}", self.0)
-    }
-}
