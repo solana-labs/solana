569451c3b62ea97e6017e2eb07d718d8ca5d4ae7
Sam Kim <skim13@cs.stanford.edu>
2023-05-28 13:52:21 +0900

    rename `sigma_proof::ctxt_ctxt_equality_proof` to non-abbreviated name

--- zk-token-sdk/src/zk_token_elgamal/convert.rs
+++ zk-token-sdk/src/zk_token_elgamal/convert.rs
@@ -64,8 +64,8 @@ mod target_arch {
             },
             range_proof::{errors::RangeProofError, RangeProof},
             sigma_proofs::{
+                ciphertext_ciphertext_equality_proof::CiphertextCiphertextEqualityProof,
                 ciphertext_commitment_equality_proof::CiphertextCommitmentEqualityProof,
-                ctxt_ctxt_equality_proof::CiphertextCiphertextEqualityProof,
                 errors::*,
                 fee_proof::FeeSigmaProof,
                 pubkey_proof::PubkeyValidityProof,
