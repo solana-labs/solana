cc7e4ebe0616adc70378c30b34f75819b866d22c
samkim-crypto <skim13@cs.stanford.edu>
2023-05-31 08:12:38 +0900

    [zk-token-sdk] Refactor `zk-token-elgamal` conversion code for authenticated encryption, range proof, and sigma proof types (#31855)
    * refactor convert logic for `AeCiphertext`
    
    * refactor convert logic for `RangeProof`
    
    * refactor convert logic for sigma proof types
    
    * Apply suggestions from code review
    
    Co-authored-by: Tyera <teulberg@gmail.com>
    
    ---------
    
    Co-authored-by: Tyera <teulberg@gmail.com>
    (cherry picked from commit e4fe9335e7471994d594561d329807aab9492b13)

--- zk-token-sdk/src/zk_token_elgamal/pod/auth_encryption.rs
+++ zk-token-sdk/src/zk_token_elgamal/pod/auth_encryption.rs
@@ -1,16 +1,21 @@
+//! Plain Old Data types for the AES128-GCM-SIV authenticated encryption scheme.
+
+#[cfg(not(target_os = "solana"))]
+use crate::{encryption::auth_encryption as decoded, errors::ProofError};
 use {
     crate::zk_token_elgamal::pod::{Pod, Zeroable},
     base64::{prelude::BASE64_STANDARD, Engine},
     std::fmt,
 };
 
-/// Serialization for AeCiphertext
+/// The `AeCiphertext` type as a `Pod`.
 #[derive(Clone, Copy, PartialEq, Eq)]
 #[repr(transparent)]
 pub struct AeCiphertext(pub [u8; 36]);
 
-// `AeCiphertext` is a Pod and Zeroable.
-// Add the marker traits manually because `bytemuck` only adds them for some `u8` arrays
+// `AeCiphertext` is a wrapper type for a byte array, which is both `Pod` and `Zeroable`. However,
+// the marker traits `bytemuck::Pod` and `bytemuck::Zeroable` can only be derived for power-of-two
+// length byte arrays. Directly implement these traits for `AeCiphertext`.
 unsafe impl Zeroable for AeCiphertext {}
 unsafe impl Pod for AeCiphertext {}
 
@@ -29,5 +34,21 @@ impl fmt::Display for AeCiphertext {
 impl Default for AeCiphertext {
     fn default() -> Self {
         Self::zeroed()
+    }
+}
+
+#[cfg(not(target_os = "solana"))]
+impl From<decoded::AeCiphertext> for AeCiphertext {
+    fn from(decoded_ciphertext: decoded::AeCiphertext) -> Self {
+        Self(decoded_ciphertext.to_bytes())
+    }
+}
+
+#[cfg(not(target_os = "solana"))]
+impl TryFrom<AeCiphertext> for decoded::AeCiphertext {
+    type Error = ProofError;
+
+    fn try_from(pod_ciphertext: AeCiphertext) -> Result<Self, Self::Error> {
+        Self::from_bytes(&pod_ciphertext.0).ok_or(ProofError::CiphertextDeserialization)
     }
 }
