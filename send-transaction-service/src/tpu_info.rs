1a0e0f1d79ebea3d535c1738631fee5e50825d3e
Lijun Wang <83639177+lijunwangs@users.noreply.github.com>
2023-06-01 13:18:49 -0700

    Address some code review comment from Behzad: get_leader_tpus per protocol

--- send-transaction-service/src/tpu_info.rs
+++ send-transaction-service/src/tpu_info.rs
@@ -1,16 +1,16 @@
 use {solana_client::connection_cache::Protocol, std::net::SocketAddr};
 
 pub trait TpuInfo {
-    fn refresh_recent_peers(&mut self, protocol: Protocol);
-    fn get_leader_tpus(&self, max_count: u64) -> Vec<&SocketAddr>;
+    fn refresh_recent_peers(&mut self);
+    fn get_leader_tpus(&self, max_count: u64, protocol: Protocol) -> Vec<&SocketAddr>;
 }
 
 #[derive(Clone)]
 pub struct NullTpuInfo;
 
 impl TpuInfo for NullTpuInfo {
-    fn refresh_recent_peers(&mut self, _protocol: Protocol) {}
-    fn get_leader_tpus(&self, _max_count: u64) -> Vec<&SocketAddr> {
+    fn refresh_recent_peers(&mut self) {}
+    fn get_leader_tpus(&self, _max_count: u64, _protocol: Protocol) -> Vec<&SocketAddr> {
         vec![]
     }
 }
