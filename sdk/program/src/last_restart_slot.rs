e2c836d774e799ab3fbb265aad7c76ce82102f02
Godmode Galactus <godmodegalactus@gmail.com>
2023-06-05 17:36:46 +0200

    fixing the CI

--- sdk/program/src/last_restart_slot.rs
+++ sdk/program/src/last_restart_slot.rs
@@ -5,16 +5,8 @@ use solana_sdk_macro::CloneZeroed;
 pub type Slot = u64;
 
 #[repr(C)]
-#[derive(Serialize, Deserialize, Debug, CloneZeroed, PartialEq, Eq)]
+#[derive(Serialize, Deserialize, Debug, CloneZeroed, PartialEq, Eq, Default)]
 pub struct LastRestartSlot {
     /// The last restart `Slot`.
     pub last_restart_slot: Slot,
-}
-
-impl Default for LastRestartSlot {
-    fn default() -> Self {
-        Self {
-            last_restart_slot: 0,
-        }
-    }
 }
