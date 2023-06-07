520cae7d96d183cff0c37f0388e22b4966a8ea83
HaoranYi <haoran.yi@solana.com>
2023-06-07 15:33:43 +0000

    update abi hash

--- runtime/src/bank.rs
+++ runtime/src/bank.rs
@@ -222,7 +222,7 @@ struct RentMetrics {
 }
 
 pub type BankStatusCache = StatusCache<Result<()>>;
-#[frozen_abi(digest = "BzMfUBpuJsPJRaBW3YFz3J4cKKtnDuvQSChcHTPqtoUu")]
+#[frozen_abi(digest = "3FiwE61TtjxHenszm3oFTzmHtGQGohJz3YN3TSTwcbUM")]
 pub type BankSlotDelta = SlotDelta<Result<()>>;
 
 #[derive(Default, Copy, Clone, Debug, PartialEq, Eq)]
