a8e2f581e4d2351b9829a01c4b52321275117079
Sam Kim <skim13@cs.stanford.edu>
2023-06-15 14:30:00 +0900

    remove decoded `TransferPubkey`

--- zk-token-sdk/src/zk_token_elgamal/convert.rs
+++ zk-token-sdk/src/zk_token_elgamal/convert.rs
@@ -50,10 +50,7 @@ impl From<PodRistrettoPoint> for pod::DecryptHandle {
 mod target_arch {
     use {
         super::pod,
-        crate::{
-            curve25519::scalar::PodScalar, errors::ProofError,
-            instruction::transfer::TransferPubkeys,
-        },
+        crate::{curve25519::scalar::PodScalar, errors::ProofError},
         curve25519_dalek::{ristretto::CompressedRistretto, scalar::Scalar},
         std::convert::TryFrom,
     };
@@ -81,28 +78,6 @@ mod target_arch {
     impl From<pod::CompressedRistretto> for CompressedRistretto {
         fn from(pod: pod::CompressedRistretto) -> Self {
             Self(pod.0)
-        }
-    }
-
-    impl From<TransferPubkeys> for pod::TransferPubkeys {
-        fn from(keys: TransferPubkeys) -> Self {
-            Self {
-                source_pubkey: keys.source_pubkey.into(),
-                destination_pubkey: keys.destination_pubkey.into(),
-                auditor_pubkey: keys.auditor_pubkey.into(),
-            }
-        }
-    }
-
-    impl TryFrom<pod::TransferPubkeys> for TransferPubkeys {
-        type Error = ProofError;
-
-        fn try_from(pod: pod::TransferPubkeys) -> Result<Self, Self::Error> {
-            Ok(Self {
-                source_pubkey: pod.source_pubkey.try_into()?,
-                destination_pubkey: pod.destination_pubkey.try_into()?,
-                auditor_pubkey: pod.auditor_pubkey.try_into()?,
-            })
         }
     }
 }
