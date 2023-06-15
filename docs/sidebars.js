eabe1070667e87f447b9cb892e2d916ca5b68e34
Raza <42661870+AlmostEfficient@users.noreply.github.com>
2023-06-15 14:35:19 +1200

    [docs] Add beginner-friendly intro to Solana guide  (#31861)
    * Docs: Create intro to Solana guide
    
    * Docs: Fix formatting on getstarted/overview
    
    * Docs: Fix linter errors on getstarted/overview

--- docs/sidebars.js
+++ docs/sidebars.js
@@ -94,6 +94,11 @@ module.exports = {
         },
         {
           type: "doc",
+          id: "getstarted/overview",
+          label: "Overview",
+        },
+        {
+          type: "doc",
           id: "getstarted/hello-world",
           label: "Hello world",
         },
