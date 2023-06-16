62f08ecb8bf738c77ccc7596201338fdc0194603
samkim-crypto <skim13@cs.stanford.edu>
2023-06-16 17:53:36 +0900

    [zk-token-sdk] Define `FeeEncryption` as a wrapper around `GroupedElGamalCiphertext` (#32144)
    * define `FeeEncryption` as a wrapper around `GroupedElGamalCiphertext`
    
    * define pod `FeeEncryption` as a wrapper around `GroupedElGamalCiphertext2Handles`
    
    * update proof data computation
    
    * add comments about unwraps
    
    (cherry picked from commit 37f51e8376248ab3c8fc26f4ca8483fbada37da1)

--- zk-token-sdk/src/instruction/transfer/encryption.rs
+++ zk-token-sdk/src/instruction/transfer/encryption.rs
@@ -1,11 +1,8 @@
 #[cfg(not(target_os = "solana"))]
-use crate::{
-    encryption::{
-        elgamal::{DecryptHandle, ElGamalPubkey},
-        grouped_elgamal::{GroupedElGamal, GroupedElGamalCiphertext},
-        pedersen::{Pedersen, PedersenCommitment, PedersenOpening},
-    },
-    zk_token_elgamal::pod,
+use crate::encryption::{
+    elgamal::{DecryptHandle, ElGamalPubkey},
+    grouped_elgamal::{GroupedElGamal, GroupedElGamalCiphertext},
+    pedersen::{PedersenCommitment, PedersenOpening},
 };
 
 #[derive(Clone, Copy, Debug, Eq, PartialEq)]
@@ -54,15 +51,10 @@ impl TransferAmountCiphertext {
     }
 }
 
-// FeeEncryption
-#[derive(Clone)]
+#[derive(Clone, Copy, Debug, Eq, PartialEq)]
 #[repr(C)]
 #[cfg(not(target_os = "solana"))]
-pub struct FeeEncryption {
-    pub commitment: PedersenCommitment,
-    pub destination_handle: DecryptHandle,
-    pub withdraw_withheld_authority_handle: DecryptHandle,
-}
+pub struct FeeEncryption(pub(crate) GroupedElGamalCiphertext<2>);
 
 #[cfg(not(target_os = "solana"))]
 impl FeeEncryption {
@@ -71,22 +63,29 @@ impl FeeEncryption {
         destination_pubkey: &ElGamalPubkey,
         withdraw_withheld_authority_pubkey: &ElGamalPubkey,
     ) -> (Self, PedersenOpening) {
-        let (commitment, opening) = Pedersen::new(amount);
-        let fee_encryption = Self {
-            commitment,
-            destination_handle: destination_pubkey.decrypt_handle(&opening),
-            withdraw_withheld_authority_handle: withdraw_withheld_authority_pubkey
-                .decrypt_handle(&opening),
-        };
+        let opening = PedersenOpening::new_rand();
+        let grouped_ciphertext = GroupedElGamal::<2>::encrypt_with(
+            [destination_pubkey, withdraw_withheld_authority_pubkey],
+            amount,
+            &opening,
+        );
 
-        (fee_encryption, opening)
+        (Self(grouped_ciphertext), opening)
     }
 
-    pub fn to_pod(&self) -> pod::FeeEncryption {
-        pod::FeeEncryption {
-            commitment: self.commitment.into(),
-            destination_handle: self.destination_handle.into(),
-            withdraw_withheld_authority_handle: self.withdraw_withheld_authority_handle.into(),
-        }
+    pub fn get_commitment(&self) -> &PedersenCommitment {
+        &self.0.commitment
+    }
+
+    pub fn get_destination_handle(&self) -> &DecryptHandle {
+        // `FeeEncryption` is a wrapper for `GroupedElGamalCiphertext<2>`, which holds
+        // exactly two decryption handles.
+        self.0.handles.get(0).unwrap()
+    }
+
+    pub fn get_withdraw_withheld_authority_handle(&self) -> &DecryptHandle {
+        // `FeeEncryption` is a wrapper for `GroupedElGamalCiphertext<2>`, which holds
+        // exactly two decryption handles.
+        self.0.handles.get(1).unwrap()
     }
 }
