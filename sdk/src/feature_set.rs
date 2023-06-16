ec9fdbf2c3c69e609fa5432c74893ea1dcf06d20
HaoranYi <haoran.yi@gmail.com>
2023-06-16 08:41:08 -0500

    Add epoch reward feature id (#32051)
    * add epoch reward feature id
    
    * add feature gate issue id
    
    ---------
    
    Co-authored-by: HaoranYi <haoran.yi@solana.com>

--- sdk/src/feature_set.rs
+++ sdk/src/feature_set.rs
@@ -368,6 +368,9 @@ pub mod fix_recent_blockhashes {
 pub mod update_rewards_from_cached_accounts {
     solana_sdk::declare_id!("28s7i3htzhahXQKqmS2ExzbEoUypg9krwvtK2M9UWXh9");
 }
+pub mod enable_partitioned_epoch_reward {
+    solana_sdk::declare_id!("HCnE3xQoZtDz9dSVm3jKwJXioTb6zMRbgwCmGg3PHHk8");
+}
 
 pub mod spl_token_v3_4_0 {
     solana_sdk::declare_id!("Ftok4njE8b7tDffYkC5bAbCaQv5sL6jispYrprzatUwN");
@@ -751,6 +754,7 @@ lazy_static! {
         (executables_incur_cpi_data_cost::id(), "Executables incur CPI data costs"),
         (fix_recent_blockhashes::id(), "stop adding hashes for skipped slots to recent blockhashes"),
         (update_rewards_from_cached_accounts::id(), "update rewards from cached accounts"),
+        (enable_partitioned_epoch_reward::id(), "enable partitioned rewards at epoch boundary #32166"),
         (spl_token_v3_4_0::id(), "SPL Token Program version 3.4.0 release #24740"),
         (spl_associated_token_account_v1_1_0::id(), "SPL Associated Token Account Program version 1.1.0 release #24741"),
         (default_units_per_instruction::id(), "Default max tx-wide compute units calculated per instruction"),
