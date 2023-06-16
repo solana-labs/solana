98f9255b1488ca92905b6c9d5f13dee1640342be
Godmode Galactus <godmodegalactus@gmail.com>
2023-06-16 21:20:08 +0200

    reverting sysvar_cache.rs because change was not necessary

--- runtime/src/bank/sysvar_cache.rs
+++ runtime/src/bank/sysvar_cache.rs
@@ -7,8 +7,7 @@ impl Bank {
     pub(crate) fn fill_missing_sysvar_cache_entries(&self) {
         let mut sysvar_cache = self.sysvar_cache.write().unwrap();
         sysvar_cache.fill_missing_entries(|pubkey, callback| {
-            let acc = self.get_account_with_fixed_root(pubkey);
-            if let Some(account) = acc {
+            if let Some(account) = self.get_account_with_fixed_root(pubkey) {
                 callback(account.data());
             }
         });
