4ea3d943a1e00d17038ab65ae34f40dd86928831
samkim-crypto <skim13@cs.stanford.edu>
2023-06-18 07:51:49 +0900

    [zk-token-sdk] Refactor pod `FeeParameters` conversion and remove manual byte conversion for the type (#32149)
    * replace `decoded::TransferAmountCiphertext` to `DecodedTransferAmountCiphertext`
    
    * refactor pod convert logic for fee parameters
    
    * remove manual byte conversions for `FeeParameters`
    
    * fix error from rebase
    
    (cherry picked from commit 428283c9ef6a04fdd864022564f5bfc479176ae9)

--- zk-token-sdk/src/instruction/transfer/mod.rs
+++ zk-token-sdk/src/instruction/transfer/mod.rs
@@ -8,7 +8,6 @@ use {
         elgamal::ElGamalCiphertext,
         pedersen::{PedersenCommitment, PedersenOpening},
     },
-    arrayref::{array_ref, array_refs},
     curve25519_dalek::scalar::Scalar,
 };
 #[cfg(not(target_os = "solana"))]
@@ -91,25 +90,4 @@ pub struct FeeParameters {
     pub fee_rate_basis_points: u16,
     /// Maximum fee assessed on transfers, expressed as an amount of tokens
     pub maximum_fee: u64,
-}
-
-#[cfg(not(target_os = "solana"))]
-impl FeeParameters {
-    pub fn to_bytes(&self) -> [u8; 10] {
-        let mut bytes = [0u8; 10];
-        bytes[..2].copy_from_slice(&self.fee_rate_basis_points.to_le_bytes());
-        bytes[2..10].copy_from_slice(&self.maximum_fee.to_le_bytes());
-
-        bytes
-    }
-
-    pub fn from_bytes(bytes: &[u8]) -> Self {
-        let bytes = array_ref![bytes, 0, 10];
-        let (fee_rate_basis_points, maximum_fee) = array_refs![bytes, 2, 8];
-
-        Self {
-            fee_rate_basis_points: u16::from_le_bytes(*fee_rate_basis_points),
-            maximum_fee: u64::from_le_bytes(*maximum_fee),
-        }
-    }
 }
