b363d5a9e4805f6d6bf311f43c426f47b7b6222c
Michael Hubbard <michaelh@laine.co.za>
2023-06-21 19:54:54 +0200

    Format and adjust padding

--- cli-output/src/cli_output.rs
+++ cli-output/src/cli_output.rs
@@ -226,12 +226,8 @@ impl fmt::Display for CliBlockProduction {
             f,
             "{}",
             style(format!(
-                "  {:<44}  {:>15}  {:>15}  {:>15}  {:>23}",
-                "Identity",
-                "Leader Slots",
-                "Blocks Produced",
-                "Skipped Slots",
-                "Skip Rate",
+                "  {:<44}  {:>15}  {:>15}  {:>15}  {:>15}",
+                "Identity", "Leader Slots", "Blocks Produced", "Skipped Slots", "Skip Rate",
             ))
             .bold()
         )?;
