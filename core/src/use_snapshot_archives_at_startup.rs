24cf0edf2252125834290e403b46d128ee74a14b
brooks <brooks@solana.com>
2023-06-23 08:43:56 -0400

    remove default

--- core/src/use_snapshot_archives_at_startup.rs
+++ core/src/use_snapshot_archives_at_startup.rs
@@ -2,13 +2,12 @@ use strum::{Display, EnumString, EnumVariantNames, IntoStaticStr, VariantNames};
 
 /// When should snapshot archives be used at startup?
 #[derive(
-    Debug, Default, Clone, Copy, PartialEq, Eq, Display, EnumString, EnumVariantNames, IntoStaticStr,
+    Debug, Clone, Copy, PartialEq, Eq, Display, EnumString, EnumVariantNames, IntoStaticStr,
 )]
 #[strum(serialize_all = "kebab-case")]
 pub enum UseSnapshotArchivesAtStartup {
     /// If snapshot archives are used, they will be extracted and overwrite any existing state
     /// already on disk.  This will incur the associated runtime costs for extracting.
-    #[default]
     Always,
     /// If snapshot archive are not used, then the local snapshot state already on disk is
     /// used instead.  If there is no local state on disk, startup will fail.
@@ -44,6 +43,6 @@ pub mod cli {
         UseSnapshotArchivesAtStartup::VARIANTS
     }
     pub fn default_value() -> &'static str {
-        UseSnapshotArchivesAtStartup::default().into()
+        UseSnapshotArchivesAtStartup::Always.into()
     }
 }
