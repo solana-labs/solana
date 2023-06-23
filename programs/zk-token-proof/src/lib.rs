83439d484f60d597375195287676d6f6722b98ee
Ryo Onodera <ryoqun@gmail.com>
2023-06-23 15:41:54 +0900

    make ci fail with insufficient dev-utils marking

--- programs/zk-token-proof/src/lib.rs
+++ programs/zk-token-proof/src/lib.rs
@@ -64,7 +64,7 @@ where
             return Err(InstructionError::InvalidAccountData);
         }
 
-        proof_context_account.set_data(context_state_data)?;
+        proof_context_account.set_data_from_slice(&context_state_data)?;
     }
 
     Ok(())
