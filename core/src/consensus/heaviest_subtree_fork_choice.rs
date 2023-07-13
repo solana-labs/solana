60130fdd75fe3fe59249926dcb3a59aa5e8845a3
Brooks <brooks@solana.com>
2023-07-13 13:51:15 -0400

    Uses while-let (#32479)

--- core/src/consensus/heaviest_subtree_fork_choice.rs
+++ core/src/consensus/heaviest_subtree_fork_choice.rs
@@ -356,8 +356,7 @@ impl HeaviestSubtreeForkChoice {
         let mut tree_root = None;
         // Find the subtrees to prune
         let mut to_visit = vec![self.tree_root];
-        while !to_visit.is_empty() {
-            let cur_slot = to_visit.pop().unwrap();
+        while let Some(cur_slot) = to_visit.pop() {
             if cur_slot == new_root {
                 tree_root = Some(new_root);
                 continue;
@@ -532,8 +531,7 @@ impl HeaviestSubtreeForkChoice {
         let mut split_tree_fork_infos = HashMap::new();
         let mut to_visit = vec![*slot_hash_key];
 
-        while !to_visit.is_empty() {
-            let current_node = to_visit.pop().unwrap();
+        while let Some(current_node) = to_visit.pop() {
             let current_fork_info = self
                 .fork_infos
                 .remove(&current_node)
