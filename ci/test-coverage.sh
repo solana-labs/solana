a9b31902cb8fc0679f9829ed6c2b99f515c0a424
yihau <yihau.chen@icloud.com>
2023-07-14 15:40:13 +0800

    ci: handle http code for uploading coverage test as well

--- ci/test-coverage.sh
+++ ci/test-coverage.sh
@@ -32,7 +32,25 @@ else
   # We normalize CI to `1`; but codecov expects it to be `true` to detect Buildkite...
   # Unfortunately, codecov.io fails sometimes:
   #   curl: (7) Failed to connect to codecov.io port 443: Connection timed out
-  CI=true bash <(while ! curl -sS --retry 5 --retry-delay 2 --retry-connrefused https://codecov.io/bash; do sleep 10; done) -Z -X gcov -f target/cov/lcov.info
+  retry=5
+  while :; do
+    if ((retry > 0)); then
+      echo "fetching coverage_bash_script..."
+    else
+      echo "can't fetch coverage_bash_script successfully"
+      exit 1
+    fi
+
+    http_code=$(curl -s -o coverage_bash_script -w "%{http_code}" https://codecov.io/bash)
+    if [[ "$http_code" = "200" ]]; then
+      break
+    fi
+
+    echo "got http_code $http_code"
+    retry="$((retry - 1))"
+    sleep 2
+  done
+  CI=true bash <coverage_bash_script -Z -X gcov -f target/cov/lcov.info
 
   annotate --style success --context codecov.io \
     "CodeCov report: https://codecov.io/github/solana-labs/solana/commit/${CI_COMMIT:0:9}"
