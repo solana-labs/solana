792cc81a35aadec9b69a97c171a0b0e75f539e0f
Ryo Onodera <ryoqun@gmail.com>
2023-07-15 14:57:18 +0900

    save 2023y07m15d14h57m18s254633486ns

--- poh/src/poh_recorder.rs
+++ poh/src/poh_recorder.rs
@@ -20,7 +20,6 @@ use {
     },
     solana_ledger::{
         blockstore::Blockstore,
-        genesis_utils::{create_genesis_config, GenesisConfigInfo},
         leader_schedule_cache::LeaderScheduleCache,
     },
     solana_measure::{measure, measure_us},
@@ -44,6 +43,13 @@ use {
     },
     thiserror::Error,
 };
+
+#[cfg(feature = "dev-context-only-utils")]
+use {
+    solana_ledger::{
+        genesis_utils::{create_genesis_config, GenesisConfigInfo},
+    }
+}
 
 pub const GRACE_TICKS_FACTOR: u64 = 2;
 pub const MAX_GRACE_SLOTS: u64 = 2;
