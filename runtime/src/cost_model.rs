47ac22f055e1e95b3e3d282c5cc90c6d905c48e9
Tao Zhu <tao@solana.com>
2023-07-18 21:20:27 +0000

    fix backport merge conflict

--- runtime/src/cost_model.rs
+++ runtime/src/cost_model.rs
@@ -175,54 +175,31 @@ impl CostModel {
             }
         }
 
-        // calculate bpf cost based on compute budget instructions
-        let mut budget = ComputeBudget::default();
+        // if tx contained user-space instructions and a more accurate estimate available correct it,
+        // where "user-space instructions" must be specifically checked by
+        // 'compute_unit_limit_is_set' flag, because compute_budget does not distinguish
+        // builtin and bpf instructions when calculating default compute-unit-limit. (see
+        // compute_budget.rs test `test_process_mixed_instructions_without_compute_budget`)
+        if bpf_costs > 0 && compute_unit_limit_is_set {
+            // calculate bpf cost based on compute budget instructions
+            let mut budget = ComputeBudget::default();
 
-        // Starting from v1.15, cost model uses compute_budget.set_compute_unit_limit to
-        // measure bpf_costs (code below), vs earlier versions that use estimated
-        // bpf instruction costs. The calculated transaction costs are used by leaders
-        // during block packing, different costs for same transaction due to different versions
-        // will not impact consensus. So for v1.15+, should call compute budget with
-        // the feature gate `enable_request_heap_frame_ix` enabled.
-        let enable_request_heap_frame_ix = true;
-        let result = budget.process_instructions(
-            transaction.message().program_instructions_iter(),
-            feature_set.is_active(&use_default_units_in_fee_calculation::id()),
-            !feature_set.is_active(&remove_deprecated_request_unit_ix::id()),
-            enable_request_heap_frame_ix,
-            feature_set.is_active(&add_set_tx_loaded_accounts_data_size_instruction::id()),
-        );
+            // Starting from v1.15, cost model uses compute_budget.set_compute_unit_limit to
+            // measure bpf_costs (code below), vs earlier versions that use estimated
+            // bpf instruction costs. The calculated transaction costs are used by leaders
+            // during block packing, different costs for same transaction due to different versions
+            // will not impact consensus. So for v1.15+, should call compute budget with
+            // the feature gate `enable_request_heap_frame_ix` enabled.
+            let enable_request_heap_frame_ix = true;
+            let _result = budget.process_instructions(
+                transaction.message().program_instructions_iter(),
+                feature_set.is_active(&use_default_units_in_fee_calculation::id()),
+                !feature_set.is_active(&remove_deprecated_request_unit_ix::id()),
+                enable_request_heap_frame_ix,
+                feature_set.is_active(&add_set_tx_loaded_accounts_data_size_instruction::id()),
+            );
 
-<<<<<<< HEAD:runtime/src/cost_model.rs
-        // if tx contained user-space instructions and a more accurate estimate available correct it
-        if bpf_costs > 0 && result.is_ok() {
             bpf_costs = budget.compute_unit_limit
-=======
-        // if failed to process compute_budget instructions, the transaction will not be executed
-        // by `bank`, therefore it should be considered as no execution cost by cost model.
-        match result {
-            Ok(_) => {
-                // if tx contained user-space instructions and a more accurate estimate available correct it,
-                // where "user-space instructions" must be specifically checked by
-                // 'compute_unit_limit_is_set' flag, because compute_budget does not distinguish
-                // builtin and bpf instructions when calculating default compute-unit-limit. (see
-                // compute_budget.rs test `test_process_mixed_instructions_without_compute_budget`)
-                if bpf_costs > 0 && compute_unit_limit_is_set {
-                    bpf_costs = compute_budget.compute_unit_limit
-                }
-
-                if feature_set
-                    .is_active(&include_loaded_accounts_data_size_in_fee_calculation::id())
-                {
-                    loaded_accounts_data_size_cost =
-                        Self::calculate_loaded_accounts_data_size_cost(&compute_budget);
-                }
-            }
-            Err(_) => {
-                builtin_costs = 0;
-                bpf_costs = 0;
-            }
->>>>>>> c69bc00f69 (cost model could double count builtin instruction cost (#32422)):cost-model/src/cost_model.rs
         }
 
         tx_cost.builtins_execution_cost = builtin_costs;
