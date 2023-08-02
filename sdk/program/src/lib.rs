8c148868912bcb7e8ea419965d0fc37dcaab4b67
Jon Cinque <me@jonc.dev>
2023-08-02 23:15:24 +0200

    sdk: Limited Borsh 0.9 support (Pubkey and helpers) (#32511)
    * sdk: Implement Borsh 0.9 traits on Pubkey
    
    * Alphabetize cargo.toml
    
    * Add backwards-compatible borsh file
    
    * Add borsh0_10.rs for more clarity
    
    * Deprecate `borsh` utils, use borsh0_10 everywhere
    
    * Mark borsh 0.9 helpers as deprecated
    
    * Add macros for deriving helper impls
    
    * Add borsh 0.9 tests
    
    * Refactor tests into macro

--- sdk/program/src/lib.rs
+++ sdk/program/src/lib.rs
@@ -477,6 +477,8 @@ pub(crate) mod atomic_u64;
 pub mod big_mod_exp;
 pub mod blake3;
 pub mod borsh;
+pub mod borsh0_10;
+pub mod borsh0_9;
 pub mod bpf_loader;
 pub mod bpf_loader_deprecated;
 pub mod bpf_loader_upgradeable;
