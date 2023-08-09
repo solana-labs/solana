3ed34106df2f91dc6a7d5e524b0e75e5d35aa4b7
Ashwin Sekar <ashwin@solana.com>
2023-08-09 15:45:02 -0700

    stake: pub use config constants for api backwards compatibility (#32769)
    * stake: pub use config constants for api backwards compatibility
    
    * Add deprecate
    
    Co-authored-by: Trent Nelson <trent.a.b.nelson@gmail.com>
    
    ---------
    
    Co-authored-by: Trent Nelson <trent.a.b.nelson@gmail.com>
    (cherry picked from commit 85a9ebc0af1dc0f4d22c3ff976d06c3282f2aba2)

--- sdk/program/src/stake/config.rs
+++ sdk/program/src/stake/config.rs
@@ -1,10 +1,12 @@
 //! config for staking
 //!  carries variables that the stake program cares about
 
-use {
-    super::state::{DEFAULT_SLASH_PENALTY, DEFAULT_WARMUP_COOLDOWN_RATE},
-    serde_derive::{Deserialize, Serialize},
-};
+#[deprecated(
+    since = "1.16.7",
+    note = "Please use `solana_sdk::stake::state::{DEFAULT_SLASH_PENALTY, DEFAULT_WARMUP_COOLDOWN_RATE}` instead"
+)]
+pub use super::state::{DEFAULT_SLASH_PENALTY, DEFAULT_WARMUP_COOLDOWN_RATE};
+use serde_derive::{Deserialize, Serialize};
 
 // stake config ID
 crate::declare_deprecated_id!("StakeConfig11111111111111111111111111111111");
