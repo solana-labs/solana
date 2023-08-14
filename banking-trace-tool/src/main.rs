f76f04d552afbeeeba23b37931f16fe463140d79
Andrew Fitzgerald <apfitzge@gmail.com>
2023-08-14 18:18:45 +0000

    basic cli and event file loop

--- banking-trace-tool/src/main.rs
+++ banking-trace-tool/src/main.rs
@@ -1,3 +1,25 @@
+use {clap::Parser, std::path::PathBuf};
+
+#[derive(Parser)]
+struct Args {
+    /// The path to the banking trace event files.
+    #[clap(short, long)]
+    path: PathBuf,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let Args { path } = Args::parse();
+
+    for index in 0.. {
+        let event_filename = if index == 0 {
+            "events".to_owned()
+        } else {
+            format!("events.{index}")
+        };
+        let event_file = path.join(event_filename);
+        if !event_file.exists() {
+            break;
+        }
+        println!("Reading events from {}:", event_file.display());
+    }
 }
