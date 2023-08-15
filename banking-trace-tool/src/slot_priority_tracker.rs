29ac92041e2300520f9e18580bcece73c7d84f69
Andrew Fitzgerald <apfitzge@gmail.com>
2023-08-15 17:13:19 +0000

    remove debug break

--- banking-trace-tool/src/slot_priority_tracker.rs
+++ banking-trace-tool/src/slot_priority_tracker.rs
@@ -71,7 +71,6 @@ impl SlotPriorityTracker {
     pub fn report(&self, kind: TrackingKind, verbosity: TrackingVerbosity) {
         for slot_data in &self.data {
             slot_data.report(kind, verbosity);
-            break;
         }
     }
 
