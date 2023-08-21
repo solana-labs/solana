a10437f89f95f203e26babc81bb800dabe3bf0e3
greg <greg.cusack@solana.com>
2023-08-21 12:15:30 -0700

    fix versioning in cargo.toml to run kube-rs. add simply pod watcher

--- net/k8s-cluster/src/main.rs
+++ net/k8s-cluster/src/main.rs
@@ -1,12 +1,22 @@
 use {
     kube::{
-        api::{ListParams, Meta},
-        Client, Config, CustomResource, CustomResourceExt, Error, KubeError,
-        Resource
+        api::{ListParams, Api},
+        Client,
     },
+    k8s_openapi::api::core::v1::Pod,
     log::*,
+    // anyhow::Result,
 };
 
-fn main() {
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
     info!("hey");
+    let client = Client::try_default().await?;
+    let pods: Api<Pod> = Api::default_namespaced(client);
+    let pod_list = pods.list(&ListParams::default()).await?;
+    let names = pod_list.into_iter()
+        .map(|pod| pod.metadata.name.unwrap_or("".into()))
+        .collect::<Vec<String>>();
+    println!("{names:?}");
+    Ok(())
 }
