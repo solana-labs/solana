7d3f8775a0ebe053a508d36fbeb1d0cf4d5579b2
Joe <jcaulfield135@gmail.com>
2023-08-22 12:37:45 -0600

    add verifiably built program

--- runtime/src/inline_feature_gate_program.rs
+++ runtime/src/inline_feature_gate_program.rs
@@ -1,5 +1,5 @@
 //! Contains replacement program IDs for the feature gate program
 
 pub(crate) mod noop_program {
-    solana_sdk::declare_id!("5Re2FUHmvSdem1KV2bU5GFvqZpP2MXj76yVhMKKbP45o");
+    solana_sdk::declare_id!("2rqZsQBbacRbuAuTSuJ7n49UQT9fzes8RLggFcmB9YuN");
 }
