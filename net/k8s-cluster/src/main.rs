f0bcb8e488b6a0a3baab3a65c509d5d1c08b0c11
greg <greg.cusack@solana.com>
2023-08-22 14:25:33 -0700

    clean up

--- net/k8s-cluster/src/main.rs
+++ net/k8s-cluster/src/main.rs
@@ -1,36 +1,23 @@
 use {
-    clap::{crate_description, crate_name, App, Arg, ArgMatches, value_t_or_exit},
-    kube::{
-        api::{ListParams, Api, PostParams, ObjectMeta},
-        Client,
-    },
+    clap::{crate_description, crate_name, value_t_or_exit, App, Arg, ArgMatches},
     k8s_openapi::{
         api::{
+            apps::v1::{Deployment, DeploymentSpec},
             core::v1::{
-                Pod,
-                Container,
-                PodSpec,
-                PodTemplateSpec,
-                ServiceSpec,
-                ServicePort,
-                Service,
-                EnvVar,
-                EnvVarSource,
-                ObjectFieldSelector,
-                PodStatus,
+                Container, EnvVar, EnvVarSource, ObjectFieldSelector, PodSpec, PodTemplateSpec,
+                Service, ServicePort, ServiceSpec,
             },
-            apps::v1::{
-                Deployment,
-                DeploymentSpec,
-            }
         },
         apimachinery::pkg::apis::meta::v1::LabelSelector,
     },
+    kube::{
+        api::{Api, ObjectMeta, PostParams},
+        Client,
+    },
     log::*,
     serde_json,
-    std::collections::BTreeMap
+    std::collections::BTreeMap,
 };
-
 
 fn parse_matches() -> ArgMatches<'static> {
     App::new(crate_name!())
@@ -56,22 +43,22 @@ fn parse_matches() -> ArgMatches<'static> {
                 .takes_value(true)
                 .default_value("1")
                 .help("Number of validator replicas to deploy"),
-        ).arg(
+        )
+        .arg(
             Arg::with_name("container_name")
                 .long("container")
                 .takes_value(true)
                 .required(true)
                 .help("Validator Container name"),
-        ).arg(
+        )
+        .arg(
             Arg::with_name("image_name")
                 .long("image")
                 .takes_value(true)
                 .required(true)
                 .help("Docker Image of Validator to deploy"),
         )
-
         .get_matches()
-
 }
 
 #[tokio::main]
@@ -82,59 +69,71 @@ async fn main() {
     solana_logger::setup();
     let matches = parse_matches();
     let namespace = matches.value_of("cluster_namespace").unwrap_or_default();
-    let app_name = matches.value_of("app_name").expect("Application name is required");
+    let app_name = matches
+        .value_of("app_name")
+        .expect("Application name is required");
     let replicas = value_t_or_exit!(matches, "number_of_replicas", i32);
-    let container_name = matches.value_of("container_name").expect("Container name is required");
-    let image_name = matches.value_of("image_name").expect("Image name is required");
+    let container_name = matches
+        .value_of("container_name")
+        .expect("Container name is required");
+    let image_name = matches
+        .value_of("image_name")
+        .expect("Image name is required");
 
     info!("namespace: {}", namespace);
 
-    let _ = deployment_runner(app_name, namespace, container_name, image_name, replicas).await;
-
-    // info!("dep: {:?}", dep_res.unwrap());
-    let _ = service_runner(app_name, namespace).await;
-
-    // info!("service: {:?}", serv_res.unwrap());
     let client = Client::try_default().await.unwrap();
-    // let deployment = get_deployment_info(client.clone(), app_name, namespace).await.unwrap();
-    // let deployment_json = serde_json::to_string_pretty(&deployment).unwrap();
-    // info!("{}", deployment_json);
 
+    let mut label_selector = BTreeMap::new(); // Create a JSON map for label selector
+    label_selector.insert("app.kubernetes.io/name".to_string(), app_name.to_string());
 
-    // let service = get_service_info(client.clone(), app_name, namespace).await.unwrap();
-    // let service_json = serde_json::to_string_pretty(&service).unwrap();
-    // info!("{}", service_json);
+    let _ = create_deployment(
+        client.clone(),
+        app_name,
+        namespace,
+        container_name,
+        image_name,
+        replicas,
+        &label_selector,
+    )
+    .await;
 
-    let res = check_service_matching_deployment(client, app_name, namespace).await;
+    info!("Deployment created successfully in the specified namespace!");
 
+    let _ = create_service(client.clone(), app_name, namespace, &label_selector).await;
+    info!("Service created successfully in the specified namespace!");
 
-
+    let _ = check_service_matching_deployment(client, app_name, namespace).await;
 }
 
 async fn check_service_matching_deployment(
     client: Client,
     app_name: &str,
-    namespace: &str, 
+    namespace: &str,
 ) -> Result<(), Box<dyn std::error::Error>> {
     // Get the Deployment
     let deployment_api: Api<Deployment> = Api::namespaced(client.clone(), namespace);
-    let deployment = deployment_api.get(format!("{}-deployment", app_name).as_str()).await?;
-    let deployment_json = serde_json::to_string_pretty(&deployment).unwrap();
-    info!("{}", deployment_json);
+    let deployment = deployment_api
+        .get(format!("{}-deployment", app_name).as_str())
+        .await?;
+    // let deployment_json = serde_json::to_string_pretty(&deployment).unwrap();
+    // info!("{}", deployment_json);
 
     // Get the Service
     let service_api: Api<Service> = Api::namespaced(client, namespace);
-    let service = service_api.get(format!("{}-service", app_name).as_str()).await?;
-    let service_json = serde_json::to_string_pretty(&service).unwrap();
-    info!("{}", service_json);
+    let service = service_api
+        .get(format!("{}-service", app_name).as_str())
+        .await?;
+    // let service_json = serde_json::to_string_pretty(&service).unwrap();
+    // info!("{}", service_json);
 
     let deployment_labels = deployment
         .spec
         .and_then(|spec| {
-            Some(spec.selector).and_then(|selector| { 
-                selector.match_labels.and_then(|val| {
-                    val.get("app.kubernetes.io/name").cloned()
-                })
+            Some(spec.selector).and_then(|selector| {
+                selector
+                    .match_labels
+                    .and_then(|val| val.get("app.kubernetes.io/name").cloned())
             })
         })
         .clone();
@@ -142,118 +141,24 @@ async fn check_service_matching_deployment(
     let service_labels = service
         .spec
         .and_then(|spec| {
-            spec.selector.and_then(|val| {
-                val.get("app.kubernetes.io/name").cloned()
-            })
+            spec.selector
+                .and_then(|val| val.get("app.kubernetes.io/name").cloned())
         })
         .clone();
 
-    // match(de)
-    info!("dep, serve labels: {:?}, {:?}", deployment_labels, service_labels);
+    info!(
+        "dep, serve labels: {:?}, {:?}",
+        deployment_labels, service_labels
+    );
 
-    let are_equal = match(deployment_labels, service_labels) {
-        (Some(dep_label), Some(serv_label)) => {
-            dep_label == serv_label
-        },
+    let are_equal = match (deployment_labels, service_labels) {
+        (Some(dep_label), Some(serv_label)) => dep_label == serv_label,
         _ => false,
     };
 
     if !are_equal {
         error!("Deployment and Service labels are not the same!");
-    } 
-
-    Ok(())
-}
-
-// #[tokio::main]
-async fn deployment_runner(
-    app_name: &str,
-    namespace: &str,
-    container_name: &str,
-    image_name: &str,
-    replicas: i32,
-) -> Result<Deployment, kube::Error> {
-    info!("suhhhhh");
-    let client = Client::try_default().await?;
-
-    // Create the Deployment
-    let deployment = create_deployment(
-        client.clone(), 
-        app_name,
-        namespace,
-        container_name,
-        image_name,
-        replicas
-    ).await;
-
-    info!("Deployment created successfully in the specified namespace!");
-    deployment
-}
-
-
-async fn service_runner(
-    app_name: &str,
-    namespace: &str
-) -> Result<Service, kube::Error> {
-    info!("suhhhhh serv");
-    let client = Client::try_default().await?;
-
-    // Create the Deployment
-    let service = create_service(client, app_name, namespace).await;
-    info!("Service created successfully in the specified namespace!");
-
-    service
-}
-
-async fn get_deployment_info(
-    client: Client,
-    app_name: &str,
-    namespace: &str,
-) -> Result<Deployment, kube::Error> {
-    let api: Api<Deployment> = Api::namespaced(client, namespace);
-
-    api.get(format!("{}-deployment", app_name).as_str()).await
-}
-
-async fn get_service_info(
-    client: Client,
-    app_name: &str,
-    namespace: &str,
-) -> Result<Service, kube::Error> {
-    let api: Api<Service> = Api::namespaced(client, namespace);
-    api.get(format!("{}-service", app_name).as_str()).await
-}
-
-#[tokio::main]
-async fn run_controller(
-    namespace: &str,
-) -> Result<(), Box<dyn std::error::Error>> {
-    let client = Client::try_default().await?;
-    // let pods: Api<Pod> = Api::default_namespaced(client);
-    let pods: Api<Pod> = Api::namespaced(client, namespace);
-    let pod_list = pods.list(&ListParams::default()).await?;
-    let names = pod_list.into_iter()
-        .map(|pod| pod.metadata.name.unwrap_or("".into()))
-        .collect::<Vec<String>>();
-    info!("Pods in ns {}, {names:?}", namespace);
-    Ok(())
-}
-
-#[tokio::main]
-async fn run_deployer(
-    namespace: &str,
-    deployment: &Deployment,
-) -> Result<(), Box<dyn std::error::Error>> {
-
-    let client = Client::try_default().await?;
-
-
-    let api: Api<Deployment> = Api::namespaced(client.clone(), namespace);
-    let post_params = PostParams::default();
-    // Apply the Deployment
-    api.create(&post_params, &deployment).await?;
-
-    println!("Deployment created successfully in the specified namespace!");
+    }
 
     Ok(())
 }
@@ -265,14 +170,11 @@ async fn create_deployment(
     container_name: &str,
     image_name: &str,
     replicas: i32,
+    label_selector: &BTreeMap<String, String>,
 ) -> Result<Deployment, kube::Error> {
-// ) -> Result<(), Box<dyn std::error::Error>> {
-    let mut label_selector = BTreeMap::new();  // Create a JSON map for label selector
-    label_selector.insert("app.kubernetes.io/name".to_string(), app_name.to_string());
-    
     let env_var = EnvVar {
         name: "MY_POD_IP".to_string(),
-        value_from: Some(EnvVarSource { 
+        value_from: Some(EnvVarSource {
             field_ref: Some(ObjectFieldSelector {
                 field_path: "status.podIP".to_string(),
                 ..Default::default()
@@ -304,7 +206,7 @@ async fn create_deployment(
     let deployment_spec = DeploymentSpec {
         replicas: Some(replicas),
         selector: LabelSelector {
-            match_labels: Some(label_selector),
+            match_labels: Some(label_selector.clone()),
             ..Default::default()
         },
         template: pod_spec,
@@ -333,9 +235,8 @@ async fn create_service(
     client: Client,
     app_name: &str,
     namespace: &str,
+    label_selector: &BTreeMap<String, String>,
 ) -> Result<Service, kube::Error> {
-    let mut label_selector = BTreeMap::new();  // Create a JSON map for label selector
-    label_selector.insert("app.kubernetes.io/name".to_string(), app_name.to_string());
     let service = Service {
         metadata: ObjectMeta {
             name: Some(format!("{}-service", app_name).to_string()),
@@ -343,24 +244,26 @@ async fn create_service(
             ..Default::default()
         },
         spec: Some(ServiceSpec {
-            selector: Some(label_selector),
+            selector: Some(label_selector.clone()),
             cluster_ip: Some("None".into()),
             // cluster_ips: None,
-            ports: Some(vec![ServicePort {
-                port: 8899, // RPC Port
-                name: Some("rpc-port".to_string()),
-                ..Default::default()
-            },
-            ServicePort {
-                port: 8001, //Gossip Port
-                name: Some("gossip-port".to_string()),
-                ..Default::default()
-            },
-            ServicePort {
-                port: 9900, //Faucet Port
-                name: Some("faucet-port".to_string()),
-                ..Default::default()
-            }]),
+            ports: Some(vec![
+                ServicePort {
+                    port: 8899, // RPC Port
+                    name: Some("rpc-port".to_string()),
+                    ..Default::default()
+                },
+                ServicePort {
+                    port: 8001, //Gossip Port
+                    name: Some("gossip-port".to_string()),
+                    ..Default::default()
+                },
+                ServicePort {
+                    port: 9900, //Faucet Port
+                    name: Some("faucet-port".to_string()),
+                    ..Default::default()
+                },
+            ]),
             ..Default::default()
         }),
         ..Default::default()
@@ -371,5 +274,4 @@ async fn create_service(
 
     // Create the Service object in the cluster
     service_api.create(&post_params, &service).await
-
 }
