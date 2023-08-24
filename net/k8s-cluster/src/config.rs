1bee02aa532012fbc7eb7a1f1e3204e85fd14d90
greg <greg.cusack@solana.com>
2023-08-23 19:02:27 -0700

    prepareDeploy tar finished

--- net/k8s-cluster/src/config.rs
+++ net/k8s-cluster/src/config.rs
@@ -1,10 +1,5 @@
-
 #[derive(Clone, Debug)]
 pub struct SetupConfig<'a> {
     pub namespace: &'a str,
     pub num_validators: i32,
 }
-
-
-
-
--- net/k8s-cluster/src/lib.rs
+++ net/k8s-cluster/src/lib.rs
@@ -1,10 +1,6 @@
 use {
-    std::{
-        env,
-        fs,
-        path::PathBuf,
-    },
     lazy_static::lazy_static,
+    std::{env, fs, path::PathBuf},
 };
 
 lazy_static! {
@@ -18,11 +14,13 @@ pub fn initialize_globals() {
 pub mod config;
 pub mod setup;
 
-
 pub fn get_solana_root() -> PathBuf {
     if let Ok(current_dir) = env::current_dir() {
         if let Ok(canonical_dir) = fs::canonicalize(&current_dir) {
-            return canonical_dir.parent().unwrap_or(&canonical_dir).to_path_buf();
+            return canonical_dir
+                .parent()
+                .unwrap_or(&canonical_dir)
+                .to_path_buf();
         }
     }
     panic!("Failed to get Solana root directory");
