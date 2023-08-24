b55de1f0780e4bd4c71db91166579f1eee25b983
Wen <crocoxu@gmail.com>
2023-08-23 19:41:19 -0700

    Print heaviest_fork detailed aggregate.

--- wen-restart/src/heaviest_fork_aggregate.rs
+++ wen-restart/src/heaviest_fork_aggregate.rs
@@ -1,24 +1,29 @@
 use {
     crate::epoch_stakes_map::EpochStakesMap,
+    log::*,
     solana_gossip::crds_value::Percent,
     solana_sdk::{clock::Slot, hash::Hash, pubkey::Pubkey},
-    std::collections::HashSet,
+    std::collections::{HashMap, HashSet},
 };
 
 pub struct HeaviestForkAggregate {
     my_slot: Slot,
     my_hash: Hash,
     active_peers: HashSet<Pubkey>,
+    aggregate: HashMap<(Slot, Hash), HashSet<Pubkey>>,
 }
 
 impl HeaviestForkAggregate {
     pub(crate) fn new(my_pubkey: Pubkey, my_slot: Slot, my_hash: Hash) -> Self {
-        let mut peers = HashSet::new();
-        peers.insert(my_pubkey);
+        let mut active_peers = HashSet::new();
+        active_peers.insert(my_pubkey);
+        let mut aggregate = HashMap::new();
+        aggregate.insert((my_slot, my_hash), active_peers.clone());
         Self {
             my_slot,
             my_hash,
-            active_peers: peers,
+            active_peers,
+            aggregate,
         }
     }
 
@@ -32,14 +37,13 @@ impl HeaviestForkAggregate {
         heaviest_fork_list
             .into_iter()
             .for_each(|(slot, hash, percent)| {
-                if slot == self.my_slot && hash == self.my_hash {
-                    self.active_peers.insert(percent.from);
-                }
+                let pubkey = percent.from;
+                self.aggregate.entry((slot, hash)).or_insert(HashSet::new()).insert(pubkey);
+                self.active_peers.insert(pubkey);
             });
-        let total_active_stake = self
-            .active_peers
-            .iter()
-            .map(|pubkey| {
+        let mut stake_on_my_vote = 0;
+        for ((slot, hash), peers) in self.aggregate.iter() {
+            let total_stake = peers.iter().map(|pubkey| {
                 node_stakes
                     .node_id_to_vote_accounts()
                     .get(pubkey)
@@ -48,6 +52,11 @@ impl HeaviestForkAggregate {
             })
             .reduce(|a, b| a + b)
             .unwrap();
-        return (total_active_stake as f64 / total_epoch_stake as f64, self.active_peers.len());
+            if slot == &self.my_slot && hash == &self.my_hash {
+                stake_on_my_vote = total_stake;
+            }
+            info!("HeaviestFork: ({}, {}) : {} {:?}", slot, hash, total_stake, peers);
+        }
+        return (stake_on_my_vote as f64 / total_epoch_stake as f64, self.active_peers.len());
     }
 }
