f1ec16d7804dad44fe60acd0bb90e41b0b3b8533
greg <greg.cusack@solana.com>
2023-08-29 10:02:16 -0700

    wip...genesis

--- net/k8s-cluster/src/setup.rs
+++ net/k8s-cluster/src/setup.rs
@@ -1,16 +1,24 @@
+use crate::SOLANA_ROOT;
+
 use {
     clap::ArgMatches,
     crate::initialize_globals,
     bip39::{Mnemonic, MnemonicType, Seed, Language},
+    log::*,
     solana_sdk::signature::{
         Keypair,
         keypair_from_seed,
         Signer,
+        write_keypair,
+        write_keypair_file,
     },
     solana_clap_v3_utils::{
         keygen,
         keypair,
+        input_parsers::STDOUT_OUTFILE_TOKEN,
     },
+    std::error::Error,
+
 };
 
 #[derive(Clone, Debug)]
@@ -19,6 +27,22 @@ pub struct SetupConfig<'a> {
     pub num_validators: i32,
     pub prebuild_genesis: bool,
 }
+
+fn output_keypair(
+    keypair: &Keypair,
+    outfile: &str,
+    source: &str,
+) -> Result<(), Box<dyn Error>> {
+    if outfile == STDOUT_OUTFILE_TOKEN {
+        let mut stdout = std::io::stdout();
+        write_keypair(keypair, &mut stdout)?;
+    } else {
+        write_keypair_file(keypair, outfile)?;
+        println!("Wrote {source} keypair to {outfile}");
+    }
+    Ok(())
+}
+
 
 
 pub struct Genesis<'a> {
@@ -40,7 +64,10 @@ impl<'a> Genesis<'a> {
         &self,
     ) {
         let total_validators = self.config.num_validators + 1; // add 1 for bootstrap
-        for _ in 0..total_validators {
+        for i in 0..total_validators {
+            info!("generating keypair index: {}", i);
+            let filename = format!("keypair-validator-{}", i);
+            let outfile = SOLANA_ROOT.join(filename);
             let (passphrase, passphrase_message) = keygen::mnemonic::no_passphrase_and_message();
 
             let word_count: usize = 12; //default
@@ -48,6 +75,11 @@ impl<'a> Genesis<'a> {
             let mnemonic = Mnemonic::new(mnemonic_type, Language::English);
             let seed = Seed::new(&mnemonic, &passphrase);
             let keypair = keypair_from_seed(seed.as_bytes()).unwrap();
+
+            if let Some(outfile) = outfile.to_str() {
+                output_keypair(&keypair, outfile, "new")
+                    .map_err(|err| format!("Unable to write {outfile}: {err}"));
+            }
 
             let phrase: &str = mnemonic.phrase();
             let divider = String::from_utf8(vec![b'='; phrase.len()]).unwrap();
