35a6c664d328b37db4a059b61117109e686e65c0
Andrew Fitzgerald <apfitzge@gmail.com>
2023-08-31 17:14:41 +0000

    transaction state

--- core/src/banking_stage/transaction_scheduler/transaction_packet_container.rs
+++ core/src/banking_stage/transaction_scheduler/transaction_packet_container.rs
@@ -1,40 +1,144 @@
 use {
     super::transaction_priority_id::TransactionPriorityId,
-    crate::banking_stage::{
-        immutable_deserialized_packet::ImmutableDeserializedPacket,
-        scheduler_messages::TransactionId, unprocessed_packet_batches::DeserializedPacket,
-    },
+    crate::banking_stage::scheduler_messages::TransactionId,
     min_max_heap::MinMaxHeap,
+    solana_runtime::transaction_priority_details::TransactionPriorityDetails,
     solana_sdk::{slot_history::Slot, transaction::SanitizedTransaction},
     std::collections::HashMap,
 };
 
-/// Simple wrapper to tie a `SanitizedTransaction` with the maximum slot
-/// it can be processed at without re-sanitization.
+#[allow(clippy::large_enum_variant)]
+pub(crate) enum TransactionState {
+    Unprocessed {
+        transaction_ttl: SanitizedTransactionTTL,
+        transaction_priority_details: TransactionPriorityDetails,
+        forwarded: bool,
+    },
+    Pending {
+        transaction_priority_details: TransactionPriorityDetails,
+        forwarded: bool,
+    },
+}
+
+impl TransactionState {
+    fn new(
+        transaction_ttl: SanitizedTransactionTTL,
+        transaction_priority_details: TransactionPriorityDetails,
+        forwarded: bool,
+    ) -> Self {
+        Self::Unprocessed {
+            transaction_ttl,
+            transaction_priority_details,
+            forwarded,
+        }
+    }
+
+    pub(crate) fn transaction_priority_details(&self) -> &TransactionPriorityDetails {
+        match self {
+            Self::Unprocessed {
+                transaction_priority_details,
+                ..
+            } => transaction_priority_details,
+            Self::Pending {
+                transaction_priority_details,
+                ..
+            } => transaction_priority_details,
+        }
+    }
+
+    pub(crate) fn priority(&self) -> u64 {
+        self.transaction_priority_details().priority
+    }
+
+    pub(crate) fn forwarded(&self) -> bool {
+        match self {
+            Self::Unprocessed { forwarded, .. } => *forwarded,
+            Self::Pending { forwarded, .. } => *forwarded,
+        }
+    }
+
+    pub(crate) fn set_forwarded(&mut self) {
+        match self {
+            Self::Unprocessed { forwarded, .. } => *forwarded = true,
+            Self::Pending { forwarded, .. } => *forwarded = true,
+        }
+    }
+
+    fn transition_to_pending(&mut self) -> SanitizedTransactionTTL {
+        match self.take() {
+            TransactionState::Unprocessed {
+                transaction_ttl,
+                transaction_priority_details,
+                forwarded,
+            } => {
+                *self = TransactionState::Pending {
+                    transaction_priority_details,
+                    forwarded,
+                };
+                transaction_ttl
+            }
+            TransactionState::Pending { .. } => {
+                panic!("transaction already pending");
+            }
+        }
+    }
+
+    fn transition_to_unprocessed(&mut self, transaction_ttl: SanitizedTransactionTTL) {
+        match self.take() {
+            TransactionState::Unprocessed { .. } => panic!("already unprocessed"),
+            TransactionState::Pending {
+                transaction_priority_details,
+                forwarded,
+            } => {
+                *self = Self::Unprocessed {
+                    transaction_ttl,
+                    transaction_priority_details,
+                    forwarded,
+                }
+            }
+        }
+    }
+
+    pub(crate) fn transaction_ttl(&self) -> &SanitizedTransactionTTL {
+        match self {
+            Self::Unprocessed {
+                transaction_ttl, ..
+            } => transaction_ttl,
+            Self::Pending { .. } => panic!("transaction is pending"),
+        }
+    }
+
+    /// Internal helper to transitioning between states.
+    /// Replaces `self` with a dummy state that will immediately be overwritten in transition.
+    fn take(&mut self) -> Self {
+        core::mem::replace(
+            self,
+            Self::Pending {
+                transaction_priority_details: TransactionPriorityDetails {
+                    priority: 0,
+                    compute_unit_limit: 0,
+                },
+                forwarded: false,
+            },
+        )
+    }
+}
+
 pub(crate) struct SanitizedTransactionTTL {
     pub(crate) transaction: SanitizedTransaction,
     pub(crate) max_age_slot: Slot,
 }
 
 pub(crate) struct TransactionPacketContainer {
-    /// Unscheduled unique identifiers ordered by priority
     priority_queue: MinMaxHeap<TransactionPriorityId>,
-    /// Map from `TransactionId` to `SanitizedTransactionTTL`.
-    /// Contains transactions that have not yet been scheduled for consuming.
-    id_to_transaction_ttl: HashMap<TransactionId, SanitizedTransactionTTL>,
-    /// Map from `TransactionId` to `DeserializedPacket`.
-    /// Contains packets for the entire lifetime of packet/transaction.
-    /// These will only be removed once a transaction has finished processing,
-    /// or if the packets are forwarded without holding.
-    id_to_packet: HashMap<TransactionId, DeserializedPacket>,
+    id_to_transaction_state: HashMap<TransactionId, TransactionState>,
 }
 
 impl TransactionPacketContainer {
     pub(crate) fn with_capacity(capacity: usize) -> Self {
         Self {
             priority_queue: MinMaxHeap::with_capacity(capacity),
-            id_to_transaction_ttl: HashMap::with_capacity(capacity),
-            id_to_packet: HashMap::with_capacity(capacity),
+            id_to_transaction_state: HashMap::with_capacity(capacity),
         }
     }
 
@@ -56,47 +160,64 @@ impl TransactionPacketContainer {
         (0..n).map_while(|_| self.priority_queue.pop_max())
     }
 
-    /// Get packet by id.
-    pub(crate) fn get_packet(&self, id: &TransactionId) -> Option<&DeserializedPacket> {
-        self.id_to_packet.get(id)
+    /// Serialize entire priority queue. `hold` indicates whether the priority queue should
+    /// be drained or not.
+    pub(crate) fn priority_ordered_ids(&mut self, hold: bool) -> Vec<TransactionPriorityId> {
+        let priority_queue = if hold {
+            self.priority_queue.clone()
+        } else {
+            let capacity = self.priority_queue.capacity();
+            core::mem::replace(
+                &mut self.priority_queue,
+                MinMaxHeap::with_capacity(capacity),
+            )
+        };
+
+        priority_queue.into_vec_desc()
     }
 
-    /// Get mutable packet by id.
-    pub(crate) fn get_mut_packet(&mut self, id: &TransactionId) -> Option<&mut DeserializedPacket> {
-        self.id_to_packet.get_mut(id)
+    /// Get transaction state by id.
+    pub(crate) fn get_mut_transaction_state(
+        &mut self,
+        id: &TransactionId,
+    ) -> Option<&mut TransactionState> {
+        self.id_to_transaction_state.get_mut(id)
     }
 
-    /// Get transaction by id.
+    /// Get reference to `SanitizedTransactionTTL` by id.
     /// Panics if the transaction does not exist.
-    pub(crate) fn get_transaction(&self, id: &TransactionId) -> &SanitizedTransactionTTL {
-        self.id_to_transaction_ttl
+    pub(crate) fn get_transaction_ttl(
+        &self,
+        id: &TransactionId,
+    ) -> Option<&SanitizedTransactionTTL> {
+        self.id_to_transaction_state
             .get(id)
-            .expect("transaction must exist")
+            .map(|state| state.transaction_ttl())
     }
 
     /// Take transaction by id.
     /// Panics if the transaction does not exist.
     pub(crate) fn take_transaction(&mut self, id: &TransactionId) -> SanitizedTransactionTTL {
-        self.id_to_transaction_ttl
-            .remove(id)
+        self.id_to_transaction_state
+            .get_mut(id)
             .expect("transaction must exist")
+            .transition_to_pending()
     }
 
     /// Insert a new transaction into the container's queues and maps.
     pub(crate) fn insert_new_transaction(
         &mut self,
         transaction_id: TransactionId,
-        packet: ImmutableDeserializedPacket,
         transaction_ttl: SanitizedTransactionTTL,
+        transaction_priority_details: TransactionPriorityDetails,
     ) {
-        let priority_id = TransactionPriorityId::new(packet.priority(), transaction_id);
+        let priority_id =
+            TransactionPriorityId::new(transaction_priority_details.priority, transaction_id);
         if self.push_id_into_queue(priority_id) {
-            self.id_to_packet.insert(
+            self.id_to_transaction_state.insert(
                 transaction_id,
-                DeserializedPacket::from_immutable_section(packet),
+                TransactionState::new(transaction_ttl, transaction_priority_details, false),
             );
-            self.id_to_transaction_ttl
-                .insert(transaction_id, transaction_ttl);
         }
     }
 
@@ -104,25 +225,14 @@ impl TransactionPacketContainer {
     pub(crate) fn retry_transaction(
         &mut self,
         transaction_id: TransactionId,
-        transaction: SanitizedTransaction,
-        max_age_slot: Slot,
+        transaction_ttl: SanitizedTransactionTTL,
     ) {
-        let priority = self
-            .id_to_packet
-            .get(&transaction_id)
-            .unwrap()
-            .immutable_section()
-            .priority();
-        let priority_id = TransactionPriorityId::new(priority, transaction_id);
-        if self.push_id_into_queue(priority_id) {
-            self.id_to_transaction_ttl.insert(
-                transaction_id,
-                SanitizedTransactionTTL {
-                    transaction,
-                    max_age_slot,
-                },
-            );
-        }
+        let transaction_state = self
+            .get_mut_transaction_state(&transaction_id)
+            .expect("transaction must exist");
+        let priority_id = TransactionPriorityId::new(transaction_state.priority(), transaction_id);
+        transaction_state.transition_to_unprocessed(transaction_ttl);
+        self.push_id_into_queue(priority_id);
     }
 
     /// Pushes a transaction id into the priority queue, without inserting the packet or transaction.
@@ -144,8 +254,7 @@ impl TransactionPacketContainer {
 
     /// Remove packet and transaction by id.
     pub(crate) fn remove_by_id(&mut self, id: &TransactionId) {
-        self.id_to_packet.remove(id);
-        self.id_to_transaction_ttl.remove(id);
+        self.id_to_transaction_state.remove(id);
     }
 }
 
@@ -153,16 +262,15 @@ impl TransactionPacketContainer {
 mod tests {
     use {
         super::*,
-        solana_perf::packet::Packet,
         solana_sdk::{
             compute_budget::ComputeBudgetInstruction, hash::Hash, message::Message,
             signature::Keypair, signer::Signer, system_instruction, transaction::Transaction,
         },
     };
 
-    fn test_packet_and_transaction(
+    fn test_transaction(
         priority: u64,
-    ) -> (ImmutableDeserializedPacket, SanitizedTransactionTTL) {
+    ) -> (SanitizedTransactionTTL, TransactionPriorityDetails) {
         let from_keypair = Keypair::new();
         let ixs = vec![
             system_instruction::transfer(
@@ -175,21 +283,22 @@ mod tests {
         let message = Message::new(&ixs, Some(&from_keypair.pubkey()));
         let tx = Transaction::new(&[&from_keypair], message, Hash::default());
 
-        let packet = Packet::from_data(None, tx.clone()).unwrap();
-        let packet = ImmutableDeserializedPacket::new(packet).unwrap();
-
         let transaction_ttl = SanitizedTransactionTTL {
             transaction: SanitizedTransaction::from_transaction_for_tests(tx),
             max_age_slot: Slot::MAX,
         };
-        (packet, transaction_ttl)
+        (transaction_ttl, TransactionPriorityDetails { priority, compute_unit_limit: 0 })
     }
 
     fn push_to_container(container: &mut TransactionPacketContainer, num: usize) {
         for id in 0..num as u64 {
             let priority = id;
-            let (packet, transaction_ttl) = test_packet_and_transaction(priority);
-            container.insert_new_transaction(TransactionId::new(id), packet, transaction_ttl);
+            let (transaction_ttl, transaction_priority_details) = test_transaction(priority);
+            container.insert_new_transaction(
+                TransactionId::new(id),
+                transaction_ttl,
+                transaction_priority_details,
+            );
         }
     }
 
@@ -208,13 +317,12 @@ mod tests {
         push_to_container(&mut container, 5);
 
         assert_eq!(container.priority_queue.len(), 1);
-        assert_eq!(container.id_to_packet.len(), 1);
-        assert_eq!(container.id_to_transaction_ttl.len(), 1);
+        assert_eq!(container.id_to_transaction_state.len(), 1);
         assert_eq!(
             container
-                .id_to_packet
+                .id_to_transaction_state
                 .iter()
-                .map(|p| p.1.immutable_section().priority())
+                .map(|ts| ts.1.priority())
                 .next()
                 .unwrap(),
             4
@@ -245,12 +353,10 @@ mod tests {
 
         container.remove_by_id(&TransactionId::new(3));
         assert_eq!(container.priority_queue.len(), 5); // remove_by_id does not remove from priority queue
-        assert_eq!(container.id_to_packet.len(), 4);
-        assert_eq!(container.id_to_transaction_ttl.len(), 4);
+        assert_eq!(container.id_to_transaction_state.len(), 4);
 
         container.remove_by_id(&TransactionId::new(7));
-        assert_eq!(container.id_to_packet.len(), 4);
-        assert_eq!(container.id_to_transaction_ttl.len(), 4);
+        assert_eq!(container.id_to_transaction_state.len(), 4);
     }
 
     #[test]
@@ -258,8 +364,7 @@ mod tests {
         let mut container = TransactionPacketContainer::with_capacity(1);
         assert!(container.push_id_into_queue(TransactionPriorityId::new(1, TransactionId::new(0))));
         assert_eq!(container.priority_queue.len(), 1);
-        assert_eq!(container.id_to_packet.len(), 0);
-        assert_eq!(container.id_to_transaction_ttl.len(), 0);
+        assert_eq!(container.id_to_transaction_state.len(), 0);
 
         assert!(container.push_id_into_queue(TransactionPriorityId::new(1, TransactionId::new(1))));
         assert_eq!(container.priority_queue.len(), 1);
@@ -272,23 +377,18 @@ mod tests {
     fn test_get_packet_entry_missing() {
         let mut container = TransactionPacketContainer::with_capacity(5);
         push_to_container(&mut container, 5);
-        assert!(container.get_packet(&TransactionId::new(7)).is_none());
+        assert!(container
+            .get_mut_transaction_state(&TransactionId::new(7))
+            .is_none());
     }
 
     #[test]
     fn test_get_packet_entry() {
         let mut container = TransactionPacketContainer::with_capacity(5);
         push_to_container(&mut container, 5);
-        assert!(container.get_packet(&TransactionId::new(3)).is_some());
-    }
-
-    #[test]
-    #[should_panic(expected = "transaction must exist")]
-    fn test_get_transaction_panic() {
-        let mut container = TransactionPacketContainer::with_capacity(5);
-        push_to_container(&mut container, 5);
-
-        let _ = container.get_transaction(&TransactionId::new(7));
+        assert!(container
+            .get_mut_transaction_state(&TransactionId::new(3))
+            .is_some());
     }
 
     #[test]
@@ -296,29 +396,10 @@ mod tests {
         let mut container = TransactionPacketContainer::with_capacity(5);
         push_to_container(&mut container, 5);
 
-        let transaction_id = TransactionId::new(3);
-        let _ = container.get_transaction(&transaction_id);
-        let _ = container.get_transaction(&transaction_id);
-    }
-
-    #[test]
-    #[should_panic(expected = "transaction must exist")]
-    fn test_take_transaction_panic() {
-        let mut container = TransactionPacketContainer::with_capacity(5);
-        push_to_container(&mut container, 5);
-
-        let _ = container.take_transaction(&TransactionId::new(7));
-    }
-
-    #[test]
-    fn test_take_transaction() {
-        let mut container = TransactionPacketContainer::with_capacity(5);
-        push_to_container(&mut container, 5);
-
-        let transaction_id = TransactionId::new(3);
-        let _ = container.take_transaction(&transaction_id);
-        assert!(!container
-            .id_to_transaction_ttl
-            .contains_key(&transaction_id));
+        let existing_id = TransactionId::new(3);
+        let non_existing_id = TransactionId::new(7);
+        assert!(container.get_mut_transaction_state(&existing_id).is_some());
+        assert!(container.get_mut_transaction_state(&existing_id).is_some());
+        assert!(container.get_mut_transaction_state(&non_existing_id).is_none());
     }
 }
