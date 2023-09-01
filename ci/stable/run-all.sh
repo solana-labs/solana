104bfe433c4c7be3a13675889a198a531bb20893
yihau <yihau.chen@icloud.com>
2023-09-01 16:39:35 +0800

    ci: remove unstable options for stable tests

--- ci/stable/run-all.sh
+++ ci/stable/run-all.sh
@@ -12,8 +12,4 @@ source "$here"/../common/limit-threads.sh
 #shellcheck source=ci/stable/common.sh
 source "$here"/common.sh
 
-if need_to_generate_test_result; then
-  _ cargo test --jobs "$JOBS" --workspace --tests --verbose -- -Z unstable-options --format json --report-time | tee results.json
-else
-  _ ci/intercept.sh cargo test --jobs "$JOBS" --workspace --tests --verbose -- --nocapture
-fi
+_ ci/intercept.sh cargo test --jobs "$JOBS" --workspace --tests --verbose -- --nocapture
