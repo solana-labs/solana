bb6c42de3f17828f418e60f2e29bfcad36a12b4f
brooks <brooks@solana.com>
2023-09-05 17:26:17 -0400

    pr: move datapoint

--- runtime/src/accounts_background_service.rs
+++ runtime/src/accounts_background_service.rs
@@ -511,8 +511,18 @@ impl PrunedBanksRequestHandler {
         // Group the banks into slices with the same slot
         let grouped_banks_to_purge: Vec<_> =
             GroupBy::new(banks_to_purge.as_slice(), |a, b| a.0 == b.0).collect();
+
+        // Log whenever we need to handle banks with the same slot.  Purposely do this *before* we
+        // call `purge_slot()` to ensure we get the datapoint (in case there's an assert/panic).
         let num_banks_with_same_slot =
             num_banks_to_purge.saturating_sub(grouped_banks_to_purge.len());
+        if num_banks_with_same_slot > 0 {
+            datapoint_info!(
+                "pruned_banks_request_handler",
+                ("num_pruned_banks", num_banks_to_purge, i64),
+                ("num_banks_with_same_slot", num_banks_with_same_slot, i64),
+            );
+        }
 
         // Purge all the slots in parallel
         // Banks for the same slot are purged sequentially
@@ -524,14 +534,6 @@ impl PrunedBanksRequestHandler {
                 })
             });
         });
-
-        if num_banks_with_same_slot > 0 {
-            datapoint_info!(
-                "pruned_banks_request_handler",
-                ("num_pruned_banks", num_banks_to_purge, i64),
-                ("num_banks_with_same_slot", num_banks_with_same_slot, i64),
-            );
-        }
 
         num_banks_to_purge
     }
