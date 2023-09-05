eb458402f8ce1e799949aebd78403c692635e192
brooks <brooks@solana.com>
2023-09-05 17:15:48 -0400

    Adds metrics when purging banks with the same slot

--- runtime/src/accounts_background_service.rs
+++ runtime/src/accounts_background_service.rs
@@ -511,6 +511,8 @@ impl PrunedBanksRequestHandler {
         // Group the banks into slices with the same slot
         let grouped_banks_to_purge: Vec<_> =
             GroupBy::new(banks_to_purge.as_slice(), |a, b| a.0 == b.0).collect();
+        let num_banks_with_same_slot =
+            num_banks_to_purge.saturating_sub(grouped_banks_to_purge.len());
 
         // Purge all the slots in parallel
         // Banks for the same slot are purged sequentially
@@ -522,6 +524,14 @@ impl PrunedBanksRequestHandler {
                 })
             });
         });
+
+        if num_banks_with_same_slot > 0 {
+            datapoint_info!(
+                "pruned_banks_request_handler",
+                ("num_pruned_banks", num_banks_to_purge, i64),
+                ("num_banks_with_same_slot", num_banks_with_same_slot, i64),
+            );
+        }
 
         num_banks_to_purge
     }
