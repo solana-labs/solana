0b0199b611132ba6c7b2081c1ef34f2270d3f04a
Alexander Meißner <AlexanderMeissner@gmx.net>
2023-09-06 16:23:24 +0200

    Moves AddressLookupTableAccount into address_lookup_table.

--- sdk/program/src/address_lookup_table/mod.rs
+++ sdk/program/src/address_lookup_table/mod.rs
@@ -9,3 +9,12 @@ pub mod state;
 pub mod program {
     crate::declare_id!("AddressLookupTab1e1111111111111111111111111");
 }
+
+/// The definition of address lookup table accounts.
+///
+/// As used by the `crate::message::v0` message format.
+#[derive(Debug, PartialEq, Eq, Clone)]
+pub struct AddressLookupTableAccount {
+    pub key: crate::pubkey::Pubkey,
+    pub addresses: Vec<crate::pubkey::Pubkey>,
+}
