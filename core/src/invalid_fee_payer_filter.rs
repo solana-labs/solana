72213804685988b25fd213055f42bb2d5847d185
Andrew Fitzgerald <apfitzge@gmail.com>
2023-09-07 17:40:53 +0000

    move interval to filter, not stats

--- core/src/invalid_fee_payer_filter.rs
+++ core/src/invalid_fee_payer_filter.rs
@@ -13,6 +13,7 @@ pub struct InvalidFeePayerFilter {
     capacity: usize,
     recent_invalid_fee_payers: DashSet<Pubkey>,
     stats: InvalidFeePayerFilterStats,
+    interval: AtomicInterval,
 }
 
 impl Default for InvalidFeePayerFilter {
@@ -21,15 +22,19 @@ impl Default for InvalidFeePayerFilter {
             capacity: 16 * 1024,
             recent_invalid_fee_payers: DashSet::new(),
             stats: InvalidFeePayerFilterStats::default(),
+            interval: AtomicInterval::default(),
         }
     }
 }
 
 impl InvalidFeePayerFilter {
     /// Reset the filter if enough time has passed since last reset.
+    /// If reset, also report stats.
     pub fn reset_on_interval(&self) {
-        if self.stats.try_report() {
+        const RESET_INTERVAL_MS: u64 = 2000;
+        if self.interval.should_update(RESET_INTERVAL_MS) {
             self.reset();
+            self.stats.report();
         }
     }
 
@@ -66,28 +71,21 @@ impl InvalidFeePayerFilter {
 
 #[derive(Default)]
 struct InvalidFeePayerFilterStats {
-    interval: AtomicInterval,
     num_added: AtomicUsize,
     num_rejects: AtomicUsize,
 }
 
 impl InvalidFeePayerFilterStats {
-    fn try_report(&self) -> bool {
-        const REPORT_INTERVAL_MS: u64 = 2000;
-
-        if self.interval.should_update(REPORT_INTERVAL_MS) {
-            let num_added = self.num_added.swap(0, Ordering::Relaxed);
-            if num_added > 0 {
-                let num_rejects = self.num_rejects.swap(0, Ordering::Relaxed);
-                datapoint_info!(
-                    "invalid_fee_payer_filter_stats",
-                    ("added", num_added, i64),
-                    ("rejects", num_rejects, i64),
-                );
-            }
-            true
-        } else {
-            false
+    /// Report metrics since last report, if any invalid fee-payers have been detected.
+    fn report(&self) {
+        let num_added = self.num_added.swap(0, Ordering::Relaxed);
+        if num_added > 0 {
+            let num_rejects = self.num_rejects.swap(0, Ordering::Relaxed);
+            datapoint_info!(
+                "invalid_fee_payer_filter_stats",
+                ("added", num_added, i64),
+                ("rejects", num_rejects, i64),
+            );
         }
     }
 }
