11ed3c70b27054b694613153afd4f20d8dd54fb5
greg <greg.cusack@solana.com>
2023-09-11 15:02:10 -0700

    fix faucet issue. was base64. add ledger to validator

--- net/k8s-cluster/src/scripts/validator-startup-script.sh
+++ net/k8s-cluster/src/scripts/validator-startup-script.sh
@@ -2,19 +2,19 @@
 set -e
 /home/solana/k8s-cluster/src/scripts/decode-accounts.sh -t "validator"
 
-# nohup solana-validator \
-#   --no-os-network-limits-test \
-#   --identity identity.json \
-#   --vote-account vote.json \
-#   --entrypoint $BOOTSTRAP_GOSSIP_PORT \
-#   --rpc-faucet-address $BOOTSTRAP_FAUCET_PORT \
-#   --gossip-port 8001 \
-#   --rpc-port 8899 \
-#   --ledger ledger \
-#   --log logs/solana-validator.log \
-#   --full-rpc-api \
-#   --allow-private-addr \
-#   >logs/init-validator.log 2>&1 &
+nohup solana-validator \
+  --no-os-network-limits-test \
+  --identity identity.json \
+  --vote-account vote.json \
+  --entrypoint $BOOTSTRAP_GOSSIP_PORT \
+  --rpc-faucet-address $BOOTSTRAP_FAUCET_PORT \
+  --gossip-port 8001 \
+  --rpc-port 8899 \
+  --ledger ledger \
+  --log logs/solana-validator.log \
+  --full-rpc-api \
+  --allow-private-addr \
+  >logs/init-validator.log 2>&1 &
 
 
 # # Sleep for an hour (3600 seconds)
