22e3f9f2e89b8a48e44c405832d5917eb2f24572
greg <greg.cusack@solana.com>
2023-09-11 14:47:39 -0700

    modify bootstrap scripts

--- net/k8s-cluster/src/scripts/bootstrap-startup-script.sh
+++ net/k8s-cluster/src/scripts/bootstrap-startup-script.sh
@@ -5,23 +5,23 @@ set -e
 mkdir -p /home/solana/ledger
 tar -xvf /home/solana/genesis/genesis.tar.bz2 -C /home/solana/ledger
 
-nohup solana-validator \
-  --no-os-network-limits-test \
-  --no-wait-for-vote-to-start-leader \
-  --full-snapshot-interval-slots 200 \
-  --identity identity.json \
-  --vote-account vote.json \
-  --ledger ledger \
-  --log logs/solana-validator.log \
-  --gossip-host $MY_POD_IP \
-  --gossip-port 8001 \
-  --rpc-port 8899 \
-  --rpc-faucet-address 127.0.0.1:9900 \
-  --full-rpc-api \
-  --allow-private-addr \
-  >logs/init-validator.log 2>&1 &
+# nohup solana-validator \
+#   --no-os-network-limits-test \
+#   --no-wait-for-vote-to-start-leader \
+#   --full-snapshot-interval-slots 200 \
+#   --identity identity.json \
+#   --vote-account vote.json \
+#   --ledger ledger \
+#   --log logs/solana-validator.log \
+#   --gossip-host $MY_POD_IP \
+#   --gossip-port 8001 \
+#   --rpc-port 8899 \
+#   --rpc-faucet-address 127.0.0.1:9900 \
+#   --full-rpc-api \
+#   --allow-private-addr \
+#   >logs/init-validator.log 2>&1 &
 
-  nohup solana-faucet --keypair faucet.json >logs/faucet.log 2>&1 &
+# nohup solana-faucet --keypair faucet.json >logs/faucet.log 2>&1 &
 
 # # Sleep for an hour (3600 seconds)
-# sleep 3600
+sleep 3600
