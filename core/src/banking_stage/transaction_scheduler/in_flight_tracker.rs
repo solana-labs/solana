874f2c071347179ab4c392c971f85d4dbe5ba6b2
Andrew Fitzgerald <apfitzge@gmail.com>
2023-09-11 18:19:43 +0000

    improve interface, add tests

--- core/src/banking_stage/transaction_scheduler/in_flight_tracker.rs
+++ core/src/banking_stage/transaction_scheduler/in_flight_tracker.rs
@@ -6,14 +6,19 @@ use {
 /// Tracks the number of transactions that are in flight for each thread.
 pub struct InFlightTracker {
     num_in_flight_per_thread: Vec<usize>,
-    batch_id_to_thread_id: HashMap<TransactionBatchId, ThreadId>,
+    batches: HashMap<TransactionBatchId, BatchEntry>,
+}
+
+struct BatchEntry {
+    thread_id: ThreadId,
+    num_transactions: usize,
 }
 
 impl InFlightTracker {
     pub fn new(num_threads: usize) -> Self {
         Self {
             num_in_flight_per_thread: vec![0; num_threads],
-            batch_id_to_thread_id: HashMap::new(),
+            batches: HashMap::new(),
         }
     }
 
@@ -31,22 +36,84 @@ impl InFlightTracker {
         thread_id: ThreadId,
     ) {
         self.num_in_flight_per_thread[thread_id] += num_transactions;
-        self.batch_id_to_thread_id.insert(batch_id, thread_id);
+        assert!(
+            self.batches
+                .insert(
+                    batch_id,
+                    BatchEntry {
+                        thread_id,
+                        num_transactions,
+                    }
+                )
+                .is_none(),
+            "batch id {batch_id} is already being tracked"
+        );
     }
 
-    /// Removes `num_transactions` from the thread which the batch was
-    /// assigned to.
-    pub fn complete_batch(
-        &mut self,
-        batch_id: TransactionBatchId,
-        num_transactions: usize,
-    ) -> ThreadId {
-        let thread_id = self
-            .batch_id_to_thread_id
+    /// Stop tracking the batch with given `batch_id`.
+    /// Removes the number of transactions for the scheduled thread.
+    /// Returns the thread id that the batch was scheduled on.
+    ///
+    /// # Panics
+    /// Panics if the batch id does not exist in the tracker.
+    pub fn complete_batch(&mut self, batch_id: TransactionBatchId) -> ThreadId {
+        let BatchEntry {
+            thread_id,
+            num_transactions,
+        } = self
+            .batches
             .remove(&batch_id)
-            .expect("transaction batch id should exist in in-flight tracker");
+            .expect("batch id {batch} is not being tracked");
         self.num_in_flight_per_thread[thread_id] -= num_transactions;
 
         thread_id
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    #[should_panic(expected = "is already being tracked")]
+    fn test_in_flight_tracker_duplicate_batch() {
+        let mut in_flight_tracker = InFlightTracker::new(2);
+
+        let batch_id = TransactionBatchId::new(0);
+        let batch_num_transactions = 2;
+
+        in_flight_tracker.track_batch(batch_id, batch_num_transactions, 0);
+        in_flight_tracker.track_batch(batch_id, batch_num_transactions, 1);
+    }
+
+    #[test]
+    #[should_panic(expected = "is not being tracked")]
+    fn test_in_flight_tracker_untracked_batch() {
+        let mut in_flight_tracker = InFlightTracker::new(2);
+        in_flight_tracker.complete_batch(TransactionBatchId::new(5));
+    }
+
+    #[test]
+    fn test_in_flight_tracker() {
+        let mut in_flight_tracker = InFlightTracker::new(2);
+
+        let batch_id_0 = TransactionBatchId::new(0);
+        let batch_id_1 = TransactionBatchId::new(1);
+
+        // Add a batch with 2 transactions to thread 0.
+        in_flight_tracker.track_batch(batch_id_0, 2, 0);
+        assert_eq!(in_flight_tracker.num_in_flight_per_thread(), &[2, 0]);
+
+        // Add a batch with 1 transaction to thread 1.
+        in_flight_tracker.track_batch(batch_id_1, 1, 1);
+        assert_eq!(in_flight_tracker.num_in_flight_per_thread(), &[2, 1]);
+
+        // Complete the batch with 2 transactions on thread 0.
+        in_flight_tracker.complete_batch(batch_id_0);
+        assert_eq!(in_flight_tracker.num_in_flight_per_thread(), &[0, 1]);
+
+        // Complete the batch with 1 transaction on thread 1.
+        in_flight_tracker.complete_batch(batch_id_1);
+        assert_eq!(in_flight_tracker.num_in_flight_per_thread(), &[0, 0]);
     }
 }
