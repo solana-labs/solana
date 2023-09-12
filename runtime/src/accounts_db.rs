41942f59f14f3fa48aabc925d96638584a633e1e
Brooks <brooks@solana.com>
2023-09-12 11:26:08 -0400

    Uses hex for the hash in accounts hash cache file names (#33196)
    (cherry picked from commit 21e7f98b3a78adf65e91cc29a8a234cfc4983bf0)

--- runtime/src/accounts_db.rs
+++ runtime/src/accounts_db.rs
@@ -7138,7 +7138,7 @@ impl AccountsDb {
                     // so, build a file name:
                     let hash = hasher.finish();
                     let file_name = format!(
-                        "{}.{}.{}.{}.{}",
+                        "{}.{}.{}.{}.{:016x}",
                         range_this_chunk.start,
                         range_this_chunk.end,
                         bin_range.start,
