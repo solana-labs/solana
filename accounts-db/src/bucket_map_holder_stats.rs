18231e9a5a242c4c0e7567a71480c1b043a14677
Jeff Washington (jwash) <jeff.washington@solana.com>
2023-09-25 12:19:52 -0700

    dump final startup index stats only after startup is complete (#33400)

--- accounts-db/src/bucket_map_holder_stats.rs
+++ accounts-db/src/bucket_map_holder_stats.rs
@@ -226,7 +226,7 @@ impl BucketMapHolderStats {
         // sum of elapsed time in each thread
         let mut thread_time_elapsed_ms = elapsed_ms * storage.threads as u64;
         if disk.is_some() {
-            if startup || was_startup {
+            if was_startup {
                 // these stats only apply at startup
                 datapoint_info!(
                     "accounts_index_startup",
