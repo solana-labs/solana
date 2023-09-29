2845d9414fb5ebdab524058d03fc83d29b9b80e3
Tao Zhu <tao@solana.com>
2023-09-29 22:23:08 +0000

    create separate structs for runtime in different state, add coresponding traits for transacion_meta

--- runtime-transaction/src/transaction_meta.rs
+++ runtime-transaction/src/transaction_meta.rs
@@ -1,16 +1,40 @@
 //! Transaction Meta contains data that follows a transaction through the
-//! execution pipeline in runtime. Metadata can include limits specified by
-//! compute-budget instructions, simple-vote flag, transactino costs,
-//! durable nonce account etc; Metadata can be lazily populated as
-//! transaction goes through execution path.
+//! execution pipeline in runtime. Examples of metadata could be limits
+//! specified by compute-budget instructions, simple-vote flag, transaction
+//! costs, durable nonce account etc;
 //!
+//! The premiss is if anything qualifies as metadata, then it must be valid
+//! and available as long as the transaction itself is valid and available.
+//! Hence they are not Option<T> type. Their visibility at different states
+//! are defined in traits.
+//!
+use solana_sdk::hash::Hash;
+
+/// metadata can be extracted statically from sanitized transaction,
+/// for example: message hash, simple-vote-tx flag, compute budget limits,
+pub trait StaticMeta {
+    fn message_hash(&self) -> &Hash;
+    fn is_simple_vote_tx(&self) -> bool;
+}
+
+/// Statically loaded meta is a supertrait of Dynamically loaded meta, when
+/// transaction transited successfully into dynamically loaded, it should
+/// have both meta data populated and available.
+/// Dynamic metadata available after accounts addresses are loaded from
+/// on-chain ALT, examples are: transaction usage costs, nonce account.
+pub trait DynamicMeta: StaticMeta {}
 
 #[derive(Clone, Debug, Default, PartialEq, Eq)]
 pub struct TransactionMeta {
+    pub message_hash: Hash,
     pub is_simple_vote_tx: bool,
 }
 
 impl TransactionMeta {
+    pub fn set_message_hash(&mut self, message_hash: Hash) {
+        self.message_hash = message_hash;
+    }
+
     pub fn set_is_simple_vote_tx(&mut self, is_simple_vote_tx: bool) {
         self.is_simple_vote_tx = is_simple_vote_tx;
     }
