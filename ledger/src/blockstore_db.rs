67b6177824798dd9acd3bd5f9882845d97ce2e05
kirill lykov <lykov.kirill@gmail.com>
2023-10-03 22:03:00 +0200

    address PR comments: remove unnecessary error from msg
    Co-authored-by: steviez <stevecz@umich.edu>

--- ledger/src/blockstore_db.rs
+++ ledger/src/blockstore_db.rs
@@ -110,21 +110,21 @@ pub enum BlockstoreError {
     ShredForIndexExists,
     #[error("invalid shred data")]
     InvalidShredData(Box<bincode::ErrorKind>),
-    #[error("RocksDB error, error: {0}")]
+    #[error("RocksDB error: {0}")]
     RocksDb(#[from] rocksdb::Error),
     #[error("slot is not rooted")]
     SlotNotRooted,
     #[error("dead slot")]
     DeadSlot,
-    #[error("io error, error: {0}")]
+    #[error("io error: {0}")]
     Io(#[from] std::io::Error),
-    #[error("serialization error, error: {0}")]
+    #[error("serialization error: {0}")]
     Serialize(#[from] Box<bincode::ErrorKind>),
-    #[error("fs extra error, error: {0}")]
+    #[error("fs extra error: {0}")]
     FsExtraError(#[from] fs_extra::error::Error),
     #[error("slot cleaned up")]
     SlotCleanedUp,
-    #[error("unpack error, error: {0}")]
+    #[error("unpack error: {0}")]
     UnpackError(#[from] UnpackError),
     #[error("unable to set open file description limit")]
     UnableToSetOpenFileDescriptorLimit,
@@ -134,9 +134,9 @@ pub enum BlockstoreError {
     EmptyEpochStakes,
     #[error("no vote timestamps in range")]
     NoVoteTimestampsInRange,
-    #[error("protobuf encode error, error: {0}")]
+    #[error("protobuf encode error: {0}")]
     ProtobufEncodeError(#[from] prost::EncodeError),
-    #[error("protobuf decode error, error: {0}")]
+    #[error("protobuf decode error: {0}")]
     ProtobufDecodeError(#[from] prost::DecodeError),
     #[error("parent entries unavailable")]
     ParentEntriesUnavailable,
