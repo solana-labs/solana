099c4d7b61d2953cebb2cf99340f2272dc9fe4bd
Andrew Fitzgerald <apfitzge@gmail.com>
2023-10-09 21:36:37 +0000

    Add Batches

--- core/src/banking_stage/transaction_scheduler/prio_graph_scheduler.rs
+++ core/src/banking_stage/transaction_scheduler/prio_graph_scheduler.rs
@@ -1,11 +1,17 @@
 use {
     super::{
-        in_flight_tracker::InFlightTracker, scheduler_error::SchedulerError,
-        thread_aware_account_locks::ThreadAwareAccountLocks,
+        in_flight_tracker::InFlightTracker,
+        scheduler_error::SchedulerError,
+        thread_aware_account_locks::{ThreadAwareAccountLocks, ThreadId},
         transaction_state_container::TransactionStateContainer,
     },
-    crate::banking_stage::scheduler_messages::{ConsumeWork, FinishedConsumeWork},
+    crate::banking_stage::{
+        consumer::TARGET_NUM_TRANSACTIONS_PER_BATCH,
+        read_write_account_set::ReadWriteAccountSet,
+        scheduler_messages::{ConsumeWork, FinishedConsumeWork, TransactionId},
+    },
     crossbeam_channel::{Receiver, Sender},
+    solana_sdk::{slot_history::Slot, transaction::SanitizedTransaction},
 };
 
 pub(crate) struct PrioGraphScheduler {
@@ -41,5 +47,54 @@ impl PrioGraphScheduler {
         _container: &mut TransactionStateContainer,
     ) -> Result<usize, SchedulerError> {
         todo!()
+    }
+}
+
+struct Batches {
+    ids: Vec<Vec<TransactionId>>,
+    transactions: Vec<Vec<SanitizedTransaction>>,
+    max_age_slots: Vec<Vec<Slot>>,
+    total_cus: Vec<u64>,
+    locks: Vec<ReadWriteAccountSet>,
+}
+
+impl Batches {
+    fn new(num_threads: usize) -> Self {
+        Self {
+            ids: vec![Vec::with_capacity(TARGET_NUM_TRANSACTIONS_PER_BATCH); num_threads],
+            transactions: vec![Vec::with_capacity(TARGET_NUM_TRANSACTIONS_PER_BATCH); num_threads],
+            max_age_slots: vec![Vec::with_capacity(TARGET_NUM_TRANSACTIONS_PER_BATCH); num_threads],
+            total_cus: vec![0; num_threads],
+            locks: std::iter::repeat_with(ReadWriteAccountSet::default)
+                .take(num_threads)
+                .collect(),
+        }
+    }
+
+    fn take_batch(
+        &mut self,
+        thread_id: ThreadId,
+    ) -> (
+        Vec<TransactionId>,
+        Vec<SanitizedTransaction>,
+        Vec<Slot>,
+        u64,
+    ) {
+        self.locks[thread_id].clear();
+        (
+            core::mem::replace(
+                &mut self.ids[thread_id],
+                Vec::with_capacity(TARGET_NUM_TRANSACTIONS_PER_BATCH),
+            ),
+            core::mem::replace(
+                &mut self.transactions[thread_id],
+                Vec::with_capacity(TARGET_NUM_TRANSACTIONS_PER_BATCH),
+            ),
+            core::mem::replace(
+                &mut self.max_age_slots[thread_id],
+                Vec::with_capacity(TARGET_NUM_TRANSACTIONS_PER_BATCH),
+            ),
+            core::mem::replace(&mut self.total_cus[thread_id], 0),
+        )
     }
 }
