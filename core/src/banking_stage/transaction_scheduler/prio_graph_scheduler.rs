dfdc9a838f6f0ee5b9beb45e40fa92451cdf40e5
Andrew Fitzgerald <apfitzge@gmail.com>
2023-10-09 23:32:52 +0000

    Add schedule tests

--- core/src/banking_stage/transaction_scheduler/prio_graph_scheduler.rs
+++ core/src/banking_stage/transaction_scheduler/prio_graph_scheduler.rs
@@ -136,9 +136,6 @@ impl PrioGraphScheduler {
                     continue;
                 };
 
-                // Transaction is scheduable and is assigned to `thread_id`.
-                num_scheduled += 1;
-
                 // Track the chain-id to thread-index mapping.
                 chain_id_to_thread_index.insert(prio_graph.chain_id(&id), thread_id);
 
@@ -339,4 +336,245 @@ impl Batches {
             core::mem::replace(&mut self.total_cus[thread_id], 0),
         )
     }
+}
+
+#[cfg(test)]
+mod tests {
+    use {
+        super::*,
+        crate::banking_stage::consumer::TARGET_NUM_TRANSACTIONS_PER_BATCH,
+        crossbeam_channel::{unbounded, Receiver},
+        itertools::Itertools,
+        solana_runtime::transaction_priority_details::TransactionPriorityDetails,
+        solana_sdk::{
+            compute_budget::ComputeBudgetInstruction, hash::Hash, message::Message, pubkey::Pubkey,
+            signature::Keypair, signer::Signer, system_instruction, transaction::Transaction,
+        },
+        std::borrow::Borrow,
+    };
+
+    macro_rules! txid {
+        ($value:expr) => {
+            TransactionId::new($value)
+        };
+    }
+
+    macro_rules! txids {
+        ([$($element:expr),*]) => {
+            vec![ $(txid!($element)),* ]
+        };
+    }
+
+    fn create_test_frame(num_threads: usize) -> (PrioGraphScheduler, Vec<Receiver<ConsumeWork>>) {
+        let (consume_work_senders, consume_work_receivers) =
+            (0..num_threads).map(|_| unbounded()).unzip();
+        let scheduler = PrioGraphScheduler::new(consume_work_senders);
+        (scheduler, consume_work_receivers)
+    }
+
+    fn prioritized_tranfers(
+        from_keypair: &Keypair,
+        to_pubkeys: impl IntoIterator<Item = impl Borrow<Pubkey>>,
+        lamports: u64,
+        priority: u64,
+    ) -> SanitizedTransaction {
+        let to_pubkeys_lamports = to_pubkeys
+            .into_iter()
+            .map(|pubkey| *pubkey.borrow())
+            .zip(std::iter::repeat(lamports))
+            .collect_vec();
+        let mut ixs =
+            system_instruction::transfer_many(&from_keypair.pubkey(), &to_pubkeys_lamports);
+        let prioritization = ComputeBudgetInstruction::set_compute_unit_price(priority);
+        ixs.push(prioritization);
+        let message = Message::new(&ixs, Some(&from_keypair.pubkey()));
+        let tx = Transaction::new(&[from_keypair], message, Hash::default());
+        SanitizedTransaction::from_transaction_for_tests(tx)
+    }
+
+    fn create_container(
+        tx_infos: impl IntoIterator<
+            Item = (
+                impl Borrow<Keypair>,
+                impl IntoIterator<Item = impl Borrow<Pubkey>>,
+                u64,
+                u64,
+            ),
+        >,
+    ) -> TransactionStateContainer {
+        let mut container = TransactionStateContainer::with_capacity(10 * 1024);
+        for (index, (from_keypair, to_pubkeys, lamports, priority)) in
+            tx_infos.into_iter().enumerate()
+        {
+            let id = TransactionId::new(index as u64);
+            let transaction =
+                prioritized_tranfers(from_keypair.borrow(), to_pubkeys, lamports, priority);
+            let transaction_ttl = SanitizedTransactionTTL {
+                transaction,
+                max_age_slot: Slot::MAX,
+            };
+            container.insert_new_transaction(
+                id,
+                transaction_ttl,
+                TransactionPriorityDetails {
+                    priority,
+                    compute_unit_limit: 1,
+                },
+            );
+        }
+
+        container
+    }
+
+    fn collect_work(
+        receiver: &Receiver<ConsumeWork>,
+    ) -> (Vec<ConsumeWork>, Vec<Vec<TransactionId>>) {
+        receiver
+            .try_iter()
+            .map(|work| {
+                let ids = work.ids.clone();
+                (work, ids)
+            })
+            .unzip()
+    }
+
+    #[test]
+    fn test_schedule_disconnected_channel() {
+        let (mut scheduler, work_receivers) = create_test_frame(1);
+        let mut container = create_container([(&Keypair::new(), &[Pubkey::new_unique()], 1, 1)]);
+
+        drop(work_receivers); // explicitly drop receivers
+        assert_matches!(
+            scheduler.schedule(&mut container),
+            Err(SchedulerError::DisconnectedSendChannel(_))
+        );
+    }
+
+    #[test]
+    fn test_schedule_single_threaded_no_conflicts() {
+        let (mut scheduler, work_receivers) = create_test_frame(1);
+        let mut container = create_container([
+            (&Keypair::new(), &[Pubkey::new_unique()], 1, 1),
+            (&Keypair::new(), &[Pubkey::new_unique()], 2, 2),
+        ]);
+
+        let num_scheduled = scheduler.schedule(&mut container).unwrap();
+        assert_eq!(num_scheduled, 2);
+        assert_eq!(collect_work(&work_receivers[0]).1, vec![txids!([1, 0])]);
+    }
+
+    #[test]
+    fn test_schedule_single_threaded_conflict() {
+        let (mut scheduler, work_receivers) = create_test_frame(1);
+        let pubkey = Pubkey::new_unique();
+        let mut container = create_container([
+            (&Keypair::new(), &[pubkey], 1, 1),
+            (&Keypair::new(), &[pubkey], 1, 2),
+        ]);
+
+        let num_scheduled = scheduler.schedule(&mut container).unwrap();
+        assert_eq!(num_scheduled, 2);
+        assert_eq!(
+            collect_work(&work_receivers[0]).1,
+            vec![txids!([1]), txids!([0])]
+        );
+    }
+
+    #[test]
+    fn test_schedule_consume_single_threaded_multi_batch() {
+        let (mut scheduler, work_receivers) = create_test_frame(1);
+        let mut container = create_container(
+            (0..4 * TARGET_NUM_TRANSACTIONS_PER_BATCH)
+                .map(|i| (Keypair::new(), [Pubkey::new_unique()], i as u64, 1)),
+        );
+
+        // expect 4 full batches to be scheduled
+        let num_scheduled = scheduler.schedule(&mut container).unwrap();
+        assert_eq!(num_scheduled, 4 * TARGET_NUM_TRANSACTIONS_PER_BATCH);
+
+        let thread0_work_counts: Vec<_> = work_receivers[0]
+            .try_iter()
+            .map(|work| work.ids.len())
+            .collect();
+        assert_eq!(thread0_work_counts, [TARGET_NUM_TRANSACTIONS_PER_BATCH; 4]);
+    }
+
+    #[test]
+    fn test_schedule_simple_thread_selection() {
+        let (mut scheduler, work_receivers) = create_test_frame(2);
+        let mut container =
+            create_container((0..4).map(|i| (Keypair::new(), [Pubkey::new_unique()], 1, i)));
+
+        let num_scheduled = scheduler.schedule(&mut container).unwrap();
+        assert_eq!(num_scheduled, 4);
+        assert_eq!(collect_work(&work_receivers[0]).1, [txids!([3, 1])]);
+        assert_eq!(collect_work(&work_receivers[1]).1, [txids!([2, 0])]);
+    }
+
+    #[test]
+    fn test_schedule_look_ahead() {
+        let (mut scheduler, work_receivers) = create_test_frame(2);
+
+        let accounts = (0..4).map(|_| Keypair::new()).collect_vec();
+        let mut container = create_container([
+            (&accounts[0], &[accounts[1].pubkey()], 1, 2),
+            (&accounts[2], &[accounts[3].pubkey()], 1, 1),
+            (&accounts[1], &[accounts[2].pubkey()], 1, 0),
+        ]);
+
+        // high priority transactions [0, 1] do not conflict, and would be
+        // scheduled to *different* threads without chain-id look-ahead.
+        // Because low priority transaction [2] conflicts with both, it will
+        // cause transaction [1] to be scheduled onto the same thread as
+        // transaction [0].
+        let num_scheduled = scheduler.schedule(&mut container).unwrap();
+        assert_eq!(num_scheduled, 3);
+        assert_eq!(
+            collect_work(&work_receivers[0]).1,
+            [txids!([0, 1]), txids!([2])]
+        );
+    }
+
+    // #[test]
+    // fn test_schedule_priority_guard() {
+    //     let (mut scheduler, work_receivers) = create_test_frame(2);
+
+    //     let accounts = (0..6).map(|_| Keypair::new()).collect_vec();
+    //     let mut container = create_container([
+    //         (&accounts[0], vec![accounts[1].pubkey()], 1, 3),
+    //         (&accounts[2], vec![accounts[3].pubkey()], 1, 2),
+    //         (
+    //             &accounts[1],
+    //             vec![accounts[2].pubkey(), accounts[4].pubkey()],
+    //             1,
+    //             1,
+    //         ),
+    //         (&accounts[4], vec![accounts[5].pubkey()], 1, 0),
+    //     ]);
+
+    //     // high priority transactions [0, 1] do not conflict, and would be
+    //     // scheduled to *different* threads without chain-id look-ahead.
+    //     // low priority transaction [2] conflicts with both, and thus will
+    //     // cause transaction [1] to be scheduled onto the same thread as
+    //     // transaction [0].
+    //     let num_scheduled = scheduler.schedule(&mut container).unwrap();
+    //     assert_eq!(num_scheduled, 2);
+    //     let (thread_0_work, thread_0_ids) = collect_work(&work_receivers[0]);
+    //     assert_eq!(thread_0_ids, [txids!([0])]);
+    //     assert_eq!(collect_work(&work_receivers[1]).1, [txids!([1])]);
+
+    //     // Cannot schedule even on next pass because of lock conflicts
+    //     let num_scheduled = scheduler.schedule(&mut container).unwrap();
+    //     assert_eq!(num_scheduled, 0);
+
+    //     // Complete batch on thread 0. Remaining txs can be scheduled onto thread 1
+    //     scheduler.complete_batch(thread_0_work[0].batch_id, &thread_0_work[0].transactions);
+    //     let num_scheduled = scheduler.schedule(&mut container).unwrap();
+    //     assert_eq!(num_scheduled, 2);
+
+    //     assert_eq!(
+    //         collect_work(&work_receivers[1]).1,
+    //         [txids!([2]), txids!([3])]
+    //     );
+    // }
 }
