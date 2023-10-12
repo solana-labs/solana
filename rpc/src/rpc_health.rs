82fa51c27bd7fc9ac663e8eed5b6f5da199528d0
Steven Czabaniuk <steven@solana.com>
2023-10-12 18:23:08 -0500

    Add unit test

--- rpc/src/rpc_health.rs
+++ rpc/src/rpc_health.rs
@@ -52,11 +52,11 @@ impl RpcHealth {
             }
         }
 
-        if !self.startup_verification_complete.load(Ordering::Acquire) {
-            return RpcHealthStatus::Unknown;
-        }
         if self.override_health_check.load(Ordering::Relaxed) {
             return RpcHealthStatus::Ok;
+        }
+        if !self.startup_verification_complete.load(Ordering::Acquire) {
+            return RpcHealthStatus::Unknown;
         }
 
         // A node can observe votes by both replaying blocks and observing gossip.
@@ -129,5 +129,86 @@ impl RpcHealth {
     #[cfg(test)]
     pub(crate) fn stub_set_health_status(&self, stub_health_status: Option<RpcHealthStatus>) {
         *self.stub_health_status.write().unwrap() = stub_health_status;
+    }
+}
+
+#[cfg(test)]
+pub mod tests {
+    use {
+        super::*,
+        solana_ledger::{
+            genesis_utils::{create_genesis_config, GenesisConfigInfo},
+            get_tmp_ledger_path_auto_delete,
+        },
+        solana_runtime::{bank::Bank, bank_forks::BankForks},
+        solana_sdk::{clock::UnixTimestamp, hash::Hash, pubkey::Pubkey},
+    };
+
+    #[test]
+    fn test_get_health() {
+        let ledger_path = get_tmp_ledger_path_auto_delete!();
+        let blockstore = Arc::new(Blockstore::open(ledger_path.path()).unwrap());
+        let GenesisConfigInfo { genesis_config, .. } = create_genesis_config(100);
+        let bank = Bank::new_for_tests(&genesis_config);
+        let bank_forks = Arc::new(RwLock::new(BankForks::new(bank)));
+        let optimistically_confirmed_bank =
+            OptimisticallyConfirmedBank::locked_from_bank_forks_root(&bank_forks);
+        let bank0 = bank_forks.read().unwrap().root_bank();
+        assert!(bank0.slot() == 0);
+
+        let health_check_slot_distance = 10;
+        let override_health_check = Arc::new(AtomicBool::new(true));
+        let startup_verification_complete = Arc::clone(bank0.get_startup_verification_complete());
+        let health = RpcHealth::new(
+            optimistically_confirmed_bank.clone(),
+            blockstore.clone(),
+            health_check_slot_distance,
+            override_health_check.clone(),
+            startup_verification_complete,
+        );
+
+        // Override health check set to true - status is ok
+        assert_eq!(health.check(), RpcHealthStatus::Ok);
+
+        // Remove the override - status now unknown with incomplete startup verification
+        override_health_check.store(false, Ordering::Relaxed);
+        assert_eq!(health.check(), RpcHealthStatus::Unknown);
+
+        // Mark startup verification complete - status still unknown as no slots have been
+        // optimistically confirmed yet
+        bank0.set_startup_verification_complete();
+        assert_eq!(health.check(), RpcHealthStatus::Unknown);
+
+        // Mark slot 15 as being optimistically confirmed in the Blockstore, this could
+        // happen if the cluster confirmed the slot and this node became aware through gossip,
+        // but this node has not yet replayed slot 15. The local view of the latest optimistic
+        // slot is still slot 0 so status will be behind
+        blockstore
+            .insert_optimistic_slot(15, &Hash::default(), UnixTimestamp::default())
+            .unwrap();
+        assert_eq!(health.check(), RpcHealthStatus::Behind { num_slots: 15 });
+
+        // Simulate this node observing slot 4 as optimistically confirmed - status still behind
+        let bank4 = Arc::new(Bank::new_from_parent(bank0, &Pubkey::default(), 4));
+        optimistically_confirmed_bank.write().unwrap().bank = bank4.clone();
+        assert_eq!(health.check(), RpcHealthStatus::Behind { num_slots: 11 });
+
+        // Simulate this node observing slot 5 as optimistically confirmed - status now ok
+        // as distance is <= health_check_slot_distance
+        let bank5 = Arc::new(Bank::new_from_parent(bank4, &Pubkey::default(), 5));
+        optimistically_confirmed_bank.write().unwrap().bank = bank5.clone();
+        assert_eq!(health.check(), RpcHealthStatus::Ok);
+
+        // Node now up with tip of cluster
+        let bank15 = Arc::new(Bank::new_from_parent(bank5, &Pubkey::default(), 15));
+        optimistically_confirmed_bank.write().unwrap().bank = bank15.clone();
+        assert_eq!(health.check(), RpcHealthStatus::Ok);
+
+        // Node "beyond" tip of cluster - this technically isn't possible but could be
+        // observed locally due to a race between updates to Blockstore and
+        // OptimisticallyConfirmedBank. Either way, not a problem and status is ok.
+        let bank16 = Arc::new(Bank::new_from_parent(bank15, &Pubkey::default(), 16));
+        optimistically_confirmed_bank.write().unwrap().bank = bank16.clone();
+        assert_eq!(health.check(), RpcHealthStatus::Ok);
     }
 }
