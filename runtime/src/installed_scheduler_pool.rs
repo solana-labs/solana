213aaf94a9da5c8d95691ee0ed748e6b45b7cac4
Ryo Onodera <ryoqun@gmail.com>
2023-10-14 00:02:29 +0900

    save 2023y10m14d00h02m29s133898654ns

--- runtime/src/installed_scheduler_pool.rs
+++ runtime/src/installed_scheduler_pool.rs
@@ -274,6 +274,10 @@ impl BankWithScheduler {
         self.inner.bank.clone()
     }
 
+    pub fn clone(&self) -> usize {
+        0
+    }
+
     pub fn register_tick(&self, hash: &Hash) {
         self.inner.bank.register_tick(hash, &self.inner.scheduler);
     }
