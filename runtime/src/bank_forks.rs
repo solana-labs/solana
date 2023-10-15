d998a8a674a1c4cf0be7e8edff92c3ba566399e3
Ryo Onodera <ryoqun@gmail.com>
2023-10-15 15:33:54 +0900

    save 2023y10m15d15h33m54s534680045ns

--- runtime/src/bank_forks.rs
+++ runtime/src/bank_forks.rs
@@ -88,7 +88,7 @@ impl BankForks {
     }
 
     pub fn banks(&self) -> HashMap<Slot, Arc<Bank>> {
-        self.banks.iter().map(|(&k, b)| (k, b.clone())).collect()
+        self.banks.iter().map(|(&k, b)| (k, b.clone_without_scheduler())).collect()
     }
 
     pub fn get_vote_only_mode_signal(&self) -> Arc<AtomicBool> {
