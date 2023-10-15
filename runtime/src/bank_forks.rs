1cf3dfbc6d185e7cb198c4d7823513ff6bde74b6
Ryo Onodera <ryoqun@gmail.com>
2023-10-15 15:20:27 +0900

    save 2023y10m15d15h20m27s123724700ns

--- runtime/src/bank_forks.rs
+++ runtime/src/bank_forks.rs
@@ -228,14 +228,14 @@ impl BankForks {
         bank
     }
 
-    #[cfg(feature = "dev-context-only-utils")]
-    pub fn insert_without_scheduler(&mut self, bank: Bank) -> Arc<Bank> {
-        self.insert(bank).clone_without_scheduler()
-    }
-
     pub fn insert_from_ledger(&mut self, bank: Bank) -> BankWithScheduler {
         self.highest_slot_at_startup = std::cmp::max(self.highest_slot_at_startup, bank.slot());
         self.insert(bank)
+    }
+
+    #[cfg(feature = "dev-context-only-utils")]
+    pub fn insert_without_scheduler(&mut self, bank: Bank) -> Arc<Bank> {
+        self.insert(bank).clone_without_scheduler()
     }
 
     pub fn remove(&mut self, slot: Slot) -> Option<Arc<Bank>> {
