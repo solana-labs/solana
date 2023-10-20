69978695edb12b05bca4b6541714f375531c1bc7
samkim-crypto <skim13@cs.stanford.edu>
2023-10-20 16:13:45 -0700

    bump deprecation version to 1.17.2

--- clap-v3-utils/src/input_validators.rs
+++ clap-v3-utils/src/input_validators.rs
@@ -60,7 +60,7 @@ where
 
 // Return an error if a pubkey cannot be parsed.
 #[deprecated(
-    since = "1.17.0",
+    since = "1.17.2",
     note = "please use `clap::value_parser!(Pubkey)` instead"
 )]
 pub fn is_pubkey(string: &str) -> Result<(), String> {
@@ -81,7 +81,7 @@ where
 
 // Return an error if a keypair file cannot be parsed.
 #[deprecated(
-    since = "1.17.0",
+    since = "1.17.2",
     note = "please use `SignerSourceParseBuilder::new().allow_file_path().build()` instead"
 )]
 pub fn is_keypair<T>(string: T) -> Result<(), String>
@@ -95,7 +95,7 @@ where
 
 // Return an error if a keypair file cannot be parsed
 #[deprecated(
-    since = "1.17.0",
+    since = "1.17.2",
     note = "please use `SignerSourceParseBuilder::new().allow_file_path().allow_prompt().build()` instead"
 )]
 pub fn is_keypair_or_ask_keyword<T>(string: T) -> Result<(), String>
@@ -112,7 +112,7 @@ where
 
 // Return an error if a `SignerSourceKind::Prompt` cannot be parsed
 #[deprecated(
-    since = "1.17.0",
+    since = "1.17.2",
     note = "please use `SignerSourceParseBuilder::new().allow_prompt().build()` instead"
 )]
 pub fn is_prompt_signer_source(string: &str) -> Result<(), String> {
@@ -132,7 +132,7 @@ pub fn is_prompt_signer_source(string: &str) -> Result<(), String> {
 
 // Return an error if string cannot be parsed as pubkey string or keypair file location
 #[deprecated(
-    since = "1.17.0",
+    since = "1.17.2",
     note = "please use `SignerSourceParseBuilder::new().allow_pubkey().allow_path().build()` instead"
 )]
 pub fn is_pubkey_or_keypair<T>(string: T) -> Result<(), String>
@@ -145,7 +145,7 @@ where
 // Return an error if string cannot be parsed as a pubkey string, or a valid Signer that can
 // produce a pubkey()
 #[deprecated(
-    since = "1.17.0",
+    since = "1.17.2",
     note = "please use `SignerSourceParseBuilder::new().allow_pubkey().allow_path().build()` instead"
 )]
 pub fn is_valid_pubkey<T>(string: T) -> Result<(), String>
@@ -170,7 +170,7 @@ where
 // Clap validators can't check multiple fields at once, so the verification that a `--signer` is
 // also provided and correct happens in parsing, not in validation.
 #[deprecated(
-    since = "1.17.0",
+    since = "1.17.2",
     note = "please use `SignerSourceParseBuilder::new().build()` instead"
 )]
 pub fn is_valid_signer<T>(string: T) -> Result<(), String>
