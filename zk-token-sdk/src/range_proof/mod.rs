4a79a1fde31fdc922bd7433f70a377f8ad8d2ae0
samkim-crypto <skim13@cs.stanford.edu>
2023-10-20 10:31:12 -0700

    add/fix docs for range proof

--- zk-token-sdk/src/range_proof/mod.rs
+++ zk-token-sdk/src/range_proof/mod.rs
@@ -1,3 +1,15 @@
+//! The Bulletproofs range-proof implementation over Curve25519 Ristretto points.
+//!
+//! The implementation is based on the dalek-cryptography bulletproofs
+//! [implementation](https://github.com/dalek-cryptography/bulletproofs). Compared to the original
+//! implementation by dalek-cryptography:
+//! - This implementation focuses on the range proof implementation, while the dalek-cryptography
+//! crate additionally implements the general bulletproofs implementation for languages that can be
+//! represented by arithmetic circuits as well as MPC.
+//! - This implementation implements a non-interactive range proof aggregation that is specified in
+//! the original Bulletproofs [paper](https://eprint.iacr.org/2017/1066) (Section 4.3).
+//!
+
 #[cfg(not(target_os = "solana"))]
 use {
     crate::encryption::pedersen::{Pedersen, PedersenCommitment, PedersenOpening},
