3e56c5573592f421f5dc6a3ef708525bda62590d
Alexander Meißner <AlexanderMeissner@gmx.net>
2023-10-21 12:49:47 +0200

    Bump solana_rbpf to v0.8.0 (#33679)
    * Bumps solana_rbpf to v0.8.0
    
    * Adjustments:
    Replaces declare_syscall!() with declare_builtin_function!().
    Removes Config::encrypt_runtime_environment.
    Simplifies error propagation.
    
    (cherry picked from commit a5c7c999e2060971d9369bde13e05247c9ddd415)

--- runtime/benches/bank.rs
+++ runtime/benches/bank.rs
@@ -125,13 +125,13 @@ fn do_bench_transactions(
     // freeze bank so that slot hashes is populated
     bank.freeze();
 
-    declare_process_instruction!(process_instruction, 1, |_invoke_context| {
+    declare_process_instruction!(MockBuiltin, 1, |_invoke_context| {
         // Do nothing
         Ok(())
     });
 
     let mut bank = Bank::new_from_parent(Arc::new(bank), &Pubkey::default(), 1);
-    bank.add_mockup_builtin(Pubkey::from(BUILTIN_PROGRAM_ID), process_instruction);
+    bank.add_mockup_builtin(Pubkey::from(BUILTIN_PROGRAM_ID), MockBuiltin::vm);
     bank.add_builtin_account("solana_noop_program", &Pubkey::from(NOOP_PROGRAM_ID), false);
     let bank = Arc::new(bank);
     let bank_client = BankClient::new_shared(bank.clone());
