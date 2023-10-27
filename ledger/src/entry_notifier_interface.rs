5228b709f29d3fc5eb6b9e6cb54a68a3b0ca324f
vovkman <liam.vovk@gmail.com>
2023-10-27 13:08:33 -0700

    remove unused RWLock

--- ledger/src/entry_notifier_interface.rs
+++ ledger/src/entry_notifier_interface.rs
@@ -1,8 +1,4 @@
-use {
-    solana_entry::entry::EntrySummary,
-    solana_sdk::clock::Slot,
-    std::sync::{Arc, RwLock},
-};
+use {solana_entry::entry::EntrySummary, solana_sdk::clock::Slot, std::sync::Arc};
 
 pub trait EntryNotifier {
     fn notify_entry(&self, slot: Slot, index: usize, entry: &EntrySummary);
