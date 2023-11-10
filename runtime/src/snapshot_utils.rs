f287fa92e8025e35dccadfd0ddc6456e8a0768a8
brooks <brooks@solana.com>
2023-11-10 14:50:11 -0500

    Adds documentation to verify_and_unarchive_snapshots()

--- runtime/src/snapshot_utils.rs
+++ runtime/src/snapshot_utils.rs
@@ -1230,6 +1230,7 @@ pub struct BankFromDirTimings {
 // From testing, 4 seems to be a sweet spot for ranges of 60M-360M accounts and 16-64 cores. This may need to be tuned later.
 const PARALLEL_UNTAR_READERS_DEFAULT: usize = 4;
 
+/// Unarchives the given full and incremental snapshot archives, as long as they are compatible.
 pub fn verify_and_unarchive_snapshots(
     bank_snapshots_dir: impl AsRef<Path>,
     full_snapshot_archive_info: &FullSnapshotArchiveInfo,
