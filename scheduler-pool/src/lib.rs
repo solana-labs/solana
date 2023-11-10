de1d7848e770b638d28f0b793d4ca575197dfeb0
Ryo Onodera <ryoqun@gmail.com>
2023-11-11 00:01:40 +0900

    save

--- scheduler-pool/src/lib.rs
+++ scheduler-pool/src/lib.rs
@@ -862,13 +862,13 @@ impl<TH: Handler<SEA>, SEA: ScheduleExecutionArg> PooledScheduler<TH, SEA> {
     }
 }
 
-type ChannelPair<T> = (
+type ChannelPair<T, U> = (
     crossbeam_channel::Receiver<SessionedChannel<T>>,
-    crossbeam_channel::Sender<T>,
+    crossbeam_channel::Sender<U>,
 );
 
-trait WithChannelPair<T>: Send + Sync {
-    fn unwrap_channel_pair(&mut self) -> ChannelPair<T>;
+trait WithChannelPair<T, U>: Send + Sync {
+    fn unwrap_channel_pair(&mut self) -> ChannelPair<T, U>;
 }
 
 enum SessionedChannel<T> {
