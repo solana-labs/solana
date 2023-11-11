1aa1dfcb37d8be3db828b8fc4ca6f9adacb1ed8d
Ryo Onodera <ryoqun@gmail.com>
2023-11-11 15:54:01 +0900

    save

--- scheduler-pool/src/lib.rs
+++ scheduler-pool/src/lib.rs
@@ -963,7 +963,7 @@ where
         let (_transaction_sender, mut transaction_receiver) =
             unbounded::<SessionedChannel<Box<Task>>>();
         let (blocked_transaction_sender, blocked_transaction_receiver) =
-            unbounded::<SessionedChannel<Box<ExecutionEnvironment>>>();
+            unbounded::<SessionedChannel<Box<ExecutionEnvironment>, ResultWithTimings>>();
         let (idle_transaction_sender, idle_transaction_receiver) =
             unbounded::<Box<ExecutionEnvironment>>();
         let (handled_blocked_transaction_sender, handled_blocked_transaction_receiver) =
