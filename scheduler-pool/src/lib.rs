228ccdfe85550af10d69766c3bdf1661f8f8eaf4
Ryo Onodera <ryoqun@gmail.com>
2023-11-11 16:49:30 +0900

    save

--- scheduler-pool/src/lib.rs
+++ scheduler-pool/src/lib.rs
@@ -884,7 +884,7 @@ impl<T: Send + Sync, U: Send + Sync> WithChannelPair<T, U> for ChannelPairOption
 enum SessionedChannel<T, U> {
     Payload(T),
     NextSession(Box<dyn WithChannelPair<T, U>>),
-    NewContext(Box<dyn WithChannelPair<T, SchedulingContext>>),
+    NewContext(SchedulingContext),
 }
 
 impl<T: Send + Sync + 'static, U: Send + Sync + 'static> SessionedChannel<T, U> {
