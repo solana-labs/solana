d8b96f420942b7342de2e953e0dc9334bcde6d31
Ryo Onodera <ryoqun@gmail.com>
2023-11-11 21:57:16 +0900

    save

--- scheduler-pool/src/lib.rs
+++ scheduler-pool/src/lib.rs
@@ -1504,7 +1504,7 @@ where
     }
 
     fn context(&self) -> &SchedulingContext {
-        todo!(); //self.context.as_ref().expect("active context should exist")
+        self.thread_manager.read().unwrap().context
     }
 
     fn schedule_execution(&self, transaction_with_index: SEA::TransactionWithIndex<'_>) {
