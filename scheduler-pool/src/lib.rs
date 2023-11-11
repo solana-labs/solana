cdc1daa468e3e77ea3f1974e0542a5694cef1553
Ryo Onodera <ryoqun@gmail.com>
2023-11-11 22:29:01 +0900

    save

--- scheduler-pool/src/lib.rs
+++ scheduler-pool/src/lib.rs
@@ -1149,7 +1149,23 @@ where
     }
 
     fn end_session(&self) -> ResultWithTimings {
-        self.result_receiver.recv().unwrap()
+        let pair = unbounded();
+        (
+            next_schedulrable_transaction_sender,
+            next_schedulrable_transaction_receiver,
+        ) = &pair;
+
+        self.schedulrable_transaction_sender
+            .send(ChainedChannel::new_channel(
+                next_schedulrable_transaction_receiver,
+                ControlFrame::NextSession,
+            ))
+            .unwrap();
+        self.result_receiver.recv().unwrap();
+        (
+            self.schedulrable_transaction_sender,
+            self.schedulable_transaction_receiver,
+        ) = pair;
     }
 }
 
