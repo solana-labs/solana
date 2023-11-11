869fc0efd2edc9e0ef1a2889b88c305c86155f26
Ryo Onodera <ryoqun@gmail.com>
2023-11-11 21:42:43 +0900

    save

--- scheduler-pool/src/lib.rs
+++ scheduler-pool/src/lib.rs
@@ -911,7 +911,7 @@ where
         handler: TH,
         pool: Arc<SchedulerPool<PooledScheduler<TH, SEA>, TH, SEA>>,
     ) -> Self {
-        let (schedulrable_transaction_sender, mut schedulable_transaction_receiver) = unbounded();
+        let (schedulrable_transaction_sender, schedulable_transaction_receiver) = unbounded();
         Self {
             schedulrable_transaction_sender,
             schedulable_transaction_receiver,
