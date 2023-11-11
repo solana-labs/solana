2d77b3b25726076b4e4eba85d1c7758129a05dbd
Ryo Onodera <ryoqun@gmail.com>
2023-11-11 16:31:08 +0900

    save

--- scheduler-pool/src/lib.rs
+++ scheduler-pool/src/lib.rs
@@ -914,6 +914,9 @@ impl<T: Send + Sync + 'static, U: Send + Sync + 'static> SessionedChannel<T, U> 
 }
 
 impl<T: Send + Sync + 'static, U: Send + Sync + 'static> SessionedChannel2<T, U> {
+    fn new_context(receiver: Receiver<Self>, context: U) -> Self {
+        Self::NewContext(Box::new(ChannelPairOption2(Some((receiver, context)))))
+    }
 }
 
 impl<TH, SEA> ThreadManager<TH, SEA>
