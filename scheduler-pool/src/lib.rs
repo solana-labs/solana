2397e58876da09dd4547422ea17b2dfacab95d16
Ryo Onodera <ryoqun@gmail.com>
2023-11-12 16:55:36 +0900

    save

--- scheduler-pool/src/lib.rs
+++ scheduler-pool/src/lib.rs
@@ -1071,7 +1071,7 @@ where
                         };
 
                         if let Some(task) = state_machine.0.pop() {
-                            let ee = prepare_scheduled_execution(task, vec![]);
+                            let ee = ScheduleStage::prepare_scheduled_execution(task, vec![]);
                             blocked_transaction_sessioned_sender.send(ChainedChannel::Payload(ee));
                         }
                     }
