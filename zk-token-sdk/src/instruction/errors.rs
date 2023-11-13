b422fb3cc9b80307677537c0946a2cc5594fa762
samkim-crypto <skim13@cs.stanford.edu>
2023-11-13 19:17:09 +0900

    fix `thiserror` visibility

--- zk-token-sdk/src/instruction/errors.rs
+++ zk-token-sdk/src/instruction/errors.rs
@@ -1,6 +1,8 @@
+#[cfg(not(target_os = "solana"))]
 use thiserror::Error;
 
 #[derive(Error, Clone, Debug, Eq, PartialEq)]
+#[cfg(not(target_os = "solana"))]
 pub enum InstructionError {
     #[error("decryption error")]
     Decryption,
