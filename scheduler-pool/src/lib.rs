e9eb7ba442e05065b0ba5f2145011a73bc938db6
Ryo Onodera <ryoqun@gmail.com>
2023-11-14 16:09:50 +0900

    save

--- scheduler-pool/src/lib.rs
+++ scheduler-pool/src/lib.rs
@@ -720,35 +720,47 @@ where
                                 .unwrap();
                         }
 
-                        if let Ok(execution_environment) = handled_blocked_transaction_receiver.try_recv() {
-                            Self::update_result_with_timings(result_with_timings.as_mut().unwrap(), &execution_environment);
-                            state_machine.deschedule_task(execution_environment);
-                        } else if let Ok(mm) = schedulable_transaction_receiver.try_recv() {
-                            match mm {
-                                ChainedChannel::Payload(payload) => {
-                                    if let Some(ee) = state_machine.schedule_new_task(payload) {
-                                        blocked_transaction_sessioned_sender
-                                            .send(ChainedChannel::Payload(ee))
-                                            .unwrap();
-                                    }
-                                }
-                                ChainedChannel::ChannelWithPayload(new_channel) => {
-                                    let control_frame;
-                                    (schedulable_transaction_receiver, control_frame) = new_channel.channel_and_payload();
-                                    match control_frame {
-                                        ControlFrame::StartSession(context) => {
-                                            Self::propagate_context(&mut blocked_transaction_sessioned_sender, context, handler_count);
-                                        }
-                                        ControlFrame::EndSession => {
-                                            debug!("scheduler_main_loop: will_end_session = true");
-                                            will_end_session = true;
+                        let mut continue_to_drain = true;
+
+                        while continue_to_drain {
+                            if let Ok(execution_environment) = handled_blocked_transaction_receiver.try_recv() {
+                                Self::update_result_with_timings(result_with_timings.as_mut().unwrap(), &execution_environment);
+                                state_machine.deschedule_task(execution_environment);
+                            } else if let Ok(mm) = schedulable_transaction_receiver.try_recv() {
+                                match mm {
+                                    ChainedChannel::Payload(payload) => {
+                                        if let Some(ee) = state_machine.schedule_new_task(payload) {
+                                            blocked_transaction_sessioned_sender
+                                                .send(ChainedChannel::Payload(ee))
+                                                .unwrap();
                                         }
                                     }
-                                }
-                            };
-                        } else if let Ok(execution_environment) = handled_idle_transaction_receiver.try_recv() {
-                            Self::update_result_with_timings(result_with_timings.as_mut().unwrap(), &execution_environment);
-                            state_machine.deschedule_task(execution_environment);
+                                    ChainedChannel::ChannelWithPayload(new_channel) => {
+                                        let control_frame;
+                                        (schedulable_transaction_receiver, control_frame) = new_channel.channel_and_payload();
+                                        match control_frame {
+                                            ControlFrame::StartSession(context) => {
+                                                Self::propagate_context(&mut blocked_transaction_sessioned_sender, context, handler_count);
+                                            }
+                                            ControlFrame::EndSession => {
+                                                debug!("scheduler_main_loop: will_end_session = true");
+                                                will_end_session = true;
+                                            }
+                                        }
+                                    }
+                                };
+                            } else if let Ok(execution_environment) = handled_idle_transaction_receiver.try_recv() {
+                                Self::update_result_with_timings(result_with_timings.as_mut().unwrap(), &execution_environment);
+                                state_machine.deschedule_task(execution_environment);
+                            } else {
+                                continue_to_drain = false;
+                            }
+
+                            if let Some(ee) = state_machine.schedule_retryable_task() {
+                                blocked_transaction_sessioned_sender
+                                    .send(ChainedChannel::Payload(ee))
+                                    .unwrap();
+                            }
                         }
                     }
 
