cb7f9395ecd1a951c8c84fda5fea35f7f0ccab91
Tyera Eulberg <tyera@solana.com>
2023-11-15 15:36:23 -0700

    Add helper fn to stop retrying if table not found

--- storage-bigtable/src/bigtable.rs
+++ storage-bigtable/src/bigtable.rs
@@ -6,7 +6,7 @@ use {
         compression::{compress_best, decompress},
         root_ca_certificate, CredentialType,
     },
-    backoff::{future::retry, ExponentialBackoff},
+    backoff::{future::retry, Error as BackoffError, ExponentialBackoff},
     log::*,
     std::{
         str::FromStr,
@@ -82,6 +82,15 @@ pub enum Error {
 
     #[error("Timeout")]
     Timeout,
+}
+
+fn check_table_not_found<T>(result: Result<T>) -> std::result::Result<T, BackoffError<Error>> {
+    if let Err(Error::Rpc(ref err)) = result {
+        if err.code() == tonic::Code::NotFound && err.message().starts_with("table") {
+            return Err(BackoffError::Permanent(Error::Rpc(err.clone())));
+        }
+    }
+    Ok(result?)
 }
 
 impl std::convert::From<std::io::Error> for Error {
