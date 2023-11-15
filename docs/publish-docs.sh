302919e66101e980335c0dd21eac9dff512df45c
nickfrosty <nfrostbutter@gmail.com>
2023-11-15 13:43:27 -0500

    refactor: simplified runtime redirects

--- docs/publish-docs.sh
+++ docs/publish-docs.sh
@@ -105,9 +105,7 @@ cat > "$CONFIG_FILE" <<EOF
     { "source": "/validator/overview/what-is-an-rpc-node", "destination": "/what-is-an-rpc-node" },
     { "source": "/validator/overview/what-is-a-validator", "destination": "/what-is-a-validator" },
 
-    { "source": "/developing/runtime-facilities/programs", "destination": "/runtime/programs" },
-    { "source": "/developing/runtime-facilities/sysvars", "destination": "/runtime/sysvars" },
-    { "source": "/developing/runtime-facilities/zk-token-proof", "destination": "/runtime/zk-token-proof" },
+    { "source": "/developing/runtime-facilities/:path*", "destination": "/runtime/:path*" },
     
     { "destination": "https://solana.com/docs/terminology", "source": "/terminology" },
     { "destination": "https://solana.com/docs/core/rent", "source": "/developing/intro/rent" },
