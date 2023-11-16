da67e080d02574a5737753831cac7536df28848d
Ryo Onodera <ryoqun@gmail.com>
2023-11-16 23:47:52 +0900

    save

--- scheduler-pool/src/lib.rs
+++ scheduler-pool/src/lib.rs
@@ -660,6 +660,7 @@ where
                         }
                         log_interval_counter += 1;
                     }
+                    () => { interval_log!("interval"); }
                 };
 
                 while !will_end_thread {
