a8863bd9fed3d3a27f6448e36f2e101d00caaa80
samkim-crypto <skim13@cs.stanford.edu>
2023-11-17 09:21:08 +0900

    [zk-token-proof] Feature gate transfer with fee related proofs (#34103)
    feature gate zk-token-proof transfer with fee

--- sdk/src/feature_set.rs
+++ sdk/src/feature_set.rs
@@ -728,6 +728,10 @@ pub mod disable_rent_fees_collection {
     solana_sdk::declare_id!("CJzY83ggJHqPGDq8VisV3U91jDJLuEaALZooBrXtnnLU");
 }
 
+pub mod enable_zk_transfer_with_fee {
+    solana_sdk::declare_id!("zkNLP7EQALfC1TYeB3biDU7akDckj8iPkvh9y2Mt2K3");
+}
+
 lazy_static! {
     /// Map of feature identifiers to user-visible description
     pub static ref FEATURE_NAMES: HashMap<Pubkey, &'static str> = [
@@ -905,6 +909,7 @@ lazy_static! {
         (update_hashes_per_tick6::id(), "Update desired hashes per tick to 10M"),
         (validate_fee_collector_account::id(), "validate fee collector account #33888"),
         (disable_rent_fees_collection::id(), "Disable rent fees collection #33945"),
+        (enable_zk_transfer_with_fee::id(), "enable Zk Token proof program transfer with fee"),
         /*************** ADD NEW FEATURES HERE ***************/
     ]
     .iter()
