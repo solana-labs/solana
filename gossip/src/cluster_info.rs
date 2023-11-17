ca6ab08555f9839216a86378f5aa74fe2df94daa
Ashwin Sekar <ashwin@solana.com>
2023-11-17 11:59:53 -0500

    gossip: process duplicate proofs for merkle root conflicts  (#34066)
    * gossip: process duplicate proofs for merkle root conflicts
    
    * pr comments + abi

--- gossip/src/cluster_info.rs
+++ gossip/src/cluster_info.rs
@@ -268,7 +268,7 @@ pub fn make_accounts_hashes_message(
 pub(crate) type Ping = ping_pong::Ping<[u8; GOSSIP_PING_TOKEN_SIZE]>;
 
 // TODO These messages should go through the gpu pipeline for spam filtering
-#[frozen_abi(digest = "HvA9JnnQrJnmkcGxrp8SmTB1b4iSyQ4VK2p6LpSBaoWR")]
+#[frozen_abi(digest = "FW5Ycg6GXPsY5Ek9b2VjP69toxRb95bSNQRRWLSdKv2Y")]
 #[derive(Serialize, Deserialize, Debug, AbiEnumVisitor, AbiExample)]
 #[allow(clippy::large_enum_variant)]
 pub(crate) enum Protocol {
