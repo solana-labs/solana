0b593956cd2310283703ad6f19efaf4fee03a306
Ryo Onodera <ryoqun@gmail.com>
2023-11-18 22:19:12 +0900

    save

--- scheduler-pool/src/lib.rs
+++ scheduler-pool/src/lib.rs
@@ -888,6 +888,8 @@ where
         let drop_main_loop = || {
             move || loop {
                 while let Ok(ee) = drop_receiver.try_recv() {
+                    use solana_runtime::transaction_priority_details::GetTransactionPriorityDetails;
+
                     let sig = ee.task.tx.0.signature().to_string();
 
                     solana_metrics::datapoint_info!(
