17b77b8e4163b1e134159f4183488012ca584940
Ryo Onodera <ryoqun@gmail.com>
2023-11-19 14:56:46 +0900

    save

--- scheduler-pool/src/lib.rs
+++ scheduler-pool/src/lib.rs
@@ -1379,8 +1379,7 @@ where
     }
 
     fn context(&self) -> SchedulingContext {
-        use std::ops::Deref;
-        self.thread_manager.read().unwrap().active_context().unwrap().deref().clone()
+        self.thread_manager.read().unwrap().active_context().unwrap()
     }
 
     fn schedule_execution(&self, transaction_with_index: SEA::TransactionWithIndex<'_>) {
