1ab7c53b207460dff550ef002353abdd7c21ccf7
Ryo Onodera <ryoqun@gmail.com>
2023-11-19 16:27:43 +0900

    save

--- scheduler-pool/src/lib.rs
+++ scheduler-pool/src/lib.rs
@@ -107,7 +107,12 @@ where
                     if let Ok(thread_manager) = watchdog_receiver.try_recv() {
                         weak_thread_managers.push(thread_manager);
                     }
-                    weak_thread_managers.retain(|thread_manager| thread_manager.upgrade().is_some());
+                    weak_thread_managers.retain(|thread_manager| {
+                        let Some(thread_manager) = thread_manager.upgrade() else {
+                            return false;
+                        }
+                        true
+                    });
                     std::thread::sleep(std::time::Duration::from_secs(1));
                 }
             }
