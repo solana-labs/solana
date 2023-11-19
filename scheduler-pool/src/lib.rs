0fe818dbbadc9eba372479041b2959c9c78df9a8
Ryo Onodera <ryoqun@gmail.com>
2023-11-19 16:44:46 +0900

    save

--- scheduler-pool/src/lib.rs
+++ scheduler-pool/src/lib.rs
@@ -101,7 +101,7 @@ where
     SEA: ScheduleExecutionArg,
 {
     fn new(thread_manager: Weak<RwLock<ThreadManager<TH, SEA>>>) -> Self {
-        Self { thread_manager, updated_at: SystemTime::now() }
+        Self { thread_manager, updated_at: SystemTime::now(), tick: 0 }
     }
 
     fn update_to_retain(&mut self) -> bool {
