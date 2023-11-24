795f30bbb86cbdf539f3154b90f5bb0d8cc36b0e
Ryo Onodera <ryoqun@gmail.com>
2023-11-24 15:28:08 +0900

    save

--- scheduler-pool/src/lib.rs
+++ scheduler-pool/src/lib.rs
@@ -399,7 +399,7 @@ impl Task {
     }
 
     fn lock_attempts_mut(&self) -> &mut Vec<LockAttempt> {
-        unsafe { &mut *self.lock_attempts.0.lock_attempts.get() }
+        unsafe { &mut *self.lock_attempts.0.get().lock_attempts }
     }
 
     pub fn currently_contended(&self) -> bool {
