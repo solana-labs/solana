193166ffd62c87047090af904f4026a2a6563039
Ryo Onodera <ryoqun@gmail.com>
2023-11-25 23:23:15 +0900

    save

--- scheduler-pool/src/lib.rs
+++ scheduler-pool/src/lib.rs
@@ -1549,7 +1549,7 @@ where
                 .chain(readonly_lock_iter)
                 .collect::<Vec<_>>();
             let uw = UniqueWeight::max_value() - index as UniqueWeight;
-            let task = Task::new_for_queue(uw, transaction.clone(), locks);
+            let task = TaskInner::new(uw, transaction.clone(), locks);
             self.ensure_thread_manager_started().send_task(task);
         });
     }
