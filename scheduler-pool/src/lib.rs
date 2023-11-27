baa85fed536a263a3e57dd1616a7d6d1ba32b726
Ryo Onodera <ryoqun@gmail.com>
2023-11-27 16:15:02 +0900

    save

--- scheduler-pool/src/lib.rs
+++ scheduler-pool/src/lib.rs
@@ -117,7 +117,7 @@ where
         let Some(thread_manager) = self.thread_manager.upgrade() else {
             return false;
         };
-        let Some(tid) = thread_manager.read().unwrap().tid_if_not_primary() else {
+        let Some(tid) = thread_manager.read().unwrap().active_tid_if_not_primary() else {
             self.tick = 0;
             self.updated_at = Instant::now();
             return true;
@@ -1227,7 +1227,7 @@ where
         }
     }
 
-    fn tid_if_not_primary(&self) -> Option<Tid> {
+    fn active_tid_if_not_primary(&self) -> Option<Tid> {
         if self.is_primary {
             self.scheduler_thread_and_tid.as_ref().map(|&(_, tid)| tid)
         } else {
