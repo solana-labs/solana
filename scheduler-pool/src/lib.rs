9a9d69ba39e25bd21359dbb735d7c641c4391c69
Ryo Onodera <ryoqun@gmail.com>
2023-11-27 14:16:42 +0900

    save

--- scheduler-pool/src/lib.rs
+++ scheduler-pool/src/lib.rs
@@ -140,7 +140,7 @@ where
             self.tick = current_tick;
             self.updated_at = Instant::now();
         } else {
-            let elapsed = self.updated_at.elapsed()
+            let elapsed = self.updated_at.elapsed(); 
             if elapsed > Duration::from_secs(60) {
                 const BITS_PER_HEX_DIGIT: usize = 4;
                 let mut thread_manager = thread_manager.write().unwrap();
