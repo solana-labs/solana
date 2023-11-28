bbf9ae4e8eab527cb979c5a8eba4a7f5c3deb9c3
Ryo Onodera <ryoqun@gmail.com>
2023-11-28 14:57:09 +0900

    save

--- scheduler-pool/src/lib.rs
+++ scheduler-pool/src/lib.rs
@@ -1450,7 +1450,7 @@ impl ScheduleStage {
         trace!("successful lock: (from_runnable: {})", from_runnable,);
 
         if !from_runnable {
-            for (usage, attempt) in usages.into_iter().zip(next_task.lock_attempts_mut().iter()) {
+            for (usage, attempt) in usages.into_iter().zip(next_task.lock_attempts_mut().iter_mut()) {
                 attempt.page_mut().current_usage = usage;
             }
             // as soon as next tack is succeeded in locking, trigger re-checks on read only
