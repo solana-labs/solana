a0462a21d2b8e62dc33288f195c052a4389015d8
Ryo Onodera <ryoqun@gmail.com>
2023-11-28 15:15:06 +0900

    save

--- scheduler-pool/src/lib.rs
+++ scheduler-pool/src/lib.rs
@@ -553,7 +553,11 @@ pub struct AddressBook {
 
 impl AddressBook {
     pub fn load(&self, address: Pubkey) -> Page {
-        self.book.entry(address).or_insert_default().clone()
+        self.book.entry(address).or_insert_with_defa(|| {
+            Page(ByAddress(PageRc::new(UnsafeCell::new(PageInner::new(
+                Usage::Unused,
+            )))))
+        }).clone()
     }
 
     pub fn page_count(&self) -> usize {
