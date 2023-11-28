ec140a7bc2723bba13494807c249732f3e62fdf3
Ryo Onodera <ryoqun@gmail.com>
2023-11-28 14:12:37 +0900

    save

--- scheduler-pool/src/lib.rs
+++ scheduler-pool/src/lib.rs
@@ -1369,14 +1369,14 @@ impl ScheduleStage {
         };
 
         if matches!(lock_status, LockStatus::Succeded(_)) {
-            let strictly_lockable = (
+            let strictly_lockable = 
                 page.heaviest_blocked_task().map(|heaviest_blocked_weight| heaviest_blocked_weight == *unique_weight).unwrap_or(true) || 
                 // this _read-only_ unique_weight is heavier than any of contened write locks.
                 attempt.requested_usage == RequestedUsage::Readonly && page
                     .heaviest_blocked_writing_task_weight()
                     .map(|existing_unique_weight| *unique_weight > existing_unique_weight)
                     .unwrap_or(true)
-            );
+            ;
 
             if !strictly_lockable {
                 lock_status = LockStatus::Failed 
