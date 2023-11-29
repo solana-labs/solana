f211c86e69ce3eed47c64fc598acf54e35503405
Lijun Wang <83639177+lijunwangs@users.noreply.github.com>
2023-11-29 11:31:11 -0800

    Support logging in geyser plugins (#34101)
    The change creates a new interface in the GeyserPlugin interface to allow the runtime to pass the logging configuration to the plugin.

--- geyser-plugin-manager/src/geyser_plugin_manager.rs
+++ geyser-plugin-manager/src/geyser_plugin_manager.rs
@@ -107,6 +107,8 @@ impl GeyserPluginManager {
             });
         }
 
+        setup_logger_for_plugin(&*new_plugin)?;
+
         // Call on_load and push plugin
         new_plugin
             .on_load(new_config_file, false)
@@ -193,6 +195,8 @@ impl GeyserPluginManager {
             });
         }
 
+        setup_logger_for_plugin(&*new_plugin)?;
+
         // Attempt to on_load with new plugin
         match new_plugin.on_load(new_parsed_config_file, true) {
             // On success, push plugin and library
@@ -226,6 +230,20 @@ impl GeyserPluginManager {
         drop(current_lib);
         info!("Unloaded plugin {name} at idx {idx}");
     }
+}
+
+// Initialize logging for the plugin
+fn setup_logger_for_plugin(new_plugin: &dyn GeyserPlugin) -> Result<(), jsonrpc_core::Error> {
+    new_plugin
+        .setup_logger(log::logger(), log::max_level())
+        .map_err(|setup_logger_err| jsonrpc_core::Error {
+            code: ErrorCode::InvalidRequest,
+            message: format!(
+                "setup_logger method of plugin {} failed: {setup_logger_err}",
+                new_plugin.name()
+            ),
+            data: None,
+        })
 }
 
 #[derive(Debug)]
