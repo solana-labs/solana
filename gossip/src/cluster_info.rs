dbd5159db8fa1dc47a90718e266850e357cb8a7a
Wen <crocoxu@gmail.com>
2023-11-29 10:05:16 -0800

    Forgot to update ABI digest.

--- gossip/src/cluster_info.rs
+++ gossip/src/cluster_info.rs
@@ -268,7 +268,7 @@ pub fn make_accounts_hashes_message(
 pub(crate) type Ping = ping_pong::Ping<[u8; GOSSIP_PING_TOKEN_SIZE]>;
 
 // TODO These messages should go through the gpu pipeline for spam filtering
-#[frozen_abi(digest = "GfkQsAuPD6gBkqvLpDvZMNxSCKmimMfTwKSuDeysxVGM")]
+#[frozen_abi(digest = "ogEqvffeEkPpojAaSiUbCv2HdJcdXDQ1ykgYyvKvLo2")]
 #[derive(Serialize, Deserialize, Debug, AbiEnumVisitor, AbiExample)]
 #[allow(clippy::large_enum_variant)]
 pub(crate) enum Protocol {
