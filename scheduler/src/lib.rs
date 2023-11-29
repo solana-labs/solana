a8e339f95d9750ba71e3fcf636e2add1abb20813
Ryo Onodera <ryoqun@gmail.com>
2023-11-29 21:47:31 +0900

    save

--- scheduler/src/lib.rs
+++ scheduler/src/lib.rs
@@ -501,7 +501,7 @@ impl SchedulingStateMachine {
                 continue;
             }
 
-            let heaviest_uncontended_now = unlock_attempt.page_mut().heaviest_still_blocked_task(unsafe { &mut Token::assume_on_the_scheduler_thread() });
+            let heaviest_uncontended_now = unlock_attempt.page_mut(token).heaviest_still_blocked_task(unsafe { &mut Token::assume_on_the_scheduler_thread() });
             if let Some((uncontended_task, _ru)) = heaviest_uncontended_now {
                 retryable_task_queue
                     .entry(uncontended_task.unique_weight)
