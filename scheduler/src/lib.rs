72be1ff6e7c931628f9b2a0736535c75ed9510df
Ryo Onodera <ryoqun@gmail.com>
2023-12-01 10:59:29 +0900

    save

--- scheduler/src/lib.rs
+++ scheduler/src/lib.rs
@@ -1,10 +1,1 @@
-#[derive(Debug, Clone, Copy)]
-pub enum SchedulingMode {
-    BlockVerification,
-}
-
-pub trait WithSchedulingMode {
-    fn mode(&self) -> SchedulingMode;
-}
-
 // This file will be populated with actual implementation later.
