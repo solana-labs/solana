d5eee0195049d65a47c7220bb2a9053b8766e333
behzad nouri <behzadnouri@gmail.com>
2023-12-06 22:47:46 +0000

    adds feature gated code to drop legacy shreds (#34328)

--- sdk/src/feature_set.rs
+++ sdk/src/feature_set.rs
@@ -732,6 +732,10 @@ pub mod enable_zk_transfer_with_fee {
     solana_sdk::declare_id!("zkNLP7EQALfC1TYeB3biDU7akDckj8iPkvh9y2Mt2K3");
 }
 
+pub mod drop_legacy_shreds {
+    solana_sdk::declare_id!("GV49KKQdBNaiv2pgqhS2Dy3GWYJGXMTVYbYkdk91orRy");
+}
+
 lazy_static! {
     /// Map of feature identifiers to user-visible description
     pub static ref FEATURE_NAMES: HashMap<Pubkey, &'static str> = [
@@ -910,6 +914,7 @@ lazy_static! {
         (validate_fee_collector_account::id(), "validate fee collector account #33888"),
         (disable_rent_fees_collection::id(), "Disable rent fees collection #33945"),
         (enable_zk_transfer_with_fee::id(), "enable Zk Token proof program transfer with fee"),
+        (drop_legacy_shreds::id(), "drops legacy shreds #34328"),
         /*************** ADD NEW FEATURES HERE ***************/
     ]
     .iter()
