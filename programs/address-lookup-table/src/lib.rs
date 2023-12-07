4eb46ad8eaecc6fc37379ba0fcab9f133ffd73a4
acheron <98934430+acheroncrypto@users.noreply.github.com>
2023-12-07 16:04:04 +0100

    Fix `solana-address-lookup-table-program` compilation (#34353)
    * Add  checks to decide which SDK crate to use
    
    * Make  module only available in non-program environments
    
    * Remove `solana-sdk` export and only export from `solana-program`
    
    (cherry picked from commit 46921b3135d47165c7ac9a93e85ba7776f7827d1)

--- programs/address-lookup-table/src/lib.rs
+++ programs/address-lookup-table/src/lib.rs
@@ -2,13 +2,14 @@
 #![cfg_attr(RUSTC_WITH_SPECIALIZATION, feature(specialization))]
 #![cfg_attr(RUSTC_NEEDS_PROC_MACRO_HYGIENE, feature(proc_macro_hygiene))]
 
+#[cfg(not(target_os = "solana"))]
 pub mod processor;
 
 #[deprecated(
     since = "1.17.0",
-    note = "Please use `solana_sdk::address_lookup_table` instead"
+    note = "Please use `solana_program::address_lookup_table` instead"
 )]
-pub use solana_sdk::address_lookup_table::{
+pub use solana_program::address_lookup_table::{
     error, instruction,
     program::{check_id, id, ID},
     state,
