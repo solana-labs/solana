19429a414bd649836302851e6cd8cab2947516c9
Jon C <me@jonc.dev>
2023-12-07 23:48:26 +0100

    compute-budget: Move `pack` under dev-context-only-utils

--- sdk/src/compute_budget.rs
+++ sdk/src/compute_budget.rs
@@ -63,7 +63,7 @@ impl ComputeBudgetInstruction {
 
     /// Serialize Instruction using borsh, this is only used in runtime::cost_model::tests but compilation
     /// can't be restricted as it's used across packages
-    // #[cfg(test)]
+    #[cfg(feature = "dev-context-only-utils")]
     pub fn pack(self) -> Result<Vec<u8>, borsh::io::Error> {
         borsh::to_vec(&self)
     }
