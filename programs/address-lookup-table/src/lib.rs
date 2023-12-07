2bce424490fcbdb4bfcd24ecd13ac3675de3422d
acheron <acheroncrypto@gmail.com>
2023-12-07 13:59:54 +0100

    Remove `solana-sdk` export and only export from `solana-program`

--- programs/address-lookup-table/src/lib.rs
+++ programs/address-lookup-table/src/lib.rs
@@ -5,18 +5,6 @@
 #[cfg(not(target_os = "solana"))]
 pub mod processor;
 
-#[cfg(not(target_os = "solana"))]
-#[deprecated(
-    since = "1.17.0",
-    note = "Please use `solana_sdk::address_lookup_table` instead"
-)]
-pub use solana_sdk::address_lookup_table::{
-    error, instruction,
-    program::{check_id, id, ID},
-    state,
-};
-
-#[cfg(target_os = "solana")]
 #[deprecated(
     since = "1.17.0",
     note = "Please use `solana_program::address_lookup_table` instead"
