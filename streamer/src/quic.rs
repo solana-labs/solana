67b8f0606cf7383c6453818a2cdf96ce09b64d44
Lijun Wang <83639177+lijunwangs@users.noreply.github.com>
2023-12-15 18:17:35 -0800

    Set worker threads to 4 for quic-server for ease of debug

--- streamer/src/quic.rs
+++ streamer/src/quic.rs
@@ -104,6 +104,7 @@ pub(crate) fn configure_server(
 fn rt() -> Runtime {
     tokio::runtime::Builder::new_multi_thread()
         .thread_name("quic-server")
+        .worker_threads(4)
         .enable_all()
         .build()
         .unwrap()
