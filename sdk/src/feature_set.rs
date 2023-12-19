515587be62dab47596f6d63e5c27aa78db08e965
Ashwin Sekar <ashwin@solana.com>
2023-12-19 00:05:13 +0000

    fix feature set conflict

--- sdk/src/feature_set.rs
+++ sdk/src/feature_set.rs
@@ -732,17 +732,10 @@ pub mod drop_legacy_shreds {
     solana_sdk::declare_id!("GV49KKQdBNaiv2pgqhS2Dy3GWYJGXMTVYbYkdk91orRy");
 }
 
-<<<<<<< HEAD
-=======
-pub mod allow_commission_decrease_at_any_time {
-    solana_sdk::declare_id!("decoMktMcnmiq6t3u7g5BfgcQu91nKZr6RvMYf9z1Jb");
-}
-
 pub mod consume_blockstore_duplicate_proofs {
     solana_sdk::declare_id!("6YsBCejwK96GZCkJ6mkZ4b68oP63z2PLoQmWjC7ggTqZ");
 }
 
->>>>>>> 4a8d27d921 (replay: feature flag consumption of duplicate proofs from blockstore (#34372))
 lazy_static! {
     /// Map of feature identifiers to user-visible description
     pub static ref FEATURE_NAMES: HashMap<Pubkey, &'static str> = [
@@ -921,11 +914,7 @@ lazy_static! {
         (validate_fee_collector_account::id(), "validate fee collector account #33888"),
         (enable_zk_transfer_with_fee::id(), "enable Zk Token proof program transfer with fee"),
         (drop_legacy_shreds::id(), "drops legacy shreds #34328"),
-<<<<<<< HEAD
-=======
-        (allow_commission_decrease_at_any_time::id(), "Allow commission decrease at any time in epoch #33843"),
         (consume_blockstore_duplicate_proofs::id(), "consume duplicate proofs from blockstore in consensus #34372")
->>>>>>> 4a8d27d921 (replay: feature flag consumption of duplicate proofs from blockstore (#34372))
         /*************** ADD NEW FEATURES HERE ***************/
     ]
     .iter()
