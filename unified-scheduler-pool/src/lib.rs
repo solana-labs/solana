310d044702700b55ec2c47b8d907974b4d416580
Ryo Onodera <ryoqun@gmail.com>
2023-12-20 14:50:53 +0900

    save

--- unified-scheduler-pool/src/lib.rs
+++ unified-scheduler-pool/src/lib.rs
@@ -682,7 +682,7 @@ where
                 self.schedulable_transaction_receiver.clone();
             let mut blocked_transaction_sessioned_sender =
                 blocked_transaction_sessioned_sender.clone();
-            let result_with_timings: ResultWithTimings = std::mem::replace(self.result_with_timings, (Ok(()), ExecuteTimings::default()));
+            let result_with_timings: ResultWithTimings = std::mem::replace(&mut self.result_with_timings, (Ok(()), ExecuteTimings::default()));
             drop_sender
                 .send(SessionedMessage::StartSession(result_with_timings))
                 .unwrap();
