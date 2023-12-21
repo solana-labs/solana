3a9d88578a2b87d7fbe917a6ca293e86c19f7de4
yihau <yihau.chen@icloud.com>
2023-12-22 03:37:14 +0800

    fix conflict

--- scripts/cargo-clippy.sh
+++ scripts/cargo-clippy.sh
@@ -15,52 +15,8 @@ set -o errexit
 
 here="$(dirname "$0")"
 
-<<<<<<< HEAD
-if [[ -z $cargo ]]; then
-  echo >&2 "Failed to find cargo. Mac readlink doesn't support -f. Consider switching
-  to gnu readlink with 'brew install coreutils' and then symlink greadlink as
-  /usr/local/bin/readlink."
-  exit 1
-fi
-
-# shellcheck source=ci/rust-version.sh
-source "$here/../ci/rust-version.sh"
-
-nightly_clippy_allows=(--allow=clippy::redundant_clone)
-
-# Use nightly clippy, as frozen-abi proc-macro generates a lot of code across
-# various crates in this whole monorepo (frozen-abi is enabled only under nightly
-# due to the use of unstable rust feature). Likewise, frozen-abi(-macro) crates'
-# unit tests are only compiled under nightly.
-# Similarly, nightly is desired to run clippy over all of bench files because
-# the bench itself isn't stabilized yet...
-#   ref: https://github.com/rust-lang/rust/issues/66287
-scripts/cargo-for-all-lock-files.sh -- \
-  "+${rust_nightly}" clippy \
-  --workspace --all-targets --features dummy-for-ci-check -- \
-  --deny=warnings \
-  --deny=clippy::default_trait_access \
-  --deny=clippy::integer_arithmetic \
-  --deny=clippy::used_underscore_binding \
-  "${nightly_clippy_allows[@]}"
-
-# temporarily run stable clippy as well to scan the codebase for
-# `redundant_clone`s, which is disabled as nightly clippy is buggy:
-#   https://github.com/solana-labs/solana/issues/31834
-#
-# can't use --all-targets:
-#   error[E0554]: `#![feature]` may not be used on the stable release channel
-scripts/cargo-for-all-lock-files.sh -- \
-  clippy \
-  --workspace --tests --bins --examples --features dummy-for-ci-check -- \
-  --deny=warnings \
-  --deny=clippy::default_trait_access \
-  --deny=clippy::integer_arithmetic \
-  --deny=clippy::used_underscore_binding
-=======
 # stable
 "$here/cargo-clippy-stable.sh"
 
 # nightly
 "$here/cargo-clippy-nightly.sh"
->>>>>>> 5247be17a9 (ci: split cargo-clippy into stable and nightly (#34566))
