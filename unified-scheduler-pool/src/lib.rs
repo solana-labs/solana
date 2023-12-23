bf6f67c8b365426119f03a066d7f3555851736cb
Ryo Onodera <ryoqun@gmail.com>
2023-12-23 14:00:13 +0900

    save

--- unified-scheduler-pool/src/lib.rs
+++ unified-scheduler-pool/src/lib.rs
@@ -714,7 +714,7 @@ where
         let (handled_idle_transaction_sender, handled_idle_transaction_receiver) =
             unbounded::<Box<ExecutedTask>>();
         let (drop_sender, drop_receiver) =
-            unbounded::<SessionedMessage<Box<ExecutedTask>, ResultWithTimings>>();
+            unbounded::<SessionedMessage<Box<ExecutedTask>, ()>>();
         let (drop_sender2, drop_receiver2) = unbounded::<ResultWithTimings>();
         let scheduler_id = self.scheduler_id;
         let mut slot = context.bank().slot();
