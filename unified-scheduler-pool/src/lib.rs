db4d5e5639ddb8e94768794e7aaa3cff7b20d763
Ryo Onodera <ryoqun@gmail.com>
2023-12-23 15:42:45 +0900

    save

--- unified-scheduler-pool/src/lib.rs
+++ unified-scheduler-pool/src/lib.rs
@@ -1215,7 +1215,7 @@ where
             let abort_detected = self.ensure_thread_manager_started(&self.context)
                 .send_task(task);
             if abort_detected {
-                let thread_manager = self.inner.thread_manager.write().unwrap();
+                let mut thread_manager = self.inner.thread_manager.write().unwrap();
                 thread_manager.stop_and_join_threads();
                 thread_manager.session_result_with_timings.lock().unwrap().as_ref().unwrap().0.clone()
             } else {
