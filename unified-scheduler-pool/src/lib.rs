dea7fd54617f854e7ead8b4e4e1b4bb41f340c70
Ryo Onodera <ryoqun@gmail.com>
2023-12-23 15:46:11 +0900

    save

--- unified-scheduler-pool/src/lib.rs
+++ unified-scheduler-pool/src/lib.rs
@@ -364,6 +364,7 @@ impl<SEA: ScheduleExecutionArg> TaskHandler<SEA> for DefaultTaskHandler {
             batch,
             transaction_indexes: vec![index],
         };
+        std::thread::sleep(Duration::from_secs(20));
 
         *result = execute_batch(
             &batch_with_indexes,
