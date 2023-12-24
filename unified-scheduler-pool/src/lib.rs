586db724e81c1172a94b3bf04f4bcacaedc215d2
Ryo Onodera <ryoqun@gmail.com>
2023-12-24 13:53:17 +0900

    save

--- unified-scheduler-pool/src/lib.rs
+++ unified-scheduler-pool/src/lib.rs
@@ -783,7 +783,7 @@ where
                             recv(handled_blocked_transaction_receiver) -> executed_task => {
                                 let executed_task = executed_task.unwrap();
                                 state_machine.deschedule_task(&executed_task.task);
-                                let r = executed_task.is_err().then(|a| executed_task.result_with_timings.clone());
+                                let r = executed_task.is_err().then(|| executed_task.result_with_timings.clone());
                                 executed_task_sender.send_buffered(SessionedMessage::Payload(executed_task)).unwrap();
                                 if Some(r) = r {
                                     log_scheduler!("T:aborted");
