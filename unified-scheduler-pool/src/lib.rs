6065e5e927273400f6d51804b8f2789a4fbc17ad
Ryo Onodera <ryoqun@gmail.com>
2023-12-25 10:35:46 +0900

    save

--- unified-scheduler-pool/src/lib.rs
+++ unified-scheduler-pool/src/lib.rs
@@ -726,7 +726,12 @@ where
             // race-free in ensure_thread_manager_started()...
             warn!("try_start_threads(): already started");
             return Ok(());
-        } else if self.session_result_with_timings.as_ref().map(|(result, _)| result.is_err()).unwrap_or(false) {
+        } else if self
+            .session_result_with_timings
+            .as_ref()
+            .map(|(result, _)| result.is_err())
+            .unwrap_or(false)
+        {
             warn!("try_start_threads(): skipping starting due to err; cleared session result");
             return self.reset_session_on_error();
         }
