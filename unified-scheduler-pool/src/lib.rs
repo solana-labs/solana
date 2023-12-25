0d5443254395aa607def623ceb1c8c65a1d780ba
Ryo Onodera <ryoqun@gmail.com>
2023-12-25 16:55:35 +0900

    save

--- unified-scheduler-pool/src/lib.rs
+++ unified-scheduler-pool/src/lib.rs
@@ -208,6 +208,9 @@ where
                 let mut thread_managers: Vec<WatchedThreadManager<S, TH, SEA>> = vec![];
 
                 'outer: loop {
+                    let Some(scheduler_pool) = scheduler_pool.upgrade() else {
+                        break;
+                    }
                     let mut schedulers = scheduler_pool.scheduler_inners.lock().unwrap();
                     let schedulers_len_pre_retain = schedulers.len();
                     schedulers.retain_mut(|scheduler| scheduler.retire_if_stale());
