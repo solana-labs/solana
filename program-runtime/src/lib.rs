1529fee5a27197d1a104258467a64e72d702a1bd
brooks <brooks@solana.com>
2024-01-10 08:17:18 -0500

    Removes AccountsDataMeter

--- program-runtime/src/lib.rs
+++ program-runtime/src/lib.rs
@@ -10,7 +10,6 @@ extern crate eager;
 extern crate solana_metrics;
 
 pub use solana_rbpf;
-pub mod accounts_data_meter;
 pub mod compute_budget;
 pub mod compute_budget_processor;
 pub mod invoke_context;
