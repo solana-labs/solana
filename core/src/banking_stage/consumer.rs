fa221e89dd030dc349a80990bbb73e7f4a0fdb0d
Andrew Fitzgerald <apfitzge@gmail.com>
2024-01-10 09:07:28 -0800

    MI selection validates fee-payer

--- core/src/banking_stage/consumer.rs
+++ core/src/banking_stage/consumer.rs
@@ -29,7 +29,9 @@ use {
     },
     solana_sdk::{
         clock::{Slot, FORWARD_TRANSACTIONS_TO_LEADER_AT_SLOT_OFFSET, MAX_PROCESSING_AGE},
-        feature_set, saturating_add_assign,
+        feature_set,
+        message::SanitizedMessage,
+        saturating_add_assign,
         timing::timestamp,
         transaction::{self, AddressLoader, SanitizedTransaction, TransactionError},
     },
@@ -402,46 +404,16 @@ impl Consumer {
         let check_results =
             bank.check_transactions(txs, &pre_results, MAX_PROCESSING_AGE, &mut error_counters);
 
-        let fee_check_results: Vec<_> = check_results
+        let check_results = check_results
             .into_iter()
             .zip(txs)
-            .map(|((result, _nonce), tx)| {
-                result?; // if there's already error do nothing
-                let fee_payer = tx.message().fee_payer();
-                let budget_limits =
-                    process_compute_budget_instructions(tx.message().program_instructions_iter())?
-                        .into();
-                let fee = bank.fee_structure.calculate_fee(
-                    tx.message(),
-                    bank.get_lamports_per_signature(),
-                    &budget_limits,
-                    bank.feature_set.is_active(
-                        &feature_set::include_loaded_accounts_data_size_in_fee_calculation::id(),
-                    ),
-                );
-                let (mut fee_payer_account, _slot) = bank
-                    .rc
-                    .accounts
-                    .accounts_db
-                    .load_with_fixed_root(&bank.ancestors, fee_payer)
-                    .ok_or(TransactionError::AccountNotFound)?;
-
-                validate_fee_payer(
-                    fee_payer,
-                    &mut fee_payer_account,
-                    0,
-                    &mut error_counters,
-                    bank.rent_collector(),
-                    fee,
-                )
-            })
-            .collect();
+            .map(|((result, _nonce), _tx)| result);
 
         let mut output = self.process_and_record_transactions_with_pre_results(
             bank,
             txs,
             chunk_offset,
-            fee_check_results.into_iter(),
+            check_results,
         );
 
         // Accumulate error counters from the initial checks into final results
@@ -735,6 +707,39 @@ impl Consumer {
             execute_and_commit_timings,
             error_counters,
         }
+    }
+
+    pub fn check_fee_payer_unlocked(
+        bank: &Bank,
+        message: &SanitizedMessage,
+        error_counters: &mut TransactionErrorMetrics,
+    ) -> Result<(), TransactionError> {
+        let fee_payer = message.fee_payer();
+        let budget_limits =
+            process_compute_budget_instructions(message.program_instructions_iter())?.into();
+        let fee = bank.fee_structure.calculate_fee(
+            message,
+            bank.get_lamports_per_signature(),
+            &budget_limits,
+            bank.feature_set.is_active(
+                &feature_set::include_loaded_accounts_data_size_in_fee_calculation::id(),
+            ),
+        );
+        let (mut fee_payer_account, _slot) = bank
+            .rc
+            .accounts
+            .accounts_db
+            .load_with_fixed_root(&bank.ancestors, fee_payer)
+            .ok_or(TransactionError::AccountNotFound)?;
+
+        validate_fee_payer(
+            fee_payer,
+            &mut fee_payer_account,
+            0,
+            error_counters,
+            bank.rent_collector(),
+            fee,
+        )
     }
 
     fn accumulate_execute_units_and_time(execute_timings: &ExecuteTimings) -> (u64, u64) {
