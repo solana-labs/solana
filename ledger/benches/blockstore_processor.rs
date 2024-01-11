d2dfb1440dbc6d97f2be49d4189ae02e286e9e61
Andrew Fitzgerald <apfitzge@gmail.com>
2024-01-11 09:19:00 -0800

    remove move and bank clone

--- ledger/benches/blockstore_processor.rs
+++ ledger/benches/blockstore_processor.rs
@@ -128,20 +128,17 @@ fn bench_execute_batch(
     let mut batches_iter = batches.into_iter();
 
     let mut timing = ExecuteTimings::default();
-    bencher.iter({
-        let bank = bank.clone();
-        move || {
-            let batch = batches_iter.next().unwrap();
-            execute_batch(
-                &batch,
-                &bank,
-                None,
-                None,
-                &mut timing,
-                None,
-                &prioritization_fee_cache,
-            )
-        }
+    bencher.iter(|| {
+        let batch = batches_iter.next().unwrap();
+        execute_batch(
+            &batch,
+            &bank,
+            None,
+            None,
+            &mut timing,
+            None,
+            &prioritization_fee_cache,
+        )
     });
 }
 
