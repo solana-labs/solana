166be2995ee76e51a343f04e41d348cfeb1cc8d9
Tyera <tyera@solana.com>
2024-01-10 21:20:15 -0700

    Bigtable: update google proto files and allow configuration of max_message_size (#34740)
    * Update proto files with tonic-build v0.9.2
    
    * Manually ignore invalid doc-tests
    
    * Add new ReadRowsRequest fields
    
    * Add LedgerStorageConfig::max_message_size and default value
    
    * Add BigtableConnection::max_message_size and use on client creation
    
    * Add max_message_size to RpcBigtableConfig and make const pub
    
    * Add solana-validator cli arg

--- validator/src/main.rs
+++ validator/src/main.rs
@@ -1230,6 +1230,7 @@ pub fn main() {
             timeout: value_t!(matches, "rpc_bigtable_timeout", u64)
                 .ok()
                 .map(Duration::from_secs),
+            max_message_size: value_t_or_exit!(matches, "rpc_bigtable_max_message_size", usize),
         })
     } else {
         None
