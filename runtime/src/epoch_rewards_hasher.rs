4385ed11b1a955642944a6b11353c09b1fba9187
Tyera <tyera@solana.com>
2024-01-11 23:15:55 -0700

    Populate partitioned-rewards PDA during calculation (#34624)
    * Add epoch_rewards_partition_data module
    
    * Rename variable
    
    * Pass hasher's parent_blockhash up to begin_partitioned_rewards
    
    * Populate epoch rewards partition data account in begin_partitioned_rewards
    
    * Fix method name

--- runtime/src/epoch_rewards_hasher.rs
+++ runtime/src/epoch_rewards_hasher.rs
@@ -46,7 +46,7 @@ pub(crate) fn hash_rewards_into_partitions(
     num_partitions: usize,
 ) -> Vec<StakeRewards> {
     let hasher = EpochRewardsHasher::new(num_partitions, parent_blockhash);
-    let mut result = vec![vec![]; num_partitions];
+    let mut rewards = vec![vec![]; num_partitions];
 
     for reward in stake_rewards {
         // clone here so the hasher's state is re-used on each call to `hash_address_to_partition`.
@@ -55,9 +55,9 @@ pub(crate) fn hash_rewards_into_partitions(
         let partition_index = hasher
             .clone()
             .hash_address_to_partition(&reward.stake_pubkey);
-        result[partition_index].push(reward);
+        rewards[partition_index].push(reward);
     }
-    result
+    rewards
 }
 
 #[cfg(test)]
