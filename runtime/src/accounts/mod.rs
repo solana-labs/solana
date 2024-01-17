04739f04bd91c11bc66bff95bce4946207d8bb93
Tao Zhu <tao@solana.com>
2024-01-17 02:10:57 +0000

    harden accounts tests

--- runtime/src/accounts/mod.rs
+++ runtime/src/accounts/mod.rs
@@ -624,7 +624,7 @@ mod tests {
             &RentCollector::default(),
             error_counters,
             &all_features_except(exclude_features),
-            &FeeStructure::default(),
+            &FeeStructure::new(lamports_per_signature as f64, 0.0, vec![(1_400_000, 0.0)])
         )
     }
 
