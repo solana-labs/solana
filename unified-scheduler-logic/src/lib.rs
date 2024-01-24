bd103865df740f46fe31e8e9acae30b35f23cc0b
Ryo Onodera <ryoqun@gmail.com>
2024-01-24 12:46:16 +0900

    Introduce primitive threading in unified scheduler (#34676)
    * Introduce primitive threading in unified scheduler
    
    * Make the internal struct ExecutedTask not pub
    
    * Improve wording a bit
    
    * Explain scheduler main loop's overhead sensitivity
    
    * Improve wording a bit
    
    * Define ChainedChannel{Sender, Receiver} wrappers
    
    * Clean up a bit
    
    * Use derivative to avoid manual Clone impl
    
    * Clarify comment
    
    * Remove extra whitespace in comment
    
    * Remove unneeded dyn trait for ChainedChannel
    
    * Remove the accumulator thread for now
    
    * Fix typo
    
    * Use unimplemented!() to convey intention better

--- unified-scheduler-logic/src/lib.rs
+++ unified-scheduler-logic/src/lib.rs
@@ -1,1 +1,20 @@
-// This file will be populated with actual implementation later.
+use solana_sdk::transaction::SanitizedTransaction;
+
+pub struct Task {
+    transaction: SanitizedTransaction,
+    index: usize,
+}
+
+impl Task {
+    pub fn create_task(transaction: SanitizedTransaction, index: usize) -> Self {
+        Task { transaction, index }
+    }
+
+    pub fn task_index(&self) -> usize {
+        self.index
+    }
+
+    pub fn transaction(&self) -> &SanitizedTransaction {
+        &self.transaction
+    }
+}
