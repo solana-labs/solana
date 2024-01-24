62e7ebd0cccb4b7c3a9597b76b04ec827667051f
Andrew Fitzgerald <apfitzge@gmail.com>
2024-01-24 15:30:32 -0800

    BlockProductionMethod::CentralScheduler as default (#34891)

--- core/src/validator.rs
+++ core/src/validator.rs
@@ -168,8 +168,8 @@ impl BlockVerificationMethod {
 #[derive(Clone, EnumString, EnumVariantNames, Default, IntoStaticStr, Display)]
 #[strum(serialize_all = "kebab-case")]
 pub enum BlockProductionMethod {
-    #[default]
     ThreadLocalMultiIterator,
+    #[default]
     CentralScheduler,
 }
 
