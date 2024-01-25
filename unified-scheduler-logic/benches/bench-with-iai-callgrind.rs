7185650e6c5da178855cb3657e3cfb9f07f2875a
Ryo Onodera <ryoqun@gmail.com>
2024-01-25 21:48:30 +0900

    save

--- unified-scheduler-logic/benches/bench-with-iai-callgrind.rs
+++ unified-scheduler-logic/benches/bench-with-iai-callgrind.rs
@@ -106,7 +106,10 @@ fn bench_schedule_task_conflicting(account_count: usize) {
 #[bench::three(3)]
 #[bench::normal(32)]
 #[bench::large(64)]
-#[bench::max(128)]
+#[bench::large2(128)]
+#[bench::large3(256)]
+#[bench::large4(1024)]
+#[bench::larget(2048)]
 fn bench_schedule_task_conflicting_hot(account_count: usize) {
     toggle_collect();
     let mut accounts = vec![];
