0af807014cc24832a349d40de983342d12d30a20
Drew Nutter <dnut@users.noreply.github.com>
2024-01-25 12:20:49 -0700

    fix(accounts-db): Switch secondary indices from dashmap to hashmap.
    Fixes OOM bug on high core machines where memory utilization can exceed 1 TB.
    
    program_id_index and spl_token_mint_index previously used DashMapSecondaryIndexEntry for index entries. This changes them to RwLockSecondaryIndexEntry to reduce memory and cpu usage.

--- accounts-db/src/accounts_index.rs
+++ accounts-db/src/accounts_index.rs
@@ -679,8 +679,8 @@ pub enum AccountsIndexScanResult {
 pub struct AccountsIndex<T: IndexValue, U: DiskIndexValue + From<T> + Into<T>> {
     pub account_maps: LockMapType<T, U>,
     pub bin_calculator: PubkeyBinCalculator24,
-    program_id_index: SecondaryIndex<DashMapSecondaryIndexEntry>,
-    spl_token_mint_index: SecondaryIndex<DashMapSecondaryIndexEntry>,
+    program_id_index: SecondaryIndex<RwLockSecondaryIndexEntry>,
+    spl_token_mint_index: SecondaryIndex<RwLockSecondaryIndexEntry>,
     spl_token_owner_index: SecondaryIndex<RwLockSecondaryIndexEntry>,
     pub roots_tracker: RwLock<RootsTracker>,
     ongoing_scan_roots: RwLock<BTreeMap<Slot, u64>>,
@@ -727,10 +727,10 @@ impl<T: IndexValue, U: DiskIndexValue + From<T> + Into<T>> AccountsIndex<T, U> {
         Self {
             account_maps,
             bin_calculator,
-            program_id_index: SecondaryIndex::<DashMapSecondaryIndexEntry>::new(
+            program_id_index: SecondaryIndex::<RwLockSecondaryIndexEntry>::new(
                 "program_id_index_stats",
             ),
-            spl_token_mint_index: SecondaryIndex::<DashMapSecondaryIndexEntry>::new(
+            spl_token_mint_index: SecondaryIndex::<RwLockSecondaryIndexEntry>::new(
                 "spl_token_mint_index_stats",
             ),
             spl_token_owner_index: SecondaryIndex::<RwLockSecondaryIndexEntry>::new(
@@ -2081,17 +2081,17 @@ pub mod tests {
         }
     }
 
-    fn create_dashmap_secondary_index_state() -> (usize, usize, AccountSecondaryIndexes) {
+    fn create_spl_token_mint_secondary_index_state() -> (usize, usize, AccountSecondaryIndexes) {
         {
             // Check that we're actually testing the correct variant
             let index = AccountsIndex::<bool, bool>::default_for_tests();
-            let _type_check = SecondaryIndexTypes::DashMap(&index.spl_token_mint_index);
+            let _type_check = SecondaryIndexTypes::RwLock(&index.spl_token_mint_index);
         }
 
         (0, PUBKEY_BYTES, spl_token_mint_index_enabled())
     }
 
-    fn create_rwlock_secondary_index_state() -> (usize, usize, AccountSecondaryIndexes) {
+    fn create_spl_token_owner_secondary_index_state() -> (usize, usize, AccountSecondaryIndexes) {
         {
             // Check that we're actually testing the correct variant
             let index = AccountsIndex::<bool, bool>::default_for_tests();
@@ -3449,8 +3449,8 @@ pub mod tests {
     }
 
     #[test]
-    fn test_purge_exact_dashmap_secondary_index() {
-        let (key_start, key_end, secondary_indexes) = create_dashmap_secondary_index_state();
+    fn test_purge_exact_spl_token_mint_secondary_index() {
+        let (key_start, key_end, secondary_indexes) = create_spl_token_mint_secondary_index_state();
         let index = AccountsIndex::<bool, bool>::default_for_tests();
         run_test_purge_exact_secondary_index(
             &index,
@@ -3462,8 +3462,8 @@ pub mod tests {
     }
 
     #[test]
-    fn test_purge_exact_rwlock_secondary_index() {
-        let (key_start, key_end, secondary_indexes) = create_rwlock_secondary_index_state();
+    fn test_purge_exact_spl_token_owner_secondary_index() {
+        let (key_start, key_end, secondary_indexes) = create_spl_token_owner_secondary_index_state();
         let index = AccountsIndex::<bool, bool>::default_for_tests();
         run_test_purge_exact_secondary_index(
             &index,
@@ -3667,8 +3667,8 @@ pub mod tests {
     }
 
     #[test]
-    fn test_dashmap_secondary_index() {
-        let (key_start, key_end, secondary_indexes) = create_dashmap_secondary_index_state();
+    fn test_spl_token_mint_secondary_index() {
+        let (key_start, key_end, secondary_indexes) = create_spl_token_mint_secondary_index_state();
         let index = AccountsIndex::<bool, bool>::default_for_tests();
         for token_id in [inline_spl_token::id(), inline_spl_token_2022::id()] {
             run_test_spl_token_secondary_indexes(
@@ -3683,8 +3683,8 @@ pub mod tests {
     }
 
     #[test]
-    fn test_rwlock_secondary_index() {
-        let (key_start, key_end, secondary_indexes) = create_rwlock_secondary_index_state();
+    fn test_spl_token_owner_secondary_index() {
+        let (key_start, key_end, secondary_indexes) = create_spl_token_owner_secondary_index_state();
         let index = AccountsIndex::<bool, bool>::default_for_tests();
         for token_id in [inline_spl_token::id(), inline_spl_token_2022::id()] {
             run_test_spl_token_secondary_indexes(
@@ -3788,8 +3788,8 @@ pub mod tests {
     }
 
     #[test]
-    fn test_dashmap_secondary_index_same_slot_and_forks() {
-        let (key_start, key_end, account_index) = create_dashmap_secondary_index_state();
+    fn test_spl_token_mint_secondary_index_same_slot_and_forks() {
+        let (key_start, key_end, account_index) = create_spl_token_mint_secondary_index_state();
         let index = AccountsIndex::<bool, bool>::default_for_tests();
         for token_id in [inline_spl_token::id(), inline_spl_token_2022::id()] {
             run_test_secondary_indexes_same_slot_and_forks(
@@ -3805,7 +3805,7 @@ pub mod tests {
 
     #[test]
     fn test_rwlock_secondary_index_same_slot_and_forks() {
-        let (key_start, key_end, account_index) = create_rwlock_secondary_index_state();
+        let (key_start, key_end, account_index) = create_spl_token_owner_secondary_index_state();
         let index = AccountsIndex::<bool, bool>::default_for_tests();
         for token_id in [inline_spl_token::id(), inline_spl_token_2022::id()] {
             run_test_secondary_indexes_same_slot_and_forks(
