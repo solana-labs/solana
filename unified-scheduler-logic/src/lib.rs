cc0f2f00710ffee491271d20bd3c0b4b3d083c26
Ryo Onodera <ryoqun@gmail.com>
2024-01-25 22:43:13 +0900

    save

--- unified-scheduler-logic/src/lib.rs
+++ unified-scheduler-logic/src/lib.rs
@@ -13,7 +13,7 @@ enum LockStatus {
     Succeded(Usage),
     Failed,
 }
-static_assertions::const_assert_eq!(std::mem::size_of::<LockStatus>(), 16);
+static_assertions::const_assert_eq!(std::mem::size_of::<LockStatus>(), 8);
 
 pub type Task = Arc<TaskInner>;
 static_assertions::const_assert_eq!(std::mem::size_of::<Task>(), 8);
