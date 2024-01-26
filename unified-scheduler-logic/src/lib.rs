57c7911ccc75a6384288688beb7d78028936d4a3
Ryo Onodera <ryoqun@gmail.com>
2024-01-26 15:45:59 +0900

    save

--- unified-scheduler-logic/src/lib.rs
+++ unified-scheduler-logic/src/lib.rs
@@ -309,7 +309,7 @@ impl SchedulingStateMachine {
 
     pub fn schedule_task(&mut self, task: Task) -> Option<Task> {
         // .contains_key() is too costly for assert!()
-        assert!(task.is_new());
+        assert!(task.is_new(&self.task_token));
         debug_assert!(!self.retryable_task_queue.contains_key(&task.unique_weight));
 
         self.total_task_count += 1;
