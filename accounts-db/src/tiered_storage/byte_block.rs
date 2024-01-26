4eb9aa7f7ffb0a7628e6b1af2047898b2226761f
Yueh-Hsuan Chiang <yueh-hsuan@solana.com>
2024-01-26 05:29:00 -0800

    [TieredStorage] Avoid AccountHash copy in AccountMetaOptionalFields

--- accounts-db/src/tiered_storage/byte_block.rs
+++ accounts-db/src/tiered_storage/byte_block.rs
@@ -96,7 +96,7 @@ impl ByteBlockWriter {
             size += self.write_pod(&rent_epoch)?;
         }
         if let Some(hash) = opt_fields.account_hash {
-            size += self.write_pod(&hash)?;
+            size += self.write_pod(hash)?;
         }
 
         debug_assert_eq!(size, opt_fields.size());
@@ -352,11 +352,12 @@ mod tests {
         let mut writer = ByteBlockWriter::new(format);
         let mut opt_fields_vec = vec![];
         let mut some_count = 0;
+        let acc_hash = AccountHash(Hash::new_unique());
 
         // prepare a vector of optional fields that contains all combinations
         // of Some and None.
         for rent_epoch in [None, Some(test_epoch)] {
-            for account_hash in [None, Some(AccountHash(Hash::new_unique()))] {
+            for account_hash in [None, Some(&acc_hash)] {
                 some_count += rent_epoch.iter().count() + account_hash.iter().count();
 
                 opt_fields_vec.push(AccountMetaOptionalFields {
@@ -397,7 +398,7 @@ mod tests {
             }
             if let Some(expected_hash) = opt_fields.account_hash {
                 let hash = read_pod::<AccountHash>(&decoded_buffer, offset).unwrap();
-                assert_eq!(hash, &expected_hash);
+                assert_eq!(hash, expected_hash);
                 verified_count += 1;
                 offset += std::mem::size_of::<AccountHash>();
             }
