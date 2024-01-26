083890928f3558b616850f863e46253c77db9b40
Tao Zhu <82401714+tao-stones@users.noreply.github.com>
2024-01-26 12:28:44 -0600

    refactor unused parameter (#34970)

--- sdk/src/fee.rs
+++ sdk/src/fee.rs
@@ -92,13 +92,12 @@ impl FeeStructure {
     pub fn calculate_fee(
         &self,
         message: &SanitizedMessage,
-        lamports_per_signature: u64,
+        _unused: u64,
         budget_limits: &FeeBudgetLimits,
         include_loaded_account_data_size_in_fee: bool,
     ) -> u64 {
         self.calculate_fee_details(
             message,
-            lamports_per_signature,
             budget_limits,
             include_loaded_account_data_size_in_fee,
         )
@@ -110,7 +109,6 @@ impl FeeStructure {
     pub fn calculate_fee_details(
         &self,
         message: &SanitizedMessage,
-        _lamports_per_signature: u64,
         budget_limits: &FeeBudgetLimits,
         include_loaded_account_data_size_in_fee: bool,
     ) -> FeeDetails {
