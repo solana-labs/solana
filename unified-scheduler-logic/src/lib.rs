e6e816138645e989389b1009bc67ebda6f99aa7c
Ryo Onodera <ryoqun@gmail.com>
2024-01-26 15:15:17 +0900

    save

--- unified-scheduler-logic/src/lib.rs
+++ unified-scheduler-logic/src/lib.rs
@@ -782,7 +782,7 @@ mod tests {
 
         let mut state_machine = SchedulingStateMachine::default();
         assert_matches!(state_machine.schedule_task(task1.clone()), Some(_));
-        assert_matches!(state_machine.schedule_task(task2.clone()), Some(_));
+        assert_matches!(state_machine.schedule_task(task2.clone()), None);
 
         state_machine.deschedule_task(&task1);
         state_machine.deschedule_task(&task2);
