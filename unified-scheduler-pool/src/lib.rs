064258d9ee34fcd8b5995f679859637b2d7275ab
Ryo Onodera <ryoqun@gmail.com>
2024-01-27 22:08:54 +0900

    save

--- unified-scheduler-pool/src/lib.rs
+++ unified-scheduler-pool/src/lib.rs
@@ -1420,7 +1420,7 @@ where
         transaction_with_index: SEA::TransactionWithIndex<'_>,
     ) -> Result<()> {
         transaction_with_index.with_transaction_and_index(|transaction, index| {
-            let task = SchedulingStateMachine::create_task(transaction.clone(), index, |pubkey| {
+            let task = SchedulingStateMachine::create_task(transaction.clone(), index, &mut |pubkey| {
                 self.inner.address_book.load(pubkey)
             });
             let abort_detected = self
