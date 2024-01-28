6c11b175d62fc45431f6a000297e54ea659c5c2b
Ryo Onodera <ryoqun@gmail.com>
2024-01-28 14:42:12 +0900

    save

--- unified-scheduler-logic/src/lib.rs
+++ unified-scheduler-logic/src/lib.rs
@@ -951,7 +951,7 @@ mod tests {
         assert_matches!(state_machine.schedule_task(task2.clone()), None);
         assert_matches!(state_machine.schedule_task(task3.clone()), None);
 
-        assert_eq!(state_machine.active_task_count(), 1);
+        assert_eq!(state_machine.active_task_count(), 3);
         assert_eq!(state_machine.handled_task_count(), 0);
         assert_eq!(state_machine.retryable_task_count(), 0);
         state_machine.deschedule_task(&task1);
