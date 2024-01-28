c8ed27bc632f8e65f97de1b55663b550bb351318
Ryo Onodera <ryoqun@gmail.com>
2024-01-28 14:25:00 +0900

    save

--- unified-scheduler-logic/src/lib.rs
+++ unified-scheduler-logic/src/lib.rs
@@ -356,7 +356,7 @@ impl PageInner {
     }
 
     fn heaviest_blocked_task(&mut self) -> Option<UniqueWeight> {
-        self.blocked_tasks.last_key().map(|entry| *entry.key())
+        self.blocked_tasks.last().map(|entry| *entry.key())
     }
 
     fn heaviest_still_blocked_task(
