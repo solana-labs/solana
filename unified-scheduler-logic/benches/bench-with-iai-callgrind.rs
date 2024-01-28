316df1b39368808972ce23033b06623b42f3899c
Ryo Onodera <ryoqun@gmail.com>
2024-01-28 14:09:58 +0900

    save

--- unified-scheduler-logic/benches/bench-with-iai-callgrind.rs
+++ unified-scheduler-logic/benches/bench-with-iai-callgrind.rs
@@ -96,6 +96,39 @@ fn bench_drop_task(account_count: usize) {
 }
 
 #[library_benchmark]
+#[bench::one(1)]
+fn bench_insert_task(account_count: usize) {
+    toggle_collect();
+    let mut accounts = vec![];
+    for _ in 0..account_count {
+        accounts.push(AccountMeta::new(Keypair::new().pubkey(), true));
+    }
+
+    let payer = Keypair::new();
+    let memo_ix = Instruction {
+        program_id: Pubkey::default(),
+        accounts,
+        data: vec![0x00],
+    };
+    let mut ixs = vec![];
+    for _ in 0..1 {
+        ixs.push(memo_ix.clone());
+    }
+    let msg = Message::new(&ixs, Some(&payer.pubkey()));
+    let txn = Transaction::new_unsigned(msg);
+    //panic!("{:?}", txn);
+    //assert_eq!(wire_txn.len(), 3);
+    let tx0 = SanitizedTransaction::from_transaction_for_tests(txn);
+    let task = SchedulingStateMachine::create_task(tx0, 0, &mut |_| Page::default());
+
+    let b = std::collections::BTreeMap::new();
+    toggle_collect();
+    b.inset(task.unique_weight, task);
+    toggle_collect();
+    drop(b);
+}
+
+#[library_benchmark]
 #[bench::min(0)]
 #[bench::one(1)]
 #[bench::two(2)]
@@ -331,7 +364,7 @@ fn bench_deschedule_task(account_count: usize) {
 
 library_benchmark_group!(
     name = bench_scheduling_state_machine;
-    benchmarks = bench_drop_task, bench_schedule_task, bench_schedule_task_conflicting, bench_schedule_task_conflicting_hot, bench_deschedule_task, bench_deschedule_task_conflicting, bench_schedule_retryable_task
+    benchmarks = bench_drop_task, bench_insert_task, bench_schedule_task, bench_schedule_task_conflicting, bench_schedule_task_conflicting_hot, bench_deschedule_task, bench_deschedule_task_conflicting, bench_schedule_retryable_task
 );
 
 main!(library_benchmark_groups = bench_scheduling_state_machine);
