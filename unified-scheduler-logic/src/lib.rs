5289837d64b81d7f2a9bd29f9afa9128da9edba5
Ryo Onodera <ryoqun@gmail.com>
2024-01-29 22:19:30 +0900

    save

--- unified-scheduler-logic/src/lib.rs
+++ unified-scheduler-logic/src/lib.rs
@@ -238,18 +238,18 @@ impl PageInner {
     fn heaviest_blocked_task(&self) -> Option<&Task> {
         let heaviest_writable = self
             .w_blocked_tasks
-            .last_key_value()
+            .first_key_value()
             .map(|(_, task)| (task, RequestedUsage::Writable));
         let heaviest_readonly = self
             .r_blocked_tasks
-            .last_key_value()
+            .first_key_value()
             .map(|(_, task)| (task, RequestedUsage::Readonly));
 
         match (heaviest_writable, heaviest_readonly) {
             (None, None) => None,
             (Some(a), None) | (None, Some(a)) => Some(a.0),
             (Some(a), Some(b)) => {
-                Some(std::cmp::max_by(a, b, |w, r| w.0.unique_weight.cmp(&r.0.unique_weight)).0)
+                Some(std::cmp::min_by(a, b, |w, r| w.0.unique_weight.cmp(&r.0.unique_weight)).0)
             }
         }
     }
