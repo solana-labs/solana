1c5d9cbd431ee6219e32404d53d709cbf055c6d4
Ryo Onodera <ryoqun@gmail.com>
2024-01-29 16:22:35 +0900

    save

--- unified-scheduler-logic/src/lib.rs
+++ unified-scheduler-logic/src/lib.rs
@@ -227,26 +227,29 @@ impl PageInner {
         assert!(removed_entry.is_some());
     }
 
-    #[inline(always)]
     fn heaviest_blocked_writing_task(&self) -> Option<&Task> {
         self.w_blocked_tasks.last_key_value().map(|(_k, v)| v)
     }
 
-    #[inline(always)]
     fn heaviest_blocked_readonly_task(&self) -> Option<&Task> {
         self.r_blocked_tasks.last_key_value().map(|(_k, v)| v)
     }
 
-    #[inline(always)]
     fn heaviest_blocked_task(&self) -> Option<&Task> {
-        let heaviest_writable = self.heaviest_blocked_writing_task();
-        let heaviest_readonly = self.heaviest_blocked_readonly_task();
+        let heaviest_writable = self
+            .w_blocked_tasks
+            .last_key_value()
+            .map(|(_, task)| (task, RequestedUsage::Writable));
+        let heaviest_readonly = self
+            .r_blocked_tasks
+            .last_key_value()
+            .map(|(_, task)| (task, RequestedUsage::Readonly));
 
         match (heaviest_writable, heaviest_readonly) {
             (None, None) => None,
-            (Some(a), None) | (None, Some(a)) => Some(a),
+            (Some(a), None) | (None, Some(a)) => Some(a.0),
             (Some(a), Some(b)) => {
-                Some(std::cmp::max_by(a, b, |w, r| w.unique_weight.cmp(&r.unique_weight)))
+                Some(std::cmp::max_by(a, b, |w, r| w.0.unique_weight.cmp(&r.0.unique_weight)).0)
             }
         }
     }
