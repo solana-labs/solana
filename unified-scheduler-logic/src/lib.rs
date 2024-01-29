4879e4c48fd51e62d6cfb06d07ecb4f5a1d0a39c
Ryo Onodera <ryoqun@gmail.com>
2024-01-29 16:32:48 +0900

    save

--- unified-scheduler-logic/src/lib.rs
+++ unified-scheduler-logic/src/lib.rs
@@ -137,7 +137,7 @@ impl TaskInner {
         &self.transaction
     }
 
-    fn lock_attempts_mut<'t>(&self, task_token: &'t mut TaskToken) -> impl Iterator<Item = &mut LockAttempt> {
+    fn lock_attempts_mut<'t>(&self, task_token: &'t mut TaskToken) -> impl Iterator<Item = &mut LockAttempt> + &'t {
         self.task_status.borrow_mut(task_token).writable_lock_attempts.iter_mut()
     }
 
