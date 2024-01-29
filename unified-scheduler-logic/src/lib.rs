6928122f890468f84222ec13e2747dd03e61d6f6
Ryo Onodera <ryoqun@gmail.com>
2024-01-29 13:48:09 +0900

    save

--- unified-scheduler-logic/src/lib.rs
+++ unified-scheduler-logic/src/lib.rs
@@ -306,7 +306,7 @@ impl PageInner {
     fn heaviest_still_blocked_task(
         &self,
     ) -> Option<&(Task, RequestedUsage)> {
-        self.blocked_tasks.last_entry().map(|entry| entry.value())
+        self.blocked_tasks.last_entry().map(|entry| entry.get())
     }
 }
 
