fb9fc5cd3ff23873e2ee400ac530fe1534b27841
Ryo Onodera <ryoqun@gmail.com>
2024-01-29 22:17:34 +0900

    save

--- unified-scheduler-logic/src/lib.rs
+++ unified-scheduler-logic/src/lib.rs
@@ -392,7 +392,7 @@ impl SchedulingStateMachine {
                 // page.
                 (page
                     .heaviest_blocked_task()
-                    .map(|existing_task| this_unique_weight >= existing_task.unique_weight)
+                    .map(|existing_task| this_unique_weight >= *existing_task.key())
                     .unwrap_or(true)) ||
                 // this _read-only_ unique_weight is heavier than any of contened write locks.
                 (matches!(requested_usage, RequestedUsage::Readonly) && page
