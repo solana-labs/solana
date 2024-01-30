f54d28319ba0c2d75691aaf30646f2ce9f62aab9
Ryo Onodera <ryoqun@gmail.com>
2024-01-30 16:24:21 +0900

    save

--- unified-scheduler-logic/src/lib.rs
+++ unified-scheduler-logic/src/lib.rs
@@ -251,7 +251,7 @@ impl PageInner {
     fn heavier_task<'a>(
         x: Option<(&'a UniqueWeight, &'a Task)>,
         y: Option<(&'a UniqueWeight, &'a Task)>,
-    ) -> Option<&'a Task> {
+    ) -> Option<(&'a UniqueWeight, &'a Task)> {
         cmp::max_by(x, y, |x, y| x.map(|x| x.0).cmp(&y.map(|y| y.0)))
     }
 }
