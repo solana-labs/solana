802bfdf2e28c1a2a2fd354cbe877099518d7d1d5
Ryo Onodera <ryoqun@gmail.com>
2024-01-30 09:28:48 +0900

    save

--- unified-scheduler-logic/src/lib.rs
+++ unified-scheduler-logic/src/lib.rs
@@ -240,7 +240,7 @@ impl PageInner {
             .w_blocked_tasks
             .first_key_value()
             .map(|(_, task)| (task, RequestedUsage::Writable))
-            .1
+            .map(|a| a.0)
             /*
         let heaviest_readonly = self
             .r_blocked_tasks
