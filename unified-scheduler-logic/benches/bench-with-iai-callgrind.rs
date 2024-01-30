35f0b425b7f7694b49352cb5d422c76ddaf39b5c
Ryo Onodera <ryoqun@gmail.com>
2024-01-30 15:20:54 +0900

    save

--- unified-scheduler-logic/benches/bench-with-iai-callgrind.rs
+++ unified-scheduler-logic/benches/bench-with-iai-callgrind.rs
@@ -58,7 +58,7 @@ fn bench_schedule_task(account_count: usize) {
     let task = SchedulingStateMachine::create_task(tx0, 0, &mut |_| Page::default());
     let mut scheduler = SchedulingStateMachine::default();
     toggle_collect();
-    let task = scheduler.do_schedule_task(task, |_task| {
+    let task = scheduler.schedule_task(task, |_task| {
         toggle_collect();
     }).unwrap();
     drop(task);
@@ -367,7 +367,7 @@ fn bench_schedule_retryable_task(account_count: usize) {
     assert_matches!(scheduler.schedule_task_for_test(task2), None);
     scheduler.deschedule_task(&task);
     toggle_collect();
-    let retried_task = scheduler.do_schedule_retryable_task(|_task| {
+    let retried_task = scheduler.schedule_retryable_task(|_task| {
         toggle_collect();
     }).unwrap();
     assert_eq!(task.transaction(), retried_task.transaction());
