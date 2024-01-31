b98239d28347b0d52668a7fad7840a6e1c01617e
Ryo Onodera <ryoqun@gmail.com>
2024-01-31 22:21:12 +0900

    save

--- unified-scheduler-logic/benches/bench-with-iai-callgrind.rs
+++ unified-scheduler-logic/benches/bench-with-iai-callgrind.rs
@@ -44,9 +44,9 @@ impl BL {
 
     #[inline(always)]
     pub fn alloc2(&mut self, bytes: usize, align: usize) -> *mut u8 {
-        let start = Self::align_allocation(self.cursor, align);
+        //let start = Self::align_allocation(self.cursor, align);
         //let new_cursor = unsafe { start.add(bytes) };
-        let new_cursor = unsafe { cursor.add(bytes + cursor.align_offset(align)) }
+        let new_cursor = unsafe { self.cursor.add(bytes + self.cursor.align_offset(align)) }
         if new_cursor <= self.limit {
             self.cursor = new_cursor;
             start
