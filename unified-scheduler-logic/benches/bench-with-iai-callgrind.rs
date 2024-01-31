74ab13acdc218462e48d4c05e6dbde256ecb4bfb
Ryo Onodera <ryoqun@gmail.com>
2024-01-31 23:09:01 +0900

    save

--- unified-scheduler-logic/benches/bench-with-iai-callgrind.rs
+++ unified-scheduler-logic/benches/bench-with-iai-callgrind.rs
@@ -30,7 +30,7 @@ impl BL {
     #[inline(always)]
     pub fn alloc2(&mut self, bytes: usize) -> *mut u8 {
         loop {
-            self.cursor = unsafe { ((((self.cursor.sub(bytes)) as usize) - 15) & !15) as _ };
+            self.cursor = unsafe { (((self.cursor.sub(bytes)) as usize) & !15) as _ };
             if self.cursor >= self.limit {
                 return self.cursor;
             } else if self.limit == usize::max_value() as _ {
