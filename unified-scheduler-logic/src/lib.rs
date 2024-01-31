5afef3ba4b3c412abf5607be3e4d6a7c00537e1a
Ryo Onodera <ryoqun@gmail.com>
2024-01-31 23:41:22 +0900

    save

--- unified-scheduler-logic/src/lib.rs
+++ unified-scheduler-logic/src/lib.rs
@@ -483,6 +483,7 @@ impl SchedulingStateMachine {
             let ret = on_success(&task);
             match task_source {
                 TaskSource::Retryable => {
+                    panic!();
                     for attempt in task.lock_attempts_mut(&mut self.task_token) {
                         let page = attempt.page_mut(&mut self.page_token);
                         page.usage = attempt.uncommited_usage;
