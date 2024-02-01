638a4a860b1a1067f9d0be6914a7da9e74877cf4
Ryo Onodera <ryoqun@gmail.com>
2024-02-01 14:08:11 +0900

    save

--- unified-scheduler-logic/src/lib.rs
+++ unified-scheduler-logic/src/lib.rs
@@ -183,10 +183,6 @@ impl LockAttempt {
     fn page_mut<'t>(&self, page_token: &'t mut PageToken) -> &'t mut PageInner {
         self.page.0.borrow_mut(page_token)
     }
-
-    fn page_mut_unchecked(&self) -> &mut PageInner {
-        self.page.0.borrow_mut_unchecked()
-    }
 }
 
 #[derive(Copy, Clone, Debug, Default)]
