913dd72a4fe6d25ed627980312c22d7f678462ad
Ryo Onodera <ryoqun@gmail.com>
2024-02-01 16:31:55 +0900

    save

--- unified-scheduler-logic/src/lib.rs
+++ unified-scheduler-logic/src/lib.rs
@@ -301,6 +301,7 @@ impl SchedulingStateMachine {
         task: Task,
         on_success: impl FnOnce(&Task) -> R,
     ) -> Option<R> {
+        self.last_unique_weight = task.unique_weight;
         let ret = self.try_lock_for_task(TaskSource::Runnable, task, on_success);
         self.total_task_count.increment_self();
         self.active_task_count.increment_self();
