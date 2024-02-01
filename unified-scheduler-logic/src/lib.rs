323cbbbb348c0a24dcc326225e01f905d1cd3ab5
Ryo Onodera <ryoqun@gmail.com>
2024-02-01 21:30:31 +0900

    save

--- unified-scheduler-logic/src/lib.rs
+++ unified-scheduler-logic/src/lib.rs
@@ -323,7 +323,7 @@ impl SchedulingStateMachine {
     pub fn schedule_retryable_task<R>(&mut self, on_success: impl FnOnce(&Task) -> R) -> Option<R> {
         self.reschedule_count.increment_self();
         self.rescheduled_task_count.increment_self();
-        self.unblocked_task_queue.pop_last().map(|(_, task)| {
+        self.unblocked_task_queue.pop_last().map(|(_, ref task)| {
             on_success(&task)
         })
     }
