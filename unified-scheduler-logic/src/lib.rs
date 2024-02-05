913bae79ce8c6ed1a98e2ac83fa899e0e94675e2
Ryo Onodera <ryoqun@gmail.com>
2024-02-05 16:46:37 +0900

    save

--- unified-scheduler-logic/src/lib.rs
+++ unified-scheduler-logic/src/lib.rs
@@ -482,12 +482,10 @@ impl SchedulingStateMachine {
     fn unlock_after_execution(&mut self, task: &Task) {
         for unlock_attempt in task.lock_attempts(&self.lock_attempt_token) {
             let page = unlock_attempt.page_mut(&mut self.page_token);
-            //let mut should_continue = true;
 
             let mut heaviest_uncontended_now = Self::unlock(page, unlock_attempt);
 
-            while heaviest_uncontended_now.is_some() {
-                let (uncontended_task, requested_usage) = heaviest_uncontended_now.unwrap();
+            while let Some((uncontended_task, requested_usage)) = heaviest_uncontended_now {
                     let new_count = uncontended_task
                         .blocked_lock_count_mut(&mut self.blocked_lock_count_token)
                         .decrement_self()
