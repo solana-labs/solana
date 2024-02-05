e35da8e03252cbc635c7e94dee8e087c74aaca2f
Ryo Onodera <ryoqun@gmail.com>
2024-02-05 15:19:16 +0900

    save

--- unified-scheduler-logic/src/lib.rs
+++ unified-scheduler-logic/src/lib.rs
@@ -146,11 +146,11 @@ impl TokenTrait<PageInner, PageInner> for Token<PageInner, PageInner> {
 
 impl TokenTrait<TaskStatus, Counter> for Token<TaskStatus, Counter> {
     fn partial_borrow<'a>(v: &'a TaskStatus) -> &'a Counter {
-        v
+        &v.unblocked_task_count
     }
 
     fn partial_borrow_mut<'a>(v: &'a mut TaskStatus) -> &'a mut Counter {
-        v
+        &mut v.unblocked_task_count
     }
 }
 
