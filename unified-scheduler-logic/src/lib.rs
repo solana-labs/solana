632b94b14d754fc86d28cf3ee5f3be25f7f2b1e0
Ryo Onodera <ryoqun@gmail.com>
2024-02-05 15:04:15 +0900

    save

--- unified-scheduler-logic/src/lib.rs
+++ unified-scheduler-logic/src/lib.rs
@@ -166,7 +166,7 @@ const_assert_eq!(mem::size_of::<PageToken>(), 0);
 type TaskToken = Token<TaskStatus, TaskStatus>;
 const_assert_eq!(mem::size_of::<TaskToken>(), 0);
 
-type LockAttemptToken = Token<TaskStatus, TaskStatus>;
+type LockAttemptToken = Token<TaskStatus, Vec<LockAttempt>>;
 const_assert_eq!(mem::size_of::<LockAttemptToken>(), 0);
 
 impl TaskStatus {
