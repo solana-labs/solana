0febd9781a6fff3cfdc94b37fd6c91ded8811df8
Ryo Onodera <ryoqun@gmail.com>
2024-02-05 23:26:39 +0900

    save

--- unified-scheduler-logic/src/lib.rs
+++ unified-scheduler-logic/src/lib.rs
@@ -314,7 +314,7 @@ impl SchedulingStateMachine {
     pub fn schedule_task(&mut self, task: Task) -> Option<Task> {
         self.total_task_count.increment_self();
         self.active_task_count.increment_self();
-        self.try_lock_for_task(task)
+        self.try_lock_for_task(task);
     }
 
     pub fn has_unblocked_task(&self) -> bool {
@@ -322,8 +322,10 @@ impl SchedulingStateMachine {
     }
 
     pub fn schedule_unblocked_task(&mut self) -> Option<Task> {
-        self.unblocked_task_count.increment_self();
-        self.unblocked_task_queue.pop_front()
+        self.unblocked_task_queue.pop_front().map(|task| {
+            self.unblocked_task_count.increment_self();
+            task
+        })
     }
 
     pub fn deschedule_task(&mut self, task: &Task) {
