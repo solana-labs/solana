f3e00add388fefe15ce247e1a0935d8ee75c8171
Ryo Onodera <ryoqun@gmail.com>
2024-02-05 14:35:36 +0900

    save

--- unified-scheduler-logic/src/lib.rs
+++ unified-scheduler-logic/src/lib.rs
@@ -124,7 +124,7 @@ const_assert_eq!(mem::size_of::<PageToken>(), 0);
 type TaskToken = Token<TaskStatus>;
 const_assert_eq!(mem::size_of::<TaskToken>(), 0);
 
-type LockAttemptToken = Token<LockAttempt>;
+type LockAttemptToken = Token<TaskStatus>;
 const_assert_eq!(mem::size_of::<LockAttemptToken>(), 0);
 
 impl TaskStatus {
@@ -153,7 +153,7 @@ impl TaskInner {
         &self.transaction
     }
 
-    fn lock_attempts_mut<'t>(&self, task_token: &'t mut LockAttemptToken) -> &'t mut Vec<LockAttempt> {
+    fn lock_attempts_mut<'t>(&self, task_token: &'t mut TaskToken) -> &'t mut Vec<LockAttempt> {
         &mut self.task_status.borrow_mut(task_token).lock_attempts
     }
 
@@ -164,7 +164,7 @@ impl TaskInner {
             .provisional_lock_count
     }
 
-    fn lock_attempts<'t>(&self, task_token: &'t LockAttemptToken) -> &'t Vec<LockAttempt> {
+    fn lock_attempts<'t>(&self, task_token: &'t TaskToken) -> &'t Vec<LockAttempt> {
         &self.task_status.borrow(task_token).lock_attempts
     }
 
