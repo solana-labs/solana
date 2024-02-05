f1e0df4cebf62f5aac28bba79b5a74ee076bf83d
Ryo Onodera <ryoqun@gmail.com>
2024-02-05 16:01:16 +0900

    save

--- unified-scheduler-logic/src/lib.rs
+++ unified-scheduler-logic/src/lib.rs
@@ -501,13 +501,13 @@ impl SchedulingStateMachine {
                     match Self::attempt_lock_address(page, requested_usage) {
                         LockStatus::Failed | LockStatus::Succeded(Usage::Unused) => unreachable!(),
                         LockStatus::Succeded(usage) => {
+                            page.usage = usage;
                             if matches!(usage, Usage::Readonly(_)) {
                                 heaviest_uncontended_now = page.heaviest_blocked_task();
                                 if matches!(heaviest_uncontended_now, Some((_, RequestedUsage::Readonly))) {
                                     should_continue = true;
                                 }
                             }
-                            page.usage = usage;
                         }
                     }
                 }
