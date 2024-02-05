548564ac407ec13b0672467fd12abbf77c0e9b92
Ryo Onodera <ryoqun@gmail.com>
2024-02-05 16:07:44 +0900

    save

--- unified-scheduler-logic/src/lib.rs
+++ unified-scheduler-logic/src/lib.rs
@@ -1011,7 +1011,7 @@ mod tests {
         page.0.borrow_mut(&mut state_machine.page_token).usage =
             Usage::Readonly(ShortCounter::one());
         SchedulingStateMachine::unlock(
-            &mut page.0,
+            &mut Arc::get_mut(page.0),
             &LockAttempt::new(page, RequestedUsage::Writable),
         );
     }
