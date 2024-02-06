58c6eb895eeb26aa539ba2062b5aa0911d6c13f2
Ryo Onodera <ryoqun@gmail.com>
2024-02-06 15:31:50 +0900

    save

--- unified-scheduler-pool/benches/lib.rs
+++ unified-scheduler-pool/benches/lib.rs
@@ -29,6 +29,7 @@ use solana_runtime::installed_scheduler_pool::DefaultScheduleExecutionArg;
 use solana_sdk::scheduling::SchedulingMode;
 use solana_unified_scheduler_logic::SchedulingStateMachine;
 use solana_unified_scheduler_pool::SpawnableScheduler;
+use solana_unified_scheduler_logic::Page;
 
 #[derive(Debug, Clone)]
 struct DummyTaskHandler;
