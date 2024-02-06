ad0e9a3089b1294f05c55816e85d40b3197393d8
Ryo Onodera <ryoqun@gmail.com>
2024-02-06 14:39:36 +0900

    save

--- unified-scheduler-logic/src/lib.rs
+++ unified-scheduler-logic/src/lib.rs
@@ -296,7 +296,7 @@ impl PageInner {
     }
 }
 
-const_assert_eq!(mem::size_of::<TokenCell<PageInner>>(), 40);
+//const_assert_eq!(mem::size_of::<TokenCell<PageInner>>(), 40);
 
 // very opaque wrapper type; no methods just with .clone() and ::default()
 #[derive(Debug, Clone, Default)]
