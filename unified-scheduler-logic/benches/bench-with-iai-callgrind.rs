b0d875561b71e57692c24493d9272979e9b3b47e
Ryo Onodera <ryoqun@gmail.com>
2024-02-14 14:19:12 +0900

    save

--- unified-scheduler-logic/benches/bench-with-iai-callgrind.rs
+++ unified-scheduler-logic/benches/bench-with-iai-callgrind.rs
@@ -1,8 +1,8 @@
 #![allow(clippy::arithmetic_side_effects)]
 #![cfg(not(target_os = "linux"))]
 
-#![cfg(target_os = "linux")]
-fn main() {}
+#[cfg(target_os = "linux")]
+pub fn main() {}
 
 
 #[global_allocator]
