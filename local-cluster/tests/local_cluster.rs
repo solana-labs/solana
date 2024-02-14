bfe9e566f7b0d6ea9f554909bfee427263ef6117
Ryo Onodera <ryoqun@gmail.com>
2024-02-14 13:59:15 +0900

    save

--- local-cluster/tests/local_cluster.rs
+++ local-cluster/tests/local_cluster.rs
@@ -4,6 +4,7 @@ use {
     crossbeam_channel::{unbounded, Receiver},
     gag::BufferRedirect,
     log::*,
+    rand::seq::SliceRandom,
     serial_test::serial,
     solana_accounts_db::{
         hardened_unpack::open_genesis_config, utils::create_accounts_run_and_snapshot_dirs,
@@ -99,7 +100,6 @@ use {
         time::{Duration, Instant},
     },
 };
- use rand::seq::SliceRandom;
 
 #[test]
 fn test_local_cluster_start_and_exit() {
@@ -5499,7 +5499,10 @@ fn test_randomly_mixed_block_verification_methods_between_bootstrap_and_not() {
     );
 
     // Randomly switch to use unified scheduler
-     let mut methods = [BlockVerificationMethod::UnifiedScheduler, BlockVerificationMethod::BlockstoreProcessor];
+    let mut methods = [
+        BlockVerificationMethod::UnifiedScheduler,
+        BlockVerificationMethod::BlockstoreProcessor,
+    ];
     methods.shuffle(&mut rand::thread_rng());
     for (validator_config, method) in config.validator_configs.iter_mut().zip(methods) {
         validator_config.block_verification_method = method;
