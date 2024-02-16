5d567b8022690b413ba7a21ded43bd8828cc4a41
Ryo Onodera <ryoqun@gmail.com>
2024-02-16 15:52:40 +0900

    save

--- unified-scheduler-logic/src/lib.rs
+++ unified-scheduler-logic/src/lib.rs
@@ -13,8 +13,13 @@
 //! its perf footprint is well understood.  It really focuses on its job: sorting transactions in
 //! executable order. 
 //!
-//! And its algorithm is very fast, real-time, and unbatched. The whole unified-scheduler
-//! architecture is designed from grounds up to support the fastest execution of this scheduling code. For that end, several unusual implementation tactics are employed.
+//! And its algorithm is very fast for high throughput, real-time for low latency. The whole
+//! unified-scheduler architecture is designed from grounds up to support the fastest execution of
+//! this scheduling code. For that end, unified scheduler preloads address-specific locking state
+//! data struture (called `Page`) for the scheduling code to offload the job. This reloading is
+//! done inside `create_task()`. Thus, task scheduling complexitiy can be reduced to several
+//! word-sized loads/stores basically, strictly proportional to the number of addresses in a given
+//! transaction.
 #[cfg(feature = "dev-context-only-utils")]
 use qualifier_attr::{field_qualifiers, qualifiers};
 use {
