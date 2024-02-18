7b63445f250016f39e33f353e5ceb78acdf32caa
Ryo Onodera <ryoqun@gmail.com>
2024-02-18 15:08:44 +0900

    save

--- unified-scheduler-logic/src/lib.rs
+++ unified-scheduler-logic/src/lib.rs
@@ -577,9 +577,10 @@ impl SchedulingStateMachine {
     /// Creates a new task with [`SanitizedTransaction`](SanitizedTransaction) with all of its
     /// corresponding [`Page`]s preloaded. 
     ///
-    /// Closure is used to delegate the (possibly multi-thread friendly) implementation details of
-    /// looking a page up with [`pubkey`](Pubkey). It's the caller's responsibility to ensure the
-    /// same instance is returned from the closure given a particular pubkey.
+    /// Closure (`page_loader`) is used to delegate the (possibly multi-thread friendly)
+    /// implementation details of looking a page up with [`pubkey`](Pubkey). It's the caller's
+    /// responsibility to ensure the same instance is returned from the closure given a particular
+    /// pubkey.
     pub fn create_task(
         transaction: SanitizedTransaction,
         index: usize,
