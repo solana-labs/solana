5078419f4226d6656ba9a6535235ce616bc47d12
Ryo Onodera <ryoqun@gmail.com>
2024-02-19 00:02:03 +0900

    save

--- unified-scheduler-logic/src/lib.rs
+++ unified-scheduler-logic/src/lib.rs
@@ -393,7 +393,7 @@ impl PageInner {
     fn pop_blocked_next_readonly_task(&self) -> Option<(&Task, RequestedUsage)> {
         self.blocked_next_task()
             .filter(|(_task, requested_usage)| matches!(requested_usage, RequestedUsage::Readonly))
-            .map(|_| self.pop_unblocked_next_task())
+            .and(|_| self.pop_unblocked_next_task())
     }
 }
 
