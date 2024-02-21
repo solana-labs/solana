1bfbbf798b3be61c433ce8cca21ce20a3fd4883e
Ryo Onodera <ryoqun@gmail.com>
2024-02-21 16:46:28 +0900

    save

--- unified-scheduler-logic/src/lib.rs
+++ unified-scheduler-logic/src/lib.rs
@@ -1099,6 +1099,13 @@ mod tests {
         // the above deschedule_task(task1) call should only unblock task2 and task3 because these
         // are read-locking. And shouldn't unblock task4 because it's write-locking
         assert_matches!(state_machine.schedule_unblocked_task(), None);
+        state_machine.deschedule_task(&task3);
+        assert_matches!(
+            state_machine
+                .schedule_unblocked_task()
+                .map(|t| t.task_index()),
+            None,
+        );
     }
 
     #[test]
