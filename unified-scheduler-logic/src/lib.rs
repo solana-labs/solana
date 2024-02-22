ab8cf306f6d616d6648db722a3b4dab110849c1f
Ryo Onodera <ryoqun@gmail.com>
2024-02-22 11:17:46 +0900

    save

--- unified-scheduler-logic/src/lib.rs
+++ unified-scheduler-logic/src/lib.rs
@@ -926,27 +926,6 @@ mod tests {
     }
 
     #[test]
-    fn test_schedule_unblocked_task3() {
-        let sanitized = simplest_transaction();
-        let address_loader = &mut create_address_loader(None);
-        let task1 = SchedulingStateMachine::create_task(sanitized.clone(), 3, address_loader);
-        let task2 = SchedulingStateMachine::create_task(sanitized.clone(), 4, address_loader);
-        let task3 = SchedulingStateMachine::create_task(sanitized.clone(), 5, address_loader);
-
-        let mut state_machine = unsafe {
-            SchedulingStateMachine::exclusively_initialize_current_thread_for_scheduling()
-        };
-        assert_matches!(state_machine.schedule_task(task1.clone()), Some(_));
-        assert_matches!(state_machine.schedule_task(task2.clone()), None);
-
-        assert_eq!(state_machine.unblocked_task_queue_count(), 0);
-        state_machine.deschedule_task(&task1);
-        assert_eq!(state_machine.unblocked_task_queue_count(), 1);
-
-        assert_matches!(state_machine.schedule_task(task3.clone()), None);
-    }
-
-    #[test]
     fn test_schedule_multiple_readonly_task() {
         let conflicting_address = Pubkey::new_unique();
         let sanitized1 = transaction_with_readonly_address(conflicting_address);
