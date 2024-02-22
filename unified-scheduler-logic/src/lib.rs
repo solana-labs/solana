ab66b6cceac10d43f44c1282f8aec316ad2dd7b6
Ryo Onodera <ryoqun@gmail.com>
2024-02-22 11:39:59 +0900

    save

--- unified-scheduler-logic/src/lib.rs
+++ unified-scheduler-logic/src/lib.rs
@@ -498,8 +498,8 @@ impl SchedulingStateMachine {
     #[must_use]
     pub fn schedule_task(&mut self, task: Task) -> Option<Task> {
         let new_task_index = task.task_index();
-        if let Some(ref old_task_index) = self.last_task_index.insert(new_task_index) {
-            assert!(new_task_index > old_task_index);
+        if let Some(old_task_index) = self.last_task_index.insert(new_task_index) {
+            assert!(new_task_index > *old_task_index);
         }
         self.total_task_count.increment_self();
         self.active_task_count.increment_self();
