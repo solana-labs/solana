679b26d7dc3b7cb22e26111f9ebcf64a0e0a33ba
Ryo Onodera <ryoqun@gmail.com>
2024-02-22 12:09:08 +0900

    save

--- unified-scheduler-logic/src/lib.rs
+++ unified-scheduler-logic/src/lib.rs
@@ -1324,7 +1324,7 @@ mod tests {
         let mut state_machine = unsafe {
             SchedulingStateMachine::exclusively_initialize_current_thread_for_scheduling()
         };
-        state_machine.schedule_task(task1.clone());
+        let _ = state_machine.schedule_task(task1.clone());
         state_machine.deschedule_task(&task2);
     }
 }
