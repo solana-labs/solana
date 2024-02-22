ffb457f9700b6aa6fa5e265b43f693aca592205f
Ryo Onodera <ryoqun@gmail.com>
2024-02-22 14:19:30 +0900

    save

--- validator/src/cli.rs
+++ validator/src/cli.rs
@@ -1534,10 +1534,9 @@ pub fn app<'a>(version: &'a str, default_args: &'a DefaultArgs) -> App<'a, 'a> {
         .arg(
             Arg::with_name("unified_scheduler_handler_threads")
                 .long("unified-scheduler-handler-threads")
-                .value_name("THREADS")
-                .takes_value(true)
-                .global(true)
                 .hidden(hidden_unless_forced())
+                .value_name("COUNT")
+                .takes_value(true)
                 .validator(|s| is_within_range(s, 1..))
                 .help(DefaultSchedulerPool::cli_message()),
         )
