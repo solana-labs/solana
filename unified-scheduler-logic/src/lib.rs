7ee39f1d75e3c31559417cffa76ba33d35e60ead
Ryo Onodera <ryoqun@gmail.com>
2024-02-22 11:50:26 +0900

    save

--- unified-scheduler-logic/src/lib.rs
+++ unified-scheduler-logic/src/lib.rs
@@ -519,9 +519,9 @@ impl SchedulingStateMachine {
     }
 
     pub fn deschedule_task(&mut self, task: &Task) {
-        let old_task_index = task.task_index();
-        if let Some(new_task_index) = self.last_task_index {
-            assert!(new_task_index >= old_task_index, "bad task index: {new_task_index} > {old_task_index}");
+        let largest_task_index = task.task_index();
+        if let Some(largest_task_index) = self.last_task_index {
+            assert!(existing_task_index <= largest_task_index, "bad task index: {existing_task_index} > {largest_task_index}");
         }
         self.active_task_count.decrement_self();
         self.handled_task_count.increment_self();
