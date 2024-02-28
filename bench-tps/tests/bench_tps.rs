17ed63ccca9dc786e2b38d5459309162e447b550
greg <greg.cusack@anza.xyz>
2024-02-28 23:33:54 +0000

    cargo-fmt

--- bench-tps/tests/bench_tps.rs
+++ bench-tps/tests/bench_tps.rs
@@ -1,23 +1,34 @@
 #![allow(clippy::arithmetic_side_effects)]
 
 use {
-    serial_test::serial, solana_bench_tps::{
+    serial_test::serial,
+    solana_bench_tps::{
         bench::{do_bench_tps, generate_and_fund_keypairs},
         cli::{Config, InstructionPaddingConfig},
         send_batch::generate_durable_nonce_accounts,
-    }, solana_client::{
+    },
+    solana_client::{
         connection_cache::ConnectionCache,
         tpu_client::{TpuClient, TpuClientConfig},
-    }, solana_core::validator::ValidatorConfig, solana_faucet::faucet::run_local_faucet, solana_local_cluster::{
+    },
+    solana_core::validator::ValidatorConfig,
+    solana_faucet::faucet::run_local_faucet,
+    solana_local_cluster::{
         local_cluster::{ClusterConfig, LocalCluster},
         validator_configs::make_identical_validator_configs,
-    }, solana_rpc::rpc::JsonRpcConfig, solana_rpc_client::rpc_client::RpcClient, solana_sdk::{
+    },
+    solana_rpc::rpc::JsonRpcConfig,
+    solana_rpc_client::rpc_client::RpcClient,
+    solana_sdk::{
         account::{Account, AccountSharedData},
         commitment_config::CommitmentConfig,
         fee_calculator::FeeRateGovernor,
         rent::Rent,
         signature::{Keypair, Signer},
-    }, solana_streamer::socket::SocketAddrSpace, solana_test_validator::TestValidatorGenesis, std::{sync::Arc, time::Duration}
+    },
+    solana_streamer::socket::SocketAddrSpace,
+    solana_test_validator::TestValidatorGenesis,
+    std::{sync::Arc, time::Duration},
 };
 
 fn program_account(program_data: &[u8]) -> AccountSharedData {
@@ -46,25 +57,22 @@ fn test_bench_tps_local_cluster(config: Config) {
     const NUM_NODES: usize = 1;
     let mut cluster_config = ClusterConfig {
         node_stakes: vec![999_990; NUM_NODES],
-            cluster_lamports: 200_000_000,
-            validator_configs: make_identical_validator_configs(
-                &ValidatorConfig {
-                    rpc_config: JsonRpcConfig {
-                        faucet_addr: Some(faucet_addr),
-                        ..JsonRpcConfig::default_for_test()
-                    },
-                    ..ValidatorConfig::default_for_test()
+        cluster_lamports: 200_000_000,
+        validator_configs: make_identical_validator_configs(
+            &ValidatorConfig {
+                rpc_config: JsonRpcConfig {
+                    faucet_addr: Some(faucet_addr),
+                    ..JsonRpcConfig::default_for_test()
                 },
-                NUM_NODES,
-            ),
-            native_instruction_processors,
-            additional_accounts,
-            ..ClusterConfig::default()
+                ..ValidatorConfig::default_for_test()
+            },
+            NUM_NODES,
+        ),
+        native_instruction_processors,
+        additional_accounts,
+        ..ClusterConfig::default()
     };
-    let cluster = LocalCluster::new(
-        &mut cluster_config,
-        SocketAddrSpace::Unspecified,
-    );
+    let cluster = LocalCluster::new(&mut cluster_config, SocketAddrSpace::Unspecified);
 
     cluster.transfer(&cluster.funding_keypair, &faucet_pubkey, 100_000_000);
 
@@ -75,9 +83,10 @@ fn test_bench_tps_local_cluster(config: Config) {
     let rpc_pubsub_url = format!("ws://{}/", cluster.entry_point_info.rpc_pubsub().unwrap());
 
     let client = match TpuClient::new_with_connection_cache(
-        Arc::new(RpcClient::new(
-            format!("http://{}", cluster.entry_point_info.rpc().unwrap()),
-        )),
+        Arc::new(RpcClient::new(format!(
+            "http://{}",
+            cluster.entry_point_info.rpc().unwrap()
+        ))),
         rpc_pubsub_url.as_str(),
         TpuClientConfig::default(),
         cache.clone(),
