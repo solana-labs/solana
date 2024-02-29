22d2fd962562ba48cf511f8c77ef907b097c0983
greg <greg.cusack@anza.xyz>
2024-02-29 00:30:47 +0000

    rm logging

--- local-cluster/src/local_cluster.rs
+++ local-cluster/src/local_cluster.rs
@@ -196,9 +196,6 @@ impl LocalCluster {
                     .collect()
             }
         };
-        if let Some(keys) = &config.validator_keys {
-            println!("validator key len 1: {}", keys.len());
-        }
 
         let vote_keys = {
             if let Some(ref node_vote_keys) = config.node_vote_keys {
@@ -315,9 +312,7 @@ impl LocalCluster {
         );
 
         validators.insert(leader_pubkey, cluster_leader);
-        if let Some(keys) = &config.validator_keys {
-            println!("validator key len 2: {}", keys.len());
-        }
+
         let mut cluster = Self {
             funding_keypair: mint_keypair,
             entry_point_info: leader_contact_info,
@@ -383,10 +378,6 @@ impl LocalCluster {
             socket_addr_space,
         )
         .unwrap();
-
-        if let Some(keys) = &config.validator_keys {
-            println!("validator key len 3: {}", keys.len());
-        }
 
         cluster
     }
