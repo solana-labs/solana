683e1071f7a8ef3d178fbca833f90567da18105e
samkim-crypto <skim13@cs.stanford.edu>
2024-03-15 09:09:41 +0900

    [zk-token-sdk] Implement `FromStr` for `ElGamalPubkey`, `ElGamalCiphertext`, and `AeCiphertext` (#130)
    * add `ParseError` in `zk-token-elgamal`
    
    * implement `FromStr` for `ElGamalPubkey` and `ElGamalCiphertext`
    
    * implement `FromStr` for `AeCiphertext`
    
    * fix target
    
    * cargo fmt
    
    * use constants for byte length check
    
    * make `FromStr` functions available on chain
    
    * use macros for the `FromStr` implementations
    
    * restrict `from_str` macro to `pub(crate)`
    
    * decode directly into array
    
    * cargo fmt
    
    * Apply suggestions from code review
    
    Co-authored-by: Jon C <me@jonc.dev>
    
    * remove unnecessary imports
    
    * remove the need for `ParseError` dependency
    
    ---------
    
    Co-authored-by: Jon C <me@jonc.dev>

--- zk-token-sdk/src/zk_token_elgamal/pod/elgamal.rs
+++ zk-token-sdk/src/zk_token_elgamal/pod/elgamal.rs
@@ -7,7 +7,7 @@ use {
 };
 use {
     crate::{
-        zk_token_elgamal::pod::{pedersen::PEDERSEN_COMMITMENT_LEN, Pod, Zeroable},
+        zk_token_elgamal::pod::{impl_from_str, pedersen::PEDERSEN_COMMITMENT_LEN, Pod, Zeroable},
         RISTRETTO_POINT_LEN,
     },
     base64::{prelude::BASE64_STANDARD, Engine},
@@ -17,11 +17,17 @@ use {
 /// Byte length of an ElGamal public key
 const ELGAMAL_PUBKEY_LEN: usize = RISTRETTO_POINT_LEN;
 
+/// Maximum length of a base64 encoded ElGamal public key
+const ELGAMAL_PUBKEY_MAX_BASE64_LEN: usize = 44;
+
 /// Byte length of a decrypt handle
 pub(crate) const DECRYPT_HANDLE_LEN: usize = RISTRETTO_POINT_LEN;
 
 /// Byte length of an ElGamal ciphertext
 const ELGAMAL_CIPHERTEXT_LEN: usize = PEDERSEN_COMMITMENT_LEN + DECRYPT_HANDLE_LEN;
+
+/// Maximum length of a base64 encoded ElGamal ciphertext
+const ELGAMAL_CIPHERTEXT_MAX_BASE64_LEN: usize = 88;
 
 /// The `ElGamalCiphertext` type as a `Pod`.
 #[derive(Clone, Copy, Pod, Zeroable, PartialEq, Eq)]
@@ -45,6 +51,12 @@ impl Default for ElGamalCiphertext {
         Self::zeroed()
     }
 }
+
+impl_from_str!(
+    TYPE = ElGamalCiphertext,
+    BYTES_LEN = ELGAMAL_CIPHERTEXT_LEN,
+    BASE64_LEN = ELGAMAL_CIPHERTEXT_MAX_BASE64_LEN
+);
 
 #[cfg(not(target_os = "solana"))]
 impl From<decoded::ElGamalCiphertext> for ElGamalCiphertext {
@@ -78,6 +90,12 @@ impl fmt::Display for ElGamalPubkey {
         write!(f, "{}", BASE64_STANDARD.encode(self.0))
     }
 }
+
+impl_from_str!(
+    TYPE = ElGamalPubkey,
+    BYTES_LEN = ELGAMAL_PUBKEY_LEN,
+    BASE64_LEN = ELGAMAL_PUBKEY_MAX_BASE64_LEN
+);
 
 #[cfg(not(target_os = "solana"))]
 impl From<decoded::ElGamalPubkey> for ElGamalPubkey {
@@ -127,5 +145,34 @@ impl TryFrom<DecryptHandle> for decoded::DecryptHandle {
 
     fn try_from(pod_handle: DecryptHandle) -> Result<Self, Self::Error> {
         Self::from_bytes(&pod_handle.0).ok_or(ElGamalError::CiphertextDeserialization)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use {super::*, crate::encryption::elgamal::ElGamalKeypair, std::str::FromStr};
+
+    #[test]
+    fn elgamal_pubkey_fromstr() {
+        let elgamal_keypair = ElGamalKeypair::new_rand();
+        let expected_elgamal_pubkey: ElGamalPubkey = (*elgamal_keypair.pubkey()).into();
+
+        let elgamal_pubkey_base64_str = format!("{}", expected_elgamal_pubkey);
+        let computed_elgamal_pubkey = ElGamalPubkey::from_str(&elgamal_pubkey_base64_str).unwrap();
+
+        assert_eq!(expected_elgamal_pubkey, computed_elgamal_pubkey);
+    }
+
+    #[test]
+    fn elgamal_ciphertext_fromstr() {
+        let elgamal_keypair = ElGamalKeypair::new_rand();
+        let expected_elgamal_ciphertext: ElGamalCiphertext =
+            elgamal_keypair.pubkey().encrypt(0_u64).into();
+
+        let elgamal_ciphertext_base64_str = format!("{}", expected_elgamal_ciphertext);
+        let computed_elgamal_ciphertext =
+            ElGamalCiphertext::from_str(&elgamal_ciphertext_base64_str).unwrap();
+
+        assert_eq!(expected_elgamal_ciphertext, computed_elgamal_ciphertext);
     }
 }
