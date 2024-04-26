3e3b16c0c6c9a2b349ac37c7cf83bc04afca3ffa
samkim-crypto <skim13@cs.stanford.edu>
2024-04-27 06:08:49 +0900

    [syscalls] Clean-up curve25519 msm length feature gate check (#1066)

--- programs/bpf_loader/src/syscalls/mod.rs
+++ programs/bpf_loader/src/syscalls/mod.rs
@@ -1140,14 +1140,8 @@ declare_builtin_function!(
             curve_syscall_traits::*, edwards, ristretto, scalar,
         };
 
-        let restrict_msm_length = invoke_context
-            .feature_set
-            .is_active(&feature_set::curve25519_restrict_msm_length::id());
-        #[allow(clippy::collapsible_if)]
-        if restrict_msm_length {
-            if points_len > 512 {
-                return Err(Box::new(SyscallError::InvalidLength));
-            }
+        if points_len > 512 {
+            return Err(Box::new(SyscallError::InvalidLength));
         }
 
         match curve_id {
