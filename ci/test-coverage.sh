186befeba95fcd43a9df625f233dc4fdec252c7d
Yihau Chen <yihau.chen@icloud.com>
2024-05-13 13:37:10 +0800

    ci: source based coverage (#1260)
    * ci: source based coverage
    
    * point to file explorer
    
    * fix sanity
    
    * break line
    
    * pass @ arg from ci/coverage to script/coverage
    
    * move source script to the top
    
    * direct to the summary page
    
    * explain why nightly
    
    * don't rug orignal rustflag
    
    * rearrange args order
    
    * bring back the trace level logging
    
    * remove _
    
    * fix log level
    
    * add reference to envs
    
    * put cov result into target/cov
    
    * move output to target/cov
    
    * XXX hack buildkite pipeline
    
    * XXX: test for no log
    
    * Revert "XXX hack buildkite pipeline"
    
    This reverts commit effb72e78fa1a46e6e7f9b8a3791e8cc8dc1903e.
    
    * Revert "XXX: test for no log"
    
    This reverts commit 975482de9b21129f9ddd1d19d39d8f60500445e5.
    
    * don't use trace log level temporarily
    
    * only search coverage report in the main folder

--- ci/test-coverage.sh
+++ ci/test-coverage.sh
@@ -1,6 +1,16 @@
 #!/usr/bin/env bash
+
 set -e
-cd "$(dirname "$0")/.."
+here=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
+
+# shellcheck source=ci/upload-ci-artifact.sh
+source "$here/upload-ci-artifact.sh"
+
+if [[ -z $CI ]]; then
+  echo "This script is used by CI environment. \
+Use \`scripts/coverage.sh\` directly if you only want to obtain the coverage report"
+  exit 1
+fi
 
 annotate() {
   ${BUILDKITE:-false} && {
@@ -8,29 +18,27 @@ annotate() {
   }
 }
 
-source ci/upload-ci-artifact.sh
-source scripts/ulimit-n.sh
+# run coverage for all
+SHORT_CI_COMMIT=${CI_COMMIT:0:9}
+COMMIT_HASH=$SHORT_CI_COMMIT "$here/../scripts/coverage.sh" "$@"
 
-scripts/coverage.sh "$@"
+# compress coverage reports
+HTML_REPORT_TAR_NAME="coverage.tar.gz"
+HTML_REPORT_TAR_PATH="$here/../target/cov/${SHORT_CI_COMMIT}/$HTML_REPORT_TAR_NAME"
+tar zcf "$HTML_REPORT_TAR_PATH" "$here/../target/cov/${SHORT_CI_COMMIT}/coverage"
 
-if [[ -z $CI ]]; then
-  exit
-fi
-
-report=coverage-"${CI_COMMIT:0:9}".tar.gz
-mv target/cov/report.tar.gz "$report"
-upload-ci-artifact "$report"
-
+# upload reports to buildkite
+upload-ci-artifact "$HTML_REPORT_TAR_PATH"
 annotate --style success --context lcov-report \
-  "lcov report: <a href=\"artifact://$report\">$report</a>"
+  "lcov report: <a href=\"artifact://$HTML_REPORT_TAR_NAME\">$HTML_REPORT_TAR_NAME</a>"
 
 echo "--- codecov.io report"
 if [[ -z "$CODECOV_TOKEN" ]]; then
   echo "^^^ +++"
   echo CODECOV_TOKEN undefined, codecov.io upload skipped
 else
-  codecov -t "${CODECOV_TOKEN}"
+  codecov -t "${CODECOV_TOKEN}" --dir "$here/../target/cov/${SHORT_CI_COMMIT}"
 
   annotate --style success --context codecov.io \
-    "CodeCov report: https://codecov.io/github/anza-xyz/agave/commit/${CI_COMMIT:0:9}"
+    "CodeCov report: https://codecov.io/github/anza-xyz/agave/commit/$CI_COMMIT"
 fi
--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -1,136 +1,81 @@
 #!/usr/bin/env bash
 #
-# Runs all tests and collects code coverage
+# Run tests and collect code coverage
 #
-# Warning: this process is a little slow
+# == Usage:
+#
+# Run all:
+#   $ ./script/coverage.sh
+#
+# Run for specific packages
+#   $ ./script/coverage.sh -p solana-account-decoder
+#   $ ./script/coverage.sh -p solana-account-decoder -p solana-accounts-db [-p ...]
+#
+# Custom folder name. (default: $(git rev-parse --short=9 HEAD))
+#   $ COMMIT_HASH=xxx ./script/coverage.sh -p solana-account-decoder
 #
 
-if ! command -v grcov; then
+set -e
+here=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
+
+# Check for grcov commands
+if ! command -v grcov >/dev/null 2>&1; then
   echo "Error: grcov not found.  Try |cargo install grcov|"
   exit 1
 fi
 
-if [[ ! "$(grcov --version)" =~ 0.8.[0-9] ]]; then
-  echo Error: Required grcov version not installed
+# Use nightly as we have some nightly-only tests (frozen-abi)
+# shellcheck source=ci/rust-version.sh
+source "$here/../ci/rust-version.sh" nightly
 
-  echo "Installed version: $(grcov --version)"
-  exit 1
+# get commit hash. it will be used to name output folder
+if [ -z "$COMMIT_HASH" ]; then
+  COMMIT_HASH=$(git rev-parse --short=9 HEAD)
 fi
 
-set -e
-cd "$(dirname "$0")/.."
-source ci/_
+# Clean up
+rm -rf "$here/../target/cov/$COMMIT_HASH"
+find "$here/.." -type f -name '*.prof*' -exec rm {} +
+find "$here/.." -type f -name '*lcov*' -exec rm {} +
 
-cargo="$(readlink -f "./cargo")"
-
-: "${CI_COMMIT:=local}"
-reportName="lcov-${CI_COMMIT:0:9}"
+# https://doc.rust-lang.org/rustc/instrument-coverage.html
+export RUSTFLAGS="-C instrument-coverage $RUSTFLAGS"
+export LLVM_PROFILE_FILE="$here/../target/cov/${COMMIT_HASH}/profraw/default-%p-%m.profraw"
 
 if [[ -z $1 ]]; then
-  packages=(--lib --all --exclude solana-local-cluster)
+  PACKAGES=(--lib --all --exclude solana-local-cluster)
 else
-  packages=("$@")
+  PACKAGES=("$@")
 fi
-
-coverageFlags=()
-coverageFlags+=(-Zprofile)               # Enable coverage
-coverageFlags+=("-Aincomplete_features") # Suppress warnings due to frozen abi, which is harmless for it
-if [[ $(uname) != Darwin ]]; then        # macOS skipped due to https://github.com/rust-lang/rust/issues/63047
-  coverageFlags+=("-Clink-dead-code")    # Dead code should appear red in the report
-fi
-coverageFlags+=("-Ccodegen-units=1")     # Disable code generation parallelism which is unsupported under -Zprofile (see [rustc issue #51705]).
-coverageFlags+=("-Cinline-threshold=0")  # Disable inlining, which complicates control flow.
-coverageFlags+=("-Copt-level=0")
-coverageFlags+=("-Coverflow-checks=off") # Disable overflow checks, which create unnecessary branches.
-
-export RUSTFLAGS="${coverageFlags[*]} $RUSTFLAGS"
-export CARGO_INCREMENTAL=0
-export RUST_BACKTRACE=1
-export RUST_MIN_STACK=8388608
-
-echo "--- remove old coverage results"
-if [[ -d target/cov ]]; then
-  find target/cov -type f -name '*.gcda' -delete
-fi
-rm -rf target/cov/$reportName
-mkdir -p target/cov
-
-# Mark the base time for a clean room dir
-touch target/cov/before-test
-
-# Force rebuild of possibly-cached proc macro crates and build.rs because
-# we always want stable coverage for them
-# Don't support odd file names in our repo ever
-if [[ -n $CI || -z $1 ]]; then
-  # shellcheck disable=SC2046
-  touch \
-    $(git ls-files :**/build.rs) \
-    $(git grep -l "proc-macro.*true" :**/Cargo.toml | sed 's|Cargo.toml|src/lib.rs|')
-fi
-
-#shellcheck source=ci/common/limit-threads.sh
-source ci/common/limit-threads.sh
-
-_ "$cargo" nightly test --jobs "$JOBS" --target-dir target/cov --no-run "${packages[@]}"
 
 TEST_ARGS=(
-  --nocapture
   --skip shred::merkle::test::test_make_shreds_from_data::
   --skip shred::merkle::test::test_make_shreds_from_data_rand::
   --skip shred::merkle::test::test_recover_merkle_shreds::
 )
 
-# most verbose log level (trace) is enabled for all solana code to make log!
-# macro code green always
-if RUST_LOG=solana=trace _ ci/intercept.sh "$cargo" nightly test --jobs "$JOBS" --target-dir target/cov "${packages[@]}" -- "${TEST_ARGS[@]}" ; then
-  test_status=0
-else
-  test_status=$?
-  echo "Failed: $test_status"
-  echo "^^^ +++"
-  if [[ -n $CI ]]; then
-    exit $test_status
-  fi
-fi
-touch target/cov/after-test
+cargo +"$rust_nightly" test --target-dir "$here/../target/cov" "${PACKAGES[@]}" -- "${TEST_ARGS[@]}"
 
+# Generate test reports
 echo "--- grcov"
-
-# Create a clean room dir only with updated gcda/gcno files for this run,
-# because our cached target dir is full of other builds' coverage files
-rm -rf target/cov/tmp
-mkdir -p target/cov/tmp
-
-# Can't use a simpler construct under the condition of SC2044 and bash 3
-# (macOS's default). See: https://github.com/koalaman/shellcheck/wiki/SC2044
-find target/cov -type f -name '*.gcda' -newer target/cov/before-test ! -newer target/cov/after-test -print0 |
-  (while IFS= read -r -d '' gcda_file; do
-    gcno_file="${gcda_file%.gcda}.gcno"
-    ln -sf "../../../$gcda_file" "target/cov/tmp/$(basename "$gcda_file")"
-    ln -sf "../../../$gcno_file" "target/cov/tmp/$(basename "$gcno_file")"
-  done)
-
-(
-  grcov_args=(
-    target/cov/tmp
-    --llvm
-    --ignore \*.cargo\*
-    --ignore \*build.rs
-    --ignore bench-tps\*
-    --ignore upload-perf\*
-    --ignore bench-streamer\*
-    --ignore local-cluster\*
-  )
-
-  set -x
-  grcov "${grcov_args[@]}" -t html -o target/cov/$reportName
-  grcov "${grcov_args[@]}" -t lcov -o target/cov/lcov.info
-
-  cd target/cov
-  tar zcf report.tar.gz $reportName
+grcov_common_args=(
+  "$here/.."
+  --source-dir "$here/.."
+  --binary-path "$here/../target/cov/debug"
+  --llvm
+  --ignore \*.cargo\*
+  --ignore \*build.rs
+  --ignore bench-tps\*
+  --ignore upload-perf\*
+  --ignore bench-streamer\*
+  --ignore local-cluster\*
+  --ignore-not-existing
 )
 
-ls -l target/cov/$reportName/index.html
-ln -sfT $reportName target/cov/LATEST
+grcov "${grcov_common_args[@]}" -t html -o "$here/../target/cov/${COMMIT_HASH}/coverage/html"
+echo "html: $here/../target/cov/${COMMIT_HASH}/coverage/html"
 
-exit $test_status
+grcov "${grcov_common_args[@]}" -t lcov -o "$here/../target/cov/${COMMIT_HASH}/coverage/lcov.info"
+echo "lcov: $here/../target/cov/${COMMIT_HASH}/coverage/lcov.info"
+
+ln -sfT "$here/../target/cov/${COMMIT_HASH}" "$here/../target/cov/LATEST"
