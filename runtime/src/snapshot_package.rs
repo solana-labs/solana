b128fca595d79b92c3a375e8af719309c166dba3
Brooks <brooks@anza.xyz>
2024-05-14 13:59:01 -0400

    Decorates AccountsPackage::default_for_tests() with DCOU (#1348)

--- runtime/src/snapshot_package.rs
+++ runtime/src/snapshot_package.rs
@@ -8,7 +8,7 @@ use {
     log::*,
     solana_accounts_db::{
         accounts::Accounts,
-        accounts_db::{AccountStorageEntry, AccountsDb},
+        accounts_db::AccountStorageEntry,
         accounts_hash::{AccountsHash, AccountsHashKind},
         epoch_accounts_hash::EpochAccountsHash,
     },
@@ -160,7 +160,9 @@ impl AccountsPackage {
 
     /// Create a new Accounts Package where basically every field is defaulted.
     /// Only use for tests; many of the fields are invalid!
+    #[cfg(feature = "dev-context-only-utils")]
     pub fn default_for_tests() -> Self {
+        use solana_accounts_db::accounts_db::AccountsDb;
         let accounts_db = AccountsDb::default_for_tests();
         let accounts = Accounts::new(Arc::new(accounts_db));
         Self {
