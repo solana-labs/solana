e1fc69934b6a8c53ace42fa0b1c13f16625d0363
Tao Zhu <82401714+tao-stones@users.noreply.github.com>
2024-05-15 10:12:55 -0400

    exit early if buffer is empty (#1351)
    * exit early if cache is empty
    
    * remove unnecessary warn

--- runtime/src/prioritization_fee_cache.rs
+++ runtime/src/prioritization_fee_cache.rs
@@ -293,6 +293,10 @@ impl PrioritizationFeeCache {
         bank_id: BankId,
         metrics: &PrioritizationFeeCacheMetrics,
     ) {
+        if unfinalized.is_empty() {
+            return;
+        }
+
         // prune cache by evicting write account entry from prioritization fee if its fee is less
         // or equal to block's minimum transaction fee, because they are irrelevant in calculating
         // block minimum fee.
@@ -303,7 +307,6 @@ impl PrioritizationFeeCache {
                     .split_off(&slot.checked_sub(MAX_UNFINALIZED_SLOTS).unwrap_or_default());
 
                 let Some(mut slot_prioritization_fee) = unfinalized.remove(&slot) else {
-                    warn!("Finalized slot {slot} not found");
                     return;
                 };
 
