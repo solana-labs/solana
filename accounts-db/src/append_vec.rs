12b6665a99ccf1ebff11c3919ae10312c39d7b11
Yihau Chen <yihau.chen@icloud.com>
2024-05-29 01:01:21 +0800

    test: remove some useless cases in the test (#1485)
    remove unused ub test

--- accounts-db/src/append_vec.rs
+++ accounts-db/src/append_vec.rs
@@ -1409,13 +1409,6 @@ pub mod tests {
                     // Depending on use, *executable_bool can be truthy or falsy due to direct memory manipulation
                     // assert_eq! thinks *executable_bool is equal to false but the if condition thinks it's not, contradictorily.
                     assert!(!*executable_bool);
-                    #[cfg(not(target_arch = "aarch64"))]
-                    {
-                        const FALSE: bool = false; // keep clippy happy
-                        if *executable_bool == FALSE {
-                            panic!("This didn't occur if this test passed.");
-                        }
-                    }
                     assert_eq!(*account.ref_executable_byte(), crafted_executable);
                 }
 
