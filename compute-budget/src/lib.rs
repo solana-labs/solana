20549d93420bff03b74c38dd1194ae479ba0824c
Joe C <joe.caulfield@anza.xyz>
2024-06-10 13:09:10 -0500

    compute budget: add frozen-abi build script (#1653)
    compute budget: frozen-abi build script

--- compute-budget/src/lib.rs
+++ compute-budget/src/lib.rs
@@ -1,4 +1,5 @@
 //! Solana compute budget types and default configurations.
+#![cfg_attr(RUSTC_WITH_SPECIALIZATION, feature(min_specialization))]
 
 pub mod compute_budget;
 pub mod compute_budget_processor;
