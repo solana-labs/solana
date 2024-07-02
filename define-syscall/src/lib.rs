8eef73da38f36b686a42ecd081da42820551b643
Kevin Heavey <heaveykevin2@gmail.com>
2024-07-03 01:38:10 +0400

    Extract define-syscall crate (#1827)
    * move define_syscall macro to its own crate
    
    * use solana-define-syscall in solana-poseidon
    
    * add missing dep to solana-program
    
    * update crate version to match workspace
    
    * macro hygiene fix
    
    * fix define_syscall call
    
    * re-export sys_hash in solana-program for backwards compatibility
    
    * convert tabs to spaces
    
    * put re-export behind #[cfg(target_feature = "static-syscalls")]

---
+++ define-syscall/src/lib.rs
@@ -0,0 +1,84 @@
+#[cfg(target_feature = "static-syscalls")]
+#[macro_export]
+macro_rules! define_syscall {
+    (fn $name:ident($($arg:ident: $typ:ty),*) -> $ret:ty) => {
+        #[inline]
+        pub unsafe fn $name($($arg: $typ),*) -> $ret {
+            // this enum is used to force the hash to be computed in a const context
+            #[repr(usize)]
+            enum Syscall {
+                Code = $crate::sys_hash(stringify!($name)),
+            }
+
+            let syscall: extern "C" fn($($arg: $typ),*) -> $ret = core::mem::transmute(Syscall::Code);
+            syscall($($arg),*)
+        }
+
+    };
+    (fn $name:ident($($arg:ident: $typ:ty),*)) => {
+        define_syscall!(fn $name($($arg: $typ),*) -> ());
+    }
+}
+
+#[cfg(not(target_feature = "static-syscalls"))]
+#[macro_export]
+macro_rules! define_syscall {
+    (fn $name:ident($($arg:ident: $typ:ty),*) -> $ret:ty) => {
+        extern "C" {
+            pub fn $name($($arg: $typ),*) -> $ret;
+        }
+    };
+    (fn $name:ident($($arg:ident: $typ:ty),*)) => {
+        define_syscall!(fn $name($($arg: $typ),*) -> ());
+    }
+}
+
+#[cfg(target_feature = "static-syscalls")]
+pub const fn sys_hash(name: &str) -> usize {
+    murmur3_32(name.as_bytes(), 0) as usize
+}
+
+#[cfg(target_feature = "static-syscalls")]
+const fn murmur3_32(buf: &[u8], seed: u32) -> u32 {
+    const fn pre_mix(buf: [u8; 4]) -> u32 {
+        u32::from_le_bytes(buf)
+            .wrapping_mul(0xcc9e2d51)
+            .rotate_left(15)
+            .wrapping_mul(0x1b873593)
+    }
+
+    let mut hash = seed;
+
+    let mut i = 0;
+    while i < buf.len() / 4 {
+        let buf = [buf[i * 4], buf[i * 4 + 1], buf[i * 4 + 2], buf[i * 4 + 3]];
+        hash ^= pre_mix(buf);
+        hash = hash.rotate_left(13);
+        hash = hash.wrapping_mul(5).wrapping_add(0xe6546b64);
+
+        i += 1;
+    }
+
+    match buf.len() % 4 {
+        0 => {}
+        1 => {
+            hash = hash ^ pre_mix([buf[i * 4], 0, 0, 0]);
+        }
+        2 => {
+            hash = hash ^ pre_mix([buf[i * 4], buf[i * 4 + 1], 0, 0]);
+        }
+        3 => {
+            hash = hash ^ pre_mix([buf[i * 4], buf[i * 4 + 1], buf[i * 4 + 2], 0]);
+        }
+        _ => { /* unreachable!() */ }
+    }
+
+    hash = hash ^ buf.len() as u32;
+    hash = hash ^ (hash.wrapping_shr(16));
+    hash = hash.wrapping_mul(0x85ebca6b);
+    hash = hash ^ (hash.wrapping_shr(13));
+    hash = hash.wrapping_mul(0xc2b2ae35);
+    hash = hash ^ (hash.wrapping_shr(16));
+
+    hash
+}
--- poseidon/src/lib.rs
+++ poseidon/src/lib.rs
@@ -170,15 +170,7 @@ impl PoseidonHash {
 }
 
 #[cfg(target_os = "solana")]
-extern "C" {
-    pub fn sol_poseidon(
-        parameters: u64,
-        endianness: u64,
-        vals: *const u8,
-        val_len: u64,
-        hash_result: *mut u8,
-    ) -> u64;
-}
+solana_define_syscall::define_syscall!(fn sol_poseidon(parameters: u64, endianness: u64, vals: *const u8, val_len: u64, hash_result: *mut u8) -> u64);
 
 /// Return a Poseidon hash for the given data with the given elliptic curve and
 /// endianness.
