c848645aba522fbe64dec529c95656efb0c3ce64
Tyera <tyera@anza.xyz>
2024-07-03 14:06:54 -0600

    Add suggestion to deprecation message (#1993)

--- rpc-client/src/rpc_client.rs
+++ rpc-client/src/rpc_client.rs
@@ -1776,7 +1776,9 @@ impl RpcClient {
     /// ```
     #[deprecated(
         since = "1.18.18",
-        note = "Do not use; getStakeActivation is deprecated on the JSON-RPC server"
+        note = "Do not use; getStakeActivation is deprecated on the JSON-RPC server. Please use \
+                the stake account and StakeHistory sysvar to call \
+                `Delegation::stake_activating_and_deactivating()` instead"
     )]
     #[allow(deprecated)]
     pub fn get_stake_activation(
