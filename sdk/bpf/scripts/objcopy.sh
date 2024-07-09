9cf5939965e42b3f18121c871f347ee686589b40
Lucas Ste <38472950+LucasSte@users.noreply.github.com>
2024-07-09 16:25:25 -0300

    Remove `cargo-build-bpf` and `cargo-test-bpf` (#2040)

--- sdk/bpf/scripts/objcopy.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-bpf_sdk=$(cd "$(dirname "$0")/.." && pwd)
-# shellcheck source=sdk/bpf/env.sh
-source "$bpf_sdk"/env.sh
-exec "$bpf_sdk"/dependencies/bpf-tools/llvm/bin/llvm-objcopy "$@"
