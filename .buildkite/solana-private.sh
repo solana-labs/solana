0d191c267e4d94db780d349ccf0f6fff080f3ac1
Yihau Chen <yihau.chen@icloud.com>
2024-07-19 21:14:11 +0800

    ci: update the private pipeline script (#2163)
    * clean up
    
    * update private pipline's permission

--- .buildkite/solana-private.sh
+++ .buildkite/solana-private.sh
@@ -8,10 +8,8 @@
 #
 
 set -e
-# NAME=$(buildkite-agent meta-data get name)
 cd "$(dirname "$0")"/..
 source ci/_
-sudo chmod 0777 ci/buildkite-solana-private.sh
 
 _ ci/buildkite-solana-private.sh pipeline.yml
 echo +++ pipeline
