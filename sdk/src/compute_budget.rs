0a0bb75db4681ad770bc1213c52137b943d18449
Kevin Heavey <heaveykevin2@gmail.com>
2024-07-22 23:25:27 +0400

    sdk: fix conditional borsh usage in compute_budget.rs (#2221)

--- sdk/src/compute_budget.rs
+++ sdk/src/compute_budget.rs
@@ -12,7 +12,8 @@ crate::declare_id!("ComputeBudget111111111111111111111111111111");
 
 /// Compute Budget Instructions
 #[cfg_attr(feature = "frozen-abi", derive(AbiExample, AbiEnumVisitor))]
-#[derive(BorshDeserialize, BorshSerialize, Clone, Debug, Deserialize, PartialEq, Eq, Serialize)]
+#[cfg_attr(feature = "borsh", derive(BorshSerialize, BorshDeserialize))]
+#[derive(Clone, Debug, Deserialize, PartialEq, Eq, Serialize)]
 pub enum ComputeBudgetInstruction {
     Unused, // deprecated variant, reserved value.
     /// Request a specific transaction-wide program heap region size in bytes.
