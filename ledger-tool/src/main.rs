1e3cea6082139867d139bffb291221c1a13c0aff
Brooks <brooks@anza.xyz>
2024-07-24 20:53:42 -0400

    Waits for initial startup verification to complete in ledger-tool create-snapshot (#2279)

--- ledger-tool/src/main.rs
+++ ledger-tool/src/main.rs
@@ -1887,11 +1887,6 @@ fn main() {
                         process_options,
                         None,
                     );
-                    // Snapshot creation will implicitly perform AccountsDb
-                    // flush and clean operations. These operations cannot be
-                    // run concurrently, so ensure ABS is stopped to avoid that
-                    // possibility.
-                    accounts_background_service.join().unwrap();
 
                     let mut bank = bank_forks
                         .read()
@@ -1901,6 +1896,24 @@ fn main() {
                             eprintln!("Error: Slot {snapshot_slot} is not available");
                             exit(1);
                         });
+
+                    // Snapshot creation will implicitly perform AccountsDb
+                    // flush and clean operations. These operations cannot be
+                    // run concurrently, so ensure ABS is stopped to avoid that
+                    // possibility.
+                    accounts_background_service.join().unwrap();
+
+                    // Similar to waiting for ABS to stop, we also wait for the initial startup
+                    // verification to complete. The startup verification runs in the background
+                    // and verifies the snapshot's accounts hashes are correct. We only want a
+                    // single accounts hash calculation to run at a time, and since snapshot
+                    // creation below will calculate the accounts hash, we wait for the startup
+                    // verification to complete before proceeding.
+                    bank.rc
+                        .accounts
+                        .accounts_db
+                        .verify_accounts_hash_in_bg
+                        .wait_for_complete();
 
                     let child_bank_required = rent_burn_percentage.is_ok()
                         || hashes_per_tick.is_some()
