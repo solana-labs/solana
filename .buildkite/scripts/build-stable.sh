01782ccfcbc1722eaccb5094a73cedcc37ce2571
Yihau Chen <yihau.chen@icloud.com>
2024-08-15 11:38:01 +0800

    ci: split local-cluster test into 10 jobs (#2576)
    * ci: split local-cluster test into 10 jobs
    
    * ci: reduce timout miniutes for local-cluster

--- .buildkite/scripts/build-stable.sh
+++ .buildkite/scripts/build-stable.sh
@@ -26,9 +26,9 @@ local_cluster_partitions=$(
 {
   "name": "local-cluster",
   "command": "ci/docker-run-default-image.sh ci/stable/run-local-cluster-partially.sh",
-  "timeout_in_minutes": 30,
+  "timeout_in_minutes": 15,
   "agent": "$agent",
-  "parallelism": 5,
+  "parallelism": 10,
   "retry": 3
 }
 EOF
