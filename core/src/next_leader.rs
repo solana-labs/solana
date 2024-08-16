f4e2fa9fe293f97df777503e9fd426ed5ac760d9
Justin Starry <justin@anza.xyz>
2024-08-16 10:56:33 +0800

    fix: send votes to the immediate next leader (#2607)
    * fix: send votes to the immediate next leader
    
    * feedback

--- core/src/next_leader.rs
+++ core/src/next_leader.rs
@@ -1,9 +1,37 @@
 use {
+    itertools::Itertools,
     solana_gossip::{cluster_info::ClusterInfo, contact_info::ContactInfo},
     solana_poh::poh_recorder::PohRecorder,
     solana_sdk::{clock::FORWARD_TRANSACTIONS_TO_LEADER_AT_SLOT_OFFSET, pubkey::Pubkey},
     std::{net::SocketAddr, sync::RwLock},
 };
+
+/// Returns a list of tpu vote sockets for the leaders of the next N fanout
+/// slots. Leaders and sockets are deduped.
+pub(crate) fn upcoming_leader_tpu_vote_sockets(
+    cluster_info: &ClusterInfo,
+    poh_recorder: &RwLock<PohRecorder>,
+    fanout_slots: u64,
+) -> Vec<SocketAddr> {
+    let upcoming_leaders = {
+        let poh_recorder = poh_recorder.read().unwrap();
+        (1..=fanout_slots)
+            .filter_map(|n_slots| poh_recorder.leader_after_n_slots(n_slots))
+            .collect_vec()
+    };
+
+    upcoming_leaders
+        .into_iter()
+        .dedup()
+        .filter_map(|leader_pubkey| {
+            cluster_info
+                .lookup_contact_info(&leader_pubkey, ContactInfo::tpu_vote)?
+                .ok()
+        })
+        // dedup again since leaders could potentially share the same tpu vote socket
+        .dedup()
+        .collect()
+}
 
 pub(crate) fn next_leader_tpu_vote(
     cluster_info: &ClusterInfo,
