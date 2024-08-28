ad6002f90ba8ed24ca2d7ca5e07c049498474e75
Lucas Ste <38472950+LucasSte@users.noreply.github.com>
2024-08-28 13:55:55 -0300

    Bump platform tools version (#2745)
    * Bump platform tools version
    
    * Remove unused struct

--- programs/sbf/rust/ro_modify/src/lib.rs
+++ programs/sbf/rust/ro_modify/src/lib.rs
@@ -39,22 +39,6 @@ struct SolAccountInfo {
     executable: bool,
 }
 
-/// Rust representation of C's SolSignerSeed
-#[derive(Debug)]
-#[repr(C)]
-struct SolSignerSeedC {
-    addr: u64,
-    len: u64,
-}
-
-/// Rust representation of C's SolSignerSeeds
-#[derive(Debug)]
-#[repr(C)]
-struct SolSignerSeedsC {
-    addr: u64,
-    len: u64,
-}
-
 const READONLY_ACCOUNTS: &[SolAccountInfo] = &[
     SolAccountInfo {
         is_signer: false,
--- sdk/sbf/scripts/install.sh
+++ sdk/sbf/scripts/install.sh
@@ -109,7 +109,7 @@ if [[ ! -e criterion-$version.md || ! -e criterion ]]; then
 fi
 
 # Install platform tools
-version=v1.42
+version=v1.43
 if [[ ! -e platform-tools-$version.md || ! -e platform-tools ]]; then
   (
     set -e
