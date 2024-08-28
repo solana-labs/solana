61d6a22a472f0adb0ac68c3b287a5d68a66ee7e4
Lijun Wang <83639177+lijunwangs@users.noreply.github.com>
2024-08-28 09:21:07 -0700

    Remove explicit stream finish (#2760)
    * remove finish as it is redaundant to stream drop
    
    * remove finish as it is redaundant to stream drop

--- quic-client/src/nonblocking/quic_client.rs
+++ quic-client/src/nonblocking/quic_client.rs
@@ -274,7 +274,6 @@ impl QuicClient {
         let mut send_stream = connection.open_uni().await?;
 
         send_stream.write_all(data).await?;
-        send_stream.finish().await?;
         Ok(())
     }
 
