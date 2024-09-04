d26a15aaab6bbe2eea557ec65dd40fc480e0b3f5
jeff washington <jeff.washington@solana.com>
2024-09-04 08:35:57 -0500

    min of 5M ancient storages

--- accounts-db/src/ancient_append_vecs.rs
+++ accounts-db/src/ancient_append_vecs.rs
@@ -614,7 +614,7 @@ impl AccountsDb {
             .saturating_sub(randoms as usize);
         // ideal storage size is total alive bytes of ancient storages / half of max ancient slots
         tuning.ideal_storage_size = NonZeroU64::new(
-            (infos.total_alive_bytes.0 / tuning.max_ancient_slots.max(1) as u64 * 2).max(1_000_000),
+            (infos.total_alive_bytes.0 / tuning.max_ancient_slots.max(1) as u64 * 2).max(5_000_000),
         )
         .unwrap();
         self.shrink_ancient_stats
