c9a4864eab6efec1787333fcf9741fa8fc8a70eb
Ursula <asp_woods_34@icloud.com>
2024-12-22 12:54:34 +0200

    fix: Fixed file search in the art/ directory

--- docs/convert-ascii-to-svg.sh
+++ docs/convert-ascii-to-svg.sh
@@ -1,12 +1,18 @@
 #!/usr/bin/env bash
 
-# Convert .bob and .msc files in docs/art to .svg files located where the
-# site build will find them.
+# Convert .bob and .msc files in docs/art to .svg and .png files
+# located where the site build will find them.
 
 set -e
 
 cd "$(dirname "$0")"
 output_dir=static/img
+
+# Check if the 'art' directory exists
+if [[ ! -d "art" ]]; then
+  echo "Directory 'art' does not exist."
+  exit 1
+fi
 
 svgbob_cli="$(command -v svgbob_cli || true)"
 if [[ -z "$svgbob_cli" ]]; then
@@ -16,12 +22,14 @@ fi
 
 mkdir -p "$output_dir"
 
+# Convert .bob files to .svg
 while read -r bob_file; do
   out_file=$(basename "${bob_file%.*}".svg)
   "$svgbob_cli" "$bob_file" --output "$output_dir/$out_file"
-done < <(find art/*.bob)
+done < <(find art -name "*.bob")
 
+# Convert .msc files to .png
 while read -r msc_file; do
   out_file=$(basename "${msc_file%.*}".png)
   mscgen -T png -o "$output_dir/$out_file" -i "$msc_file"
-done < <(find art/*.msc)
+done < <(find art -name "*.msc")
